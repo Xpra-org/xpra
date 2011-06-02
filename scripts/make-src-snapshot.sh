f32a9280b090a4eddde6a4474d63f2600e7b60d7
Antoine Martin <totaam@xpra.org>
2011-06-02 14:26:24 +0000

    ignore more generated files when creating src snapshot
    git-svn-id: https://xpra.org/svn/Xpra/trunk@70 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- scripts/make-src-snapshot.sh
+++ scripts/make-src-snapshot.sh
@@ -13,6 +13,6 @@ rm -fr "${DIR}/wimpiggy/lowlevel/constants.pxi"
 find ${DIR} -name ".svn" -exec rm -fr {} \;
 find ${DIR} -name ".pyc" -exec rm -fr {} \;
 
-tar -jcf ${DIR}.tar.bz2 ${DIR} --exclude install --exclude build --exclude dist --exclude Output --exclude deb --exclude MANIFEST --exclude xpra/wait_for_x_server.c --exclude wimpiggy/lowlevel/wimpiggy.lowlevel.bindings.dep
+tar -jcf ${DIR}.tar.bz2 ${DIR} --exclude install --exclude build --exclude dist --exclude Output --exclude deb --exclude MANIFEST --exclude xpra/wait_for_x_server.c --exclude wimpiggy/lowlevel/wimpiggy.lowlevel.bindings.dep --exclude wimpiggy/lowlevel/constants.pxi --exclude wimpiggy/lowlevel/bindings.c --exclude *pyc
 ls -al ${DIR}.tar.bz2
 rm -fr "${DIR}"
