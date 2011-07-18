01a3f3f737775eaaec6f27a0463f03d8d06f95d6
Antoine Martin <totaam@xpra.org>
2011-07-18 11:08:39 +0000

    we need to bundle some extra DLLs or it won't work machines which do not have those installed
    git-svn-id: https://xpra.org/svn/Xpra/trunk@100 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup-win-py2exe.py
+++ src/setup-win-py2exe.py
@@ -1,5 +1,20 @@
 from distutils.core import setup
 import py2exe
+import glob
+
+# These files cannot be re-distributed legally :(
+# So here is the md5sum so you can find the right version:
+# 6fda4c0ef8715eead5b8cec66512d3c8  Microsoft.VC90.CRT/Microsoft.VC90.CRT.manifest
+# 4a8bc195abdc93f0db5dab7f5093c52f  Microsoft.VC90.CRT/msvcm90.dll
+# 6de5c66e434a9c1729575763d891c6c2  Microsoft.VC90.CRT/msvcp90.dll
+# e7d91d008fe76423962b91c43c88e4eb  Microsoft.VC90.CRT/msvcr90.dll
+# f6a85f3b0e30c96c993c69da6da6079e  Microsoft.VC90.CRT/vcomp90.dll
+# 17683bda76942b55361049b226324be9  Microsoft.VC90.MFC/Microsoft.VC90.MFC.manifest
+# 462ddcc5eb88f34aed991416f8e354b2  Microsoft.VC90.MFC/mfc90.dll
+# b9030d821e099c79de1c9125b790e2da  Microsoft.VC90.MFC/mfc90u.dll
+# d4e7c1546cf3131b7d84b39f8da9e321  Microsoft.VC90.MFC/mfcm90.dll
+# 371226b8346f29011137c7aa9e93f2f6  Microsoft.VC90.MFC/mfcm90u.dll
+DLLs="Z:\\"
 
 setup(
     name = 'Xpra',
@@ -35,6 +50,9 @@ setup(
               },
 
     data_files=[
-                   'COPYING', 'website.url'
+                   ('COPYING'),
+                   ('website.url'),
+                   ('Microsoft.VC90.CRT', glob.glob('%s\\Microsoft.VC90.CRT\\*.*' % DLLs)),
+                   ('Microsoft.VC90.MFC', glob.glob('%s\\Microsoft.VC90.MFC\\*.*' % DLLs)),
                ]
 )
