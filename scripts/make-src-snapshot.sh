cdeed986011e8fa8ec0928db7c5ba44d23b314e1
Antoine Martin <totaam@xpra.org>
2011-07-20 16:12:21 +0000

    fix path and avoid warnings on dirs we delete
    git-svn-id: https://xpra.org/svn/Xpra/trunk@109 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- scripts/make-src-snapshot.sh
+++ scripts/make-src-snapshot.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 if [ -z "${VERSION}" ]; then
-	VERSION=`PYTHONPATH="./dev" python -c "from xpra import __version__; print __version__"`
+	VERSION=`PYTHONPATH="./src" python -c "from xpra import __version__; print __version__"`
 fi
 DIR=xpra-${VERSION}
 cp -apr src ${DIR}
@@ -10,7 +10,7 @@ rm -fr "${DIR}/build"
 rm -fr "${DIR}/install"
 rm -fr "${DIR}/wimpiggy/lowlevel/bindings.c"
 rm -fr "${DIR}/wimpiggy/lowlevel/constants.pxi"
-find ${DIR} -name ".svn" -exec rm -fr {} \;
+find ${DIR} -name ".svn" -exec rm -fr {} \; 2>&1 | grep -v "No such file or directory"
 find ${DIR} -name ".pyc" -exec rm -fr {} \;
 
 tar -jcf ${DIR}.tar.bz2 ${DIR} --exclude install --exclude build --exclude dist --exclude Output --exclude deb --exclude MANIFEST --exclude xpra/wait_for_x_server.c --exclude wimpiggy/lowlevel/wimpiggy.lowlevel.bindings.dep --exclude wimpiggy/lowlevel/constants.pxi --exclude wimpiggy/lowlevel/bindings.c --exclude *pyc
