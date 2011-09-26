d078fc94acd7efd33b92d6123dc74ab679ac11ea
Antoine Martin <totaam@xpra.org>
2011-09-26 09:40:50 +0000

    add missing copyright for all files I have changed significantly
    git-svn-id: https://xpra.org/svn/Xpra/trunk@195 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup-win-py2exe.py
+++ src/setup-win-py2exe.py
@@ -1,3 +1,8 @@
+# This file is part of Parti.
+# Copyright (C) 2010-2011 Antoine Martin <antoine@devloop.org.uk>
+# Parti is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
 from distutils.core import setup
 import py2exe
 import glob
