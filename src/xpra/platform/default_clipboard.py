25e0db008e6f1c07913c7c7f70b21395a9738f7a
Antoine Martin <totaam@xpra.org>
2011-10-20 10:08:42 +0000

    generalize the keyboard name translation so we can potentially change it on construction, or even at runtime?
    git-svn-id: https://xpra.org/svn/Xpra/trunk@242 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/default_clipboard.py
+++ src/xpra/platform/default_clipboard.py
@@ -6,23 +6,27 @@
 from xpra.platform.clipboard_base import ClipboardProtocolHelperBase
 
 class ClipboardProtocolHelper(ClipboardProtocolHelperBase):
-    """ This implementation of the clipboard helper only has one
-        type (aka "selection") of clipboard: "CLIPBOARD" and it
-        converts it to "PRIMARY" when conversing with the other end.
+    """
+        This implementation of the clipboard helper only has one
+        type (aka "selection") of clipboard ("CLIPBOARD" by default)
+        and it can convert it to another clipboard name ("PRIMARY")
+        when conversing with the other end.
         This is because the server implementation always uses the 3 X11
-        clipboards whereas some clients (win32) only have one.
-        This is used by MS Windows clients.
+        clipboards whereas some clients (MS Windows) only have "CLIPBOARD"
+        and we generally want to map it to X11's "PRIMARY"...
     """
 
-    def __init__(self, send_packet_cb):
-        ClipboardProtocolHelperBase.__init__(self, send_packet_cb, ["CLIPBOARD"])
+    def __init__(self, send_packet_cb, local_clipboard="CLIPBOARD", remote_clipboard="PRIMARY"):
+        self.local_clipboard = local_clipboard
+        self.remote_clipboard = remote_clipboard
+        ClipboardProtocolHelperBase.__init__(self, send_packet_cb, [local_clipboard])
 
     def local_to_remote(self, selection):
-        if selection=="CLIPBOARD":
-            return  "PRIMARY"
+        if selection==self.local_clipboard:
+            return  self.remote_clipboard
         return  selection
 
     def remote_to_local(self, selection):
-        if selection=="PRIMARY":
-            return  "CLIPBOARD"
+        if selection==self.remote_clipboard:
+            return  self.local_clipboard
         return  selection
