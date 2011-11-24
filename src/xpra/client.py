8166c1731600c1e8e11fc1becf07ad48a0abcd80
Antoine Martin <totaam@xpra.org>
2011-11-24 19:00:04 +0000

    use more shared memory so multiple big full-screen updates will fit
    git-svn-id: https://xpra.org/svn/Xpra/trunk@298 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client.py
+++ src/xpra/client.py
@@ -385,7 +385,7 @@ class XpraClient(gobject.GObject):
             #keep a reference to it so it does not disappear!
             self._mmap_temp_file = temp
             self.mmap_file = temp.name
-            self.mmap_size = mmap.PAGESIZE*1024*8   #generally 32MB
+            self.mmap_size = max(4096, mmap.PAGESIZE)*32   #generally 128MB
             fd = temp.file.fileno()
             log("using mmap file %s, fd=%s, size=%s", self.mmap_file, fd, self.mmap_size)
             os.lseek(fd, self.mmap_size-1, os.SEEK_SET)
