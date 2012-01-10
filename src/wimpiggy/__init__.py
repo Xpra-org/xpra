756598320cad6466ca8c83cd46526b8a57d9de99
Antoine Martin <totaam@xpra.org>
2012-01-10 21:37:40 +0000

    we need to be able to import xpra/__init__ from places which may not have a display, so cannot import gtk
    git-svn-id: https://xpra.org/svn/Xpra/trunk@420 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/wimpiggy/__init__.py
+++ src/wimpiggy/__init__.py
@@ -3,8 +3,5 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import pygtk
-pygtk.require('2.0')
-
 __version__ = "0.0.7.33"
 svn_revision="unknown"
