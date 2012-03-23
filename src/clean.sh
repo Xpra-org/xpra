58ddac83ede282c8c3d3e87b03638f7dc6445bdb
Antoine Martin <totaam@xpra.org>
2012-03-23 11:45:39 +0000

    was never meant to be committed - thanks eclipse (not)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@612 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/clean.sh
+++
@@ -1,4 +0,0 @@
-sudo rm -fr /usr/lib64/python2.7/site-packages/parti*
-sudo rm -fr /usr/lib64/python2.7/site-packages/xpra
-sudo rm -fr /usr/lib64/python2.7/site-packages/wimpiggy
-sudo rm -fr /usr/bin/xpra /usr/share/xpra
