eccaacfe8b55a655e9a705524b232a58a6054dbb
Arthur Huillet <arthur.huillet@agoctrl.org>
2012-04-04 13:50:48 +0000

    x264lib: swscale requires SWS_ACCURATE_RND to do accurate colorspace conversion... fix #95
    git-svn-id: https://xpra.org/svn/Xpra/trunk@703 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x264/x264lib.c
+++ src/xpra/x264/x264lib.c
@@ -57,7 +57,7 @@ struct x264lib_ctx *init_encoder(int width, int height)
 	ctx->encoder = x264_encoder_open(&param);
 	ctx->width = width;
 	ctx->height = height;
-	ctx->rgb2yuv = sws_getContext(ctx->width, ctx->height, PIX_FMT_RGB24, ctx->width, ctx->height, PIX_FMT_YUV420P, SWS_FAST_BILINEAR, NULL, NULL, NULL);
+	ctx->rgb2yuv = sws_getContext(ctx->width, ctx->height, PIX_FMT_RGB24, ctx->width, ctx->height, PIX_FMT_YUV420P, SWS_POINT, NULL, NULL, NULL);
 
 	return ctx;
 }
@@ -78,7 +78,7 @@ struct x264lib_ctx *init_decoder(int width, int height)
 	struct x264lib_ctx *ctx = malloc(sizeof(struct x264lib_ctx));
 	ctx->width = width;
 	ctx->height = height;
-	ctx->yuv2rgb = sws_getContext(ctx->width, ctx->height, PIX_FMT_YUV420P, ctx->width, ctx->height, PIX_FMT_RGB24, SWS_FAST_BILINEAR, NULL, NULL, NULL);
+	ctx->yuv2rgb = sws_getContext(ctx->width, ctx->height, PIX_FMT_YUV420P, ctx->width, ctx->height, PIX_FMT_RGB24, SWS_POINT | SWS_ACCURATE_RND, NULL, NULL, NULL);
 
     avcodec_register_all();
 
