fc49407904ff4c49b3320d69384d98eb2b5e21e9
Arthur Huillet <arthur.huillet@agoctrl.org>
2012-04-12 16:36:30 +0000

    x264lib: move testlib.c to strict C89 for MS VC compatibility
    git-svn-id: https://xpra.org/svn/Xpra/trunk@714 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x264/testlib.c
+++ src/xpra/x264/testlib.c
@@ -6,17 +6,19 @@
 
 int encode(const char *in_file, int w, int h, const char *out_file)
 {
+	uint8_t *out;
+	int i;
+	int sz;
+	FILE *dump;
 	struct x264lib_ctx *ctx = init_encoder(w, h);
 
 	uint8_t *b = malloc(w*h*3);
-	int i;
+
 
 	FILE *in = fopen(in_file, "r");
 	fread(b, w*h*3, 1, in);
 	fclose(in);
 
-	uint8_t *out;
-	int sz;
 	printf("Compressing image, size %d...", w*h*3);
 	if (compress_image(ctx, b, w*3, &out, &sz)) {
 		fprintf(stderr, "Error when compressing.\n");
@@ -25,7 +27,7 @@ int encode(const char *in_file, int w, int h, const char *out_file)
 
 	printf("after compressing %d bytes, ratio %f\n", sz, (float)sz/w*h*3);
 
-	FILE *dump = fopen(out_file, "w");
+	dump = fopen(out_file, "w");
 	fwrite(out, sz, 1, dump);
 	fclose(dump);
 
@@ -40,11 +42,11 @@ int decode(const char *in_file, int w, int h, const char *out_file)
 	uint8_t *out2;
 	int sz2;
 	int stride;
-	
 	struct x264lib_ctx *ctx = init_decoder(w, h);
+	FILE *fout;
 
 	uint8_t *b = malloc(w*h*3);
-	int i;
+
 
 	FILE *in = fopen(in_file, "r");
 	int sz = fread(b, 1, w*h*3, in);
@@ -54,7 +56,7 @@ int decode(const char *in_file, int w, int h, const char *out_file)
 	decompress_image(ctx, b, sz, &out2, &sz2, &stride);
 	printf("After decompressing, size %d, stride %d...\n", sz2, stride);
 
-	FILE *fout = fopen(out_file, "w");
+	fout = fopen(out_file, "w");
 	fwrite(out2, sz2, 1, fout);
 	fclose(fout);
 	return 0;
@@ -62,15 +64,18 @@ int decode(const char *in_file, int w, int h, const char *out_file)
 
 int main(int argc, char **argv)
 {
+	const char *input_file;
+	int width, height;
+	const char *output_file;
 	if (argc != 6) {
 		fprintf(stderr, "Usage: %s <encode|decode> <input_filename> <width> <height> <output_filename>\n", argv[0]);
 		return 1;
 	}
 
-	const char *input_file = argv[2];
-	int width = atoi(argv[3]);
-	int height = atoi(argv[4]);
-	const char *output_file = argv[5];
+	input_file = argv[2];
+	width = atoi(argv[3]);
+	height = atoi(argv[4]);
+	output_file = argv[5];
 
 	if (!strcmp(argv[1], "encode"))
 		return encode(input_file, width, height, output_file);
