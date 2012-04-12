478a133499948b2c4c6c720474f94c7b4b36d0b8
Arthur Huillet <arthur.huillet@agoctrl.org>
2012-04-12 15:31:57 +0000

    x264lib: update testlib.c
    git-svn-id: https://xpra.org/svn/Xpra/trunk@712 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x264/testlib.c
+++ src/xpra/x264/testlib.c
@@ -4,49 +4,77 @@
 #include <string.h>
 #include "x264lib.h"
 
-#define X 800
-#define Y 600
-#define totalSZ X*Y*3
-int main(int argc, char **argv)
+int encode(const char *in_file, int w, int h, const char *out_file)
 {
-	struct x264lib_ctx *ctx = init_encoder(X, Y);
+	struct x264lib_ctx *ctx = init_encoder(w, h);
 
-	uint8_t *b = malloc(totalSZ);
+	uint8_t *b = malloc(w*h*3);
 	int i;
 
-	FILE *in = fopen("x264test.rgb", "r");
-	fread(b, totalSZ, 1, in);
+	FILE *in = fopen(in_file, "r");
+	fread(b, w*h*3, 1, in);
 	fclose(in);
-
-	/*for (i = 0; i < totalSZ;) {
-		b[i++] = i;
-		b[i++] = 255-i;
-		b[i++] = 0;
-	}*/
 
 	uint8_t *out;
 	int sz;
-	printf("Compressing image, size %d...", totalSZ);
-	compress_image(ctx, b, X*3, &out, &sz);
-	printf("after compressing %d bytes, ratio %f\n", sz, (float)sz/totalSZ);
+	printf("Compressing image, size %d...", w*h*3);
+	if (compress_image(ctx, b, w*3, &out, &sz)) {
+		fprintf(stderr, "Error when compressing.\n");
+		return 1;
+	}
+
+	printf("after compressing %d bytes, ratio %f\n", sz, (float)sz/w*h*3);
+
+	FILE *dump = fopen(out_file, "w");
+	fwrite(out, sz, 1, dump);
+	fclose(dump);
 
 	clean_encoder(ctx);
 	free(ctx);
 
+	return 0;
+}
+
+int decode(const char *in_file, int w, int h, const char *out_file)
+{
 	uint8_t *out2;
 	int sz2;
-	ctx = init_decoder(X, Y);
 	int stride;
-	decompress_image(ctx, out, sz, &out2, &sz2, &stride);
-	printf("After decompressing, stride %d...", sz2);
-	if (sz2 != totalSZ) {
-		printf("size doesn't match original!\n");
-		return 1;
-	} else 
-		printf("OK\n");
+	
+	struct x264lib_ctx *ctx = init_decoder(w, h);
 
-	FILE *fout = fopen("decompressed.rgb", "w");
+	uint8_t *b = malloc(w*h*3);
+	int i;
+
+	FILE *in = fopen(in_file, "r");
+	int sz = fread(b, 1, w*h*3, in);
+	printf("Read %d bytes\n", sz);
+	fclose(in);
+
+	decompress_image(ctx, b, sz, &out2, &sz2, &stride);
+	printf("After decompressing, size %d, stride %d...\n", sz2, stride);
+
+	FILE *fout = fopen(out_file, "w");
 	fwrite(out2, sz2, 1, fout);
 	fclose(fout);
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 6) {
+		fprintf(stderr, "Usage: %s <encode|decode> <input_filename> <width> <height> <output_filename>\n", argv[0]);
+		return 1;
+	}
+
+	const char *input_file = argv[2];
+	int width = atoi(argv[3]);
+	int height = atoi(argv[4]);
+	const char *output_file = argv[5];
+
+	if (!strcmp(argv[1], "encode"))
+		return encode(input_file, width, height, output_file);
+	else return decode(input_file, width, height, output_file);
+
 	return 0;
 }
