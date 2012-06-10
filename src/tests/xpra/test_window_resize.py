b4d6a786be82205602de82e93e2c8a57dcbfdb6d
Antoine Martin <totaam@xpra.org>
2012-06-10 16:57:48 +0000

    #107 simple test for window resize events
    git-svn-id: https://xpra.org/svn/Xpra/trunk@913 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/test_window_resize.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import gtk 
+
+def change_callback(self, window, entry):
+	print("text=%s" % entry.get_text())
+	window.set_title(entry.get_text())
+
+width = 400
+height = 200
+def main():
+	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	window.set_size_request(width, height)
+	window.connect("delete_event", gtk.mainquit)
+	btn = gtk.Button("resize me")
+	def resize(*args):
+		global width, height
+		width = max(200, (width+20) % 600)
+		height = max(200, (height+20) % 400)
+		print("resizing to %s x %s" % (width, height))
+		window.resize(width, height)
+	btn.connect('clicked', resize)
+	window.add(btn)
+	window.show_all()
+	gtk.main()
+	return 0
+
+if __name__ == "__main__":
+	try:
+		main()
+	except Exception, e:
+		print("error: %s" % e)
+
