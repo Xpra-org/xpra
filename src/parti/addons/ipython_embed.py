3cf40f47d664408b21fd734da28ff2f1697b7b8f
Antoine Martin <totaam@xpra.org>
2012-06-10 10:41:25 +0000

    git-svn-id: https://xpra.org/svn/Xpra/trunk@911 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/parti/addons/ipython_embed.py
+++ src/parti/addons/ipython_embed.py
@@ -4,8 +4,23 @@
 # later version. See the file COPYING for details.
 
 import gtk
-from wimpiggy.pseudoclient import PseudoclientWindow
 from parti.addons.ipython_view import IPythonView
+
+class PseudoclientWindow(gtk.Window):
+    """A gtk.Window that acts like an ordinary client.
+
+    All the wm-magic that would normally accrue to a window created within our
+    process is removed.
+
+    Keyword arguments (notably 'type') are forwarded to
+    gtk.Window.__init__.
+
+    The reason this is a separate class, as opposed to say a gtk.Window
+    factory method on wm, is that this way allows for subclassing."""
+    
+    def __init__(self, wm, **kwargs):
+        gtk.Window.__init__(self, **kwargs)
+        wm._make_window_pseudoclient(self)
 
 def spawn_repl_window(wm, namespace):
     window = PseudoclientWindow(wm)
