99c32e4c4086658cb7164e746d901ed921c4d234
Antoine Martin <totaam@xpra.org>
2012-07-03 05:53:24 +0000

    distutils fix: first argument is path, not filename..
    git-svn-id: https://xpra.org/svn/Xpra/trunk@996 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -218,7 +218,7 @@ else:
                 ("share/wimpiggy", ["README.wimpiggy"]),
                 ("share/xpra/icons", glob.glob("icons/*")),
                 ("share/applications", ["xpra_launcher.desktop"]),
-                ("share/icons/xpra.png", ["xpra.png"])
+                ("share/icons", ["xpra.png"])
                 ]
     extra_options = dict(
         packages = packages,
