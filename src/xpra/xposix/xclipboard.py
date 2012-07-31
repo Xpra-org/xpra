ebf868dd9286217a47c3a665ffee8160d0a617c2
Antoine Martin <totaam@xpra.org>
2012-07-31 13:27:19 +0000

    GdkAtom is a long, when we were using ints we were just creating invalid longs from pairs of ints!
    git-svn-id: https://xpra.org/svn/Xpra/trunk@1228 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/xposix/xclipboard.py
+++ src/xpra/xposix/xclipboard.py
@@ -19,7 +19,7 @@ class ClipboardProtocolHelper(ClipboardProtocolHelperBase):
         ClipboardProtocolHelperBase.__init__(self, send_packet_cb, ["CLIPBOARD", "PRIMARY", "SECONDARY"])
 
     def _do_munge_raw_selection_to_wire(self, target, datatype, dataformat, data):
-        if format == 32 and type in ("ATOM", "ATOM_PAIR"):
+        if dataformat == 32 and datatype in ("ATOM", "ATOM_PAIR"):
             # Convert to strings and send that. Bizarrely, the atoms are
             # not actual X atoms, but an array of GdkAtom's reinterpreted
             # as a byte buffer.
@@ -38,5 +38,5 @@ class ClipboardProtocolHelper(ClipboardProtocolHelperBase):
             import gtk.gdk
             gdk_atoms = [gtk.gdk.atom_intern(a) for a in data]
             atom_array = gdk_atom_array_from_gdk_atom_objects(gdk_atoms)
-            return struct.pack("=" + "I" * len(atom_array), *atom_array)
+            return struct.pack("=" + "L" * len(atom_array), *atom_array)
         return ClipboardProtocolHelperBase._munge_wire_selection_to_raw(self, encoding, datatype, dataformat, data)
