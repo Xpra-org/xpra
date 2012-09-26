0e5463b5a8dc71a96e8f8c5042542a5d5aca9278
Antoine Martin <totaam@xpra.org>
2012-09-26 06:42:24 +0000

    use "txt" icon for dos commands and "ui" icon for launcher
    git-svn-id: https://xpra.org/svn/Xpra/trunk@1707 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -211,9 +211,9 @@ if sys.platform.startswith("win"):
     import py2exe    #@UnresolvedImport
     assert py2exe is not None
     windows = [
-                    {'script': 'win32/xpra_silent.py',                  'icon_resources': [(1, "win32/xpra.ico")],      "dest_base": "Xpra",},
+                    {'script': 'win32/xpra_silent.py',                  'icon_resources': [(1, "win32/xpra_txt.ico")],  "dest_base": "Xpra",},
                     {'script': 'xpra/gtk_view_keyboard.py',             'icon_resources': [(1, "win32/keyboard.ico")],  "dest_base": "GTK_Keyboard_Test",},
-                    {'script': 'xpra/scripts/client_launcher.py',       'icon_resources': [(1, "win32/xpra_txt.ico")],  "dest_base": "Xpra-Launcher",},
+                    {'script': 'xpra/scripts/client_launcher.py',       'icon_resources': [(1, "win32/xpra.ico")],      "dest_base": "Xpra-Launcher",},
               ]
     console = [
                     {'script': 'xpra/scripts/main.py',                  'icon_resources': [(1, "win32/xpra_txt.ico")],  "dest_base": "Xpra_cmd",}
