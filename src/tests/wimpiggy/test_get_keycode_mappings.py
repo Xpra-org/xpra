99ccd80252cbc3235ad86d76ab383983d96cff4e
Antoine Martin <totaam@xpra.org>
2012-10-05 18:40:28 +0000

    add new test to easily get keyboard info out of the wimpiggy cython functions and get better debug output
    git-svn-id: https://xpra.org/svn/Xpra/trunk@1848 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/wimpiggy/test_get_keycode_mappings.py
+++ src/tests/wimpiggy/test_get_keycode_mappings.py
@@ -4,14 +4,16 @@
 
 import pygtk
 pygtk.require('2.0')
+import gtk.gdk
 
-from wimpiggy.lowlevel import get_modifier_mappings		#@UnresolvedImport
+from wimpiggy.lowlevel import get_keycode_mappings		#@UnresolvedImport
 
 def main():
-	mappings = get_modifier_mappings()
+	mappings = get_keycode_mappings(gtk.gdk.get_default_root_window())
 	print("mappings=%s" % mappings)
 	print("")
 	for k,v in mappings.items():
 		print("%s\t\t:\t%s" % (k, v))
+
 if __name__ == "__main__":
 	main()
