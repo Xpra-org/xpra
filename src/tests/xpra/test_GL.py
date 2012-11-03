9de8dac2c293cfbab25832f43eaa07dfa4876e98
Antoine Martin <totaam@xpra.org>
2012-11-03 05:03:27 +0000

    format as a regular test with a main method
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2026 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_GL.py
+++ src/tests/xpra/test_GL.py
@@ -5,60 +5,55 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from gtk import gdk
+def main():
+    import pygtk
+    pygtk.require('2.0')
+    
+    import gtk.gdkgl, gtk.gtkgl         #@UnresolvedImport
+    assert gtk.gdkgl is not None and gtk.gtkgl is not None
+    
+    print("loading OpenGL.GL")
+    from OpenGL.GL import GL_VERSION, glGetString
+    from OpenGL.GL.ARB.fragment_program import glInitFragmentProgramARB
+    
+    glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB | gtk.gdkgl.MODE_DOUBLE | gtk.gdkgl.MODE_DEPTH)
+    print("glconfig=%s" % glconfig)
 
-import gtk.gdkgl, gtk.gtkgl         #@UnresolvedImport
-assert gtk.gdkgl is not None and gtk.gtkgl is not None
+    win = gtk.Window()
+    win.set_title('test GL')
 
-#trick to get py2exe to include what we need:
-import sys
-if sys.platform.startswith("win"):
-    print("has frozen: %s" % hasattr( sys, "frozen" ))
-    if hasattr( sys, "frozen" ):
-        print("frozen: %s" % sys.frozen)
-        if sys.frozen in ("windows_exe", "console_exe"):
-            import os
-            main_dir = os.path.dirname( unicode( sys.executable, sys.getfilesystemencoding() ) )
-            print("main_dir=%s" % main_dir)
-            sys.path.insert( 0, main_dir )
-            os.chdir( main_dir )
-        else:
-            sys.path.insert(0, ".")
-    from ctypes import util             #@UnusedImport
-    print("ctypes loaded")
-    from OpenGL.platform import win32   #@UnusedImport
-    print("OpenGL.platform loaded")
-    #import OpenGL_accelerate            #@UnusedImport @UnresolvedImport
+    glarea = gtk.gtkgl.DrawingArea(glconfig=glconfig, share_list=None, render_type=gtk.gdkgl.RGBA_TYPE)
+    win.add(glarea)
+    glarea.realize()
 
-print("loading OpenGL.GL")
-from OpenGL.GL import GL_VERSION, glGetString
-from OpenGL.GL.ARB.fragment_program import glInitFragmentProgramARB
+    glcontext = glarea.get_gl_context()
+    print("glcontext=%s" % glcontext)
 
-glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB | gtk.gdkgl.MODE_DOUBLE | gtk.gdkgl.MODE_DEPTH)
-print("glconfig=%s" % glconfig)
-pixmap = gdk.Pixmap(gdk.get_default_root_window(), 1, 1)
-print("pixmap=%s" % glconfig)
-glext = gtk.gdkgl.ext(pixmap)
-print("glext=%s" % glext)
-gldrawable = glext.set_gl_capability(glconfig)
-print("set_gl_capability gldrawable=%s" % gldrawable)
-gldrawable = gtk.gdkgl.Pixmap(glconfig, pixmap)
-print("gldrawable=%s" % gldrawable)
-if gldrawable is None:
-    raise ImportError("failed to initialize")
-glcontext = gtk.gdkgl.Context(gldrawable, direct=True)
-if not gldrawable.gl_begin(glcontext):
-    raise ImportError("gl_begin failed on %s" % gldrawable)
-try:
-    gl_major = int(glGetString(GL_VERSION)[0])
-    gl_minor = int(glGetString(GL_VERSION)[2])
-    if gl_major<=1 and gl_minor<1:
-        raise ImportError("** OpenGL output requires OpenGL version 1.1 or greater, not %s.%s" % (gl_major, gl_minor))
-    print("found valid OpenGL: %s.%s" % (gl_major, gl_minor))
-    #this allows us to do CSC via OpenGL:
-    #see http://www.opengl.org/registry/specs/ARB/fragment_program.txt
-    if not glInitFragmentProgramARB():
-        raise ImportError("** OpenGL output requires glInitFragmentProgramARB")
-finally:
-    gldrawable.gl_end()
-    del glcontext, gldrawable, glext, glconfig
+    pixmap = gtk.gdk.Pixmap(None, 120, 120, 24)
+    print("pixmap=%s" % pixmap)
+    glext = gtk.gdkgl.ext(pixmap)
+    print("glext=%s" % glext)
+    gldrawable = gtk.gdkgl.Pixmap(glconfig, pixmap)
+    print("gldrawable=%s" % gldrawable)
+    if gldrawable is None:
+        raise ImportError("failed to initialize")
+    glcontext = gtk.gdkgl.Context(gldrawable, direct=True)
+    if not gldrawable.gl_begin(glcontext):
+        raise ImportError("gl_begin failed on %s" % gldrawable)
+    try:
+        gl_major = int(glGetString(GL_VERSION)[0])
+        gl_minor = int(glGetString(GL_VERSION)[2])
+        if gl_major<=1 and gl_minor<1:
+            raise ImportError("** OpenGL output requires OpenGL version 1.1 or greater, not %s.%s" % (gl_major, gl_minor))
+        print("found valid OpenGL: %s.%s" % (gl_major, gl_minor))
+        #this allows us to do CSC via OpenGL:
+        #see http://www.opengl.org/registry/specs/ARB/fragment_program.txt
+        if not glInitFragmentProgramARB():
+            raise ImportError("** OpenGL output requires glInitFragmentProgramARB")
+    finally:
+        gldrawable.gl_end()
+        del glcontext, gldrawable, glext, glconfig
+    
+
+if __name__ == "__main__":
+    main()
