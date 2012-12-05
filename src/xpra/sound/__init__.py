581aa840d33ed7d30691714cc6f3cda983295e71
Antoine Martin <totaam@xpra.org>
2012-12-05 16:43:32 +0000

    oops: cut&paste without looking - bad!
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2172 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/sound/__init__.py
+++ src/xpra/sound/__init__.py
@@ -1,20 +1,5 @@
+#!/usr/bin/env python
 # This file is part of Parti.
-# Copyright (C) 2010 Nathaniel Smith <njs@pobox.com>
+# Copyright (C) 2010-2012 Antoine Martin <antoine@devloop.org.uk>
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
-
-# Platform-specific code for Win32.
-
-XPRA_LOCAL_SERVERS_SUPPORTED = False
-import os
-os.environ["PLINK_PROTOCOL"] = "ssh"
-DEFAULT_SSH_CMD = "plink"
-GOT_PASSWORD_PROMPT_SUGGESTION = \
-   'Perhaps you need to set up Pageant, or (less secure) use --ssh="plink -pw YOUR-PASSWORD"?\n'
-
-def add_client_options(parser):
-    from xpra.platform import add_notray_option
-    add_notray_option(parser, ", this will also disable notifications!")
-
-def get_machine_id():
-    return  ""
