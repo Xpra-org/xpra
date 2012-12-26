8b8cf1c106eedc63f3fe97fa0ac4324171ea3007
Antoine Martin <totaam@xpra.org>
2012-12-26 10:05:44 +0000

    we only require version 1.1, and we test for the extension separately.
    Also simplify the check_support method so we will raise an exception directly from there (rather than rethrow it and lose the original line number)
    
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2368 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/gl/gl_check.py
+++ src/xpra/gl/gl_check.py
@@ -4,9 +4,11 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import sys, os
 from wimpiggy.log import Logger
 log = Logger()
 
+from gtk import gdk
 import gtk.gdkgl, gtk.gtkgl         #@UnresolvedImport
 assert gtk.gdkgl is not None and gtk.gtkgl is not None
 
@@ -21,9 +23,10 @@ def check_GL_support(gldrawable, glcontext):
         from OpenGL.GL import glGetString
         gl_major = int(glGetString(GL_VERSION)[0])
         gl_minor = int(glGetString(GL_VERSION)[2])
-        MIN_VERSION = (1,3)
-        if (gl_major, gl_minor) <= MIN_VERSION:
-            raise ImportError("** OpenGL output requires OpenGL version %s or greater, not %s.%s" % (".".join(MIN_VERSION), gl_major, gl_minor))
+        MIN_VERSION = (1,1)
+        if (gl_major, gl_minor) < MIN_VERSION:
+            raise ImportError("** OpenGL output requires OpenGL version %s or greater, not %s.%s" %
+                              (".".join([str(x) for x in MIN_VERSION]), gl_major, gl_minor))
         log("found valid OpenGL version: %s.%s", gl_major, gl_minor)
 
         #this allows us to do CSC via OpenGL:
@@ -35,43 +38,39 @@ def check_GL_support(gldrawable, glcontext):
         gldrawable.gl_end()
 
 def check_support():
-    try:
-        from gtk import gdk
-        assert gtk.gdkgl.query_extension()
-    
-        #tricks to get py2exe to include what we need / load it from its unusual path:
-        import sys, os
-        if sys.platform.startswith("win"):
-            log("has frozen: %s", hasattr(sys, "frozen"))
-            if hasattr(sys, "frozen"):
-                log("found frozen path: %s", sys.frozen)
-                if sys.frozen in ("windows_exe", "console_exe"):
-                    main_dir = os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding()))
-                    log("main_dir=%s", main_dir)
-                    sys.path.insert(0, main_dir)
-                    os.chdir(main_dir)
-                else:
-                    sys.path.insert(0, ".")
-            #FIXME: ugly win32 hack for getting a drawable and context, we must use a window...
-            w = gtk.Window()
-            glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
-            glarea = gtk.gtkgl.DrawingArea(glconfig)
-            glarea.show()
-            w.add(glarea)
-            w.show()
-            gldrawable = glarea.get_gl_drawable()
-            glcontext = glarea.get_gl_context()
-            try:
-                check_GL_support(gldrawable, glcontext)
-            finally:
-                w.destroy()
-            del glcontext, gldrawable, glconfig
-        else:
-            glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
-            glext = gtk.gdkgl.ext(gdk.Pixmap(gdk.get_default_root_window(), 1, 1))
-            gldrawable = glext.set_gl_capability(glconfig)
-            glcontext = gtk.gdkgl.Context(gldrawable, direct=True)
+    assert gtk.gdkgl.query_extension()
+    #tricks to get py2exe to include what we need / load it from its unusual path:
+    if sys.platform.startswith("win"):
+        #This is supposed to help py2exe:
+        from OpenGL.platform import win32   #@UnusedImport
+        log("has frozen: %s", hasattr(sys, "frozen"))
+        if hasattr(sys, "frozen"):
+            log("found frozen path: %s", sys.frozen)
+            if sys.frozen in ("windows_exe", "console_exe"):
+                main_dir = os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding()))
+                log("main_dir=%s", main_dir)
+                sys.path.insert(0, main_dir)
+                os.chdir(main_dir)
+            else:
+                sys.path.insert(0, ".")
+        #FIXME: ugly win32 hack for getting a drawable and context, we must use a window...
+        w = gtk.Window()
+        glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
+        glarea = gtk.gtkgl.DrawingArea(glconfig)
+        glarea.show()
+        w.add(glarea)
+        w.show()
+        gldrawable = glarea.get_gl_drawable()
+        glcontext = glarea.get_gl_context()
+        try:
             check_GL_support(gldrawable, glcontext)
-            del glcontext, gldrawable, glext, glconfig
-    except Exception, e:
-        raise ImportError("** OpenGL initialization error: %s" % e)
+        finally:
+            w.destroy()
+        del glcontext, gldrawable, glconfig
+    else:
+        glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
+        glext = gtk.gdkgl.ext(gdk.Pixmap(gdk.get_default_root_window(), 1, 1))
+        gldrawable = glext.set_gl_capability(glconfig)
+        glcontext = gtk.gdkgl.Context(gldrawable, direct=True)
+        check_GL_support(gldrawable, glcontext)
+        del glcontext, gldrawable, glext, glconfig
