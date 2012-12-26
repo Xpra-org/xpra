47a51006d155adf2a12a46c1d53022621bc3324f
Antoine Martin <totaam@xpra.org>
2012-12-26 11:03:17 +0000

    move all gtkgl code into check_support and only leave pyopengl import and code in check_GL_support. Also use double buffered by default, seems to work better on win32 (is a crash better than no support?)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2370 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/gl/gl_check.py
+++ src/xpra/gl/gl_check.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # This file is part of Parti.
 # Copyright (C) 2012 Serviware (Arthur Huillet, <ahuillet@serviware.com>)
 # Copyright (C) 2012 Antoine Martin <antoine@devloop.org.uk>
@@ -8,18 +9,12 @@ import sys, os
 from wimpiggy.log import Logger
 log = Logger()
 
-from gtk import gdk
-import gtk.gdkgl, gtk.gtkgl         #@UnresolvedImport
-assert gtk.gdkgl is not None and gtk.gtkgl is not None
-
 #sanity checks: OpenGL version and fragment program support:
 def check_GL_support(gldrawable, glcontext):
     if not gldrawable.gl_begin(glcontext):
         raise ImportError("gl_begin failed on %s" % gldrawable)
-    log("pygdkglext version=%s", gtk.gdkgl.pygdkglext_version)
-    log("pygdkglext OpenGL version=%s", gtk.gdkgl.query_version())
     try:
-        from OpenGL.GL import GL_VERSION
+        from OpenGL.GL import GL_VERSION, GL_EXTENSIONS
         from OpenGL.GL import glGetString
         gl_major = int(glGetString(GL_VERSION)[0])
         gl_minor = int(glGetString(GL_VERSION)[2])
@@ -28,6 +23,7 @@ def check_GL_support(gldrawable, glcontext):
             raise ImportError("** OpenGL output requires OpenGL version %s or greater, not %s.%s" %
                               (".".join([str(x) for x in MIN_VERSION]), gl_major, gl_minor))
         log("found valid OpenGL version: %s.%s", gl_major, gl_minor)
+        log("OpenGL extensions found: %s", glGetString(GL_EXTENSIONS))
 
         #this allows us to do CSC via OpenGL:
         #see http://www.opengl.org/registry/specs/ARB/fragment_program.txt
@@ -38,12 +34,9 @@ def check_GL_support(gldrawable, glcontext):
         gldrawable.gl_end()
 
 def check_support():
-    assert gtk.gdkgl.query_extension()
     #tricks to get py2exe to include what we need / load it from its unusual path:
     if sys.platform.startswith("win"):
-        #This is supposed to help py2exe:
-        from OpenGL.platform import win32   #@UnusedImport
-        log("has frozen: %s", hasattr(sys, "frozen"))
+        log("is frozen: %s", hasattr(sys, "frozen"))
         if hasattr(sys, "frozen"):
             log("found frozen path: %s", sys.frozen)
             if sys.frozen in ("windows_exe", "console_exe"):
@@ -53,9 +46,25 @@ def check_support():
                 os.chdir(main_dir)
             else:
                 sys.path.insert(0, ".")
+        #This is supposed to help py2exe (after we setup the path):
+        from OpenGL.platform import win32   #@UnusedImport
+
+    from gtk import gdk
+    import gtk.gdkgl, gtk.gtkgl         #@UnresolvedImport
+    assert gtk.gdkgl is not None and gtk.gtkgl is not None
+    log("pygdkglext version=%s", gtk.gdkgl.pygdkglext_version)
+    log("pygdkglext OpenGL version=%s", gtk.gdkgl.query_version())
+    display_mode = (gtk.gdkgl.MODE_RGB | gtk.gdkgl.MODE_DEPTH | gtk.gdkgl.MODE_DOUBLE)
+    try:
+        glconfig = gtk.gdkgl.Config(mode=display_mode)
+    except gtk.gdkgl.NoMatches:
+        display_mode &= ~gtk.gdkgl.MODE_DOUBLE
+        glconfig = gtk.gdkgl.Config(mode=display_mode)
+    log("using display mode: %s", display_mode)
+    assert gtk.gdkgl.query_extension()
+    if sys.platform.startswith("win"):
         #FIXME: ugly win32 hack for getting a drawable and context, we must use a window...
         w = gtk.Window()
-        glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
         glarea = gtk.gtkgl.DrawingArea(glconfig)
         glarea.show()
         w.add(glarea)
@@ -66,11 +75,23 @@ def check_support():
             check_GL_support(gldrawable, glcontext)
         finally:
             w.destroy()
-        del glcontext, gldrawable, glconfig
+            del glcontext, gldrawable, glconfig
     else:
-        glconfig = gtk.gdkgl.Config(mode=gtk.gdkgl.MODE_RGB|gtk.gdkgl.MODE_SINGLE)
         glext = gtk.gdkgl.ext(gdk.Pixmap(gdk.get_default_root_window(), 1, 1))
         gldrawable = glext.set_gl_capability(glconfig)
         glcontext = gtk.gdkgl.Context(gldrawable, direct=True)
-        check_GL_support(gldrawable, glcontext)
-        del glcontext, gldrawable, glext, glconfig
+        try:
+            check_GL_support(gldrawable, glcontext)
+        finally:
+            del glcontext, gldrawable, glext, glconfig
+
+
+def main():
+    import logging
+    logging.basicConfig(format="%(asctime)s %(message)s")
+    logging.root.setLevel(logging.DEBUG)
+    check_support()
+
+
+if __name__ == "__main__":
+    main()
