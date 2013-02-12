3f899c6b016750025121dd4d0e90f7359df4b793
Antoine Martin <totaam@xpra.org>
2013-02-12 16:29:26 +0000

    merge changes from upstream rencode 1.0.2 (mostly python3 annoyances)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2727 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/rencode/__init__.py
+++ src/xpra/rencode/__init__.py
@@ -1,16 +1,10 @@
-# This file is part of Parti.
-# Copyright (C) 2010 Andrew Resch <andrewresch@gmail.com>
-# Copyright (C) 2012, 2013 Antoine Martin <antoine@devloop.org.uk>
-# Parti is released under the terms of the GNU GPL v2, or, at your option, any
-# later version. See the file COPYING for details.
-
 try:
-    from xpra.rencode._rencode import *
+    from rencode._rencode import *
 except ImportError:
-    import xpra.rencode.rencode_orig
-    prev_all = xpra.rencode.rencode_orig.__all__[:]
-    del xpra.rencode.rencode_orig.__all__
-    from xpra.rencode.rencode_orig import *
-    xpra.rencode.rencode_orig.__all__ = prev_all
+    import rencode.rencode_orig
+    prev_all = rencode.rencode_orig.__all__[:]
+    del rencode.rencode_orig.__all__
+    from rencode.rencode_orig import *
+    rencode.rencode_orig.__all__ = prev_all
 
 __all__ = ['dumps', 'loads']
--- src/xpra/rencode/rencode.pyx
+++ src/xpra/rencode/rencode.pyx
@@ -22,11 +22,21 @@
 #     Boston, MA  02110-1301, USA.
 #
 
+import sys
+
+py3 = False
+if sys.version_info.major >= 3:
+    py3 = True
+    unicode = str
+
 from cpython cimport bool
 from libc.stdlib cimport realloc, malloc, free
 from libc.string cimport memcpy
 
+__version__ = "1.0.2"
+
 cdef long long data_length = 0
+cdef bool _decode_utf8 = False
 
 # Determine host byte-order
 cdef bool big_endian = False
@@ -212,6 +222,8 @@ cdef encode_str(char **buf, int *pos, bytes x):
         write_buffer(buf, pos, <char *>x, lx)
     else:
         s = str(lx) + ":"
+        if py3:
+            s = s.encode("ascii")
         p = s
         write_buffer(buf, pos, p, len(s))
         write_buffer(buf, pos, <char *>x, lx)
@@ -239,21 +251,19 @@ cdef encode_list(char **buf, int *pos, x):
 cdef encode_dict(char **buf, int *pos, x):
     if len(x) < DICT_FIXED_COUNT:
         write_buffer_char(buf, pos, DICT_FIXED_START + len(x))
-        for k, v in x.iteritems():
+        for k, v in x.items():
             encode(buf, pos, k)
             encode(buf, pos, v)
     else:
         write_buffer_char(buf, pos, CHR_DICT)
-        for k, v in x.iteritems():
+        for k, v in x.items():
             encode(buf, pos, k)
             encode(buf, pos, v)
         write_buffer_char(buf, pos, CHR_TERM)
 
-from types import StringType, IntType, LongType, DictType, ListType, TupleType, FloatType, NoneType, UnicodeType, BooleanType
-
 cdef encode(char **buf, int *pos, data):
     t = type(data)
-    if t == IntType or t == LongType:
+    if t == int or t == long:
         if -128 <= data < 128:
             encode_char(buf, pos, data)
         elif -32768 <= data < 32768:
@@ -264,10 +274,12 @@ cdef encode(char **buf, int *pos, data):
             encode_long_long(buf, pos, data)
         else:
             s = str(data)
+            if py3:
+                s = s.encode("ascii")
             if len(s) >= MAX_INT_LENGTH:
                 raise ValueError("Number is longer than %d characters" % MAX_INT_LENGTH)
             encode_big_number(buf, pos, s)
-    elif t == FloatType:
+    elif t == float:
         if _float_bits == 32:
             encode_float32(buf, pos, data)
         elif _float_bits == 64:
@@ -275,23 +287,23 @@ cdef encode(char **buf, int *pos, data):
         else:
             raise ValueError('Float bits (%d) is not 32 or 64' % _float_bits)
 
-    elif t == StringType:
+    elif t == bytes:
         encode_str(buf, pos, data)
 
-    elif t == UnicodeType:
+    elif t == unicode:
         u = data.encode("utf8")
         encode_str(buf, pos, u)
 
-    elif t == NoneType:
+    elif t == type(None):
         encode_none(buf, pos)
 
-    elif t == BooleanType:
+    elif t == bool:
         encode_bool(buf, pos, data)
 
-    elif t == ListType or t == TupleType:
+    elif t == list or t == tuple:
         encode_list(buf, pos, data)
 
-    elif t == DictType:
+    elif t == dict:
         encode_dict(buf, pos, data)
 
 
@@ -458,9 +470,15 @@ cdef decode(char *data, int *pos):
     elif typecode == CHR_FLOAT64:
         return decode_float64(data, pos)
     elif STR_FIXED_START <= typecode < STR_FIXED_START + STR_FIXED_COUNT:
-        return decode_fixed_str(data, pos)
+        s = decode_fixed_str(data, pos)
+        if _decode_utf8:
+            s = s.decode("utf8")
+        return s
     elif 49 <= typecode <= 57:
-        return decode_str(data, pos)
+        s = decode_str(data, pos)
+        if _decode_utf8:
+            s = s.decode("utf8")
+        return s
     elif typecode == CHR_NONE:
         pos[0] += 1
         return None
@@ -479,15 +497,20 @@ cdef decode(char *data, int *pos):
     elif typecode == CHR_DICT:
         return decode_dict(data, pos)
 
-def loads(data):
+def loads(data, decode_utf8=False):
     """
     Decodes the string into an object
 
     :param data: the string to decode
     :type data: string
+    :param decode_utf8: if True, will attempt to decode all str into unicode
+                        objects using utf8
+    :type decode_utf8: bool
 
     """
     cdef int pos = 0        #@DuplicatedSignature
     global data_length
     data_length = len(data)
+    global _decode_utf8
+    _decode_utf8=decode_utf8
     return decode(data, &pos)
