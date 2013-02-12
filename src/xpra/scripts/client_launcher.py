60bda3d6913fec8c8512066f00e18a6a6f83eda8
Antoine Martin <totaam@xpra.org>
2013-02-12 15:49:19 +0000

    oops: gdk colors are 16 bit per channel
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2724 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/client_launcher.py
+++ src/xpra/scripts/client_launcher.py
@@ -349,7 +349,7 @@ class ApplicationWindow:
 		self.window.set_default_size(400, 300)
 		self.window.set_border_width(20)
 		self.window.set_title(APPLICATION_NAME)
-		self.window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=255, green=255, blue=255))
+		self.window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535, green=65535, blue=65535))
 		icon_pixbuf = get_icon("xpra.png")
 		if icon_pixbuf:
 			self.window.set_icon(icon_pixbuf)
