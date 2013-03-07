b04c8aa14517185fc03b9a56413022ec26f8ea9e
Antoine Martin <totaam@xpra.org>
2013-03-07 10:54:27 +0000

    remove compositor warning - seems to work ok now!?
    git-svn-id: https://xpra.org/svn/Xpra/trunk@2896 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/shadow_server.py
+++ src/xpra/shadow_server.py
@@ -4,8 +4,6 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import gtk.gdk
-
 from wimpiggy.log import Logger
 log = Logger()
 
@@ -13,29 +11,11 @@ from xpra.x11_server_base import X11ServerBase
 from xpra.shadow_server_base import ShadowServerBase
 
 
-def has_compositor(display):
-    try:
-        from wimpiggy.lowlevel.bindings import const,myGetSelectionOwner        #@UnresolvedImport
-        from wimpiggy.error import trap, XError                                 #@UnresolvedImport
-    except Exception, e:
-        raise Exception("failed to load bindings: %s" % e)
-    try:
-        owner = trap.call_synced(myGetSelectionOwner, display, "_NET_WM_CM_S0")
-        log("compositor: %s", owner)
-        return owner and owner!=const["XNone"]
-    except XError, e:
-        raise Exception("error testing for compositing window manager: %s" % e)
-
-
 class XpraX11ShadowServer(ShadowServerBase, X11ServerBase):
 
     def __init__(self, sockets, opts):
         ShadowServerBase.__init__(self)
         X11ServerBase.__init__(self, True, sockets, opts)
-        display = gtk.gdk.display_get_default()
-        log("display=%s", display)
-        if display.supports_composite() and has_compositor(display):
-            log.warn("display %s uses a compositing window manager, shadowing may not work as expected - sorry" % display.get_name())
 
     def _process_mouse_common(self, proto, wid, pointer, modifiers):
         #adjust pointer position for offset in client:
