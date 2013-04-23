37914947cd09b1a3c5dc7021ae1479b02f37c18a
Antoine Martin <totaam@xpra.org>
2013-04-23 08:51:19 +0000

    disable OpenGL on win32 until we can figure out how to make double-buffering work reliably
    git-svn-id: https://xpra.org/svn/Xpra/trunk@3131 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/config.py
+++ src/xpra/scripts/config.py
@@ -85,9 +85,12 @@ except:
     pass
 
 def OpenGL_safety_check():
-    if not sys.platform.startswith("win"):
-        #on other platforms, we can probe OpenGL later safely
-        return None
+    if sys.platform.startswith("win"):
+        #at the moment, we can't do double-buffered without some visual
+        #corruption, and win32 only supports double-buffered contexts
+        #so we cannot enable GL on win32!
+        return "not supported on MS Windows"
+    #FIXME: if we fix OpenGL win32 support, the code below may still be useful:
     #try to detect VirtualBox:
     #based on the code found here:
     #http://spth.virii.lu/eof2/articles/WarGame/vboxdetect.html
