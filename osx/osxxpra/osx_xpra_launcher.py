7bcdec2ee26d7208e4a587ffc76df1bf08d198a7
Antoine Martin <totaam@xpra.org>
2013-05-08 17:10:01 +0000

    force py2app to include everything from platform/darwin
    git-svn-id: https://xpra.org/svn/Xpra/trunk@3329 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- osx/osxxpra/osx_xpra_launcher.py
+++ osx/osxxpra/osx_xpra_launcher.py
@@ -2,4 +2,14 @@
 
 from xpra.client.gtk_base.client_launcher import main
 
+#force py2app to include all those:
+from xpra.platform.darwin.shadow_server import ShadowServer
+from xpra.platform.darwin.features import CAN_DAEMONIZE
+from xpra.platform.darwin.gui import get_OSXApplication
+from xpra.platform.darwin.keyboard import Keyboard
+from xpra.platform.darwin.options import add_client_options
+from xpra.platform.darwin.osx_menu import getOSXMenu
+from xpra.platform.darwin.osx_tray import OSXTray
+from xpra.platform.darwin.paths import get_resources_dir
+
 main()
