c417afdd9fb76ce7f5dfe3ca5833aa9d7417f8ce
Antoine Martin <totaam@xpra.org>
2013-05-16 06:20:47 +0000

    accept rgb32 encoding for tray
    git-svn-id: https://xpra.org/svn/Xpra/trunk@3396 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/client_tray.py
+++ src/xpra/client/gtk_base/client_tray.py
@@ -129,7 +129,7 @@ class ClientTray(ClientWidgetBase):
                                       (x, y), modifiers])
 
     def draw_region(self, x, y, width, height, coding, img_data, rowstride, packet_sequence, options, callbacks):
-        assert coding in ("rgb24", "png", "mmap"), "invalid encoding for tray data: %s" % coding
+        assert coding in ("rgb24", "rgb32", "png", "mmap"), "invalid encoding for tray data: %s" % coding
         log.info("ClientTray.draw_region(%s)", [x, y, width, height, coding, "%s bytes" % len(img_data), rowstride, packet_sequence, options, callbacks])
         if coding=="png" and False:
             colorspace = self.tray_widget.get_screen().get_rgba_colormap()
