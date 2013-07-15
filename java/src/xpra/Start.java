a2ada46c54be269c679a9ec8e1e73597761b8c15
Antoine Martin <totaam@xpra.org>
2013-07-15 03:46:41 +0000

    fix warning: close resource
    git-svn-id: https://xpra.org/svn/Xpra/trunk@3850 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- java/src/xpra/Start.java
+++ java/src/xpra/Start.java
@@ -11,11 +11,18 @@ public abstract class Start {
 	public static final int DEFAULT_PORT = 10000;
 
 	public void run(String[] args) throws IOException {
-		Socket socket = new Socket(DEFAULT_HOST, DEFAULT_PORT);
-		socket.setKeepAlive(true);
-		InputStream is = socket.getInputStream();
-		OutputStream os = socket.getOutputStream();
-		this.makeClient(is, os).run(args);
+		Socket socket = null;
+		try {
+			socket = new Socket(DEFAULT_HOST, DEFAULT_PORT);
+			socket.setKeepAlive(true);
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+			this.makeClient(is, os).run(args);
+		}
+		finally {
+			if (socket!=null)
+				socket.close();
+		}
 	}
 
 	public abstract AbstractClient makeClient(InputStream is, OutputStream os);
