e9f750b5a80a3bcdff5bf2e4968b5fdd280fb09e
Antoine Martin <totaam@xpra.org>
2013-08-23 09:19:08 +0000

    this somehow fixes the server on freebsd, see #420
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4217 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/server.py
+++ src/xpra/scripts/server.py
@@ -9,6 +9,7 @@
 #  http://lists.partiwm.org/pipermail/parti-discuss/2008-September/000042.html
 # (also do not import anything that imports gtk)
 import gobject
+import glib
 import subprocess
 import sys
 import os.path
@@ -451,7 +452,7 @@ def run_server(parser, opts, mode, xpra_file, extra_args):
         # Now we can safely load gtk and connect:
         assert "gtk" not in sys.modules
         import gtk.gdk          #@Reimport
-        gtk.gdk.threads_init()
+        glib.threads_init()
         display = gtk.gdk.Display(display_name)
         manager = gtk.gdk.display_manager_get()
         default_display = manager.get_default_display()
