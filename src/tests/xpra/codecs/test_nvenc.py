bb407aaaba41c0332cd8b8e141d629633244133c
Antoine Martin <totaam@xpra.org>
2013-09-18 10:56:12 +0000

    add test to see how many contexts we can have on each GPU
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4352 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/codecs/test_nvenc.py
+++ src/tests/xpra/codecs/test_nvenc.py
@@ -7,7 +7,8 @@
 import sys
 import threading
 from tests.xpra.codecs.test_encoder import test_encoder, gen_src_images, do_test_encoder
-TEST_DIMENSIONS = ((32, 32), (1920, 1080), (512, 512))
+#TEST_DIMENSIONS = ((32, 32), (1920, 1080), (512, 512))
+TEST_DIMENSIONS = ((1920, 1080), (512, 512), (32, 32))
 
 def test_encode_one():
     from xpra.codecs.nvenc import encoder   #@UnresolvedImport
@@ -29,6 +30,35 @@ def test_encode_all_GPUs():
         print("**********************************")
         print("testing on  %s : %s" % (device_id, info))
         test_encoder(encoder, options, TEST_DIMENSIONS)
+    print("")
+
+def test_context_limits():
+    #figure out how many contexts we can have on each card:
+    from xpra.codecs.nvenc import encoder as encoder_module   #@UnresolvedImport
+    cuda_devices = encoder_module.get_cuda_devices()
+    ec = getattr(encoder_module, "Encoder")
+    MAX_ENCODER_CONTEXTS_PER_DEVICE = 32
+    print("")
+    for w,h in TEST_DIMENSIONS:
+        print("test_context_limits() %sx%s" % (w, h))
+        src_format = encoder_module.get_colorspaces()[0]
+        for device_id, device_info in cuda_devices.items():
+            options = {"cuda_device" : device_id}
+            encoders = []
+            for i in range(MAX_ENCODER_CONTEXTS_PER_DEVICE):
+                e = ec()
+                try:
+                    e.init_context(w, h, src_format, 20, 0, options)
+                    encoders.append(e)
+                except Exception, e:
+                    print("failed to created context %s on %s: %s" % (i, device_info, e))
+                    break
+            print("device %s managed %s contexts at %sx%s" % (device_info, len(encoders), w, h))
+            for encoder in encoders:
+                try:
+                    encoder.clean()
+                except Exception, e:
+                    print("encoder cleanup error: %s" % e)
     print("")
 
 def test_parallel_encode():
@@ -76,6 +106,7 @@ def test_parallel_encode():
     print("all threads ended")
     for _, encoder, _ in encoders:
         encoder.clean()
+    print("")
 
 
 def encoding_thread(encoder, src_format, w, h, images, info):
@@ -88,8 +119,9 @@ def main():
     logging.root.setLevel(logging.INFO)
     logging.root.addHandler(logging.StreamHandler(sys.stdout))
     print("main()")
-    #test_encode_one()
-    #test_encode_all_GPUs()
+    test_encode_one()
+    test_encode_all_GPUs()
+    test_context_limits()
     test_parallel_encode()
 
 
