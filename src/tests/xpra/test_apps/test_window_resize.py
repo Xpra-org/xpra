cb236e0cc4f3a79b338fc2ca692c1b0a0725c4ba
Antoine Martin <totaam@xpra.org>
2013-11-05 05:10:45 +0000

    add ability to resize to a specific size (and can be used via keyboard)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4670 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_resize.py
+++ src/tests/xpra/test_apps/test_window_resize.py
@@ -12,7 +12,24 @@ def main():
 	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 	window.set_size_request(width, height)
 	window.connect("delete_event", gtk.mainquit)
-	btn = gtk.Button("resize me")
+	vbox = gtk.VBox()
+	hbox = gtk.HBox()
+	w_e = gtk.Entry(max=64)
+	hbox.add(w_e)
+	hbox.add(gtk.Label("x"))
+	h_e = gtk.Entry(max=64)
+	hbox.add(h_e)
+	set_size_btn = gtk.Button("set size")
+	def set_size(*args):
+		w = int(w_e.get_text())
+		h = int(h_e.get_text())
+		print("resizing to %s x %s" % (w, h))
+		window.resize(w, h)
+	set_size_btn.connect('clicked', set_size)
+	hbox.add(set_size_btn)
+	vbox.add(hbox)
+
+	btn = gtk.Button("auto resize me")
 	def resize(*args):
 		global width, height
 		width = max(200, (width+20) % 600)
@@ -20,7 +37,8 @@ def main():
 		print("resizing to %s x %s" % (width, height))
 		window.resize(width, height)
 	btn.connect('clicked', resize)
-	window.add(btn)
+	vbox.add(btn)
+	window.add(vbox)
 	window.show_all()
 	gtk.main()
 	return 0
