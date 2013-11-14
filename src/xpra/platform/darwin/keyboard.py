a48658cc59341a9319f78881580316d88353a141
Antoine Martin <totaam@xpra.org>
2013-11-14 04:13:00 +0000

    #456 + #453: * swap Control_L and Meta_L before sending to server * emulate num lock state (always starts ON) * hardcode num-lock to keycode 71 for toggling numlock
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4747 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/keyboard.py
+++ src/xpra/platform/darwin/keyboard.py
@@ -9,8 +9,52 @@ from xpra.platform.keyboard_base import KeyboardBase
 from xpra.log import Logger
 log = Logger()
 
+NUM_LOCK_KEYCODE = 71           #HARDCODED!
+
 
 class Keyboard(KeyboardBase):
+    """
+        Switch Meta_L and Control_L
+    """
 
     def __init__(self):
-        pass
+        self.meta_modifier = None
+        self.control_modifier = None
+        self.num_lock_modifier = None
+        self.num_lock_state = True
+        self.num_lock_keycode = NUM_LOCK_KEYCODE
+
+    def set_modifier_mappings(self, mappings):
+        KeyboardBase.set_modifier_mappings(self, mappings)
+        self.meta_modifier = self.modifier_keys.get("Meta_L")
+        self.control_modifier = self.modifier_keys.get("Control_L")
+        self.num_lock_modifier = self.modifier_keys.get("Num_Lock")
+        log("set_modifier_mappings(%s) meta=%s, control=%s, numlock=%s", mappings, self.meta_modifier, self.control_modifier, self.num_lock_modifier)
+
+    def mask_to_names(self, mask):
+        names = KeyboardBase.mask_to_names(self, mask)
+        if self.meta_modifier is not None and self.control_modifier is not None:
+            #we have the modifier names for both keys we may need to switch
+            if self.meta_modifier in names and self.control_modifier not in names:
+                names.remove(self.meta_modifier)
+                names.append(self.control_modifier)
+            elif self.control_modifier in names and self.meta_modifier not in names:
+                names.remove(self.control_modifier)
+                names.append(self.meta_modifier)
+        if self.num_lock_modifier is not None:
+            if self.num_lock_state and self.num_lock_modifier not in names:
+                names.append(self.num_lock_modifier)
+            elif not self.num_lock_state and self.num_lock_modifier in names:
+                names.remove(self.num_lock_modifier)
+        return names
+
+    def process_key_event(self, send_key_action_cb, wid, key_event):
+        if self.meta_modifier is not None and self.control_modifier is not None:
+            #we have the modifier names for both keys we may need to switch
+            if key_event.keyname=="Control_L":
+                key_event.keyname = "Meta_L"
+            elif key_event.keyname=="Meta_L":
+                key_event.keyname = "Control_L"
+        if key_event.keycode==self.num_lock_keycode and not key_event.pressed:
+            self.num_lock_state = not self.num_lock_state
+        send_key_action_cb(wid, key_event)
