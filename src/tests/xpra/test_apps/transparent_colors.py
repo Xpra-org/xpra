9f36ac8841ddb04fb4e53c543a36b4b0af94d525
Antoine Martin <totaam@xpra.org>
2013-12-09 06:56:21 +0000

    add shades to all colours, use black for "no colour" instead of white
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4891 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/transparent_colors.py
+++ src/tests/xpra/test_apps/transparent_colors.py
@@ -6,7 +6,7 @@ from gi.repository import Gtk   #@UnresolvedImport
 class TransparentColorWindow(Gtk.Window):
     def __init__(self):
         super(TransparentColorWindow, self).__init__()
-        self.set_position(Gtk.WindowPosition.CENTER)
+        #self.set_position(Gtk.WindowPosition.CENTER)
         self.set_default_size(320, 320)
         screen = self.get_screen()
         self.visual = screen.get_rgba_visual()
@@ -23,50 +23,42 @@ class TransparentColorWindow(Gtk.Window):
     def area_draw(self, widget, cr):
         cr.set_font_size(32)
         #Clear everything:
+        cr.save()
         cr.set_operator(cairo.OPERATOR_CLEAR)
         w, h = widget.get_size()
         cr.rectangle(0, 0, w, h)
         cr.fill()
+        cr.restore()
+        cr.set_operator(cairo.OPERATOR_SOURCE)
+
+        def paint_block(label, x, y, r, g, b):
+            #fill with colour
+            cr.set_source_rgba(r, g, b, 1)
+            cr.rectangle(x, y, x+w/2, y+h/2)
+            cr.fill()
+            #top and bottom thirds as a shade to transparent on the edges:
+            shade_h = h/2/3
+            for i in range(shade_h):
+                alpha = i/float(shade_h)
+                cr.set_source_rgba(r, g, b, alpha)
+                cr.rectangle(x, y+i, x+w/2, 1)
+                cr.fill()
+                cr.set_source_rgba(r, g, b, alpha)
+                cr.rectangle(x, y+h/2-i-1, x+w/2, 1)
+                cr.fill()
+            if label:
+                cr.set_source_rgba(1, 1, 1, 1)
+                cr.move_to(x+w/4-21*len(label)/2.0, y+h/4+8)
+                cr.show_text(label)
 
         #Red block
-        cr.set_operator(cairo.OPERATOR_SOURCE)
-        cr.set_source_rgba(1, 0, 0, 1)
-        cr.rectangle(0, 0, w/2, h/2)
-        cr.fill()
-        cr.set_source_rgba(1, 1, 1, 1)
-        cr.move_to(w/4-12, h/4+8)
-        cr.show_text("R")
+        paint_block("RED", 0, 0, 1, 0, 0)
         #Green block:
-        cr.set_operator(cairo.OPERATOR_SOURCE)
-        cr.set_source_rgba(0, 1, 0, 1)
-        cr.rectangle(w/2, 0, w/2, h/2)
-        cr.fill()
-        cr.set_source_rgba(1, 1, 1, 1)
-        cr.move_to(w*3/4-12, h/4+8)
-        cr.show_text("G")
+        paint_block("GREEN", w/2, 0, 0, 1, 0)
         #Blue block:
-        cr.set_operator(cairo.OPERATOR_SOURCE)
-        cr.set_source_rgba(0, 0, 1, 1)
-        cr.rectangle(0, h/2, w/2, h/2)
-        cr.fill()
-        cr.set_source_rgba(1, 1, 1, 1)
-        cr.move_to(w/4-12, h*3/4+8)
-        cr.show_text("B")
-
-        #Transparent Block:
-        cr.set_operator(cairo.OPERATOR_SOURCE)
-        bx = w/2
-        by = h/2
-        bw = float(w)/2/16
-        bh = float(h)/2/16
-        #print("bx=%s, by=%s, bw=%s, bh=%s" % (bx, by, bw, bh))
-        for i in range(256):
-            alpha = float(i+1)/256.0
-            cr.set_source_rgba(1, 1, 1, alpha)
-            rx, ry, rw, rh = bx+(i%16)*bw, by+(i//16)*bh, bw, bh
-            cr.rectangle(rx, ry, rw, rh)
-            #print("rectangle(%s, %s, %s, %s) alpha=%s" % (rx, ry, rw, rh, alpha))
-            cr.fill()
+        paint_block("BLUE", 0, h/2, 0, 0, 1)
+        #Black block:
+        paint_block("BLACK", w/2, h/2, 0, 0, 0)
 
 TransparentColorWindow()
 Gtk.main()
