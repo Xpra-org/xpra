efde053f2af84086a8cb8cc9330e38acba776db5
Antoine Martin <totaam@xpra.org>
2013-12-12 11:30:11 +0000

    re-enable all tests
    git-svn-id: https://xpra.org/svn/Xpra/trunk@4932 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/codecs/test_csc_opencl.py
+++ src/tests/xpra/codecs/test_csc_opencl.py
@@ -4,16 +4,16 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from tests.xpra.codecs.test_csc import test_csc_planar, test_csc_rgb, test_csc_roundtrip
 from tests.xpra.codecs.test_csc import test_all
 
 
 def test_csc_opencl():
     from xpra.codecs.csc_opencl import colorspace_converter
+    #from tests.xpra.codecs.test_csc import test_csc_planar, test_csc_rgb, test_csc_roundtrip
     #test_csc_rgb(colorspace_converter)
     #test_csc_planar(colorspace_converter)
-    test_csc_roundtrip(colorspace_converter)
-    #test_all(colorspace_converter)
+    #test_csc_roundtrip(colorspace_converter)
+    test_all(colorspace_converter)
 
 
 def main():
