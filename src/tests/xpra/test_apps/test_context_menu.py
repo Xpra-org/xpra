c728d26b7e2198ec7b32b66d62b7d89adfd772b1
Antoine Martin <totaam@xpra.org>
2014-01-04 07:59:50 +0000

    #484: more test apps, test_context_menu reproduces the bug
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5112 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/test_apps/test_context_menu.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2013 Antoine Martin <antoine@devloop.org.uk>
+
+import sys
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+
+class TestForm(object):
+
+    def    __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("destroy", gtk.main_quit)
+        self.window.set_default_size(320, 200)
+        self.window.set_border_width(20)
+
+        entry = gtk.Entry()
+        entry.set_text("hello")
+
+        self.window.add(entry)
+        self.window.show_all()
+
+
+def main():
+    TestForm()
+    gtk.main()
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
---
+++ src/tests/xpra/test_apps/test_right_click_menu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2013 Antoine Martin <antoine@devloop.org.uk>
+
+import sys
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+
+class TestForm(object):
+
+	def	__init__(self):
+		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		self.window.connect("destroy", gtk.main_quit)
+		self.window.set_default_size(320, 200)
+		self.window.set_border_width(20)
+		self.window.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+
+		btn = gtk.Button("hello")
+		btn.connect("button_press_event", self.button_clicked)
+
+		self.window.add(btn)
+		self.window.show_all()
+
+	def button_clicked(self, widget, event):
+		print("button_clicked("+str(widget)+", "+str(event)+")")
+		menu = gtk.Menu()
+		menu.append(gtk.MenuItem("Foo"))
+		menu.append(gtk.MenuItem("Bar"))
+		menu.show_all()
+		menu.popup(None, None, None, event.button, event.time)
+
+
+def main():
+	TestForm()
+	gtk.main()
+
+
+if __name__ == "__main__":
+	main()
+	sys.exit(0)
