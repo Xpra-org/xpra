cacdc9454a48985a7a87d876c5390d0bfdec73d8
Antoine Martin <totaam@xpra.org>
2014-01-06 06:50:15 +0000

    simple roundtrip test for vpx
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5124 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/codecs/test_video_codec.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2014 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import time
+
+from xpra.codecs.image_wrapper import ImageWrapper
+#from tests.xpra.test_util import dump_resource_usage, dump_threads
+from tests.xpra.codecs.test_codec import make_rgb_input, make_planar_input
+
+
+def do_test_codec_roundtrip(encoder_class, decoder_class, encoding, src_format, w, h):
+    if src_format.find("RGB")>=0 or src_format.find("BGR")>=0:
+        pixels = make_rgb_input(src_format, w, h, populate=True)
+        stride = len(src_format)*w
+        #print(" input pixels: %s (%sx%s, stride=%s, stride*h=%s)" % (len(pixels), w, h, stride, stride*h))
+        assert len(pixels)>=stride*h, "not enough pixels! (expected at least %s but got %s)" % (stride*h, len(pixels))
+        image = ImageWrapper(0, 0, w, h, pixels, src_format, 24, stride, planes=ImageWrapper.PACKED)
+    else:
+        strides, pixels = make_planar_input(src_format, w, h, populate=True)
+        image = ImageWrapper(0, 0, w, h, pixels, src_format, 24, strides, planes=ImageWrapper._3_PLANES)
+
+    quality = 100
+    speed = 100
+    scaling = (1,1)
+    options = {}
+
+    start = time.time()
+    encoder = encoder_class()
+    encoder.init_context(w, h, src_format, encoding, quality, speed, scaling, options)
+    end = time.time()
+    print("encoder %s initialized in %.1fms" % (encoder, 1000.0*(end-start)))
+
+    start = time.time()
+    decoder = decoder_class()
+    decoder.init_context(encoding, w, h, src_format)
+    end = time.time()
+    print("decoder %s initialized in %.1fms" % (decoder, 1000.0*(end-start)))
+
+    print("using %s to compress %s" % (encoder, image))
+    start = time.time()
+    data, options = encoder.compress_image(image, {})
+    end = time.time()
+    assert data is not None, "compression failed"
+    print("compressed down to %s bytes in %.1fms" % (len(data), 1000.0*(end-start)))
+
+    print("uncompressing %s bytes using %s" % (len(data), decoder))
+    options['csc'] = src_format
+    start = time.time()
+    out_image = decoder.decompress_image(data, options)
+    end = time.time()
+    assert out_image is not None, "decompression failed"
+    print("uncompressed to %s in %.1fms" % (str(out_image), 1000.0*(end-start)))
---
+++ src/tests/xpra/codecs/test_vpx.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2013 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import sys
+from tests.xpra.codecs.test_video_codec import do_test_codec_roundtrip
+from xpra.codecs.vpx.decoder import Decoder     #@UnresolvedImport
+from xpra.codecs.vpx.encoder import Encoder     #@UnresolvedImport
+
+def test_roundtrip():
+    for encoding in ("vp8", "vp9"):
+        print("")
+        print("test_roundtrip() %s" % encoding)
+        do_test_codec_roundtrip(Encoder, Decoder, encoding, "YUV420P", 640, 480)
+
+
+def main():
+    import logging
+    logging.root.setLevel(logging.INFO)
+    logging.root.addHandler(logging.StreamHandler(sys.stdout))
+    print("main()")
+    test_roundtrip()
+
+
+if __name__ == "__main__":
+    main()
