e2e415e9c08a0b3f636594729e04d4c4f52428d3
Antoine Martin <totaam@xpra.org>
2014-01-15 06:05:23 +0000

    show screen size, root window size
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5190 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/info/show_display_props.py
+++ src/tests/xpra/info/show_display_props.py
@@ -9,6 +9,7 @@ print("supports_composite=%s" %  display.supports_composite())
 for i in range(display.get_n_screens()):
     screen = display.get_screen(i)
     print("screen(%s)=%s"% (i, screen))
+    print("screen(%s) size: %sx%s / %sx%s (mm)" % (i, screen.get_width(), screen.get_height(), screen.get_width_mm(), screen.get_height_mm()))
     print("screen(%s).get_rgba_colormap()=%s" % (i, screen.get_rgba_colormap()))
     rgba_visual = screen.get_rgba_visual()
     print("screen(%s).get_rgba_visual()=%s" % (i, rgba_visual))
@@ -20,3 +21,8 @@ for i in range(display.get_n_screens()):
     for a in ("bits_per_rgb", "colormap_size", "depth", "type"):
         print("system_visual.%s: %s" % (a, getattr(sys_visual, a)))
     root = screen.get_root_window()
+    print("screen(%s) root window: %s" % (i, root.get_geometry()))
+
+print("")
+root = gtk.gdk.get_default_root_window()
+print("default root window: %s" % str(root.get_geometry()))
