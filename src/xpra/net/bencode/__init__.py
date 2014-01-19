dc341c5d842bf9ed9d485374f742f033ca1b5d9e
Antoine Martin <totaam@xpra.org>
2014-01-19 08:13:03 +0000

    give us the ability to toggle off the cython bencoder via env var (easier to compare performance)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5220 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/bencode/__init__.py
+++ src/xpra/net/bencode/__init__.py
@@ -1,6 +1,19 @@
-try:
-    from xpra.net.bencode.cython_bencode import bencode, bdecode, __version__
-except ImportError:
-    from xpra.net.bencode.bencode import bencode, bdecode, __version__
+# This file is part of Xpra.
+# Copyright (C) 2014 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+
+
+cython_bencode_loaded = False
+if os.environ.get("USE_CYTHON_BENCODE", "1")!="0":
+    try:
+        from xpra.net.bencode.cython_bencode import bencode, bdecode, __version__
+        cython_bencode_loaded = True
+    except ImportError:
+        pass
+if not cython_bencode_loaded:
+    from xpra.net.bencode.bencode import bencode, bdecode, __version__      #@Reimport
 
 __all__ = ['bencode', 'bdecode', "__version__"]
