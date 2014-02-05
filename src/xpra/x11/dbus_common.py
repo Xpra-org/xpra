06a265aceb82e0335996c7b9be8afd539a59f4b7
Antoine Martin <totaam@xpra.org>
2014-02-05 06:47:15 +0000

    split loop init from bus init (so we can have a system bus too)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5357 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/dbus_common.py
+++ src/xpra/x11/dbus_common.py
@@ -3,13 +3,21 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+_loop_init_done = False
+def loop_init():
+    global _loop_init_done
+    if _loop_init_done:
+        return
+    from dbus.mainloop.glib import DBusGMainLoop
+    DBusGMainLoop(set_as_default=True)
+    _loop_init_done = True
+
 _session_bus = None
 def init_session_bus():
     global _session_bus
     if _session_bus:
         return _session_bus
-    from dbus.mainloop.glib import DBusGMainLoop
-    DBusGMainLoop(set_as_default=True)
+    loop_init()
     import dbus
     _session_bus = dbus.SessionBus()
     return _session_bus
