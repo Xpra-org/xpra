71d6ff50019e222c8cfa343901bdaf1189681412
Antoine Martin <totaam@xpra.org>
2014-02-06 05:07:00 +0000

    add cwd to sys path so we can more easily run tests that import other tests
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5370 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/win32/python_execfile.py
+++ src/win32/python_execfile.py
@@ -25,5 +25,9 @@ filename = sys.argv[1]
 if not os.path.exists(filename):
     print("script file '%s' not found" % filename)
     sys.exit(1)
+
+cwd = os.getcwd()
+if cwd not in sys.path:
+    sys.path.append(cwd)
 execfile(filename)
 sys.exit(0)
