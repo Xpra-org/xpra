bb33ee3085f416d24a12f42b247a7c9865b57e2b
Antoine Martin <totaam@xpra.org>
2014-03-02 04:54:31 +0000

    simple test app for system tray forwarding: click on the tray to change the icon
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5658 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/test_apps/test_system_tray.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import gtk
+
+
+class StatusIcon:
+    def __init__(self):
+        self.statusicon = gtk.StatusIcon()
+        self.counter = 0
+        self.statusicon.set_from_stock(gtk.STOCK_HOME) 
+        self.statusicon.connect("popup-menu", self.popup_menu)
+        self.statusicon.connect("activate", self.activate)
+        self.statusicon.set_tooltip("StatusIcon Example")
+        #build list of stock icons:
+        self.stock = {}
+        for x in dir(gtk):
+            if x.startswith("STOCK_"):
+                self.stock[x[len("STOCK_"):]] = getattr(gtk, x)
+
+    def activate(self, *args):
+        self.counter += 1
+        name, stock = list(self.stock.items())[self.counter % len(self.stock)]
+        print("setting tray icon to: %s" % name)
+        self.statusicon.set_from_stock(stock) 
+
+    def popup_menu(self, icon, button, time):
+        menu = gtk.Menu()
+        quit_menu = gtk.MenuItem("Quit")
+        quit_menu.connect("activate", gtk.main_quit)
+        menu.append(quit_menu)
+        menu.show_all()
+        menu.popup(None, None, gtk.status_icon_position_menu, button, time, self.statusicon)
+        
+
+def main():
+    StatusIcon()
+    gtk.main()
+
+
+if __name__ == "__main__":
+    main()
