fbcc49568e3a5537207faf0161df943caf0f10e5
Antoine Martin <totaam@xpra.org>
2014-03-14 09:11:47 +0000

    #532: don't send new window dimension to the client unless it has actually changed
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5787 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/server.py
+++ src/xpra/x11/server.py
@@ -351,6 +351,9 @@ class XpraServer(gobject.GObject, X11ServerBase):
         nw,nh = window.get_property("actual-size")
         geom = self._desktop_manager.window_geometry(window)
         windowlog("XpraServer._window_resized_signaled(%s,%s) actual-size=%sx%s, current geometry=%s", window, args, nw, nh, geom)
+        if geom[2:4]==[nw, nh]:
+            #unchanged
+            return
         geom[2:4] = nw,nh
         for ss in self._server_sources.values():
             ss.resize_window(self._window_to_id[window], window, nw, nh)
