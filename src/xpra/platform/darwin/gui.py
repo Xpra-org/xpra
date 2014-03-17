44bd8d64fd95c9e0af085f26a489af867705c06d
Antoine Martin <totaam@xpra.org>
2014-03-17 15:46:42 +0000

    #492: suspend / resume hooks for osx
    git-svn-id: https://xpra.org/svn/Xpra/trunk@5828 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/gui.py
+++ src/xpra/platform/darwin/gui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # This file is part of Xpra.
 # Copyright (C) 2011-2014 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
@@ -70,3 +71,76 @@ def system_bell(*args):
         return False
     Snd.SysBeep(1)
     return True
+
+
+try:
+    import Foundation                           #@UnresolvedImport
+    import AppKit                               #@UnresolvedImport
+    import PyObjCTools.AppHelper as AppHelper   #@UnresolvedImport
+    NSObject = Foundation.NSObject
+    NSWorkspace = AppKit.NSWorkspace
+except Exception, e:
+    log.warn("failed to load critical modules for sleep notification support: %s", e)
+    NSObject = object
+    NSWorkspace = None
+
+class NotificationHandler(NSObject):
+    """Class that handles the sleep notifications."""
+
+    def handleSleepNotification_(self, aNotification):
+        log("handleSleepNotification(%s)", aNotification)
+        self.sleep_callback()
+
+    def handleWakeNotification_(self, aNotification):
+        log("handleWakeNotification(%s)", aNotification)
+        self.wake_callback()
+
+
+class ClientExtras(object):
+    def __init__(self, client, blocking=False):
+        self.client = client
+        self.blocking = blocking
+        self.setup_event_loop()
+
+    def cleanup(self):
+        self.client = None
+        self.stop_event_loop()
+
+    def stop_event_loop(self):
+        if self.notificationCenter:
+            self.notificationCenter = None
+            if self.blocking:
+                AppHelper.stopEventLoop()
+        if self.handler:
+            self.handler = None
+
+    def setup_event_loop(self):
+        if NSWorkspace is None:
+            return
+        ws = NSWorkspace.sharedWorkspace()
+        self.notificationCenter = ws.notificationCenter()
+        self.handler = NotificationHandler.new()
+        self.handler.sleep_callback = self.client.suspend
+        self.handler.wake_callback = self.client.resume
+        self.notificationCenter.addObserver_selector_name_object_(
+                 self.handler, "handleSleepNotification:",
+                 AppKit.NSWorkspaceWillSleepNotification, None)
+        self.notificationCenter.addObserver_selector_name_object_(
+                 self.handler, "handleWakeNotification:",
+                 AppKit.NSWorkspaceDidWakeNotification, None)
+        log("starting console event loop with notifcation center=%s and handler=%s", self.notificationCenter, self.handler)
+        if self.blocking:
+            #this is for running standalone
+            AppHelper.runConsoleEventLoop(installInterrupt=self.blocking)
+        #when not blocking, we rely on another part of the code
+        #to run the event loop for us
+
+
+def main():
+    from xpra.platform import init
+    init("OSX Extras")
+    ClientExtras(None, True)
+
+
+if __name__ == "__main__":
+    main()
