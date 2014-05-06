580faf6dce7ab0094a3162bcdbcffcf70f769b28
Antoine Martin <totaam@xpra.org>
2014-05-06 14:03:32 +0000

    simpler code, should handle rgb32 with alpha too (still a little buggy)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@6378 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/cairo_backing.py
+++ src/xpra/client/gtk_base/cairo_backing.py
@@ -5,20 +5,21 @@
 # later version. See the file COPYING for details.
 
 import cairo
-import array
 
+from xpra.gtk_common.gobject_compat import import_gdk, import_gobject, import_pixbufloader
+gdk = import_gdk()
+gobject = import_gobject()
+PixbufLoader = import_pixbufloader()
+
+from xpra.gtk_common.gtk_util import pixbuf_new_from_data, COLORSPACE_RGB
 from xpra.client.gtk_base.gtk_window_backing_base import GTKWindowBacking
 from xpra.client.window_backing_base import fire_paint_callbacks
-from xpra.os_util import BytesIOClass
 from xpra.codecs.loader import get_codec
+
 
 from xpra.log import Logger
 log = Logger("paint", "cairo")
 
-from xpra.gtk_common.gobject_compat import is_gtk3, import_gdk, import_gobject, import_pixbufloader
-gdk = import_gdk()
-gobject = import_gobject()
-PixbufLoader = import_pixbufloader()
 
 
 """
@@ -71,73 +72,67 @@ class CairoBacking(GTKWindowBacking):
 
 
     def paint_image(self, coding, img_data, x, y, width, height, options, callbacks):
+        log("cairo.paint_image(%s, %s bytes,%s,%s,%s,%s,%s,%s)", coding, len(img_data), x, y, width, height, options, callbacks)
         if coding.startswith("png") or coding=="jpeg":
-            gobject.idle_add(self.cairo_paint_image, img_data, x, y, callbacks)
+            def ui_paint_image():
+                success = False
+                if self._backing:
+                    success = self.cairo_paint_image(img_data, x, y)
+                fire_paint_callbacks(callbacks, success)
+            gobject.idle_add(ui_paint_image)
             return
         #this will end up calling do_paint_rgb24 after converting the pixels to RGB
         GTKWindowBacking.paint_image(self, coding, img_data, x, y, width, height, options, callbacks)
 
-    def cairo_paint_image(self, img_data, x, y, callbacks):
+    def cairo_paint_image(self, img_data, x, y):
         """ must be called from UI thread """
+        log("cairo_paint_image(%s bytes, %s, %s) backing=%s", len(img_data), x, y, self._backing)
         #load into a pixbuf
         pbl = PixbufLoader()
         pbl.write(img_data)
         pbl.close()
         pixbuf = pbl.get_pixbuf()
         del pbl
-        self.cairo_paint_pixbuf(pixbuf, x, y, callbacks)
+        return self.cairo_paint_pixbuf(pixbuf, x, y)
 
-    def cairo_paint_pixbuf(self, pixbuf, x, y, callbacks):
+    def cairo_paint_pixbuf(self, pixbuf, x, y):
         """ must be called from UI thread """
-        if self._backing is None:
-            fire_paint_callbacks(callbacks, False)
-            return
+        log("cairo_paint_pixbuf(%s, %s, %s) backing=%s", pixbuf, x, y, self._backing)
         #now use it to paint:
         gc = cairo.Context(self._backing)
         gdk.cairo_set_source_pixbuf(gc, pixbuf, x, y)
         gc.paint()
-        del pixbuf, gc
-        fire_paint_callbacks(callbacks, True)
+        return True
 
+    def _do_paint_rgb24(self, img_data, x, y, width, height, rowstride, options, callbacks):
+        log("_do_paint_rgb24")
+        return self._do_paint_rgb(False, img_data, x, y, width, height, rowstride, options, callbacks)
 
-    def do_paint_rgb24(self, img_data, x, y, width, height, rowstride, options, callbacks):
+    def _do_paint_rgb32(self, img_data, x, y, width, height, rowstride, options, callbacks):
+        log("_do_paint_rgb32")
+        return self._do_paint_rgb(True, img_data, x, y, width, height, rowstride, options, callbacks)
+
+    def _do_paint_rgb(self, has_alpha, img_data, x, y, width, height, rowstride, options, callbacks):
         """ must be called from UI thread """
-        log("cairo.do_paint_rgb24(..,%s,%s,%s,%s,%s,%s,%s)", x, y, width, height, rowstride, options, callbacks)
-        if self._backing is None:
-            fire_paint_callbacks(callbacks, False)
-            return  False
+        log("cairo._do_paint_rgb(%s, %s bytes,%s,%s,%s,%s,%s,%s,%s)", has_alpha, len(img_data), x, y, width, height, rowstride, options, callbacks)
+        rgb_format = options.strget("rgb_format", "RGB")
+        if rgb_format in ("RGBX", "RGBA"):
+            rgba = self.unpremultiply(img_data)
+            pixbuf = pixbuf_new_from_data(rgba, COLORSPACE_RGB, has_alpha, 8, width, height, rowstride, None, None)
+            return self.cairo_paint_pixbuf(pixbuf, x, y)
         PIL = get_codec("PIL")
         assert PIL, "cannot paint without PIL!"
-        if rowstride==0:
-            rowstride = width * 3
-        im = PIL.Image.frombuffer("RGB", (width, height), img_data, "raw", "RGB", rowstride)
-        if is_gtk3():
-            #FIXME: ugly
-            from gi.repository import GdkPixbuf     #@UnresolvedImport
-            data = array.array('B', im.tostring())
-            pixbuf = GdkPixbuf.Pixbuf.new_from_data(data, GdkPixbuf.Colorspace.RGB,
-                                          True, 8, width, height, width * 4,
-                                          None, None)
-            self.cairo_paint_pixbuf(pixbuf, x, y, callbacks)
-            return
-        #roundtrip via PNG! (crappy cairo API)
-        buf = BytesIOClass()
-        im.save(buf, "PNG")
-        data = buf.getvalue()
-        buf.close()
-        img_data = BytesIOClass(data)
-        surf = cairo.ImageSurface.create_from_png(img_data)
-        gc = cairo.Context(self._backing)
-        gc.set_source_surface(surf, x, y)
-        gc.paint()
-        surf.finish()
-        fire_paint_callbacks(callbacks, True)
-        return  False
-
+        im = PIL.Image.frombuffer("RGB", (width, height), img_data, "raw", rgb_format, rowstride)
+        im = im.convert("RGBX")
+        data = im.tostring("raw", "RGBX", 0, 1)
+        log.info("%s: %s bytes, now RGBX: %s bytes", rgb_format, len(img_data), len(data))
+        pixbuf = pixbuf_new_from_data(data, COLORSPACE_RGB, False, 8, width, height, width*4, None, None)
+        return self.cairo_paint_pixbuf(pixbuf, x, y)
 
     def cairo_draw(self, context):
+        log("cairo_draw(%s) backing=%s", context, self._backing)
         if self._backing is None:
-            return
+            return False
         try:
             context.set_source_surface(self._backing, 0, 0)
             context.set_operator(cairo.OPERATOR_SOURCE)
