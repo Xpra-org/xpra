c62c584892b30a22de6776317fb28ace69a58263
Antoine Martin <totaam@xpra.org>
2014-06-03 13:14:01 +0000

    example command line client which changes encoding and quality every 2 seconds
    git-svn-id: https://xpra.org/svn/Xpra/trunk@6640 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/clients/test_change_settings.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2014 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import gobject
+
+from xpra.log import Logger
+log = Logger()
+
+from xpra.client.gobject_client_base import CommandConnectClient
+
+
+class TestChangeSettings(CommandConnectClient):
+    """
+        Sending an illegal command should get us kicked out
+    """
+
+    def verify_connected(self):
+        pass
+
+    def timeout(self, *args):
+        log.info("ignoring timeout")
+
+    def make_hello(self):
+        capabilities = CommandConnectClient.make_hello(self)
+        #KISS:
+        capabilities["keyboard"] = False
+        capabilities["wants_aliases"] = False
+        return capabilities
+
+    def send_from_queue(self):
+        x = self.queue[0]
+        self.queue = self.queue[1:]
+        log.info("sending %s", x)
+        self.send(*x)
+        if len(self.queue)>0:
+            #run again
+            return True
+        self.quit(0)    #all done!
+
+    def _command_response(self, packet):
+        log.info("command_response(%s)", packet)
+
+    def _process_ping(self, packet):
+        time_to_echo = packet[1]
+        #skip load average and latency:
+        self.send("ping_echo", time_to_echo, 0, 0, 0, -1)
+        log.info("sending ping echo")
+
+    def _process_hello(self, packet):
+        hello = packet[1]
+        log.info("got hello back: %s", hello)
+        self._packet_handlers["command_response"] = self._command_response
+        self._packet_handlers["ping"] = self._process_ping
+        self.queue = []
+        for _ in range(10):
+            for encoding in ("rgb", "png", "jpeg", "h264", "vp8", "webp"):
+                self.queue.append(("command_request", "encoding", encoding))
+                self.queue.append(("command_request", "quality", "*", 1))
+                self.queue.append(("command_request", "quality", "*", 100))
+        #send one command every 2 seconds:
+        gobject.timeout_add(2*1000, self.send_from_queue)
+
+def main():
+    import sys
+    from tests.xpra.clients.test_DoS_client import test_DoS
+    test_DoS(TestChangeSettings, sys.argv)
+
+
+if __name__ == "__main__":
+    main()
