e99348642316745b461b6cc0fddaa3fcf44526fe
Antoine Martin <totaam@xpra.org>
2014-06-12 12:09:28 +0000

    big performance boost by skipping bounds checks, division checks, etc (about 50% on my test system)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@6758 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/csc_cython/colorspace_converter.pyx
+++ src/xpra/codecs/csc_cython/colorspace_converter.pyx
@@ -4,6 +4,9 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+#!python
+#cython: boundscheck=False, wraparound=False, overflowcheck=False, cdivision=True, unraisable_tracebacks=True
+
 import time
 import struct
 try:
