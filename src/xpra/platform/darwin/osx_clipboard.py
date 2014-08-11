e769b54a33e0fced431e442dd2aa96688e1c9d29
Antoine Martin <totaam@xpra.org>
2014-08-11 04:02:41 +0000

    #507: when the clipboard contents change, do send the "anti-token" if we have it!
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7231 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/osx_clipboard.py
+++ src/xpra/platform/darwin/osx_clipboard.py
@@ -178,7 +178,7 @@ class OSXClipboardProxy(ClipboardProxy):
 
     def local_clipboard_changed(self):
         log("local_clipboard_changed() greedy_client=%s, have_token=%s, blocked=%s", self._greedy_client, self._have_token, self._block_owner_change)
-        if (self._greedy_client or not self._have_token) and not self._block_owner_change:
+        if (self._greedy_client or self._have_token) and not self._block_owner_change:
             self._have_token = False
             self.emit("send-clipboard-token", self._selection)
             self._sent_token_events += 1
