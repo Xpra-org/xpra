4d5fa30f9d571cc54e2b4e15d8a38e3343ad399e
Antoine Martin <totaam@xpra.org>
2014-08-12 08:26:40 +0000

    prevent error during cleanup, avoid None values
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7265 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/protocol.py
+++ src/xpra/net/protocol.py
@@ -200,9 +200,13 @@ class Protocol(object):
             "output.cipher"         : self.cipher_out_name or "",
             "large_packets"         : self.large_packets,
             "compression_level"     : self.compression_level,
-            "max_packet_size"       : self.max_packet_size,
-            "compressor"            : compression.get_compressor_name(self._compress),
-            "encoder"               : packet_encoding.get_encoder_name(self._encoder)}
+            "max_packet_size"       : self.max_packet_size}
+        c = self._compress
+        if c:
+            info["compressor"] = compression.get_compressor_name(self._compress)
+        e = self._encoder
+        if e:
+            info["encoder"] = packet_encoding.get_encoder_name(self._encoder)
         for k,v in self.send_aliases.items():
             info["send_alias." + str(k)] = v
             info["send_alias." + str(v)] = k
