c5a304e73a4c0813193e0d11506d4ccb8874da00
Antoine Martin <totaam@xpra.org>
2014-08-27 02:52:08 +0000

    drop support for old python versions: we can always use unpack_from, also create the struct instance just once (except for pack_header_and_data)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7440 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/header.py
+++ src/xpra/net/header.py
@@ -17,17 +17,14 @@ LZO_FLAG        = 0x20
 FLAGS_NOHEADER  = 0x40
 #0x80 is free
 
-if sys.version_info[:2]>=(2,5):
-    def unpack_header(buf):
-        return struct.unpack_from('!cBBBL', buf)
-else:
-    def unpack_header(buf):
-        return struct.unpack('!cBBBL', "".join(buf))
-
+_header_unpack_struct = struct.Struct('!cBBBL')
+def unpack_header(buf):
+    return _header_unpack_struct.unpack_from(buf)
 
 #'P' + protocol-flags + compression_level + packet_index + data_size
+_header_pack_struct = struct.Struct('!BBBBL')
 def pack_header(proto_flags, level, index, payload_size):
-    return struct.pack('!BBBBL', ord("P"), proto_flags, level, index, payload_size)
+    return _header_pack_struct.pack(ord("P"), proto_flags, level, index, payload_size)
 
 if sys.version_info[0]<3:
     #before v3, python does the right thing without hassle:
