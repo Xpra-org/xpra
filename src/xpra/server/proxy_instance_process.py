5ab5f927d8276bb5057a4c7f042270ef0a0b804e
Antoine Martin <totaam@xpra.org>
2014-09-05 15:25:05 +0000

    #671: don't assume we have compression algos just because they're in the caps!
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7521 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/proxy_instance_process.py
+++ src/xpra/server/proxy_instance_process.py
@@ -18,6 +18,7 @@ log = Logger("proxy")
 
 from xpra.server.server_core import get_server_info, get_thread_info
 from xpra.scripts.server import deadly_signal
+from xpra.net import compression
 from xpra.net.compression import Compressed, compressed_wrapper
 from xpra.net.protocol import Protocol, get_network_caps
 from xpra.net.crypto import new_cipher_caps
@@ -519,9 +520,9 @@ class ProxyInstanceProcess(Process):
                     packet[8] = str(pixels)
                 else:
                     #FIXME: this is ugly and not generic!
-                    zlib = self.caps.get("zlib", True)
-                    lz4 = self.caps.get("lz4", False)
-                    lzo = self.caps.get("lzo", False)
+                    zlib = compression.use_zlib and self.caps.get("zlib", True) 
+                    lz4 = compression.use_lz4 and self.caps.get("lz4", False) 
+                    lzo = compression.use_lzo and self.caps.get("lzo", False)
                     if zlib or lz4 or lzo:
                         packet[8] = compressed_wrapper("cursor", pixels, zlib=zlib, lz4=lz4, lzo=lzo, can_inline=False)
                     else:
