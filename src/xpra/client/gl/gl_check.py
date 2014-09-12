61095613e1c0840577cbc694afe9dbc330a916b1
Antoine Martin <totaam@xpra.org>
2014-09-12 01:47:17 +0000

    #563: remove intel driver blacklisting on osx
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7578 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gl/gl_check.py
+++ src/xpra/client/gl/gl_check.py
@@ -19,8 +19,6 @@ if False:
     #for testing:
     BLACKLIST["vendor"].append("NVIDIA Corporation")
     WHITELIST["renderer"] = ["GeForce GTX 760/PCIe/SSE2"]
-
-if sys.platform.startswith("darwin"): 
     #crashes were reported with the Intel driver on OSX
     BLACKLIST["vendor"].append("Intel Inc.")
     WHITELIST["renderer"] = ["Intel HD Graphics 4000 OpenGL Engine"]
