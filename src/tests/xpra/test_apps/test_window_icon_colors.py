c2a464044439316679b1eb5042f63e2cb5eb233d
Antoine Martin <totaam@xpra.org>
2014-09-23 08:45:23 +0000

    rotate the colours in the black quadrant for testing (and stop/start with keyboard)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7762 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_icon_colors.py
+++ src/tests/xpra/test_apps/test_window_icon_colors.py
@@ -3,36 +3,51 @@
 import gtk
 from gtk import gdk
 import cairo
+import gobject
 
 class TestWindow(gtk.Window):
 
     def __init__(self):
         gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
         self.set_size_request(128, 128)
-        #generate window icon image:
-        s = 64
-        w, h = s*2, s*2
-        pixmap = gdk.Pixmap(gdk.get_default_root_window(), w, h)
-        cr = pixmap.cairo_create()
-        cr.set_operator(cairo.OPERATOR_CLEAR)
-        cr.fill()
-        cr.set_operator(cairo.OPERATOR_SOURCE)
-        for i, color in enumerate([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)]): 
-            cr.set_source_rgba(*color)
-            cr.new_path()
-            x = (i % 2) * s
-            y = (i / 2) * s
-            cr.move_to(x, y)
-            cr.line_to(x + s, y)
-            cr.line_to(x+s, y+s)
-            cr.line_to(x, y+s)
-            cr.close_path()
+        self.counter = 0
+        self.rotate = True
+        def set_icon():
+            #generate window icon image:
+            if not self.rotate:
+                return True
+            s = 64
+            w, h = s*2, s*2
+            pixmap = gdk.Pixmap(gdk.get_default_root_window(), w, h)
+            cr = pixmap.cairo_create()
+            cr.set_operator(cairo.OPERATOR_CLEAR)
             cr.fill()
-        pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, True, 8, w, h)
-        pixbuf.get_from_drawable(pixmap, pixmap.get_colormap(), 0, 0, 0, 0, w, h)
-        self.set_icon(pixbuf)
+            cr.set_operator(cairo.OPERATOR_SOURCE)
+            v = (self.counter % 256 / 255.0)
+            self.counter += 1
+            for i, color in enumerate([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1), (v, v, v, v)]):
+                cr.set_source_rgba(*color)
+                cr.new_path()
+                x = (i % 2) * s
+                y = (i / 2) * s
+                cr.move_to(x, y)
+                cr.line_to(x + s, y)
+                cr.line_to(x+s, y+s)
+                cr.line_to(x, y+s)
+                cr.close_path()
+                cr.fill()
+            pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, True, 8, w, h)
+            pixbuf.get_from_drawable(pixmap, pixmap.get_colormap(), 0, 0, 0, 0, w, h)
+            self.set_icon(pixbuf)
+            return True
+        gobject.timeout_add(20, set_icon)
         self.connect("delete_event", self.quit_cb)
+        self.set_events(gdk.KEY_PRESS_MASK)
+        self.connect("key_press_event", self.on_key_press)
         self.show_all()
+
+    def on_key_press(self, *args):
+        self.rotate = not self.rotate
 
     def quit_cb(self, *args):
         gtk.main_quit()
