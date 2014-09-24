51f73450b8792e9ba65f057ba5e781184e174fc4
Antoine Martin <totaam@xpra.org>
2014-09-24 03:14:15 +0000

    test which changes the window icon with each keypress, using icons given on the command line
    git-svn-id: https://xpra.org/svn/Xpra/trunk@7778 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/test_apps/test_window_icon_from_files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import gtk.gdk
+
+class TestWindow(gtk.Window):
+
+    def __init__(self, icons):
+        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        self.icons = icons
+        self.index = 0
+        self._set_window_icon()
+        self.set_size_request(128, 128)
+        self.connect("delete_event", self.quit_cb)
+        self.show_all()
+        self.connect("key_press_event", self._set_window_icon)
+
+    def _set_window_icon(self, *args):
+        self.set_icon(self.icons[self.index % len(self.icons)])
+        self.index += 1
+        return True
+
+    def on_key_press(self, *args):
+        self.rotate = not self.rotate
+
+    def quit_cb(self, *args):
+        gtk.main_quit()
+
+
+def main():
+    if len(sys.argv)<2:
+        print("usage: %s ICONFILE1 [ICONFILE2] [..]" % sys.argv[0])
+        sys.exit(1)
+    icons = [gtk.gdk.pixbuf_new_from_file(x) for x in sys.argv[1:]]
+    TestWindow(icons)
+    gtk.main()
+
+
+if __name__ == "__main__":
+    main()
