c5c46844878fb4935f62432ac56ec76ae80e8772
Antoine Martin <totaam@xpra.org>
2014-11-01 15:46:25 +0000

    quieten pydev
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8031 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/codecs/test_nvenc.py
+++ src/tests/xpra/codecs/test_nvenc.py
@@ -4,7 +4,8 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import sys
+#@PydevCodeAnalysisIgnore
+
 import random
 import threading
 from tests.xpra.codecs.test_encoder import test_encoder, gen_src_images, do_test_encoder, test_encoder_dimensions, test_performance
