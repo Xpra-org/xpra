3986a62431361277e8f0a02c215d05c6850d7541
Antoine Martin <totaam@xpra.org>
2014-11-04 06:32:42 +0000

    #718: add test to raise a second window, similar to the problem with the Java code - except this works as GTK uses _NET_ACTIVE_WINDOW instead of ConfigureRequest
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8050 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_raise.py
+++ src/tests/xpra/test_apps/test_window_raise.py
@@ -5,18 +5,33 @@ import gobject
 
 width = 400
 height = 200
+
 def main():
+	other = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	other.set_title("Other")
+	other.set_size_request(width/2, height/2)
+	other.connect("delete_event", gtk.mainquit)
+	other.show_all()
+
 	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	window.set_title("Main")
 	window.set_size_request(width, height)
 	window.connect("delete_event", gtk.mainquit)
 	vbox = gtk.VBox(False, 0)
-	hbox = gtk.HBox(False, 0)
-	vbox.pack_start(hbox, expand=False, fill=False, padding=10)
-	btn = gtk.Button("raise me in 5 seconds")
-	hbox.pack_start(btn, expand=False, fill=False, padding=10)
-	def move_clicked(*args):
-		gobject.timeout_add(5*1000, window.present)
-	btn.connect('clicked', move_clicked)
+	def add_button(title, callback):
+		hbox = gtk.HBox(False, 0)
+		vbox.pack_start(hbox, expand=False, fill=False, padding=10)
+		btn = gtk.Button(title)
+		hbox.pack_start(btn, expand=False, fill=False, padding=10)
+		def on_clicked(*args):
+			def after_delay():
+				print("**********************************************\nCALLING %s" % callback)
+				callback()
+				print("DONE\n**********************************************")
+			gobject.timeout_add(5*1000, after_delay)
+		btn.connect("clicked", on_clicked)
+	add_button("raise Main in 5 seconds", window.present)
+	add_button("raise Other in 5 seconds", other.present)
 	window.add(vbox)
 	window.show_all()
 	gtk.main()
