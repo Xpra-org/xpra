729b9159feb6ff0c4a7e0e3ac1ca0c749069ebd7
Antoine Martin <totaam@xpra.org>
2014-12-23 15:06:22 +0000

    fullscreen and maximized should always be sent, as we can turn them on or off!
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8273 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/source.py
+++ src/xpra/server/source.py
@@ -77,10 +77,10 @@ def make_window_metadata(window, propname, get_transient_for=None, get_window_id
         return {}
     elif propname == "window-type":
         return {"window-type" : window.get_property("window-type")}
-    elif propname in ("iconic", ):
-        #always send this one when requested
+    elif propname in ("iconic", "fullscreen", "maximized"):
+        #always send these when requested
         return {propname : bool(window.get_property(propname))}
-    elif propname in ("has-alpha", "override-redirect", "tray", "modal", "fullscreen", "maximized"):
+    elif propname in ("has-alpha", "override-redirect", "tray", "modal"):
         v = window.get_property(propname)
         if v is False:
             #save space: all these properties are assumed false if unspecified
