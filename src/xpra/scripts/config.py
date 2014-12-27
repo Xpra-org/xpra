725c26977396b98e7885ec72c511d9ab5f7905e8
Antoine Martin <totaam@xpra.org>
2014-12-27 08:27:32 +0000

    add "dbus-proxy" to config so it can be used in config files, also fix spelling in default config
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8301 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/config.py
+++ src/xpra/scripts/config.py
@@ -281,6 +281,7 @@ OPTION_TYPES = {
                     "auto-refresh-delay": float,
                     #boolean options:
                     "daemon"            : bool,
+                    "dbus-proxy"        : bool,
                     "use-display"       : bool,
                     "displayfd"         : bool,
                     "fake-xinerama"     : bool,
@@ -372,6 +373,7 @@ def get_defaults():
                     "max-bandwidth"     : 0.0,
                     "auto-refresh-delay": 0.25,
                     "daemon"            : True,
+                    "dbus-proxy"        : True,
                     "use-display"       : False,
                     "displayfd"         : False,
                     "fake-xinerama"     : True,
