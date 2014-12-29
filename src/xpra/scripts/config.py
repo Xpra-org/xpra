8ca6da4bcfca8025247f2630a20600fe9e1b10ee
Antoine Martin <totaam@xpra.org>
2014-12-29 03:42:27 +0000

    remove duplicated entries added by mistake
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8326 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/config.py
+++ src/xpra/scripts/config.py
@@ -281,7 +281,6 @@ OPTION_TYPES = {
                     "auto-refresh-delay": float,
                     #boolean options:
                     "daemon"            : bool,
-                    "dbus-proxy"        : bool,
                     "use-display"       : bool,
                     "displayfd"         : bool,
                     "fake-xinerama"     : bool,
@@ -374,7 +373,6 @@ def get_defaults():
                     "max-bandwidth"     : 0.0,
                     "auto-refresh-delay": 0.25,
                     "daemon"            : True,
-                    "dbus-proxy"        : True,
                     "use-display"       : False,
                     "displayfd"         : False,
                     "fake-xinerama"     : True,
