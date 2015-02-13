840240d538d7c3be787efffd6e5a6ff8c78ad262
Antoine Martin <totaam@xpra.org>
2015-02-13 03:30:46 +0000

    whitespace cleanup
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8656 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_strut.py
+++ src/tests/xpra/test_apps/test_window_strut.py
@@ -9,10 +9,10 @@ class PyGtkWidgetDockExample:
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_default_size(100, gtk.gdk.screen_height())
         self.window.move(gtk.gdk.screen_width()-100, 0)
-        self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)        
-        self.window.show()          
-        self.window.window.property_change("_NET_WM_STRUT", "CARDINAL", 32, 
-            gtk.gdk.PROP_MODE_REPLACE, [0, 100, 0, 0])               
+        self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)
+        self.window.show()
+        self.window.window.property_change("_NET_WM_STRUT", "CARDINAL", 32,
+            gtk.gdk.PROP_MODE_REPLACE, [0, 100, 0, 0])
 
 def main():
     PyGtkWidgetDockExample()
