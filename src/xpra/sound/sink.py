749c01785685a6f2923fb2403ef93b549776d338
Antoine Martin <totaam@xpra.org>
2015-03-18 04:56:01 +0000

    reduce the min queue size
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8794 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/sound/sink.py
+++ src/xpra/sound/sink.py
@@ -45,7 +45,7 @@ if DEFAULT_SINK not in SINKS:
     DEFAULT_SINK = SINKS[0]
 QUEUE_SILENT = 0
 QUEUE_TIME = get_queue_time(450)
-QUEUE_MIN_TIME = get_queue_time(QUEUE_TIME//2//MS_TO_NS, "MIN")
+QUEUE_MIN_TIME = get_queue_time(QUEUE_TIME//4//MS_TO_NS, "MIN")
 assert QUEUE_MIN_TIME<=QUEUE_TIME
 
 
