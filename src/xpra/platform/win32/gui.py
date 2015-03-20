d365422aa0c80850e5014ce22e453dd9134898f3
Antoine Martin <totaam@xpra.org>
2015-03-20 14:58:09 +0000

    fix possible race during cleanup: avoid None client dereference but keep claiming that we have handled the message since we must be exiting already to have cleared the reference to the client instance
    git-svn-id: https://xpra.org/svn/Xpra/trunk@8804 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/gui.py
+++ src/xpra/platform/win32/gui.py
@@ -540,11 +540,14 @@ class ClientExtras(object):
             log.info("received console event %s", str(event_name).replace("_EVENT", ""))
         else:
             log.warn("unknown console event: %s", event_name)
+        c = self.client
         if event==win32con.CTRL_C_EVENT:
-            self.client.signal_disconnect_and_quit(0, "CTRL_C")
+            if c:
+                c.signal_disconnect_and_quit(0, "CTRL_C")
             return 1
         if event==win32con.CTRL_CLOSE_EVENT:
-            self.client.signal_disconnect_and_quit(0, "CTRL_CLOSE")
+            if c:
+                c.signal_disconnect_and_quit(0, "CTRL_CLOSE")
             return 1
         return 0
 
