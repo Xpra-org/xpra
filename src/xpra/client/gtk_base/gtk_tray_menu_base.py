17a3683a02e4c6d57decb27cdba697ba367b1b81
Antoine Martin <totaam@xpra.org>
2015-04-14 18:30:22 +0000

    #724: safer workaround for buggy win32 tray behaviour: use an idle_add timer to check if we need to patch things up, and prevent unnecessary toggles
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9002 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/gtk_tray_menu_base.py
+++ src/xpra/client/gtk_base/gtk_tray_menu_base.py
@@ -68,8 +68,14 @@ class TrayCheckMenuItem(gtk.CheckMenuItem):
 
     def on_button_release_event(self, *args):
         log("TrayCheckMenuItem.on_button_release_event(%s) label=%s", args, self.label)
-        self.set_active(True)
-        self.toggled()
+        self.active_state = self.get_active()
+        def recheck():
+            state = self.active_state
+            self.active_state = None
+            if state is not None and state==self.get_active():
+                #toggle did not fire after the button release, so force it:
+                self.set_active(not state)
+        gobject.idle_add(recheck)
 
 
 def make_min_auto_menu(title, min_options, options, get_current_min_value, get_current_value, set_min_value_cb, set_value_cb):
@@ -546,7 +552,7 @@ class GTKTrayMenuBase(object):
                 gl.set_tooltip_text("no server support for runtime switching")
                 return
             def opengl_toggled(*args):
-                log("opengl_toggled%s", args)
+                log.info("opengl_toggled%s", args)
                 self.client.toggle_opengl()
             gl.connect("toggled", opengl_toggled)
         self.client.connect("handshake-complete", gl_set)
