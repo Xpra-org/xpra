e6a7260b79e4fb7f118b563478c2bc49b4feccd3
Antoine Martin <totaam@xpra.org>
2015-04-16 06:09:31 +0000

    #465 always return the buffer size
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9019 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/buffers/new_buffers.c
+++ src/xpra/codecs/buffers/new_buffers.c
@@ -16,11 +16,11 @@ PyObject *memory_as_pybuffer(void *ptr, Py_ssize_t buf_len, int readonly) {
     Py_ssize_t shape[] = {buf_len};
     int ret;
     if (readonly)
-    	ret = PyBuffer_FillInfo(&pybuf, NULL, ptr, buf_len, 0, PyBUF_SIMPLE);
+        ret = PyBuffer_FillInfo(&pybuf, NULL, ptr, buf_len, 0, PyBUF_SIMPLE);
     else
         ret = PyBuffer_FillInfo(&pybuf, NULL, ptr, buf_len, 0, PyBUF_WRITABLE);
     if (ret!=0)
-    	return NULL;
+        return NULL;
     pybuf.format = "B";
     pybuf.shape = shape;
     return PyMemoryView_FromBuffer(&pybuf);
@@ -31,8 +31,9 @@ int object_as_buffer(PyObject *obj, const void ** buffer, Py_ssize_t * buffer_le
     if (PyMemoryView_Check(obj)) {
         rpybuf = PyMemoryView_GET_BUFFER(obj);
         if (rpybuf->buf==NULL)
-        	return -1;
+            return -1;
         buffer[0] = rpybuf->buf;
+        *buffer_len = rpybuf->len;
         return 0;
     }
     return PyObject_AsReadBuffer(obj, buffer, buffer_len);
@@ -42,9 +43,10 @@ int object_as_write_buffer(PyObject *obj, void ** buffer, Py_ssize_t * buffer_le
     Py_buffer *wpybuf;
     if (PyMemoryView_Check(obj)) {
         wpybuf = PyMemoryView_GET_BUFFER(obj);
-		if (wpybuf->buf==NULL)
-			return -1;
+        if (wpybuf->buf==NULL)
+            return -1;
         buffer[0] = wpybuf->buf;
+        *buffer_len = wpybuf->len;
         return 0;
     }
     return PyObject_AsWriteBuffer(obj, buffer, buffer_len);
