89dda2e1275f4e434ef4abee1bf771c51f094aca
Antoine Martin <totaam@xpra.org>
2015-04-16 07:12:49 +0000

    #465: undo last commit (wrong version)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9023 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/xor/cyxor.pyx
+++ src/xpra/codecs/xor/cyxor.pyx
@@ -7,30 +7,26 @@ cdef extern from "stdlib.h":
     void* malloc(size_t __size)
     void free(void* mem)
 
-cdef extern from "../buffers/memalign.h":
-    void *xmemalign(size_t size) nogil
 
 cdef extern from "../buffers/buffers.h":
     int    object_as_buffer(object obj, const void ** buffer, Py_ssize_t * buffer_len)
-    object memory_as_pybuffer(void *ptr, Py_ssize_t buf_len, int readonly)
 
 
 def xor_str(buf, xor_string):
     assert len(buf)==len(xor_string), "cyxor cannot xor strings of different lengths (%s:%s vs %s:%s)" % (type(buf), len(buf), type(xor_string), len(xor_string))
-    cdef const unsigned char * cbuf = <unsigned char *> 0 #@DuplicatedSignature
+    cdef const unsigned char * cbuf                 #@DuplicatedSignature
     cdef Py_ssize_t cbuf_len = 0                    #@DuplicatedSignature
     assert object_as_buffer(buf, <const void**> &cbuf, &cbuf_len)==0, "cannot get buffer pointer for %s: %s" % (type(buf), buf)
-    cdef const unsigned char * xbuf = <unsigned char *> 0 #@DuplicatedSignature
+    cdef const unsigned char * xbuf                 #@DuplicatedSignature
     cdef Py_ssize_t xbuf_len = 0                    #@DuplicatedSignature
     assert object_as_buffer(xor_string, <const void**> &xbuf, &xbuf_len)==0, "cannot get buffer pointer for %s: %s" % (type(buf), buf)
     assert cbuf_len == xbuf_len, "python or cython bug? buffers don't have the same length?"
-    cdef unsigned char * out = <unsigned char *> xmemalign(cbuf_len)
+    cdef unsigned char * out = <unsigned char *> malloc(cbuf_len)
     assert out!=NULL, "failed to allocate cyxor output buffer"
     cdef int i                                      #@DuplicatedSignature
     try :
         for 0 <= i < cbuf_len:
             out[i] = cbuf[i] ^ xbuf[i]
-        return memory_as_pybuffer(out, cbuf_len, True)
+        return out[:cbuf_len]
     finally:
-        pass
-        #free(out)
+        free(out)
