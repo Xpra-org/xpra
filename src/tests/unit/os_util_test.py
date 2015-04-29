1a987cd62581d5e97dce36b5218e290b2fce9853
Antoine Martin <totaam@xpra.org>
2015-04-29 04:54:16 +0000

    #177: add test for os_util critical functions
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9186 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/unit/os_util_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2011-2014 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import unittest
+
+from xpra.os_util import strtobytes, bytestostr, _memoryview, memoryview_to_bytes
+
+
+class TestModuleFunctions(unittest.TestCase):
+
+    def check(self, str_value):
+        b = strtobytes(str_value)
+        assert b
+        s = bytestostr(b)
+        assert s
+        assert s==str_value
+        if not _memoryview:
+            return
+        mv = _memoryview(b)
+        mvb = memoryview_to_bytes(mv)
+        mvs = bytestostr(mvb)
+        assert mvs==str_value
+        
+
+    def test_strs(self):
+        for l in (1, 16, 255):
+            zeroes  = chr(0)*l
+            ones    = chr(1)*l
+            ff      = chr(255)*l
+            fe      = chr(254)*l
+            self.check(zeroes)
+            self.check(ones)
+            self.check(ff)
+            self.check(fe)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
