5d9f7d125da3431f6f600c9e6ea13e7470ee1161
Antoine Martin <totaam@xpra.org>
2015-05-15 11:32:02 +0000

    * always freeze the pixels and use the encoding queue if av-sync is set (video regions), even if the delay is zero (which it might be if the client side queue is almost empty) * remove the hard-coded value, we have the correct value to use
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9376 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/window_source.py
+++ src/xpra/server/window_source.py
@@ -1193,8 +1193,9 @@ class WindowSource(object):
         log("process_damage_regions: wid=%i, adding pixel data to encode queue (%ix%i - %s), elapsed time: %.1f ms, request time: %.1f ms",
                 self.wid, w, h, coding, 1000*(now-damage_time), 1000*(now-rgb_request_time))
         item = (window, damage_time, w, h, now, self.wid, image, coding, sequence, options)
-        av_delay = self.av_sync_delay*int(options.get("av-sync", False))
-        if av_delay==0:
+        av_sync = options.get("av-sync", False)
+        av_delay = self.av_sync_delay*int(av_sync)
+        if not av_sync:
             self.make_data_packet_cb(*item)
         else:
             #schedule encode via queue, after freezing the pixels:
@@ -1211,7 +1212,7 @@ class WindowSource(object):
             return      #nothing to encode, must have been picked off already
         #find the first item which is due
         #in seconds, same as time.time():
-        av_delay = 200/1000.0
+        av_delay = self.av_sync_delay/1000.0
         now = time.time()
         still_due = []
         pop = None
