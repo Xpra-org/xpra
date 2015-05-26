df8a85b81b9318cfb47912a93a2a1ea000bfadd6
Antoine Martin <totaam@xpra.org>
2015-05-26 11:40:42 +0000

    #371: remove legacy code: require the x11 keyboard bindings on posix platforms
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9524 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/xposix/keyboard.py
+++ src/xpra/platform/xposix/keyboard.py
@@ -1,12 +1,13 @@
 # This file is part of Xpra.
 # Copyright (C) 2010 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2011-2014 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2011-2015 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+from xpra.util import nonl
 from xpra.platform.keyboard_base import KeyboardBase
 from xpra.keyboard.mask import MODIFIER_MAP
-from xpra.keyboard.layouts import parse_xkbmap_query, xkbmap_query_tostring
+from xpra.keyboard.layouts import xkbmap_query_tostring
 from xpra.log import Logger
 log = Logger("keyboard", "posix")
 
@@ -20,98 +21,48 @@ except Exception as e:
 
 class Keyboard(KeyboardBase):
 
-    def exec_get_keyboard_data(self, cmd):
-        # Find the client's current keymap so we can send it to the server:
-        try:
-            from xpra.scripts.exec_util import safe_exec
-            returncode, out, _ = safe_exec(cmd)
-            if returncode==0:
-                return out.decode('utf-8')
-            log.error("'%s' failed with exit code %s", cmd, returncode)
-        except Exception as e:
-            log.error("error running '%s': %s", cmd, e)
-        return None
-
     def get_keymap_modifiers(self):
-        if keyboard_bindings:
-            try:
-                mod_mappings = keyboard_bindings.get_modifier_mappings()
-                if mod_mappings:
-                    #ie: {"shift" : ["Shift_L", "Shift_R"], "mod1" : "Meta_L", ...]}
-                    log("modifier mappings=%s", mod_mappings)
-                    meanings = {}
-                    for modifier,keys in mod_mappings.items():
-                        for _,keyname in keys:
-                            meanings[keyname] = modifier
-                    #probably a GTK bug? but easier to put here
-                    mod_missing = []
-                    numlock_mod = meanings.get("Num_Lock", [])
-                    if numlock_mod:
-                        mod_missing.append(numlock_mod)
-                    return  meanings, [], mod_missing
-            except Exception as e:
-                log.error("failed to use native get_modifier_mappings: %s", e, exc_info=True)
-        return self.modifiers_fallback()
-
-    def modifiers_fallback(self):
-        xmodmap_pm = self.exec_get_keyboard_data(["xmodmap", "-pm"])
-        if not xmodmap_pm:
-            log.warn("bindings are not available and 'xmodmap -pm' also failed, expect keyboard mapping problems")
+        if not keyboard_bindings:
+            log.warn("keyboard bindings are not available, expect keyboard mapping problems")
             return {}, [], []
-        #parse it so we can feed it back to xmodmap (ala "xmodmap -pke")
-        meanings = {}
-        for line in xmodmap_pm.splitlines()[1:]:
-            if not line:
-                continue
-            parts = line.split()
-            #ie: ['shift', 'Shift_L', '(0x32),', 'Shift_R', '(0x3e)']
-            if len(parts)>1:
-                nohex = [x for x in parts[1:] if not x.startswith("(")]
-                for x in nohex:
-                    #ie: meanings['Shift_L']=shift
-                    meanings[x] = parts[0]
-        log("get_keymap_modifiers parsed: meanings=%s", meanings)
-        return  meanings, [], []
+        try:
+            mod_mappings = keyboard_bindings.get_modifier_mappings()
+            if mod_mappings:
+                #ie: {"shift" : ["Shift_L", "Shift_R"], "mod1" : "Meta_L", ...]}
+                log("modifier mappings=%s", mod_mappings)
+                meanings = {}
+                for modifier,keys in mod_mappings.items():
+                    for _,keyname in keys:
+                        meanings[keyname] = modifier
+                #probably a GTK bug? but easier to put here
+                mod_missing = []
+                numlock_mod = meanings.get("Num_Lock", [])
+                if numlock_mod:
+                    mod_missing.append(numlock_mod)
+                return  meanings, [], mod_missing
+        except Exception as e:
+            log.error("failed to use native get_modifier_mappings: %s", e, exc_info=True)
 
     def get_x11_keymap(self):
-        if keyboard_bindings:
-            try:
-                return keyboard_bindings.get_keycode_mappings()
-            except Exception as e:
-                log.error("failed to use raw x11 keymap: %s", e)
-        return  {}
+        if not keyboard_bindings:
+            return  {}
+        try:
+            return keyboard_bindings.get_keycode_mappings()
+        except Exception as e:
+            log.error("failed to use raw x11 keymap: %s", e)
 
 
-    def get_keymap_spec_using_setxkbmap(self):
-        xkbmap_print = self.exec_get_keyboard_data(["setxkbmap", "-print"])
-        if xkbmap_print is None:
-            log.error("your keyboard mapping will probably be incorrect unless you are using a 'us' layout")
-        xkbmap_query = self.exec_get_keyboard_data(["setxkbmap", "-query"])
-        if xkbmap_query is None and xkbmap_print is not None:
-            log.error("the server will try to guess your keyboard mapping, which works reasonably well in most cases")
-            log.error("however, upgrading 'setxkbmap' to a version that supports the '-query' parameter is preferred")
-            xkbmap_query_struct = parse_xkbmap_query(xkbmap_query)
-        else:
-            xkbmap_query_struct = {}
-        return xkbmap_print, xkbmap_query, xkbmap_query_struct
-
-    def get_keymap_spec_from_xkb(self):
-        log("get_keymap_spec_from_xkb() keyboard_bindings=%s", keyboard_bindings)
+    def get_keymap_spec(self):
+        log("get_keymap_spec() keyboard_bindings=%s", keyboard_bindings)
         if not keyboard_bindings:
             return None
         _query_struct = keyboard_bindings.getXkbProperties()
         _query = xkbmap_query_tostring(_query_struct)
-        log("get_keymap_spec_from_xkb() query tostring(%s)=%s", _query_struct, _query)
-        xkbmap_print = self.exec_get_keyboard_data(["setxkbmap", "-print"])
-        return xkbmap_print or "", _query, _query_struct
-
-    def get_keymap_spec(self):
-        v = self.get_keymap_spec_from_xkb()
-        if not v:
-            v = self.get_keymap_spec_using_setxkbmap()
-        from xpra.util import nonl
-        log("get_keymap_spec()=%s", nonl(str(v)))
-        return v
+        log("get_keymap_spec() Xkb query tostring(%s)=%s", _query_struct, _query)
+        #we no longer support servers via xkbmap_print:
+        xkbmap_print = ""
+        log("get_keymap_spec()=(%s, %s, %s)", nonl(xkbmap_print), nonl(_query), nonl(_query_struct))
+        return xkbmap_print, _query, _query_struct
 
 
     def get_xkb_rules_names_property(self):
