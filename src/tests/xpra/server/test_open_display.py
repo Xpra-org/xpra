85262c57e56822f9732851905ef301f4ba36d6f9
Antoine Martin <totaam@xpra.org>
2015-05-31 07:43:35 +0000

    try to open the current $DISPLAY using wait_for_x_server
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9558 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/server/test_open_display.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2015 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from xpra.x11.bindings.wait_for_x_server import wait_for_x_server       #@UnresolvedImport
+import os
+
+wait_for_x_server(os.environ.get("DISPLAY", ":0"), 5)
