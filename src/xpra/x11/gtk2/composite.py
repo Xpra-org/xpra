40fff78cf0a8aba83ecb1e91e6b1b4b663f0e2f7
Antoine Martin <totaam@xpra.org>
2015-07-05 04:08:26 +0000

    log xshm flag
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9818 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/gtk2/composite.py
+++ src/xpra/x11/gtk2/composite.py
@@ -50,7 +50,7 @@ class CompositeHelper(AutoPropGObjectMixin, gobject.GObject):
     # This may raise XError.
     def __init__(self, window, already_composited, use_shm=False):
         super(CompositeHelper, self).__init__()
-        log("CompositeHelper.__init__(%#x, %s)", window.xid, already_composited)
+        log("CompositeHelper.__init__(%#x, %s, %s)", window.xid, already_composited, use_shm)
         self._window = window
         self._already_composited = already_composited
         self._listening_to = None
