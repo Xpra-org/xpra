4f2ad0b4308c277af0150df73010ef6040dcc5bf
Antoine Martin <totaam@xpra.org>
2015-07-16 07:18:24 +0000

    we have to actually read / do something with the image data to ensure the decoder is used
    git-svn-id: https://xpra.org/svn/Xpra/trunk@9945 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/pillow/decode.py
+++ src/xpra/codecs/pillow/decode.py
@@ -55,6 +55,9 @@ def selftest(full=False):
             buf = BytesIOClass(cdata)
             img = PIL.Image.open(buf)
             assert img, "failed to open image data"
+            data_fn = getattr(img, "tobytes", getattr(img, "tostring"))
+            raw_data = data_fn("raw", img.mode)
+            assert raw_data
             #now try with junk:
             cdata = binascii.unhexlify("ABCD"+hexdata)
             buf = BytesIOClass(cdata)
