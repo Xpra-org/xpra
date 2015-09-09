030b98f7174514bd16c19360d2bfcb3ac3c50df3
Antoine Martin <totaam@xpra.org>
2015-09-09 11:56:12 +0000

    fix transparency with cairo: must replace pixels and not just overwrite them
    git-svn-id: https://xpra.org/svn/Xpra/trunk@10577 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/cairo_backing_base.py
+++ src/xpra/client/gtk_base/cairo_backing_base.py
@@ -77,6 +77,7 @@ class CairoBackingBase(GTKWindowBacking):
         log("cairo_paint_pixbuf(%s, %s, %s) backing=%s", pixbuf, x, y, self._backing)
         #now use it to paint:
         gc = gdk_cairo_context(cairo.Context(self._backing))
+        gc.set_operator(cairo.OPERATOR_SOURCE)
         cairo_set_source_pixbuf(gc, pixbuf, x, y)
         gc.paint()
 
@@ -88,7 +89,7 @@ class CairoBackingBase(GTKWindowBacking):
         gc.set_operator(cairo.OPERATOR_CLEAR)
         gc.rectangle(x, y, img_surface.get_width(), img_surface.get_height())
         gc.fill()
-        gc.set_operator(cairo.OPERATOR_OVER)
+        gc.set_operator(cairo.OPERATOR_SOURCE)
         gc.set_source_surface(img_surface, x, y)
         gc.paint()
 
