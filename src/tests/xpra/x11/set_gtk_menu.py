8ce4d7b21f2d630cb0be197409268559aab33645
Antoine Martin <totaam@xpra.org>
2015-10-02 09:04:17 +0000

    #228 much improved test app
    git-svn-id: https://xpra.org/svn/Xpra/trunk@10716 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/x11/set_gtk_menu.py
+++ src/tests/xpra/x11/set_gtk_menu.py
@@ -4,44 +4,60 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import copy
 import sys
-from xpra.x11.gtk2.gdk_display_source import display    #@UnresolvedImport
-from xpra.x11.gtk_x11.prop import prop_set
-from xpra.dbus.helper import DBusHelper
+from xpra.x11.gtk2.gdk_display_source import display  # @UnresolvedImport
+assert display
+from xpra.x11.gtk_x11.prop import prop_set, prop_del
 from xpra.dbus.gtk_menuactions import Menus, Actions
 
-#beware: this import has side-effects:
+# beware: this import has side-effects:
 import dbus.glib
 assert dbus.glib
 import gtk
+import gobject
 
-from xpra.dbus.common import loop_init
 
+class WindowWithMenu(gtk.Window):
 
-def main(args):
-    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    window.set_size_request(200, 200)
-    window.connect("delete_event", gtk.mainquit)
-    window.realize()
-    w = window.get_window()
+    def __init__(self, *args):
+        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        self.set_size_request(320, 500)
+        self.connect("delete_event", gtk.mainquit)
+        vbox = gtk.VBox()
+        self.add(vbox)
+        vbox.add(gtk.Label("Delay before actions:"))
+        self.delay_entry = gtk.Entry(max=10)
+        self.delay_entry.set_text("0")
+        vbox.add(self.delay_entry)
+        vbox.add(gtk.Label("App ID:"))
+        self.app_id_entry = gtk.Entry(max=40)
+        self.app_id_entry.set_text("org.xpra.ExampleMenu")
+        vbox.add(self.app_id_entry)
+        self.app_path_entry = gtk.Entry(max=40)
+        self.app_path_entry.set_text("/org/xpra/ExampleMenu")
+        vbox.add(self.app_path_entry)
+        btn = gtk.Button("Publish Menu")
+        btn.connect("clicked", self.delayed_call, self.publish_menu)
+        vbox.add(btn)
+        btn = gtk.Button("Remove Menu")
+        btn.connect("clicked", self.delayed_call, self.remove_menu)
+        vbox.add(btn)
+        btn = gtk.Button("Toggle Menu")
+        btn.connect("clicked", self.delayed_call, self.toggle_menu)
+        vbox.add(btn)
+        #no services to begin with:
+        self.window_actions_service, self.app_actions_service, self.menus_service = None, None, None
+        self.init_defaults()
+        
 
-    dbus_helper = DBusHelper()
-    loop_init()
-    session_bus = dbus_helper.get_session_bus()
-
-    app_id = u"org.xpra.ExampleMenu"
-    app_path = u"/org/xpra/ExampleMenu"
-    menu_path = u"%s/menus/appmenu" % app_path
-    window_path = u"%s/window/1" % app_path
-    bus_name = session_bus.get_unique_name().decode()
-
-    def action_cb(*args):
-        print("action_cb%s" % str(args))
-    window_actions={'reset'         : [True, 'b', [], action_cb],
-                    'fullscreen'    : [True, '', [0], action_cb],
-                    'about'         : [True, '', [], action_cb],
-                    'preferences'   : [True, '', [], action_cb],
-                    'switch-tab'    : [True, 'i', [], action_cb],
+    def init_defaults(self):
+        self.window_actions = {
+                    'reset'         : [True, 'b', [], self.action_cb],
+                    'fullscreen'    : [True, '', [0], self.action_cb],
+                    'about'         : [True, '', [], self.action_cb],
+                    'preferences'   : [True, '', [], self.action_cb],
+                    'switch-tab'    : [True, 'i', [], self.action_cb],
                     'detach-tab'    : [True, '', []],
                     'save-contents' : [True, '', []],
                     'edit-profile'  : [True, 's', []],
@@ -55,9 +71,9 @@ def main(args):
                     'copy'          : [True, '', []],
                     'paste'         : [True, 's', []],
                     'find'          : [True, 's', []],
-                    'help'          : [True, '', []]}
-    Actions(app_id, window_path, session_bus, window_actions)
-    app_actions = {
+                    'help'          : [True, '', []]
+                }
+        self.app_actions = {
                     'quit'          : [True, '', []],
                     'about'         : [True, '', []],
                     'activate-tab'  : [True, 's', []],
@@ -65,10 +81,9 @@ def main(args):
                     'help'          : [True, '', []],
                     'custom'        : [True, '', []],
                   }
-    Actions(app_id, app_path, session_bus, app_actions)
-    menus = {0:
+        self.initial_menus = {0:
              {
-                0: [{':section': (0, 1)}, {':section': (0, 2)}, {':section': (0, 3)}],
+                0: [{'action': 'app.help', 'label': '_Help'}, {':section': (0, 1)}, {':section': (0, 2)}, {':section': (0, 3)}],
                 1: [{'action': 'win.new-terminal', 'label': '_New Terminal', 'target': ['default', 'default']}],
                 2: [{'action': 'app.preferences', 'label': '_Preferences'}],
                 3: [{'action': 'app.help', 'label': '_Help'},
@@ -76,40 +91,122 @@ def main(args):
                     {'action': 'app.quit', 'label': '_Quit'}
                    ]
              },
-             #not shown anywhere:
-             #1:
-             #{
+             # not shown anywhere when defined (group=1):
+             # 1:
+             # {
              #   0: [{':section': (0, 1)}],
              #   1: [{'action': 'app.custom', 'label': '_Custom'}]
-             #},
+             # },
             }
-    menus_service = Menus(app_id, menu_path, session_bus, menus)
+        self.alt_menus1 = {0:
+             {
+                0: [{'action': 'app.help', 'label': '_Help'}, {':section': (0, 1)}, {':section': (0, 2)}, {':section': (0, 3)}],
+                1: [{'action': 'win.new-terminal', 'label': '_New Terminal', 'target': ['default', 'default']}],
+                2: [{'action': 'app.preferences', 'label': '_Preferences'}],
+                3: [{'action': 'app.help', 'label': '_Help'},
+                    {'action': 'app.about', 'label': '_About'},
+                    {'action': 'app.quit', 'label': '_Quit'}
+                   ]
+             },
+             # not shown anywhere when defined (group=1):
+             # 1:
+             # {
+             #   0: [{':section': (0, 1)}],
+             #   1: [{'action': 'app.custom', 'label': '_Custom'}]
+             # },
+            }
+        self.alt_menus2 = {}
+        self.alt_menus2 = copy.deepcopy(self.alt_menus1)
+        # remove about:
+        help_about_quit = self.alt_menus2[0][3]
+        help_about_quit.remove(help_about_quit[1])
+        self.current_menus = self.initial_menus
 
-    def pset(key, value):
-        return prop_set(w, key, "utf8", value)
-    pset("_GTK_APP_MENU_OBJECT_PATH",       menu_path)
-    pset("_GTK_WINDOW_OBJECT_PATH",         window_path)
-    pset("_GTK_APPLICATION_OBJECT_PATH",    app_path)
-    pset("_GTK_UNIQUE_BUS_NAME",            bus_name)
-    pset("_GTK_APPLICATION_ID",             app_id)
-    print("gtk menu properties for window %#x on display %s" % (w.xid, display.get_name()))
 
-    import copy
-    new_menus = copy.deepcopy(menus)
-    #remove about:
-    help_about_quit = new_menus[0][3]
-    help_about_quit.remove(help_about_quit[1])
+    def delayed_call(self, btn, fn):
+        print("delayed_call(%s, %s)" % (btn, fn))
+        delay = int(self.delay_entry.get_text())
+        gobject.timeout_add(delay*1000, fn)
 
-    both_menus = [menus, new_menus]
-    def toggle_menu(*args):
-        menus_service.set_menus(both_menus[0])
-        saved = list(both_menus)
-        both_menus[0] = saved[1]
-        both_menus[1] = saved[0]
-        return True
-    import gobject
-    gobject.timeout_add(1000*5, toggle_menu)
-    window.show()
+    def publish_menu(self, *args):
+        print("publish_menu%s" % str(args))
+        self.stop_dbus_services()
+        self.set_props()
+        self.setup_dbus_services()
+        self.set_X11_props()
+
+    def remove_menu(self, *args):
+        print("remove_menu%s" % str(args))
+        self.remove_X11_props()
+        self.stop_dbus_services()
+
+
+    def set_props(self):
+        self.app_id = self.app_id_entry.get_text().decode()
+        self.app_path = self.app_path_entry.get_text().decode()
+        self.menu_path = u"%s/menus/appmenu" % self.app_path
+        self.window_path = u"%s/window/1" % self.app_path
+        from xpra.dbus.common import init_session_bus
+        session_bus = init_session_bus()
+        self.bus_name = session_bus.get_unique_name().decode()
+
+    def stop_dbus_services(self):
+        for x in (self.window_actions_service, self.app_actions_service, self.menus_service):
+            if x:
+                x.remove_from_connection()
+        self.window_actions_service, self.app_actions_service, self.menus_service = None, None, None
+
+    def setup_dbus_services(self):
+        from xpra.dbus.common import init_session_bus
+        session_bus = init_session_bus()
+        self.window_actions_service = Actions(self.app_id, self.window_path, session_bus, self.window_actions)
+        self.app_actions_service = Actions(self.app_id, self.app_path, session_bus, self.app_actions)
+        self.menus_service = Menus(self.app_id, self.menu_path, session_bus, self.current_menus)
+
+    def set_X11_props(self):
+        w = self.get_window()
+        def pset(key, value):
+            return prop_set(w, key, "utf8", value)
+        pset("_GTK_APP_MENU_OBJECT_PATH", self.menu_path)
+        pset("_GTK_WINDOW_OBJECT_PATH", self.window_path)
+        pset("_GTK_APPLICATION_OBJECT_PATH", self.app_path)
+        pset("_GTK_UNIQUE_BUS_NAME", self.bus_name)
+        pset("_GTK_APPLICATION_ID", self.app_id)
+
+    def remove_X11_props(self):
+        w = self.get_window()
+        def pdel(key):
+            return prop_del(w, key)
+        pdel("_GTK_APP_MENU_OBJECT_PATH")
+        pdel("_GTK_WINDOW_OBJECT_PATH")
+        pdel("_GTK_APPLICATION_OBJECT_PATH")
+        pdel("_GTK_UNIQUE_BUS_NAME")
+        pdel("_GTK_APPLICATION_ID")
+
+
+    def toggle_menu(self, *args):
+        print("toggle_menu()")
+        if self.current_menus == self.alt_menus1:
+            m = self.alt_menus2
+        else:
+            m = self.alt_menus1
+        self.current_menus = m
+        if self.menus_service:
+            self.menus_service.set_menus(self.current_menus)
+
+
+    def action_cb(self, *args):
+        print("action_cb%s" % str(args))
+
+
+
+def main(args):
+    from xpra.dbus.common import init_session_bus
+    from xpra.dbus.common import loop_init
+    loop_init()
+    init_session_bus()
+    w = WindowWithMenu()
+    w.show_all()
     gtk.main()
 
 
