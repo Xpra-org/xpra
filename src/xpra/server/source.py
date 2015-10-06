a9b1bc5651f639e7f998c8fe20d25d3be224ef90
Antoine Martin <totaam@xpra.org>
2015-10-06 07:19:34 +0000

    shadow servers export this property
    git-svn-id: https://xpra.org/svn/Xpra/trunk@10742 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/source.py
+++ src/xpra/server/source.py
@@ -106,7 +106,7 @@ def make_window_metadata(window, propname, get_transient_for=None, get_window_id
     elif propname in ("iconic", "fullscreen", "maximized", "decorations", "above", "below", "shaded", "sticky", "skip-taskbar", "skip-pager", "modal", "focused"):
         #always send these when requested
         return {propname : bool(raw())}
-    elif propname in ("has-alpha", "override-redirect", "tray"):
+    elif propname in ("has-alpha", "override-redirect", "tray", "shadow"):
         v = raw()
         if v is False:
             #save space: all these properties are assumed false if unspecified
