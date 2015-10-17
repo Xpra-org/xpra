9040fc0680d2a1808a8ac3d7ae8c407f4ae3215d
Antoine Martin <totaam@xpra.org>
2015-10-17 13:07:22 +0000

    workaround newer versions of Pillow which log at debug level
    git-svn-id: https://xpra.org/svn/Xpra/trunk@10898 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/pillow/__init__.py
+++ src/xpra/codecs/pillow/__init__.py
@@ -3,15 +3,28 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import os
+import logging
+PIL_DEBUG = os.environ.get("XPRA_PIL_DEBUG", "0")=="1"
+if PIL_DEBUG:
+    from xpra.log import Logger
+    log = Logger("encoder", "pillow")
+    log.info("enabling PIL.DEBUG")
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+
+#newer versions use this logger,
+#we must initialize it before we load the class:
+for x in ("Image", "PngImagePlugin", "WebPImagePlugin", "JpegImagePlugin"):
+    logger = logging.getLogger("PIL.%s" % x)
+    logger.setLevel(level)
+
 import PIL                      #@UnresolvedImport
 from PIL import Image           #@UnresolvedImport
 assert PIL is not None and Image is not None, "failed to load Pillow"
 PIL_VERSION = PIL.PILLOW_VERSION
-
-import os
-if os.environ.get("XPRA_PIL_DEBUG", "0")=="1":
-    from xpra.log import Logger
-    log = Logger("encoder", "pillow")
-    log.info("enabling PIL.DEBUG")
-    Image.DEBUG = 1
+if hasattr(Image, "DEBUG"):
+    #for older versions (pre 3.0), use Image.DEBUG flag:
+    Image.DEBUG = int(PIL_DEBUG)
 Image.init()
