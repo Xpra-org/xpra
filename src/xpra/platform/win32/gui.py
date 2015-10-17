2e94135f583cde037b0c7e850706fe83c5106667
Antoine Martin <totaam@xpra.org>
2015-10-17 05:40:45 +0000

    log area and dimensions
    git-svn-id: https://xpra.org/svn/Xpra/trunk@10889 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/gui.py
+++ src/xpra/platform/win32/gui.py
@@ -415,7 +415,8 @@ def get_workarea():
         minmy = min(x[1] for x in monitors)
         maxmx = max(x[2] for x in monitors)
         maxmy = max(x[3] for x in monitors)
-        screenlog("get_workarea() absolute total monitor dimensions: %s", (minmx, minmy, maxmx, maxmy))
+        screenlog("get_workarea() absolute total monitor area: %s", (minmx, minmy, maxmx, maxmy))
+        screenlog(" total monitor dimensions: %s", (maxmx-minmx, maxmy-minmy))
         workareas = []
         for m in win32api.EnumDisplayMonitors(None, None):
             mi = win32api.GetMonitorInfo(m[0])
