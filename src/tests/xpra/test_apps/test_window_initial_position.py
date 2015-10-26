f71d66da5438ac6f982a905cfe247b36d0bf84f5
Antoine Martin <totaam@xpra.org>
2015-10-26 09:05:19 +0000

    make it possible to specify the initial position desired on the command line as arguments - still defaults to 0,0
    git-svn-id: https://xpra.org/svn/Xpra/trunk@11023 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_initial_position.py
+++ src/tests/xpra/test_apps/test_window_initial_position.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+import sys
 import gtk
 
 width = 400
 height = 200
+
 def main():
+	x, y = 0, 0
+	if len(sys.argv)==3:
+		x, y = int(sys.argv[1]), int(sys.argv[2])
 	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 	window.set_size_request(width, height)
 	window.connect("delete_event", gtk.mainquit)
-	window.move(0, 0)
+	window.move(x, y)
 	window.show_all()
 	gtk.main()
-	return 0
-
+		
 
 if __name__ == "__main__":
 	main()
