6eba4b3ef4d2e396a1009f0939644c1b398b31f4
Antoine Martin <totaam@xpra.org>
2015-10-26 10:39:02 +0000

    example app which uses client side header bar (yuk)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@11028 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/xpra/test_apps/gtk_header_bar_window.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from gi.repository import Gtk, Gio
+
+class HeaderBarWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Stack Demo")
+        self.set_border_width(10)
+        self.set_default_size(400, 200)
+
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = "HeaderBar example"
+        self.set_titlebar(hb)
+
+        button = Gtk.Button()
+        icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        button.add(image)
+        hb.pack_end(button)
+        
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
+        box.add(button)
+
+        button = Gtk.Button()
+        button.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
+        box.add(button)
+
+        hb.pack_start(box)
+        
+        self.add(Gtk.TextView())
+
+win = HeaderBarWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
