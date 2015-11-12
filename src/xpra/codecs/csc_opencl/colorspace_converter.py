e5ef2c686dbeee176b20413d9dad7779caae5272
Antoine Martin <totaam@xpra.org>
2015-11-12 23:57:36 +0000

    remove unused import
    git-svn-id: https://xpra.org/svn/Xpra/trunk@11198 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/csc_opencl/colorspace_converter.py
+++ src/xpra/codecs/csc_opencl/colorspace_converter.py
@@ -14,7 +14,7 @@ import pyopencl             #@UnresolvedImport
 from pyopencl import mem_flags  #@UnresolvedImport
 
 from xpra.util import updict, engs
-from xpra.os_util import memoryview_to_bytes, _memoryview
+from xpra.os_util import _memoryview
 
 PREFERRED_DEVICE_TYPE = os.environ.get("XPRA_OPENCL_DEVICE_TYPE", "GPU")
 PREFERRED_DEVICE_NAME = os.environ.get("XPRA_OPENCL_DEVICE_NAME", "")
