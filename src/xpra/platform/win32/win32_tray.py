05f36ae9f263a597b84b03bdb8c1e3d25c54576b
Antoine Martin <totaam@xpra.org>
2015-12-13 14:46:03 +0000

    #636: calculate the screen offset for the tray, and re-calculate when the monitor layout changes
    git-svn-id: https://xpra.org/svn/Xpra/trunk@11389 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/win32_tray.py
+++ src/xpra/platform/win32/win32_tray.py
@@ -14,6 +14,7 @@ log = Logger("tray", "win32")
 
 from xpra.platform.win32.win32_NotifyIcon import win32NotifyIcon
 from xpra.client.tray_base import TrayBase
+from xpra.platform.win32.win32_events import get_win32_event_listener
 
 
 class Win32Tray(TrayBase):
@@ -22,8 +23,10 @@ class Win32Tray(TrayBase):
         TrayBase.__init__(self, *args)
         self.default_icon_extension = "ico"
         self.default_icon_name = "xpra.ico"
+        self.calculate_offset()
         icon_filename = self.get_tray_icon_filename(self.default_icon_filename)
         self.tray_widget = win32NotifyIcon(self.tooltip, self.move_cb, self.click_cb, self.exit_cb, None, icon_filename)
+        get_win32_event_listener().add_event_callback(win32con.WM_DISPLAYCHANGE, self.calculate_offset)
 
     def ready(self):
         pass
@@ -45,7 +48,22 @@ class Win32Tray(TrayBase):
         if self.tray_widget:
             self.tray_widget.close()
             self.tray_widget = None
+        get_win32_event_listener().remove_event_callback(win32con.WM_DISPLAYCHANGE, self.calculate_offset)
         log("Win32Tray.cleanup() ended")
+
+    def calculate_offset(self, *args):
+        #GTK returns coordinates as unsigned ints, but win32 can give us negative coordinates!
+        self.offset_x = 0
+        self.offset_y = 0
+        try:
+            for m in win32api.EnumDisplayMonitors(None, None):
+                mi = win32api.GetMonitorInfo(m[0])
+                mx1, my1, _, _ = mi['Monitor']
+                self.offset_x = max(self.offset_x, -mx1)
+                self.offset_y = max(self.offset_y, -my1)
+        except Exception as e:
+            log.warn("failed to query monitors: %s", e)
+        log("calculate_offset() x=%i, y=%i", self.offset_x, self.offset_y)
 
     def set_tooltip(self, name):
         if self.tray_widget:
@@ -71,6 +89,8 @@ class Win32Tray(TrayBase):
     def move_cb(self, *args):
         x, y = win32api.GetCursorPos()
         size = win32api.GetSystemMetrics(win32con.SM_CXSMICON)
+        x += self.offset_x
+        y += self.offset_y
         log("move_cb%s x=%s, y=%s, size=%s", args, x, y, size)
         self.recalculate_geometry(x, y, size, size)
         if self.mouseover_cb:
