a329375107b04f6214a5800292c99bb44ad3ee13
Antoine Martin <totaam@xpra.org>
2015-12-21 02:23:18 +0000

    correct libc location on FreeBSD - apparently
    git-svn-id: https://xpra.org/svn/Xpra/trunk@11450 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/net_util.py
+++ src/xpra/net/net_util.py
@@ -129,7 +129,7 @@ if not sys.platform.startswith("win"):
 	elif sys.platform.startswith("sunos"):
 		library = "libsocket.so.1"
 	elif sys.platform.startswith("freebsd"):
-		library = "/usr/lib/libc.so"
+		library = "/lib/libc.so.7"
 	elif sys.platform.startswith("openbsd"):
 		library = "libc.so"
 	try:
