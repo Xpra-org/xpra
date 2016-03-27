6359bc521305806d2f20b76ccc06992bef3e4615
Antoine Martin <totaam@xpra.org>
2016-03-27 05:06:30 +0000

    * identation: make it consistent with the rest of the code (use spaces - yuk!) * use code closer to the "official" example
    git-svn-id: https://xpra.org/svn/Xpra/trunk@12233 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/avahi_publisher.py
+++ src/xpra/net/avahi_publisher.py
@@ -9,10 +9,10 @@
 import avahi
 import dbus
 try:
-	from dbus.exceptions import DBusException
+    from dbus.exceptions import DBusException
 except:
-	#not available in all versions of the bindings?
-	DBusException = Exception
+    #not available in all versions of the bindings?
+    DBusException = Exception
 from xpra.net import XPRA_MDNS_TYPE
 
 from xpra.log import Logger
@@ -21,163 +21,179 @@ log = Logger("network", "mdns")
 from xpra.dbus.common import init_system_bus
 from xpra.net.net_util import get_iface, if_nametoindex, if_indextoname
 
-SHOW_INTERFACE = True			#publishes the name of the interface we broadcast from
+SHOW_INTERFACE = True            #publishes the name of the interface we broadcast from
 
 
 def get_interface_index(host):
-	log("get_interface_index(%s)", host)
-	if host == "0.0.0.0" or host =="" or host=="*":
-		return	avahi.IF_UNSPEC
+    log("get_interface_index(%s)", host)
+    if host == "0.0.0.0" or host =="" or host=="*":
+        return avahi.IF_UNSPEC
 
-	if not if_nametoindex:
-		log.error("cannot convert interface to index (if_nametoindex is missing), so returning 'IF_UNSPEC', avahi will publish on ALL interfaces")
-		return	avahi.IF_UNSPEC
+    if not if_nametoindex:
+        log.error("cannot convert interface to index (if_nametoindex is missing), so returning 'IF_UNSPEC', avahi will publish on ALL interfaces")
+        return avahi.IF_UNSPEC
 
-	iface = get_iface(host)
-	log("get_iface(%s)=%s", host, iface)
-	if iface is None:
-		return	avahi.IF_UNSPEC
+    iface = get_iface(host)
+    log("get_iface(%s)=%s", host, iface)
+    if iface is None:
+        return avahi.IF_UNSPEC
 
-	index = if_nametoindex(iface)
-	log("if_nametoindex(%s)=%s", iface, index)
-	if iface is None:
-		return	avahi.IF_UNSPEC
-	return index
+    index = if_nametoindex(iface)
+    log("if_nametoindex(%s)=%s", iface, index)
+    if iface is None:
+        return avahi.IF_UNSPEC
+    return index
 
 
 class AvahiPublishers:
-	"""
-	Aggregates a number of AvahiPublisher(s).
-	This takes care of constructing the appropriate AvahiPublisher
-	with the interface index and port for the given list of (host,port)s to broadcast on,
-	and to convert the text dict into a TXT string.
-	"""
+    """
+    Aggregates a number of AvahiPublisher(s).
+    This takes care of constructing the appropriate AvahiPublisher
+    with the interface index and port for the given list of (host,port)s to broadcast on,
+    and to convert the text dict into a TXT string.
+    """
 
-	def __init__(self, listen_on, service_name, service_type=XPRA_MDNS_TYPE, text_dict={}):
-		self.publishers = []
-		try:
-			bus = init_system_bus()
-		except Exception as e:
-			log.warn("failed to connect to the system dbus: %s", e)
-			log.warn(" either start a dbus session or disable mdns support")
-			return
-		for host, port in listen_on:
-			iface_index = get_interface_index(host)
-			log("iface_index(%s)=%s", host, iface_index)
-			td = text_dict
-			if SHOW_INTERFACE and if_indextoname and iface_index is not None:
-				td = text_dict.copy()
-				td["iface"] = if_indextoname(iface_index)
-			txt = []
-			if text_dict:
-				for k,v in text_dict.items():
-					txt.append("%s=%s" % (k,v))
-			if host=="0.0.0.0":
-				host = ""
-			else:
-				try:
-					import socket
-					host = socket.gethostbyaddr(host)[0]
-				except:
-					pass
-			self.publishers.append(AvahiPublisher(bus, service_name, port, service_type, domain="", host=host, text=txt, interface=iface_index))
+    def __init__(self, listen_on, service_name, service_type=XPRA_MDNS_TYPE, text_dict={}):
+        self.publishers = []
+        try:
+            bus = init_system_bus()
+        except Exception as e:
+            log.warn("failed to connect to the system dbus: %s", e)
+            log.warn(" either start a dbus session or disable mdns support")
+            return
+        for host, port in listen_on:
+            iface_index = get_interface_index(host)
+            log("iface_index(%s)=%s", host, iface_index)
+            td = text_dict
+            if SHOW_INTERFACE and if_indextoname and iface_index is not None:
+                td = text_dict.copy()
+                td["iface"] = if_indextoname(iface_index)
+            txt = []
+            if text_dict:
+                for k,v in text_dict.items():
+                    txt.append("%s=%s" % (k,v))
+            if host=="0.0.0.0":
+                host = ""
+            else:
+                try:
+                    import socket
+                    host = socket.gethostbyaddr(host)[0]
+                except:
+                    pass
+            self.publishers.append(AvahiPublisher(bus, service_name, port, service_type, domain="", host=host, text=txt, interface=iface_index))
 
-	def start(self):
-		log("avahi:starting: %s", self.publishers)
-		if not self.publishers:
-			return
-		all_err = True
-		for publisher in self.publishers:
-			if publisher.start():
-				all_err = False
-		if all_err:
-			log.warn(" to avoid this warning, disable mdns support ")
-			log.warn(" using the 'mdns=no' option")
+    def start(self):
+        log("avahi:starting: %s", self.publishers)
+        if not self.publishers:
+            return
+        all_err = True
+        for publisher in self.publishers:
+            if publisher.start():
+                all_err = False
+        if all_err:
+            log.warn(" to avoid this warning, disable mdns support ")
+            log.warn(" using the 'mdns=no' option")
 
-	def stop(self):
-		log("stopping: %s", self.publishers)
-		for publisher in self.publishers:
-			publisher.stop()
+    def stop(self):
+        log("stopping: %s", self.publishers)
+        for publisher in self.publishers:
+            publisher.stop()
 
 
 class AvahiPublisher:
 
-	def __init__(self, bus, name, port, stype=XPRA_MDNS_TYPE, domain="", host="", text=[], interface=avahi.IF_UNSPEC):
-		log("AvahiPublisher%s", (bus, name, port, stype, domain, host, text, interface))
-		self.bus = bus
-		self.name = name
-		self.stype = stype
-		self.domain = domain
-		self.host = host
-		self.port = port
-		self.text = avahi.string_array_to_txt_array(text)
-		self.interface = interface
-		self.group = None
+    def __init__(self, bus, name, port, stype=XPRA_MDNS_TYPE, domain="", host="", text=[], interface=avahi.IF_UNSPEC):
+        log("AvahiPublisher%s", (bus, name, port, stype, domain, host, text, interface))
+        self.bus = bus
+        self.name = name
+        self.stype = stype
+        self.domain = domain
+        self.host = host
+        self.port = port
+        self.text = avahi.string_array_to_txt_array(text)
+        self.interface = interface
+        self.server = None
+        self.group = None
 
-	def info(self):
-		def iface():
-			if self.interface>0:
-				return "interface %i" % self.interface
-			return "all interfaces"
-		return	"%s %s:%s %s" % (self.name, self.host, self.port, iface())
+    def get_info(self):
+        def iface():
+            if self.interface>0:
+                return "interface %i" % self.interface
+            return "all interfaces"
+        return "%s %s:%s %s" % (self.name, self.host, self.port, iface())
 
-	def __repr__(self):
-		return	"AvahiPublisher(%s)" % self.info()
+    def __repr__(self):
+        return "AvahiPublisher(%s)" % self.get_info()
 
-	def start(self):
-		try:
-			server = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
-			g = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, server.EntryGroupNew()), avahi.DBUS_INTERFACE_ENTRY_GROUP)
-		except Exception as e:
-			log.warn("failed to connect to avahi's dbus interface: %s", e)
-			return False
+    def start(self):
+        try:
+            self.server = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
+            self.group = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.EntryGroupNew()), avahi.DBUS_INTERFACE_ENTRY_GROUP)
+        except Exception as e:
+            log.warn("failed to connect to avahi's dbus interface: %s", e)
+            return False
+        log("avahi dbus server=%s", self.server)
+        log("avahi dbus group=%s", self.group)
+        self.server.connect_to_signal("StateChanged", self.server_state_changed)
+        self.server_state_changed(self.server.GetState())
 
-		try:
-			args = (self.interface, avahi.PROTO_UNSPEC,dbus.UInt32(0),
-						 self.name, self.stype, self.domain, self.host,
-						 dbus.UInt16(self.port), self.text)
-			log("calling %s%s", g, args)
-			g.AddService(*args)
-			g.Commit()
-			self.group = g
-			log("dbus service added")
-		except DBusException as e:
-			#use try+except as older versions may not have those modules?
-			message = e.get_dbus_message()
-			dbus_error_name = e.get_dbus_name()
-			if dbus_error_name=="org.freedesktop.Avahi.CollisionError":
-				log.error("error starting publisher %s: another instance already claims this dbus name: %s, message: %s", self, e, message)
-				return
-			log.warn("Warning: failed to start Avahi publisher for %s", self.info())
-			for l in str(e).splitlines():
-				for x in l.split(":", 1):
-					if x:
-						log.warn(" %s", x)
-			return False
-		return True
+    def server_state_changed(self, state):
+        log("server_state_changed(%s) on %s", state, self.server)
+        if state == avahi.SERVER_COLLISION:
+            log.error("Warning: mdns server name collision")
+            self.stop()
+        elif state == avahi.SERVER_RUNNING:
+            self.add_service()
 
-	def stop(self):
-		log("%s.stop() group=%s", self, self.group)
-		if self.group:
-			try:
-				self.group.Reset()
-				self.group = None
-			except Exception as e:
-				log.error("error stopping publisher %s: %s", self, e)
+    def add_service(self):
+        try:
+            args = (self.interface, avahi.PROTO_UNSPEC, dbus.UInt32(0),
+                         self.name, self.stype, self.domain, self.host,
+                         dbus.UInt16(self.port), self.text)
+            log("calling %s%s", self.group, args)
+            self.group.AddService(*args)
+            self.group.Commit()
+            log("dbus service added")
+        except DBusException as e:
+            #use try+except as older versions may not have those modules?
+            message = e.get_dbus_message()
+            dbus_error_name = e.get_dbus_name()
+            if dbus_error_name=="org.freedesktop.Avahi.CollisionError":
+                log.error("error starting publisher %s: another instance already claims this dbus name: %s, message: %s", self, e, message)
+                return
+            log.warn("Warning: failed to start Avahi publisher for %s", self.info())
+            for l in str(e).splitlines():
+                for x in l.split(":", 1):
+                    if x:
+                        log.warn(" %s", x)
+            return False
+        return True
+
+    def stop(self):
+        group = self.group
+        log("%s.stop() group=%s", self, group)
+        if group:
+            self.group = None
+            try:
+                group.Reset()
+            except Exception as e:
+                log.error("Error stopping avahi publisher %s:", self)
+                log.error(" %s", e)
 
 
 def main():
-	import glib
-	import random, signal
-	port = int(20000*random.random())+10000
-	host = "0.0.0.0"
-	name = "test service"
-	publisher = AvahiPublisher(name, port, stype=XPRA_MDNS_TYPE, host=host, text=["somename:somevalue"])
-	assert publisher
-	glib.idle_add(publisher.start)
-	signal.signal(signal.SIGTERM, exit)
-	glib.MainLoop().run()
+    import glib
+    import random, signal
+    port = int(20000*random.random())+10000
+    host = ""
+    name = "test service"
+    bus = init_system_bus()
+    publisher = AvahiPublisher(bus, name, port, stype=XPRA_MDNS_TYPE, host=host, text=["somename:somevalue"])
+    assert publisher
+    glib.idle_add(publisher.start)
+    signal.signal(signal.SIGTERM, exit)
+    glib.MainLoop().run()
 
 
 if __name__ == "__main__":
-	main()
+    main()
