bc41a6ef18178e3496f6d16ddadb0df01b3db0ad
Antoine Martin <totaam@xpra.org>
2016-03-27 12:30:42 +0000

    safer platform module lookup: try sys.modules directly just in case (the bug I saw was probably just due to screwed library directory)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@12240 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/version_util.py
+++ src/xpra/version_util.py
@@ -107,6 +107,7 @@ def get_version_info_full():
 
 def do_get_platform_info():
     from xpra.os_util import platform_name
+    pp = sys.modules.get("platform", pp)
     def get_processor_name():
         if pp.system() == "Windows":
             return pp.processor()
@@ -128,16 +129,13 @@ def do_get_platform_info():
         info["linux_distribution"] = pp.linux_distribution()
     info.update({
             ""          : sys.platform,
+            "name"      : platform_name(sys.platform, info.get("linux_distribution") or pp.release()),
             "release"   : pp.release(),
             "platform"  : pp.platform(),
             "machine"   : pp.machine(),
             "processor" : pp.processor(),
             "architecture" : pp.architecture(),
             })
-    try:
-        info["name"] = platform_name(sys.platform, info.get("linux_distribution") or pp.release())
-    except:
-        pass
     try:
         info["processor"] = get_processor_name()
     except:
