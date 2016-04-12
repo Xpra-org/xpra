701eed1afe5c4bffbaf142faf813cc1dcbf13b6a
Antoine Martin <totaam@xpra.org>
2016-04-12 11:04:15 +0000

    when starting shadow servers via ssh, win32 and osx don't need to guess the X11 display
    git-svn-id: https://xpra.org/svn/Xpra/trunk@12377 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/main.py
+++ src/xpra/scripts/main.py
@@ -1738,6 +1738,9 @@ def run_proxy(error_cb, opts, script_file, args, mode, defaults):
             if len(args)==1 and args[0]==":":
                 #display_name was provided:
                 display_name = args[0]
+            elif sys.platform.startswith("darwin") or sys.platform.startswith("win"):
+                #no need for a specific display
+                pass
             else:
                 display_name = guess_X11_display(opts.socket_dir, opts.socket_dirs)
                 #we now know the display name, so add it:
