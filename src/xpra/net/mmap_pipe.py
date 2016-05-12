756412fc4cf0808bc40c1bf4b8a6392db5d6b4c2
Antoine Martin <totaam@xpra.org>
2016-05-12 07:16:04 +0000

    better error message format
    git-svn-id: https://xpra.org/svn/Xpra/trunk@12555 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/mmap_pipe.py
+++ src/xpra/net/mmap_pipe.py
@@ -128,8 +128,9 @@ def init_server_mmap(mmap_filename, mmap_token=None, new_mmap_token=None):
     try:
         f = open(mmap_filename, "r+b")
     except Exception as e:
-        log.error("Error: cannot access mmap file '%s' (see mmap-group option?)", mmap_filename)
+        log.error("Error: cannot access mmap file '%s':", mmap_filename)
         log.error("  %s", e)
+        log.error(" see mmap-group option?")
         return None, 0
 
     mmap_area = None
