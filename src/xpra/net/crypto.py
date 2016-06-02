77161c378a809f550f7fcdc6e1bfe1ed0369cbc3
Antoine Martin <totaam@xpra.org>
2016-06-02 02:32:08 +0000

    #1216: fix typo (cut&paste)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@12724 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/crypto.py
+++ src/xpra/net/crypto.py
@@ -179,7 +179,7 @@ def get_encryptor(ciphername, iv, password, key_salt, iterations):
     return backend.get_encryptor(key, iv), block_size
 
 def get_decryptor(ciphername, iv, password, key_salt, iterations):
-    log("get_encryptor(%s, %s, %s, %s, %s)", ciphername, iv, password, key_salt, iterations)
+    log("get_decryptor(%s, %s, %s, %s, %s)", ciphername, iv, password, key_salt, iterations)
     if not ciphername:
         return None, 0
     assert iterations>=100
