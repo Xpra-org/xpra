861464342543b2936ab53ce722eaf911cf9e7522
Antoine Martin <totaam@xpra.org>
2016-07-19 19:40:52 +0000

    #1107: bring ffmpeg encoder code closer to the latest muxer patch to reduce diff
    git-svn-id: https://xpra.org/svn/Xpra/trunk@13044 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/enc_ffmpeg/encoder.pyx
+++ src/xpra/codecs/enc_ffmpeg/encoder.pyx
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2012-2014 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2016 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -17,24 +17,25 @@ from xpra.util import AtomicInteger, bytestostr, csv, print_nested_dict
 SAVE_TO_FILE = os.environ.get("XPRA_SAVE_TO_FILE")
 
 
-from libc.stdint cimport uint8_t, int64_t, uint8_t
+from libc.stdint cimport uint8_t, int64_t
+
+cdef extern from "../../inline.h":
+    pass
 
 cdef extern from "string.h":
     void free(void * ptr) nogil
+
+cdef extern from "../../buffers/memalign.h":
+    void *xmemalign(size_t size) nogil
 
 cdef extern from "../../buffers/buffers.h":
     int object_as_buffer(object obj, const void ** buffer, Py_ssize_t * buffer_len)
     int get_buffer_api_version()
 
-cdef extern from "../../inline.h":
-    pass
-
-cdef extern from "../../buffers/memalign.h":
-    void *xmemalign(size_t size) nogil
-
 
 cdef extern from "libavutil/mem.h":
     void av_free(void *ptr)
+    void *av_malloc(size_t size)
 
 cdef extern from "libavutil/error.h":
     int av_strerror(int errnum, char *errbuf, size_t errbuf_size)
@@ -72,8 +73,28 @@ cdef extern from "libavutil/pixfmt.h":
     AVPixelFormat AV_PIX_FMT_BGRA
     AVPixelFormat AV_PIX_FMT_GBRP
 
+
 cdef extern from "libavcodec/avcodec.h":
+    int CODEC_FLAG_UNALIGNED
+    int CODEC_FLAG_QSCALE
+    int CODEC_FLAG_4MV
+    int CODEC_FLAG_OUTPUT_CORRUPT
+    int CODEC_FLAG_QPEL
+    int CODEC_FLAG_GMC
+    int CODEC_FLAG_MV0
+    int CODEC_FLAG_INPUT_PRESERVED
+    int CODEC_FLAG_PASS1
+    int CODEC_FLAG_PASS2
+    int CODEC_FLAG_GRAY
+    int CODEC_FLAG_EMU_EDGE
+    int CODEC_FLAG_PSNR
+    int CODEC_FLAG_TRUNCATED
+    int CODEC_FLAG_NORMALIZE_AQP
+    int CODEC_FLAG_INTERLACED_DCT
+    int CODEC_FLAG_GLOBAL_HEADER
+
     int CODEC_FLAG2_FAST
+
     int CODEC_CAP_DRAW_HORIZ_BAND
     int CODEC_CAP_DR1
     int CODEC_CAP_TRUNCATED
@@ -94,27 +115,35 @@ cdef extern from "libavcodec/avcodec.h":
     int CODEC_CAP_LOSSLESS
 
     ctypedef struct AVFrame:
-        uint8_t **data
-        int *linesize
-        int width
-        int height
-        int format
-        int key_frame
-        int64_t pts
-        int coded_picture_number
-        int display_picture_number
-        int quality
-        void *opaque
+        uint8_t     **data
+        int         *linesize
+        int         width
+        int         height
+        int         format
+        int         key_frame
+        int64_t     pts
+        int         coded_picture_number
+        int         display_picture_number
+        int         quality
+        void        *opaque
         AVPictureType pict_type
     ctypedef struct AVCodec:
-        int capabilities
-        const char *name
-        const char *long_name
+        int         capabilities
+        const char  *name
+        const char  *long_name
     ctypedef struct AVDictionary:
         pass
+    int AV_PKT_FLAG_KEY
+    int AV_PKT_FLAG_CORRUPT
     ctypedef struct AVPacket:
+        int64_t pts
+        int64_t dts
         uint8_t *data
-        int      size
+        int     size
+        int     stream_index
+        int     flags
+        int64_t duration
+        int64_t pos
 
     ctypedef struct AVRational:
         int num
@@ -136,9 +165,8 @@ cdef extern from "libavcodec/avcodec.h":
         int delay
         AVRational framerate
         AVRational time_base
-
-    ctypedef struct AVFormatContext:
-        pass
+        unsigned int codec_tag
+        int64_t bit_rate
 
     AVCodecID AV_CODEC_ID_H264
     AVCodecID AV_CODEC_ID_H265
@@ -154,14 +182,114 @@ cdef extern from "libavcodec/avcodec.h":
     int avcodec_send_frame(AVCodecContext *avctx,const AVFrame *frame) nogil
     int avcodec_receive_packet(AVCodecContext *avctx, AVPacket *avpkt) nogil
 
-    int av_write_frame(AVFormatContext *s, AVPacket *pkt)
     AVFrame* av_frame_alloc()
     void av_frame_free(AVFrame **frame)
     int avcodec_close(AVCodecContext *avctx)
-    void av_frame_unref(AVFrame *frame) nogil
     void av_init_packet(AVPacket *pkt) nogil
     void av_packet_unref(AVPacket *pkt) nogil
 
+ctypedef int AVOptionType
+cdef extern from "libavutil/opt.h":
+    AVOptionType AV_OPT_TYPE_FLAGS
+    AVOptionType AV_OPT_TYPE_INT
+    AVOptionType AV_OPT_TYPE_INT64
+    AVOptionType AV_OPT_TYPE_DOUBLE
+    AVOptionType AV_OPT_TYPE_FLOAT
+    AVOptionType AV_OPT_TYPE_STRING
+    AVOptionType AV_OPT_TYPE_RATIONAL
+    AVOptionType AV_OPT_TYPE_BINARY         #offset must point to a pointer immediately followed by an int for the length
+    AVOptionType AV_OPT_TYPE_DICT
+    AVOptionType AV_OPT_TYPE_CONST
+    AVOptionType AV_OPT_TYPE_IMAGE_SIZE
+    AVOptionType AV_OPT_TYPE_PIXEL_FMT
+    AVOptionType AV_OPT_TYPE_SAMPLE_FMT
+    AVOptionType AV_OPT_TYPE_VIDEO_RATE
+    AVOptionType AV_OPT_TYPE_DURATION
+    AVOptionType AV_OPT_TYPE_COLOR
+    AVOptionType AV_OPT_TYPE_CHANNEL_LAYOUT
+    AVOptionType AV_OPT_TYPE_BOOL
+
+    int AV_OPT_SEARCH_CHILDREN
+    int AV_OPT_SEARCH_FAKE_OBJ
+
+    ctypedef struct AVOption:
+        const char *name        #short English help text
+        const char *help
+        int offset              #The offset relative to the context structure where the option value is stored. It should be 0 for named constants.
+        AVOptionType type
+        int flags
+        const char *unit
+
+    const AVOption* av_opt_next(void *obj, const AVOption *prev)        
+    void *av_opt_child_next(void *obj, void *prev)
+    int av_opt_set_int(void *obj, const char *name, int64_t val, int search_flags)
+    int av_opt_get_int(void *obj, const char *name, int search_flags, int64_t *out_val)
+
+
+cdef extern from "libavutil/log.h":
+    ctypedef struct AVClass:
+        const char  *class_name                 #The name of the class; usually it is the same name as the context structure type to which the AVClass is associated. 
+        const char  *(*item_name)(void *ctx)    #A pointer to a function which returns the name of a context instance ctx associated with the class. 
+        AVOption    *option                     #a pointer to the first option specified in the class if any or NULL
+        int         version                     #LIBAVUTIL_VERSION with which this structure was created
+        int         log_level_offset_offset     #Offset in the structure where log_level_offset is stored
+        int         parent_log_context_offset   #Offset in the structure where a pointer to the parent context for logging is stored
+        void        *(*child_next)(void *obj, void *prev)  #Return next AVOptions-enabled child or NULL
+        AVClass     *(*child_class_next)(const AVClass *prev) #Return an AVClass corresponding to the next potential AVOptions-enabled child. 
+        #AVClassCategory category                #Category used for visualization (like color) This is only set if the category is equal for all objects using this class. 
+        #AVClassCategory (*get_category)(void *ctx)
+
+
+AV_OPT_TYPES = {
+                AV_OPT_TYPE_FLAGS       : "FLAGS",
+                AV_OPT_TYPE_INT         : "INT",
+                AV_OPT_TYPE_INT64       : "INT64",
+                AV_OPT_TYPE_DOUBLE      : "DOUBLE",
+                AV_OPT_TYPE_FLOAT       : "FLOAT",
+                AV_OPT_TYPE_STRING      : "STRING",
+                AV_OPT_TYPE_RATIONAL    : "RATIONAL",
+                AV_OPT_TYPE_BINARY      : "BINARY",
+                AV_OPT_TYPE_DICT        : "DICT",
+                AV_OPT_TYPE_CONST       : "CONST",
+                AV_OPT_TYPE_IMAGE_SIZE  : "IMAGE_SIZE",
+                AV_OPT_TYPE_PIXEL_FMT   : "PIXEL_FMT",
+                AV_OPT_TYPE_SAMPLE_FMT  : "SAMPLE_FMT",
+                AV_OPT_TYPE_VIDEO_RATE  : "VIDEO_RATE",
+                AV_OPT_TYPE_DURATION    : "DURATION",
+                AV_OPT_TYPE_COLOR       : "COLOR",
+                AV_OPT_TYPE_CHANNEL_LAYOUT : "CHANNEL_LAYOUT",
+                AV_OPT_TYPE_BOOL        : "BOOL",
+                }
+
+
+PKT_FLAGS = {
+             AV_PKT_FLAG_KEY        : "KEY",
+             AV_PKT_FLAG_CORRUPT    : "CORRUPT",
+             }
+
+CODEC_FLAGS = {
+               CODEC_FLAG_UNALIGNED         : "UNALIGNED",
+               CODEC_FLAG_QSCALE            : "QSCALE",
+               CODEC_FLAG_4MV               : "4MV",
+               CODEC_FLAG_OUTPUT_CORRUPT    : "OUTPUT_CORRUPT",
+               CODEC_FLAG_QPEL              : "QPEL",
+               CODEC_FLAG_GMC               : "GMC",
+               CODEC_FLAG_MV0               : "MV0",
+               CODEC_FLAG_INPUT_PRESERVED   : "INPUT_PRESERVED",
+               CODEC_FLAG_PASS1             : "PASS1",
+               CODEC_FLAG_PASS2             : "PASS2",
+               CODEC_FLAG_GRAY              : "GRAY",
+               CODEC_FLAG_EMU_EDGE          : "EMU_EDGE",
+               CODEC_FLAG_PSNR              : "PSNR",
+               CODEC_FLAG_TRUNCATED         : "TRUNCATED",
+               CODEC_FLAG_NORMALIZE_AQP     : "NORMALIZE_AQP",
+               CODEC_FLAG_INTERLACED_DCT    : "INTERLACED_DCT",
+               CODEC_FLAG_GLOBAL_HEADER     : "GLOBAL_HEADER",
+               }
+
+CODEC_FLAGS2 = {
+                CODEC_FLAG2_FAST : "FAST",
+                }
 
 CAPS = {
         CODEC_CAP_DRAW_HORIZ_BAND       : "DRAW_HORIZ_BAND",
@@ -218,6 +346,9 @@ for pix_fmt, av_enum in FORMAT_TO_ENUM.items():
 log("AV_PIX_FMT:")
 print_nested_dict(ENUM_TO_FORMAT, print_fn=log.debug)
 
+def flagscsv(flag_dict, value=0):
+    return csv([v for k,v in flag_dict.items() if k&value])
+
 def get_version():
     return (LIBAVCODEC_VERSION_MAJOR, LIBAVCODEC_VERSION_MINOR, LIBAVCODEC_VERSION_MICRO)
 
@@ -231,8 +362,8 @@ if avcodec_find_encoder(AV_CODEC_ID_H264)!=NULL:
 #    CODECS.append("vp9")
 #if avcodec_find_encoder(AV_CODEC_ID_H265)!=NULL:
 #    CODECS.append("h265")
-#if avcodec_find_encoder(AV_CODEC_ID_MPEG4)!=NULL:
-#    CODECS.append("mpeg4")
+if avcodec_find_encoder(AV_CODEC_ID_MPEG4)!=NULL:
+    CODECS.append("mpeg4")
 log("enc_ffmpeg CODECS=%s", csv(CODECS))
 
 cdef av_error_str(int errnum):
@@ -295,10 +426,11 @@ def get_spec(encoding, colorspace):
                             setup_cost=90, width_mask=0xFFFE, height_mask=0xFFFE, max_w=MAX_WIDTH, max_h=MAX_HEIGHT)
 
 
-cdef class Encoder:
+cdef class Encoder(object):
     """
         This wraps the AVCodecContext and its configuration,
     """
+    cdef AVCodecID codec_id
     cdef AVCodec *codec
     cdef AVCodecContext *codec_ctx
     cdef AVPixelFormat pix_fmt
@@ -314,6 +446,7 @@ cdef class Encoder:
     cdef object __weakref__
 
     def init_context(self, unsigned int width, unsigned int height, src_format, dst_formats, encoding, int quality, int speed, scaling, options):    #@DuplicatedSignature
+        cdef int r
         global CODECS, generation
         assert encoding in CODECS
         assert src_format in get_input_colorspaces(encoding), "invalid colorspace: %s" % src_format
@@ -326,31 +459,40 @@ cdef class Encoder:
             raise Exception("invalid pixel format: %s", src_format)
 
         avcodec_register_all()
-        cdef AVCodecID CodecID
         if self.encoding=="h264":
-            CodecID = AV_CODEC_ID_H264
+            self.codec_id = AV_CODEC_ID_H264
         elif self.encoding=="h265":
-            CodecID = AV_CODEC_ID_H265
+            self.codec_id = AV_CODEC_ID_H265
         elif self.encoding=="vp8":
-            CodecID = AV_CODEC_ID_VP8
+            self.codec_id = AV_CODEC_ID_VP8
         elif self.encoding=="vp9":
-            CodecID = AV_CODEC_ID_VP9
+            self.codec_id = AV_CODEC_ID_VP9
         elif self.encoding=="mpeg4":
-            CodecID = AV_CODEC_ID_MPEG4
+            self.codec_id = AV_CODEC_ID_MPEG4
         else:
             raise Exception("invalid codec; %s" % self.encoding)
-        self.codec = avcodec_find_encoder(CodecID)
+        self.codec = avcodec_find_encoder(self.codec_id)
         if self.codec==NULL:
             raise Exception("codec %s not found!" % self.encoding)
-        log("%s: \"%s\", codec flags: %s", self.codec.name, self.codec.long_name, csv(v for k,v in CAPS.items() if (self.codec.capabilities & k)))
+        log("%s: \"%s\", codec flags: %s", self.codec.name, self.codec.long_name, flagscsv(CAPS, self.codec.capabilities))
 
-        #from here on, we have to call clean_encoder():
+        cdef int b_frames = 0   #int(options.get("b-frames"))
+        try:
+            self.init_encoder(b_frames)
+        except Exception as e:
+            log("init_encoder(%i) failed", b_frames, exc_info=True)
+            self.clean()
+            raise
+        else:
+            log("enc_ffmpeg.Encoder.init_context(%s, %s, %s) self=%s", self.width, self.height, self.src_format, self.get_info())
+
+    def init_encoder(self, int b_frames):
+        cdef unsigned long gen = generation.increase()
+
         self.codec_ctx = avcodec_alloc_context3(self.codec)
         if self.codec_ctx==NULL:
-            self.clean_encoder()
             raise Exception("failed to allocate codec context!")
 
-        cdef int b_frames = int(options.get("b-frames"))
         #we need a framerate.. make one up:
         self.codec_ctx.framerate.num = 1
         self.codec_ctx.framerate.den = 25
@@ -361,32 +503,38 @@ cdef class Encoder:
         self.codec_ctx.has_b_frames = b_frames
         self.codec_ctx.delay = 0
         self.codec_ctx.gop_size = 1
-        self.codec_ctx.width = width
-        self.codec_ctx.height = height
+        self.codec_ctx.width = self.width
+        self.codec_ctx.height = self.height
+        self.codec_ctx.bit_rate = 500000
         self.codec_ctx.pix_fmt = self.pix_fmt
         self.codec_ctx.thread_safe_callbacks = 1
         self.codec_ctx.thread_type = 2      #FF_THREAD_SLICE: allow more than one thread per frame
         self.codec_ctx.thread_count = 0     #auto
         self.codec_ctx.flags2 |= CODEC_FLAG2_FAST   #may cause "no deblock across slices" - which should be fine
         #av_opt_set(c->priv_data, "preset", "slow", 0)
-        cdef int r = avcodec_open2(self.codec_ctx, self.codec, NULL)
-        if r<0:
-            self.clean_encoder()
+        log("init_encoder() codec flags: %s", flagscsv(CODEC_FLAGS, self.codec_ctx.flags))
+        log("init_encoder() codec flags2: %s", flagscsv(CODEC_FLAGS2, self.codec_ctx.flags2))
+
+        r = avcodec_open2(self.codec_ctx, self.codec, NULL)   #NULL, NULL)
+        if r!=0:
             raise Exception("could not open %s encoder context: %s" % (self.encoding, av_error_str(r)))
+
         self.av_frame = av_frame_alloc()
         if self.av_frame==NULL:
-            self.clean_encoder()
             raise Exception("could not allocate an AVFrame for encoding")
         self.frames = 0
-        log("enc_ffmpeg.Encoder.init_context(%s, %s, %s) self=%s", width, height, src_format, self.get_info())
-        gen = generation.increase()
+
         if SAVE_TO_FILE is not None:
-            filename = SAVE_TO_FILE+"ffmpeg-"+self.encoding+"-"+str(gen)+".%s" % encoding
+            filename = SAVE_TO_FILE+"-"+str(gen)+".%s" % self.encoding
             self.file = open(filename, 'wb')
-            log.info("saving %s stream to %s", encoding, filename)
+            log.info("saving stream to %s", filename)
+
 
     def clean(self):
-        self.clean_encoder()
+        try:
+            self.clean_encoder()
+        except:
+            log.error("cleanup failed", exc_info=True)
         self.codec = NULL
         self.pix_fmt = 0
         self.src_format = ""
@@ -400,17 +548,12 @@ cdef class Encoder:
             self.file = None
             f.close()
 
-
     def clean_encoder(self):
-        cdef int r, i
+        cdef int r
         log("%s.clean_encoder()", self)
-
         if self.av_frame!=NULL:
             log("clean_encoder() freeing AVFrame: %#x", <unsigned long> self.av_frame)
             av_frame_free(&self.av_frame)
-            #redundant: self.frame = NULL
-
-        cdef unsigned long ctx_key          #@DuplicatedSignature
         log("clean_encoder() freeing AVCodecContext: %#x", <unsigned long> self.codec_ctx)
         if self.codec_ctx!=NULL:
             r = avcodec_close(self.codec_ctx)
@@ -502,7 +645,6 @@ cdef class Encoder:
             istrides = image.get_rowstride()
             assert len(pixels)==3, "image pixels does not have 3 planes! (found %s)" % len(pixels)
             assert len(istrides)==3, "image strides does not have 3 values! (found %s)" % len(istrides)
-    
             #populate the avframe:
             for i in range(4):
                 if i<3:
@@ -518,10 +660,10 @@ cdef class Encoder:
             self.av_frame.pts = self.frames+1
             self.av_frame.coded_picture_number = self.frames+1
             self.av_frame.display_picture_number = self.frames+1
-            if self.frames==0:
-                self.av_frame.pict_type = AV_PICTURE_TYPE_I
-            else:
-                self.av_frame.pict_type = AV_PICTURE_TYPE_P
+            #if self.frames==0:
+            #    self.av_frame.pict_type = AV_PICTURE_TYPE_I
+            #else:
+            #    self.av_frame.pict_type = AV_PICTURE_TYPE_P
             #self.av_frame.quality = 1
             frame = self.av_frame
         else:
@@ -560,19 +702,22 @@ cdef class Encoder:
                 raise Exception(av_error_str(ret))
             if ret>0:
                 free(avpkt.data)
-                self.log_error(image, ret, options, "no stream")
+                self.log_av_error(image, ret, options, "no stream")
                 raise Exception("no stream")
-            log("avcodec_receive_packet returned %#x bytes of data", avpkt.size)
+            log("avcodec_receive_packet returned %#x bytes of data, flags: %s", avpkt.size, flagscsv(PKT_FLAGS, avpkt.flags))
+            if avpkt.flags & AV_PKT_FLAG_CORRUPT:
+                free(avpkt.data)
+                self.log_error(image, "packet", options, "av packet is corrupt")
+                raise Exception("av packet is corrupt")
             packet_data = avpkt.data[:avpkt.size]
             bufs.append(packet_data)
-            if self.file and packet_data:
-                self.file.write(packet_data)
-                self.file.flush()
         av_packet_unref(&avpkt)
         free(avpkt.data)
         self.frames += 1
         data = b"".join(bufs)
         log("compress_image(%s) %5i bytes (%i buffers) for %4s frame %-3i, client options: %s", image, len(data), len(bufs), self.encoding, self.frames, client_options)
+        if data and self.file:
+            self.file.write(data)
         return data, client_options
 
     def flush(self, delayed):
