df797289b532f873e44de69665cb4415f007a4a2
Antoine Martin <totaam@xpra.org>
2016-07-27 10:27:44 +0000

    #1260: make it possible to toggle nvenc7 support on the command line
    git-svn-id: https://xpra.org/svn/Xpra/trunk@13087 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -211,7 +211,7 @@ rebuild_ENABLED         = True
 
 #allow some of these flags to be modified on the command line:
 SWITCHES = ["enc_x264", "enc_x265", "xvid", "enc_ffmpeg",
-            "nvenc4", "nvenc5", "nvenc6",
+            "nvenc4", "nvenc5", "nvenc6", "nvenc7",
             "vpx", "webp", "pillow",
             "dec_avcodec2", "csc_swscale",
             "csc_opencl", "csc_cython", "csc_opencv", "csc_libyuv",
