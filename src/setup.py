5b2f07b5a521b0e72bfefc4e3de948b89a6b2516
Antoine Martin <totaam@xpra.org>
2016-08-22 06:06:33 +0000

    v4l2 doesn't work on freebsd
    git-svn-id: https://xpra.org/svn/Xpra/trunk@13425 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -176,7 +176,7 @@ webp_ENABLED            = False
 vpx_ENABLED             = DEFAULT and pkg_config_version("1.3", "vpx", fallback=WIN32)
 enc_ffmpeg_ENABLED      = DEFAULT and pkg_config_version("56", "libavcodec")
 webcam_ENABLED          = DEFAULT and not OSX
-v4l2_ENABLED            = DEFAULT and (not WIN32 and not OSX)
+v4l2_ENABLED            = DEFAULT and (not WIN32 and not OSX and not sys.platform.startswith("freebsd"))
 #ffmpeg 2 onwards:
 dec_avcodec2_ENABLED    = DEFAULT and pkg_config_version("56", "libavcodec", fallback=WIN32)
 # some version strings I found:
