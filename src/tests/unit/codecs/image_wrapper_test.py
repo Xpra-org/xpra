c378a39370ea6ff8e2985eea45a03253ac7375c8
Antoine Martin <totaam@xpra.org>
2016-08-26 09:08:49 +0000

    * populate the whole image buffer * use more values for each pixel than just 0/1 * make the image smaller (expensive on the buildbot)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@13477 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/unit/codecs/image_wrapper_test.py
+++ src/tests/unit/codecs/image_wrapper_test.py
@@ -11,16 +11,15 @@ from xpra.codecs.image_wrapper import ImageWrapper
 class TestImageWrapper(unittest.TestCase):
 
     def test_sub_image(self):
-        W = 1920
-        H = 1080
+        W = 640
+        H = 480
         buf = bytearray(W*H*4)
-        #set the first N pixels of each line to 1:
-        SET_X = 2
-        for x in range(SET_X):
+        #the pixel value is derived from the sum of its coordinates (modulo 256)
+        for x in range(W):
             for y in range(H):
                 #4 bytes per pixel:
                 for i in range(4):
-                    buf[y*(W*4) + x*4 + i] = 1
+                    buf[y*(W*4) + x*4 + i] = (x+y) % 256
         img = ImageWrapper(0, 0, W, H, buf, "RGBX", 24, W*4, planes=ImageWrapper.PACKED, thread_safe=True)
         #print("image pixels head=%s" % (binascii.hexlify(img.get_pixels()[:128]), ))
         for x in range(3):
@@ -32,14 +31,16 @@ class TestImageWrapper(unittest.TestCase):
             #print("pixels for %ix%i: %i" % (SW, SH, len(sub_buf)))
             #print("pixels=%s" % (binascii.hexlify(sub_buf), ))
             #verify that the pixels are set to 1 / 0:
-            v = int(x<SET_X)
             for y in range(SH):
+                v = (x+y)%256
                 for i in range(4):
+                    av = sub_buf[y*(SW*4)+i]
                     try:
-                        av = ord(sub_buf[y*(SW*4)+i])
+                        #python2 (char)
+                        av = ord(av)
                     except:
-                        #python3:
-                        av = sub_buf[y*(SW*4)+i]
+                        #python3 (int already)
+                        av = int(av)
                     assert av==v, "expected value %#x for pixel (0, %i) of sub-image %s at (%i, 0), but got %#x" % (v, y, sub, x, av)
         start = time.time()
         copy = img.get_sub_image(0, 0, W, H)
