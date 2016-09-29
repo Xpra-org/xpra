e7346595a33ef168a410a6eb641d5961d6c06f07
Antoine Martin <totaam@xpra.org>
2016-09-29 11:14:00 +0000

    add test for shadow server
    git-svn-id: https://xpra.org/svn/Xpra/trunk@13906 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/tests/unit/server/shadow_server_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2016 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import time
+import unittest
+from tests.unit.server_test_util import ServerTestUtil
+
+
+class ShadowServerTest(ServerTestUtil):
+
+	def test_shadow_start_stop(self):
+		display = self.find_free_display()
+		xvfb = self.start_Xvfb(display)
+		time.sleep(1)
+		assert display in self.find_X11_displays()
+		#start server using this display:
+		server = self.check_server("shadow", display)
+		self.check_stop_server(server, "stop", display)
+		time.sleep(1)
+		assert self.pollwait(xvfb, 2) is None, "the Xvfb should not have been killed by xpra shutting down!"
+		xvfb.terminate()
+
+
+def main():
+	unittest.main()
+
+
+if __name__ == '__main__':
+	main()
