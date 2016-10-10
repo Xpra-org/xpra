aafc3404c88095380d355c5582ab186e30bc8303
Antoine Martin <totaam@xpra.org>
2016-10-10 13:48:49 +0000

    undo r14101 - this should not be needed and could confuse things or prevent refactoring
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14105 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/client_base.py
+++ src/xpra/client/client_base.py
@@ -721,8 +721,6 @@ class XpraClientBase(FileTransferHandler):
             self.source_remove(spt)
 
     def do_send_printers(self):
-        if self.exit_on_signal:
-            return
         try:
             self.send_printers_timer = None
             from xpra.platform.printing import get_printers, get_mimetypes
