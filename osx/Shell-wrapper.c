efaf17db5f4e0ae97983bef986ec0547ec90a781
Antoine Martin <totaam@xpra.org>
2016-10-17 13:14:42 +0000

    #1340: add appstore packaging script
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14183 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ osx/Shell-wrapper.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <libproc.h>
+
+/**
+ * Compile and place the resulting binary somewhere in the bundle under /Contents/
+ * this program will try to execute the file with the same name in /Contents/Resources/scripts/
+ * using the shell /bin/sh
+ */
+#define MAX_ARGV 100
+
+int main (int argc, char* argv[])
+{
+	int ret;
+	pid_t pid;
+	char pathbuf[PROC_PIDPATHINFO_MAXSIZE+10];
+	char filename[PROC_PIDPATHINFO_MAXSIZE+10];
+	char *p;
+
+	pid = getpid();
+	ret = proc_pidpath(pid, pathbuf, PROC_PIDPATHINFO_MAXSIZE);
+	if (ret <= 0) {
+		fprintf(stderr, "PID %d: proc_pidpath ();\n", pid);
+		fprintf(stderr, "	%s\n", strerror(errno));
+		return 1;
+	}
+#ifdef DEBUG
+	printf("proc %d: %s\n", pid, pathbuf);
+#endif
+	//stop on last directory separator to copy the filename:
+	p = pathbuf;
+	while (strchr(p, '/')) {
+		p= strchr(p, '/')+1;
+	}
+	strcpy(filename, p);
+	//stop on last "/Contents" directory:
+	p = pathbuf;
+	while (strstr(p, "/Contents/")) {
+		p = strstr(p, "/Contents/")+1;
+	}
+	if (!p) {
+		fprintf(stderr, "invalid command path '%s': '/Contents/' directory not found in path\n", pathbuf);
+		return 1;
+	}
+	strcpy(p, "/Contents/Resources/scripts/");
+	strcpy(pathbuf+strlen(pathbuf), filename);
+	char *new_argv[MAX_ARGV];
+	new_argv[0] = "/bin/sh";
+	new_argv[1] = pathbuf;
+	int i = 1;
+	while (argv[i]!=NULL && i<(MAX_ARGV-1)) {
+		new_argv[i+1] = argv[i];
+		i += 1;
+	}
+	new_argv[i+1] = NULL;
+#ifdef DEBUG
+	printf("execv(/bin/sh, %s, ..)\n", pathbuf);
+#endif
+	int v = execv("/bin/sh", new_argv);
+	fprintf(stderr, "execv(\"/bin/sh\", %s, ..) returned %i\n", pathbuf, v);
+	return v;
+}
---
+++ osx/make-appstore-PKG.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+echo
+echo "*******************************************************************************"
+if [ ! -d "./image/Xpra.app" ]; then
+	echo "./image/Xpra.app is missing - cannot continue"
+	exit 1
+fi
+rm -fr ./appstore/ 2>&1 > /dev/null
+mkdir appstore
+cp -R ./image/Xpra.app ./appstore/
+echo "WARNING: removing sound sub-app support"
+rm -fr ./appstore/Xpra.app/Contents/Xpra_NoDock.app
+
+#get the version and build info from the python build records:
+export PYTHONPATH="appstore/Xpra.app/Contents/Resources/lib/python/"
+VERSION=`python -c "from xpra import __version__;import sys;sys.stdout.write(__version__)"`
+REVISION=`python -c "from xpra import src_info;import sys;sys.stdout.write(str(src_info.REVISION))"`
+REV_MOD=`python -c "from xpra import src_info;import sys;sys.stdout.write(['','M'][src_info.LOCAL_MODIFICATIONS>0])"`
+BUILD_CPU=`python -c "from xpra import build_info;import sys;sys.stdout.write(str(build_info.BUILD_CPU))"`
+BUILD_INFO=""
+if [ "$BUILD_CPU" != "i386" ]; then
+	BUILD_INFO="-x86_64"
+fi
+
+PKG_FILENAME="./image/Xpra$BUILD_INFO-$VERSION-r$REVISION$REV_MOD-appstore.pkg"
+rm -f $PKG_FILENAME >& /dev/null
+echo "Making $PKG_FILENAME"
+
+rm appstore/Xpra.app/Contents/MacOS/*
+
+#move all the scripts to Resources/scripts:
+mkdir appstore/Xpra.app/Contents/Resources/scripts
+rm -f appstore/Xpra.app/Contents/MacOS/*
+for x in Bug_Report GTK_info Network_info Python Xpra Launcher Config_info Keyboard_Tool OpenGL_check PythonExecWrapper Encoding_info Keymap_info Path_info Version_info Shadow Feature_info Manual PowerMonitor Webcam_Test GStreamer_info NativeGUI_info Print Websockify; do
+	mv "appstore/Xpra.app/Contents/Helpers/$x" "appstore/Xpra.app/Contents/Resources/scripts/"
+	if [ "$x" != "PythonExecWrapper" ]; then
+		cat appstore/Xpra.app/Contents/Info.plist | sed "s+Launcher+$x+g" | sed "s+org.xpra.Xpra+org.xpra.$x+g" | sed "s+Xpra+$x+g" > ./appstore/temp.plist
+		gcc -arch i386 -o "appstore/Xpra.app/Contents/MacOS/$x" "./Shell-wrapper.c" -sectcreate __TEXT __info_plist ./appstore/temp.plist
+		rm appstore/temp.plist
+	fi
+done
+ls -la appstore/Xpra.app/Contents/MacOS
+echo
+ls -la appstore/Xpra.app/Contents/Helpers
+#remove gstreamer bits:
+rm appstore/Xpra.app/Contents/Helpers/gst*
+
+
+CODESIGN_ARGS="--force --verbose --sign \"3rd Party Mac Developer Application\" --entitlements ./Xpra.entitlements"
+eval codesign $CODESIGN_ARGS appstore/Xpra.app/Contents/MacOS/Launcher
+eval codesign $CODESIGN_ARGS appstore/Xpra.app/Contents/Helpers/*
+EXES=`find appstore/Xpra.app/Contents/MacOS -type f | grep -v "Launcher" | xargs`
+eval codesign $CODESIGN_ARGS $EXES
+
+CODESIGN_ARGS="--force --verbose --sign \"3rd Party Mac Developer Application\" --entitlements ./Inherit.entitlements"
+LIBS=`find appstore/Xpra.app/Contents/Resources -name "*so" -or -name "*dylib" | xargs`
+eval codesign $CODESIGN_ARGS $LIBS
+eval "find appstore/Xpra.app/Contents/Resources/bin/ -type f -exec codesign $CODESIGN_ARGS {} \;"
+
+CODESIGN_ARGS="--force --verbose --sign \"3rd Party Mac Developer Application\" --entitlements ./Xpra.entitlements"
+eval codesign $CODESIGN_ARGS appstore/Xpra.app/Contents/MacOS/Launcher
+
+#cat appstore/Xpra.app/Contents/Info.plist | sed "s+org.xpra.Xpra+org.xpra.Launcher+g" | sed "s+Xpra+Launcher+g" > ./appstore/temp.plist
+#mv ./appstore/temp.plist appstore/Xpra.app/Contents/Info.plist
+eval codesign $CODESIGN_ARGS appstore/Xpra.app
+productbuild --component ./appstore/Xpra.app /Applications $PKG_FILENAME --sign "3rd Party Mac Developer Installer: $CODESIGN_KEYNAME"
+
+#show resulting file and copy it to the desktop
+du -sm $PKG_FILENAME
+cp $PKG_FILENAME ~/Desktop/
+
+echo "Done PKG"
+echo "*******************************************************************************"
+echo
