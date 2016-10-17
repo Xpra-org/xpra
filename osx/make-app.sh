2e4830bdbe4e5fd002dbe3553176b7f43da09999
Antoine Martin <totaam@xpra.org>
2016-10-17 09:27:07 +0000

    ensure we also strip the OpenGL module: strip after we unpack it
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14177 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- osx/make-app.sh
+++ osx/make-app.sh
@@ -150,19 +150,6 @@ for x in "*.c" "*.pyx" "*.pxd" "constants.pxi" "constants.txt"; do
 	find $LIBDIR/python/xpra/ -name "$x" -print -exec rm "{}" \; | sed "s+$LIBDIR/python/xpra/++g" | xargs -L 1 echo "* "
 done
 
-if [ "$STRIP_SOURCE" == "1" ]; then
-	echo "removing py if we have the pyc:"
-	#only remove ".py" source if we have a binary ".pyc" for it:
-	for x in `find $LIBDIR/python/xpra -name "*.py" -type f`; do
-		d="`dirname $x`"
-		f="`basename $x`"
-		if [ -r "$d/${f}c" ]; then
-			echo "* $x"
-			rm "$x"
-		fi
-	done
-fi
-
 
 echo
 echo "*******************************************************************************"
@@ -223,6 +210,21 @@ if [ "$STRIP_NUMPY" == "1" ]; then
 	done
 	popd
 fi
+
+
+if [ "$STRIP_SOURCE" == "1" ]; then
+	echo "removing py if we have the pyc:"
+	#only remove ".py" source if we have a binary ".pyc" for it:
+	for x in `find $LIBDIR -name "*.py" -type f`; do
+		d="`dirname $x`"
+		f="`basename $x`"
+		if [ -r "$d/${f}c" ]; then
+			#echo "* $x"
+			rm "${x}"
+		fi
+	done
+fi
+
 
 #gst bits expect to find dylibs in Frameworks!?
 pushd ${CONTENTS_DIR}
