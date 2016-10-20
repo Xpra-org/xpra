a5df758cade836f05fa1126c27c6615ff6e59bae
Antoine Martin <totaam@xpra.org>
2016-10-20 11:43:55 +0000

    doh: we can execute scripts not bin! (scripts run bin with the correct arguments..)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14226 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/paths.py
+++ src/xpra/platform/darwin/paths.py
@@ -145,5 +145,5 @@ def do_get_sound_command():
     subapp = os.path.join(base, "Xpra_NoDock.app", "Contents")
     if os.path.exists(subapp) and os.path.isdir(subapp):
         base = subapp
-    helper = os.path.join(base, "Resources", "bin", "Xpra")
+    helper = os.path.join(base, "Resources", "scripts", "Xpra")
     return [helper]
