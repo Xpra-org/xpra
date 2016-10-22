5805d4bdae5ff844f45226e4a3a2a46b32de8146
Antoine Martin <totaam@xpra.org>
2016-10-22 05:35:27 +0000

    add hooks for building an MSI installer
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14252 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/win32/msi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# BATch files are useless,
+# all we want is sed,
+# but is is MSWindows so it's easier to re-invent the wheel, sigh 
+
+import sys
+
+def main():
+    if len(sys.argv)<3:
+        print("Invalid number of arguments")
+        print("Usage: %s source-msi.xml destination-msi.xml TOKEN1=VALUE1 [TOKEN2=VALUE2 [..]]", sys.argv[0])
+        sys.exit(1)
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    print("replacing tokens from file '%s' and saving to '%s'" % (src, dst))
+    data = open(src, 'rb').read().decode()
+    print("data=%i bytes %s" % (len(data), type(data)))
+    for kv in sys.argv[3:]:
+        try:
+            token, value = kv.split("=")
+        except ValueError:
+            print("skipping invalid token string '%s'" % kv)
+            continue
+        print("replacing %s with '%s' %s" % (token, value, type(token)))
+        data = data.replace("$%s" % token, value)
+        data = data.replace("${%s}" % token, value)
+    open(dst, 'wb').write(data.encode())
+
+if __name__ == '__main__':
+    main()
