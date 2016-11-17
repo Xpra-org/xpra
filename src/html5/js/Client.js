ddf5728c76b37815cb8d5b5fc45845ae42ac8267
Antoine Martin <totaam@xpra.org>
2016-11-17 13:39:24 +0000

    #1341: * allow xor digest over ssl * hide insecure password option when using ssl (irrelevant) * move insecure password option near password, disable password field if not checked
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14437 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Client.js
+++ src/html5/js/Client.js
@@ -1252,7 +1252,7 @@ XpraClient.prototype._process_challenge = function(packet, ctx) {
 		hmac.update(salt);
 		challenge_response = hmac.digest().toHex();
 	} else if (digest == "xor") {
-		if((!ctx.encryption) && (!ctx.insecure) && (ctx.host!="localhost") && (ctx.host!="127.0.0.1")) {
+		if((!ctx.ssl) && (!ctx.encryption) && (!ctx.insecure) && (ctx.host!="localhost") && (ctx.host!="127.0.0.1")) {
 			ctx.callback_close("server requested digest xor, cowardly refusing to use it without encryption with "+ctx.host);
 			return;
 		}
