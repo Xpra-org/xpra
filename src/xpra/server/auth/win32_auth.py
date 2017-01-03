e52ef425b3556ae3bcc897ef0b0fa2d7c94cb8ab
Antoine Martin <totaam@xpra.org>
2017-01-03 15:53:27 +0000

    #678 move to ctypes: auth module
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14693 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/auth/win32_auth.py
+++ src/xpra/server/auth/win32_auth.py
@@ -1,13 +1,26 @@
 # This file is part of Xpra.
-# Copyright (C) 2013 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2013-2017 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 
 from xpra.server.auth.sys_auth_base import SysAuthenticator, init, log
-import win32security            #@UnresolvedImport
-assert win32security            #avoid pydev warning
 assert init and log #tests will disable logging from here
+
+from ctypes import windll, byref, POINTER, FormatError, GetLastError
+from ctypes.wintypes import LPCWSTR, DWORD, HANDLE, BOOL
+
+MAX_COMPUTERNAME_LENGTH = 15
+LOGON32_LOGON_NETWORK_CLEARTEXT = 8
+LOGON32_PROVIDER_DEFAULT = 0
+
+CloseHandle = windll.Kernel32.CloseHandle
+CloseHandle.argtypes = [HANDLE]
+CloseHandle.restype = BOOL
+
+LogonUser = windll.Advapi32.LogonUserW
+LogonUser.argtypes = [LPCWSTR, LPCWSTR, LPCWSTR, DWORD, DWORD, POINTER(HANDLE)]
+LogonUser.restype = BOOL
 
 
 class Authenticator(SysAuthenticator):
@@ -19,7 +32,18 @@ class Authenticator(SysAuthenticator):
         return 0
 
     def check(self, password):
-        win32security.LogonUser(self.username, '', password, win32security.LOGON32_LOGON_NETWORK, win32security.LOGON32_PROVIDER_DEFAULT)
+        token = HANDLE()
+        domain = '' #os.environ.get('COMPUTERNAME')
+        status = LogonUser(self.username, domain, password,
+                     LOGON32_LOGON_NETWORK_CLEARTEXT,
+                     LOGON32_PROVIDER_DEFAULT,
+                     byref(token))
+        error = GetLastError()
+        if status:
+            CloseHandle(token)
+            return True
+        log.error("Error: win32 authentication failed:")
+        log.error(" %s", FormatError(error))
         return True
 
     def __repr__(self):
