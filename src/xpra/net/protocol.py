51bb3bc23798947e70b24beaa3ed2e616ecee7ff
Antoine Martin <totaam@xpra.org>
2017-02-06 04:53:48 +0000

    silence annoying pydev warning
    git-svn-id: https://xpra.org/svn/Xpra/trunk@14977 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/protocol.py
+++ src/xpra/net/protocol.py
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2011-2016 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2011-2017 Antoine Martin <antoine@devloop.org.uk>
 # Copyright (C) 2008, 2009, 2010 Nathaniel Smith <njs@pobox.com>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
@@ -21,7 +21,7 @@ cryptolog = Logger("network", "crypto")
 from xpra.os_util import Queue, memoryview_to_bytes
 from xpra.util import repr_ellipsized, csv, envint, envbool
 from xpra.make_thread import make_thread, start_thread
-from xpra.net import ConnectionClosedException
+from xpra.net import ConnectionClosedException          #@UndefinedVariable (pydev false positive)
 from xpra.net.bytestreams import ABORT
 from xpra.net import compression
 from xpra.net import packet_encoding
