849a4d52a66ed3d36c396c40a09d93b0e7dc2f94
Antoine Martin <totaam@xpra.org>
2017-03-25 05:39:49 +0000

    #1334: add zeroconf and its dependencies to the setup / update script and osx moduleset
    git-svn-id: https://xpra.org/svn/Xpra/trunk@15394 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/win32/UPDATE_PYTHON_LIBS.sh
+++ src/win32/UPDATE_PYTHON_LIBS.sh
@@ -6,7 +6,9 @@
 # later version. See the file COPYING for details.
 
 #using easy-install for python libraries which are not packaged by mingw:
-for x in rencode xxhash netifaces lz4 websocket-client comtypes PyOpenGL PyOpenGL_accelerate websockify cffi pycparser cryptography nvidia-ml-py; do
+# Note: netifaces not updated for now because of this bug:
+# https://bitbucket.org/al45tair/netifaces/issues/39
+for x in rencode xxhash enum34 enum-compat zeroconf lz4 websocket-client comtypes PyOpenGL PyOpenGL_accelerate websockify cffi pycparser cryptography nvidia-ml-py; do
     easy_install-2.7 -U -Z $x
     easy_install-3.5 -U -Z $x
 done
