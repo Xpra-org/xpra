7a5a9ca22a3efc89d610016128b41842cb608e8f
Antoine Martin <totaam@xpra.org>
2017-04-02 13:32:00 +0000

    make it possible to bundle extra files by placing them in this 'bundle-extra' directory, can be used to fix MSYS2 dependency breakage
    git-svn-id: https://xpra.org/svn/Xpra/trunk@15493 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -1417,6 +1417,8 @@ if WIN32:
                 if not isinstance(e, WindowsError) or (not "already exists" in str(e)): #@UndefinedVariable
                     raise
 
+    add_data_files('', glob.glob("win32\\bundle-extra\\*"))
+
     #END OF win32
 #*******************************************************************************
 else:
