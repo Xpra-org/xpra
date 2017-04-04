7c636167f8e2e631cccc43f7e9eea2cfeb5128fb
Antoine Martin <totaam@xpra.org>
2017-04-04 06:52:30 +0000

    better browser language detection
    git-svn-id: https://xpra.org/svn/Xpra/trunk@15509 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Utilities.js
+++ src/html5/js/Utilities.js
@@ -104,11 +104,36 @@ var Utilities = {
 		return 'unknown';
 	},
 
+	getFirstBrowserLanguage : function () {
+		var nav = window.navigator,
+			browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+			i,
+			language;
+		// support for HTML 5.1 "navigator.languages"
+		if (Array.isArray(nav.languages)) {
+			for (i = 0; i < nav.languages.length; i++) {
+				language = nav.languages[i];
+				if (language && language.length) {
+					return language;
+				}
+			}
+		}
+		// support for other well known properties in browsers
+		for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+			language = nav[browserLanguagePropertyKeys[i]];
+			if (language && language.length) {
+				return language;
+			}
+		}
+		return null;
+	},
+
 	getKeyboardLayout: function() {
-		//IE:
-		//navigator.systemLanguage
-		//navigator.browserLanguage
-		var v = window.navigator.userLanguage || window.navigator.language;
+		var v = Utilities.getFirstBrowserLanguage();
+		console.debug("getFirstBrowserLanguage()=", v);
+		if (v==null) {
+			return "us";
+		}
 		//ie: v="en_GB";
 		v = v.split(',')[0];
 		var l = v.split('-', 2);
