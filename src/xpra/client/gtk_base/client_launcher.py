82b311d0c08f7cf94df123225bc654bba714c0da
Antoine Martin <totaam@xpra.org>
2017-04-15 06:42:23 +0000

    make sure the session file extension is .xpra
    git-svn-id: https://xpra.org/svn/Xpra/trunk@15618 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/client_launcher.py
+++ src/xpra/client/gtk_base/client_launcher.py
@@ -821,6 +821,9 @@ class ApplicationWindow:
     def save_clicked(self, *args):
         self.update_options_from_gui()
         def do_save(filename):
+            #make sure the file extension is .xpra
+            if os.path.splitext(filename)!=".xpra":
+                filename += ".xpra"
             save_config(filename, self.config, self.config_keys, extras_types=LAUNCHER_OPTION_TYPES)
         self.choose_session_file("Save session settings to file", FILE_CHOOSER_ACTION_SAVE, gtk.STOCK_SAVE, do_save)
 
