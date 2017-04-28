07e1ae1b2a26ac72331844b0d8e79b4d2bef775e
Antoine Martin <totaam@xpra.org>
2017-04-28 16:42:08 +0000

    remove support for nvenc on 32-bit and older CUDA SDK versions
    git-svn-id: https://xpra.org/svn/Xpra/trunk@15743 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -1890,20 +1890,13 @@ if nvenc7_ENABLED:
     path_options = os.environ.get("PATH", "").split(os.path.pathsep)
     if WIN32:
         nvcc_exe = "nvcc.exe"
-        #FIXME: we try to use SDK 6.5 x86 first!
-        #(so that we can build on 32-bit envs)
         path_options = [
-                         "C:\\Program Files (x86)\\NVIDIA GPU Computing Toolkit\\CUDA\\v5.5\\bin",
-                         "C:\\Program Files (x86)\\NVIDIA GPU Computing Toolkit\\CUDA\\v6.0\\bin",
-                         "C:\\Program Files (x86)\\NVIDIA GPU Computing Toolkit\\CUDA\\v6.5\\bin",
-                         "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v6.5\\bin",
-                         "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v7.0\\bin",
                          "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v7.5\\bin",
                          "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v8.0\\bin",
                          ] + path_options
     else:
         nvcc_exe = "nvcc"
-        for v in ("-5.5", "-6.0", "-6.5", "-7.0", "-7.5", "-8.0", ""):
+        for v in ("-7.5", "-8.0", ""):
             path_options += ["/usr/local/cuda%s/bin" % v, "/opt/cuda%s/bin" % v]
     options = [os.path.join(x, nvcc_exe) for x in path_options]
     def which(cmd):
