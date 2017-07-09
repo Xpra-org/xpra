e688cf7426b78ddf95664a6ccd68fe225ba245eb
Antoine Martin <totaam@xpra.org>
2017-07-09 10:51:46 +0000

    use stronger data types
    git-svn-id: https://xpra.org/svn/Xpra/trunk@16243 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/bindings/posix_display_source.pyx
+++ src/xpra/x11/bindings/posix_display_source.pyx
@@ -1,16 +1,17 @@
 # This file is part of Xpra.
 # Copyright (C) 2008, 2009 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2010-2016 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2010-2017 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 
-# This class simply opens the local $DISPLAY and hooks it into the bindings
+# This module can be used to open the local $DISPLAY and hook it into the X11 bindings
 
 import os
 from xpra.os_util import strtobytes
-from xpra.x11.bindings.display_source cimport set_display
+from xpra.x11.bindings.display_source cimport set_display, get_display
 from xpra.x11.bindings.display_source import set_display_name
+from libc.stdint cimport uintptr_t
 
 cdef extern from "X11/Xlib.h":
     ctypedef struct Display:
@@ -24,7 +25,7 @@ def init_posix_display_source():
         raise Exception("cannot open display, the environment variable DISPLAY is not set!")
     return do_init_posix_display_source(display_name)
 
-def do_init_posix_display_source(display_name):
+cdef do_init_posix_display_source(display_name):
     if not display_name:
         raise Exception("display name not provided")
     cdef Display * display = XOpenDisplay(strtobytes(display_name))
@@ -32,9 +33,9 @@ def do_init_posix_display_source(display_name):
         raise Exception("failed to open X11 display '%s'" % display_name)
     set_display(display)
     set_display_name(display_name)
-    return <unsigned long> display
+    return <uintptr_t> display
 
-def close_display_source(unsigned long ptr):
+def close_display_source(uintptr_t ptr):
     assert ptr!=0, "invalid NULL display pointer"
     cdef Display * display = <Display *> ptr
     cdef int v = XCloseDisplay(display)
