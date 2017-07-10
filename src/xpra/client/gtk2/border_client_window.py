efa9f7043ac6627f507f23c42cc67b78e7e84c80
Antoine Martin <totaam@xpra.org>
2017-07-10 04:49:53 +0000

    update docstring
    git-svn-id: https://xpra.org/svn/Xpra/trunk@16260 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk2/border_client_window.py
+++ src/xpra/client/gtk2/border_client_window.py
@@ -1,6 +1,6 @@
 # This file is part of Xpra.
 # Copyright (C) 2011 Serviware (Arthur Huillet, <ahuillet@serviware.com>)
-# Copyright (C) 2010-2015 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2010-2017 Antoine Martin <antoine@devloop.org.uk>
 # Copyright (C) 2008, 2010 Nathaniel Smith <njs@pobox.com>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
@@ -10,10 +10,12 @@ import gtk.gdk
 from xpra.client.gtk2.client_window import ClientWindow
 
 
-"""
-Adds a red border around the window contents
-"""
 class BorderClientWindow(ClientWindow):
+    """
+    Adds a border around the window contents,
+    the colour and size can be configured with the "--border=" command line option
+    this can be toggled at runtime using the "magic_key" shortcut.
+    """
 
     __gsignals__ = ClientWindow.__common_gsignals__
 
