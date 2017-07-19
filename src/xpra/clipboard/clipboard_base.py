cd96fcd39cce280d315326836d79def1e5ad82aa
Antoine Martin <totaam@xpra.org>
2017-07-19 16:24:47 +0000

    fixup the fixup for r16414: this one is me doing it by hand to correct... and getting it wrong, which is why automated tools should get it right - damn you /bin/patch
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@16419 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/clipboard/clipboard_base.py
+++ src/xpra/clipboard/clipboard_base.py
@@ -64,8 +64,6 @@ TEXT_TARGETS = ("UTF8_STRING", "TEXT", "STRING", "text/plain")
 def nosanitize_gtkselectiondata(selectiondata):
     return selectiondata
 sanitize_gtkselectiondata = nosanitize_gtkselectiondata
-                    log.error("invalid regular expression '%s' in clipboard filter")
-        self._clipboard_request_counter = 0
 
 
 class ClipboardProtocolHelperBase(object):
@@ -83,6 +81,7 @@ class ClipboardProtocolHelperBase(object):
                     self.filter_res.append(re.compile(x))
                 except:
                     log.error("invalid regular expression '%s' in clipboard filter")
+        self._clipboard_request_counter = 0
 
 
 #may get overriden
