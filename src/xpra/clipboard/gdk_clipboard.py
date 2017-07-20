472fa9d545cbe89dbc3526b126210c3cc6ea4f44
Antoine Martin <totaam@xpra.org>
2017-07-20 09:31:17 +0000

    r16424 for v1.0.x and v2.0.x branches: the clipboard workaround does not build on macos or win32
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@16425 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/clipboard/gdk_clipboard.py
+++ src/xpra/clipboard/gdk_clipboard.py
@@ -14,12 +14,14 @@ from xpra.gtk_common.gdk_atoms import (
 from xpra.clipboard.clipboard_base import ClipboardProtocolHelperBase, log
 
 
-try:
-    from xpra.x11.gtk2.gdk_bindings import sanitize_gtkselectiondata
-    from xpra.clipboard import clipboard_base
-    clipboard_base.sanitize_gtkselectiondata = sanitize_gtkselectiondata
-except ImportError as e:
-    log.error("Error: sanitize_gtkselectiondata not found: %s", e)
+import sys
+if not (sys.platform.startswith("win") or sys.platform.startswith("darwin")):
+    try:
+        from xpra.x11.gtk2.gdk_bindings import sanitize_gtkselectiondata
+        from xpra.clipboard import clipboard_base
+        clipboard_base.sanitize_gtkselectiondata = sanitize_gtkselectiondata
+    except ImportError as e:
+        log.error("Error: sanitize_gtkselectiondata not found: %s", e)
 
 
 class GDKClipboardProtocolHelper(ClipboardProtocolHelperBase):
