e51e3265ba03e004242c73a00bb2104cdb4d43bd
Antoine Martin <totaam@xpra.org>
2017-07-30 17:59:31 +0000

    update test to match refactoring: root window must be passed in
    git-svn-id: https://xpra.org/svn/Xpra/trunk@16567 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/x11/test_xroot_props.py
+++ src/tests/xpra/x11/test_xroot_props.py
@@ -15,7 +15,8 @@ from xpra.x11.xroot_props import XRootPropWatcher
 
 def main():
     ROOT_PROPS = ["RESOURCE_MANAGER", "_NET_WORKAREA"]
-    xrpw = XRootPropWatcher(ROOT_PROPS)
+    root = gtk.gdk.get_default_root_window()
+    xrpw = XRootPropWatcher(ROOT_PROPS, root)
     gobject.timeout_add(1000, xrpw.notify_all)
     try:
         gtk.main()
