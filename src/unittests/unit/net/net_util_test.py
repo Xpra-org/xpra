abc1a7db82e459e9bb27b7ff9022af573584a429
Antoine Martin <totaam@xpra.org>
2017-08-05 05:32:42 +0000

    restore test wrongly replaced with simple import statement
    git-svn-id: https://xpra.org/svn/Xpra/trunk@16623 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/net/net_util_test.py
+++ src/unittests/unit/net/net_util_test.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 # This file is part of Xpra.
-# Copyright (C) 2011-2014 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2011-2017 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 import unittest
 
-from xpra.net.net_util import get_info
-
 
 class TestVersionUtilModule(unittest.TestCase):
 
     def test_get_info(self):
-        #from rencode import dumps as rencode_dumps, loads as rencode_loads, __version__ as rencode_version
-        from rencode import dumps as rencode_dumps
+        from xpra.net.net_util import get_info
+        get_info()
 
 
 def main():
