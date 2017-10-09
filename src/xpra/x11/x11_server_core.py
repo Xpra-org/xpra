dac8e21ba25a58673b7953e9ecde248fd7f34563
Antoine Martin <totaam@xpra.org>
2017-10-09 04:18:03 +0000

    #1656: fix search for closest match
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17118 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/x11_server_core.py
+++ src/xpra/x11/x11_server_core.py
@@ -516,7 +516,8 @@ class X11ServerCore(GTKServerBase):
         if not new_size:
             screenlog.warn("Warning: no matching resolution found for %sx%s", desired_w, desired_h)
             if len(closest)>0:
-                new_size = sorted(closest.items())[0]
+                min_dist = tuple(closest.keys())[0]
+                new_size = closest[min_dist]
                 screenlog.warn(" using %sx%s instead", *new_size)
             else:
                 root_w, root_h = self.root_window.get_size()
