04874d562e75e1baa270582de6cf6d49e61c840f
Antoine Martin <totaam@xpra.org>
2017-10-17 02:21:51 +0000

    add new required attribute and fix pydev warning
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17203 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/clients/fake_client.py
+++ src/tests/xpra/clients/fake_client.py
@@ -56,7 +56,6 @@ class FakeClient(FileTransferHandler):
         self.server_window_decorations = False
         self.server_window_frame_extents = False
         self.encoding_defaults = {}
-        self.window_configure_pointer = True
         self.xscale = 1
         self.yscale = 1
         self.log_events = True
@@ -69,7 +68,7 @@ class FakeClient(FileTransferHandler):
     def connect(self, *args):
         pass
 
-    def get_image(self, *args):
+    def get_image(self, *_args):
         return None
 
     def get_encodings(self):
