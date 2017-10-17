fcb5d15f25e0c7f824fc6549f41505f25d218712
Antoine Martin <totaam@xpra.org>
2017-10-17 03:36:17 +0000

    time value must be an int
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17210 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/sound/sink.py
+++ src/xpra/sound/sink.py
@@ -299,7 +299,7 @@ class SoundSink(SoundPipeline):
             if self.last_underrun and now-self.last_underrun<2 and mst<200:
                 #temporarily raise max level during underruns,
                 #so set_min_level has more room for manoeuver:
-                mst += max(0, 2+self.last_underrun-now)*75
+                mst += int(2+self.last_underrun-now)*75
             #cap it at 1 second:
             mst = min(mst, 1000)
             log("set_max_level overrun count=%-2i, margin=%3i, pct=%2i, cmst=%3i", olm, MARGIN, pct, cmst)
