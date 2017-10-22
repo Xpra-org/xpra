4d1b9191ec7b85b2a95f7b8957078b4e9a5ba0d0
Antoine Martin <totaam@xpra.org>
2017-10-22 08:42:28 +0000

    honour current_encoding if it is suitable
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17227 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/window/window_source.py
+++ src/xpra/server/window/window_source.py
@@ -829,6 +829,8 @@ class WindowSource(object):
 
     def get_transparent_encoding(self, pixel_count, ww, wh, speed, quality, current_encoding):
         #small areas prefer rgb, also when high speed and high quality
+        if current_encoding in ("rgb32", "png"):
+            return current_encoding
         if "rgb32" in self.common_encodings and (pixel_count<self._rgb_auto_threshold or (quality>=90 and speed>=90) or (self.image_depth>24 and self.client_bit_depth>24)):
             return "rgb32"
         if "png" in self.common_encodings and quality>75:
