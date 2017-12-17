e8f9cb02115750bef1dad1ee01052dc13a000d23
Antoine Martin <totaam@xpra.org>
2017-12-17 09:45:41 +0000

    slightly better debug format
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17680 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/ui_client_base.py
+++ src/xpra/client/ui_client_base.py
@@ -3297,7 +3297,7 @@ class UIXpraClient(XpraClientBase):
             options = packet[10]
         options = typedict(options)
         dtype = DRAW_TYPES.get(type(data), type(data))
-        drawlog("process_draw: %7i %8s for window %3i, %4ix%4i at %4i,%4i using %6s encoding with options=%s", len(data), dtype, wid, width, height, x, y, bytestostr(coding), options)
+        drawlog("process_draw: %7i %8s for window %3i, %4ix%-4i at %4i,%-4i using %6s encoding with options=%s", len(data), dtype, wid, width, height, x, y, bytestostr(coding), options)
         start = monotonic_time()
         def record_decode_time(success, message=""):
             if success>0:
