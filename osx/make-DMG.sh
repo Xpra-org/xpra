f8bb42365fca36c42390b80c6d7cf5c25a05fd0a
Antoine Martin <totaam@xpra.org>
2018-01-04 11:29:14 +0000

    #1575 * make it possible to override the "python" binary used for everything * if we're building with a python3 version, name the DMG and PKG files accordingly
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17881 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- osx/make-DMG.sh
+++ osx/make-DMG.sh
@@ -16,14 +16,19 @@ if [ ! -d "${APP_DIR}" ]; then
 fi
 
 #get the version and build info from the python build records:
+PYTHON_COMMAND="${PYTHON_COMMAND:=python}"
+PYTHON_MAJOR_VERSION=`${PYTHON_COMMAND} -c 'import sys;sys.stdout.write("%s" % sys.version_info[0])'`
 export PYTHONPATH="${APP_DIR}/Contents/Resources/lib/python/"
-VERSION=`python -c "from xpra import __version__;import sys;sys.stdout.write(__version__)"`
-REVISION=`python -c "from xpra import src_info;import sys;sys.stdout.write(str(src_info.REVISION))"`
-REV_MOD=`python -c "from xpra import src_info;import sys;sys.stdout.write(['','M'][src_info.LOCAL_MODIFICATIONS>0])"`
-BUILD_BIT=`python -c "from xpra import build_info;import sys;sys.stdout.write(str(build_info.BUILD_BIT))"`
+VERSION=`${PYTHON_COMMAND} -c "from xpra import __version__;import sys;sys.stdout.write(__version__)"`
+REVISION=`${PYTHON_COMMAND} -c "from xpra import src_info;import sys;sys.stdout.write(str(src_info.REVISION))"`
+REV_MOD=`${PYTHON_COMMAND} -c "from xpra import src_info;import sys;sys.stdout.write(['','M'][src_info.LOCAL_MODIFICATIONS>0])"`
+BUILD_BIT=`${PYTHON_COMMAND} -c "from xpra import build_info;import sys;sys.stdout.write(str(build_info.BUILD_BIT))"`
 BUILD_INFO=""
+if [ "${PYTHON_MAJOR_VERSION}" == "3" ]; then
+	BUILD_INFO="${BUILD_INFO}-Python3"
+fi
 if [ "$BUILD_BIT" != "32bit" ]; then
-	BUILD_INFO="-x86_64"
+	BUILD_INFO="${BUILD_INFO}-x86_64"
 fi
 
 DMG_NAME="Xpra$BUILD_INFO-$VERSION-r$REVISION$REV_MOD.dmg"
--- osx/make-PKG.sh
+++ osx/make-PKG.sh
@@ -17,13 +17,18 @@ fi
 
 #get the version and build info from the python build records:
 export PYTHONPATH="${APP_DIR}/Contents/Resources/lib/python/"
-VERSION=`python -c "from xpra import __version__;import sys;sys.stdout.write(__version__)"`
-REVISION=`python -c "from xpra import src_info;import sys;sys.stdout.write(str(src_info.REVISION))"`
-REV_MOD=`python -c "from xpra import src_info;import sys;sys.stdout.write(['','M'][src_info.LOCAL_MODIFICATIONS>0])"`
-BUILD_BIT=`python -c "from xpra import build_info;import sys;sys.stdout.write(str(build_info.BUILD_BIT))"`
+PYTHON_COMMAND="${PYTHON_COMMAND:=python}"
+PYTHON_MAJOR_VERSION=`${PYTHON_COMMAND} -c 'import sys;sys.stdout.write("%s" % sys.version_info[0])'`
+VERSION=`${PYTHON_COMMAND} -c "from xpra import __version__;import sys;sys.stdout.write(__version__)"`
+REVISION=`${PYTHON_COMMAND} -c "from xpra import src_info;import sys;sys.stdout.write(str(src_info.REVISION))"`
+REV_MOD=`${PYTHON_COMMAND} -c "from xpra import src_info;import sys;sys.stdout.write(['','M'][src_info.LOCAL_MODIFICATIONS>0])"`
+BUILD_BIT=`${PYTHON_COMMAND} -c "from xpra import build_info;import sys;sys.stdout.write(str(build_info.BUILD_BIT))"`
 BUILD_INFO=""
+if [ "${PYTHON_MAJOR_VERSION}" == "3" ]; then
+	BUILD_INFO="${BUILD_INFO}-Python3"
+fi
 if [ "$BUILD_BIT" != "32bit" ]; then
-	BUILD_INFO="-x86_64"
+	BUILD_INFO="${BUILD_INFO}-x86_64"
 fi
 
 PKG_FILENAME="$APP_NAME$BUILD_INFO-$VERSION-r$REVISION$REV_MOD.pkg"
