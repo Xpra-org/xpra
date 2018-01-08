21ab56bd9361741a74278f32e91d583c1c50798a
Antoine Martin <totaam@xpra.org>
2018-01-08 17:27:19 +0000

    make it possible to disable mdns backends individually
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17948 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/mdns/__init__.py
+++ src/xpra/net/mdns/__init__.py
@@ -1,20 +1,28 @@
 # This file is part of Xpra.
-# Copyright (C) 2016, 2017 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2016-2018 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 XPRA_MDNS_TYPE = '_xpra._tcp.'
 
+from xpra.util import envbool
+
+AVAHI = envbool("XPRA_AVAHI", True)
+ZEROCONF = envbool("XPRA_ZEROCONF", True)
+
 
 def get_listener_class():
     from xpra.os_util import get_util_logger
     log = get_util_logger()
-    try:
-        from xpra.net.mdns.avahi_listener import AvahiListener
-        log("AvahiListener=%s", AvahiListener)
-        return AvahiListener
-    except ImportError as e:
-        log("failed to import AvahiListener: %s", e)
+    log("mdns.get_listener_class() AVAHI=%s, ZEROCONF=%s", AVAHI, ZEROCONF)
+    if AVAHI:
+        try:
+            from xpra.net.mdns.avahi_listener import AvahiListener
+            log("AvahiListener=%s", AvahiListener)
+            return AvahiListener
+        except ImportError as e:
+            log("failed to import AvahiListener: %s", e)
+    if ZEROCONF:
         try:
             from xpra.net.mdns.zeroconf_listener import ZeroconfListener
             log("ZeroconfListener=%s", ZeroconfListener)
