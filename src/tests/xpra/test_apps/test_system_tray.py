9b876e2eb76a38f56779b527e3141b7f14b9b34e
Antoine Martin <totaam@xpra.org>
2018-01-11 12:03:35 +0000

    enhance test app with ability to generate notifications using the same app name (also customizable from the command line)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@17984 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_system_tray.py
+++ src/tests/xpra/test_apps/test_system_tray.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 
+import sys
 import gtk
 
 
 class StatusIcon:
-    def __init__(self):
+    def __init__(self, name="test", tooltip="StatusIcon Example"):
+        self.name = name
         self.statusicon = gtk.StatusIcon()
         self.counter = 0
+        self.statusicon.set_name(name)
         self.statusicon.set_from_stock(gtk.STOCK_HOME)
         self.statusicon.connect("popup-menu", self.popup_menu)
         self.statusicon.connect("activate", self.activate)
-        self.statusicon.set_tooltip("StatusIcon Example")
+        self.statusicon.set_tooltip(tooltip)
         #build list of stock icons:
         self.stock = {}
         for x in dir(gtk):
             if x.startswith("STOCK_"):
                 self.stock[x[len("STOCK_"):]] = getattr(gtk, x)
 
-    def activate(self, *args):
+    def activate(self, *_args):
         self.counter += 1
         name, stock = list(self.stock.items())[self.counter % len(self.stock)]
         print("setting tray icon to: %s" % name)
@@ -28,12 +31,32 @@ class StatusIcon:
         quit_menu = gtk.MenuItem("Quit")
         quit_menu.connect("activate", gtk.main_quit)
         menu.append(quit_menu)
+        notify_menu = gtk.MenuItem("Send Notification")
+        notify_menu.connect("activate", self.notify)
+        menu.append(notify_menu)
         menu.show_all()
         menu.popup(None, None, gtk.status_icon_position_menu, button, time, self.statusicon)
 
+    def notify(self, *_args):
+        from xpra.client.notifications.dbus_notifier import DBUS_Notifier
+        n = DBUS_Notifier()
+        n.show_notify("dbus-id", None, 0, self.name, 0, "", "Notification Summary", "Notification Body", 60, "")
+        #import pynotify
+        #pynotify.init(self.name or "Xpra")
+        #n = pynotify.Notification("Test Notification", "The message goes here", "close")
+        #n.set_urgency(pynotify.URGENCY_LOW)
+        #n.set_timeout(60)
+        #n.show()
+
 
 def main():
-    StatusIcon()
+    name = "test"
+    if len(sys.argv)>=2:
+        name = sys.argv[1]
+    tooltip = "StatusIcon Example"
+    if len(sys.argv)>=3:
+        tooltip = sys.argv[2]
+    StatusIcon(name, tooltip)
     gtk.main()
 
 
