e3cd410a40db2214bcd0236f5149a4bd36a1121f
Antoine Martin <totaam@xpra.org>
2018-01-21 11:05:59 +0000

    r18086 backport: ffmpeg 3.5 compatibility
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@18087 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/dec_avcodec2/decoder.pyx
+++ src/xpra/codecs/dec_avcodec2/decoder.pyx
@@ -66,7 +66,7 @@ cdef extern from "libavutil/pixfmt.h":
     AVPixelFormat AV_PIX_FMT_GBRP
 
 cdef extern from "libavcodec/avcodec.h":
-    int CODEC_FLAG2_FAST
+    int AV_CODEC_FLAG2_FAST
 
     ctypedef struct AVFrame:
         uint8_t **data
@@ -371,7 +371,7 @@ cdef class Decoder:
         self.codec_ctx.thread_safe_callbacks = 1
         self.codec_ctx.thread_type = 2      #FF_THREAD_SLICE: allow more than one thread per frame
         self.codec_ctx.thread_count = 0     #auto
-        self.codec_ctx.flags2 |= CODEC_FLAG2_FAST   #may cause "no deblock across slices" - which should be fine
+        self.codec_ctx.flags2 |= AV_CODEC_FLAG2_FAST    #may cause "no deblock across slices" - which should be fine
         r = avcodec_open2(self.codec_ctx, self.codec, NULL)
         if r<0:
             log.error("could not open codec: %s", av_error_str(r))
