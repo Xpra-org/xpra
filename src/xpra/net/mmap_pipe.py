0ec15c0b23f80afd8d5a04362aa0fcbd140982ef
Antoine Martin <totaam@xpra.org>
2018-02-02 11:00:42 +0000

    also show unit for min case
    git-svn-id: https://xpra.org/svn/Xpra/trunk@18256 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/mmap_pipe.py
+++ src/xpra/net/mmap_pipe.py
@@ -97,7 +97,7 @@ def init_client_mmap(mmap_group=None, socket_filename=None, size=128*1024*1024, 
                 s = os.stat(socket_filename)
                 os.fchown(fd, -1, s.st_gid)
                 os.fchmod(fd, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP)
-            assert mmap_size>=1024*1024, "mmap size is too small: %s (minimum is 1MB)" % std_unit(mmap_size)
+            assert mmap_size>=1024*1024, "mmap size is too small: %sB (minimum is 1MB)" % std_unit(mmap_size)
             assert mmap_size<=1024*1024*1024, "mmap is too big: %sB (maximum is 1GB)" % std_unit(mmap_size)
             log("using mmap file %s, fd=%s, size=%s", mmap_filename, fd, mmap_size)
             os.lseek(fd, mmap_size-1, os.SEEK_SET)
