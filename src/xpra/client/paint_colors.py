5be7add0c159c8afd1ab7b632295fc54a061a9bd
Antoine Martin <totaam@xpra.org>
2018-03-17 16:25:20 +0000

    #1781: webp was missing from the paint box colours
    git-svn-id: https://xpra.org/svn/Xpra/trunk@18745 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/paint_colors.py
+++ src/xpra/client/paint_colors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # This file is part of Xpra.
-# Copyright (C) 2017 Antoine Martin <antoine@devloop.org.uk>
+# Copyright (C) 2017,2018 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -33,6 +33,7 @@ BOX_COLORS = {
     "png/P"     : (0.2941131591796875,  0.0,                    0.509796142578125,  ALPHA),
     "rgb24"     : (0.9999847412109375,  0.6470489501953125,     0.0,                ALPHA),
     "rgb32"     : (0.9999847412109375,  0.0,                    0.0,                ALPHA),
+    "webp"      : (1.0,                 0.7529411764705882,     0.796078431372549,  ALPHA),
     "scroll"    : (0.6470489501953125,  0.164703369140625,      0.164703369140625,  ALPHA),
     "vp8"       : (0.0,                 0.501953125,            0.0,                ALPHA),
     "vp9"       : (0.901947021484375,   0.901947021484375,      0.980377197265625,  ALPHA),
