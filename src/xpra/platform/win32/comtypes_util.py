0888be9fffa7c6fa918d895d68dfae5ccaf93283
Antoine Martin <totaam@xpra.org>
2018-03-22 05:45:08 +0000

    #540: make it possible to not silence the comtypes debug logging using env var
    git-svn-id: https://xpra.org/svn/Xpra/trunk@18796 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/comtypes_util.py
+++ src/xpra/platform/win32/comtypes_util.py
@@ -7,6 +7,9 @@
 import logging
 logging.getLogger("comtypes").setLevel(logging.INFO)
 
+from xpra.util import envbool
+SILENCE_COMTYPES = envbool("XPRA_SILENCE_COMTYPES", True)
+
 
 class QuietenLogging(object):
 
@@ -15,6 +18,8 @@ class QuietenLogging(object):
         self.saved_levels = [x.getEffectiveLevel() for x in self.loggers]
 
     def __enter__(self):
+        if not SILENCE_COMTYPES:
+            return
         for logger in self.loggers:
             logger.setLevel(logging.WARNING)
         from comtypes import client                  #@UnresolvedImport
@@ -23,6 +28,8 @@ class QuietenLogging(object):
             client._generate.__verbose__ = False
 
     def __exit__(self, *_args):
+        if not SILENCE_COMTYPES:
+            return
         if self.verbose is not None:
             from comtypes import client                  #@UnresolvedImport
             client._generate.__verbose__ = self.verbose
