aaad7ed9bff31d6cbf5ea148032e8507268896cd
Antoine Martin <totaam@xpra.org>
2018-03-23 19:27:25 +0000

    missed from r18790
    git-svn-id: https://xpra.org/svn/Xpra/trunk@18809 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/xpra/platform/win32/netdev_query.py
@@ -0,0 +1,61 @@
+# This file is part of Xpra.
+# Copyright (C) 2018 Antoine Martin <antoine@devloop.org.uk>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+def logger():
+    from xpra.log import Logger
+    return Logger("network", "util", "win32")
+
+def get_interface_info(iface):
+    log = logger()
+    from xpra.platform.win32.comtypes_util import QuietenLogging
+    with QuietenLogging():
+        try:
+            from comtypes.client import CreateObject        #@UnresolvedImport
+            o = CreateObject("WbemScripting.SWbemLocator")
+            s = o.ConnectServer(".", "root\\cimv2")
+            query = "SELECT * FROM Win32_NetworkAdapter WHERE GUID='%s'" % iface
+            res = s.ExecQuery(query)
+            log("ExecQuery(%s) returned %i rows", query, res.Count)
+            if res.Count==1:
+                for r in res:
+                    props = {}
+                    for k in ("AdapterType", "Caption", "Description", "DeviceID", "GUID", "Index", "Name", "ProductName", "Speed"):
+                        try:
+                            v = r.Properties_[k]
+                        except Exception as e:
+                            log.error("Error retrieving '%s' from network adapter record:", k)
+                            log.error(" %s", e)
+                        else:
+                            props[k] = v.Value
+                    log("get_interface_info(%s)=%s" % (iface, props))
+                    return props
+        except Exception as e:
+            log("get_interface_info(%s)", iface, exc_info=True)
+            from xpra.util import first_time
+            if first_time("win32-network-query"):
+                log.error("Error: failed to query network interface:")
+                log.error(" %s", e)
+        return {}
+
+def get_interface_speed(fd, iface):
+    speed = get_interface_info(iface).get("Speed", 0)
+    try:
+        return int(speed)
+    except ValueError:
+        logger().error("Error: parsing speed value '%s'", speed, exc_info=True)
+        return 0
+
+
+def main():
+    from xpra.platform import program_context
+    with program_context("Network-Speed", "Network Speed Query Tool"):
+        from xpra.net.net_util import get_interfaces
+        interfaces = get_interfaces()
+        for iface in interfaces:
+            s = get_interface_speed(0, iface)
+            print("%s : %s" % (iface, s))
+
+if __name__ == "__main__":
+    main()
