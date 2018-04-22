8cd77bbc04692801c19bcbc9e100db67d2790074
Antoine Martin <totaam@xpra.org>
2018-04-22 16:24:54 +0000

    try harder to talk http when replying with a 400 error code
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19057 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/server_core.py
+++ src/xpra/server/server_core.py
@@ -56,11 +56,13 @@ PEEK_TIMEOUT = envint("XPRA_PEEK_TIMEOUT", 1)
 CHALLENGE_TIMEOUT = envint("XPRA_CHALLENGE_TIMEOUT", 120)
 
 
-HTTP_UNSUPORTED = b"""<head>
-<title>Error response</title>
+HTTP_UNSUPORTED = b"""HTTP/1.1 400 Bad request syntax or unsupported method
+
+<head>
+<title>Server Error</title>
 </head>
 <body>
-<h1>Error response</h1>
+<h1>Server Error</h1>
 <p>Error code 400.
 <p>Message: this port does not support HTTP requests.
 <p>Error code explanation: 400 = Bad request syntax or unsupported method.
