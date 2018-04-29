598f5aaa51ebddb806e1fbca35df492bd8c7d33e
Antoine Martin <totaam@xpra.org>
2018-04-29 06:12:44 +0000

    remove outdated comment: we support python3 here
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19111 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/shadow_x11_server.py
+++ src/xpra/x11/shadow_x11_server.py
@@ -4,7 +4,6 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-#ensures we only load GTK2:
 from xpra.x11.gtk_x11.gdk_display_source import init_display_source #@UnresolvedImport
 init_display_source()
 from xpra.x11.x11_server_core import X11ServerCore
