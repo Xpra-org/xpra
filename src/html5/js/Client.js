d550b2b1ce255f66121eabab0a06fe76d4f1630c
Antoine Martin <totaam@xpra.org>
2018-05-13 09:14:25 +0000

    drop unused legacy 'x264' encoding prefix
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19301 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Client.js
+++ src/html5/js/Client.js
@@ -1053,7 +1053,6 @@ XpraClient.prototype._make_hello = function() {
 			"vp8+webm"	: ["YUV420P"],
 			"webp"		: ["BGRX", "BGRA"],
 		},
-		"encoding.x264.YUV420P.profile"		: "baseline",
 		"encoding.h264.YUV420P.profile"		: "baseline",
 		"encoding.h264.YUV420P.level"		: "2.1",
 		"encoding.h264.cabac"				: false,
