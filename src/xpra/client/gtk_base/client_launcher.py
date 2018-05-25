e2820a83ff4c0fd269f12ae4c22cdd4044d482a5
Antoine Martin <totaam@xpra.org>
2018-05-25 06:16:33 +0000

    r19411 backport: don't prompt for ssh password if we already have it
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@19430 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/client_launcher.py
+++ src/xpra/client/gtk_base/client_launcher.py
@@ -564,6 +564,9 @@ class ApplicationWindow:
                 params["display"] = "auto"
                 params["display_as_args"] = []
             full_ssh = shlex.split(self.config.ssh)
+            ssh_cmd = full_ssh[0].lower()
+            is_putty = ssh_cmd.endswith("plink") or ssh_cmd.endswith("plink.exe")
+            params["is_putty"] = is_putty
             password = self.config.password
             host = self.config.host
             upos = host.find("@")
@@ -576,6 +579,8 @@ class ApplicationWindow:
                     #found separator: username:password@host
                     password = username[ppos+1:]
                     username = username[:ppos]
+            if password and is_putty:
+                full_ssh += ["-pw", password]
             if username:
                 params["username"] = username
                 full_ssh += ["-l", username]
@@ -583,7 +588,7 @@ class ApplicationWindow:
             if self.nostrict_host_check.get_active():
                 full_ssh += ["-o", "StrictHostKeyChecking=no"]
             if str(self.config.ssh_port)!="22":
-                if sys.platform.startswith("win"):
+                if is_putty:
                     full_ssh += ["-P", str(self.config.ssh_port)]
                 else:
                     full_ssh += ["-p", str(self.config.ssh_port)]
