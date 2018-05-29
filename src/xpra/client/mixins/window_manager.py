29bcc9e3615dd0caa3da37574f8296bc83dfed8e
Antoine Martin <totaam@xpra.org>
2018-05-29 10:19:55 +0000

    python3 strings byte us again
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19516 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/mixins/window_manager.py
+++ src/xpra/client/mixins/window_manager.py
@@ -1219,7 +1219,7 @@ class WindowClient(StubClientMixin):
         """ this runs from the draw thread above """
         wid = packet[1]
         window = self._id_to_window.get(wid)
-        if packet[0]=="eos":
+        if packet[0]==b"eos":
             if window:
                 window.eos()
             return
@@ -1227,7 +1227,7 @@ class WindowClient(StubClientMixin):
         if not window:
             #window is gone
             def draw_cleanup():
-                if coding=="mmap":
+                if coding==b"mmap":
                     assert self.mmap_enabled
                     from xpra.net.mmap_pipe import int_from_buffer
                     def free_mmap_area():
