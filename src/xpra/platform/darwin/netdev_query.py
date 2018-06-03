07d3829ca8daa1ec56c8fe4a62a33d2631d81efe
Antoine Martin <totaam@xpra.org>
2018-06-03 16:35:15 +0000

    #1860: query networkinterface on macos
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19565 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/netdev_query.py
+++ src/xpra/platform/darwin/netdev_query.py
@@ -2,3 +2,52 @@
 # Copyright (C) 2018 Antoine Martin <antoine@devloop.org.uk>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
+
+
+from SystemConfiguration import (
+    SCNetworkInterfaceCopyAll,                      #@UnresolvedImport
+    SCNetworkInterfaceGetBSDName,                   #@UnresolvedImport
+    SCNetworkInterfaceGetHardwareAddressString,     #@UnresolvedImport
+    SCNetworkInterfaceGetInterfaceType,             #@UnresolvedImport
+    SCNetworkInterfaceGetSupportedProtocolTypes,    #@UnresolvedImport
+    )
+
+
+def get_interface_info(_fd, iface):
+    r = SCNetworkInterfaceCopyAll()
+    if len(r)==1:
+        return do_get_interface_info(r[0])
+    if iface:
+        for scnetworkinterface in r:
+            if str(SCNetworkInterfaceGetBSDName(scnetworkinterface))==iface:
+                return do_get_interface_info(scnetworkinterface)
+    return {}
+
+def do_get_interface_info(scnetworkinterface):
+    info = {}
+    bsdname = SCNetworkInterfaceGetBSDName(scnetworkinterface)
+    if bsdname:
+        info["name"] = str(bsdname)
+    hw = SCNetworkInterfaceGetHardwareAddressString(scnetworkinterface)
+    if hw:
+        info["hardware-address"] = str(hw)
+    t = SCNetworkInterfaceGetInterfaceType(scnetworkinterface)
+    if t:
+        info["adapter-type"] = str(t)
+    #SCNetworkInterfaceGetLocalizedDisplayName
+    p = SCNetworkInterfaceGetSupportedProtocolTypes(scnetworkinterface)
+    if p:
+        info["protocols"] = tuple(str(x) for x in p)
+    return info
+
+
+def main():
+    r = SCNetworkInterfaceCopyAll()
+    print("%i interfaces:" % len(r))
+    for scnetworkinterface in r:
+        info = do_get_interface_info(scnetworkinterface)
+        print(info)
+
+
+if __name__ == "__main__":
+    main()
