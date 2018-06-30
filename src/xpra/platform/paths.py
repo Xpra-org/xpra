04e2b130926bcc0a4063678e61660871e067a482
Antoine Martin <totaam@xpra.org>
2018-06-30 11:29:27 +0000

    prefer XDG_RUNTIME_DIR location for run-xpra script
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19793 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/paths.py
+++ src/xpra/platform/paths.py
@@ -101,7 +101,7 @@ def do_get_script_bin_dirs():
 def get_remote_run_xpra_scripts():
     return envaslist_or_delegate("XPRA_REMOTE_RUN_XPRA_SCRIPTS", do_get_remote_run_xpra_scripts)
 def do_get_remote_run_xpra_scripts():
-    return ["~/.xpra/run-xpra", "$XDG_RUNTIME_DIR/xpra/run-xpra", "xpra", "/usr/local/bin/xpra"]
+    return ["$XDG_RUNTIME_DIR/xpra/run-xpra", "~/.xpra/run-xpra", "xpra", "/usr/local/bin/xpra"]
 
 
 def get_sshpass_command():
