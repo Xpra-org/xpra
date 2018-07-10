bfeea4eb79bcd9d6256fb2eba66331001286c97f
Antoine Martin <totaam@xpra.org>
2018-07-10 18:37:50 +0000

    opencv is broken again..
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19918 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -1452,7 +1452,7 @@ if WIN32:
                     #this is a mac osx thing:
                     "ctypes.macholib")
 
-    if webcam_ENABLED:
+    if webcam_ENABLED and False:
         external_includes.append("cv2")
     else:
         remove_packages("cv2")
