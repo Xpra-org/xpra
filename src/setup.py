6d5f215ca5a0811aad03a106000b030257e31108
Antoine Martin <totaam@xpra.org>
2018-07-10 14:50:42 +0000

    #1884: disable deprecation warnings with python 3.7
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19908 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -729,6 +729,10 @@ def exec_pkgconfig(*pkgs_options, **ekw):
             eifd = []
         for eif in eifd:
             add_to_keywords(kw, 'extra_compile_args', eif)
+    if sys.version_info>=(3,7):
+        #we'll switch to the "new" buffer interface after we drop support for Python 2.7
+        #until then, silence those deprecation warnings:
+        add_to_keywords(kw, 'extra_compile_args', "-Wno-error=deprecated-declarations")
     if PIC_ENABLED:
         add_to_keywords(kw, 'extra_compile_args', "-fPIC")
     if debug_ENABLED:
