650437714c15c8a1ff29e2407c6c32c42403e48f
Antoine Martin <totaam@xpra.org>
2018-07-29 17:42:07 +0000

    #1646 add paramiko to win32 setup and update scripts
    git-svn-id: https://xpra.org/svn/Xpra/trunk@19972 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/win32/UPDATE_PYTHON_LIBS.sh
+++ src/win32/UPDATE_PYTHON_LIBS.sh
@@ -8,7 +8,7 @@
 #using easy-install for python libraries which are not packaged by mingw:
 # currently disabled, build from patched source only: websockify
 # currently disabled, do not update past 1.8.x: cryptography
-for x in rencode xxhash enum34 enum-compat zeroconf lz4 websocket-client comtypes PyOpenGL PyOpenGL_accelerate cffi cryptography pycparser nvidia-ml-py appdirs setproctitle netifaces pyu2f python-ldap ldap3;
+for x in rencode xxhash enum34 enum-compat zeroconf lz4 websocket-client comtypes PyOpenGL PyOpenGL_accelerate cffi cryptography pycparser nvidia-ml-py appdirs setproctitle netifaces pyu2f python-ldap ldap3 bcrypt pynacl paramiko;
 do
     easy_install-2.7 -U -Z $x
     easy_install-3.7 -U -Z $x
