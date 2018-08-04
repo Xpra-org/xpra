110d39ad4fba87db6808cb650f6fa71355ff027c
Antoine Martin <totaam@xpra.org>
2018-08-04 18:13:29 +0000

    switch to using mingw packages if those are available
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20022 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/win32/MINGW_SETUP.sh
+++ src/win32/MINGW_SETUP.sh
@@ -36,19 +36,22 @@ $PACMAN --noconfirm -S ${XPKG}lz4 ${XPKG}lzo2 ${XPKG}xxhash ${XPKG}libsodium
 #python3 GStreamer bindings:
 $PACMAN --noconfirm -S ${XPKG}gst-python
 #development tools and libs for building extra packages:
-$PACMAN --noconfirm -S base-devel ${XPKG}yasm ${XPKG}nasm subversion rsync gtk-doc ${XPKG}cmake ${XPKG}gcc ${XPKG}pkg-config ${XPKG}libffi ${XPKG}gss ${XPKG}openldap
-#python libraries and install and packaging tools:
-$PACMAN --noconfirm -S ${XPKG}python2-numpy ${XPKG}python2-pillow ${XPKG}cython2 ${XPKG}python2-setuptools ${XPKG}python2-cx_Freeze ${XPKG}python3-cx_Freeze zip
-#python3 versions (not all are really needed if just using python3 for sound):
-$PACMAN --noconfirm -S ${XPKG}python3-numpy ${XPKG}python3-pillow ${XPKG}cython
+$PACMAN --noconfirm -S base-devel ${XPKG}yasm ${XPKG}nasm subversion rsync zip gtk-doc ${XPKG}cmake ${XPKG}gcc ${XPKG}pkg-config ${XPKG}libffi ${XPKG}gss ${XPKG}openldap
+#python libraries and packaging tools:
+for x in cryptography cffi pycparser numpy pillow cx_Freeze appdirs; do
+	$PACMAN --noconfirm -S ${XPKG}python2-${x}
+	$PACMAN --noconfirm -S ${XPKG}python3-${x}
+done
+$PACMAN --noconfirm -S ${XPKG}cython2 ${XPKG}python2-setuptools
+$PACMAN --noconfirm -S ${XPKG}cython
 #using easy-install for python libraries which are not packaged by mingw:
 #build pynacl against the system library:
 export SODIUM_INSTALL=system
 easy_install-2.7 -U -Z enum34 enum-compat
-for x in rencode xxhash zeroconf lz4 websocket-client netifaces comtypes PyOpenGL PyOpenGL_accelerate websockify cffi pycparser cryptography nvidia-ml-py appdirs setproctitle pyu2f python-ldap ldap3 bcrypt pynacl paramiko; do
+for x in rencode xxhash zeroconf lz4 websocket-client netifaces comtypes PyOpenGL PyOpenGL_accelerate websockify nvidia-ml-py setproctitle pyu2f python-ldap ldap3 bcrypt pynacl paramiko; do
     easy_install-2.7 -U -Z $x
     easy_install-3.7 -U -Z $x
 done
 
 #for webcam support:
-$PACMAN --noconfirm -S ${XPKG}opencv ${XPKG}hdf5 ${XPKG}tesseract-ocr
+#$PACMAN --noconfirm -S ${XPKG}opencv ${XPKG}hdf5 ${XPKG}tesseract-ocr
--- src/win32/UPDATE_PYTHON_LIBS.sh
+++ src/win32/UPDATE_PYTHON_LIBS.sh
@@ -8,7 +8,7 @@
 #using easy-install for python libraries which are not packaged by mingw:
 # currently disabled, build from patched source only: websockify
 export SODIUM_INSTALL=system
-for x in rencode xxhash enum34 enum-compat zeroconf lz4 websocket-client comtypes PyOpenGL PyOpenGL_accelerate cffi pycparser cryptography nvidia-ml-py appdirs setproctitle cryptography netifaces pyu2f python-ldap ldap3 bcrypt pynacl paramiko;
+for x in rencode xxhash zeroconf lz4 websocket-client netifaces comtypes PyOpenGL PyOpenGL_accelerate websockify nvidia-ml-py setproctitle pyu2f python-ldap ldap3 bcrypt pynacl paramiko; do
 do
     easy_install-2.7 -U -Z $x
     easy_install-3.7 -U -Z $x
