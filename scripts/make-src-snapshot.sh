a677063e0062e0506886ddb09f7252a9ae101cd4
Antoine Martin <totaam@xpra.org>
2018-09-30 06:15:49 +0000

    r20424 backport: fedora 29 compatibility
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@20561 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- scripts/make-src-snapshot.sh
+++ scripts/make-src-snapshot.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 if [ -z "${VERSION}" ]; then
-	VERSION=`PYTHONPATH="./src" python -c "from xpra import __version__; print(__version__)"`
+	VERSION=`PYTHONPATH="./src" python2 -c "from xpra import __version__; print(__version__)"`
 fi
 DIR=xpra-${VERSION}
 rm -fr "${DIR}"
@@ -10,14 +10,14 @@ rm -f "src/xpra/build_info.py"
 rm -f "src/xpra/src_info.py"
 #record current svn info into xpra/src_info.py:
 pushd src
-PYTHONPATH="." python -c "from add_build_info import record_src_info;record_src_info()"
+PYTHONPATH="." python2 -c "from add_build_info import record_src_info;record_src_info()"
 svn info > ./svn-info
 svnversion > ./svn-version
 popd
 cp -apr src ${DIR}
 pushd "${DIR}"
 rm -fr "dist" "build" "install" "MANIFEST"
-python ./setup.py clean
+python2 ./setup.py clean
 popd
 find ${DIR} -name ".svn" -exec rm -fr {} \; 2>&1 | grep -v "No such file or directory"
 find ${DIR} -name ".pyc" -exec rm -fr {} \;
@@ -28,7 +28,7 @@ find ${DIR} -name "*.pyd" -exec rm -fr {} \;
 find ${DIR} -name "*.egg" -exec rm -fr {} \;
 
 RAW_SVN_VERSION=`svnversion`
-SVN_REVISION=`python -c "x=\"$RAW_SVN_VERSION\";y=x.split(\":\");y.reverse();z=y[0];print \"\".join([c for c in z if c in \"0123456789\"])"`
+SVN_REVISION=`python2 -c "x=\"$RAW_SVN_VERSION\";y=x.split(\":\");y.reverse();z=y[0];print \"\".join([c for c in z if c in \"0123456789\"])"`
 MODULE_DIRS="xpra"
 echo "adding svn revision ${SVN_REVISION} to __init__.py in ${MODULE_DIRS}"
 for module in ${MODULE_DIRS}; do
