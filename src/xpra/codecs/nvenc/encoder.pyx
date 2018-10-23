75884fb5023ac5a732f0d0de06585420e5c1217c
Antoine Martin <totaam@xpra.org>
2018-10-23 15:27:38 +0000

    add debug to parseguid since I have seen some mysterious failures
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20796 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/nvenc/encoder.pyx
+++ src/xpra/codecs/nvenc/encoder.pyx
@@ -1124,7 +1124,10 @@ cdef GUID c_parseguid(src) except *:
     parts = src.split("-")    #ie: ["CE788D20", "AAA9", ...]
     nparts = []
     for i, s in (0, 4), (1, 2), (2, 2), (3, 2), (4, 6):
-        b = atob(array.array('B', binascii.unhexlify(strtobytes(parts[i]))))
+        part = parts[i]
+        binv = binascii.unhexlify(strtobytes(part))
+        b = atob(array.array(b'B', binv))
+        log("c_parseguid bytes(%s)=%r", part, b)
         v = 0
         for j in range(s):
             c = b[j]
