000423c42991b77e7123112500f599ad4739702b
Antoine Martin <totaam@xpra.org>
2018-10-29 08:52:29 +0000

    try to preserve the python interpreter version when launching xpra subcommands
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20870 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/xposix/paths.py
+++ src/xpra/platform/xposix/paths.py
@@ -168,3 +168,10 @@ def do_get_sound_command():
     if SOUND_PYTHON3:
         return ["python3"]+get_xpra_command()
     return get_xpra_command()
+
+def do_get_xpra_command():
+    #try to use the same "xpra" executable that launched this server,
+    #whilst also preserving the python interpreter version:
+    if sys.argv and sys.argv[0].lower().endswith("/xpra"):
+        return ["python%i" % sys.version_info[0], sys.argv[0]]
+    return ["xpra"]
