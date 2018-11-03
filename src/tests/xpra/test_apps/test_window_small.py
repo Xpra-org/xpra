9247c149a4172a5e5c2a228ec91bd8ba5e458179
Antoine Martin <totaam@xpra.org>
2018-11-03 11:27:25 +0000

    enable the crasher OR window, add non-decorated window
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20923 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_small.py
+++ src/tests/xpra/test_apps/test_window_small.py
@@ -5,7 +5,7 @@ import gtk
 width = 50
 height = 10
 
-def make_win(window_type=gtk.WINDOW_TOPLEVEL, min_width=-1, min_height=-1, max_width=-1, max_height=-1):
+def make_win(window_type=gtk.WINDOW_TOPLEVEL, min_width=-1, min_height=-1, max_width=-1, max_height=-1, decorated=True):
 	window = gtk.Window(window_type)
 	window.set_title("min=%s - max=%s" % ((min_width, min_height), (max_width, max_height)))
 	#window.set_size_request(width, height)
@@ -17,11 +17,13 @@ def make_win(window_type=gtk.WINDOW_TOPLEVEL, min_width=-1, min_height=-1, max_w
 							width_inc=8, height_inc=8,
 							#min_aspect=1, max_aspect=1,
 							)
+	window.set_decorated(decorated)
 	window.show_all()
 
 def main():
 	make_win(gtk.WINDOW_TOPLEVEL, width, height, width, height)
-	#make_win(gtk.WINDOW_POPUP, width, height, width, height)
+	make_win(gtk.WINDOW_TOPLEVEL, width, height, width, height, False)
+	make_win(gtk.WINDOW_POPUP, width, height, width, height)
 	gtk.main()
 
 
