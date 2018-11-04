05097475745461cb30eef35fbad8be676468810b
Antoine Martin <totaam@xpra.org>
2018-11-04 14:05:17 +0000

    fix icons datatype warning: don't coerce to string when filtering
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20943 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/source/encodings_mixin.py
+++ src/xpra/server/source/encodings_mixin.py
@@ -293,8 +293,8 @@ class EncodingsMixin(StubSourceMixin):
         for k in c.keys():
             #yaml gives us str..
             k = strtobytes(k)
-            if k.startswith(b"theme.") or  k.startswith(b"encoding.icons."):
-                self.icons_encoding_options[k.replace(b"encoding.icons.", b"").replace(b"theme.", b"")] = c.strget(k)
+            if k.startswith(b"theme.") or k.startswith(b"encoding.icons."):
+                self.icons_encoding_options[k.replace(b"encoding.icons.", b"").replace(b"theme.", b"")] = c.strlistget(k)
             elif k.startswith(b"encoding."):
                 stripped_k = k[len(b"encoding."):]
                 if stripped_k in (b"transparency",
