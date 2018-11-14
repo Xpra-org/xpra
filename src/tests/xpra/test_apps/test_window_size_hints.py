8dbf429f61f1a9663089d7603f3af809ca192e8e
Antoine Martin <totaam@xpra.org>
2018-11-14 08:01:30 +0000

    #1969: add tiny window to test platform min-size
    git-svn-id: https://xpra.org/svn/Xpra/trunk@20991 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/tests/xpra/test_apps/test_window_size_hints.py
+++ src/tests/xpra/test_apps/test_window_size_hints.py
@@ -5,9 +5,9 @@ import gtk
 width = 400
 height = 200
 
-def make_win(min_width=-1, min_height=-1, max_width=-1, max_height=-1):
+def make_win(title=None, min_width=-1, min_height=-1, max_width=-1, max_height=-1):
 	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-	window.set_title("min=%s - max=%s" % ((min_width, min_height), (max_width, max_height)))
+	window.set_title(title or ("min=%s - max=%s" % ((min_width, min_height), (max_width, max_height))))
 	#window.set_size_request(width, height)
 	window.connect("delete_event", gtk.mainquit)
 	window.set_geometry_hints(window,
@@ -20,10 +20,10 @@ def make_win(min_width=-1, min_height=-1, max_width=-1, max_height=-1):
 	window.show_all()
 
 def main():
-	make_win()
-	make_win(0, 0, width, height)
-	make_win(width//2, height//2)
-	make_win(width//2, height//2, width*2, height*2)
+	make_win("tiny", 0, 0, 5, 5)
+	make_win(None, 0, 0, width, height)
+	make_win(None, width//2, height//2)
+	make_win(None, width//2, height//2, width*2, height*2)
 	gtk.main()
 
 
