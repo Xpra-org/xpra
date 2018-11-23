59ba767792904823fa1d63eea44aa88e9cfc8e4e
Antoine Martin <totaam@xpra.org>
2018-11-23 05:57:00 +0000

    r20996 minimal backport: sanitization code must use the correct attribute names to find values to sanitize (it runs after the renaming)
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@21082 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/gtk2/models/size_hints_util.py
+++ src/xpra/x11/gtk2/models/size_hints_util.py
@@ -39,7 +39,7 @@ def sanitize_size_hints(size_hints):
             if f is None or f<=0 or f>=MAX_ASPECT:
                 log.warn("clearing invalid aspect hint value for %s: %s", attr, v)
                 del size_hints[attr]
-    for attr in ["max_size", "min_size", "base_size", "resize_inc",
+    for attr in ["maximum-size", "minimum-size", "base-size", "increment",
                  "min_aspect_ratio", "max_aspect_ratio"]:
         v = size_hints.get(attr)
         if v is not None:
@@ -51,19 +51,19 @@ def sanitize_size_hints(size_hints):
                 log("clearing invalid size hint value for %s: %s", attr, v)
                 del size_hints[attr]
     #if max-size is smaller than min-size (bogus), clamp it..
-    mins = size_hints.get("min_size")
-    maxs = size_hints.get("max_size")
+    mins = size_hints.get("minimum-size")
+    maxs = size_hints.get("maximum-size")
     if mins is not None and maxs is not None:
         minw,minh = mins
         maxw,maxh = maxs
         if minw<=0 and minh<=0:
             #doesn't do anything
-            size_hints["min_size"] = None
+            size_hints["minimum-size"] = None
         if maxw<=0 or maxh<=0:
             #doesn't mak sense!
-            size_hints["max_size"] = None
+            size_hints["maximum-size"] = None
         if maxw<minw or maxh<minh:
-            size_hints["min_size"] = max(minw, maxw), max(minh, maxh)
-            size_hints["max_size"] = size_hints.min_size
+            size_hints["minimum-size"] = max(minw, maxw), max(minh, maxh)
+            size_hints["maximum-size"] = size_hints.min_size
             log.warn("invalid min_size=%s / max_size=%s changed to: %s / %s",
-                     mins, maxs, size_hints["min_size"], size_hints["max_size"])
+                     mins, maxs, size_hints["minimum-size"], size_hints["maximum-size"])
