fb6611d6ade788f30f3647a0e2b560f6151ffd0d
Antoine Martin <totaam@xpra.org>
2018-11-26 08:54:35 +0000

    add shadow x11 capture test command
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21099 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/shadow_x11_server.py
+++ src/xpra/x11/shadow_x11_server.py
@@ -168,3 +168,32 @@ class ShadowX11Server(GTKShadowServerBase, X11ServerCore):
         assert encoding=="png"  #use fixed encoding for now
         from xpra.net.compression import Compressed
         return ["screenshot", w, h, encoding, rowstride, Compressed(encoding, data)]
+
+
+def main(filename):
+    from xpra.os_util import StringIOClass, memoryview_to_bytes
+    from xpra.gtk_common.gtk_util import get_default_root_window, get_root_size
+    root = get_default_root_window()
+    capture = setup_capture(root)
+    capture.refresh()
+    w, h = get_root_size()
+    image = capture.get_image(0, 0, w, h)
+    from PIL import Image
+    fmt = image.get_pixel_format().replace("X", "A")
+    pixels = memoryview_to_bytes(image.get_pixels())
+    pil_image = Image.frombuffer("RGBA", (w, h), pixels, "raw", fmt, image.get_rowstride())
+    buf = StringIOClass()
+    pil_image.save(buf, "png")
+    data = buf.getvalue()
+    buf.close()
+    with open(filename, "wb") as f:
+        f.write(data)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)!=2:
+        print("usage: %s filename.png" % sys.argv[0])
+        v = 1
+    else:
+        v = main(sys.argv[1])
+    sys.exit(v)
