245d7619634e50f98aeff0f0c6819249385cb9ba
Antoine Martin <totaam@xpra.org>
2019-01-19 08:10:56 +0000

    use '&' instead of python's modulo (faster / smaller code), remove redundant check
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21396 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/xor/cyxor.pyx
+++ src/xpra/codecs/xor/cyxor.pyx
@@ -58,8 +58,8 @@ def hybi_unmask(data, unsigned int offset, unsigned int datalen):
     dp = buf+offset+4
     #we skip the first 'align' bytes in the output buffer,
     #to ensure that its alignment is the same as the input data buffer
-    cdef unsigned int align = (<uintptr_t> dp) % 4
-    cdef unsigned int initial_chars = (4-align) % 4
+    cdef unsigned int align = (<uintptr_t> dp) & 0x3
+    cdef unsigned int initial_chars = (4-align) & 0x3
     cdef MemBuf out_buf = getbuf(datalen+align)
     op = <uintptr_t> out_buf.get_mem()
     #char pointers:
@@ -68,9 +68,8 @@ def hybi_unmask(data, unsigned int offset, unsigned int datalen):
     cdef unsigned char *ocbuf = <unsigned char *> op
     cdef unsigned int i, j
     #bytes at a time until we reach the 32-bit boundary:
-    if initial_chars:
-        for 0 <= i < initial_chars:
-            ocbuf[align+i] = dcbuf[i] ^ mcbuf[i%4]
+    for 0 <= i < initial_chars:
+        ocbuf[align+i] = dcbuf[i] ^ mcbuf[i & 0x3]
     #32-bit pointers:
     cdef uint32_t *dbuf
     cdef uint32_t *obuf
@@ -85,15 +84,14 @@ def hybi_unmask(data, unsigned int offset, unsigned int datalen):
             mask_value = 0
             for 0 <= i < 4:
                 mask_value = mask_value<<8
-                mask_value += mcbuf[(3-i+initial_chars) % 4]
+                mask_value += mcbuf[(3-i+initial_chars) & 0x3]
             for 0 <= i < uint32_steps:
                 obuf[i] = dbuf[i] ^ mask_value
         #bytes at a time again at the end:
-        last_chars = (datalen-initial_chars) % 4
-        if last_chars:
-            for 0 <= i < last_chars:
-                j = datalen-last_chars+i
-                ocbuf[align+j] = dcbuf[j] ^ mcbuf[j%4]
+        last_chars = (datalen-initial_chars) & 0x3
+        for 0 <= i < last_chars:
+            j = datalen-last_chars+i
+            ocbuf[align+j] = dcbuf[j] ^ mcbuf[j & 0x3]
     if align>0:
         return memoryview(out_buf)[align:]
     return memoryview(out_buf)
