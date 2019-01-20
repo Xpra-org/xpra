a7a680376b2f50414aacc9f549860100b54a4594
Antoine Martin <totaam@xpra.org>
2019-01-20 12:16:21 +0000

    make it consistent with the rest of the project: use spaces not tabs
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21422 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/mdns/zeroconf_listener.py
+++ src/xpra/net/mdns/zeroconf_listener.py
@@ -5,7 +5,7 @@
 # later version. See the file COPYING for details.
 
 import socket
-from zeroconf import ServiceBrowser, Zeroconf		#@UnresolvedImport
+from zeroconf import ServiceBrowser, Zeroconf        #@UnresolvedImport
 
 from xpra.log import Logger
 
@@ -14,83 +14,87 @@ log = Logger("network", "mdns")
 
 class ZeroconfListener(object):
 
-	def __init__(self, service_type, mdns_found=None, mdns_add=None, mdns_remove=None):
-		log("ZeroconfListener%s", (service_type, mdns_found, mdns_add, mdns_remove))
-		self.zeroconf = Zeroconf()
-		self.browser = None
-		if not service_type.endswith("local."):
-			service_type += "local."
-		self.service_type = service_type
-		self.mdns_found = mdns_found
-		self.mdns_add = mdns_add
-		self.mdns_remove = mdns_remove
+    def __init__(self, service_type, mdns_found=None, mdns_add=None, mdns_remove=None):
+        log("ZeroconfListener%s", (service_type, mdns_found, mdns_add, mdns_remove))
+        self.zeroconf = Zeroconf()
+        self.browser = None
+        if not service_type.endswith("local."):
+            service_type += "local."
+        self.service_type = service_type
+        self.mdns_found = mdns_found
+        self.mdns_add = mdns_add
+        self.mdns_remove = mdns_remove
 
-	def __repr__(self):
-		return "ZeroconfListener(%s)" % self.service_type
+    def __repr__(self):
+        return "ZeroconfListener(%s)" % self.service_type
 
-	def remove_service(self, zeroconf, stype, name):
-		log("remove_service%s", (zeroconf, stype, name))
-		if self.mdns_remove:
-			domain = "local"
-			self.mdns_remove(0, 0, name, stype, domain, 0)
+    def remove_service(self, zeroconf, stype, name):
+        log("remove_service%s", (zeroconf, stype, name))
+        if self.mdns_remove:
+            domain = "local"
+            self.mdns_remove(0, 0, name, stype, domain, 0)
 
-	def add_service(self, zeroconf, stype, name):
-		log("add_service%s", (zeroconf, stype, name))
-		info = zeroconf.get_service_info(stype, name)
-		log("service info: %s", info)
-		if self.mdns_add:
-			interface = 0
-			protocol = 0
-			name = info.name
-			domain = "local"
-			address = socket.inet_ntoa(info.address)
-			self.mdns_add(interface, protocol, info.name, info.type, domain, info.server, address, info.port, info.properties)
+    def add_service(self, zeroconf, stype, name):
+        log("add_service%s", (zeroconf, stype, name))
+        info = zeroconf.get_service_info(stype, name)
+        log("service info: %s", info)
+        if self.mdns_add:
+            interface = 0
+            protocol = 0
+            name = info.name
+            stype = info.type
+            domain = "local"
+            server = info.server
+            address = socket.inet_ntoa(info.address)
+            port = info.port
+            props = info.properties
+            self.mdns_add(interface, protocol, name, stype, domain, server, address, port, props)
 
-	def start(self):
-		self.browser = ServiceBrowser(self.zeroconf, self.service_type, listener=self)
-		log("ServiceBrowser%s=%s", (self.zeroconf, self.service_type, self), self.browser)
+    def start(self):
+        self.browser = ServiceBrowser(self.zeroconf, self.service_type, listener=self)
+        log("ServiceBrowser%s=%s", (self.zeroconf, self.service_type, self), self.browser)
 
-	def stop(self):
-		b = self.browser
-		if b:
-			self.browser = None
-			try:
-				b.cancel()
-			except:
-				pass
-		zc = self.zeroconf
-		if zc:
-			self.zeroconf = None
-			try:
-				zc.close()
-			except:
-				pass
+    def stop(self):
+        b = self.browser
+        if b:
+            self.browser = None
+            try:
+                b.cancel()
+            except:
+                pass
+        zc = self.zeroconf
+        if zc:
+            self.zeroconf = None
+            try:
+                zc.close()
+            except:
+                pass
 
 
 def main():
-	def mdns_found(*args):
-		print("mdns_found: %s" % (args, ))
-	def mdns_add(*args):
-		print("mdns_add: %s" % (args, ))
-	def mdns_remove(*args):
-		print("mdns_remove: %s" % (args, ))
+    def mdns_found(*args):
+        print("mdns_found: %s" % (args, ))
+    def mdns_add(*args):
+        print("mdns_add: %s" % (args, ))
+    def mdns_remove(*args):
+        print("mdns_remove: %s" % (args, ))
 
-	from xpra.gtk_common.gobject_compat import import_glib
-	glib = import_glib()
-	glib.threads_init()
-	loop = glib.MainLoop()
+    from xpra.gtk_common.gobject_compat import import_glib
+    glib = import_glib()
+    glib.threads_init()
+    loop = glib.MainLoop()
 
-	from xpra.platform import program_context
-	with program_context("zeroconf-listener", "zeroconf-listener"):
-		from xpra.net.mdns import XPRA_MDNS_TYPE
-		listener = ZeroconfListener(XPRA_MDNS_TYPE+"local.", mdns_found, mdns_add, mdns_remove)
-		log("listener=%s" % listener)
-		listener.start()
-		try:
-			loop.run()
-		finally:
-			listener.stop()
+    from xpra.platform import program_context
+    with program_context("zeroconf-listener", "zeroconf-listener"):
+        from xpra.net.mdns import XPRA_MDNS_TYPE
+        listener = ZeroconfListener(XPRA_MDNS_TYPE+"local.", mdns_found, mdns_add, mdns_remove)
+        log("listener=%s" % listener)
+        listener.start()
+        try:
+            loop.run()
+        finally:
+            listener.stop()
 
 
 if __name__ == "__main__":
-	main()
+    main()
