7cdc09dab3b3a343f5e210678bbd3840897c47cb
Antoine Martin <totaam@xpra.org>
2019-02-19 14:07:10 +0000

    stricter exception checks, better warning message format
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21716 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/models/size_hints_util.py
+++ src/xpra/x11/models/size_hints_util.py
@@ -1,6 +1,6 @@
 # This file is part of Xpra.
 # Copyright (C) 2008, 2009 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2011-2015 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2011-2019 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -18,14 +18,14 @@ def sanitize_size_hints(size_hints):
         Some applications may set nonsensical values,
         try our best to come up with something that can actually be used.
     """
-    if size_hints is None:
+    if not size_hints:
         return
     for attr in ("min-aspect", "max-aspect"):
         v = size_hints.get(attr)
         if v is not None:
             try:
                 f = float(v)
-            except:
+            except ValueError:
                 f = None
             if f is None or f<=0 or f>=MAX_ASPECT:
                 log.warn("Warning: clearing invalid aspect hint value for %s: %s", attr, v)
@@ -35,7 +35,7 @@ def sanitize_size_hints(size_hints):
         if v is not None:
             try:
                 f = float(v[0])/float(v[1])
-            except:
+            except (ValueError, ZeroDivisionError):
                 f = None
             if f is None or f<=0 or f>=MAX_ASPECT:
                 log.warn("Warning: clearing invalid aspect hint value for %s: %s", attr, v)
@@ -45,7 +45,7 @@ def sanitize_size_hints(size_hints):
         if v is not None:
             try:
                 w,h = v
-            except:
+            except (ValueError, TypeError):
                 w,h = None,None
             if (w is None or h is None) or w>=MAX_WINDOW_SIZE or h>=MAX_WINDOW_SIZE:
                 log("clearing invalid size hint value for %s: %s", attr, v)
@@ -55,9 +55,9 @@ def sanitize_size_hints(size_hints):
         if v is not None:
             try:
                 w,h = v
-            except:
+            except (ValueError, TypeError):
                 w,h = None,None
-            if (w is None or w==0 or h is None or h==0) or w>=MAX_WINDOW_SIZE or h>=MAX_WINDOW_SIZE:
+            if (not w or not h) or w>=MAX_WINDOW_SIZE or h>=MAX_WINDOW_SIZE:
                 log.warn("Warning: clearing invalid size hint value for %s: %s", attr, v)
                 del size_hints[attr]
     #if max-size is smaller than min-size (bogus), clamp it..
@@ -98,5 +98,6 @@ def sanitize_size_hints(size_hints):
                 size_hints["minimum-size"] = minw, minh
                 size_hints["maximum-size"] = maxw, maxh
         if clamped:
-            log.warn("Warning: invalid min_size=%s / max_size=%s changed to: %s / %s",
-                     mins, maxs, size_hints.get("minimum-size"), size_hints.get("maximum-size"))
+            log.warn("Warning: invalid size hints")
+            log.warn(" min_size=%s / max_size=%s", mins, maxs)
+            log.warn(" changed to: %s / %s", size_hints.get("minimum-size"), size_hints.get("maximum-size"))
