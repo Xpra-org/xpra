1243ca812cf39b933b8e1e9a47b28242c32d236f
Antoine Martin <totaam@xpra.org>
2019-02-19 14:06:19 +0000

    implement calc_constrained_size for python3
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21715 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/gtk_common/gtk3/gdk_bindings.pyx
+++ src/xpra/gtk_common/gtk3/gdk_bindings.pyx
@@ -8,8 +8,7 @@
 
 from __future__ import absolute_import
 
-
-from libc.stdint cimport uintptr_t
+from math import ceil, floor
 
 from xpra.log import Logger
 log = Logger("bindings", "gtk")
@@ -84,5 +83,60 @@ cdef GdkWindow *get_gdkwindow(pywindow):
     return <GdkWindow*>unwrap(pywindow, Gdk.Window)
 
 def calc_constrained_size(int width, int height, object hints):
-    log.warn("Warning: calc_constrained_size not implemented yet for GTK3")
+    if not hints:
+        return width, height
+
+    def getintpair(key, int dv1, int dv2):
+        v = hints.get(key)
+        if v:
+            try:
+                return int(v[0]), int(v[1])
+            except (ValueError, IndexError):
+                pass
+        return dv1, dv2
+
+    cdef int min_width, min_height
+    cdef int max_width, max_height
+    cdef int base_width, base_height
+    cdef int e_width, e_height
+    cdef int increment_x, increment_y
+
+    min_width, min_height = getintpair("minimum-size", 0, 0)
+    if min_width>0:
+        width = max(min_width, width)
+    if min_height>0:
+        height = max(min_height, height)
+
+    max_width, max_height = getintpair("maximum-size", 2**16-1, 2**16-1)
+    if max_width>0:
+        width = min(max_width, width)
+    if max_height>0:
+        height = max(min_height, height)
+
+    base_width, base_height = getintpair("base-size", 0, 0)
+    increment_x, increment_y = getintpair("increment", 0, 0)
+    if increment_x:
+        e_width = max(width-base_width, 0)
+        width -= e_width%increment_x
+    if increment_y:
+        e_height = max(height-base_height, 0)
+        height -= e_height%increment_y
+
+    cdef double min_aspect, max_aspect 
+    if "min_aspect" in hints:
+        min_aspect = hints.get("min_aspect")
+        assert min_aspect>0
+        if width/height<min_aspect:
+            height = ceil(width*min_aspect)
+            if increment_y>1 or base_height>0:
+                e_height = max(height-base_height, 0)
+                height += increment_y-(e_height%increment_y)
+    if "max_aspect" in hints:
+        max_aspect = hints.get("max_aspect")
+        assert max_aspect>0
+        if width/height>max_aspect:
+            height = floor(width*max_aspect)
+            if increment_y>1 or base_height>0:
+                e_height = max(height-base_height, 0)
+                height = max(1, height-e_height%increment_y)
     return width, height
