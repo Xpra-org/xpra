75eb56559a9c0334b1dcdb37d79f8922c6d48cda
Antoine Martin <totaam@xpra.org>
2019-02-19 14:05:42 +0000

    remove unused logger
    git-svn-id: https://xpra.org/svn/Xpra/trunk@21714 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/gtk_common/gtk2/gdk_bindings.pyx
+++ src/xpra/gtk_common/gtk2/gdk_bindings.pyx
@@ -1,6 +1,6 @@
 # This file is part of Xpra.
 # Copyright (C) 2008, 2009 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2010-2017 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2010-2019 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -13,7 +13,7 @@ import gtk
 from gtk import gdk
 
 from xpra.log import Logger
-log = Logger("bindings", "gtk")
+
 clipboardlog = Logger("clipboard", "gtk")
 
 
