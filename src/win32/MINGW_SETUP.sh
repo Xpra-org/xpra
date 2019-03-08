f419451811af0beb1beb3812084a8fcaa95b5e8f
Antoine Martin <totaam@xpra.org>
2019-03-08 10:38:12 +0000

    we don't use xxhash as a library any more
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22011 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/win32/MINGW_SETUP.sh
+++ src/win32/MINGW_SETUP.sh
@@ -32,7 +32,7 @@ $PACMAN --noconfirm -S ${XPKG}python2 ${XPKG}python2-pygtk ${XPKG}gtkglext ${XPK
 #media libraries (more than we actually need):
 $PACMAN --noconfirm -S ${XPKG}ffmpeg ${XPKG}libyuv-git ${XPKG}gst-plugins-good ${XPKG}gst-plugins-bad ${XPKG}gst-plugins-ugly
 #network layer libraries:
-$PACMAN --noconfirm -S ${XPKG}lz4 ${XPKG}lzo2 ${XPKG}xxhash openssh sshpass
+$PACMAN --noconfirm -S ${XPKG}lz4 ${XPKG}lzo2 openssh sshpass
 #python3 GStreamer bindings:
 $PACMAN --noconfirm -S ${XPKG}gst-python
 #development tools and libs for building extra packages:
