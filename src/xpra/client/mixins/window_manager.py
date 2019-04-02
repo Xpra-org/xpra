e4b57aaad671c8deb065c4c933f272aa6b9ad10c
Antoine Martin <totaam@xpra.org>
2019-04-02 14:16:40 +0000

    support BGRA icon data (not implemented server side yet - it's difficult..)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22262 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/mixins/window_manager.py
+++ src/xpra/client/mixins/window_manager.py
@@ -638,9 +638,15 @@ class WindowClient(StubClientMixin):
                 self, width, height, coding, len(data), ICON_SHRINKAGE, ICON_OVERLAY)
         if coding=="default":
             img = self.overlay_image
-        elif coding == "premult_argb32":            #we usually cannot do in-place and this is not performance critical
-            from xpra.codecs.argb.argb import unpremultiply_argb    #@UnresolvedImport
-            data = unpremultiply_argb(data)
+        elif coding == "BGRA":
+            rowstride = width*4
+            img = Image.frombytes("RGBA", (width,height), memoryview_to_bytes(data), "raw", "BGRA", rowstride, 1)
+            has_alpha = True
+        elif coding in ("BGRA", "premult_argb32"):
+            if coding == "premult_argb32":
+                #we usually cannot do in-place and this is not performance critical
+                from xpra.codecs.argb.argb import unpremultiply_argb    #@UnresolvedImport
+                data = unpremultiply_argb(data)
             rowstride = width*4
             img = Image.frombytes("RGBA", (width,height), memoryview_to_bytes(data), "raw", "BGRA", rowstride, 1)
             has_alpha = True
