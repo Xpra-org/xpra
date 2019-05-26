d5baf0fa7800d4cc721a56580f7773173670a992
Antoine Martin <totaam@xpra.org>
2019-05-26 09:46:29 +0000

    this parameter is the datatype, not data length
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22789 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Client.js
+++ src/html5/js/Client.js
@@ -2920,7 +2920,7 @@ XpraClient.prototype.send_clipboard_token = function(data) {
 		return;
 	}
 	this.debug("keyboard", "sending clipboard token with data:", data);
-	var packet = ["clipboard-token", "CLIPBOARD", [], "UTF8_STRING", "UTF8_STRING", data.length, "bytes", data, false, true, true];
+	var packet = ["clipboard-token", "CLIPBOARD", [], "UTF8_STRING", "UTF8_STRING", 8, "bytes", data, false, true, true];
 	this.send(packet);
 }
 
