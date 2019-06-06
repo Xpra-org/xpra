98706ca8aed9faa043e320719b50fc56598bcedc
Antoine Martin <totaam@xpra.org>
2019-06-06 04:38:56 +0000

    #2289 add shim to make brotli decoding work with IE
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22861 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/lib/es6-shim.js
+++ src/html5/js/lib/es6-shim.js
@@ -174,3 +174,44 @@ if (!Array.from) {
 if (!Element.prototype.matches) {
 	Element.prototype.matches = Element.prototype.msMatchesSelector;
 }
+
+
+if (!Int32Array.__proto__.from) {
+    (function () {
+        Int32Array.__proto__.from = function (obj, func, thisObj) {
+
+            var typedArrayClass = Int32Array.__proto__;
+            if(typeof this !== 'function') {
+                throw new TypeError('# is not a constructor');
+            }
+            if (this.__proto__ !== typedArrayClass) {
+                throw new TypeError('this is not a typed array.');
+            }
+ 
+            func = func || function (elem) {
+                    return elem;
+                };
+
+            if (typeof func !== 'function') {
+                throw new TypeError('specified argument is not a function');
+            }
+
+            obj = Object(obj);
+            if (!obj['length']) {
+                return new this(0);
+            }
+            var copy_data = [];
+            for(var i = 0; i < obj.length; i++) {
+                copy_data.push(obj[i]);
+            }
+
+            copy_data = copy_data.map(func, thisObj);
+
+            var typed_array = new this(copy_data.length);
+            for(var i = 0; i < typed_array.length; i++) {
+                typed_array[i] = copy_data[i];
+            }
+            return typed_array;
+        }
+    })();
+}
