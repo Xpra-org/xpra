b9432624f0a19285ce70893093120084773878df
Antoine Martin <totaam@xpra.org>
2019-06-12 10:20:41 +0000

    #1978: generate screenshot by drawing all the windows onto a canvas
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22917 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Utilities.js
+++ src/html5/js/Utilities.js
@@ -423,6 +423,20 @@ var Utilities = {
 		return window.btoa(s);
 	},
 
+	convertDataURIToBinary : function (dataURI) {
+		var BASE64_MARKER = ';base64,';
+		var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+		var base64 = dataURI.substring(base64Index);
+		var raw = window.atob(base64);
+		var rawLength = raw.length;
+		var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+		for(i = 0; i < rawLength; i++) {
+			array[i] = raw.charCodeAt(i);
+		}
+		return array;
+	},
+
 	/**
 	 * XmlHttpRequest's getAllResponseHeaders() method returns a string of response
 	 * headers according to the format described here:
