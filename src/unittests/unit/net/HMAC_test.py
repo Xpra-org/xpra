2ec395c0fa1d409c82acdba1bfb26bad52558aa6
Antoine Martin <totaam@xpra.org>
2019-06-13 16:00:11 +0000

    #2329 use sha1 if md5 is not available
    git-svn-id: https://xpra.org/svn/Xpra/trunk@22935 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/net/HMAC_test.py
+++ src/unittests/unit/net/HMAC_test.py
@@ -13,9 +13,14 @@ class TestHMAC(unittest.TestCase):
     def test_hardcoded(self):
         password = b"71051d81d27745b59c1c56c6e9046c19697e452453e04aa5abbd52c8edc8c232"
         salt = b"99ea464f-7117-4e38-95b3-d3aa80e7b806"
-        hmac_hash = hmac.HMAC(password, salt, digestmod=hashlib.md5)
-        hd = hmac_hash.hexdigest()
-        ed = "dc26a074c9378b1b5735a27563320a26"
+        try:
+            hmac_hash = hmac.HMAC(password, salt, digestmod=hashlib.md5)
+            hd = hmac_hash.hexdigest()
+            ed = "dc26a074c9378b1b5735a27563320a26"
+        except ValueError:
+            hmac_hash = hmac.HMAC(password, salt, digestmod=hashlib.sha1)
+            hd = hmac_hash.hexdigest()
+            ed = "5529d27aef1b7420fb9d696f1c04eaad5dcc1515"
         assert hd == ed, "expected digest %s but got %s" % (ed, hd)
 
 def main():
