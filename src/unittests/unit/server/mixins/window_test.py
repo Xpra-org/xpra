865f02311164283293ffe971572706c43ec44051
Antoine Martin <totaam@xpra.org>
2019-07-10 19:39:34 +0000

    #1773: add window server test
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23134 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/server/mixins/window_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2018-2019 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import unittest
+
+from xpra.util import AdHocStruct
+from unit.server.mixins.servermixintest_util import ServerMixinTest
+
+
+class WebcamMixinTest(ServerMixinTest):
+
+    def test_mmap(self):
+        from xpra.server.mixins.window_server import WindowServer
+        opts = AdHocStruct()
+        opts.min_size = "10x10"
+        opts.max_size = "16384x8192"
+        def load_existing_windows():
+            pass
+        def _WindowServer():
+            ws = WindowServer()
+            ws.load_existing_windows = load_existing_windows
+            return ws
+        self._test_mixin_class(_WindowServer, opts)
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
