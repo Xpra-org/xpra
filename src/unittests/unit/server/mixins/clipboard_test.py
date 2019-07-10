4c2c52d61f765493a9623b2ecce60b65ecb3141f
Antoine Martin <totaam@xpra.org>
2019-07-10 19:14:22 +0000

    #1773: add clipboard test (missed from r23123)
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23125 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/server/mixins/clipboard_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2018 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+import unittest
+
+from xpra.util import AdHocStruct
+from xpra.os_util import POSIX, OSX
+from unit.server.mixins.servermixintest_util import ServerMixinTest
+
+
+class ClipboardMixinTest(ServerMixinTest):
+
+    def test_clipboard(self):
+        if os.environ.get("DISPLAY") and POSIX and not OSX and os.environ.get("GDK_BACKEND", "x11")=="x11":
+            from xpra.x11.gtk_x11.gdk_display_source import init_gdk_display_source
+            init_gdk_display_source()
+        from xpra.server.mixins.clipboard_server import ClipboardServer
+        from xpra.server.source.clipboard_connection import ClipboardConnection
+        opts = AdHocStruct()
+        opts.clipboard = "yes"
+        opts.clipboard_direction = "both"
+        opts.clipboard_filter_file = None
+        self._test_mixin_class(ClipboardServer, opts, {}, ClipboardConnection)
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
