0d23675f94115a5a87a63cbef29442e33995c649
Antoine Martin <totaam@xpra.org>
2019-07-10 19:36:57 +0000

    #1773: add webcam test
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23133 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/server/mixins/webcam_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2018-2019 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import unittest
+
+from xpra.util import AdHocStruct
+from unit.server.mixins.servermixintest_util import ServerMixinTest
+
+
+class WebcamMixinTest(ServerMixinTest):
+
+    def test_mmap(self):
+        from xpra.server.mixins.webcam_server import WebcamServer
+        opts = AdHocStruct()
+        opts.webcam = "yes"
+        self._test_mixin_class(WebcamServer, opts)
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
