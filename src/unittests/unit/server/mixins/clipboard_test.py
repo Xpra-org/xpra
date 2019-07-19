2d3a68653f6ffcd721058c1e93abf61d70810837
Antoine Martin <totaam@xpra.org>
2019-07-19 17:45:03 +0000

    don't rely on an existing X11 display to run this test
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23231 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/mixins/clipboard_test.py
+++ src/unittests/unit/server/mixins/clipboard_test.py
@@ -4,27 +4,28 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import os
 import unittest
 
 from xpra.util import AdHocStruct
 from xpra.os_util import POSIX, OSX
 from unit.server.mixins.servermixintest_util import ServerMixinTest
+from unit.process_test_util import DisplayContext
 
 
 class ClipboardMixinTest(ServerMixinTest):
 
     def test_clipboard(self):
-        if os.environ.get("DISPLAY") and POSIX and not OSX and os.environ.get("GDK_BACKEND", "x11")=="x11":
-            from xpra.x11.gtk_x11.gdk_display_source import init_gdk_display_source
-            init_gdk_display_source()
-        from xpra.server.mixins.clipboard_server import ClipboardServer
-        from xpra.server.source.clipboard_connection import ClipboardConnection
-        opts = AdHocStruct()
-        opts.clipboard = "yes"
-        opts.clipboard_direction = "both"
-        opts.clipboard_filter_file = None
-        self._test_mixin_class(ClipboardServer, opts, {}, ClipboardConnection)
+        with DisplayContext():
+            if POSIX and not OSX:
+                from xpra.x11.gtk_x11.gdk_display_source import init_gdk_display_source
+                init_gdk_display_source()
+            from xpra.server.mixins.clipboard_server import ClipboardServer
+            from xpra.server.source.clipboard_connection import ClipboardConnection
+            opts = AdHocStruct()
+            opts.clipboard = "yes"
+            opts.clipboard_direction = "both"
+            opts.clipboard_filter_file = None
+            self._test_mixin_class(ClipboardServer, opts, {}, ClipboardConnection)
 
 def main():
     unittest.main()
