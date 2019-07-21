93c9850d05d26cf0ee3fe55499993851370c34a1
Antoine Martin <totaam@xpra.org>
2019-07-21 08:18:08 +0000

    start a dbus server to test notification forwarding
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23253 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/mixins/notification_test.py
+++ src/unittests/unit/server/mixins/notification_test.py
@@ -4,22 +4,42 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import os
 import unittest
 
 from xpra.util import AdHocStruct
+from xpra.os_util import OSEnvContext
 from unit.server.mixins.servermixintest_util import ServerMixinTest
 
 
 class NotificationForwarderMixinTest(ServerMixinTest):
 
     def test_notification(self):
-        from xpra.server.mixins.notification_forwarder import NotificationForwarder
-        opts = AdHocStruct()
-        opts.notifications = "yes"
-        self._test_mixin_class(NotificationForwarder, opts)
-        self.verify_packet_error(("notification-close", 1, "test", "hello"))
-        self.verify_packet_error(("notification-action", 1))
-        self.handle_packet(("set-notify", False))
+        with OSEnvContext():
+            #remove any existing dbus environment so we don't pollute it:
+            for k in tuple(os.environ.keys()):
+                if k.startswith("DBUS"):
+                    del os.environ[k]
+            #start a dbus server:
+            from xpra.server.dbus.dbus_start import start_dbus
+            dbus_pid, dbus_env = start_dbus("dbus-launch --sh-syntax --close-stderr")
+            print("dbus_pid=%s" % (dbus_pid))
+            print("dbus_env=%s" % (dbus_env))
+            try:
+                if dbus_env:
+                    os.environ.update(dbus_env)
+
+                from xpra.server.mixins.notification_forwarder import NotificationForwarder
+                opts = AdHocStruct()
+                opts.notifications = "yes"
+                self._test_mixin_class(NotificationForwarder, opts)
+                self.verify_packet_error(("notification-close", 1, "test", "hello"))
+                self.verify_packet_error(("notification-action", 1))
+                self.handle_packet(("set-notify", False))
+            finally:
+                if dbus_pid:
+                    import signal
+                    os.kill(dbus_pid, signal.SIGINT)
 
 def main():
     unittest.main()
--- src/xpra/server/dbus/dbus_start.py
+++ src/xpra/server/dbus/dbus_start.py
@@ -18,10 +18,12 @@ log = Logger("dbus")
 
 def start_dbus(dbus_launch):
     if not dbus_launch or dbus_launch.lower() in FALSE_OPTIONS:
+        log("start_dbus(%s) disabled", dbus_launch)
         return 0, {}
     bus_address = os.environ.get("DBUS_SESSION_BUS_ADDRESS")
     log("dbus_launch=%s, current DBUS_SESSION_BUS_ADDRESS=%s", dbus_launch, bus_address)
     if bus_address:
+        log("start_dbus(%s) disabled, found an existing DBUS_SESSION_BUS_ADDRESS=%s", dbus_launch, bus_address)
         return 0, {}
     assert POSIX
     try:
