9e4e2bc9af2dad3437d1eea97dc9a4e7ffb1f032
Antoine Martin <totaam@xpra.org>
2019-07-29 09:43:28 +0000

    #2217: make it easier to see paint and resizing separately by making it possible to artificially delaying draw packets
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23336 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/mixins/window_manager.py
+++ src/xpra/client/mixins/window_manager.py
@@ -48,6 +48,7 @@ MOUSE_SHOW = envbool("XPRA_MOUSE_SHOW", True)
 
 PAINT_FAULT_RATE = envint("XPRA_PAINT_FAULT_INJECTION_RATE")
 PAINT_FAULT_TELL = envbool("XPRA_PAINT_FAULT_INJECTION_TELL", True)
+PAINT_DELAY = envint("XPRA_PAINT_DELAY", 0)
 
 WM_CLASS_CLOSEEXIT = os.environ.get("XPRA_WM_CLASS_CLOSEEXIT", "Xephyr").split(",")
 TITLE_CLOSEEXIT = os.environ.get("XPRA_TITLE_CLOSEEXIT", "Xnest").split(",")
@@ -1351,8 +1352,10 @@ class WindowClient(StubClientMixin):
     ######################################################################
     # painting windows:
     def _process_draw(self, packet):
-        #self.timeout_add(1000*1, self._draw_queue.put, packet)
-        self._draw_queue.put(packet)
+        if PAINT_DELAY>0:
+            self.timeout_add(PAINT_DELAY, self._draw_queue.put, packet)
+        else:
+            self._draw_queue.put(packet)
 
     def _process_eos(self, packet):
         self._draw_queue.put(packet)
