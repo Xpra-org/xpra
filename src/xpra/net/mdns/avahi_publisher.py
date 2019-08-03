5488bb43c1719ef9460185da229385fb67026ea3
Antoine Martin <totaam@xpra.org>
2019-08-03 09:06:53 +0000

    if we fail to add a service, don't try to update its txt record and cause another error
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23404 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/net/mdns/avahi_publisher.py
+++ src/xpra/net/mdns/avahi_publisher.py
@@ -88,7 +88,7 @@ class AvahiPublishers(object):
                             fqdn += ".local"
                         log("cannot find a fully qualified domain name for '%s', using: %s", host, fqdn)
                 except (OSError, IOError, IndexError):
-                    log("failed to get hostbyaddr", exc_info=True)
+                    log("failed to get hostbyaddr for '%s'", host, exc_info=True)
             self.publishers.append(AvahiPublisher(bus, service_name, port, service_type, domain="", host=fqdn, text=txt, interface=iface_index))
 
     def start(self):
