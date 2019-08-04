8a3cccbf0e8e404e9673637ef2f7ba7b83c52862
Antoine Martin <totaam@xpra.org>
2019-08-04 12:03:00 +0000

    self tests need to be able to remove encodings that failed
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23426 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/pillow/encoder.py
+++ src/xpra/codecs/pillow/encoder.py
@@ -35,7 +35,7 @@ def do_get_encodings():
         if stripped in Image.SAVE:
             encodings.append(encoding)
     log("do_get_encodings()=%s", encodings)
-    return tuple(encodings)
+    return encodings
 
 def get_encodings():
     return ENCODINGS
