c8f1e8f5e251d96784640f75a75ed3219945dffa
Antoine Martin <totaam@xpra.org>
2019-08-05 13:37:50 +0000

    r21977 + r21978 backport: fix build with newer versions of gcc
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@23448 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -2419,6 +2419,8 @@ libav_common = dec_avcodec2_ENABLED or csc_swscale_ENABLED
 toggle_packages(libav_common, "xpra.codecs.libav_common")
 if libav_common:
     avutil_pkgconfig = pkgconfig("avutil")
+    if get_gcc_version()>=[9, 0]:
+        add_to_keywords(avutil_pkgconfig, 'extra_compile_args', "-Wno-error=attributes")
     cython_add(Extension("xpra.codecs.libav_common.av_log",
                 ["xpra/codecs/libav_common/av_log.pyx"],
                 **avutil_pkgconfig))
@@ -2427,6 +2429,8 @@ if libav_common:
 toggle_packages(dec_avcodec2_ENABLED, "xpra.codecs.dec_avcodec2")
 if dec_avcodec2_ENABLED:
     avcodec2_pkgconfig = pkgconfig("avcodec", "avutil", "avformat")
+    if get_gcc_version()>=[9, 0]:
+        add_to_keywords(avcodec2_pkgconfig, 'extra_compile_args', "-Wno-error=attributes")
     cython_add(Extension("xpra.codecs.dec_avcodec2.decoder",
                 ["xpra/codecs/dec_avcodec2/decoder.pyx", "xpra/codecs/dec_avcodec2/register_compat.c"]+membuffers_c,
                 **avcodec2_pkgconfig))
@@ -2444,6 +2448,8 @@ if csc_libyuv_ENABLED:
 toggle_packages(csc_swscale_ENABLED, "xpra.codecs.csc_swscale")
 if csc_swscale_ENABLED:
     swscale_pkgconfig = pkgconfig("swscale", "avutil")
+    if get_gcc_version()>=[9, 0]:
+        add_to_keywords(swscale_pkgconfig, 'extra_compile_args', "-Wno-error=attributes")
     cython_add(Extension("xpra.codecs.csc_swscale.colorspace_converter",
                 ["xpra/codecs/csc_swscale/colorspace_converter.pyx"]+membuffers_c,
                 **swscale_pkgconfig))
