2291a53065ae49f5225849584f9223a54ac031a5
Antoine Martin <totaam@xpra.org>
2019-08-07 02:27:04 +0000

    #2380 fix stuck keys on macos when swap_keys is disabled
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23461 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Client.js
+++ src/html5/js/Client.js
@@ -782,7 +782,7 @@ XpraClient.prototype._keyb_process = function(pressed, event) {
 		var me = this;
 		setTimeout(function () {
 			me.send(packet);
-			if (pressed && me.swap_keys && raw_modifiers.includes("meta") && ostr!="meta") {
+			if (pressed && Utilities.isMacOS() && raw_modifiers.includes("meta") && ostr!="meta") {
 				//macos will swallow the key release event if the meta modifier is pressed,
 				//so simulate one immediately:
 				packet = ["key-action", me.topwindow, keyname, false, modifiers, keyval, str, keycode, group];
