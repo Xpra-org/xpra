eed4a02b8d156c89af426d0271facf846f2d18ad
Antoine Martin <totaam@xpra.org>
2019-08-27 10:22:34 +0000

    #1527 use ssl cert if we find it
    git-svn-id: https://xpra.org/svn/Xpra/trunk@23605 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/win32_proxy_service.py
+++ src/xpra/scripts/win32_proxy_service.py
@@ -3,6 +3,19 @@
 import os
 import sys
 
+
+def get_commonappdata_dir():
+    CSIDL_COMMON_APPDATA = 35
+    try:
+        import ctypes
+        buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+        shell32 = ctypes.WinDLL("shell32", use_last_error=True)
+        SHGetFolderPath = shell32.SHGetFolderPathW
+        SHGetFolderPath(0, CSIDL_COMMON_APPDATA, None, 0, buf)
+        return buf.value
+    except:
+        return None
+
 def main():
     os.environ["XPRA_REDIRECT_OUTPUT"] = "1"
     #os.environ["XPRA_LOG_FILENAME"] = "E:\\Proxy.log"
@@ -19,8 +32,12 @@ def main():
         "--bind-tcp=0.0.0.0:14500",
         "--tcp-auth=sys",
         "-d", "win32,proxy",
-        #"--ssl-cert="{commonappdata}\Xpra\ssl-cert.pem"";
         ] + sys.argv[1:]
+    commonappdata = get_commonappdata_dir()
+    if commonappdata:
+        ssl_cert = os.path.join(commonappdata, "Xpra", "ssl-cert.pem")
+        if os.path.exists(ssl_cert):
+            args.append("--ssl-cert=%s" % ssl_cert)
     sys.exit(main(sys.argv[0], args))
 
 if __name__ == "__main__":
