65bc66e66b8c1d4cac71259f8aa644eae1ef8d7b
Antoine Martin <totaam@xpra.org>
2019-09-22 07:31:41 +0000

    r23880 backport: tabs vs spaces
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@23883 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Client.js
+++ src/html5/js/Client.js
@@ -2411,14 +2411,14 @@ XpraClient.prototype._process_notify_show = function(packet, ctx) {
 					});
 				}
 			}*/
-		    var notification = new Notification(summary, { body: body, icon: icon_url });
-		    notification.onclose = function() {
-		    	var reason = 2;	//closed by the user - best guess...
+			var notification = new Notification(summary, { body: body, icon: icon_url });
+			notification.onclose = function() {
+				var reason = 2;	//closed by the user - best guess...
 				ctx.send(["notification-close", nid, reason, ""]);
-		    };
-		    notification.onclick = function() {
-		    	ctx.log("user clicked on notification", nid);
-		    }
+			};
+			notification.onclick = function() {
+				ctx.log("user clicked on notification", nid);
+			}
 		}
 		//we have notification support in the browser
 		if (Notification.permission === "granted") {
@@ -2430,7 +2430,7 @@ XpraClient.prototype._process_notify_show = function(packet, ctx) {
 				if (permission === "granted") {
 					notify();
 				}
-		    });
+			});
 			return;
 		}
 	}
