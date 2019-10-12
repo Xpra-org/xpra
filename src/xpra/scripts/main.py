1ab10c822f25c881f39ea9adbb321b2cf9b6ebfc
Antoine Martin <totaam@xpra.org>
2019-10-12 15:24:11 +0000

    unused imports
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24113 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/main.py
+++ src/xpra/scripts/main.py
@@ -18,8 +18,8 @@ import shlex
 import traceback
 
 from xpra.platform.dotxpra import DotXpra
-from xpra.util import csv, envbool, envint, repr_ellipsized, nonl, pver, DEFAULT_PORT, DEFAULT_PORTS
-from xpra.exit_codes import EXIT_SSL_FAILURE, EXIT_STR, EXIT_UNSUPPORTED
+from xpra.util import csv, envbool, envint, nonl, pver, DEFAULT_PORT, DEFAULT_PORTS
+from xpra.exit_codes import EXIT_STR, EXIT_UNSUPPORTED
 from xpra.os_util import (
     get_util_logger, getuid, getgid,
     monotonic_time, setsid, bytestostr, use_tty,
