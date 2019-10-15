40d3a9d8715dd87509044147dbc762ab3b995760
Antoine Martin <totaam@xpra.org>
2019-10-15 16:05:58 +0000

    avoid warnings on win32: we have to call gi.require_version before importing anything
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24143 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/gtk_server_base.py
+++ src/xpra/server/gtk_server_base.py
@@ -8,6 +8,10 @@
 
 import sys
 import os.path
+import gi
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Pango', '1.0')
 from gi.repository import GLib, Gdk, Gtk
 
 from xpra.util import flatten_dict, envbool
