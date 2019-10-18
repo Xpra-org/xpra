cc398e19c240d5b3d1b255e8b0cfe0e18cfa5abf
Antoine Martin <totaam@xpra.org>
2019-10-18 12:35:14 +0000

    undo r24175 and only remove those files from the PKG
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24181 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup.py
+++ src/setup.py
@@ -1453,9 +1453,7 @@ else:
         man_path = "share/man"
         if OPENBSD:
             man_path = "man"
-        if not OSX:
-            #breaks installation with MacOS Catalina (see ticket #2453)
-            add_data_files("%s/man1" % man_path,  ["man/xpra.1", "man/xpra_launcher.1"])
+        add_data_files("%s/man1" % man_path,  ["man/xpra.1", "man/xpra_launcher.1"])
         add_data_files("share/applications",  glob.glob("xdg/*.desktop"))
         add_data_files("share/mime/packages", ["xdg/application-x-xpraconfig.xml"])
         add_data_files("share/icons",         ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
@@ -1505,8 +1503,7 @@ else:
                 if chmod:
                     os.chmod(dst_file, chmod)
 
-            if printing_ENABLED and POSIX and not OSX:
-                #breaks installation with MacOS Catalina (see ticket #2453)
+            if printing_ENABLED and POSIX:
                 #install "/usr/lib/cups/backend" with 0700 permissions:
                 copytodir("cups/xpraforwarder", "lib/cups/backend", chmod=0o700)
 
