2111a5638eef84eeba6a932824063cf9fd01bdaa
Antoine Martin <totaam@xpra.org>
2019-10-23 12:20:28 +0000

    #2450 reject any target we never claimed to support
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24248 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/clipboard.py
+++ src/xpra/platform/win32/clipboard.py
@@ -187,9 +187,15 @@ class Win32ClipboardProxy(ClipboardProxyCore):
         self.send_clipboard_token_handler(self)
 
     def get_contents(self, target, got_contents):
+        log("get_contents%s", (target, got_contents))
         if target=="TARGETS":
             #we only support text at the moment:
             got_contents("ATOM", 32, ["text/plain", "text/plain;charset=utf-8", "UTF8_STRING"])
+            return
+        if target not in ("text/plain", "text/plain;charset=utf-8", "UTF8_STRING"):
+            #we don't know how to handle this target,
+            #return an empty response:
+            got_contents(target, 8, b"")
             return
         def got_text(text):
             log("got_text(%s)", repr_ellipsized(bytestostr(text)))
