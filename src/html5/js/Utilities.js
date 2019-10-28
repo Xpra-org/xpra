ad4139e89c784c9dbb6ac77044c4c8ff6cf56b49
Antoine Martin <totaam@xpra.org>
2019-10-28 15:02:35 +0000

    #2237 add site wide default settings
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24291 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Utilities.js
+++ src/html5/js/Utilities.js
@@ -437,6 +437,37 @@ var Utilities = {
 		return array;
 	},
 
+
+	parseINIString : function (data) {
+	    var regex = {
+	        section: /^\s*\[\s*([^\]]*)\s*\]\s*$/,
+	        param: /^\s*([^=]+?)\s*=\s*(.*?)\s*$/,
+	        comment: /^\s*;.*$/
+	    };
+	    var value = {};
+	    var lines = data.split(/[\r\n]+/);
+	    var section = null;
+	    lines.forEach(function(line){
+	        if(regex.comment.test(line)){
+	            return;
+	        }else if(regex.param.test(line)){
+	            var match = line.match(regex.param);
+	            if(section){
+	                value[section][match[1]] = match[2];
+	            }else{
+	                value[match[1]] = match[2];
+	            }
+	        }else if(regex.section.test(line)){
+	            var match = line.match(regex.section);
+	            value[match[1]] = {};
+	            section = match[1];
+	        }else if(line.length == 0 && section){
+	            section = null;
+	        };
+	    });
+	    return value;
+	},
+
 	/**
 	 * XmlHttpRequest's getAllResponseHeaders() method returns a string of response
 	 * headers according to the format described here:
