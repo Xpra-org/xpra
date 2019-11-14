6fda203f701a6f03e385a49d308975a3c3220e6b
Antoine Martin <totaam@xpra.org>
2019-11-14 04:55:35 +0000

    r24426 + r24428 backport: only enable video by default on 64-bit browsers, log 64-bit info
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@24429 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/html5/js/Utilities.js
+++ src/html5/js/Utilities.js
@@ -259,6 +259,26 @@ var Utilities = {
 		return navigator.userAgent.includes("MSIE") || navigator.userAgent.includes("Trident/");
 	},
 
+	is_64bit : function() {
+		var _to_check = [] ;
+		if (window.navigator.cpuClass)
+			_to_check.push((window.navigator.cpuClass + "").toLowerCase());
+		if (window.navigator.platform)
+			_to_check.push((window.navigator.platform + "").toLowerCase());
+		if (navigator.userAgent)
+			_to_check.push((navigator.userAgent + "").toLowerCase());
+		var _64bits_signatures = ["x86_64", "x86-64", "Win64", "x64;", "amd64", "AMD64", "WOW64", "x64_64", "ia64", "sparc64", "ppc64", "IRIX64"];
+		var _i, _c;
+		for (_c=0; _c<_to_check.length; _c++) {
+			for (_i=0 ; _i<_64bits_signatures.length; _i++) {
+				if (_to_check[_c].indexOf(_64bits_signatures[_i].toLowerCase())!=-1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	},
+
 	getSimpleUserAgentString : function() {
 		if (Utilities.isFirefox()) {
 			return "Firefox";
