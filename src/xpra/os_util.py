d742a333839b62d5db35780129b37a06c8fa7f50
Antoine Martin <totaam@xpra.org>
2019-12-01 11:58:15 +0000

    doh, fixup r24552 and remove value used for testing
    git-svn-id: https://xpra.org/svn/Xpra/trunk@24553 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/os_util.py
+++ src/xpra/os_util.py
@@ -305,7 +305,7 @@ def get_linux_distribution():
         #linux_distribution is deprecated in Python 3.5 and it causes warnings,
         #so use our own code first:
         import subprocess
-        cmd = ["xlsb_release", "-a"]
+        cmd = ["lsb_release", "-a"]
         try:
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out = p.communicate()[0]
