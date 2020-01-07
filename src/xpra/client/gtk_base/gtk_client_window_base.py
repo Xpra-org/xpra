e91513267b0c9ed4c25b972a8730905925bf4f7c
Antoine Martin <totaam@xpra.org>
2020-01-07 18:21:16 +0000

    fixup backport: use gtk version agnostic compat alias
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@24928 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/gtk_client_window_base.py
+++ src/xpra/client/gtk_base/gtk_client_window_base.py
@@ -818,7 +818,7 @@ class GTKClientWindowBase(ClientWindowBase, gtk.Window):
             #if we're changing the maximized state
             state_updates["maximized"] = bool(event.new_window_state & self.WINDOW_STATE_MAXIMIZED)
         if event.changed_mask & self.WINDOW_STATE_FOCUSED:
-            state_updates["focused"] = bool(event.new_window_state & Gdk.WindowState.FOCUSED)
+            state_updates["focused"] = bool(event.new_window_state & self.WINDOW_STATE_FOCUSED)
         self.update_window_state(state_updates)
 
     def update_window_state(self, state_updates):
