93b527942f67d376ce86a1a7bdd4b9d9e6a1945c
Antoine Martin <totaam@xpra.org>
2020-01-13 13:58:49 +0000

    r24944 backport: endpoint value may not be a string, so we have to convert it
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@24948 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/mixins/tray.py
+++ src/xpra/client/mixins/tray.py
@@ -135,6 +135,6 @@ class TrayClient(StubClientMixin):
                 t.append(cinfo.get("endpoint", bytestostr(conn.target)))
         if not t:
             t.insert(0, u"Xpra")
-        v = u"\n".join(t)
+        v = u"\n".join(str(x) for x in t)
         log("get_tray_title()=%s (items=%s)", nonl(v), tuple(strtobytes(x) for x in t))
         return v
