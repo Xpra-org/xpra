6b7a0e245de1bedd9bce5a1f210c8050607a7a43
Antoine Martin <totaam@xpra.org>
2020-02-19 04:21:38 +0000

    log subprocess exception error
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25289 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/run.py
+++ src/unittests/unit/run.py
@@ -48,8 +48,8 @@ def main():
         cmd = run_cmd + [p]
         try:
             proc = subprocess.Popen(cmd)
-        except OSError:
-            write("failed to execute %s" % p)
+        except OSError as e:
+            write("failed to execute %s: %s" % (p, e))
             return 1
         v = proc.wait()
         if v!=0:
