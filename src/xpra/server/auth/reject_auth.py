2b0aee8f7e6a51a14657720ccbad3fcd7cfd13a0
Antoine Martin <totaam@xpra.org>
2020-02-26 16:23:24 +0000

    this function is no longer used or needed
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25354 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/auth/reject_auth.py
+++ src/xpra/server/auth/reject_auth.py
@@ -1,13 +1,10 @@
 # This file is part of Xpra.
-# Copyright (C) 2014-2018 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2014-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 
 from xpra.net.digest import get_salt, choose_digest
-
-def init(_opts):
-    pass
 
 
 class Authenticator:
