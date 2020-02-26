f604ef20ec4c98ce7be198314d1193fd1d6d74cd
Antoine Martin <totaam@xpra.org>
2020-02-26 12:29:47 +0000

    full test coverage of digests
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25342 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/net/digest_test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of Xpra.
+# Copyright (C) 2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+#pylint: disable=line-too-long
+
+import unittest
+
+from xpra.net.digest import (
+    get_digests, get_digest_module,
+    choose_digest, gendigest, verify_digest,
+    get_salt,
+    )
+
+class TestDigest(unittest.TestCase):
+
+    def test_invalid_digest(self):
+        for invalid_digest in (None, "foo"):
+            assert get_digest_module(invalid_digest) is None
+            assert gendigest(invalid_digest, "bar", "0"*16) is None
+
+    def test_all_digests(self):
+        for digest in get_digests():
+            if digest.startswith("hmac"):
+                m = get_digest_module(digest)
+                assert m is not None, "digest module not found for '%s'" % digest
+            salt = get_salt(32)
+            password = "secret"
+            d = gendigest(digest, password, salt)
+            assert d is not None
+            verify_digest(digest, password, salt, d)
+
+    def test_choose_digest(self):
+        for h in ("hmac+sha512", "hmac+sha384", "hmac+sha256", "hmac+sha224", "hmac+sha1", "hmac+md5",
+                  "xor", "des"):
+            assert choose_digest((h,))==h
+            assert choose_digest((h, "hmac+sha512"))=="hmac+sha512"
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
