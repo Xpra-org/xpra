849385577fc7b3962b769f72f5b9078960ef6ce8
Antoine Martin <totaam@xpra.org>
2020-02-27 13:43:25 +0000

    test displayfd
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25422 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/platform/displayfd_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+import tempfile
+import unittest
+
+from xpra.os_util import load_binary_file
+from xpra.platform.displayfd import write_displayfd, read_displayfd
+
+class PlatformInfoTest(unittest.TestCase):
+
+    def test_write(self):
+        temp = tempfile.NamedTemporaryFile(prefix="xpra.", suffix=".displayfd-test")
+        fd = temp.file.fileno()
+        display = ":999"
+        write_displayfd(fd, display)
+        #read what was written:
+        readback = load_binary_file(temp.name).decode().rstrip("\n")
+        assert readback==display, "expected %s but got %s" % (display, readback)
+        #file descriptor is already closed,
+        #so this throws an exception
+        try:
+            temp.close()
+        except OSError:
+            pass
+
+    def test_read(self):
+        display = ":999"
+        f = tempfile.NamedTemporaryFile(prefix="xpra.", suffix=".displayfd-test", delete=False)
+        f.write((display+"\n").encode())
+        f.close()
+        fd = os.open(f.name, os.O_RDONLY)
+        d = read_displayfd(fd).decode().rstrip("\n")
+        assert d==display, "expected %s but got %s" % (display, d)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
