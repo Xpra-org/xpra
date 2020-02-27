96883f9c603c199d944a0960324d7abe2173dff8
Antoine Martin <totaam@xpra.org>
2020-02-27 07:44:47 +0000

    test failures to close mmap temp file
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25392 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/client/mixins/mmap_test.py
+++ src/unittests/unit/client/mixins/mmap_test.py
@@ -15,6 +15,9 @@ from unit.client.mixins.clientmixintest_util import ClientMixinTest
 class MixinsTest(ClientMixinTest):
 
 	def test_mmap(self):
+		class badfile:
+			def close(self):
+				raise Exception("test close failure handling")
 		for mmap in ("off", "on",
 					"/tmp/xpra-mmap-test-file-%i" % os.getpid(),
 					"/tmp/xpra-fail-mmap-test-file-%i" % os.getpid(),
@@ -28,6 +31,11 @@ class MixinsTest(ClientMixinTest):
 			fail = bool(m.mmap_filename) and m.mmap_filename.find("fail")>=0
 			assert m.mmap_enabled == (mmap!="off" and not fail)
 			assert len(self.exit_codes)==int(fail)
+			m.cleanup()
+			#no-op:
+			m.cleanup()
+			m.mmap_tempfile = badfile()
+			m.cleanup()
 
 	def make_caps(self, caps=None):
 		d = super().make_caps(caps)
