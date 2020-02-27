0dfa85d8ebdbbee7cf187d89a03fd760e7c3acfd
Antoine Martin <totaam@xpra.org>
2020-02-27 03:27:05 +0000

    remove unused method: we can't instantiate this class
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25366 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/auth/fail_auth.py
+++ src/xpra/server/auth/fail_auth.py
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2014-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2014-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -7,6 +7,3 @@
 class Authenticator:
     def __init__(self, username="", **kwargs):
         raise Exception("failing")
-
-    def __repr__(self):
-        return "fail"
