cf78d4ba160c22e378f6f7ac93e280d3c8d9ad34
Antoine Martin <totaam@xpra.org>
2020-02-27 08:00:55 +0000

    packaging workaround no longer needed?
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25396 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/__init__.py
+++ src/xpra/platform/__init__.py
@@ -1,16 +1,11 @@
 # This file is part of Xpra.
 # Copyright (C) 2010 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2011-2017 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2011-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 import os
 import sys
-
-try:
-    import encodings
-except ImportError:
-    pass
 
 
 _init_done = False
