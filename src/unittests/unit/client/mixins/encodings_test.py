4b4d218ca3e218034cfa8b2821009ffdb4af4a1f
Antoine Martin <totaam@xpra.org>
2020-02-27 04:31:46 +0000

    better test coverage
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25368 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/client/mixins/encodings_test.py
+++ src/unittests/unit/client/mixins/encodings_test.py
@@ -25,12 +25,52 @@ class DisplayClientTest(ClientMixinTest):
 		opts.video_decoders = []
 		opts.csc_modules = []
 		opts.video_encoders = []
-		self._test_mixin_class(Encodings, opts, {
+		m = self._test_mixin_class(Encodings, opts, {
 			"encodings" : ["rgb"],
 			"encodings.core" : ["rgb32", "rgb24", "png"],
 			"encodings.problematic" : [],
 			"encoding" : ""
 			})
+		m.set_encoding("auto")
+		def f(fn, err):
+			try:
+				fn()
+			except Exception:
+				pass
+			else:
+				raise Exception(err)
+		def set_invalid_encoding():
+			m.set_encoding("invalid")
+		f(set_invalid_encoding, "should not be able to set encoding 'invalid'")
+		#quality:
+		for q in (-1, 0, 1, 99, 100):
+			m.quality = q
+			m.send_quality()
+		for q in (-2, 101):
+			m.quality = q
+			f(m.send_quality, "should not be able to send invalid quality %i" % q)
+		#min-quality:
+		for q in (-1, 0, 1, 99, 100):
+			m.min_quality = q
+			m.send_min_quality()
+		for q in (-2, 101):
+			m.min_quality = q
+			f(m.send_min_quality, "should not be able to send invalid min-quality %i" % q)
+		#speed:
+		for s in (-1, 0, 1, 99, 100):
+			m.speed = s
+			m.send_speed()
+		for s in (-2, 101):
+			m.speed = s
+			f(m.send_speed, "should not be able to send invalid speed %i" % s)
+		#min-speed:
+		for s in (-1, 0, 1, 99, 100):
+			m.min_speed = s
+			m.send_min_speed()
+		for s in (-2, 101):
+			m.min_speed = s
+			f(m.send_min_speed, "should not be able to send invalid min-speed %i" % s)
+
 
 def main():
 	unittest.main()
