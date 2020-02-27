0b46583e8fd358a741a541c3a72ab153415708da
Antoine Martin <totaam@xpra.org>
2020-02-27 07:07:38 +0000

    test no-op stop_worker and ensure error_item is triggered
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25383 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/background_worker_test.py
+++ src/unittests/unit/server/background_worker_test.py
@@ -15,16 +15,21 @@ class BackgroundWorkerTest(unittest.TestCase):
     def test_run(self):
         assert get_worker(False) is None
         w = get_worker()
-        def slow_item():
-            time.sleep(1)
-        #trigger the warning with more than 10 items:
-        for _ in range(12):
-            w.add(slow_item)
+        assert repr(w)
         def error_item():
             raise Exception("work item test error")
         add_work_item(error_item)
+        def slow_item():
+            time.sleep(1)
+        #trigger the warning with more than 10 items:
+        for _ in range(11):
+            w.add(slow_item)
         stop_worker()
         stop_worker(True)
+        #no-op:
+        stop_worker(True)
+        #let the worker print its messages:
+        time.sleep(1)
 
 def main():
     unittest.main()
