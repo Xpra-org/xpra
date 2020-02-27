4582855cbec31bbe2e1edd70efe2808a7e57683f
Antoine Martin <totaam@xpra.org>
2020-02-27 08:27:20 +0000

    this must be overriden, no need to have test coverage for it
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25405 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/shadow_server.py
+++ src/xpra/platform/shadow_server.py
@@ -1,12 +1,11 @@
 # This file is part of Xpra.
-# Copyright (C) 2010 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2011-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2011-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 from xpra.platform import platform_import
 
-def ShadowServer():
+def ShadowServer(): # pragma: no cover
     raise NotImplementedError()
 
 platform_import(globals(), "shadow_server", True, "ShadowServer")
