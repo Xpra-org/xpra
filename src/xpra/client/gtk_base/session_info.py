faadab1c3c7ee6e6919d358f3ffa95b7f4445c74
Antoine Martin <totaam@xpra.org>
2020-03-06 03:06:36 +0000

    #2624 show 'session identifying' elements in session info window title
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25520 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/session_info.py
+++ src/xpra/client/gtk_base/session_info.py
@@ -127,11 +127,7 @@ class SessionInfo(Gtk.Window):
         self.last_populate_statistics = 0
         self.is_closed = False
         self.get_pixbuf = get_pixbuf
-        if not self.session_name or self.session_name=="Xpra":
-            title = "Session Info"
-        else:
-            title = "%s: Session Info" % self.session_name
-        self.set_title(title)
+        self.set_title(self.get_window_title())
         self.set_destroy_with_parent(True)
         self.set_resizable(True)
         self.set_decorated(True)
@@ -475,6 +471,21 @@ class SessionInfo(Gtk.Window):
             GLib.timeout_add(100, self.populate_sound_stats)
         add_close_accel(self, self.destroy)
 
+
+    def get_window_title(self):
+        t = ["Session Info"]
+        c = self.client
+        if c:
+            if c.session_name or c.server_session_name:
+                t.append(c.session_name or c.server_session_name)
+            p = c._protocol
+            if p:
+                conn = getattr(p, "_conn", None)
+                if conn:
+                    cinfo = conn.get_info()
+                    t.append(cinfo.get("endpoint", bytestostr(conn.target)))
+        v = " - ".join(str(x) for x in t)
+        return v
 
     def table_tab(self, icon_filename, title, populate_cb):
         tb = TableBuilder()
