6ab773171ff09b0773c671fd4c1b9d717151e6ad
Antoine Martin <totaam@xpra.org>
2020-03-09 02:54:52 +0000

    better coverage
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25557 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/video_scoring_test.py
+++ src/unittests/unit/server/video_scoring_test.py
@@ -7,18 +7,37 @@
 import unittest
 
 from xpra.util import AdHocStruct
-from xpra.server.window.video_scoring import get_quality_score
+from xpra.server.window.video_scoring import get_quality_score, get_speed_score
 
 
 class TestVideoScoring(unittest.TestCase):
 
-    def test_score(self):
+    def test_quality_score(self):
         csc_spec = AdHocStruct()
-        csc_spec.quality = 10
+        csc_spec.quality = 50
         encoder_spec = AdHocStruct()
-        encoder_spec.quality = 10
+        encoder_spec.quality = 50
+        encoder_spec.has_lossless_mode = False
+        s1 = get_quality_score("YUV420P", csc_spec, encoder_spec, (1, 1))
+        s2 = get_quality_score("BGRA", csc_spec, encoder_spec, (1, 1))
+        assert s2>s1
+        s3 = get_quality_score("YUV420P", csc_spec, encoder_spec, (1, 1), min_quality=50)
+        assert s3<s1
+        s4 = get_quality_score("YUV420P", csc_spec, encoder_spec, (2, 2))
+        assert s4>s2
+
+    def test_speed_score(self):
+        csc_spec = AdHocStruct()
+        csc_spec.speed = 50
+        encoder_spec = AdHocStruct()
+        encoder_spec.speed = 50
         encoder_spec.has_lossless_mode = True
-        get_quality_score("YUV420P", csc_spec, encoder_spec, (1, 1))
+        s1 = get_speed_score("YUV420P", csc_spec, encoder_spec, (1, 1))
+        s2 = get_speed_score("YUV420P", csc_spec, encoder_spec, (2, 2))
+        assert s2>s1
+        s3 = get_speed_score("YUV420P", csc_spec, encoder_spec, (1, 1), min_speed=60)
+        assert s3<s2
+
 
 def main():
     unittest.main()
