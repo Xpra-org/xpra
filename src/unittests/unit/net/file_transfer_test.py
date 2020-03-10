9d792367eb5b683248bfa751a04b4d6c68aaf48d
Antoine Martin <totaam@xpra.org>
2020-03-10 04:17:50 +0000

    better coverage: add file transfer test
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25585 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/net/file_transfer_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+import unittest
+
+from xpra.util import typedict
+from xpra.net.file_transfer import (
+    basename, safe_open_download_file,
+    FileTransferAttributes, FileTransferHandler,
+    )
+
+
+class TestVersionUtilModule(unittest.TestCase):
+
+    def test_basename(self):
+        assert basename("hello")=="hello"
+        assert basename("/path/to/foo")=="foo"
+        assert basename("\\other\\path\\bar")=="bar"
+
+
+    def test_safe_open(self):
+        filename, fd = safe_open_download_file("hello", "application/pdf")
+        try:
+            dupe_filename, dupe_fd = safe_open_download_file("hello", "application/pdf")
+            assert dupe_filename!=filename
+            try:
+                os.close(dupe_fd)
+            finally:
+                os.unlink(dupe_filename)
+            os.close(fd)
+        finally:
+            os.unlink(filename)
+
+
+    def test_file_transfer_attributes(self):
+        fta = FileTransferAttributes()
+        assert fta.get_file_transfer_features()
+        assert fta.get_info()
+
+    def test_file_transfer_handler(self):
+        fth = FileTransferHandler()
+        fth.init_attributes()
+        assert fth.get_open_env()
+        caps = typedict()
+        fth.parse_file_transfer_caps(caps)
+        assert fth.get_info()
+        fth.check_digest("foo", "000", "000", "xor")
+        try:
+            fth.check_digest("foo", "000", "001", "sha1")
+        except Exception:
+            pass
+        else:
+            raise Exception("digest mismatch should trigger an exception!")
+        fth.cleanup()
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
