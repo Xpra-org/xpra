a82866b8a97eb2c56909fe5219163e17f11a9301
Antoine Martin <totaam@xpra.org>
2020-03-22 17:40:51 +0000

    make it possible to run this script without specifying PYTHON, gtk-mac-bundler needs it defined so export it there
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25730 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- osx/make-app.sh
+++ osx/make-app.sh
@@ -141,7 +141,7 @@ echo
 echo "*******************************************************************************"
 echo "calling 'gtk-mac-bundler Xpra.bundle' in `pwd`"
 #we have to make sure we use python2 here (not ported yet):
-python2 ~/.local/bin/gtk-mac-bundler Xpra.bundle
+PYTHON=${PYTHON} python2 ~/.local/bin/gtk-mac-bundler Xpra.bundle
 if [ "$?" != "0" ]; then
 	echo "ERROR: gtk-mac-bundler failed"
 	exit 1
