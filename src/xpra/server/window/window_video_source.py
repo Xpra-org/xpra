265317e4a5179cb9fe7a098dd16c146a509eddb7
Antoine Martin <totaam@xpra.org>
2020-03-28 02:44:02 +0000

    r24278 backport: fix lost video screen updates
    git-svn-id: https://xpra.org/svn/Xpra/tags/v1.0.x@25805 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/window/window_video_source.py
+++ src/xpra/server/window/window_video_source.py
@@ -816,6 +816,7 @@ class WindowVideoSource(WindowSource):
 
     def cancel_encode_from_queue(self):
         #free all items in the encode queue:
+        self.encode_from_queue_due = 0
         eqt = self.encode_from_queue_timer
         avsynclog("cancel_encode_from_queue() timer=%s for wid=%i", eqt, self.wid)
         if eqt:
@@ -838,10 +839,10 @@ class WindowVideoSource(WindowSource):
         #must be called from the UI thread for synchronization
         #we ensure that the timer will fire no later than av_delay
         #re-scheduling it if it was due later than that
-        due = time.time()+av_delay
+        due = time.time()+av_delay/1000.0
         if self.encode_from_queue_due==0 or due<self.encode_from_queue_due:
-            self.encode_from_queue_due = due
             self.cancel_encode_from_queue()
+            self.encode_from_queue_due = due
             def timer_encode_from_queue():
                 self.encode_from_queue_timer = None
                 self.encode_from_queue_due = 0
