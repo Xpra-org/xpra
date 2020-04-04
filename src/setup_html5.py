e8bceb7eccc9a3c96963c06337fb90e2e3025433
Antoine Martin <totaam@xpra.org>
2020-04-04 11:19:49 +0000

    #2705 fix input for the minifier using regexp substitutions
    git-svn-id: https://xpra.org/svn/Xpra/trunk@25965 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/setup_html5.py
+++ src/setup_html5.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-
 # This file is part of Xpra.
-# Copyright (C) 2017-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2017-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file LICENSE for details.
 
+import re
 import sys
+import shutil
 import os.path
+import tempfile
 
 
 def glob_recurse(srcdir):
@@ -83,64 +85,92 @@ def install_html5(install_dir="www", minifier="uglifyjs", gzip=True, brotli=True
     for k,files in glob_recurse("html5").items():
         if k!="":
             k = os.sep+k
-        for f in files:
-            src = os.path.join(os.getcwd(), f)
-            parts = f.split(os.path.sep)
+        for fname in files:
+            src = os.path.join(os.getcwd(), fname)
+            parts = fname.split(os.path.sep)
             if parts[0]=="html5":
-                f = os.path.join(*parts[1:])
+                fname = os.path.join(*parts[1:])
             if install_dir==".":
                 install_dir = os.getcwd()
-            dst = os.path.join(install_dir, f)
+            dst = os.path.join(install_dir, fname)
             if os.path.exists(dst):
                 os.unlink(dst)
             #try to find an existing installed library and symlink it:
-            symlink_options = symlinks.get(os.path.basename(f), [])
+            symlink_options = symlinks.get(os.path.basename(fname), [])
             if install_symlink(symlink_options, dst):
                 #we've created a symlink, skip minification and compression
                 continue
             ddir = os.path.split(dst)[0]
             if ddir and not os.path.exists(ddir):
                 os.makedirs(ddir, 0o755)
-            ftype = os.path.splitext(f)[1].lstrip(".")
-            if minifier and ftype=="js":
+            ftype = os.path.splitext(fname)[1].lstrip(".")
+            bname = os.path.basename(src)
+
+            fsrc = src
+            if ftype=="js":
+                #save to a temporary file after replacing strings:
+                with open(src, mode='br') as f:
+                    data = f.read().decode("latin1")
+                if bname=="Utilities.js":
+                    print("adding revision info to %s" % (bname,))
+                    if REVISION:
+                        data = data.replace('REVISION : "0",',
+                                            'REVISION : "%i",' % REVISION)
+                    if LOCAL_MODIFICATIONS:
+                        data = data.replace('LOCAL_MODIFICATIONS : "0",',
+                                            'LOCAL_MODIFICATIONS : "%i",' % LOCAL_MODIFICATIONS)
                 if minifier=="uglifyjs":
-                    minify_cmd = ["uglifyjs",
-                                  "--screw-ie8",
-                                  src,
-                                  "-o", dst,
-                                  "--compress",
-                                  ]
-                else:
-                    assert minifier=="yuicompressor"
-                    import yuicompressor        #@UnresolvedImport
-                    jar = yuicompressor.get_jar_filename()
-                    java_cmd = os.environ.get("JAVA", "java")
-                    minify_cmd = [java_cmd, "-jar", jar,
-                                  src,
-                                  "--nomunge",
-                                  "--line-break", "400",
-                                  "--type", ftype,
-                                  "-o", dst,
-                                  ]
-                r = get_status_output(minify_cmd)[0]
-                if r!=0:
-                    print("Error: failed to minify '%s', command returned error %i" % (f, r))
-                    if verbose:
-                        print(" command: %s" % (minify_cmd,))
-                else:
-                    print("minified %s" % (f, ))
+                    for regexp, replacewith in {
+                        "^\s*for\s*\(\s*let\s+"     : "for(var ",
+                        "^\s*let\s+"                : "var ",
+                        "^\s*for\s*\(\s*const\s+"   : "for(var ",
+                        "^\s*const\s+"              : "var ",
+                        }.items():
+                        p = re.compile(regexp)
+                        newdata = []
+                        for line in data.splitlines():
+                            newdata.append(p.sub(replacewith, line))
+                        data = "\n".join(newdata)
+                f = tempfile.NamedTemporaryFile(prefix="uglify-%s" % bname, delete=False)
+                f.file.write(data.encode("latin1"))
+                f.file.flush()
+                f.close()
+                os.chmod(f.name, 0o644)
+                fsrc = f.name
+                rm = f.name
+
+                if minifier:
+                    if minifier=="uglifyjs":
+                        minify_cmd = ["uglifyjs",
+                                      "--screw-ie8",
+                                      fsrc,
+                                      "-o", dst,
+                                      "--compress",
+                                      ]
+                    else:
+                        assert minifier=="yuicompressor"
+                        import yuicompressor        #@UnresolvedImport
+                        jar = yuicompressor.get_jar_filename()
+                        java_cmd = os.environ.get("JAVA", "java")
+                        minify_cmd = [java_cmd, "-jar", jar,
+                                      fsrc,
+                                      "--nomunge",
+                                      "--line-break", "400",
+                                      "--type", ftype,
+                                      "-o", dst,
+                                      ]
+                    try:
+                        r = get_status_output(minify_cmd)[0]
+                    finally:
+                        if rm and False:
+                            os.unlink(rm)
+                    if r!=0:
+                        raise Exception("Error: failed to minify '%s', command %s returned error %i" % (f, minify_cmd, r))
+                    print("minified %s" % (fname, ))
             else:
-                r = -1
-            if r!=0:
-                with open(src, "rb") as f:
-                    js = f.read().decode("latin1")
-                if REVISION:
-                    js = js.replace('REVISION : "0",', 'REVISION : "%i",' % REVISION)
-                if LOCAL_MODIFICATIONS:
-                    js = js.replace('LOCAL_MODIFICATIONS : "0",', 'LOCAL_MODIFICATIONS : "%i",' % LOCAL_MODIFICATIONS)
-                with open(dst, "wb") as f:
-                    f.write(js.encode("latin1"))
+                shutil.copyfile(src, dst)
                 os.chmod(dst, 0o644)
+
             if ftype not in ("png", ):
                 if gzip:
                     gzip_dst = "%s.gz" % dst
