a7de6fcb3985131b00e645b9941366dabde433c3
Antoine Martin <totaam@xpra.org>
2020-04-06 16:56:11 +0000

    r26009 + r26010 backport: fix changes to speed and quality from the system tray
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@26011 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/gtk_tray_menu_base.py
+++ src/xpra/client/gtk_base/gtk_tray_menu_base.py
@@ -995,11 +995,11 @@ class GTKTrayMenuBase(object):
         return self.client.quality
     def set_min_quality(self, q):
         self.client.min_quality = q
-        self.client.quality = 0
+        self.client.quality = -1
         self.client.send_min_quality()
         self.client.send_quality()
     def set_quality(self, q):
-        self.client.min_quality = 0
+        self.client.min_quality = -1
         self.client.quality = q
         self.client.send_min_quality()
         self.client.send_quality()
@@ -1044,11 +1044,11 @@ class GTKTrayMenuBase(object):
         return self.client.speed
     def set_min_speed(self, s):
         self.client.min_speed = s
-        self.client.speed = 0
+        self.client.speed = -1
         self.client.send_min_speed()
         self.client.send_speed()
     def set_speed(self, s):
-        self.client.min_speed = 0
+        self.client.min_speed = -1
         self.client.speed = s
         self.client.send_min_speed()
         self.client.send_speed()
