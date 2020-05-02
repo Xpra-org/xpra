678cb2a2b2654a575c194bebcd19c13ecda661fb
Antoine Martin <totaam@xpra.org>
2020-05-02 15:10:44 +0000

    #1527 explicitly try to hide the cmd window, the shadow server will exit-with-children by default, poll the named-pipe rather than just polling the process
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26225 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/proxy_server.py
+++ src/xpra/platform/win32/proxy_server.py
@@ -4,6 +4,7 @@
 # later version. See the file COPYING for details.
 
 import os
+from time import sleep
 
 from xpra.server.proxy.proxy_server import ProxyServer as _ProxyServer
 from xpra.platform.paths import get_app_dir
@@ -21,16 +22,17 @@ def exec_command(username, args, exe, cwd, env):
     log("logon_msv1_s4u(%s)=%s", username, logon_info)
     from xpra.platform.win32.create_process_lib import (
         Popen,
-        CREATIONINFO, CREATION_TYPE_TOKEN,
+        CREATIONINFO, CREATION_TYPE_TOKEN, STARTF_USESHOWWINDOW,
         LOGON_WITH_PROFILE, CREATE_NEW_PROCESS_GROUP, STARTUPINFO,
         )
     creation_info = CREATIONINFO()
     creation_info.dwCreationType = CREATION_TYPE_TOKEN
-    creation_info.dwLogonFlags = LOGON_WITH_PROFILE
+    creation_info.dwLogonFlags = LOGON_WITH_PROFILE | STARTF_USESHOWWINDOW
     creation_info.dwCreationFlags = CREATE_NEW_PROCESS_GROUP
     creation_info.hToken = logon_info.Token
     log("creation_info=%s", creation_info)
     startupinfo = STARTUPINFO()
+    startupinfo.wShowWindow = 0     #aka win32.con.SW_HIDE
     startupinfo.lpDesktop = "WinSta0\\Default"
     startupinfo.lpTitle = "Xpra-Shadow"
     from subprocess import PIPE
@@ -50,12 +52,15 @@ class ProxyServer(_ProxyServer):
 
     def start_win32_shadow(self, username, password, new_session_dict):
         log("start_win32_shadow%s", (username, "..", new_session_dict))
-        #first, Logon:
-        try:
-            from xpra.platform.win32.desktoplogon_lib import Logon
-            Logon(strtobytes(username), strtobytes(password))
-        except Exception:
-            log.error("Error: failed to logon as '%s'", username, exc_info=True)
+        from xpra.platform.win32.wtsapi import find_session
+        session_info = find_session(username)
+        if not session_info:
+            #first, Logon:
+            try:
+                from xpra.platform.win32.desktoplogon_lib import Logon
+                Logon(strtobytes(username), strtobytes(password))
+            except Exception:
+                log.error("Error: failed to logon as '%s'", username, exc_info=True)
         #hwinstaold = set_window_station("winsta0")
         #whoami = os.path.join(get_app_dir(), "whoami.exe")
         #exec_command([whoami])
@@ -69,24 +74,27 @@ class ProxyServer(_ProxyServer):
         #use paexec to access the GUI session:
         if envbool("XPRA_PAEXEC", True) and os.path.exists(paexec) and os.path.isfile(paexec):
             #find the session-id to shadow:
-            from xpra.platform.win32.wtsapi import find_session
-            info = find_session(username)
-            if info:
+            if not session_info:
+                session_info = find_session(username)
+            if session_info:
                 cmd = [
                     "paexec.exe",
-                    "-i", str(info["SessionID"]), "-s",
+                    "-i", str(session_info["SessionID"]), "-s",
                     ]
                 exe = paexec
             else:
                 log.warn("Warning: session not found for username '%s'", username)
         else:
             log.warn("Warning: starting without paexec, expect a black screen")
-        
+
         cmd += [
             shadow_command,
             "--bind=%s" % named_pipe,
             #"--tray=no",
             ]
+        #unless explicitly stated otherwise, exit with client:
+        if new_session_dict.get("exit-with-client", None) is not False:
+            cmd.append("--exit-with-client=yes")
         from xpra.log import debug_enabled_categories
         if debug_enabled_categories:
             cmd += ["-d", ",".join(tuple(debug_enabled_categories))]
@@ -96,18 +104,27 @@ class ProxyServer(_ProxyServer):
         #env["XPRA_REDIRECT_OUTPUT"] = "1"
         #env["XPRA_LOG_FILENAME"] = "E:\\Shadow-Instance.log"
         proc = exec_command(username, cmd, exe, app_dir, env)
+        from xpra.platform.win32.dotxpra import DotXpra
+        dotxpra = DotXpra()
         #TODO: poll the named pipe instead of waiting
-        r = pollwait(proc, 4)
-        if r is not None:
-            log("pollwait=%s", r)
-            try:
-                log("stdout=%s", proc.stdout.read())
-                log("stderr=%s", proc.stderr.read())
-            except (OSError, AttributeError):
-                log("failed to read stdout / stderr of subprocess", exc_info=True)
-            if r!=0:
-                raise Exception("shadow subprocess failed with exit code %s" % r)
-            else:
-                raise Exception("shadow subprocess has already terminated")
+        for t in range(10):
+            r = pollwait(proc, 1)
+            if r is not None:
+                log("pollwait=%s", r)
+                try:
+                    log("stdout=%s", proc.stdout.read())
+                    log("stderr=%s", proc.stderr.read())
+                except (OSError, AttributeError):
+                    log("failed to read stdout / stderr of subprocess", exc_info=True)
+                if r!=0:
+                    raise Exception("shadow subprocess failed with exit code %s" % r)
+                else:
+                    raise Exception("shadow subprocess has already terminated")
+            if t>=4:
+                state = dotxpra.get_display_state(named_pipe)
+                log("get_display_state(%s)=%s", state)
+                if state==DotXpra.LIVE:
+                    sleep(2)
+                    break
         self.child_reaper.add_process(proc, "server-%s" % username, "xpra shadow", True, True)
         return proc, "named-pipe://%s" % named_pipe, named_pipe
