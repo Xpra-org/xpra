2090cfd731775a7b5c4fc80ed4f8fd684c615d0d
Antoine Martin <totaam@xpra.org>
2020-05-06 08:10:04 +0000

    remove unused import
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26264 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/codecs/enc_x264/encoder.pyx
+++ src/xpra/codecs/enc_x264/encoder.pyx
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2012-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2012-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -12,7 +12,7 @@ log = Logger("encoder", "x264")
 
 from xpra.util import nonl, envint, envbool, typedict, csv, AtomicInteger
 from xpra.os_util import bytestostr, strtobytes
-from xpra.codecs.codec_constants import get_subsampling_divs, video_spec
+from xpra.codecs.codec_constants import video_spec
 from collections import deque
 from xpra.buffers.membuf cimport object_as_buffer   #pylint: disable=syntax-error
 
