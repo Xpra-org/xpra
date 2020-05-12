4c8e238556d64e7362b60b5f409f41379b45236d
Antoine Martin <totaam@xpra.org>
2020-05-12 14:05:05 +0000

    ensure we try to guess the content-type at least once initially
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26325 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/models/base.py
+++ src/xpra/x11/models/base.py
@@ -392,12 +392,15 @@ class BaseWindowModel(CoreX11WindowModel):
             #the _XPRA_CONTENT_TYPE property takes precedence
             return
         content_type = guess_content_type(self)
-        metalog("content_type=%s", content_type)
+        metalog("guess_content_type(%s)=%s", self, content_type)
         self._updateprop("content-type", content_type)
 
     def _handle_xpra_content_type_change(self):
         content_type = self.prop_get("_XPRA_CONTENT_TYPE", "latin1", True) or ""
         metalog("content_type=%s", content_type)
+        if not content_type:
+            content_type = guess_content_type(self)
+            metalog("guess_content_type(%s)=%s", self, content_type)
         self._updateprop("content-type", content_type)
 
     def _handle_xpra_quality_change(self):
