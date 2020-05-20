8c0bee32af0f90c21f051e5d4ee361fb5e0f1276
Antoine Martin <totaam@xpra.org>
2020-05-20 08:57:24 +0000

    r26421 backport: use Xkb to lookup keysyms (not enabled by default in v3.0.x)
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@26422 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/x11/server_keyboard_config.py
+++ src/xpra/x11/server_keyboard_config.py
@@ -6,7 +6,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from xpra.util import csv, nonl, envbool
+from xpra.util import csv, nonl, envbool, repr_ellipsized
 from xpra.os_util import bytestostr
 from xpra.gtk_common.keymap import get_gtk_keymap
 from xpra.gtk_common.gtk_util import keymap_get_for_display, display_get_default, get_default_root_window
@@ -19,7 +19,7 @@ from xpra.x11.xkbhelper import (
     do_set_keymap, set_all_keycodes, set_keycode_translation,
     get_modifiers_from_meanings, get_modifiers_from_keycodes,
     clear_modifiers, set_modifiers, map_missing_modifiers,
-    clean_keyboard_state,
+    clean_keyboard_state, get_keycode_mappings,
     DEBUG_KEYSYMS,
     )
 from xpra.x11.bindings.keyboard_bindings import X11KeyboardBindings #@UnresolvedImport
@@ -404,7 +404,7 @@ class KeyboardConfig(KeyboardConfigBase):
         with xsync:
             clean_keyboard_state()
             #keycodes:
-            keycode_to_keynames = X11Keyboard.get_keycode_mappings()
+            keycode_to_keynames = get_keycode_mappings()
             self.keycode_translation = {}
             #prefer keycodes that don't use the lowest level+mode:
             default_for_keyname = {}
@@ -442,7 +442,7 @@ class KeyboardConfig(KeyboardConfigBase):
             self.update_keycode_mappings()
 
     def update_keycode_mappings(self):
-        self.keycode_mappings = X11Keyboard.get_keycode_mappings()
+        self.keycode_mappings = get_keycode_mappings()
 
 
     def do_get_keycode(self, client_keycode, keyname, pressed, modifiers, group):
