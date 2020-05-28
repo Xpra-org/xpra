7be98b175c3180fd3b4156be255e9c462f38f08d
Antoine Martin <totaam@xpra.org>
2020-05-28 15:32:04 +0000

    make lack of opencv less scary on win32
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26503 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/mixins/webcam.py
+++ src/xpra/client/mixins/webcam.py
@@ -72,12 +72,10 @@ class WebcamForwarder(StubClientMixin):
                     assert cv2 and Image
                 except ImportError as e:
                     log("init webcam failure", exc_info=True)
-                    if WIN32 and BITS==32:
-                        log.info("32-bit builds do not support webcam forwarding")
-                    else:
-                        log.warn("Warning: failed to import opencv:")
-                        log.warn(" %s", e)
-                        log.warn(" webcam forwarding is disabled")
+                    if WIN32:
+                        log.info("opencv not found:")
+                        log.info(" %s", e)
+                        log.info(" webcam forwarding is not available")
                     self.webcam_forwarding = False
         log("webcam forwarding: %s", self.webcam_forwarding)
 
