26f8b713f8419eaec147da5da4cd589ff9e1eaf2
Antoine Martin <totaam@xpra.org>
2020-06-11 12:42:31 +0000

    the value should always exist, but let's make the intent clearer
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26696 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gl/gtk3/gl_client_window.py
+++ src/xpra/client/gl/gtk3/gl_client_window.py
@@ -66,7 +66,7 @@ class GLClientWindowBase(GTK3ClientWindow):
 
     def set_alpha(self):
         GTK3ClientWindow.set_alpha(self)
-        rgb_formats = self._client_properties.get("encodings.rgb_formats", [])
+        rgb_formats = self._client_properties.setdefault("encodings.rgb_formats", [])
         #gl_window_backing supports BGR(A) too:
         if "RGBA" in rgb_formats:
             rgb_formats.append("BGRA")
