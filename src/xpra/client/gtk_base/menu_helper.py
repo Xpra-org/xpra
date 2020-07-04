8eea8bc809e3696d2c65421094c080602de2ed6f
Antoine Martin <totaam@xpra.org>
2020-07-04 18:15:57 +0000

    replace hackish buggy code with find_library()
    git-svn-id: https://xpra.org/svn/Xpra/trunk@26879 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gtk_base/menu_helper.py
+++ src/xpra/client/gtk_base/menu_helper.py
@@ -191,6 +191,7 @@ class MenuHelper:
                     Structure, POINTER,
                     cdll, create_string_buffer, c_int, c_char_p,
                     )
+                from ctypes.util import find_library
                 class QRCode(Structure):
                     _fields_ = (
                         ("version", c_int),
@@ -198,30 +199,29 @@ class MenuHelper:
                         ("data", c_char_p),
                         )
                 PQRCode = POINTER(QRCode)
-                if WIN32:
-                    libqrencode = cdll.LoadLibrary("libqrencode.dll")
-                else:
-                    libqrencode = cdll.LoadLibrary("libqrencode.so")
-                encodeString8bit = libqrencode.QRcode_encodeString8bit
-                encodeString8bit.argtypes = (c_char_p, c_int, c_int)
-                encodeString8bit.restype = PQRCode
-                QRcode_free = libqrencode.QRcode_free
-                QRcode_free.argtypes = (PQRCode,)
-                def qrencode_ctypes_fn(s):
-                    data = create_string_buffer(s.encode("latin1"))
-                    qrcode = encodeString8bit(data, 0, 0).contents
-                    try:
-                        size = qrcode.width
-                        pixels = bytearray(size*size)
-                        pdata = qrcode.data
-                        for i in range(size*size):
-                            pixels[i] = 0 if (pdata[i] & 0x1) else 255
-                        return Image.frombytes('L', (size, size), bytes(pixels))
-                    finally:
-                        QRcode_free(qrcode)
-                qrencode_fn = qrencode_ctypes_fn
+                lib_file = find_library("libqrencode")
+                if lib_file:
+                    libqrencode = cdll.LoadLibrary(lib_file)
+                    encodeString8bit = libqrencode.QRcode_encodeString8bit
+                    encodeString8bit.argtypes = (c_char_p, c_int, c_int)
+                    encodeString8bit.restype = PQRCode
+                    QRcode_free = libqrencode.QRcode_free
+                    QRcode_free.argtypes = (PQRCode,)
+                    def qrencode_ctypes_fn(s):
+                        data = create_string_buffer(s.encode("latin1"))
+                        qrcode = encodeString8bit(data, 0, 0).contents
+                        try:
+                            size = qrcode.width
+                            pixels = bytearray(size*size)
+                            pdata = qrcode.data
+                            for i in range(size*size):
+                                pixels[i] = 0 if (pdata[i] & 0x1) else 255
+                            return Image.frombytes('L', (size, size), bytes(pixels))
+                        finally:
+                            QRcode_free(qrcode)
+                    qrencode_fn = qrencode_ctypes_fn
             except Exception:
-                log("failed to load qrencode via ctypes", exc_info=True)
+                log.error("failed to load qrencode via ctypes", exc_info=True)
 
         def show_qr(*_args):
             uri = self.client.display_desc.get("display_name")
