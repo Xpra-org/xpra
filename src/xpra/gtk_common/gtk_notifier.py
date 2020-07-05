10d719f2a5533d50403d4e33effd607eb6548193
Antoine Martin <totaam@xpra.org>
2020-07-05 14:16:43 +0000

    notification popups should not steal focus
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@26893 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/gtk_common/gtk_notifier.py
+++ src/xpra/gtk_common/gtk_notifier.py
@@ -173,6 +173,8 @@ class Popup(gtk.Window):
         self.nid = nid
         gtk.Window.__init__(self)
 
+        self.set_accept_focus(False)
+        self.set_focus_on_map(False)
         self.set_size_request(stack.size_x, -1)
         self.set_decorated(False)
         self.set_deletable(False)
