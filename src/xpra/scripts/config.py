6fbc11af80645eb2557777f5d964c15f31938289
Antoine Martin <totaam@xpra.org>
2020-08-15 09:19:09 +0000

    #2834 switch to Xvfb on Debian and Ubuntu
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27165 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/scripts/config.py
+++ src/xpra/scripts/config.py
@@ -11,7 +11,7 @@ from xpra.util import csv
 from xpra.os_util import (
     WIN32, OSX, POSIX,
     osexpand, getuid, getgid, get_username_for_uid,
-    is_Debian, is_arm,
+    is_Debian, is_Ubuntu, is_arm,
     )
 
 def warn(msg):
@@ -133,6 +133,10 @@ def detect_xvfb_command(conf_dir="/etc/xpra/", bin_dir=None,
         return get_Xvfb_command()
     if is_arm():
         #arm struggles to launch Xdummy, so use Xvfb:
+        return get_Xvfb_command()
+    if is_Ubuntu() or is_Debian():
+        #These distros do weird things and this can cause the real X11 server to crash
+        #see ticket #2834
         return get_Xvfb_command()
 
     xorg_bin = get_xorg_bin()
