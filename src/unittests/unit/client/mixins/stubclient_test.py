a4b90e3de2a333a32fa279a5cc5cddc30fec1f20
Antoine Martin <totaam@xpra.org>
2020-08-17 14:45:19 +0000

    #2598 full test coverage for stub
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27186 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/client/mixins/stubclient_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2019-2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+#pylint: disable=line-too-long
+
+import unittest
+
+from xpra.util import AdHocStruct
+from unit.client.mixins.clientmixintest_util import ClientMixinTest
+from xpra.client.mixins.stub_client_mixin import StubClientMixin
+
+
+class StubClientTest(ClientMixinTest):
+
+	def test_mixin(self):
+		opts = AdHocStruct()
+		self._test_mixin_class(StubClientMixin, opts, {})
+
+	def test_compressed_wrapper(self):
+		s = StubClientMixin()
+		s.compressed_wrapper("text", "foo", 1)
+		try:
+			s.compressed_wrapper("text", "bar", 0)
+		except Exception:
+			pass
+		else:
+			raise Exception("should have failed with level 0")
+
+
+def main():
+	unittest.main()
+
+
+if __name__ == '__main__':
+	main()
