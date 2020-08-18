71d01bc35b361389b3b45a9ca749418e0d087900
Antoine Martin <totaam@xpra.org>
2020-08-18 16:55:16 +0000

    use the shared vfb code to add a new test: grok
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27197 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/x11/keyboard_test.py
+++ src/unittests/unit/x11/keyboard_test.py
@@ -13,28 +13,44 @@ from xpra.os_util import OSX, POSIX
 
 class TestX11Keyboard(ServerTestUtil):
 
-    def test_unicode(self):
+    def setUp(self):
+        ServerTestUtil.setUp(self)
         display = self.find_free_display()
-        xvfb = self.start_Xvfb(display)
+        self.xvfb = self.start_Xvfb(display)
         os.environ["DISPLAY"] = display
         os.environ["GDK_BACKEND"] = "x11"
-        from xpra.x11.bindings.posix_display_source import X11DisplayContext    #@UnresolvedImport
-        with X11DisplayContext(display):
-            from xpra.x11.bindings.keyboard_bindings import X11KeyboardBindings        #@UnresolvedImport
-            keyboard_bindings = X11KeyboardBindings()
-            for x in (
-                "2030", "0005", "0010", "220F", "2039", "2211",
-                "2248", "FB01", "F8FF", "203A", "FB02", "02C6",
-                "02DA", "02DC", "2206", "2044", "25CA",
-                ):
-                #hex form:
-                hk = keyboard_bindings.parse_keysym("0x"+x)
-                #osx U+ form:
-                uk = keyboard_bindings.parse_keysym("U+"+x)
-                log("keysym(U+%s)=%#x, keysym(0x%s)=%#x", x, uk, x, hk)
-                assert hk and uk
-                assert uk == hk, "failed to get unicode keysym %s" % x
-        xvfb.terminate()
+        from xpra.x11.bindings.posix_display_source import init_posix_display_source    #@UnresolvedImport
+        self.display_ptr = init_posix_display_source()
+
+    def tearDown(self):
+        ServerTestUtil.tearDown(self)
+        from xpra.x11.bindings.posix_display_source import close_display_source         #@UnresolvedImport
+        close_display_source(self.display_ptr)
+        self.xvfb.terminate()
+
+
+    def test_unicode(self):
+        from xpra.x11.bindings.keyboard_bindings import X11KeyboardBindings        #@UnresolvedImport
+        keyboard_bindings = X11KeyboardBindings()
+        for x in (
+            "2030", "0005", "0010", "220F", "2039", "2211",
+            "2248", "FB01", "F8FF", "203A", "FB02", "02C6",
+            "02DA", "02DC", "2206", "2044", "25CA",
+            ):
+            #hex form:
+            hk = keyboard_bindings.parse_keysym("0x"+x)
+            #osx U+ form:
+            uk = keyboard_bindings.parse_keysym("U+"+x)
+            log("keysym(U+%s)=%#x, keysym(0x%s)=%#x", x, uk, x, hk)
+            assert hk and uk
+            assert uk == hk, "failed to get unicode keysym %s" % x
+
+    def test_grok_modifier_map(self):
+        from xpra.x11.gtk_x11.keys import grok_modifier_map
+        from gi.repository.Gdk import Display
+        display = Display.get_default()
+        grok_modifier_map(display, None)
+        grok_modifier_map(display, {})
 
 
 def main():
