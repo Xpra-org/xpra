8ccb2dc5c3c88b5611462a4e15a7d6d669a4636d
Antoine Martin <totaam@xpra.org>
2020-08-20 16:01:35 +0000

    #2598 better coverage
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27245 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/net/net_util_test.py
+++ src/unittests/unit/net/net_util_test.py
@@ -9,11 +9,16 @@ import unittest
 from xpra.net.net_util import (
     get_info, get_interfaces, get_interfaces_addresses, #get_interface,
     get_gateways, get_bind_IPs, do_get_bind_ifacemask,
-    get_ssl_info,
+    get_ssl_info, get_interface,
+    if_nametoindex, if_indextoname, get_iface,
+    get_free_tcp_port,
     )
 
 
 class TestVersionUtilModule(unittest.TestCase):
+
+    def test_tcp_port(self):
+        assert get_free_tcp_port()>0
 
     def test_netifaces(self):
         ifaces = get_interfaces()
@@ -21,6 +26,13 @@ class TestVersionUtilModule(unittest.TestCase):
             return
         for iface in ifaces:
             do_get_bind_ifacemask(iface)
+            try:
+                i = if_nametoindex(iface)
+            except Exception:
+                pass
+            else:
+                assert if_indextoname(i)==iface
+            get_iface(iface)
         ia = get_interfaces_addresses()
         assert ia
         #for iface, address in ia.items():
@@ -30,6 +42,25 @@ class TestVersionUtilModule(unittest.TestCase):
         get_bind_IPs()
         get_ssl_info()
         get_info()
+
+        assert if_indextoname(-1) is None
+
+        def invalid_iface(s):
+            v = get_iface(s)
+            if v is not None:
+                raise Exception("invalid IP '%s' should not return interface '%s'" % (s, v))
+        invalid_iface(None)
+        invalid_iface("")
+        invalid_iface("%")
+        invalid_iface(":")
+        invalid_iface("INVALIDHOSTNAME")
+        invalid_iface("10.0.0")
+        get_iface("localhost")
+
+        assert get_interface("invalid") is None
+
+    def test_ssl_info(self):
+        assert get_ssl_info(True)
 
 
 def main():
