607f42860fdf519bce5e8176f4806954fb446855
Antoine Martin <totaam@xpra.org>
2020-08-20 11:08:04 +0000

    #2598 full test coverage for background worker
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27232 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/background_worker_test.py
+++ src/unittests/unit/server/background_worker_test.py
@@ -7,7 +7,7 @@
 import time
 import unittest
 
-from xpra.server.background_worker import get_worker, add_work_item, stop_worker
+from xpra.server.background_worker import get_worker, add_work_item, stop_worker, Worker_Thread
 
 
 class BackgroundWorkerTest(unittest.TestCase):
@@ -19,10 +19,18 @@ class BackgroundWorkerTest(unittest.TestCase):
         def error_item():
             raise Exception("work item test error")
         add_work_item(error_item)
+        #add the same item twice, with "no-duplicates"
+        #(should only get added once)
+        ndc = []
+        def nodupe():
+            ndc.append(True)
+        w.add(nodupe, False)
+        w.add(nodupe, False)
+        time.sleep(1)
+        #trigger the warning with more than 10 items:
         def slow_item():
             time.sleep(1)
-        #trigger the warning with more than 10 items:
-        for _ in range(11):
+        for _ in range(12):
             w.add(slow_item)
         stop_worker()
         stop_worker(True)
@@ -30,6 +38,14 @@ class BackgroundWorkerTest(unittest.TestCase):
         stop_worker(True)
         #let the worker print its messages:
         time.sleep(1)
+        assert len(ndc)==1, "nodupe item should have been run once only, got %i" % (len(ndc), )
+
+    def test_normal_stop(self):
+        w = Worker_Thread()
+        w.start()
+        w.add(None)
+        time.sleep(1)
+        assert w.exit is True
 
 def main():
     unittest.main()
