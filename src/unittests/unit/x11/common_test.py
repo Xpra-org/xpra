1427127d09600cd13b313e14dfeee68038608f6e
Antoine Martin <totaam@xpra.org>
2020-08-20 11:13:59 +0000

    #2598 full test coverage for X11Event repr
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27233 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/x11/common_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import unittest
+
+
+
+class TestDisplayUtil(unittest.TestCase):
+
+    def test_repr(self):
+        from xpra.x11.common import X11Event, REPR_FUNCTIONS
+        class Custom():
+            def repr(self):
+                return "Custom"
+        def custom_repr(*_args):
+            return "XXXXX"
+        REPR_FUNCTIONS[Custom] = custom_repr
+        name = "00name00"
+        e = X11Event(name)
+        e.display = "00display00"
+        e.type = "00type00"
+        e.serial = 255
+        e.custom = Custom()
+        assert repr(e).find(name)>0
+        assert repr(e).find(e.display)<0
+        assert repr(e).find(e.type)<0
+        assert repr(e).find("%#x" % e.serial)>0
+        assert repr(e).find("XXXXX")>0
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
