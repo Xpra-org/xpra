0232c2c31540c90807e8935127368bf576eab3da
Antoine Martin <totaam@xpra.org>
2020-08-20 10:20:07 +0000

    test all bit depth combinations between server, vfb and client
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27229 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/client/x11_client_test.py
+++ src/unittests/unit/client/x11_client_test.py
@@ -6,10 +6,13 @@
 
 import os
 import time
+import uuid
 import unittest
+
 from xpra.util import envint
 from xpra.exit_codes import EXIT_STR
 from xpra.os_util import load_binary_file, pollwait, OSX, POSIX
+from xpra.platform.paths import get_download_dir
 from unit.client.x11_client_test_util import X11ClientTestUtil, log
 
 CLIENT_TIMEOUT = envint("XPRA_TEST_CLIENT_TIMEOUT", 20)
@@ -17,19 +20,20 @@ CLIENT_TIMEOUT = envint("XPRA_TEST_CLIENT_TIMEOUT", 20)
 
 class X11ClientTest(X11ClientTestUtil):
 
-	def do_test_connect(self, sharing, *client_args):
+	def do_test_connect(self, disconnect=True, client_args=(), server_args=()):
 		display = self.find_free_display()
 		log("starting test server on %s", display)
-		server = self.check_start_server(display, "--start=xterm", "--sharing=%s" % sharing)
-		xvfb1, client1 = self.run_client(display, "--sharing=%s" % sharing, *client_args)
+		server_args = ["--start=xterm"] + list(server_args)
+		server = self.check_start_server(display, *server_args)
+		xvfb1, client1 = self.run_client(display, *client_args)
 		r = pollwait(client1, CLIENT_TIMEOUT)
 		assert r is None, "client1 exited with code %s" % EXIT_STR.get(r, r)
-		xvfb2, client2 = self.run_client(display, "--sharing=%s" % sharing, *client_args)
+		xvfb2, client2 = self.run_client(display, *client_args)
 		r = pollwait(client2, CLIENT_TIMEOUT)
 		assert r is None, "client2 exited with code %s" % EXIT_STR.get(r, r)
-		if not sharing:
+		if disconnect:
 			#starting a second client should disconnect the first when not sharing
-			assert pollwait(client1, 2) is not None, "the first client should have been disconnected (sharing off)"
+			assert pollwait(client1, 2) is not None, "the first client should have been disconnected"
 		#killing the Xvfb should kill the client
 		xvfb1.terminate()
 		xvfb2.terminate()
@@ -41,20 +45,51 @@ class X11ClientTest(X11ClientTestUtil):
 
 
 	def test_connect(self):
-		self.do_test_connect(False)
+		self.do_test_connect()
 
 	def test_sharing(self):
-		self.do_test_connect(True)
+		self.do_test_connect(False, server_args=("--sharing=yes",))
 
 	def test_opengl(self):
-		self.do_test_connect(False, "--opengl=yes")
+		self.do_test_connect(client_args=("--opengl=yes",))
 
 	def test_multiscreen(self):
 		client_display = self.find_free_display()
 		xvfb = self.start_Xvfb(client_display, screens=[(1024,768), (1200, 1024)])
-		#multiscreen requires Xvfb, which does not support opengl:
-		return xvfb, self.do_run_client(client_display, "--opengl=no")
+		#multiscreen requires Xvfb, which may not support opengl:
+		self.do_run_client(client_display, "--opengl=no").terminate()
+		xvfb.terminate()
 
+	def test_nocomposite(self):
+		client_display = self.find_free_display()
+		self.start_Xvfb(client_display, extensions=("-Composite"))
+		self.do_run_client(client_display).terminate()
+
+
+	def _test_client_depth(self, server_display):
+		#start vfb with display-depth:
+		for client_display_depth in (16, 24, 30):
+			client_display = self.find_free_display()
+			xvfb = self.start_Xvfb(client_display, depth=client_display_depth)
+			try:
+				for client_depth in (16, 24, 30):
+					client = self.do_run_client(client_display, server_display,
+											"--pixel-depth=%i" % client_depth)
+					r = pollwait(client, 5)
+					assert r is None, "client has terminated with exit code %i" % r
+					client.terminate()
+			finally:
+				xvfb.terminate()
+
+	def test_depth(self):
+		for server_depth in (16, 24, 30):
+			server_display = self.find_free_display()
+			log("depth=%i starting test server on %s", server_depth, server_display)
+			server = self.check_start_server(server_display,
+											"--start=xterm", "--sync-xvfb=50",
+											"--pixel-depth=%i" % server_depth)
+			self._test_client_depth(server_display)
+			server.terminate()
 
 	def do_test_control_send_file(self, data):
 		f = self._temp_file(data)
@@ -69,7 +104,6 @@ class X11ClientTest(X11ClientTestUtil):
 			assert pollwait(send_file, CLIENT_TIMEOUT)==0, "send-file command returncode is %s" % send_file.poll()
 			time.sleep(1)
 			#now verify the file can be found in the download directory
-			from xpra.platform.paths import get_download_dir
 			filename = os.path.join(os.path.expanduser(get_download_dir()), os.path.basename(f.name))
 			assert os.path.exists(filename), "cannot find %s" % filename
 			readback = load_binary_file(filename)
@@ -83,7 +117,6 @@ class X11ClientTest(X11ClientTestUtil):
 			f.close()
 
 	def test_control_send_file(self):
-		import uuid
 		data = b"".join(uuid.uuid4().bytes for _ in range(100))
 		self.do_test_control_send_file(data)
 
