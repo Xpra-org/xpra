bad7677c12cf6f7f9573993c1a259abcd34265f6
Antoine Martin <totaam@xpra.org>
2020-08-22 15:29:39 +0000

    fix SetRefreshDelay: using dbus types breaks 'xpra info'
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27271 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/shadow/shadow_dbus_server.py
+++ src/xpra/server/shadow/shadow_dbus_server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # This file is part of Xpra.
-# Copyright (C) 2015-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2015-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 import dbus.service
 
-from xpra.server.dbus.dbus_server import DBUS_Server, INTERFACE
+from xpra.server.dbus.dbus_server import DBUS_Server, INTERFACE, ni
 from xpra.log import Logger
 log = Logger("dbus", "server")
 
@@ -15,4 +15,5 @@ class Shadow_DBUS_Server(DBUS_Server):
 
     @dbus.service.method(INTERFACE, in_signature='i')
     def SetRefreshDelay(self, milliseconds):
-        return self.server.set_refresh_delay(milliseconds)
+        log("SetRefreshDelay(%i)", milliseconds)
+        return self.server.set_refresh_delay(ni(milliseconds))
