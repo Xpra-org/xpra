ece9cba8a5757436d087ae002a00d3cb827896be
Antoine Martin <totaam@xpra.org>
2020-08-23 05:06:05 +0000

    #2598 better test coverage of dbus_common
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27283 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/server/dbus_test.py
+++ src/unittests/unit/server/dbus_test.py
@@ -4,24 +4,26 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import os
 import unittest
 
-from xpra.os_util import OSX, POSIX
+from xpra.os_util import OSX, POSIX, OSEnvContext
 
 
 def noop(*_args, **_kwargs):
     pass
 
+class FakeLogger:
+    error = noop
+    info = noop
+    debug = noop
+    def __call__(self, *args, **kwargs):
+        noop(*args, **kwargs)
+
 class DBUSTest(unittest.TestCase):
 
     def test_exception_wrap(self):
         from xpra.server.dbus import dbus_common
-        class FakeLogger:
-            error = noop
-            info = noop
-            debug = noop
-            def __call__(self, *args, **kwargs):
-                noop(*args, **kwargs)
         dbus_common.log = FakeLogger()
         def r1():
             return 1
@@ -32,6 +34,44 @@ class DBUSTest(unittest.TestCase):
         assert dbus_common.dbus_exception_wrap(r1)==1
         assert dbus_common.dbus_exception_wrap(rimporterror)==None
         assert dbus_common.dbus_exception_wrap(rfail)==None
+
+
+    def test_start_dbus(self):
+        from xpra.server.dbus.dbus_start import start_dbus
+        def f(v):
+            r, d = start_dbus(v)
+            assert r==0 and not d, "dbus should not have started for '%s'" % v
+        def w(v):
+            r, d = start_dbus(v)
+            assert r>0 and d, "dbus should have started for '%s'" % v
+        def rm():
+            try:
+                del os.environ["DBUS_SESSION_BUS_ADDRESS"]
+            except KeyError:
+                pass
+        with OSEnvContext():
+            rm()
+            f("no")
+            f("0")
+            os.environ["DBUS_SESSION_BUS_ADDRESS"] = "whatever"
+            f("dbus-launch")
+            rm()
+            f("this-is-not-a-valid-command")
+            f("shlex-parsing-error '")
+            f("echo set DBUS_SESSION_BUS_PID")
+            w("echo set DBUS_SESSION_BUS_PID=50")
+            w("echo \"set DBUS_SESSION_BUS_PID='100';\"")
+            w("echo set DBUS_SESSION_BUS_PID=150;")
+            w("echo setenv DBUS_SESSION_BUS_PID 200")
+            w("printf \"export DBUS_SESSION_BUS_PID\nset DBUS_SESSION_BUS_PID=250\n\"")
+
+
+    def test_save_dbus_env(self):
+        from xpra.server.dbus import dbus_start
+        dbus_start.log = FakeLogger()
+        with OSEnvContext():
+            #assert get_saved_dbus_env()
+            pass
 
 
 def main():
