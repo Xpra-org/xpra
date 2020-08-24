c73a4e38a90b5dd0a3591d6a4bd1c075acf642a2
Antoine Martin <totaam@xpra.org>
2020-08-24 15:37:40 +0000

    #2598 add tests for rfb
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27308 3bb7dfac-3a0b-4e04-842a-767bc560f471

---
+++ src/unittests/unit/server/rfb/rfb_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# This file is part of Xpra.
+# Copyright (C) 2020 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+
+import unittest
+
+from xpra.util import AdHocStruct
+from xpra.codecs.image_wrapper import ImageWrapper
+from xpra.server.rfb.rfb_source import RFBSource
+
+def noop(*_args):
+    pass
+
+
+class TestRFB(unittest.TestCase):
+
+    def test_rfb_source(self):
+        #fake protocol:
+        p = AdHocStruct()
+        p.send = noop
+        p.queue_size = lambda : 1
+        #fake window:
+        window = AdHocStruct()
+        def get_image(x, y, w, h):
+            stride = (w+8)*4
+            pixels = b"0"*stride*h
+            return ImageWrapper(x, y, w, h, pixels, "BGRX", 24, stride, 4)
+        window.get_image = get_image
+        window.acknowledge_changes = noop
+        for protocol in (p, None):
+            s = RFBSource(protocol, True)
+            assert s.get_info()
+            s.get_window_info(())
+            s.ping()
+            s.keys_changed()
+            s.set_default_keymap()
+            s.send_cursor()
+            s.send_server_event()
+            s.update_mouse()
+            s.damage(1, window, 0, 0, 1024, 768, {"polling" : True})
+            s.damage(1, window, 0, 0, 2, 2)
+            s.send_clipboard("foo")
+            s.bell()
+            assert not s.is_closed()
+            s.close()
+            #noop:
+            s.damage(1, window, 0, 0, 2, 2)
+            assert s.is_closed()
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
