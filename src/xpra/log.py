dcb64e971e54391ccef32a0b939f457d0f968a44
Antoine Martin <totaam@xpra.org>
2020-09-03 13:53:20 +0000

    python2 doesn't have a RecursionError..
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.1.x@27405 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/log.py
+++ src/xpra/log.py
@@ -443,7 +443,7 @@ class SIGPIPEStreamHandler(logging.StreamHandler):
             # issue 35046: merged two stream.writes into one.
             stream.write(msg + self.terminator)
             self.flush()
-        except RecursionError:  # See issue 36272
+        except RuntimeError:  # See issue 36272
             raise
         except BrokenPipeError:
             pass
