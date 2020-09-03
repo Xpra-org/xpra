b27ad3fd47342f1605ae0e569156fb7f2f948c90
Antoine Martin <totaam@xpra.org>
2020-09-03 11:56:38 +0000

    typo editing file
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.1.x@27403 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/util.py
+++ src/xpra/util.py
@@ -1,4 +1,4 @@
-e This file is part of Xpra.
+# This file is part of Xpra.
 # Copyright (C) 2008, 2009 Nathaniel Smith <njs@pobox.com>
 # Copyright (C) 2013-2019 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
