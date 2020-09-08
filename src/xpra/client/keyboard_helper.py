02f3b37280b9268a19ddfe742eb7f5c58d5524f1
Antoine Martin <totaam@xpra.org>
2020-09-08 15:06:54 +0000

    split parsing function into its own module
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27433 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/keyboard_helper.py
+++ src/xpra/client/keyboard_helper.py
@@ -5,10 +5,9 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import os
-
-from xpra.util import nonl, csv, std, envbool, print_nested_dict, ellipsizer
-from xpra.os_util import POSIX, bytestostr
+from xpra.client.keyboard_shortcuts_parser import parse_shortcuts
+from xpra.util import nonl, csv, std, envbool, ellipsizer
+from xpra.os_util import bytestostr
 from xpra.log import Logger
 
 log = Logger("keyboard")
@@ -87,136 +86,7 @@ class KeyboardHelper:
 
 
     def parse_shortcuts(self, strs):
-        if not strs:
-            """ if none are defined, add this as default
-            it would be nicer to specify it via OptionParser in main
-            but then it would always have to be there with no way of removing it
-            whereas now it is enough to define one (any shortcut)
-            """
-            strs = ["meta+shift+F4:quit"]
-        log("parse_shortcuts(%s)" % str(strs))
-        shortcuts = {}
-        #modifier names contains the internal modifiers list, ie: "mod1", "control", ...
-        #but the user expects the name of the key to be used, ie: "alt" or "super"
-        #whereas at best, we keep "Alt_L" : "mod1" mappings... (xposix)
-        #so generate a map from one to the other:
-        modifier_names = {}
-        from xpra.keyboard.mask import DEFAULT_MODIFIER_MEANINGS, DEFAULT_MODIFIER_NUISANCE
-        meanings = self.xkbmap_mod_meanings or DEFAULT_MODIFIER_MEANINGS
-        DEFAULT_MODIFIER_IGNORE_KEYNAMES = ["Caps_Lock", "Num_Lock", "Scroll_Lock"]
-        for pub_name,mod_name in meanings.items():
-            if mod_name in DEFAULT_MODIFIER_NUISANCE or pub_name in DEFAULT_MODIFIER_IGNORE_KEYNAMES:
-                continue
-            #just hope that xxx_L is mapped to the same modifier as xxx_R!
-            if pub_name.endswith("_L") or pub_name.endswith("_R"):
-                pub_name = pub_name[:-2]
-            elif pub_name=="ISO_Level3_Shift":
-                pub_name = "AltGr"
-            if pub_name not in modifier_names:
-                modifier_names[pub_name.lower()] = mod_name
-            if pub_name.lower()=="control":
-                #alias "control" to "ctrl" as it is often used:
-                modifier_names["ctrl"] = mod_name
-        log("parse_shortcuts: modifier names=%s", modifier_names)
-
-        #figure out the default shortcut modifiers
-        #accept "," or "+" as delimiter:
-        shortcut_modifiers = self.shortcut_modifiers.lower().replace(",", "+").split("+")
-        def mod_defaults():
-            mods = ["meta", "shift"]
-            if POSIX:
-                #gnome intercepts too many of the Alt+Shift shortcuts,
-                #so use control with gnome:
-                if os.environ.get("XDG_CURRENT_DESKTOP")=="GNOME":
-                    mods = ["control", "shift"]
-            return mods
-        if shortcut_modifiers==["auto"]:
-            shortcut_modifiers = mod_defaults()
-        elif shortcut_modifiers==["none"]:
-            shortcut_modifiers = []
-        else:
-            r = []
-            for x in shortcut_modifiers:
-                x = x.lower()
-                if x not in modifier_names:
-                    log.warn("Warning: invalid shortcut modifier '%s'", x)
-                else:
-                    r.append(x)
-            if shortcut_modifiers and not r:
-                log.warn(" using defaults instead")
-                shortcut_modifiers = mod_defaults()
-            else:
-                shortcut_modifiers = r
-        log("shortcut modifiers=%s", shortcut_modifiers)
-
-        for s in strs:
-            #example for s: Control+F8:some_action()
-            parts = s.split(":", 1)
-            if len(parts)!=2:
-                log.error("Error: invalid key shortcut '%s'", s)
-                continue
-            #example for action: "quit"
-            action = parts[1].strip()
-            args = ()
-            if action.find("(")>0 and action.endswith(")"):
-                try:
-                    action, all_args = action[:-1].split("(", 1)
-                    args = []
-                    for x in all_args.split(","):
-                        x = x.strip()
-                        if not x:
-                            continue
-                        if (x[0]=='"' and x[-1]=='"') or (x[0]=="'" and x[-1]=="'"):
-                            args.append(x[1:-1])
-                        if x=="None":
-                            args.append(None)
-                        if x.find("."):
-                            args.append(float(x))
-                        else:
-                            args.append(int(x))
-                    args = tuple(args)
-                except Exception as e:
-                    log.warn("Warning: failed to parse arguments of shortcut:")
-                    log.warn(" '%s': %s", s, e)
-                    continue
-            action = action.replace("-", "_")       #must be an object attribute
-            log("action(%s)=%s%s", s, action, args)
-
-            #example for keyspec: ["Alt", "F8"]
-            keyspec = parts[0].split("+")
-            modifiers = []
-            if len(keyspec)>1:
-                valid = True
-                #ie: ["Alt"]
-                for mod in keyspec[:len(keyspec)-1]:
-                    mod = mod.lower()
-                    if mod=="none":
-                        continue
-                    elif mod=="#":
-                        #this is the placeholder for the list of shortcut modifiers:
-                        for x in shortcut_modifiers:
-                            imod = modifier_names.get(x)
-                            if imod:
-                                modifiers.append(imod)
-                        continue
-                    #find the real modifier for this name:
-                    #ie: "alt_l" -> "mod1"
-                    imod = modifier_names.get(mod)
-                    if not imod:
-                        log.warn("Warning: invalid modifier '%s' in keyboard shortcut '%s'", mod, s)
-                        log.warn(" the modifiers must be one of: %s", csv(modifier_names.keys()))
-                        valid = False
-                        break
-                    modifiers.append(imod)
-                if not valid:
-                    continue
-            #should we be validating the keyname?
-            keyname = keyspec[len(keyspec)-1]
-            shortcuts.setdefault(keyname, []).append((modifiers, action, args))
-            log("shortcut(%s)=%s", s, csv((modifiers, action, args)))
-        log("parse_shortcuts(%s)=%s" % (str(strs), shortcuts))
-        print_nested_dict(shortcuts, print_fn=log)
-        return shortcuts
+        return parse_shortcuts(strs, self.shortcut_modifiers, self.xkbmap_mod_meanings)
 
     def key_handled_as_shortcut(self, window, key_name, modifiers, depressed):
         #find the shortcuts that may match this key:
