bd15fc53a1c7f6b5bff10a6c4832bacfb2132c61
Antoine Martin <totaam@xpra.org>
2020-09-24 09:28:01 +0000

    fix missing pixels on the edge of video areas in 'auto' encoding mode
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27535 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/window/window_video_source.py
+++ src/xpra/server/window/window_video_source.py
@@ -900,7 +900,8 @@ class WindowVideoSource(WindowSource):
         # * we want av-sync
         # * the video encoder needs a thread safe image
         #   (the xshm backing may change from underneath us if we don't freeze it)
-        must_freeze = av_delay>0 or ((coding in self.video_encodings or coding=="auto") and not image.is_thread_safe())
+        video_mode = coding in self.video_encodings or coding=="auto"
+        must_freeze = av_delay>0 or (video_mode and not image.is_thread_safe())
         log("process_damage_region: av_delay=%s, must_freeze=%s, size=%s, encoding=%s",
             av_delay, must_freeze, (w, h), coding)
         if must_freeze:
@@ -921,7 +922,7 @@ class WindowVideoSource(WindowSource):
                 self.encode_queue.append(item)
                 self.schedule_encode_from_queue(av_delay)
         #now figure out if we need to send edges separately:
-        if coding in self.video_encodings and self.edge_encoding and not VIDEO_SKIP_EDGE:
+        if video_mode and self.edge_encoding and not VIDEO_SKIP_EDGE:
             dw = w - (w & self.width_mask)
             dh = h - (h & self.height_mask)
             if dw>0 and h>0:
