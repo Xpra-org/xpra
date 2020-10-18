bf0db63a97ab0aea5f7ccae33a7770b5c7bf3e09
Antoine Martin <totaam@xpra.org>
2020-10-18 10:38:31 +0000

    #2901 use svnversion of 'src'
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27687 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/add_build_info.py
+++ src/add_build_info.py
@@ -286,7 +286,7 @@ def get_svn_props(warn=True):
                 "LOCAL_MODIFICATIONS" : "unknown"
             }
     #find revision:
-    proc = subprocess.Popen("svnversion -n ..", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen("svnversion -n .", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (out, _) = proc.communicate()
     if proc.returncode!=0:
         print("'svnversion -n' failed with return code %s" % proc.returncode)
