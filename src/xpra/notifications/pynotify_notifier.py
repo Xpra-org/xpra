4eff619b8c4f924f4b8c3e83d9ffc88f09fa9f37
Antoine Martin <totaam@xpra.org>
2020-11-04 11:52:28 +0000

    make it easier to test notifications with custom messages
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27836 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/notifications/pynotify_notifier.py
+++ src/xpra/notifications/pynotify_notifier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # This file is part of Xpra.
-# Copyright (C) 2011-2018 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2011-2020 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import sys
 import notify2                 #@UnresolvedImport
 
 from xpra.notifications.notifier_base import NotifierBase
@@ -42,13 +43,19 @@ class PyNotify_Notifier(NotifierBase):
         pass
 
 
-def main():
+def main(args):
     import gi
     gi.require_version("Gtk", "3.0")
     from gi.repository import GLib, Gtk
+    summary = "Summary"
+    body = "Body..."
+    if len(sys.argv)>1:
+        summary = sys.argv[1]
+    if len(sys.argv)>2:
+        body = sys.argv[2]
     def show():
         n = PyNotify_Notifier()
-        n.show_notify("", None, 0, "Test", 0, "", "Summary", "Body...", ["0", "Hello", "1", "Bye"], {}, 0, "")
+        n.show_notify("", None, 0, "Test", 0, "", summary, body, ["0", "Hello", "1", "Bye"], {}, 0, "")
         return False
     GLib.idle_add(show)
     GLib.timeout_add(20000, Gtk.main_quit)
@@ -56,4 +63,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
