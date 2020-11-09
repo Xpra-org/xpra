541b8b2b75254fd1cbe627a5c2d1a67643a0e1a9
Antoine Martin <totaam@xpra.org>
2020-11-09 12:32:38 +0000

    add missing import
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27890 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/darwin/shadow_server.py
+++ src/xpra/platform/darwin/shadow_server.py
@@ -10,6 +10,7 @@ from xpra.util import envbool
 from xpra.os_util import memoryview_to_bytes
 from xpra.scripts.config import InitExit
 from xpra.scripts.main import check_display
+from xpra.exit_codes import EXIT_FAILURE
 from xpra.server.gtk_server_base import GTKServerBase
 from xpra.server.shadow.gtk_shadow_server_base import GTKShadowServerBase
 from xpra.platform.darwin.keyboard_config import KeyboardConfig
