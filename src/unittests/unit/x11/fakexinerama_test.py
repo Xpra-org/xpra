0c4ff117f5b257037d44900a8dc972037e999e3e
Antoine Martin <totaam@xpra.org>
2020-11-15 15:47:30 +0000

    better test coverage and silence warnings
    git-svn-id: https://xpra.org/svn/Xpra/trunk@27953 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/unittests/unit/x11/fakexinerama_test.py
+++ src/unittests/unit/x11/fakexinerama_test.py
@@ -16,10 +16,29 @@ class FakeXineramaTest(unittest.TestCase):
         assert find_libfakeXinerama()
 
     def test_config(self):
-        from xpra.x11.fakeXinerama import save_fakeXinerama_config, cleanup_fakeXinerama
-        ss = ()
-        save_fakeXinerama_config(True, "", ss)
-        cleanup_fakeXinerama()
+        from xpra.x11.fakeXinerama import save_fakeXinerama_config, cleanup_fakeXinerama, log
+        #silence warnings during tests:
+        log.warn = log.debug
+        def get_display_info(*monitors):
+            #display_name, width, height, width_mm, height_mm, \
+            #monitors, work_x, work_y, work_width, work_height = s[:11]
+            return (
+                "fake-display",
+                1920, 1080, 400, 300,
+                monitors, 0, 60, 1920, 1020,
+                )
+        monitor0 = ("plug0", 0, 0, 1920, 1080, 400, 300)
+        monitor1 = ("plug1", 1920, 0, 1920, 1080, 300, 200)
+        for ss in (
+            get_display_info(),
+            get_display_info((0, 0)),
+            get_display_info(monitor0),
+            get_display_info(monitor0, monitor1),
+            (800, 600),
+            (1, 2, 3, 4, 5),
+            ):
+            save_fakeXinerama_config(True, "", (ss, ))
+            cleanup_fakeXinerama()
 
 
 def main():
