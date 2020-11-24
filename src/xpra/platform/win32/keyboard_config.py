0f91459b438cb2b4568ee19b741cd37584c4da41
Antoine Martin <totaam@xpra.org>
2020-11-24 15:26:56 +0000

    r28013 backport: fix macos and win32 shadow server key mapping (regression from capslock fix)
    git-svn-id: https://xpra.org/svn/Xpra/tags/v4.0.x@28014 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/win32/keyboard_config.py
+++ src/xpra/platform/win32/keyboard_config.py
@@ -36,9 +36,9 @@ class KeyboardConfig(KeyboardConfigBase):
         return "win32.KeyboardConfig"
 
 
-    def do_get_keycode(self, client_keycode, keyname, pressed, modifiers, group):
+    def do_get_keycode(self, client_keycode, keyname, pressed, modifiers, keystr, group):
         keycode = KEYCODES.get(keyname, -1)
-        log("get_keycode%s=%s", (client_keycode, keyname, pressed, modifiers, group), keycode)
+        log("get_keycode%s=%s", (client_keycode, keyname, pressed, modifiers, keystr, group), keycode)
         return keycode, group
 
     def make_keymask_match(self, modifier_list, ignored_modifier_keycode=None, ignored_modifier_keynames=None):
