89cae78f4d2af9dc2062fe30268b7ac901f1bd97
Antoine Martin <totaam@xpra.org>
2020-11-24 13:09:48 +0000

    r28004 backport: fix su / ssh connection errors by re-adding ~/.xpra as a socket-dir
    git-svn-id: https://xpra.org/svn/Xpra/tags/v4.0.x@28006 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/platform/xposix/paths.py
+++ src/xpra/platform/xposix/paths.py
@@ -146,6 +146,9 @@ def do_get_socket_dirs():
         SOCKET_DIRS.append("/run/xpra")
     elif os.path.exists("/var/run"):
         SOCKET_DIRS.append("/var/run/xpra")
+    #Debian and Ubuntu often don't create a reliable XDG_RUNTIME_DIR
+    #other distros may not create one when using "su"
+    SOCKET_DIRS.append("~/.xpra")
     return SOCKET_DIRS
 
 def do_get_default_log_dirs():
