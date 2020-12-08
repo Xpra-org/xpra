d52fde6f32d7465e902f7e5fd82566bfe06892f9
Antoine Martin <totaam@xpra.org>
2020-12-08 12:46:32 +0000

    r28097 + r28099 backport: fix file-transfers with non-ascii filenames
    git-svn-id: https://xpra.org/svn/Xpra/tags/v3.0.x@28102 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/client/gobject_client_base.py
+++ src/xpra/client/gobject_client_base.py
@@ -10,7 +10,7 @@ from xpra.util import (
     nonl, sorted_nicely, print_nested_dict, envint, flatten_dict,
     disconnect_is_an_error, repr_ellipsized, DONE, first_time,
     )
-from xpra.os_util import bytestostr, get_hex_uuid, PYTHON3, POSIX, OSX
+from xpra.os_util import bytestostr, strtobytes, get_hex_uuid, PYTHON3, POSIX, OSX
 from xpra.client.client_base import XpraClientBase, EXTRA_TIMEOUT
 from xpra.exit_codes import (
     EXIT_OK, EXIT_CONNECTION_LOST, EXIT_TIMEOUT, EXIT_INTERNAL_ERROR,
@@ -404,7 +404,12 @@ class ControlXpraClient(CommandConnectClient):
     def make_hello(self):
         capabilities = GObjectXpraClient.make_hello(self)
         log("make_hello() adding command request '%s' to %s", self.command, capabilities)
-        capabilities["command_request"] = self.command
+        def b(s):
+            try:
+                return s.encode("utf8")
+            except:
+                return strtobytes(s)
+        capabilities["command_request"] = tuple(b(x) for x in self.command)
         capabilities["request"] = "command"
         return capabilities
 
