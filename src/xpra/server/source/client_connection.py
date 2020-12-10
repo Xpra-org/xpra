6aa046639c7ebc0d7bd6b14879f923e8c66444b2
Antoine Martin <totaam@xpra.org>
2020-12-10 15:07:22 +0000

    when we use send_async, ensure we don't set the 'have-more' flag so the packet does not get delayed unnecessarily
    git-svn-id: https://xpra.org/svn/Xpra/trunk@28114 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/source/client_connection.py
+++ src/xpra/server/source/client_connection.py
@@ -339,6 +339,7 @@ class ClientConnection(StubSourceMixin):
 
     def send_async(self, *parts, **kwargs):
         kwargs["synchronous"] = False
+        kwargs["will_have_more"] = False
         self.send(*parts, **kwargs)
 
 
