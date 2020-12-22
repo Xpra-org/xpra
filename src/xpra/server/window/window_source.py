1d1cd7d50f7bba754fbc5443df7317318293ff21
Antoine Martin <totaam@xpra.org>
2020-12-22 11:47:56 +0000

    r28223 backport: ensure screen refresh speed recovers more quickly
    git-svn-id: https://xpra.org/svn/Xpra/tags/v4.0.x@28225 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/window/window_source.py
+++ src/xpra/server/window/window_source.py
@@ -1362,7 +1362,7 @@ class WindowSource(WindowIconSource):
         delay = max(delay, options.get("min_delay", 0))
         delay = min(delay, options.get("max_delay", self.batch_config.max_delay))
         delay = int(delay)
-        elapsed = now-self.batch_config.last_event
+        elapsed = int(1000*(now-self.batch_config.last_event))
         if elapsed>delay:
             #batch delay has already elapsed since we last processed a screen update,
             #so we don't need to wait much longer:
