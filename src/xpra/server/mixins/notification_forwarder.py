95c24436feb8d471d43ed977f877cba275558bf6
Antoine Martin <totaam@xpra.org>
2020-12-28 11:41:56 +0000

    fix connection errors when a non-ui client is connected (ie: 'xpra _monitor')
    git-svn-id: https://xpra.org/svn/Xpra/trunk@28267 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/server/mixins/notification_forwarder.py
+++ src/xpra/server/mixins/notification_forwarder.py
@@ -52,7 +52,10 @@ class NotificationForwarder(StubServerMixin):
     def parse_hello(self, ss, _caps, send_ui):
         log("parse_hello(%s, {...}, %s) notifications_forwarder=%s", ss, send_ui, self.notifications_forwarder)
         if send_ui and self.notifications_forwarder:
-            client_notification_actions = dict((s.uuid,s.send_notifications_actions) for s in self._server_sources.values())
+            client_notification_actions = dict(
+                (s.uuid, getattr(s, "send_notifications_actions", False))
+                for s in self._server_sources.values()
+                )
             log("client_notification_actions=%s", client_notification_actions)
             self.notifications_forwarder.support_actions = any(v for v in client_notification_actions.values())
 
