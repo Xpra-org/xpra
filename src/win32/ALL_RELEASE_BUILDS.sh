674548a24a169d7234510e975e5c7453407b1063
totaam <antoine@xpra.org>
2021-02-01 16:56:02 +0700

    stop making Python2 / GTK2 builds with v3.1.x

--- src/win32/ALL_RELEASE_BUILDS.sh
+++ src/win32/ALL_RELEASE_BUILDS.sh
@@ -5,9 +5,9 @@ RUN_INSTALLER=${RUN_INSTALLER:-0}
 DO_ZIP=${DO_ZIP:-1}
 DO_MSI=${DO_MSI:-1}
 
-for PYTHON in python2 python3; do
-	for CLIENT_ONLY in 0 1; do
-		echo "********************************************************************************"
-		CLIENT_ONLY=${CLIENT_ONLY} DO_ZIP=${DO_ZIP} DO_INSTALLER=${DO_INSTALLER} RUN_INSTALLER=${RUN_INSTALLER} DO_MSI=${DO_MSI} PYTHON=$PYTHON sh win32/MINGW_BUILD.sh /silent
-	done
+#for PYTHON in python2 python3; do
+PYTHON="python3"
+for CLIENT_ONLY in 0 1; do
+	echo "********************************************************************************"
+	CLIENT_ONLY=${CLIENT_ONLY} DO_ZIP=${DO_ZIP} DO_INSTALLER=${DO_INSTALLER} RUN_INSTALLER=${RUN_INSTALLER} DO_MSI=${DO_MSI} PYTHON=$PYTHON sh win32/MINGW_BUILD.sh /silent
 done
