5286af5814ae911b4d8373e71e5289e0e4c1922d
totaam <antoine@xpra.org>
2021-02-25 12:44:35 +0700

    distro specific magic tags and add revision no to package name

--- packaging/buildah/build_debs.sh
+++ packaging/buildah/build_debs.sh
@@ -15,10 +15,28 @@ VERSION=4.1
 tar -Jxf pkgs/xpra-$VERSION.tar.xz
 pushd xpra-$VERSION
 ln -sf packaging/debian .
+
+#the control file has a few distribution specific entries
+#ie:
+# '#buster:         ,libturbojpeg0'
+#we uncomment the lines for this specific distro (by adding a new line after "#$DISTRO:"):
+#first figure out the distribution's codename:
+DEBIAN_FRONTEND=noninteractive apt-get install lsb-release perl -y
+CODENAME=`lsb_release -c | awk '{print $2}'`
+#ie: CODENAME=bionic
+perl -i.bak -pe "s/#${CODENAME}:/#${CODENAME}:\\n/g" debian/control
+
+#add revision to version number to changelog
+REVISION=`PYTHONPATH=. python -c 'from xpra.src_info import REVISION;print(REVISION)'`
+if [ "${REVISION}" != "" ]; then
+	perl -i.bak -pe "s/-/-r${REVISION}-/g" debian/changelog
+fi
+
 debuild -us -uc -b
 popd
 eval `dpkg-architecture -s`
 REPO_ARCH_PATH="repo/main/binary-$DEB_BUILD_ARCH"
 mkdir -p $REPO_ARCH_PATH
 rm xpra-build-deps*
+ls -la ./xpra*deb
 mv ./xpra*deb ./xpra*changes $REPO_ARCH_PATH
