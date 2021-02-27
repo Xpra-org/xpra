4c7ae7a6d87ae510eecc58607615c68b95c1353d
totaam <antoine@xpra.org>
2021-02-27 22:58:12 +0700

    only build packages we don't already have and fix revision replace to only replace the first line of the control file, add readme

--- packaging/buildah/build_debs.sh
+++ packaging/buildah/build_debs.sh
@@ -10,6 +10,11 @@ apt-get dist-upgrade -y
 mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
 #mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --yes' debian/control
 
+eval `dpkg-architecture -s`
+REPO_ARCH_PATH="`pwd`/repo/main/binary-$DEB_BUILD_ARCH"
+mkdir -p $REPO_ARCH_PATH
+rm xpra-build-deps*
+
 VERSION=4.1
 tar -Jxf pkgs/xpra-$VERSION.tar.xz
 pushd xpra-$VERSION
@@ -25,16 +30,23 @@ CODENAME=`lsb_release -c | awk '{print $2}'`
 perl -i.bak -pe "s/#${CODENAME}:/#${CODENAME}:\\n/g" debian/control
 
 #add revision to version number to changelog
-REVISION=`PYTHONPATH=. python -c 'from xpra.src_info import REVISION;print(REVISION)'`
-if [ "${REVISION}" != "" ]; then
-	perl -i.bak -pe "s/-/-r${REVISION}-/g" debian/changelog
+REVISION=`PYTHONPATH=. python3 -c 'from xpra.src_info import REVISION;print(REVISION)'`
+if [ "${REVISION}" == "" ]; then
+	echo "cannot build: xpra revision not found in src_info"
+	exit 1
 fi
+head -n 1 "./debian/changelog" | sed "s/-/-r${REVISION}-/g" > "debian/changelog.new"
+tail -n +2 "./debian/changelog" >> "./debian/changelog.new"
+mv "./debian/changelog.new" "./debian/changelog"
+head -n 10 "./debian/changelog"
 
-debuild -us -uc -b
+#now figure out if this packages is already in the repository:
+CHANGELOG_VERSION=`head -n 1 ."./debian/changelog" | sed 's/.*(//g' | sed 's/).*//g'`
+DEB_FILENAME="xpra-${CHANGELOG_VERSION}_$DEB_BUILD_ARCH.deb"
+MATCH=`find $REPO_ARCH_PATH/ -name "${DEB_FILENAME}" | wc -l`
+if [ "$MATCH" == "0" ]; then
+	debuild -us -uc -b
+	ls -la ../xpra*deb
+	mv ../xpra*deb ../xpra*changes $REPO_ARCH_PATH
+fi
 popd
-eval `dpkg-architecture -s`
-REPO_ARCH_PATH="repo/main/binary-$DEB_BUILD_ARCH"
-mkdir -p $REPO_ARCH_PATH
-rm xpra-build-deps*
-ls -la ./xpra*deb
-mv ./xpra*deb ./xpra*changes $REPO_ARCH_PATH
