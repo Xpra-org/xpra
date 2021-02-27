83e3aece9d3949a9137679014ba66fd32bd8846e
totaam <antoine@xpra.org>
2021-02-27 23:02:45 +0700

    superseded by buildah files and we don't support centos7 in this branch anyway

--- packaging/docker/build/centos7/build_centos7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-docker build -f Dockerfile.centos7 -t xprabuild-centos7:latest .
-docker run -it \
--e XPRA_REVISION=$( svnversion ) \
---mount type=bind,source="${PWD}/../../../",target=/home/builder/mount \
-xprabuild-centos7:latest
