615b24c9649e7eb64d4982d8fec51079dd7fc0f1
totaam <antoine@xpra.org>
2021-02-28 23:56:59 +0700

    update paths

--- packaging/MSWindows/ALL_RELEASE_BUILDS.sh
+++ packaging/MSWindows/ALL_RELEASE_BUILDS.sh
@@ -5,7 +5,9 @@ RUN_INSTALLER=${RUN_INSTALLER:-0}
 DO_ZIP=${DO_ZIP:-1}
 DO_MSI=${DO_MSI:-1}
 
+MSWINDOWS_DIR=`dirname $(readlink -f $0)`
+
 for CLIENT_ONLY in 0 1; do
 	echo "********************************************************************************"
-	CLIENT_ONLY=${CLIENT_ONLY} DO_ZIP=${DO_ZIP} DO_INSTALLER=${DO_INSTALLER} RUN_INSTALLER=${RUN_INSTALLER} DO_MSI=${DO_MSI} PYTHON=python3 sh win32/MINGW_BUILD.sh /silent
+	CLIENT_ONLY=${CLIENT_ONLY} DO_ZIP=${DO_ZIP} DO_INSTALLER=${DO_INSTALLER} RUN_INSTALLER=${RUN_INSTALLER} DO_MSI=${DO_MSI} PYTHON=python3 sh ${MSWINDOWS_DIR}/MINGW_BUILD.sh /silent
 done
