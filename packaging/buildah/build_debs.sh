385f23f2d2eea6a501ff4ae9a14a682ab7c5f649
totaam <antoine@xpra.org>
2021-03-29 22:08:12 +0700

    only build debs we have source for use new build process for xpra-html5, fix ugly path typo

--- packaging/buildah/build_debs.sh
+++ packaging/buildah/build_debs.sh
@@ -4,20 +4,21 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
-#the image should already have everything needed,
-#unless something was added to the control file
-#after the image had already been generated:
-mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
-#mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --yes' debian/control
-rm xpra-build-deps*
-
 eval `dpkg-architecture -s`
 REPO_ARCH_PATH="`pwd`/repo/main/binary-$DEB_BUILD_ARCH"
 mkdir -p $REPO_ARCH_PATH
 
+
 #find the latest version we can build:
-XPRA_TAR_XZ=`ls pkgs/xpra-* | sort -n | tail -n 1`
-if [ "${XPRA_TAR_XZ}" == "1" ]; then
+XPRA_TAR_XZ=`ls pkgs/xpra-* | grep -v html5 | sort -n | tail -n 1`
+if [ ! -z "${XPRA_TAR_XZ}" ]; then
+	#the image should already have everything needed,
+	#unless something was added to the control file
+	#after the image had already been generated:
+	mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
+	#mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --yes' debian/control
+	rm -f xpra-build-deps*
+
 	rm -fr xpra-*
 	tar -Jxf ${XPRA_TAR_XZ}
 	pushd xpra-*
@@ -44,7 +45,7 @@ if [ "${XPRA_TAR_XZ}" == "1" ]; then
 	head -n 10 "./debian/changelog"
 
 	#now figure out if this package is already in the repository:
-	CHANGELOG_VERSION=`head -n 1 ."./debian/changelog" | sed 's/.*(//g' | sed 's/).*//g'`
+	CHANGELOG_VERSION=`head -n 1 "./debian/changelog" | sed 's/.*(//g' | sed 's/).*//g'`
 	DEB_FILENAME="xpra-${CHANGELOG_VERSION}_$DEB_BUILD_ARCH.deb"
 	MATCH=`find $REPO_ARCH_PATH/ -name "${DEB_FILENAME}" | wc -l`
 	if [ "$MATCH" == "0" ]; then
@@ -57,19 +58,15 @@ fi
 
 
 XPRA_HTML5_TAR_XZ=`ls pkgs/xpra-html5-* | sort -n | tail -n 1`
-if [ "${XPRA_HTML5_TAR_XZ}" == "1" ]; then
+if [ ! -z "${XPRA_HTML5_TAR_XZ}" ]; then
 	rm -fr xpra-html5-*
 	tar -Jxf ${XPRA_HTML5_TAR_XZ}
 	pushd xpra-html5-*
 
-	#figure out if this package is already in the repository:
-	CHANGELOG_VERSION=`head -n 1 ."./debian/changelog" | sed 's/.*(//g' | sed 's/).*//g'`
-	DEB_FILENAME="xpra-html5-${CHANGELOG_VERSION}_$DEB_BUILD_ARCH.deb"
-	MATCH=`find $REPO_ARCH_PATH/ -name "${DEB_FILENAME}" | wc -l`
-	if [ "$MATCH" == "0" ]; then
-		debuild -us -uc -b
-		ls -la ../xpra-html5-*deb
-		mv ../xpra-html5-*deb ../xpra*changes $REPO_ARCH_PATH
-	fi
+	mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
+	rm -f xpra-html5-build-deps*
+
+	python3 ./setup.py deb
+	mv ./dist/xpra-html5-*.deb $REPO_ARCH_PATH
 	popd
 fi
