fe04e862f1afed0c1508ff5567875edc273d02c5
totaam <antoine@xpra.org>
2021-03-31 15:30:17 +0700

    #3063 workaround for Big Sur dylib cache mess

--- xpra/platform/darwin/__init__.py
+++ xpra/platform/darwin/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2011-2020 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2011-2021 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -45,3 +45,18 @@ def get_OSXApplication():
         macapp = GtkosxApplication.Application()
         macapp.connect("NSApplicationWillTerminate", quit_handler)
     return macapp
+
+
+#workaround for Big Sur dylib cache mess:
+#https://stackoverflow.com/a/65599706/428751
+def patch_find_library():
+    from ctypes import util  #pylint: disable=import-outside-toplevel
+    orig_util_find_library = util.find_library
+    def new_util_find_library(name):
+        res = orig_util_find_library(name)
+        if res:
+            return res
+        return '/System/Library/Frameworks/'+name+'.framework/'+name
+    util.find_library = new_util_find_library
+if os.environ.get("XPRA_OSX_PATCH_FIND_LIBRARY", "1")=="1":
+    patch_find_library()
