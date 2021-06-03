d3f339f551a8ed07d6ea6a126edc87d4b1b95f45
totaam <antoine@xpra.org>
2021-06-03 14:01:21 +0700

    build ffmpeg-xpra on Debian Stretch

--- src/packaging/debian/build.sh
+++ src/packaging/debian/build.sh
@@ -5,6 +5,10 @@ if [ "${DEBUG:-0}" == "1" ]; then
 	BASH="bash -x"
 fi
 
+lsb_release -c | grep stretch
+if [ "$?" == "0" ]; then
+	$BASH ./ffmpeg-xpra.sh
+fi
 $BASH ./libcuda1.sh
 $BASH ./libnvidia-fbc1.sh
 $BASH ./xpra-html5.sh
