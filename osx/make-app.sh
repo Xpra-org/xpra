5a2bc49e873dc1b3d4f24630ab5b8ea7898be090
totaam <antoine@xpra.org>
2021-06-11 17:12:22 +0700

    fix mysterious gi errors when calling py2app

--- osx/make-app.sh
+++ osx/make-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "${DYLD_FALLBACK_LIBRARY_PATH}" ]; then
+	export DYLD_FALLBACK_LIBRARY_PATH="${JHBUILD_PREFIX}/lib/"
+fi
+
 PYTHON="${PYTHON:=python}"
 PYTHON_MAJOR_VERSION=`$PYTHON -c 'import sys;sys.stdout.write("%s" % sys.version_info[0])'`
 PYTHON_MINOR_VERSION=`$PYTHON -c 'import sys;sys.stdout.write("%s" % sys.version_info[1])'`
