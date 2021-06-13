f0df7b4a0180fb7a8e0021dad11377e903d74f96
totaam <antoine@xpra.org>
2021-06-13 19:38:18 +0700

    use the atom to unregister the class more reliably

--- xpra/platform/win32/gl_context.py
+++ xpra/platform/win32/gl_context.py
@@ -1,9 +1,10 @@
 # This file is part of Xpra.
-# Copyright (C) 2017-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2017-2021 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from ctypes import sizeof, byref, FormatError
+from ctypes import sizeof, byref, cast, c_void_p, FormatError
+from ctypes.wintypes import LPCWSTR
 
 from xpra.client.gl.gl_check import check_PyOpenGL_support
 from xpra.platform.win32.gui import get_window_handle
@@ -13,7 +14,7 @@ from xpra.platform.win32.constants import (
     )
 from xpra.platform.win32.common import (
     GetDC, SwapBuffers, ChoosePixelFormat, DescribePixelFormat, SetPixelFormat,
-    BeginPaint, EndPaint, DestroyWindow, UnregisterClassA,
+    BeginPaint, EndPaint, DestroyWindow, UnregisterClassW,
     GetModuleHandleA, RegisterClassExA, CreateWindowExA, DefWindowProcA, WNDPROC, WNDCLASSEX
     )
 from xpra.platform.win32.glwin32 import (
@@ -114,11 +115,13 @@ class WGLContext:
         finally:
             hwnd = self.hwnd
             self.destroy()
-            if hwnd:
-                if not DestroyWindow(hwnd):
-                    log.warn("Warning: failed to destroy temporary OpenGL test window")
-            if not UnregisterClassA(classname, hInst):
+            if hwnd and not DestroyWindow(hwnd):
+                log.warn("Warning: failed to destroy temporary OpenGL test window")
+            latom = c_void_p(reg_atom)
+            if not UnregisterClassW(cast(latom, LPCWSTR), hInst):
                 log.warn("Warning: failed to unregister class for OpenGL test window")
+                log.warn(" for class %r and module handle %#x:", classname, hInst or 0)
+                log.warn(" '%s'", FormatError())
 
     def get_bit_depth(self):
         return 0
