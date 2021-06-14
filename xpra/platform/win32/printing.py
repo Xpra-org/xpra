3a5279c86641284b6a6f7ccad6f568622e5de701
totaam <antoine@xpra.org>
2021-06-14 23:01:55 +0700

    fix warning message format

--- xpra/platform/win32/printing.py
+++ xpra/platform/win32/printing.py
@@ -184,7 +184,7 @@ def get_printers():
                 eprinters.append(name)
             log("%s printers: %s", penum, eprinters)
         except Exception as e:
-            log.warn("Warning: failed to query %s printers: %s", penum)
+            log.warn("Warning: failed to query %s printers:", penum)
             log.warn(" %s", e)
             log("query error", exc_info=True)
             del e
