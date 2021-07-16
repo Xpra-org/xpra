5b47e9068ddebd988b6035a8cf65784e446c1f3c
totaam <antoine@xpra.org>
2021-07-16 23:51:20 +0700

    fix some pylint warnings: define dummy functions to import

--- xpra/platform/autostart.py
+++ xpra/platform/autostart.py
@@ -3,6 +3,13 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+def set_autostart(_enabled):
+    pass
+
+def get_status():
+    return ""
+
+
 from xpra.platform import platform_import
 platform_import(globals(), "autostart", False,
                 "set_autostart", "get_status")
