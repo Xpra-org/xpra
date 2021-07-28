910f18a651644f0c3f163a64c9652ead826dc8f4
totaam <antoine@xpra.org>
2021-07-28 11:28:12 +0700

    only xpra displays can be DEAD

--- xpra/scripts/main.py
+++ xpra/scripts/main.py
@@ -3555,12 +3555,15 @@ def get_display_info(display):
         display_info.update(wminfo)
         mode = wminfo.get("xpra-server-mode", "")
         #seamless servers and non-xpra servers should have a window manager:
-        if (not mode or mode.find("seamless")>=0) and not wminfo.get("_NET_SUPPORTING_WM_CHECK"):
+        if (mode.find("seamless")>=0) and not wminfo.get("_NET_SUPPORTING_WM_CHECK"):
             display_info["state"] = "DEAD"
-        #check if the xpra server process still exists:
-        pid = wminfo.get("xpra-server-pid")
-        if pid and os.path.exists("/proc") and not os.path.exists("/proc/%s" % pid):
-            display_info["state"] = "DEAD"
+        else:
+            wmname = wminfo.get("wmname")
+            if wmname and wmname.find("xpra")>=0:
+                #check if the xpra server process still exists:
+                pid = wminfo.get("xpra-server-pid")
+                if pid and os.path.exists("/proc") and not os.path.exists("/proc/%s" % pid):
+                    display_info["state"] = "DEAD"
     else:
         display_info.update({"state" : "UNKNOWN"})
     return display_info
