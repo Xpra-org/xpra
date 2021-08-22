7fde123df900ebe4ff678bff5bbe9ee389b3f810
totaam <antoine@xpra.org>
2021-08-22 21:32:15 +0700

    #3239 find_lib expects the full lib name

--- xpra/x11/fakeXinerama.py
+++ xpra/x11/fakeXinerama.py
@@ -46,7 +46,7 @@ def find_libfakeXinerama():
             log("find_libfakeXinerama()", exc_info=True)
             log.error("Error: cannot launch ldconfig -p to locate libfakeXinerama:")
             log.error(" %s", e)
-    return find_lib(libname)
+    return find_lib("libfakeXinerama.so.1")
 
 current_xinerama_config = None
 
