eea3a9975cb702f41e32e3ce1071e0ce48418e95
totaam <antoine@xpra.org>
2021-08-23 20:40:17 +0700

    #3242 remove deprecated AVFMT_FLAG_MP4A_LATM

--- xpra/codecs/enc_ffmpeg/encoder.pyx
+++ xpra/codecs/enc_ffmpeg/encoder.pyx
@@ -483,7 +483,6 @@ cdef extern from "libavformat/avformat.h":
     int AVFMT_FLAG_DISCARD_CORRUPT  #Discard frames marked corrupted
     int AVFMT_FLAG_FLUSH_PACKETS    #Flush the AVIOContext every packet
     int AVFMT_FLAG_BITEXACT
-    int AVFMT_FLAG_MP4A_LATM        #Enable RTP MP4A-LATM payload
     int AVFMT_FLAG_SORT_DTS         #try to interleave outputted packets by dts (using this flag can slow demuxing down)
     int AVFMT_FLAG_PRIV_OPT         #Enable use of private options by delaying codec open (this could be made default once all code is converted)
     int AVFMT_FLAG_KEEP_SIDE_DATA   #Don't merge side data but keep it separate.
@@ -694,7 +693,6 @@ FMT_FLAGS = {
     AVFMT_FLAG_DISCARD_CORRUPT : "DISCARD_CORRUPT",
     AVFMT_FLAG_FLUSH_PACKETS   : "FLUSH_PACKETS",
     AVFMT_FLAG_BITEXACT        : "BITEXACT",
-    AVFMT_FLAG_MP4A_LATM       : "MP4A_LATM",
     AVFMT_FLAG_SORT_DTS        : "SORT_DTS",
     AVFMT_FLAG_PRIV_OPT        : "PRIV_OPT",
     AVFMT_FLAG_KEEP_SIDE_DATA  : "KEEP_SIDE_DATA",
