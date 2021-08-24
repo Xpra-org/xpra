2f25d996a62af8b8278e8a52cf68e2c1d3cf5b5e
totaam <antoine@xpra.org>
2021-08-24 21:10:59 +0700

    split large main function

--- xpra/platform/netdev_query.py
+++ xpra/platform/netdev_query.py
@@ -1,8 +1,9 @@
 # This file is part of Xpra.
-# Copyright (C) 2017-2020 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2017-2021 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import socket
 
 def get_interface_info(*_args):
     return {}
@@ -18,13 +19,44 @@ platform_import(globals(), "netdev_query", False,
                 )
 
 
-def main():
-    import sys
-    import socket
-
+def print_address(iface, addr, defs):
     from xpra.os_util import POSIX
     from xpra.util import print_nested_dict
-    from xpra.net.net_util import import_netifaces, get_interfaces, if_nametoindex
+    for d in defs:
+        ip = d.get("addr")
+        if ip:
+            stype = {
+                socket.AF_INET  : "IPv4",
+                socket.AF_INET6 : "IPv6",
+                }[addr]
+            print(" * %s:     %s" % (stype, ip))
+            if POSIX:
+                from xpra.net.socket_util import create_tcp_socket
+                try:
+                    sock = create_tcp_socket(ip, 0)
+                    sockfd = sock.fileno()
+                    info = get_interface_info(sockfd, iface)
+                    if info:
+                        print_nested_dict(info, prefix="    ", lchar="-")
+                finally:
+                    sock.close()
+
+def print_iface(iface):
+    from xpra.os_util import POSIX
+    from xpra.net.net_util import import_netifaces
+    netifaces = import_netifaces()
+    addresses = netifaces.ifaddresses(iface)     #@UndefinedVariable pylint: disable=no-member
+    for addr, defs in addresses.items():
+        if addr in (socket.AF_INET, socket.AF_INET6):
+            print_address(iface, addr, defs)
+    if not POSIX:
+        info = get_interface_info(0, iface)
+        if info:
+            print("  %s" % info)
+
+def main():
+    import sys
+    from xpra.net.net_util import get_interfaces, if_nametoindex
     from xpra.platform import program_context
     from xpra.log import Logger, enable_color, add_debug_category, enable_debug_for
     log = Logger("network")
@@ -37,37 +69,12 @@ def main():
             log.enable_debug()
 
         print("Network interfaces found:")
-        netifaces = import_netifaces()
         for iface in get_interfaces():
             if if_nametoindex:
                 print("* %s (index=%s)" % (iface.ljust(20), if_nametoindex(iface)))
             else:
                 print("* %s" % iface)
-            addresses = netifaces.ifaddresses(iface)     #@UndefinedVariable pylint: disable=no-member
-            for addr, defs in addresses.items():
-                if addr in (socket.AF_INET, socket.AF_INET6):
-                    for d in defs:
-                        ip = d.get("addr")
-                        if ip:
-                            stype = {
-                                socket.AF_INET  : "IPv4",
-                                socket.AF_INET6 : "IPv6",
-                                }[addr]
-                            print(" * %s:     %s" % (stype, ip))
-                            if POSIX:
-                                from xpra.net.socket_util import create_tcp_socket
-                                try:
-                                    sock = create_tcp_socket(ip, 0)
-                                    sockfd = sock.fileno()
-                                    info = get_interface_info(sockfd, iface)
-                                    if info:
-                                        print_nested_dict(info, prefix="    ", lchar="-")
-                                finally:
-                                    sock.close()
-            if not POSIX:
-                info = get_interface_info(0, iface)
-                if info:
-                    print("  %s" % info)
+            print_iface(iface)
 
 
 if __name__ == "__main__":
