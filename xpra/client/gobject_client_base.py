19897adb2eba869538228c42724ed029ef52a01b
totaam <antoine@xpra.org>
2021-10-12 12:19:14 +0700

    #3300 we now often get the correct type (a string)
    thanks to the switch to rencodeplus

--- xpra/client/gobject_client_base.py
+++ xpra/client/gobject_client_base.py
@@ -239,7 +239,7 @@ class ScreenshotXpraClient(CommandConnectClient):
 
     def _process_screenshot(self, packet):
         (w, h, encoding, _, img_data) = packet[1:6]
-        assert encoding==b"png", "expected png screenshot data but got %s" % bytestostr(encoding)
+        assert bytestostr(encoding)=="png", "expected png screenshot data but got %s" % bytestostr(encoding)
         if not img_data:
             self.warn_and_quit(EXIT_OK,
                                "screenshot is empty and has not been saved (maybe there are no windows or they are not currently shown)")
