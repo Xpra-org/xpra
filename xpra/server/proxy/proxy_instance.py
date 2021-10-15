a9f5e5e99681d7760d5516bc4004a80a83c6980e
totaam <antoine@xpra.org>
2021-10-15 22:32:44 +0700

    rgb pass-through fixes
    * we can only re-compress rgb pixel data so let other encodings through
    * do allow video compression of 30-bit and 15-bit rgb,
    * leave rgb encoding unmodified (stripping alpha does not change the encoding)

--- xpra/server/proxy/proxy_instance.py
+++ xpra/server/proxy/proxy_instance.py
@@ -626,10 +626,14 @@ class ProxyInstance:
     def process_draw(self, packet):
         wid, x, y, width, height, encoding, pixels, _, rowstride, client_options = packet[1:11]
         encoding = bytestostr(encoding)
-        #never modify mmap packets
+        #never modify mmap or scroll packets:
         if encoding in ("mmap", "scroll"):
             return True
-
+        #we can only use video encoders on RGB data:
+        if encoding not in ("rgb24", "rgb32", "r210", "BGR565"):
+            #this prevents compression and inlining of pixel data:
+            packet[7] = Compressed("%s pixels" % encoding, pixels)
+            return True
         client_options = typedict(client_options)
         #we have a proxy video packet:
         rgb_format = client_options.strget("rgb_format", "")
@@ -652,8 +656,9 @@ class ProxyInstance:
                 if Xindex>=0 and len(rgb_format)==4:
                     #force clear alpha (which may be garbage):
                     newdata = bytearray(pixels)
+                    c = chr(255)
                     for i in range(len(pixels)/4):
-                        newdata[i*4+Xindex] = chr(255)
+                        newdata[i*4+Xindex] = c
                     packet[9] = client_options.intget("rowstride", 0)
                     cdata = bytes(newdata)
                 else:
@@ -665,10 +670,10 @@ class ProxyInstance:
                 new_client_options = client_options
             wrapped = Compressed("%s pixels" % encoding, cdata)
             #rgb32 is always supported by all clients:
-            return send_updated("rgb32", wrapped, new_client_options)
+            return send_updated(encoding, wrapped, new_client_options)
 
         proxy_video = client_options.boolget("proxy", False)
-        if PASSTHROUGH_RGB and (encoding in ("rgb32", "rgb24") or proxy_video):
+        if PASSTHROUGH_RGB:
             #we are dealing with rgb data, so we can pass it through:
             return passthrough(proxy_video)
         if not self.video_encoder_types or not client_options or not proxy_video:
