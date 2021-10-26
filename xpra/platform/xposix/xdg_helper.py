280f95a48dd22763242c27c424640d5e4540daf1
totaam <antoine@xpra.org>
2021-10-26 17:08:24 +0700

    #3326 faster icon lookup
    * use the builtin python xdg cache
    * disable globs by default
    * increase cache timeout whilst we're using it

--- xpra/platform/xposix/xdg_helper.py
+++ xpra/platform/xposix/xdg_helper.py
@@ -16,14 +16,18 @@ from time import monotonic
 from typing import Generator as generator       #@UnresolvedImport, @UnusedImport
 
 from xpra.util import envbool, print_nested_dict, first_time, engs
-from xpra.os_util import load_binary_file, OSEnvContext
+from xpra.os_util import DummyContextManager, OSEnvContext, get_saved_env
 from xpra.codecs import icon_util
 from xpra.platform.paths import get_icon_filename
 from xpra.log import Logger, add_debug_category
 
 log = Logger("exec", "menu")
 
-LOAD_GLOB = envbool("XPRA_XDG_LOAD_GLOB", True)
+LOAD_FROM_RESOURCES = envbool("XPRA_XDG_LOAD_FROM_RESOURCES", True)
+LOAD_FROM_PIXMAPS = envbool("XPRA_XDG_LOAD_FROM_PIXMAPS", True)
+LOAD_FROM_THEME = envbool("XPRA_XDG_LOAD_FROM_THEME", True)
+LOAD_GLOB = envbool("XPRA_XDG_LOAD_GLOB", False)
+
 EXPORT_ICONS = envbool("XPRA_XDG_EXPORT_ICONS", True)
 DEBUG_COMMANDS = os.environ.get("XPRA_XDG_DEBUG_COMMANDS", "").split(",")
 EXPORT_TERMINAL_APPLICATIONS = envbool("XPRA_XDG_EXPORT_TERMINAL_APPLICATIONS", False)
@@ -63,58 +67,148 @@ def export(entry, properties):
             l("error on %s", entry, exc_info=True)
             log.error("Error parsing '%s': %s", prop, e)
     l("properties(%s)=%s", name, props)
-    #load icon binary data:
-    icon = props.get("Icon")
-    icondata = load_icon_from_theme(icon)
-    if icondata:
-        bdata, ext = icondata
-        props["IconData"] = bdata
-        props["IconType"] = ext
+    load_entry_icon(props)
     return props
 
 
-def load_icon_from_theme(icon_name, theme=None):
-    if not EXPORT_ICONS or not icon_name:
-        return None
-    from xdg import IconTheme
-    filename = IconTheme.getIconPath(icon_name, theme=theme)
-    if not filename:
-        return None
-    return icon_util.load_icon_from_file(filename)
+MAX_THEMES = 2
+IconTheme = Config = themes = None
+IconLoadingContext = DummyContextManager
+if LOAD_FROM_THEME:
+    try:
+        from xdg import IconTheme, Config
+    except ImportError:
+        log("python xdg is missing", exc_info=True)
+    else:
+        class KeepCacheLoadingContext():
+            __slots__ = ("cache_time")
+            def __enter__(self):
+                self.cache_time = Config.cache_time
+                Config.cache_time = 9999
+            def __exit__(self, *_args):
+                if self.cache_time!=9999:
+                    Config.cache_time = self.cache_time
+            def __repr__(self):
+                return "KeepCacheLoadingContext"
+        IconLoadingContext = KeepCacheLoadingContext
+        def init_themes():
+            if not hasattr(IconTheme, "__get_themes"):
+                return
+            global themes
+            themes = {}
+            def addtheme(name):
+                if not name or name in themes or len(themes)>=MAX_THEMES:
+                    return
+                for theme in IconTheme.__get_themes(name):
+                    if theme and theme.name not in themes:
+                        themes[theme.name] = theme
+            addtheme(Config.icon_theme)
+            addtheme(get_saved_env().get("XDG_MENU_PREFIX"))
+            addtheme(get_saved_env().get("XDG_SESSION_DESKTOP"))
+            if len(themes)>MAX_THEMES:
+                for x in glob.glob("%s/share/icons/*/index.theme" % sys.prefix):
+                    parts = x.split(os.path.sep)
+                    name = parts[-2]
+                    addtheme(name)
+                    if len(themes)>MAX_THEMES:
+                        break
+            log("icon themes=%s", themes)
+        init_themes()
 
-def get_xdg_icon_dirs():
-    if not LOAD_GLOB or not EXPORT_ICONS:
-        return None
-    #doesn't work with IconTheme.getIconPath,
-    #so do it the hard way:
-    from xdg import IconTheme
-    return getattr(IconTheme, "icondirs", [])
+EXTENSIONS = ("png", "svg", "xpm")
 
-def load_glob_icon(submenu_data, main_dirname="categories"):
-    if not LOAD_GLOB or not EXPORT_ICONS:
-        return None
-    icondirs = get_xdg_icon_dirs()
-    if not icondirs:
-        return None
+
+def load_entry_icon(props):
+    #load icon binary data
+    names = []
     for x in ("Icon", "Name", "GenericName"):
-        name = submenu_data.get(x)
-        if name:
-            v = find_icon((main_dirname, "*"), icondirs, name)
-            if v:
-                return v
+        name = props.get(x)
+        if name and name not in names:
+            names.append(name)
+            continue
+    for x in ("Exec", "TryExec"):
+        cmd = props.get(x)
+        if cmd and not cmd.endswith(os.path.sep):
+            cmd = os.path.basename(cmd)
+            if cmd not in names:
+                names.append(cmd)
+    filename = find_icon(*names)
+    icondata = None
+    if filename:
+        icondata = icon_util.load_icon_from_file(filename)
+        if icondata:
+            bdata, ext = icondata
+            props["IconData"] = bdata
+            props["IconType"] = ext
+    if not icondata:
+        log("no icon found for %s from %s", names, props)
+    return props
+
+
+def find_icon(*names):
+    if not EXPORT_ICONS:
+        return None
+    return find_resources_icon(*names) or \
+        find_pixmap_icon(*names) or \
+        find_theme_icon(*names) or \
+        find_glob_icon("apps", *names)
+
+def find_resources_icon(*names):
+    if not LOAD_FROM_RESOURCES:
+        return None
+    #loads the icon from our own list of icons:
+    for name in names:
+        fn = get_icon_filename(name)
+        if fn and os.path.exists(fn):
+            return fn
     return None
 
-def find_icon(dirnames, icondirs, name):
-    extensions = ("png", "svg", "xpm")
+def find_pixmap_icon(*names):
+    if not LOAD_FROM_PIXMAPS:
+        return None
+    pixmaps_dir = "%s/share/pixmaps" % sys.prefix
+    pixmaps_dirs = (pixmaps_dir, os.path.join(pixmaps_dir, "comps"))
+    for d in pixmaps_dirs:
+        if not os.path.exists(d) or not os.path.isdir(d):
+            return None
+        for name in names:
+            for ext in EXTENSIONS:
+                fn = os.path.join(d, "%s.%s" % (name, ext))
+                if fn and os.path.exists(fn):
+                    return fn
+    return None
+
+def find_theme_icon(*names):
+    if not LOAD_FROM_THEME:
+        return None
+    global IconTheme, Config, themes
+    if not (IconTheme and Config and themes):
+        return None
+    size = Config.icon_size
+    #log.info("IconTheme.LookupIcon%s", (icon_name, size, themes.keys(), ("png", "svg", "xpm")))
+    for name in names:
+        for theme in themes.values():
+            fn = IconTheme.LookupIcon(name, size, theme=theme, extensions=EXTENSIONS)
+            if fn and os.path.exists(fn):
+                return fn
+    return None
+
+def find_glob_icon(*names, category="categories"):
+    if not LOAD_GLOB:
+        return None
+    icondirs = getattr(IconTheme, "icondirs", [])
+    if not icondirs:
+        return None
+    dirnames = (category, )
     pathnames = []
-    noext = os.path.splitext(name)[0]
-    for dn in dirnames:
-        for d in icondirs:
-            for ext in extensions:
-                pathnames += [
-                    os.path.join(d, "*", "*", dn, "%s.%s" % (noext, ext)),
-                    os.path.join(d, "*", dn, "*", "%s.%s" % (noext, ext)),
-                    ]
+    for name in names:
+        for dn in dirnames:
+            for d in icondirs:
+                for ext in EXTENSIONS:
+                    pathnames += [
+                        os.path.join(d, "*", "*", dn, "%s.%s" % (name, ext)),
+                        os.path.join(d, "*", dn, "*", "%s.%s" % (name, ext)),
+                        ]
     for pathname in pathnames:
         filenames = glob.glob(pathname)
         log("glob(%s) matches %i filenames", pathname, len(filenames))
@@ -157,7 +251,7 @@ def load_xdg_entry(de):
     icondata = props.get("IconData")
     if not icondata:
         #try harder:
-        icondata = load_glob_icon(de, "apps")
+        icondata = find_glob_icon(de, category="apps")
         if icondata:
             bdata, ext = icondata
             props["IconData"] = bdata
@@ -173,7 +267,7 @@ def load_xdg_menu(submenu):
     icondata = submenu_data.get("IconData")
     if not icondata:
         #try harder:
-        icondata = load_glob_icon(submenu_data, "categories")
+        icondata = find_glob_icon(submenu_data, category="categories")
         if icondata:
             bdata, ext = icondata
             submenu_data["IconData"] = bdata
@@ -219,7 +313,8 @@ def remove_icons(menu_data):
 def load_xdg_menu_data():
     icon_util.large_icons.clear()
     start = monotonic()
-    xdg_menu_data = do_load_xdg_menu_data()
+    with IconLoadingContext():
+        xdg_menu_data = do_load_xdg_menu_data()
     end = monotonic()
     if xdg_menu_data:
         l = sum(len(x) for x in xdg_menu_data.values())
@@ -339,8 +434,10 @@ def do_load_xdg_menu_data():
 
 def load_desktop_sessions():
     xsessions_dir = "%s/share/xsessions" % sys.prefix
+    if not os.path.exists(xsessions_dir) or not os.path.isdir(xsessions_dir):
+        return {}
     xsessions = {}
-    if os.path.exists(xsessions_dir):
+    with IconLoadingContext():
         from xdg.DesktopEntry import DesktopEntry
         for f in os.listdir(xsessions_dir):
             filename = os.path.join(xsessions_dir, f)
@@ -349,18 +446,20 @@ def load_desktop_sessions():
                 entry = load_xdg_entry(de)
                 if not entry:
                     continue
-                if getattr(entry, "IconData", None) is None:
-                    names = get_icon_names_for_session(de.getName().lower())
-                    v = find_session_icon(*names)
+                name = de.getName()
+                if not entry.get("IconData"):
+                    names = get_icon_names_for_session(name.lower())
+                    v = find_icon(*names)
                     if v:
                         entry["IconData"] = v[0]
                         entry["IconType"] = v[1]
-                xsessions[de.getName()] = entry
+                xsessions[name] = entry
             except Exception as e:
                 log("load_desktop_sessions(%s)", remove_icons, exc_info=True)
                 log.error("Error loading desktop entry '%s':", filename)
                 log.error(" %s", e)
     return xsessions
+
 
 def get_icon_names_for_session(name):
     ALIASES = {
@@ -368,28 +467,15 @@ def get_icon_names_for_session(name):
         "xfce"      : ["org.xfce.xfdesktop", ]
         }
     names = [name]+ALIASES.get(name, [])
-    for split in (" on ", " session", " classic"):
+    for split in (" on ", " session", "-session", " classic"):
         if name.find(split)>0:     #ie: "gnome on xorg"
             short_name = name.split(split)[0]
-            names += [short_name] + ALIASES.get(short_name, [])   # -> "gnome"
+            names += [
+                short_name,
+                "%s-session" % short_name,
+                "%s-desktop" % short_name,
+                ] + ALIASES.get(short_name, [])   # -> "gnome"
     return names
-
-def find_session_icon(*names):
-    icondirs = None
-    for name in names:
-        fn = get_icon_filename(name)
-        if fn:
-            data = load_binary_file(fn)
-            if data:
-                return data, os.path.splitext(fn)[1].lstrip(".")
-        if icondirs is None:
-            icondirs = get_xdg_icon_dirs()
-        if icondirs:
-            #v = find_icon(("categories", "apps", "places", "*"), icondirs, name)
-            v = find_icon(("*", ), icondirs, name)
-            if v:
-                return v
-    return None
 
 
 def main():
