5fda52708a0c68f040cba09323383e19cffc13c3
totaam <antoine@xpra.org>
2021-11-02 22:57:46 +0700

    #3340 use the new unprefixed codesign keyname

--- osx/make-all.sh
+++ osx/make-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${CODESIGN_KEYNAME}" ]; then
-	export CODESIGN_KEYNAME="Antoine Martin"
+	export CODESIGN_KEYNAME="xpra.org"
 fi
 
 ./make-app.sh && ./sign-app.sh && ./make-DMG.sh && ./make-PKG.sh
