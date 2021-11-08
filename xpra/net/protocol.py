18afc6020b12d2cc09ad2b88f1153c3cd478cdc7
totaam <antoine@xpra.org>
2021-11-08 09:50:29 +0700

    #3348 rencode can raise IndexError when failing to parse

--- xpra/net/protocol.py
+++ xpra/net/protocol.py
@@ -1005,7 +1005,7 @@ class Protocol:
                 except InvalidPacketEncodingException as e:
                     self.invalid("invalid packet encoding: %s" % e, data)
                     return
-                except (ValueError, TypeError) as e:
+                except (ValueError, TypeError, IndexError) as e:
                     etype = packet_encoding.get_packet_encoding_type(protocol_flags)
                     log.error("Error parsing %s packet:", etype)
                     log.error(" %s", e)
