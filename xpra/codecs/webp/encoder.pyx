25b8f1dc25c27bea9272daf9e1c2676219f2648c
totaam <antoine@xpra.org>
2021-11-09 16:24:03 +0700

    #3337 use more conservative default image hint and preset
    we can use more specialized ones if we have the content type hint

--- xpra/codecs/webp/encoder.pyx
+++ xpra/codecs/webp/encoder.pyx
@@ -319,8 +319,8 @@ CONTENT_TYPE_HINT = {
     "picture"   : WEBP_HINT_PICTURE,
     }
 
-cdef WebPImageHint DEFAULT_IMAGE_HINT = HINT_NAME_TO_CONSTANT.get(os.environ.get("XPRA_WEBP_IMAGE_HINT", "graph").lower(), WEBP_HINT_GRAPH)
-cdef WebPPreset DEFAULT_PRESET = PRESET_NAME_TO_CONSTANT.get(os.environ.get("XPRA_WEBP_PRESET", "text").lower(), WEBP_PRESET_TEXT)
+cdef WebPImageHint DEFAULT_IMAGE_HINT = HINT_NAME_TO_CONSTANT.get(os.environ.get("XPRA_WEBP_IMAGE_HINT", "graph").lower(), WEBP_HINT_DEFAULT)
+cdef WebPPreset DEFAULT_PRESET = PRESET_NAME_TO_CONSTANT.get(os.environ.get("XPRA_WEBP_PRESET", "text").lower(), WEBP_PRESET_DEFAULT)
 cdef WebPPreset PRESET_SMALL = PRESET_NAME_TO_CONSTANT.get(os.environ.get("XPRA_WEBP_PRESET_SMALL", "icon").lower(), WEBP_PRESET_ICON)
 
 
