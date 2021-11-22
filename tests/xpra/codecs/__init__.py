0b68db8677d63c417b79da45dc927b0295f750e1
totaam <antoine@xpra.org>
2021-11-22 22:05:53 +0700

    add codec benchmark tool

---
+++ tests/xpra/codecs/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# This file is part of Xpra.
+# Copyright (C) 2021 Antoine Martin <antoine@xpra.org>
---
+++ tests/xpra/codecs/benchmark.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# This file is part of Xpra.
+# Copyright (C) 2021 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import sys
+from time import monotonic
+
+from xpra.util import csv
+from xpra.codecs.image_wrapper import ImageWrapper
+
+N = 10
+
+def main(argv):
+    assert len(argv)>1, "specify images to benchmark"
+    from xpra.codecs.loader import load_codecs, get_codec
+    loaded = load_codecs(encoders=True, decoders=False, csc=False, video=False)
+    print("loaded: %s" % (loaded,))
+    for codec in loaded:
+        print("%s : %s" % (codec, get_codec(codec)))
+
+    from PIL import Image
+    for f in argv[1:]:
+        img = Image.open(f)
+        pixel_format = "RGBA"
+        if img.mode!=pixel_format:
+            pixel_format = "RGB"
+            img = img.convert(pixel_format)
+        w, h = img.size
+        rgb_data = img.tobytes("raw")
+        stride = w * len(pixel_format)
+        image = ImageWrapper(0, 0, w, h,
+                             rgb_data, pixel_format, 32, stride,
+                             planes=ImageWrapper.PACKED, thread_safe=True)
+        print()
+        print("%s : %s : %s" % (f, img, image))
+        for warmup in (True, False):
+            for codec in loaded:
+                mod = get_codec(codec)
+                encodings = mod.get_encodings()
+                if not warmup:
+                    print("%s : %s" % (codec, csv(encodings)))
+                #print("%s" % (dir(mod), ))
+                for e in encodings:
+                    start = monotonic()
+                    sizes = []
+                    n = 1 if warmup else N
+                    for _ in range(n):
+                        r = mod.encode(e, image)
+                        cdata = r[1]
+                        sizes.append(len(cdata))
+                    end = monotonic()
+                    if not warmup:
+                        cratio = w*h*len(pixel_format) * N / sum(sizes)
+                        mps = w*h*len(pixel_format) / (end-start)
+                        print("%-16s : %3i%%    -    %i MPixels/s" % (e, cratio, mps//1024//1024))
+
+if __name__ == '__main__':
+    main(sys.argv)
