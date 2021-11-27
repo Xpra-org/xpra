1c92f205afe655716e1180ac0166e4d5944d2433
totaam <antoine@xpra.org>
2021-11-27 15:58:10 +0700

    #3328 add window information tool

---
+++ tests/xpra/x11/xres_get_pid.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# This file is part of Xpra.
+# Copyright (C) 2016 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import sys
+
+
+def main(args):
+    from xpra.x11.bindings.posix_display_source import init_posix_display_source
+    init_posix_display_source()
+    from xpra.x11.bindings.res_bindings import ResBindings
+    res = ResBindings()
+    for x in args[1:]:
+        if x.startswith("0x"):
+            w = int(x[2:], 16)
+        else:
+            w = int(x)
+        pid = res.get_pid(w)
+        cmdline = ""
+        if pid:
+            from xpra.os_util import load_binary_file
+            cmdline = (load_binary_file("/proc/%i/cmdline" % pid) or b"").decode()
+        print("pid(%#x)=%s      %s" % (w, pid, cmdline))
+
+if __name__ == "__main__":
+    main(sys.argv)
