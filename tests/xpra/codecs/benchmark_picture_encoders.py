6782b3fda4a8ac2d9409e7b6cd348591bd9fcbc3
totaam <antoine@xpra.org>
2021-12-04 18:43:33 +0700

    generic picture encoder benchmark

--- tests/xpra/codecs/benchmark_picture_encoders.py
+++ tests/xpra/codecs/benchmark_picture_encoders.py
@@ -10,20 +10,22 @@ from time import monotonic
 from xpra.codecs.image_wrapper import ImageWrapper
 
 N = 10
+CODECS = ("enc_rgb", "enc_pillow", "enc_spng", "enc_webp", "enc_jpeg", "enc_nvjpeg")
 
-def main(argv):
-    assert len(argv)>1, "specify images to benchmark"
+def main(fmt="png", files=()):
+    assert len(files)>1, "specify images to benchmark"
     from xpra.net import compression
     compression.init_all()
     from xpra.codecs.loader import load_codec, get_codec
-
-    FORMAT = "png"
-    CODECS = ("enc_spng", "enc_pillow")
+    encoders = []
     for codec in CODECS:
         load_codec(codec)
+        enc = get_codec(codec)
+        if enc and fmt in enc.get_encodings():
+            encoders.append(enc)
 
     from PIL import Image
-    for f in argv[1:]:
+    for f in files:
         img = Image.open(f)
         if img.mode not in ("RGBA", "RGB"):
             img = img.convert("RGB")
@@ -36,18 +38,19 @@ def main(argv):
         image = ImageWrapper(0, 0, w, h,
                              rgb_data, pixel_format, len(pixel_format)*8, stride,
                              planes=ImageWrapper.PACKED, thread_safe=True)
-        for codec in CODECS:
-            enc = get_codec(codec)
+        for enc in encoders:
             start = monotonic()
             for _ in range(N):
                 try:
-                    r = enc.encode(FORMAT, image)
+                    r = enc.encode(fmt, image)
                 except Exception:
-                    print("error on %s.%s" % (enc.get_type(), enc.encode))
+                    print("error on %s %s" % (enc.get_type(), enc.encode))
                     raise
                 if not r:
-                    print("Error: no data")
+                    print("Error: no data for %s %s" % (enc.get_type(), enc.encode))
                     break
+            if not r:
+                continue
             end = monotonic()
             print("%-10s   :    %.1f MPixels/s" % (enc.get_type(), w*h*N/(end-start)/1024/1024))
             cdata = r[1]
@@ -58,4 +61,10 @@ def main(argv):
             #img.show()
 
 if __name__ == '__main__':
-    main(sys.argv)
+    assert len(sys.argv)>1
+    files = sys.argv[1:]
+    fmt = "png"
+    if files[0] in ("png", "webp", "jpeg", "rgb24", "rgb32"):
+        fmt = files[0]
+        files = files[1:]
+    main(fmt, files)
