c1196cfc61b3946a47e42dc92a666bfed9b6fec7
totaam <antoine@xpra.org>
2022-01-20 17:02:04 +0700

    #3435 check blue image, tolerate minor rounding differences

--- tests/unittests/unit/codecs/csc_colorspace_test.py
+++ tests/unittests/unit/codecs/csc_colorspace_test.py
@@ -5,10 +5,25 @@
 # later version. See the file COPYING for details.
 
 import unittest
+import binascii
 
 from xpra.os_util import hexstr, memoryview_to_bytes
 from xpra.codecs import loader
 from xpra.codecs.codec_checks import make_test_image
+
+
+def h2b(s):
+    return binascii.unhexlify(s)
+
+def cmpp(p1, p2):
+    #compare planes, tolerate a rounding difference of 1
+    l = min(len(p1), len(p2))
+    for i in range(l):
+        v1 = p1[i]
+        v2 = p2[i]
+        if abs(v2-v1)>1:
+            return False
+    return True
 
 
 class Test_CSC_Colorspace(unittest.TestCase):
@@ -16,10 +31,10 @@ class Test_CSC_Colorspace(unittest.TestCase):
     def _test_csc(self, mod,
                  width=16, height=16,
                  in_csc="BGRX", out_csc="YUV420P",
-                 pixel=b"\0\0\0\0", expected=(
-                     b'\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10',
-                     b'\x80\x80\x80\x80\x80\x80\x80\x80',
-                     b'\x80\x80\x80\x80\x80\x80\x80\x80',
+                 pixel="00000000", expected=(
+                     '10101010101010101010101010101010',
+                     '8080808080808080',
+                     '8080808080808080',
                      )):
         loader.load_codec(mod)
         csc_mod = loader.get_codec(mod)
@@ -35,21 +50,22 @@ class Test_CSC_Colorspace(unittest.TestCase):
                          width, height, out_csc)
         image = make_test_image(in_csc, width, height)
         size = image.get_rowstride()//4*image.get_height()
-        bgrx = pixel*size
+        bgrx = h2b(pixel)*size
         image.set_pixels(bgrx)
         out_image = csc.convert_image(image)
         csc.clean()
         assert out_image.get_planes()>=len(expected)
         #now verify the value for each plane specified:
-        for i, v in enumerate(expected):
+        for i, v_str in enumerate(expected):
             plane = out_image.get_pixels()[i]
             #plane_stride = out_image.get_rowstride()[i]
             #assert len(plane)>=plane_stride*out_image.get_height()
             plane_bytes = memoryview_to_bytes(plane)
-            if plane_bytes[:len(v)]!=v:
-                raise Exception("plane %s, expected %s but got %s" % (
-                    out_csc[i], hexstr(v), hexstr(plane_bytes[:len(v)])))
-            print("%s %s : %s (%i bytes - %s)" % (mod, out_csc[i], hexstr(plane), len(plane), type(plane)))
+            v = h2b(v_str)
+            if not cmpp(plane_bytes, v):
+                raise Exception("%s: plane %s, expected %s but got %s" % (
+                    mod, out_csc[i], v_str, hexstr(plane_bytes[:len(v)])))
+            #print("%s %s : %s (%i bytes - %s)" % (mod, out_csc[i], hexstr(plane), len(plane), type(plane)))
 
     def test_BGRX_to_YUV420P(self):
         for mod in loader.CODEC_OPTIONS:
@@ -59,10 +75,18 @@ class Test_CSC_Colorspace(unittest.TestCase):
             self._test_csc(mod)
             #white:
             self._test_csc(mod,
-                           pixel=b"\xff\xff\xff\xff", expected=(
-                               b'\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb\xeb',
-                               b'\x80\x80\x80\x80\x80\x80\x80\x80',
-                               b'\x80\x80\x80\x80\x80\x80\x80\x80',
+                           pixel="ffffffff", expected=(
+                               'ebebebebebebebebebebebebebebebeb',
+                               '8080808080808080',
+                               '8080808080808080',
+                               )
+                           )
+            #blue?
+            self._test_csc(mod,
+                           pixel="ff000000", expected=(
+                               '29292929292929292929292929292929',
+                               'f0f0f0f0f0f0f0f0',
+                               '6e6e6e6e6e6e6e6e',
                                )
                            )
 
