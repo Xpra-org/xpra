448fd48b220bcc1f8db739ef5a7d366e92da36bf
Michael Davidsaver <mdavidsaver@gmail.com>
2022-02-20 18:15:14 -0800

    teach --auth=exec about websocket auth.
    Pass path and headers as ~CGI

--- xpra/server/auth/exec_auth.py
+++ xpra/server/auth/exec_auth.py
@@ -4,7 +4,9 @@
 # later version. See the file COPYING for details.
 
 import os
-from subprocess import Popen
+import base64, binascii
+from urllib.parse import urlparse
+from subprocess import Popen, DEVNULL
 from gi.repository import GLib
 
 from xpra.util import envint, typedict
@@ -12,6 +14,7 @@ from xpra.os_util import OSX
 from xpra.child_reaper import getChildReaper
 from xpra.server.auth.sys_auth_base import SysAuthenticator, log
 from xpra.platform.features import EXECUTABLE_EXTENSION
+from xpra.net.websockets.handler import WebSocketRequestHandler
 
 TIMEOUT = envint("XPRA_EXEC_AUTH_TIMEOUT", 600)
 
@@ -22,6 +25,7 @@ class Authenticator(SysAuthenticator):
         log("exec.Authenticator(%s)", kwargs)
         self.command = kwargs.pop("command", "")
         self.timeout = kwargs.pop("timeout", TIMEOUT)
+        self.http_request = kwargs.pop('http_request')
         self.timer = None
         self.proc = None
         self.timeout_event = False
@@ -50,7 +54,12 @@ class Authenticator(SysAuthenticator):
     def authenticate(self, caps : typedict) -> bool:
         info = "Connection request from %s" % self.connection_str
         cmd = [self.command, info, str(self.timeout)]
-        with Popen(cmd) as proc:
+        env = stdin = stdout = None
+        if self.http_request:
+            stdin = stdout = DEVNULL
+            env = os.environ.copy()
+            add_cgi_headers(env, self.http_request)
+        with Popen(cmd, env=env, stdin=stdin, stdout=stdout) as proc:
             self.proc = proc
             log("authenticate(..) Popen(%s)=%s", cmd, proc)
             #if required, make sure we kill the command when it times out:
@@ -87,3 +96,54 @@ class Authenticator(SysAuthenticator):
 
     def __repr__(self):
         return "exec"
+
+def add_cgi_headers(env : dict, request : WebSocketRequestHandler):
+    url = urlparse(request.path)
+    parts = url.path.rsplit('/', 1)
+    if len(parts)==1: # no '/', act as if there is a trailing '/'
+        parts = [parts, '']
+
+    # Not passing through any content
+    env['CONTENT_TYPE'] = ''
+    env['CONTENT_LENGTH'] = '0'
+
+    # see http.server.CGIHTTPRequestHandler.run_cgi()
+    # also RFC3875
+    env['SERVER_SOFTWARE'] = 'XPRA'
+    env['SERVER_PROTOCOL'] = request.protocol_version
+    env['GATEWAY_INTERFACE'] = 'CGI/1.1'
+    env['REQUEST_METHOD'] = request.command
+    env['PATH_INFO'] = parts[0]
+    env['PATH_TRANSLATED'] = parts[0] # TODO: window-ish path?
+    env['SCRIPT_NAME'] = parts[1]
+    if url.query:
+        env['QUERY_STRING'] = url.query
+    env['REMOTE_ADDR'] = request.client_address[0]
+
+    # request.server not provided, and not that interesting anyway...
+    #env['SERVER_NAME'] = request.server.server_name
+    #env['SERVER_PORT'] = str(request.server.server_port)
+
+    agent = request.headers.get('user-agent')
+    if agent:
+        env['HTTP_USER_AGENT'] = agent
+
+    cookies = [co for co in request.headers.get_all('cookie', []) if co]
+    if cookies:
+        env['COOKIE'] = ', '.join(cookies)
+
+    auth = request.headers.get("auth", "").split()
+    if len(auth)==2 and auth[0].lower()=='basic':
+        env['AUTH_TYPE'] = 'basic'
+        try:
+            # headers originally decoded as 'iso-8859-1' by http.client.parse_headers()
+            # http.server.CGIHTTPRequestHandler.run_cgi() re-encodes them as 'ascii'
+            # we follow parse_headers().
+            # Shouldn't make any difference if payload is really base64 encoded.
+            auth = base64.decodebytes(auth[1].encode('iso-8859-1')).decode()
+            # we decode the output as utf-8 because everyone loves crazy password rules
+            env['REMOTE_USER'], env['REMOTE_IDENT'] = auth.split(':',1)
+        except (TypeError, binascii.Error, UnicodeError):
+            pass
+
+    # TODO: pass remaining headers as HTTP_* ?
