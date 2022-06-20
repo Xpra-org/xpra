Xpra-org/xpra

commit aa2f1e1b4197a39eed0d6ae6a44a798ab3913adb

no time to test GTK2 on MacOS, assume it is still broken

xpra/client/gtk_base/gtk_tray_menu_base.py
@@ -9,7 +9,7 @@ import re
 
 from xpra.gtk_common.gobject_compat import import_gtk, import_glib, import_pixbufloader
 from xpra.util import CLIENT_EXIT, iround, envbool, repr_ellipsized, reverse_dict
-from xpra.os_util import bytestostr, OSX, WIN32, PYTHON3
+from xpra.os_util import bytestostr, OSX, WIN32, PYTHON2,
 from xpra.gtk_common.gtk_util import (
     ensure_item_selected, menuitem, popup_menu_workaround, CheckMenuItem,
     get_pixbuf_from_data, scaled_image,
@@ -34,7 +34,7 @@ bandwidthlog = Logger("bandwidth", "network")
 gtk = import_gtk()
 glib = import_glib()
 
-HIDE_DISABLED_MENU_ENTRIES = envbool("XPRA_HIDE_DISABLED_MENU_ENTRIES", PYTHON3)
+HIDE_DISABLED_MENU_ENTRIES = envbool("XPRA_HIDE_DISABLED_MENU_ENTRIES", PYTHON2 and OSX)
 
 SHOW_TITLE_ITEM = envbool("XPRA_SHOW_TITLE_ITEM", True)
 SHOW_VERSION_CHECK = envbool("XPRA_SHOW_VERSION_CHECK", True)

xpra/client/gtk_base/menu_helper.py
@@ -5,7 +5,7 @@
 # later version. See the file COPYING for details.
 
 from xpra.util import envbool
-from xpra.os_util import OSX, PYTHON3
+from xpra.os_util import OSX, PYTHON2
 from xpra.gtk_common.gtk_util import menuitem
 from xpra.gtk_common.about import about, close_about
 from xpra.platform.gui import get_icon_size
@@ -14,7 +14,7 @@ from xpra.gtk_common.gtk_util import import_gtk
 
 gtk = import_gtk()
 
-HIDE_DISABLED_MENU_ENTRIES = envbool("XPRA_HIDE_DISABLED_MENU_ENTRIES", PYTHON3)
+HIDE_DISABLED_MENU_ENTRIES = envbool("XPRA_HIDE_DISABLED_MENU_ENTRIES", PYTHON2 and OSX)
 
 def make_min_auto_menu(title, min_options, options,
                        get_current_min_value,

commit ba83f273f6596f1c5ebad7d4f3747261671a0a9b

typo introduced in aa2f1e1b4197a39eed0d6ae6a44a798ab3913adb

xpra/client/gtk_base/gtk_tray_menu_base.py
@@ -9,7 +9,7 @@ import re
 
 from xpra.gtk_common.gobject_compat import import_gtk, import_glib, import_pixbufloader
 from xpra.util import CLIENT_EXIT, iround, envbool, repr_ellipsized, reverse_dict
-from xpra.os_util import bytestostr, OSX, WIN32, PYTHON2,
+from xpra.os_util import bytestostr, OSX, WIN32, PYTHON2
 from xpra.gtk_common.gtk_util import (
     ensure_item_selected, menuitem, popup_menu_workaround, CheckMenuItem,
     get_pixbuf_from_data, scaled_image,