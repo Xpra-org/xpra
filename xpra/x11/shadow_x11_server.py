8fbda4d27d542a29f7e43a1ed09931f75b51149a
totaam <antoine@xpra.org>
2022-09-15 21:38:11 +0700

    we don't use gtk2

--- xpra/x11/shadow_x11_server.py
+++ xpra/x11/shadow_x11_server.py
@@ -214,7 +214,7 @@ class ShadowX11Server(GTKShadowServerBase, X11ServerCore):
     def make_hello(self, source):
         capabilities = X11ServerCore.make_hello(self, source)
         capabilities.update(GTKShadowServerBase.make_hello(self, source))
-        capabilities["server_type"] = "Python/gtk2/x11-shadow"
+        capabilities["server_type"] = "Python/gtk3/x11-shadow"
         return capabilities
 
     def get_info(self, proto, *_args):
