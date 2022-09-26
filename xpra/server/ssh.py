a44ee9ada5dd3fe64bc950b4836467fa9aa82a55
totaam <antoine@xpra.org>
2022-09-26 16:01:42 +0700

    simplify

--- xpra/server/ssh.py
+++ xpra/server/ssh.py
@@ -196,7 +196,8 @@ class SSHServer(paramiko.ServerInterface):
             if err:
                 chan_send(channel.send_stderr, err)
             channel.send_exit_status(exit_status)
-            self.event.set()
+            #self.event.set()
+            #channel.close()
             return True
         log(f"check_channel_exec_request({channel}, {command})")
         cmd = shlex.split(decode_str(command))
@@ -279,51 +280,50 @@ class SSHServer(paramiko.ServerInterface):
                 return fail(f"Warning: unsupported xpra subcommand '{cmd[1]}'")
             #we're ready to use this socket as an xpra channel
             self._run_proxy(channel)
+            return True
+        #plain 'ssh' clients execute a long command with if+else statements,
+        #try to detect it and extract the actual command the client is trying to run.
+        #ie:
+        #['sh', '-c',
+        # ': run-xpra _proxy;xpra initenv;\
+        #  if [ -x $XDG_RUNTIME_DIR/xpra/run-xpra ]; then $XDG_RUNTIME_DIR/xpra/run-xpra _proxy;\
+        #  elif [ -x ~/.xpra/run-xpra ]; then ~/.xpra/run-xpra _proxy;\
+        #  elif type "xpra" > /dev/null 2>&1; then xpra _proxy;\
+        #  elif [ -x /usr/local/bin/xpra ]; then /usr/local/bin/xpra _proxy;\
+        #  else echo "no run-xpra command found"; exit 1; fi']
+        #if .* ; then .*/run-xpra _proxy;
+        log(f"parse cmd={cmd} (len={len(cmd)})")
+        if len(cmd)==1:         #ie: 'thelongcommand'
+            parse_cmd = cmd[0]
+        elif len(cmd)==3 and cmd[:2]==["sh", "-c"]:     #ie: 'sh' '-c' 'thelongcommand'
+            parse_cmd = cmd[2]
         else:
-            #plain 'ssh' clients execute a long command with if+else statements,
-            #try to detect it and extract the actual command the client is trying to run.
-            #ie:
-            #['sh', '-c',
-            # ': run-xpra _proxy;xpra initenv;\
-            #  if [ -x $XDG_RUNTIME_DIR/xpra/run-xpra ]; then $XDG_RUNTIME_DIR/xpra/run-xpra _proxy;\
-            #  elif [ -x ~/.xpra/run-xpra ]; then ~/.xpra/run-xpra _proxy;\
-            #  elif type "xpra" > /dev/null 2>&1; then xpra _proxy;\
-            #  elif [ -x /usr/local/bin/xpra ]; then /usr/local/bin/xpra _proxy;\
-            #  else echo "no run-xpra command found"; exit 1; fi']
-            #if .* ; then .*/run-xpra _proxy;
-            log(f"parse cmd={cmd} (len={len(cmd)})")
-            if len(cmd)==1:         #ie: 'thelongcommand'
-                parse_cmd = cmd[0]
-            elif len(cmd)==3 and cmd[:2]==["sh", "-c"]:     #ie: 'sh' '-c' 'thelongcommand'
-                parse_cmd = cmd[2]
-            else:
-                parse_cmd = ""
-            #for older clients, try to parse the long command
-            #and identify the subcommands from there
-            subcommands = {}
-            for s in parse_cmd.split("if "):
-                if (s.startswith("type \"xpra\"") or
-                    s.startswith("which \"xpra\"") or
-                    s.startswith("command -v \"xpra\"") or
-                    s.startswith("[ -x")
-                    ) and s.find("then ")>0:
-                    then_str = s.split("then ", 1)[1]
-                    #ie: then_str="$XDG_RUNTIME_DIR/xpra/run-xpra _proxy; el"
-                    if then_str.find(";")>0:
-                        then_str = then_str.split(";")[0]
-                    parts = shlex.split(then_str)
-                    if len(parts)>=2:
-                        subcommand = parts[1]       #ie: "_proxy"
-                        subcommands[subcommand] = parts
-            log(f"subcommands={subcommands}")
-            proxy_cmd = subcommands.get("_proxy")
-            if proxy_cmd:
-                setup_agent(proxy_cmd)
-                self._run_proxy(channel)
-            else:
-                return fail("Warning: unsupported ssh command:",
-                            f" {cmd!r}")
-        return True
+            parse_cmd = ""
+        #for older clients, try to parse the long command
+        #and identify the subcommands from there
+        subcommands = {}
+        for s in parse_cmd.split("if "):
+            if (s.startswith("type \"xpra\"") or
+                s.startswith("which \"xpra\"") or
+                s.startswith("command -v \"xpra\"") or
+                s.startswith("[ -x")
+                ) and s.find("then ")>0:
+                then_str = s.split("then ", 1)[1]
+                #ie: then_str="$XDG_RUNTIME_DIR/xpra/run-xpra _proxy; el"
+                if then_str.find(";")>0:
+                    then_str = then_str.split(";")[0]
+                parts = shlex.split(then_str)
+                if len(parts)>=2:
+                    subcommand = parts[1]       #ie: "_proxy"
+                    subcommands[subcommand] = parts
+        log(f"subcommands={subcommands}")
+        proxy_cmd = subcommands.get("_proxy")
+        if proxy_cmd:
+            setup_agent(proxy_cmd)
+            self._run_proxy(channel)
+            return True
+        return fail("Warning: unsupported ssh command:",
+                    f" {cmd!r}")
 
     def _run_proxy(self, channel):
         pc = self.proxy_channel
