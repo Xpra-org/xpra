0f40b5a66c4f93ef91bdd52f2e4ccdbe24a8bdb0
totaam <antoine@xpra.org>
2022-09-26 19:01:53 +0700

    add utility for dumping xpra's x11 properties

---
+++ tests/xpra/x11/print_xpra_props.py
@@ -0,0 +1,15 @@
+# This file is part of Xpra.
+# Copyright (C) 2012-2022 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from xpra.x11.gtk3 import gdk_display_source    #@UnresolvedImport, @Reimport
+gdk_display_source.init_gdk_display_source()
+
+from xpra.x11.bindings.window_bindings import X11WindowBindings #@UnresolvedImport
+X11Window = X11WindowBindings()
+
+from xpra.server.server_uuid import get_mode, get_uuid
+
+print(f"mode={get_mode()}")
+print(f"uuid={get_uuid()}")
