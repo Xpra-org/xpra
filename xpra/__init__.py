750764e468e87f747c6f8f335d574475903d9025
totaam <antoine@xpra.org>
2022-10-21 22:24:08 +0700

    bump year

--- xpra/__init__.py
+++ xpra/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of Xpra.
 # Copyright (C) 2008 Nathaniel Smith <njs@pobox.com>
-# Copyright (C) 2009-2020 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2009-2022 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
