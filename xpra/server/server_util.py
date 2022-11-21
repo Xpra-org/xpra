d9f8b7e6a9fa1e3b34ccfe7d0087f9053ac5bb91
totaam <antoine@xpra.org>
2022-11-21 22:18:26 +0700

    #3690 uinput packages can be broken in new ways

--- xpra/server/server_util.py
+++ xpra/server/server_util.py
@@ -471,17 +471,17 @@ def has_uinput():
     try:
         import uinput
         assert uinput
-    except NameError as e:
-        log = get_util_logger()
-        log("has_uinput()", exc_info=True)
-        log.warn("Warning: the system python uinput module looks broken:")
-        log.warn(" %s", e)
-        return False
     except ImportError as e:
         log = get_util_logger()
         log("has_uinput()", exc_info=True)
         log.info("cannot access python uinput module:")
         log.info(" %s", e)
+        return False
+    except Exception as e:
+        log = get_util_logger()
+        log("has_uinput()", exc_info=True)
+        log.warn("Warning: the system python uinput module looks broken:")
+        log.warn(" %s", e)
         return False
     try:
         uinput.fdopen()         #@UndefinedVariable
