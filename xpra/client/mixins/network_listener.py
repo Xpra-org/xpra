00bcf3eeed21acf120cd62ad8daab649ebbb0b1a
totaam <antoine@xpra.org>
2022-11-24 15:39:41 +0700

    add missing import

--- xpra/client/mixins/network_listener.py
+++ xpra/client/mixins/network_listener.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 from xpra.version_util import version_str
-from xpra.util import envint, envfloat, typedict, DETACH_REQUEST, PROTOCOL_ERROR
+from xpra.util import envint, envfloat, envbool, typedict, DETACH_REQUEST, PROTOCOL_ERROR
 from xpra.os_util import bytestostr, get_machine_id, WIN32
 from xpra.net.bytestreams import log_new_connection
 from xpra.net.socket_util import create_sockets, add_listen_socket, accept_connection, setup_local_sockets
