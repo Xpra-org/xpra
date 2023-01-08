8a8af19caf4f64e02b1d2f43d645e4b3957f3302
totaam <antoine@xpra.org>
2023-01-08 23:20:13 +0700

    validate dimensions to prevent potential crashes

--- xpra/codecs/enc_x264/encoder.pyx
+++ xpra/codecs/enc_x264/encoder.pyx
@@ -494,6 +494,10 @@ cdef class Encoder:
         assert cs_info is not None, "invalid source format: %s, must be one of: %s" % (src_format, COLORSPACE_FORMATS.keys())
         assert encoding=="h264", "invalid encoding: %s" % encoding
         assert scaling==(1,1), "x264 does not handle scaling"
+        if width%2!=0 and src_format not in ("BGRX", "YUV444P"):
+            raise ValueError(f"invalid odd width {width} for {src_format}")
+        if height%2!=0 and src_format in ("YUV420P", "YUV422P"):
+            raise ValueError(f"invalid odd height {height} for {src_format}")
         self.width = width
         self.height = height
         self.quality = quality
