314cce176f043070bbc12bbd93a5e939a97ed9d3
adamnew123456 <adamnew123456@gmail.com>
2023-01-14 21:12:01 +0000

    Update proc Cython extension to use public API
    readproc.h is not exported by libproc2 as per:
    https://gitlab.com/procps-ng/procps/-/commit/d974f064b5a269fe8bd009d1430d17218aaf07b7
    The openproc/readproc/closeproc functions used in this module are not
    part of the public API anymore and neither is pid_t.
    
    Using the public API requires more ceremony but returns all the info
    that was useful from readproc.

--- xpra/platform/xposix/proc.pyx
+++ xpra/platform/xposix/proc.pyx
@@ -13,32 +13,62 @@ DEF PROC_FILLSTATUS = 0x0020    #read status
 DEF PROC_PID = 0x1000           #process id numbers ( 0   terminated )
 
 
-cdef extern from "proc/readproc.h":
-    ctypedef int pid_t
-    ctypedef struct proc_t:
-        int ppid
-        int pgrp        #process group id
-        int session     #session id
-        int nlwp        #stat,status     number of threads, or 0 if no clue
-        int tgid        #(special)       thread group ID, the POSIX PID (see also: tid)
-        int tty         #stat            full device number of controlling terminal
-
-    proc_t * get_proc_stats(pid_t pid, proc_t *p)
-
-    ctypedef struct PROCTAB:
+cdef extern from "libproc2/pids.h":
+    # Opaque handle used by procps to store its buffers
+    struct pids_info:
         pass
 
-    PROCTAB* openproc(int flags, pid_t *pid)
-    proc_t* readproc(PROCTAB *PT, proc_t *p)
-    void closeproc(PROCTAB* PT)
+    # Reports counts of queried processes (both total, and per-state which we don't use)
+    struct pids_counts:
+        int total
+
+    # Variant type containing a value for each query flag
+    union pids_result_variant:
+        int s_int
+
+    struct pids_result:
+        int item
+        pids_result_variant result
+
+    struct pids_stack:
+        pids_result *head
+
+    # Return buffer that reports queried process counts and actual results 
+    struct pids_fetch:
+        pids_counts *counts
+        pids_stack **stacks
+
+    # Flag used to query the parent process, part of the pids_item enum
+    int PIDS_ID_PPID 
+
+    # Flag used to return one value per process, part of pids_select_type enum.
+    # All we care about here is PPID which is the same for each thread within a
+    # process.
+    int PIDS_SELECT_PID
+
+    int procps_pids_new(pids_info **info, int *items, int numitems)
+    int procps_pids_unref(pids_info **info)
+    pids_fetch *procps_pids_select(pids_info *info, unsigned int *pids, int pidcount, int select_type)
 
 def get_parent_pid(int pid):
-    cdef proc_t proc_info
-    memset(&proc_info, 0, sizeof(proc_t))
-    cdef PROCTAB *pt_ptr = openproc(PROC_FILLSTATUS | PROC_PID, &pid)
-    try:
-        if readproc(pt_ptr, &proc_info):
-            return proc_info.ppid
-    finally:
-        closeproc(pt_ptr);
-    return 0
+    cdef pids_info *handle
+    cdef int selector
+
+    handle = NULL
+    selector = PIDS_ID_PPID
+    if procps_pids_new(&handle, &selector, 1) != 0:
+        return 0
+
+    cdef unsigned int upid
+    cdef pids_fetch *query
+    cdef int retval
+
+    upid = pid
+    query = procps_pids_select(handle, &upid, 1, PIDS_SELECT_PID)
+    if query == NULL or query.counts.total != 1 or query.stacks[0].head.item != PIDS_ID_PPID:
+        retval = 0
+    else:
+        retval = query.stacks[0].head.result.s_int
+
+    procps_pids_unref(&handle)
+    return retval
