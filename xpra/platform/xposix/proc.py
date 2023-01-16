7fdf3df413b8082df1d91e90c6931e96343c23c9
totaam <antoine@xpra.org>
2023-01-16 10:50:51 +0700

    #3738 executable needs shebang

--- xpra/platform/xposix/proc.py
+++ xpra/platform/xposix/proc.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # This file is part of Xpra.
 # Copyright (C) 2023 Chris Marchetti <adamnew123456@gmail.com>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
