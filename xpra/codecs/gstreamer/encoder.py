5a0b8f7cefce366b844108f220f5f10a2a641902
totaam <antoine@xpra.org>
2023-01-25 12:53:36 +0700

    add debug logging

--- xpra/codecs/gstreamer/encoder.py
+++ xpra/codecs/gstreamer/encoder.py
@@ -167,6 +167,8 @@ def init_all_specs(*exclude):
     global SPECS, COLORSPACES
     SPECS = specs
     COLORSPACES = colorspaces
+    log("init_all_specs%s SPECS={specs}", exclude, SPECS)
+    log("init_all_specs%s COLORSPACES={specs}", exclude, COLORSPACES)
 init_all_specs()
 
 
