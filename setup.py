5903bdd4d4ffbf19189fbcd6b1cc7497f01c1aff
totaam <antoine@xpra.org>
2023-01-26 15:50:35 +0700

    always use '-Wno-error=address' when compiling C++ on MS Windows

--- setup.py
+++ setup.py
@@ -652,6 +652,9 @@ def ace(modnames="xpra.x11.bindings.xxx", pkgconfig_names="", optimize=None, **k
                 value = (value, )
             add_to_keywords(pkgc, addto, *value)
     pkgc.update(kwargs)
+    if WIN32 and kwargs.get("language", "")=="c++":
+        #all C++ modules trigger an address warning in the module initialization code:
+        add_to_keywords.append(pkgc, "extra_compile_args", "-Wno-error=address")
     add_cython_ext(modname, src, **pkgc)
 
 def tace(toggle, *args, **kwargs):
@@ -1994,12 +1997,8 @@ toggle_packages(client_ENABLED and gtk3_ENABLED, "xpra.client.gtk_base")
 toggle_packages(client_ENABLED and opengl_ENABLED and gtk3_ENABLED, "xpra.client.gl.gtk3")
 toggle_packages(client_ENABLED and gtk3_ENABLED and example_ENABLED, "xpra.client.gtk_base.example")
 if client_ENABLED and WIN32 and MINGW_PREFIX:
-    extra_compile_args = []
-    if WIN32:
-        extra_compile_args.append("-Wno-error=address")
     ace("xpra.platform.win32.propsys,xpra/platform/win32/setappid.cpp",
         language="c++",
-        extra_compile_args=extra_compile_args,
         extra_link_args = ("-luuid", "-lshlwapi", "-lole32", "-static-libgcc")
         )
 
@@ -2268,8 +2267,7 @@ tace(dec_avcodec2_ENABLED, "xpra.codecs.ffmpeg.decoder,xpra/codecs/ffmpeg/regist
 tace(csc_swscale_ENABLED, "xpra.codecs.ffmpeg.colorspace_converter", "libswscale,libavutil")
 tace(enc_ffmpeg_ENABLED, "xpra.codecs.ffmpeg.encoder", "libavcodec,libavformat,libavutil", extra_compile_args="-Wno-deprecated-declarations")
 toggle_packages(csc_libyuv_ENABLED, "xpra.codecs.libyuv")
-tace(csc_libyuv_ENABLED, "xpra.codecs.libyuv.colorspace_converter", "libyuv", language="c++",
-        extra_compile_args = ("-Wno-error=address", ) if WIN32 else ())
+tace(csc_libyuv_ENABLED, "xpra.codecs.libyuv.colorspace_converter", "libyuv", language="c++")
 toggle_packages(csc_cython_ENABLED, "xpra.codecs.csc_cython")
 tace(csc_cython_ENABLED, "xpra.codecs.csc_cython.colorspace_converter", optimize=3)
 toggle_packages(vpx_ENABLED, "xpra.codecs.vpx")
