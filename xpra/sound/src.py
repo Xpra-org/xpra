d4ee5089df226e2166327c4fc8261986d37e0cb6
totaam <antoine@xpra.org>
2023-01-27 18:35:39 +0700

    silence linter warning

--- xpra/sound/src.py
+++ xpra/sound/src.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # This file is part of Xpra.
-# Copyright (C) 2010-2022 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2010-2023 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -8,7 +8,7 @@ import sys
 import os.path
 from queue import Queue
 from time import monotonic
-from gi.repository import GObject
+from gi.repository import GObject  # @UnresolvedImport
 
 from xpra.os_util import SIGNAMES
 from xpra.util import csv, envint, envbool, envfloat
