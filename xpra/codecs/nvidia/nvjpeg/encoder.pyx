ac307c9ddd25f95dc3bd2b4f6dc83428e0750d84
totaam <antoine@xpra.org>
2023-02-09 21:20:01 +0700

    differentiate from plain 'jpeg' encoder

--- xpra/codecs/nvidia/nvjpeg/encoder.pyx
+++ xpra/codecs/nvidia/nvjpeg/encoder.pyx
@@ -150,7 +150,7 @@ def get_specs(encoding, colorspace):
         video_spec(
             "jpeg", input_colorspace=colorspace, output_colorspaces=(colorspace, ),
             has_lossless_mode=False,
-            codec_class=Encoder, codec_type="jpeg",
+            codec_class=Encoder, codec_type="nvjpeg",
             setup_cost=20, cpu_cost=0, gpu_cost=100,
             min_w=16, min_h=16, max_w=16*1024, max_h=16*1024,
             can_scale=True,
