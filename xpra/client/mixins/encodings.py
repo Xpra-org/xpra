Xpra-org/xpra

commit 75cfd2b7f1944bb2a3ab35460bcf9babc82f72b9

only enable an encoding if we have an encoder for it

xpra/server/window/window_video_source.py
@@ -144,7 +144,9 @@ class WindowVideoSource(WindowSource):
             self.non_video_encodings = ()
             self.common_video_encodings = ()
             return
-        self.video_encodings = tuple(x for x in self.video_helper.get_encodings() if x in self.server_core_encodings)
+        #make sure we actually have encoders for these:
+        enc_options = set(self.server_core_encodings) & set(self._encoders.keys())
+        self.video_encodings = tuple(x for x in self.video_helper.get_encodings() if x in enc_options)
         video_enabled = []
         for x in self.video_encodings:
             self.append_encoder(x, self.video_encode)
@@ -154,7 +156,7 @@ class WindowVideoSource(WindowSource):
         add("auto", self.video_encode)
         #these are used for non-video areas, ensure "jpeg" is used if available
         #as we may be dealing with large areas still, and we want speed:
-        nv_common = (set(self.server_core_encodings) & set(self.core_encodings)) - set(self.video_encodings)
+        nv_common = (enc_options & set(self.core_encodings)) - set(self.video_encodings)
         self.non_video_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER
                                          if x in nv_common)
         log("init_encoders() non-video-encodings(%s, %s, %s)=%s",

commit 9d0c775e528ae5a20db8885cdd1000d432444266

#3744 re-work the encoding filtering
seems to have been broken by 75cfd2b7f1944bb2a3ab35460bcf9babc82f72b9.
use a utility function for sorting encodings,
'common_encodings' doesn't include video encodings...

xpra/client/mixins/encodings.py
@@ -6,7 +6,7 @@
 
 import os
 
-from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER
+from xpra.codecs.codec_constants import preforder
 from xpra.codecs.loader import load_codec, codec_versions, has_codec, get_codec
 from xpra.codecs.video_helper import getVideoHelper
 from xpra.scripts.config import parse_bool_or_int
@@ -57,7 +57,7 @@ def get_core_encodings():
         if encoding not in core_encodings:
             core_encodings.append(encoding)
     #remove duplicates and use preferred encoding order:
-    return [x for x in PREFERRED_ENCODING_ORDER if x in set(core_encodings)]
+    return preforder(core_encodings)
 
 
 class Encodings(StubClientMixin):
@@ -281,7 +281,7 @@ class Encodings(StubClientMixin):
             cenc.append("rgb")
         if "grayscale" not in cenc:
             cenc.append("grayscale")
-        return [x for x in PREFERRED_ENCODING_ORDER if x in cenc and x not in ("rgb32", "rgb24")]
+        return preforder(x for x in cenc if x not in ("rgb32", "rgb24"))
 
     def get_cursor_encodings(self):
         e = ["raw"]

xpra/codecs/codec_constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This file is part of Xpra.
-# Copyright (C) 2012-2019 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2012-2023 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -63,6 +63,9 @@ def get_subsampling_divs(pixel_format):
     if pixel_format not in PIXEL_SUBSAMPLING:
         raise Exception(f"invalid pixel format: {pixel_format!r}")
     return PIXEL_SUBSAMPLING.get(pixel_format)
+
+def preforder(encodings):
+    return tuple(x for x in PREFERRED_ENCODING_ORDER if x in encodings)
 
 
 RGB_FORMATS = (

xpra/scripts/parsing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # This file is part of Xpra.
 # Copyright (C) 2011 Serviware (Arthur Huillet, <ahuillet@serviware.com>)
-# Copyright (C) 2010-2022 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2010-2023 Antoine Martin <antoine@xpra.org>
 # Copyright (C) 2008 Nathaniel Smith <njs@pobox.com>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
@@ -1815,11 +1815,11 @@ When unspecified, all the available codecs are allowed and the first one is used
 
 def validated_encodings(encodings):
     try:
-        from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER
+        from xpra.codecs.codec_constants import preforder
     except ImportError:
         return []
     lower_encodings = [x.lower() for x in encodings]
-    validated = [x for x in PREFERRED_ENCODING_ORDER if x.lower() in lower_encodings]
+    validated = preforder(lower_encodings)
     if not validated:
         raise InitException("no valid encodings specified")
     return validated

xpra/server/mixins/encoding_server.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # This file is part of Xpra.
-# Copyright (C) 2010-2022 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2010-2023 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 #pylint: disable-msg=E1101
 
 from xpra.scripts.config import parse_bool_or_int
 from xpra.os_util import bytestostr, OSX
-from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER
+from xpra.codecs.codec_constants import preforder
 from xpra.codecs.loader import get_codec, has_codec, codec_versions, load_codec
 from xpra.codecs.video_helper import getVideoHelper
 from xpra.server.mixins.stub_server_mixin import StubServerMixin
@@ -185,7 +185,7 @@ class EncodingServer(StubServerMixin):
                                    if (x.startswith("png") or x.startswith("rgb") or x=="webp"))
         log("allowed encodings=%s, encodings=%s, core encodings=%s, lossless encodings=%s",
             self.allowed_encodings, encs, core_encs, self.lossless_encodings)
-        pref = [x for x in PREFERRED_ENCODING_ORDER if x in self.encodings]
+        pref = preforder(self.encodings)
         if pref:
             self.default_encoding = pref[0]
         else:

xpra/server/window/window_source.py
@@ -24,7 +24,7 @@ from xpra.rectangle import rectangle, add_rectangle, remove_rectangle, merge_all
 from xpra.simple_stats import get_list_stats
 from xpra.codecs.rgb_transform import rgb_reformat
 from xpra.codecs.loader import get_codec
-from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER, LOSSY_PIXEL_FORMATS
+from xpra.codecs.codec_constants import preforder, LOSSY_PIXEL_FORMATS
 from xpra.net.compression import use
 from xpra.log import Logger
 
@@ -866,7 +866,7 @@ class WindowSource(WindowIconSource):
             self.get_best_encoding = self.encoding_is_mmap
             return
         common_encodings = [x for x in self._encoders if x in self.core_encodings and x not in exclude]
-        self.common_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER if x in common_encodings)
+        self.common_encodings = preforder(common_encodings)
         if not self.common_encodings:
             raise Exception("no common encodings found (server: %s vs client: %s, excluding: %s)" % (
                 csv(self._encoders.keys()), csv(self.core_encodings), csv(exclude)))
@@ -893,7 +893,7 @@ class WindowSource(WindowIconSource):
             are = tuple(x for x in self.common_encodings if x in ropts and x in TRANSPARENCY_ENCODINGS)
         if not are:
             are = tuple(x for x in self.common_encodings if x in ropts) or self.common_encodings
-        self.auto_refresh_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER if x in are)
+        self.auto_refresh_encodings = preforder(are)
         log("update_encoding_selection: client refresh encodings=%s, auto_refresh_encodings=%s",
             self.client_refresh_encodings, self.auto_refresh_encodings)
         self.update_quality()

xpra/server/window/window_video_source.py
@@ -25,7 +25,7 @@ from xpra.rectangle import rectangle, merge_all          #@UnresolvedImport
 from xpra.server.window.motion import ScrollData                    #@UnresolvedImport
 from xpra.server.window.video_subregion import VideoSubregion, VIDEO_SUBREGION
 from xpra.server.window.video_scoring import get_pipeline_score
-from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER, EDGE_ENCODING_ORDER
+from xpra.codecs.codec_constants import PREFERRED_ENCODING_ORDER, EDGE_ENCODING_ORDER, preforder
 from xpra.codecs.loader import has_codec
 from xpra.util import parse_scaling_value, engs, envint, envbool, csv, roundup, print_nested_dict, first_time, typedict
 from xpra.log import Logger
@@ -145,10 +145,10 @@ class WindowVideoSource(WindowSource):
             self.common_video_encodings = ()
             return
         #make sure we actually have encoders for these:
-        enc_options = set(self.server_core_encodings) & set(self._encoders.keys())
-        self.video_encodings = tuple(x for x in self.video_helper.get_encodings() if x in enc_options)
+        self.video_encodings = preforder(self.video_helper.get_encodings())
+        self.common_video_encodings = preforder(set(self.video_encodings) & set(self.core_encodings))
         video_enabled = []
-        for x in self.video_encodings:
+        for x in self.common_video_encodings:
             self.append_encoder(x, self.video_encode)
             video_enabled.append(x)
         #video_encode() is used for more than just video encoders:
@@ -156,13 +156,13 @@ class WindowVideoSource(WindowSource):
         add("auto", self.video_encode)
         #these are used for non-video areas, ensure "jpeg" is used if available
         #as we may be dealing with large areas still, and we want speed:
+        enc_options = set(self.server_core_encodings) & set(self._encoders.keys())
         nv_common = (enc_options & set(self.core_encodings)) - set(self.video_encodings)
-        self.non_video_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER
-                                         if x in nv_common)
-        log("init_encoders() non-video-encodings(%s, %s, %s)=%s",
-            self.server_core_encodings, self.core_encodings, self.video_encodings, self.non_video_encodings)
-        self.common_video_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER
-                                            if x in self.video_encodings and x in self.core_encodings)
+        self.non_video_encodings = preforder(nv_common)
+        log(f"init_encoders() server core encodings={self.server_core_encodings}, client core encodings={self.core_encodings}")
+        log(f" video encodings={self.video_encodings}")
+        log(f" common video encodings={self.common_video_encodings}")
+        log(f" non video encodings={self.non_video_encodings}")
         if "scroll" in self.server_core_encodings:
             add("scroll", self.scroll_encode)
 
@@ -429,8 +429,7 @@ class WindowVideoSource(WindowSource):
                 else:
                     log(*msg_args)
                 log(" csc modes=%", self.full_csc_modes)
-        self.common_video_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER
-                                            if x in self.video_encodings and x in self.core_encodings)
+        self.common_video_encodings = preforder(set(self.video_encodings) & set(self.core_encodings))
         log("update_encoding_options: common_video_encodings=%s, csc_encoder=%s, video_encoder=%s",
             self.common_video_encodings, self._csc_encoder, self._video_encoder)
         super().update_encoding_selection(encoding, exclude, init)
@@ -449,7 +448,7 @@ class WindowVideoSource(WindowSource):
         #encodings may have changed, so redo this:
         nv_common = set(self.picture_encodings) & set(self.core_encodings)
         log("nv_common(%s & %s)=%s", self.picture_encodings, self.core_encodings, nv_common)
-        self.non_video_encodings = tuple(x for x in PREFERRED_ENCODING_ORDER if x in nv_common)
+        self.non_video_encodings = preforder(nv_common)
         if not VIDEO_SKIP_EDGE:
             try:
                 self.edge_encoding = next(x for x in EDGE_ENCODING_ORDER if x in self.non_video_encodings)