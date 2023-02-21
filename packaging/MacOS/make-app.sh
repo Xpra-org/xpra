c784f94c41023b9f375ef0158a4d700b9b031dac
totaam <antoine@xpra.org>
2023-02-21 21:10:56 +0700

    #3706 macos packaging of gstreamer video encoder dependencies

--- packaging/MacOS/make-app.sh
+++ packaging/MacOS/make-app.sh
@@ -351,7 +351,7 @@ GST_PLUGIN_DIR="./gstreamer-1.0"
 if [ "$STRIP_GSTREAMER_PLUGINS" == "1" ]; then
 	KEEP="./gstreamer-1.0.keep"
 	mkdir ${KEEP}
-	PLUGINS="app audio coreelements cutter removesilence faac faad flac oss osxaudio speex volume vorbis wav lame opus ogg gdp isomp4 matroska"
+	PLUGINS="app audio coreelements cutter removesilence faac faad flac oss osxaudio speex volume vorbis wav lame opus ogg gdp isomp4 matroska videoconvert x264 vpx"
 	#video support:
 	PLUGINS="${PLUGINS} vpx x264 aom openh264 videoconvert videorate videoscale"
 	for x in $PLUGINS; do
