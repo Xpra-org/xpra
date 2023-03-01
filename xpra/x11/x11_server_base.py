f19ab187037dad63984e11c94044ca5ca03075b7
totaam <antoine@xpra.org>
2023-03-01 12:13:09 +0700

    #3774 also fix calls in the verify method

--- xpra/x11/x11_server_base.py
+++ xpra/x11/x11_server_base.py
@@ -199,9 +199,9 @@ class X11ServerBase(X11ServerCore):
         xtest = XTestPointerDevice()
         ox, oy = 100, 100
         with xlog:
-            xtest.move_pointer(0, ox, oy)
+            xtest.move_pointer(ox, oy)
         nx, ny = 200, 200
-        self.pointer_device.move_pointer(0, nx, ny)
+        self.pointer_device.move_pointer(nx, ny)
         def verify_uinput_moved():
             pos = None  #@UnusedVariable
             with xswallow:
