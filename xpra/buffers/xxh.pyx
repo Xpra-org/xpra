cb3bf51376215b7df8e65d3718e1810c80e9ed5e
totaam <antoine@xpra.org>
2023-03-28 19:32:55 +0700

    cleanup

--- xpra/buffers/xxh.pyx
+++ xpra/buffers/xxh.pyx
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This file is part of Xpra.
-# Copyright (C) 2015-2021 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2015-2023 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -11,11 +11,7 @@ from libc.stdint cimport uint64_t
 
 cdef extern from "xxhash.h":
     ctypedef uint64_t XXH64_hash_t
-    #XXH64_hash_t XXH64(const void* input, size_t length, unsigned long long seed) nogil
     XXH64_hash_t XXH3_64bits(const void* data, size_t len) nogil
-
-#cdef unsigned long long xxh64(const void* input, size_t length, unsigned long long seed) nogil:
-#    return XXH64(input, length, seed)
 
 cdef uint64_t xxh3(const void* input, size_t length) nogil:
     return XXH3_64bits(input, length)
