e9f5433d067c89bcd8fcaf26023abb41b37b62aa
totaam <antoine@xpra.org>
2023-03-31 17:36:51 +0700

    fix CI: use full imports

--- xpra/platform/xposix/gui.py
+++ xpra/platform/xposix/gui.py
@@ -42,19 +42,22 @@ def X11WindowBindings():
     xb = x11_bindings()
     if not xb:
         return None
-    return xb.window_bindings.X11WindowBindings()
+    from xpra.x11.bindings.window_bindings import X11WindowBindings  # @UnresolvedImport
+    return X11WindowBindings()
 
 def X11RandRBindings():
     xb = x11_bindings()
     if not xb:
         return None
-    return xb.randr_bindings.RandRBindings()
+    from xpra.x11.bindings.randr_bindings import RandRBindings  # @UnresolvedImport
+    return RandRBindings()
 
 def X11XI2Bindings():
     xb = x11_bindings()
     if not xb:
         return None
-    return xb.xi2_bindings.X11XI2Bindings()
+    from xpra.x11.bindings.xi2_bindings import X11XI2Bindings  # @UnresolvedImport
+    return X11XI2Bindings()
 
 
 device_bell = None
