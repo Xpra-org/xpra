694b1f5f0ecfa4aa02a43d45f7e95f74a4700778
totaam <antoine@xpra.org>
2023-04-04 21:51:54 +0700

    #3706 need to specify the stream-format
    otherwise the decoders don't know how to split the nals

--- xpra/codecs/gstreamer/encoder.py
+++ xpra/codecs/gstreamer/encoder.py
@@ -241,7 +241,7 @@ class Encoder(VideoPipeline):
             ]
         if self.encoding=="h264":
             profile = get_profile(options, self.encoding, self.colorspace, "constrained-baseline")
-            elements.append(f"video/x-{self.encoding},profile={profile},alignment=au")
+            elements.append(f"video/x-{self.encoding},profile={profile},alignment=au,stream-format=byte-stream")
         elements.append("appsink name=sink emit-signals=true max-buffers=10 drop=true sync=false async=false qos=false")
         if not self.setup_pipeline_and_bus(elements):
             raise RuntimeError("failed to setup gstreamer pipeline")
