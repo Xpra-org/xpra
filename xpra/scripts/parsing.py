0c08c4c3d8f3253bb6fe9c67cf9761c5086f3957
totaam <antoine@xpra.org>
2023-04-10 14:49:42 +0700

    #3820 avoid errors with malformed display names

--- xpra/scripts/parsing.py
+++ xpra/scripts/parsing.py
@@ -417,7 +417,7 @@ def parse_display_name(error_cb, opts, display_name, cmdline=(), find_session_by
         psep += ":"
         pos += 1
     scount = 0
-    while display_name[pos]=="/" and scount<2:
+    while pos>len(display_name) and display_name[pos]=="/" and scount<2:
         psep += "/"
         pos += 1
         scount += 1
