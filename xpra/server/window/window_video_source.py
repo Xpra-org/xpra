00a654c97b44d9d064be5c8ccb3774e6e7750a78
totaam <antoine@xpra.org>
2023-04-21 20:12:34 +0700

    also honour png/L when scrolling

--- xpra/server/window/window_video_source.py
+++ xpra/server/window/window_video_source.py
@@ -579,6 +579,8 @@ class WindowVideoSource(WindowSource):
         depth = self.image_depth
         if (depth==8 and "png/P" in encoding_options) or self.encoding=="png/P":
             return "png/P"
+        if self.encoding=="png/L":
+            return "png/L"
         if self._mmap_size>0:
             return "mmap"
         return super().do_get_auto_encoding(ww, wh, options, current_encoding or self.encoding, encoding_options)
