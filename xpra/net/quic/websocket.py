39ff6e70226e9d09f52ef0f817de9dcbfac0ac77
totaam <antoine@xpra.org>
2023-05-12 16:28:57 +0700

    #3850 skip empty values

--- xpra/net/quic/websocket.py
+++ xpra/net/quic/websocket.py
@@ -15,7 +15,7 @@ from xpra.util import ellipsizer, first_time
 from xpra.log import Logger
 log = Logger("quic")
 
-SUBSTREAM_PACKET_TYPES = os.environ.get("XPRA_QUIC_SUBSTREAM_PACKET_TYPES", "sound,webcam,draw").split(",")
+SUBSTREAM_PACKET_TYPES = tuple(x for x in os.environ.get("XPRA_QUIC_SUBSTREAM_PACKET_TYPES", "sound,webcam,draw").split(",") if x)
 
 
 class ServerWebSocketConnection(XpraQuicConnection):
