3120eda9aee9066c30f5cf50c8a57aad1d65e766
totaam <antoine@xpra.org>
2023-05-12 15:16:04 +0700

    stop as soon as we find a valid backend

--- xpra/net/socket_util.py
+++ xpra/net/socket_util.py
@@ -829,7 +829,7 @@ def mdns_publish(display_name, listen_on, text_dict=None):
     except ImportError as e:
         log(f"mdns support is not installed: {e}")
         return ()
-    PREFER_ZEROCONF = envbool("XPRA_PREFER_ZEROCONF", True)
+    PREFER_ZEROCONF = envbool("XPRA_PREFER_ZEROCONF", False)
     imports = [import_zeroconf, import_avahi]
     if not PREFER_ZEROCONF:
         imports = reversed(imports)
@@ -838,6 +838,7 @@ def mdns_publish(display_name, listen_on, text_dict=None):
     for i in imports:
         try:
             MDNSPublishers, get_interface_index = i()
+            break
         except ImportError as e:
             log("mdns import failure", exc_info=True)
             exceptions.append(e)
