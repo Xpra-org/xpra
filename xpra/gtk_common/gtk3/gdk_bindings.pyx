a2450cb945a18355ef91ab2dc01310d17bf42b63
totaam <antoine@xpra.org>
2023-05-21 21:06:14 +0700

    typo

--- xpra/gtk_common/gtk3/gdk_bindings.pyx
+++ xpra/gtk_common/gtk3/gdk_bindings.pyx
@@ -40,7 +40,7 @@ cdef extern from "pygobject-3.0/pygobject.h":
         GType gtype
 
 
-def get_display_for(obj) -> Gtk.Display:
+def get_display_for(obj) -> Gdk.Display:
     if obj is None:
         raise TypeError("Cannot get a display: instance is None!")
     if isinstance(obj, Gdk.Display):
