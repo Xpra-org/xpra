f8590909973a5303607638a5b7efc4887b985082
totaam <antoine@xpra.org>
2023-06-08 11:05:56 +0700

    also handle 'ValueError' in fallback path

--- xpra/platform/xposix/appindicator_tray.py
+++ xpra/platform/xposix/appindicator_tray.py
@@ -27,8 +27,12 @@ try:
     gi.require_version("AyatanaAppIndicator3", "0.1")  # @UndefinedVariable
     from gi.repository import AyatanaAppIndicator3 as AppIndicator3 #pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
 except (ImportError, ValueError):
-    gi.require_version("AppIndicator3", "0.1")  # @UndefinedVariable
-    from gi.repository import AppIndicator3 #pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+    try:
+        gi.require_version("AppIndicator3", "0.1")  # @UndefinedVariable
+        from gi.repository import AppIndicator3 #pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+        log("loaded AppIndicator3")
+    except ValueError as e:
+        raise ImportError(f"failed to load `AppIndicator3`: {e}")
 
 log = Logger("tray", "posix")
 
