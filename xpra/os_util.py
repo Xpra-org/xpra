2fe6958bdfb9360e18dd8950efe12e602ae76de0
Antoine Martin <antoine@xpra.org>
2023-07-12 18:29:07 +0200

    we must compare using strings

--- xpra/os_util.py
+++ xpra/os_util.py
@@ -356,7 +356,7 @@ def is_distribution_variant(variant=b"Debian") -> bool:
         pass
     try:
         d = get_linux_distribution()[0]
-        if d==variant:
+        if d==bytestostr(variant):
             return True
         if variant==b"RedHat" and d.startswith(bytestostr(variant)):
             return True
