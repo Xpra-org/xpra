0779d44b59066d7e1d87dd3b87f1cf90c20c33fc
Antoine Martin <antoine@xpra.org>
2023-07-14 21:28:06 +0200

    always return a boolean

--- xpra/server/auth/ldap_auth.py
+++ xpra/server/auth/ldap_auth.py
@@ -111,7 +111,6 @@ class Authenticator(SysAuthenticatorBase):
             return True
         except INVALID_CREDENTIALS:
             log("check(..)", exc_info=True)
-            return False
         except SERVER_DOWN as e:
             log("check(..)", exc_info=True)
             log.warn("Warning: LDAP %sserver at %s:%i is unreachable", ["", "TLS "][self.tls], self.host, self.port)
@@ -120,7 +119,7 @@ class Authenticator(SysAuthenticatorBase):
             log("check(..)", exc_info=True)
             log.warn("Error: ldap authentication failed:")
             emsg(e)
-            return False
+        return False
 
 
 def main(argv):
