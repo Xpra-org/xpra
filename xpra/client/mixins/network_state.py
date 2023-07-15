6384917b05adb08bae004a5198ed00c21c2f9033
Antoine Martin <antoine@xpra.org>
2023-07-15 19:40:32 +0200

    #3623 use NM to get adapter type

--- xpra/client/mixins/network_state.py
+++ xpra/client/mixins/network_state.py
@@ -6,6 +6,7 @@
 
 import os
 import re
+import sys
 from time import monotonic
 from collections import deque
 from typing import Dict, Any, Tuple, Callable
@@ -33,14 +34,52 @@ LOG_INFO_RESPONSE : str = os.environ.get("XPRA_LOG_INFO_RESPONSE", "")
 AUTO_BANDWIDTH_PCT : int = envint("XPRA_AUTO_BANDWIDTH_PCT", 80)
 assert 1<AUTO_BANDWIDTH_PCT<=100, "invalid value for XPRA_AUTO_BANDWIDTH_PCT: %i" % AUTO_BANDWIDTH_PCT
 
-LOCAL_JITTER : int = envint("XPRA_LOCAL_JITTER", 0)
+ETHERNET_JITTER : int = envint("XPRA_LOCAL_JITTER", 0)
 WAN_JITTER : int = envint("XPRA_WAN_JITTER", 20)
+ADSL_JITTER : int = envint("XPRA_WAN_JITTER", 20)
 WIRELESS_JITTER : int = envint("XPRA_WIRELESS_JITTER", 1000)
+
+WIFI_LIMIT : int = envint("XPRA_WIFI_LIMIT", 10*1000*1000)
+ADSL_LIMIT : int = envint("XPRA_WIFI_LIMIT", 1000*1000)
+
+
+def get_NM_adapter_type(device_name) -> str:
+    if not any (sys.modules.get(f"gi.repository.{mod}") for mod in ("GLib", "Gtk")):
+        log("get_NM_adapter_type() no main loop")
+        return ""
+    try:
+        import gi
+        gi.require_version("NM", "1.0")
+        from gi.repository import NM
+    except ImportError:
+        log("get_NM_adapter_type() no network-manager bindings")
+        return ""
+    nmclient = NM.Client.new(None)
+    if device_name:
+        nmdevice = nmclient.get_device_by_iface(device_name)
+    else:
+        connection = nmclient.get_primary_connection()
+        if not connection:
+            return ""
+        try:
+            nmdevice = connection.get_controller()
+        except AttributeError:
+            nmdevice = connection.get_master()
+    if not nmdevice:
+        return ""
+    log(f"NM device {device_name!r}: {nmdevice.get_vendor()} {nmdevice.get_product()}")
+    nmstate = nmdevice.get_state()
+    log(f"NM state({device_name})={nmstate}")
+    if nmdevice.get_state() != NM.DeviceState.ACTIVATED:
+        log.info(f"ignoring {device_name}: {nmstate.value_name}")
+        return ""
+    adapter_type = nmdevice.get_device_type().value_name
+    log(f"NM device-type({device_name})={adapter_type}")
+    return adapter_type
 
 
 def parse_speed(v):
     return parse_with_unit("speed", v)
-
 
 def get_device_value(coptions : Dict, device_info : Dict, attr: str, conv: Callable = str, default_value: Any = ""):
     # first try an env var:
@@ -59,6 +98,46 @@ def get_device_value(coptions : Dict, device_info : Dict, attr: str, conv: Calla
             log.warn("Warning: invalid value for network attribute '%s'", attr)
             log.warn(" %r: %s", v, e)
     return default_value
+
+def guess_adapter_type(name:str) -> str:
+    dnl = name.lower()
+    if dnl.startswith("wlan") or dnl.startswith("wlp") or any(dnl.find(x) >= 0 for x in ("wireless", "wlan", "80211", "modem")):
+        return "wireless"
+    if dnl == "lo" or dnl.find("loopback") >= 0 or dnl.startswith("local"):
+        return "loopback"
+    if any(dnl.find(x) >= 0 for x in ("fiber", "1394", "infiniband", "tun", "tap", "vlan")):
+        return "local"
+    if any(dnl.find(x) >= 0 for x in ("ether", "local", "fiber", "1394", "infiniband", "veth")):
+        return "ethernet"
+    if dnl.find("wan") >= 0:
+        return "wan"
+    return ""
+
+def jitter_for_adapter_type(adapter_type:str) -> int:
+    if not adapter_type:
+        return -1
+    at = adapter_type.lower()
+    def anyfind(*args):
+        return any(at.find(str(x))>=0 for x in args)
+    if anyfind("loopback"):
+        return 0
+    if anyfind("ether", "local", "fiber", "1394", "infiniband"):
+        return ETHERNET_JITTER
+    if at.find("wan") >= 0:
+        return WAN_JITTER
+    if anyfind("wireless", "wifi", "wimax", "modem", "mesh"):
+        return WIRELESS_JITTER
+    return -1
+
+def guess_bandwidth_limit(adapter_type:str) -> int:
+    at = adapter_type.lower()
+    def anyfind(*args):
+        return any(at.find(str(x))>=0 for x in args)
+    if anyfind("wireless", "wifi", "wimax", "modem"):
+        return WIFI_LIMIT
+    if anyfind("adsl", "ppp"):
+        return ADSL_LIMIT
+    return 0
 
 
 class NetworkState(StubClientMixin):
@@ -140,7 +219,6 @@ class NetworkState(StubClientMixin):
         #get socket speed if we have it:
         pinfo = self._protocol.get_info()
         device_info = pinfo.get("socket", {}).get("device", {})
-        connection_data = {}
         try:
             coptions = self._protocol._conn.options
         except AttributeError:
@@ -148,37 +226,26 @@ class NetworkState(StubClientMixin):
         log("get_caps() device_info=%s, connection options=%s", device_info, coptions)
         def device_value(attr: str, conv: Callable = str, default_value: Any = ""):
             return get_device_value(coptions, device_info, attr, conv, default_value)
-        #network interface speed:
+        device_name = device_info.get("name", "")
+        log("get_caps() found device name=%s", device_name)
+        default_adapter_type = guess_adapter_type(get_NM_adapter_type(device_name) or device_name)
+        adapter_type = device_value("adapter-type", str, default_adapter_type)
+        log("get_caps() found adapter-type=%s", adapter_type)
         socket_speed = device_value("speed", parse_speed, 0)
         log("get_caps() found socket_speed=%s", socket_speed)
-        if socket_speed:
-            connection_data["speed"] = socket_speed
-        default_adapter_type = device_value("adapter-type")
-        log("get_caps() found default adapter-type=%s", default_adapter_type)
-        device_name = device_value("name")
-        log("get_caps() found device name=%s", device_name)
-        jitter = device_value("jitter", int, -1)
-        if jitter<0:
-            at = default_adapter_type.lower()
-            if device_name.startswith("wlan") or device_name.startswith("wlp") or device_name.find("wifi")>=0:
-                jitter = WIRELESS_JITTER
-                device_info["adapter-type"] = "wireless"
-            elif device_name=="lo":
-                jitter = LOCAL_JITTER
-                device_info["adapter-type"] = "loopback"
-            elif any(at.find(x)>=0 for x in ("ether", "local", "fiber", "1394", "infiniband")):
-                jitter = LOCAL_JITTER
-            elif at.find("wan")>=0:
-                jitter = WAN_JITTER
-            elif at.find("wireless")>=0 or at.find("wlan")>=0 or at.find("wifi")>=0 or at.find("80211")>=0:
-                jitter = WIRELESS_JITTER
-        if jitter>=0:
-            connection_data["jitter"] = jitter
-        adapter_type = device_value("adapter-type", str, default_adapter_type)
+        jitter = device_value("jitter", int, jitter_for_adapter_type(adapter_type))
+        log("get_caps() found jitter=%s", jitter)
+
+        connection_data = {}
         if adapter_type:
             connection_data["adapter-type"] = adapter_type
+        if jitter>=0:
+            connection_data["jitter"] = jitter
+        if socket_speed:
+            connection_data["speed"] = socket_speed
         log("get_caps() connection-data=%s", connection_data)
         caps["connection-data"] = connection_data
+
         bandwidth_limit = self.bandwidth_limit
         bandwidthlog("bandwidth-limit setting=%s, socket-speed=%s", self.bandwidth_limit, socket_speed)
         if bandwidth_limit is None:
@@ -186,7 +253,7 @@ class NetworkState(StubClientMixin):
                 #auto: use 80% of socket speed if we have it:
                 bandwidth_limit = socket_speed*AUTO_BANDWIDTH_PCT//100 or 0
             else:
-                bandwidth_limit = 0
+                bandwidth_limit = guess_bandwidth_limit(adapter_type)
         bandwidthlog("bandwidth-limit capability=%s", bandwidth_limit)
         if bandwidth_limit>0:
             caps["bandwidth-limit"] = bandwidth_limit
