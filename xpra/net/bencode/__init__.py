aa1e8642b427d6a09bc6972c7480133055e9ef4d
Antoine Martin <antoine@xpra.org>
2023-07-22 12:00:32 +0200

    fix XPRA_USE_CYTHON_BENCODE=0 as used by the unit tests

--- xpra/net/bencode/__init__.py
+++ xpra/net/bencode/__init__.py
@@ -10,12 +10,13 @@ def init() -> Tuple[Callable[[Any],bytes],Callable[[bytes],Any],Tuple[Any,...]]:
     from xpra.util import envbool
     if envbool("XPRA_USE_CYTHON_BENCODE", True):
         try:
-            from xpra.net.bencode import cython_bencode as bencoder  # type: ignore[attr-defined]
+            from xpra.net.bencode import cython_bencode # type: ignore[attr-defined]
+            return cython_bencode.bencode, cython_bencode.bdecode, cython_bencode.__version__
         except ImportError as e:
             from xpra.os_util import get_util_logger
             get_util_logger().warn("Warning: cannot load cython bencode module: %s", e)
-            from xpra.net.bencode import python_bencode as bencoder
-    return bencoder.bencode, bencoder.bdecode, bencoder.__version__
+    from xpra.net.bencode import python_bencode
+    return python_bencode.bencode, python_bencode.bdecode, python_bencode.__version__
 
 
 bencode, bdecode, __version__ = init()
