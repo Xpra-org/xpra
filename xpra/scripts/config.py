4e2465570d11d8a8368981a67617b5f59f706f62
Antoine Martin <antoine@xpra.org>
2023-07-26 14:16:10 +0200

    don't enable ffmpeg encoder by default

--- xpra/scripts/config.py
+++ xpra/scripts/config.py
@@ -1096,7 +1096,7 @@ def get_defaults():
                     "remote-xpra"       : get_remote_run_xpra_scripts(),
                     "encodings"         : ["all"],
                     "proxy-video-encoders" : ["none"],
-                    "video-encoders"    : ["all", "-gstreamer"],
+                    "video-encoders"    : ["all", "-gstreamer", "-ffmpeg"],
                     "csc-modules"       : ["all"],
                     "video-decoders"    : ["all"],
                     "speaker-codec"     : [],
