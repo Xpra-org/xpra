f3f18aa5459a8b335441ba7b7f3bb53bf0b4bd4f
Antoine Martin <antoine@xpra.org>
2023-07-27 23:27:11 +0200

    bump the debian script version

--- packaging/debian/xpra.sh
+++ packaging/debian/xpra.sh
@@ -7,7 +7,7 @@ if [ -z "${REPO_ARCH_PATH}" ]; then
 fi
 
 #find the latest version we can build:
-XPRA_TAR_XZ=`ls ../pkgs/xpra-5.*tar.xz | grep -v html5 | sort -V | tail -n 1`
+XPRA_TAR_XZ=`ls ../pkgs/xpra-6.*tar.xz | grep -v html5 | sort -V | tail -n 1`
 if [ -z "${XPRA_TAR_XZ}" ]; then
 	echo "no xpra source found"
 	exit 0
