f6a4f17bc8f7c28debf25a1d44a4bd01ef7756fa
totaam <antoine@xpra.org>
2023-08-15 23:29:02 +0700

    fix missing import

--- xpra/notifications/common.py
+++ xpra/notifications/common.py
@@ -154,4 +154,5 @@ def get_gtk_theme_icon(icon_string:str):
     mode = "RGB"
     if icon.get_has_alpha():
         mode = "RGBA"
+    Image = PIL_Image()
     return Image.frombytes(mode, (w, h), data, "raw", mode, rowstride)
