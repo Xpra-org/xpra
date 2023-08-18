38654545dfffd7de8fa72d18e4a9bc25ac631d3d
totaam <antoine@xpra.org>
2023-08-18 21:44:44 +0700

    fix xdg import

--- xpra/platform/posix/menu_helper.py
+++ xpra/platform/posix/menu_helper.py
@@ -78,9 +78,9 @@ themes : Dict[str,Any] = {}
 IconLoadingContext : Type = DummyContextManager
 if LOAD_FROM_THEME:
     try:
-        import xdg
-        IconTheme = xdg.IconTheme
-        Config = xdg.Config
+        from xdg import IconTheme as IT, Config as C
+        IconTheme = IT
+        Config = C
     except (ImportError, AttributeError):
         log("python xdg is missing", exc_info=True)
     else:
