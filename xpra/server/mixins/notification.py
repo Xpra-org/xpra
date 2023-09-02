a1fbc4a384a0d850927777ae3d880fc9a6bba298
totaam <antoine@xpra.org>
2023-09-02 23:34:39 +0700

    oops: fixup 5dd1336a5cc9f7863f796b5ca399f460c775a676

--- xpra/server/mixins/notification.py
+++ xpra/server/mixins/notification.py
@@ -148,7 +148,7 @@ class NotificationForwarder(StubServerMixin):
             ss.notify_close(int(nid))
 
 
-    def _process_set_notify(self, proto, packet : PacketType) -> None:
+    def _process_notification_status(self, proto, packet : PacketType) -> None:
         assert self.notifications, "cannot toggle notifications: the feature is disabled"
         ss = self.get_server_source(proto)
         if ss:
