d009888ab554e3ffd9b91ab7f618a2c3f5f1610b
totaam <antoine@xpra.org>
2023-09-15 20:36:45 +0700

    better test for gnome and variants

--- xpra/client/gtk3/gtk_client_base.py
+++ xpra/client/gtk3/gtk_client_base.py
@@ -801,7 +801,8 @@ class GTKXpraClient(GObjectXpraClient, UIXpraClient):
         if not is_Wayland():
             try:
                 from xpra.client.gtk3.statusicon_tray import GTKStatusIconTray
-                if os.environ.get("XDG_SESSION_DESKTOP", "").lower().find("gnome")>=0:
+                from xpra.os_util import is_gnome
+                if is_gnome():
                     # unlikely to work, so try last
                     tray_classes.append(GTKStatusIconTray)
                 else:
