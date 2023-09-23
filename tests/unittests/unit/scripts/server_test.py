e2f1bc766bb751dcc3c24b13dfcbf7ef974b7e78
totaam <antoine@xpra.org>
2023-09-23 22:26:11 +0700

    add server script test

---
+++ tests/unittests/unit/scripts/server_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This file is part of Xpra.
+# Copyright (C) 2023 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import unittest
+
+from xpra.scripts.server import is_splash_enabled
+
+class TestMain(unittest.TestCase):
+
+    def test_splash_enabled(self):
+        assert is_splash_enabled("foo", True, None, ":10") is False, "splash should not be enabled for daemons"
+        assert is_splash_enabled("foo", False, False, ":10") is False, "splash should not be enabled for splash=False"
+        assert is_splash_enabled("foo", False, True, ":10") is True, "splash should be enabled for splash=True"
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
