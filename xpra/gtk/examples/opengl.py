a3c265d6a1eceed08094423ececa2cc5ea032381
totaam <antoine@xpra.org>
2023-10-05 21:59:09 +0700

    use the program wrapper to name the process and window

--- xpra/gtk/examples/opengl.py
+++ xpra/gtk/examples/opengl.py
@@ -6,30 +6,32 @@
 def main(argv=()):
     from xpra.log import Logger
     log = Logger("opengl")
-    try:
-        from xpra.os_util import is_X11
-        from xpra.client.gl.window import get_gl_client_window_module, test_gl_client_window
-        if is_X11():
-            from xpra.x11.gtk3.display_source import init_gdk_display_source
-            init_gdk_display_source()
-        if "-v" in argv or "--verbose" in argv:
-            log.enable_debug()
-        opengl_props, gl_client_window_module = get_gl_client_window_module(True)
-        log("do_run_glcheck() opengl_props=%s, gl_client_window_module=%s", opengl_props, gl_client_window_module)
-        gl_client_window_class = gl_client_window_module.GLClientWindow
-        pixel_depth = 0
-        log("do_run_glcheck() gl_client_window_class=%s, pixel_depth=%s", gl_client_window_class, pixel_depth)
-        #if pixel_depth not in (0, 16, 24, 30) and pixel_depth<32:
-        #    pixel_depth = 0
-        draw_result = test_gl_client_window(gl_client_window_class, pixel_depth=pixel_depth, show=True)
-        success = draw_result.pop("success", False)
-        opengl_props.update(draw_result)
-        if not success:
-            opengl_props["safe"] = False
-        return 0
-    except Exception:
-        log("do_run_glcheck(..)", exc_info=True)
-        return 1
+    from xpra.platform import program_context
+    with program_context("opengl", "OpenGL"):
+        try:
+            from xpra.os_util import is_X11
+            from xpra.client.gl.window import get_gl_client_window_module, test_gl_client_window
+            if is_X11():
+                from xpra.x11.gtk3.display_source import init_gdk_display_source
+                init_gdk_display_source()
+            if "-v" in argv or "--verbose" in argv:
+                log.enable_debug()
+            opengl_props, gl_client_window_module = get_gl_client_window_module(True)
+            log("do_run_glcheck() opengl_props=%s, gl_client_window_module=%s", opengl_props, gl_client_window_module)
+            gl_client_window_class = gl_client_window_module.GLClientWindow
+            pixel_depth = 0
+            log("do_run_glcheck() gl_client_window_class=%s, pixel_depth=%s", gl_client_window_class, pixel_depth)
+            #if pixel_depth not in (0, 16, 24, 30) and pixel_depth<32:
+            #    pixel_depth = 0
+            draw_result = test_gl_client_window(gl_client_window_class, pixel_depth=pixel_depth, show=True)
+            success = draw_result.pop("success", False)
+            opengl_props.update(draw_result)
+            if not success:
+                opengl_props["safe"] = False
+            return 0
+        except Exception:
+            log("do_run_glcheck(..)", exc_info=True)
+            return 1
 
 if __name__ == "__main__":
     import sys
