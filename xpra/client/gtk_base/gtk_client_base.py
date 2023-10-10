89b4c484760d98bf7e4f087407d02c39e1e19d64
totaam <antoine@xpra.org>
2023-10-10 17:33:47 +0700

    prefer native trays to Gtk.StatusIcon

--- xpra/client/gtk_base/gtk_client_base.py
+++ xpra/client/gtk_base/gtk_client_base.py
@@ -644,7 +644,7 @@ class GTKXpraClient(GObjectXpraClient, UIXpraClient):
         if not is_Wayland():
             try:
                 from xpra.client.gtk_base.statusicon_tray import GTKStatusIconTray
-                if os.environ.get("XDG_SESSION_DESKTOP", "").lower().find("gnome")>=0:
+                if os.environ.get("XDG_SESSION_DESKTOP", "").lower().find("gnome")>=0 or WIN32 or OSX:
                     # unlikely to work, so try last
                     tray_classes.append(GTKStatusIconTray)
                 else:
