17ee27c9e1aa61a4a9bfddebd1f42de0d827d23f
totaam <antoine@xpra.org>
2023-11-15 22:21:25 +0700

    ffmpeg-xpra 6.1 for DEB

--- src/packaging/debian/ffmpeg-xpra.sh
+++ src/packaging/debian/ffmpeg-xpra.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -e "../pkgs/ffmpeg-4.4.tar.xz" ]; then
+if [ ! -e "../pkgs/ffmpeg-6.1.tar.xz" ]; then
 	echo "ffmpeg source not found"
 	exit 1
 fi
-tar -Jxf ../pkgs/ffmpeg-4.4.tar.xz
-mkdir ffmpeg-4.4/debian
-cp -apr ffmpeg-xpra/* ffmpeg-4.4/debian
-pushd ffmpeg-4.4
+tar -Jxf ../pkgs/ffmpeg-6.1.tar.xz
+mkdir ffmpeg-6.1/debian
+cp -apr ffmpeg-xpra/* ffmpeg-6.1/debian
+pushd ffmpeg-6.1
 apt-get install -y debhelper libvpx-dev libx264-dev yasm
 debuild -us -uc -b
 apt-get install -y ../ffmpeg-xpra*deb
