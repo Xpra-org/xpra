ae8e1ee940b9c6bf67f4600845b3b2b42dcb6998
totaam <antoine@xpra.org>
2023-11-15 22:25:04 +0700

    stretch is no longer supported

--- src/packaging/debian/build.sh
+++ src/packaging/debian/build.sh
@@ -5,10 +5,6 @@ if [ "${DEBUG:-0}" == "1" ]; then
 	BASH="bash -x"
 fi
 
-lsb_release -c | grep stretch
-if [ "$?" == "0" ]; then
-	$BASH ./ffmpeg-xpra.sh
-fi
 if [ `arch` == "x86_64" ]; then
 	$BASH ./libcuda1.sh
 	$BASH ./libnvidia-fbc1.sh
