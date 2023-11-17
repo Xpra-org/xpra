c819f21c03f2913a3aef9894477cf719afb53274
totaam <antoine@xpra.org>
2023-11-17 22:42:15 +0700

    #3313 self-signed certificates fail

--- xpra/net/common.py
+++ xpra/net/common.py
@@ -52,7 +52,7 @@ class ConnectionClosedException(Exception):
 
 MAX_PACKET_SIZE : int = envint("XPRA_MAX_PACKET_SIZE", 16*1024*1024)
 FLUSH_HEADER : bool = envbool("XPRA_FLUSH_HEADER", True)
-SSL_UPGRADE : bool = envbool("XPRA_SSL_UPGRADE", True)
+SSL_UPGRADE : bool = envbool("XPRA_SSL_UPGRADE", False)
 
 SOCKET_TYPES : tuple[str, ...] = ("tcp", "ws", "wss", "ssl", "ssh", "rfb", "vsock", "socket", "named-pipe", "quic")
 
