11ffe8d047ad8c5aa7b5285cb0bca91cc9dc15ac
totaam <antoine@xpra.org>
2023-11-17 18:14:46 +0700

    cosmetic

--- xpra/notifications/common.py
+++ xpra/notifications/common.py
@@ -12,7 +12,7 @@ from xpra.log import Logger
 
 log = Logger("dbus", "notify")
 
-IconData : TypeAlias = tuple[str,int,int,bytes]
+IconData : TypeAlias = tuple[str, int, int, bytes]
 
 
 def PIL_Image():
@@ -34,10 +34,10 @@ def parse_image_data(data) -> IconData | None:
         Image = PIL_Image()
         if not Image:
             return None
-        if channels==4:
+        if channels == 4:
             rgb_format = "BGRA"
             fmt = "RGBA"
-        elif channels==3:
+        elif channels == 3:
             rgb_format = "BGR"
             fmt = "RGB"
         else:
@@ -45,10 +45,11 @@ def parse_image_data(data) -> IconData | None:
         if isinstance(pixels, (list, tuple)):
             pixels = bytes(pixels)
         img = Image.frombytes(fmt, (width, height), pixels, "raw", rgb_format, rowstride)
-        if channels==4 and not has_alpha:
+        if channels == 4 and not has_alpha:
             img = img.convert("RGB")
         return image_data(img)
     return None
+
 
 def parse_image_path(path:str) -> IconData | None:
     if path and os.path.exists(path):
@@ -64,9 +65,10 @@ def parse_image_path(path:str) -> IconData | None:
             log.error(f" using path {path!r}:")
             estr = str(e)
             if estr.endswith(f"{path!r}"):
-                estr=estr[:-len(f"{path!r}")]
+                estr = estr[:-len(f"{path!r}")]
             log.error(f" {estr}")
     return None
+
 
 def image_data(img) -> IconData:
     buf = BytesIO()
@@ -78,10 +80,10 @@ def image_data(img) -> IconData:
 
 
 def get_notification_icon(icon_string:str) -> IconData | None:
-    #this method *must* be called from the UI thread
-    #since we may end up calling svg_to_png which uses Cairo
+    # this method *must* be called from the UI thread
+    # since we may end up calling svg_to_png which uses Cairo
     #
-    #the string may be:
+    # the string may be:
     # * a path which we will load using pillow
     # * a name we lookup in the current theme
     if not icon_string:
@@ -89,16 +91,16 @@ def get_notification_icon(icon_string:str) -> IconData | None:
     MAX_SIZE = 256
     img = None
     w = h = 0
-    from PIL import Image  #pylint: disable=import-outside-toplevel
+    from PIL import Image
     if os.path.isabs(icon_string):
         if os.path.exists(icon_string) and os.path.isfile(icon_string):
             try:
                 if icon_string.endswith(".svg"):
-                    from xpra.codecs.icon_util import svg_to_png  #pylint: disable=import-outside-toplevel
+                    from xpra.codecs.icon_util import svg_to_png
                     svg_data = load_binary_file(icon_string)
                     png_data = svg_to_png(icon_string, svg_data, MAX_SIZE, MAX_SIZE)
                     return "png", MAX_SIZE, MAX_SIZE, png_data
-                #should we be using pillow.decoder.open_only here? meh
+                # should we be using pillow.decoder.open_only here? meh
                 img = Image.open(icon_string)
             except Exception as e:
                 log("%s(%s)", Image.open, icon_string)
@@ -108,15 +110,15 @@ def get_notification_icon(icon_string:str) -> IconData | None:
             else:
                 w, h = img.size
         if not img:
-            #we failed to load it using the absolute path,
-            #so try to locate this icon without the path or extension:
+            # we failed to load it using the absolute path,
+            # so try to locate this icon without the path or extension:
             icon_string = os.path.splitext(os.path.basename(icon_string))[0]
     if not img:
-        #try to find it in the theme:
+        # try to find it in the theme:
         img = get_gtk_theme_icon(icon_string)
     if not img:
         return None
-    if w>MAX_SIZE or h>MAX_SIZE:
+    if w > MAX_SIZE or h > MAX_SIZE:
         img = img.resize((MAX_SIZE, MAX_SIZE), Image.Resampling.LANCZOS)
         w = h = MAX_SIZE
     buf = BytesIO()
