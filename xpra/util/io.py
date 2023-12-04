b6a456192a64005e14c234b0e3a0899e83030260
Antoine Martin <antoine@xpra.org>
2023-12-04 19:38:36 +0700

    missed files from 52800820f2f7c457c9ca9e256dd3bae2c0d9e1cb

---
+++ xpra/util/io.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+# This file is part of Xpra.
+# Copyright (C) 2013-2023 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+import re
+import stat
+import sys
+from subprocess import PIPE, Popen
+from tempfile import NamedTemporaryFile
+from typing import Callable, Any
+
+from xpra.common import noerr
+from xpra.util.str_fn import bytestostr
+
+util_logger = None
+
+
+def get_util_logger():
+    global util_logger
+    if not util_logger:
+        from xpra.log import Logger
+        util_logger = Logger("network", "util")
+    return util_logger
+
+
+def load_binary_file(filename) -> bytes:
+    if not os.path.exists(filename):
+        return b""
+    try:
+        with open(filename, "rb") as f:
+            return f.read()
+    except Exception as e:  # pragma: no cover
+        log = get_util_logger()
+        log.debug(f"load_binary_file({filename})", exc_info=True)
+        log.warn(f"Warning: failed to load {filename!r}")
+        log.warn(f" {e}")
+        return b""
+
+
+def filedata_nocrlf(filename:str) -> bytes:
+    v = load_binary_file(filename)
+    if v is None:
+        log = get_util_logger()
+        log.error(f"failed to load {filename!r}")
+        return b""
+    return v.strip(b"\n\r")
+
+
+def is_socket(sockpath:str, check_uid:int|None=None) -> bool:
+    try:
+        s = os.stat(sockpath)
+    except OSError as e:
+        get_util_logger().debug(f"is_socket({sockpath}) path cannot be accessed: {e}")
+        # socket cannot be accessed
+        return False
+    if not stat.S_ISSOCK(s.st_mode):
+        return False
+    if check_uid is not None and s.st_uid!=check_uid:
+        # socket uid does not match
+        get_util_logger().debug(f"is_socket({sockpath}) uid {s.st_uid} does not match {check_uid}")
+        return False
+    return True
+
+
+def is_writable(path : str, uid:int, gid:int) -> bool:
+    if uid == 0:
+        return True
+    try:
+        s = os.stat(path)
+    except OSError as e:
+        get_util_logger().debug(f"is_writable({path}) path cannot be accessed: {e}")
+        # socket cannot be accessed
+        return False
+    mode = s.st_mode
+    if s.st_uid==uid and mode & stat.S_IWUSR:
+        # uid has write access
+        return True
+    if s.st_gid==gid and mode & stat.S_IWGRP:
+        # gid has write access:
+        return True
+    return False
+
+
+def stderr_print(msg:str= "") -> bool:
+    stderr = sys.stderr
+    if stderr:
+        try:
+            noerr(stderr.write, msg + "\n")
+            noerr(stderr.flush)
+            return True
+        except (OSError, AttributeError):
+            pass
+    return False
+
+
+class CaptureStdErr:
+    __slots__ = ("savedstderr", "tmp", "stderr")
+    def __init__(self, *_args):
+        self.savedstderr = None
+        self.stderr = b""
+
+    def __enter__(self):
+        noerr(sys.stderr.flush) # <--- important when redirecting to files
+        self.savedstderr = os.dup(2)
+        self.tmp = NamedTemporaryFile(prefix="stderr")
+        fd = self.tmp.fileno()
+        os.dup2(fd, 2)
+        sys.stderr = os.fdopen(self.savedstderr, "w")
+
+    def __exit__(self, *_args):
+        try:
+            fd = self.tmp.fileno()
+            os.lseek(fd, 0, 0)
+            self.stderr = os.read(fd, 32768)
+            self.tmp.close()
+        except OSError as e:
+            noerr(sys.stderr.write, f"oops: {e}\n")
+        if self.savedstderr is not None:
+            os.dup2(self.savedstderr, 2)
+
+
+def path_permission_info(filename : str, ftype=None) -> tuple[str, ...]:
+    from xpra.os_util import POSIX
+    if not POSIX:
+        return ()
+    info = []
+    try:
+        stat_info = os.stat(filename)
+        if not ftype:
+            ftype = "file"
+            if os.path.isdir(filename):
+                ftype = "directory"
+        operm = oct(stat.S_IMODE(stat_info.st_mode))
+        info.append(f"permissions on {ftype} {filename}: {operm}")
+        # pylint: disable=import-outside-toplevel
+        import pwd
+        import grp
+        user = pwd.getpwuid(stat_info.st_uid)[0]
+        group = grp.getgrgid(stat_info.st_gid)[0]
+        info.append(f"ownership {user}:{group}")
+    except Exception as e:
+        info.append(f"failed to query path information for {filename!r}: {e}")
+    return tuple(info)
+
+
+def disable_stdout_buffering():
+    import gc
+    # Appending to gc.garbage is a way to stop an object from being
+    # destroyed.  If the old sys.stdout is ever collected, it will
+    # close() stdout, which is not good.
+    gc.garbage.append(sys.stdout)
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+
+def setbinarymode(fd:int):
+    from xpra.os_util import WIN32
+    if WIN32:
+        # turn on binary mode:
+        try:
+            import msvcrt
+            msvcrt.setmode(fd, os.O_BINARY)         # pylint: disable=no-member
+        except OSError:
+            get_util_logger().error("setting stdin to binary mode failed", exc_info=True)
+
+
+def livefds() -> set[int]:
+    live = set()
+    try:
+        maxfd = os.sysconf("SC_OPEN_MAX")
+    except (ValueError, AttributeError):
+        maxfd = 256
+    for fd in range(0, maxfd):
+        try:
+            s = os.fstat(fd)
+        except OSError:
+            continue
+        else:
+            if s:
+                live.add(fd)
+    return live
+
+
+def use_tty() -> bool:
+    from xpra.util.env import envbool
+    if envbool("XPRA_NOTTY", False):
+        return False
+    from xpra.platform.gui import use_stdin
+    return use_stdin()
+
+
+def use_gui_prompt() -> bool:
+    from xpra.os_util import WIN32, OSX
+    return WIN32 or OSX or not use_tty()
+
+
+class umask_context:
+    __slots__ = ("umask", "orig_umask")
+    def __init__(self, umask):
+        self.umask = umask
+    def __enter__(self):
+        self.orig_umask = os.umask(self.umask)
+    def __exit__(self, *_args):
+        os.umask(self.orig_umask)
+    def __repr__(self):
+        return f"umask_context({self.umask})"
+
+
+def find_lib_ldconfig(libname:str) -> str:
+    libname = re.escape(libname)
+    arch_map = {"x86_64": "libc6,x86-64"}
+    arch = arch_map.get(os.uname()[4], "libc6")
+    pattern = r'^\s+lib%s\.[^\s]+ \(%s(?:,.*?)?\) => (.*lib%s[^\s]+)' % (libname, arch, libname)
+    # try to find ldconfig first, which may not be on the $PATH
+    # (it isn't on Debian..)
+    ldconfig = "ldconfig"
+    for d in ("/sbin", "/usr/sbin"):
+        t = os.path.join(d, "ldconfig")
+        if os.path.exists(t):
+            ldconfig = t
+            break
+    import subprocess
+    p = subprocess.Popen(f"{ldconfig} -p", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    data = bytestostr(p.communicate()[0])
+    libpath = re.search(pattern, data, re.MULTILINE)        #@UndefinedVariable
+    if libpath:
+        return libpath.group(1)
+    return ""
+
+
+def find_lib(libname: str) -> str:
+    # it would be better to rely on dlopen to find the paths,
+    # but I cannot find a way of getting ctypes to tell us the path
+    # it found the library in
+    libpaths = os.environ.get("LD_LIBRARY_PATH", "").split(":")
+    if sys.platlibdir not in libpaths:
+        libpaths.append(sys.platlibdir)
+    for libpath in libpaths:
+        if not libpath or not os.path.exists(libpath):
+            continue
+        libname_so = os.path.join(libpath, libname)
+        if os.path.exists(libname_so):
+            return libname_so
+    return ""
+
+
+def pollwait(process, timeout=5) -> int | None:
+    from subprocess import TimeoutExpired
+    try:
+        return process.wait(timeout)
+    except TimeoutExpired:
+        return None
+
+
+def find_in_PATH(command:str) -> str | None:
+    path = os.environ.get("PATH", None)
+    if not path:
+        return None
+    paths = path.split(os.pathsep)
+    for p in paths:
+        f = os.path.join(p, command)
+        if os.path.isfile(f):
+            return f
+    return None
+
+
+def get_which_impl() -> Callable[[str],str]:
+    try:
+        from shutil import which
+        return which
+    except ImportError:
+        pass
+    try:
+        from distutils.spawn import find_executable # pylint: disable=deprecated-module
+        return find_executable
+    except ImportError:
+        return find_in_PATH
+
+
+def which(command:str) -> str | None:
+    find_executable = get_which_impl()
+    try:
+        return find_executable(command)
+    except Exception:
+        get_util_logger().debug(f"find_executable({command})", exc_info=True)
+        return None
+
+
+def get_status_output(*args, **kwargs) -> tuple[int,Any,Any]:
+    kwargs |= {
+        "stdout"    : PIPE,
+        "stderr"    : PIPE,
+        "universal_newlines"    : True,
+    }
+    try:
+        p = Popen(*args, **kwargs)
+    except Exception as e:
+        from xpra.log import Logger
+        log = Logger("util")
+        log.error(f"Error running {args},{kwargs}: {e}")
+        return -1, "", ""
+    stdout, stderr = p.communicate()
+    return p.returncode, stdout, stderr
+
+
+def get_proc_cmdline(pid: int) -> tuple[str, ...]:
+    from xpra.os_util import POSIX
+    if pid and POSIX:
+        # try to find the command via /proc:
+        proc_cmd_line = os.path.join("/proc", f"{pid}", "cmdline")
+        if os.path.exists(proc_cmd_line):
+            cmdline = load_binary_file(proc_cmd_line).rstrip(b"\0").split(b"\0")
+            try:
+                return tuple(x.decode() for x in cmdline)
+            except Exception:
+                return tuple(bytestostr(x) for x in cmdline)
+    return ()
---
+++ xpra/util/system.py
@@ -0,0 +1,395 @@
+#!/usr/bin/env python3
+# This file is part of Xpra.
+# Copyright (C) 2013-2023 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+import os
+import re
+import sys
+import signal
+import threading
+from subprocess import Popen, PIPE
+from threading import Thread
+from typing import Any
+
+from xpra.os_util import POSIX, LINUX, OSX, WIN32, no_idle
+from xpra.util.env import _saved_env
+from xpra.util.thread import main_thread
+from xpra.util.io import load_binary_file, get_util_logger
+from xpra.util.str_fn import bytestostr
+
+
+SIGNAMES : dict[int, str] = {}
+for signame in (sig for sig in dir(signal) if sig.startswith("SIG") and not sig.startswith("SIG_")):
+    try:
+        SIGNAMES[int(getattr(signal, signame))] = signame
+    except ValueError:
+        pass
+
+
+def set_proc_title(title) -> None:
+    try:
+        import setproctitle  # pylint: disable=import-outside-toplevel
+        setproctitle.setproctitle(title)  # @UndefinedVariable pylint: disable=c-extension-no-member
+    except ImportError as e:
+        get_util_logger().debug("setproctitle is not installed: %s", e)
+
+
+def register_SIGUSR_signals(idle_add=no_idle) -> None:
+    if os.name!="posix":
+        return
+    from xpra.util.pysystem import dump_gc_frames
+    from xpra.util.pysystem import dump_all_frames
+
+    def sigusr1(*_args):
+        log = get_util_logger().info
+        log("SIGUSR1")
+        idle_add(dump_all_frames, log)
+
+    def sigusr2(*_args):
+        log = get_util_logger().info
+        log("SIGUSR2")
+        idle_add(dump_gc_frames, log)
+    signal.signal(signal.SIGUSR1, sigusr1)
+    signal.signal(signal.SIGUSR2, sigusr2)
+
+
+def is_Wayland() -> bool:
+    return _is_Wayland(_saved_env)
+
+
+def _is_Wayland(env : dict) -> bool:
+    backend = env.get("GDK_BACKEND", "")
+    if backend == "wayland":
+        return True
+    return backend!="x11" and (
+        bool(env.get("WAYLAND_DISPLAY")) or env.get("XDG_SESSION_TYPE")=="wayland"
+        )
+
+
+def is_distribution_variant(variant=b"Debian") -> bool:
+    if not POSIX:
+        return False
+    try:
+        v = load_os_release_file()
+        return any(l.find(variant)>=0 for l in v.splitlines() if l.startswith(b"NAME="))
+    except Exception:
+        pass
+    try:
+        d = get_linux_distribution()[0]
+        if d== bytestostr(variant):
+            return True
+        if variant==b"RedHat" and d.startswith(bytestostr(variant)):
+            return True
+    except Exception:
+        pass
+    return False
+
+
+def get_distribution_version_id() -> str:
+    if not POSIX:
+        return ""
+    try:
+        v = load_os_release_file()
+        for line in v.splitlines():
+            l = line.decode()
+            if l.startswith("VERSION_ID="):
+                return l.split("=", 1)[1].strip('"')
+    except Exception:
+        pass
+    return ""
+
+
+os_release_file_data : bytes | None = None
+
+
+def load_os_release_file() -> bytes:
+    global os_release_file_data
+    if os_release_file_data is None:
+        try:
+            os_release_file_data = load_binary_file("/etc/os-release") or b""
+        except OSError: # pragma: no cover
+            os_release_file_data = b""
+    return os_release_file_data
+
+
+def is_Ubuntu() -> bool:
+    return is_distribution_variant(b"Ubuntu")
+
+
+def is_Debian() -> bool:
+    return is_distribution_variant(b"Debian")
+
+
+def is_Raspbian() -> bool:
+    return is_distribution_variant(b"Raspbian")
+
+
+def is_Fedora() -> bool:
+    return is_distribution_variant(b"Fedora")
+
+
+def is_Arch() -> bool:
+    return is_distribution_variant(b"Arch")
+
+
+def is_CentOS() -> bool:
+    return is_distribution_variant(b"CentOS")
+
+
+def is_AlmaLinux() -> bool:
+    return is_distribution_variant(b"AlmaLinux")
+
+
+def is_RockyLinux() -> bool:
+    return is_distribution_variant(b"Rocky Linux")
+
+
+def is_OracleLinux() -> bool:
+    return is_distribution_variant(b"Oracle Linux")
+
+
+def is_RedHat() -> bool:
+    return is_distribution_variant(b"RedHat")
+
+
+def is_openSUSE() -> bool:
+    return is_distribution_variant(b"openSUSE")
+
+
+def is_arm() -> bool:
+    import platform
+    return platform.uname()[4].startswith("arm")
+
+
+_linux_distribution = ("", "", "")
+
+
+def get_linux_distribution() -> tuple[str,str,str]:
+    global _linux_distribution
+    if LINUX and _linux_distribution == ("", "", ""):
+        # linux_distribution is deprecated in Python 3.5,
+        # and it causes warnings,
+        # so we use our own code first:
+        cmd = ["lsb_release", "-a"]
+        try:
+            p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+            out = p.communicate()[0]
+            assert p.returncode==0 and out
+        except Exception:
+            try:
+                import platform
+                _linux_distribution = platform.linux_distribution()  # pylint: disable=deprecated-method, no-member
+            except Exception:
+                _linux_distribution = ("unknown", "unknown", "unknown")
+        else:
+            d = {}
+            for line in bytestostr(out).splitlines():
+                parts = line.rstrip("\n\r").split(":", 1)
+                if len(parts) == 2:
+                    d[parts[0].lower().replace(" ", "_")] = parts[1].strip()
+            _linux_distribution = (
+                d.get("distributor_id","unknown"),
+                d.get("release", "unknown"),
+                d.get("codename", "unknown"),
+            )
+    return _linux_distribution
+
+
+def is_unity() -> bool:
+    d = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
+    return d.find("unity")>=0 or d.find("ubuntu") >= 0
+
+
+def is_gnome() -> bool:
+    if os.environ.get("XDG_SESSION_DESKTOP", "").split("-", 1)[0] in ("i3", "ubuntu", ):
+        # "i3-gnome" is not really gnome... ie: the systray does work!
+        return False
+    return os.environ.get("XDG_CURRENT_DESKTOP", "").lower().find("gnome")>=0
+
+
+def is_kde() -> bool:
+    return os.environ.get("XDG_CURRENT_DESKTOP", "").lower().find("kde")>=0
+
+
+def get_loaded_kernel_modules(*modlist):
+    loaded = []
+    if LINUX and os.path.exists("/sys/module"):
+        for mod in modlist:
+            if os.path.exists(f"/sys/module/{mod}"):  # pragma: no cover
+                loaded.append(mod)
+    return loaded
+
+
+def is_WSL() -> bool:
+    if not POSIX:
+        return False
+    r = None
+    for f in ("/proc/sys/kernel/osrelease", "/proc/version"):
+        r = load_binary_file(f)
+        if r:
+            break
+    return r is not None and r.find(b"Microsoft")>=0
+
+
+def get_generic_os_name() -> str:
+    return do_get_generic_os_name().lower()
+
+
+def do_get_generic_os_name() -> str:
+    for k,v in {
+            "linux"     : "Linux",
+            "darwin"    : "MacOS",
+            "win"       : "Win32",
+            "freebsd"   : "FreeBSD",
+            }.items():
+        if sys.platform.startswith(k):
+            return v
+    return sys.platform     # pragma: no cover
+
+
+def is_X11() -> bool:
+    if OSX or WIN32:
+        return False
+    if os.environ.get("XPRA_NOX11", "")=="1":
+        return False
+    try:
+        from xpra import x11
+        assert x11
+    except ImportError:
+        # x11 is not installed, so assume it isn't used
+        return False
+    try:
+        from xpra.x11.gtk3.bindings import is_X11_Display
+        return is_X11_Display()
+    except ImportError:
+        get_util_logger().debug("failed to load x11 bindings", exc_info=True)
+        return True
+
+
+def nn(x) -> str:
+    if x is None:
+        return ""
+    return str(x)
+
+
+def get_frame_info(ignore_threads:tuple[Thread,...]=()) -> dict[str|int,Any]:
+    info : dict[str|int,Any] = {
+        "count"         : threading.active_count() - len(ignore_threads),
+        "native-id"     : threading.get_native_id(),
+        }
+    try:
+        import traceback
+        thread_ident : dict[int | None, str | None] = {}
+        for t in threading.enumerate():
+            if t not in ignore_threads:
+                thread_ident[t.ident] = t.name
+            else:
+                thread_ident[t.ident] = None
+        thread_ident |= {
+                threading.current_thread().ident  : "info",
+                main_thread.ident                 : "main",
+                }
+        frames = sys._current_frames()  # pylint: disable=protected-access
+        stack = None
+        for i,frame_pair in enumerate(frames.items()):
+            stack = traceback.extract_stack(frame_pair[1])
+            tident = thread_ident.get(frame_pair[0], "unknown")
+            if tident is None:
+                continue
+            # sanitize stack to prevent None values (which cause encoding errors with the bencoder)
+            sanestack = []
+            for entry in stack:
+                sanestack.append(tuple(nn(x) for x in entry))
+            info[i] = {
+                ""          : tident,
+                "stack"     : sanestack,
+                }
+        del frames, stack
+    except Exception as e:
+        get_util_logger().error("failed to get frame info: %s", e)
+    return info
+
+
+def get_info_env() -> dict[str,str]:
+    filtered_env = os.environ.copy()
+    if filtered_env.get('XPRA_PASSWORD'):
+        filtered_env['XPRA_PASSWORD'] = "*****"
+    if filtered_env.get('XPRA_ENCRYPTION_KEY'):
+        filtered_env['XPRA_ENCRYPTION_KEY'] = "*****"
+    return filtered_env
+
+
+def get_sysconfig_info() -> dict[str,Any]:
+    import sysconfig
+    sysinfo : dict[str,Any] = {}
+    log = get_util_logger()
+    for attr in (
+        "platform",
+        "python-version",
+        "config-vars",
+        "paths",
+        ):
+        fn = "get_"+attr.replace("-", "_")
+        getter = getattr(sysconfig, fn, None)
+        if getter:
+            try:
+                sysinfo[attr] = getter()  # pylint: disable=not-callable
+            except ModuleNotFoundError:
+                log("sysconfig.%s", fn, exc_info=True)
+                if attr == "config-vars" and WIN32:
+                    continue
+                log.warn("Warning: failed to collect %s sysconfig information", attr)
+            except Exception:
+                log.error("Error calling sysconfig.%s", fn, exc_info=True)
+    return sysinfo
+
+
+def platform_release(release):
+    if OSX:
+        systemversion_plist = "/System/Library/CoreServices/SystemVersion.plist"
+        try:
+            import plistlib
+            with open(systemversion_plist, "rb") as f:
+                pl = plistlib.load(f)
+            return pl['ProductUserVisibleVersion']
+        except Exception as e:
+            get_util_logger().debug("platform_release(%s)", release, exc_info=True)
+            get_util_logger().warn("Warning: failed to get release information")
+            get_util_logger().warn(f" from {systemversion_plist}:")
+            get_util_logger().warn(f" {e}")
+    return release
+
+
+def platform_name(sys_platform=sys.platform, release=None) -> str:
+    if not sys_platform:
+        return "unknown"
+    platforms = {
+        "win32"    : "Microsoft Windows",
+        "cygwin"   : "Windows/Cygwin",
+        "linux.*"  : "Linux",
+        "darwin"   : "Mac OS X",
+        "freebsd.*": "FreeBSD",
+        "os2"      : "OS/2",
+        }
+
+    def rel(v):
+        values = [v]
+        if isinstance(release, (tuple, list)):
+            values += list(release)
+        else:
+            values.append(release)
+        return " ".join(str(x) for x in values if x and x != "unknown")
+    for k, v in platforms.items():
+        regexp = re.compile(k)
+        if regexp.match(sys_platform):
+            return rel(v)
+    return rel(sys_platform)
+
+
+def is_systemd_pid1() -> bool:
+    if not POSIX:
+        return False
+    d = load_binary_file("/proc/1/cmdline")
+    return d.find(b"/systemd") >= 0
