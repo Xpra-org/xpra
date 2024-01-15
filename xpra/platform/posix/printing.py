f9b7b0c9effda40a64ec690ef270ab7c96760351
Antoine Martin <antoine@xpra.org>
2024-01-15 21:48:38 +0700

    fix sonarqube linter warning

--- xpra/platform/posix/printing.py
+++ xpra/platform/posix/printing.py
@@ -1,5 +1,5 @@
 # This file is part of Xpra.
-# Copyright (C) 2014, 2015 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2014-2024 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
@@ -13,4 +13,5 @@ from xpra.platform.pycups_printing import (
     get_info,
 )
 
-assert get_printers and print_files and printing_finished and init_printing and cleanup_printing and get_info   # noqa: E501 type: ignore[truthy-function]
+for x in (get_printers, print_files, printing_finished, init_printing, cleanup_printing, get_info):
+    assert callable(x), f"{x} is not callable"
