923107bd5b2aa684687465cbd2b77d97600bf835
Antoine Martin <antoine@xpra.org>
2024-01-15 21:51:51 +0700

    match expected (unused) type

--- xpra/platform/win32/dotxpra.py
+++ xpra/platform/win32/dotxpra.py
@@ -42,7 +42,7 @@ class DotXpra:
         return [self.socket_path(local_display_name)]
 
     def socket_path(self, local_display_name: str):
-        return norm_makepath(None, local_display_name)
+        return norm_makepath("", local_display_name)
 
     def get_display_state(self, display: str):
         return self.get_server_state(PIPE_PREFIX+display)
