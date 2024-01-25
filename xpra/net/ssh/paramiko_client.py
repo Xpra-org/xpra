Xpra-org/xpra

commit 4b50a2560ab4d8dbf9054982c2862880c393bc95

#4083 remove 'nogssapi' workaround
distributions have had enough time to update their libraries

xpra/net/ssh/paramiko_client.py
@@ -17,7 +17,7 @@ from xpra.platform.paths import get_ssh_known_hosts_files
 from xpra.platform.info import get_username
 from xpra.scripts.config import parse_bool, TRUE_OPTIONS
 from xpra.scripts.pinentry import input_pass, confirm
-from xpra.net.ssh.util import nogssapi_context, get_default_keyfiles
+from xpra.net.ssh.util import get_default_keyfiles
 from xpra.net.bytestreams import SocketConnection, SOCKET_TIMEOUT
 from xpra.util.thread import start_thread
 from xpra.exit_codes import ExitCode
@@ -203,74 +203,73 @@ def connect_to(display_desc):
         log("connect_to(%s)", display_desc, exc_info=True)
         raise InitExit(ExitCode.SSH_FAILURE, msg) from None
 
-    with nogssapi_context():
-        from paramiko import SSHConfig, ProxyCommand
-        ssh_config = SSHConfig()
+    from paramiko import SSHConfig, ProxyCommand
+    ssh_config = SSHConfig()
 
-        def ssh_lookup(key):
-            return safe_lookup(ssh_config, key)
-        user_config_file = os.path.expanduser("~/.ssh/config")
-        sock = None
-        host_config = None
-        if os.path.exists(user_config_file):
-            with open(user_config_file, encoding="utf8") as f:
-                try:
-                    ssh_config.parse(f)
-                except Exception as e:
-                    log(f"parse({user_config_file})", exc_info=True)
-                    log.error(f"Error parsing {user_config_file!r}:")
-                    log.estr(e)
-            log(f"parsed user config {user_config_file!r}")
+    def ssh_lookup(key):
+        return safe_lookup(ssh_config, key)
+    user_config_file = os.path.expanduser("~/.ssh/config")
+    sock = None
+    host_config = None
+    if os.path.exists(user_config_file):
+        with open(user_config_file, encoding="utf8") as f:
             try:
-                log("%i hosts found", len(ssh_config.get_hostnames()))
-            except KeyError:
-                pass
-            host_config = ssh_lookup(host)
-            if host_config:
-                log(f"got host config for {host!r}: {host_config}")
-                host = host_config.get("hostname", host)
-                username = host_config.get("user", username)
-                port = host_config.get("port", port)
-                try:
-                    port = int(port)
-                except (TypeError, ValueError):
-                    raise InitExit(ExitCode.SSH_FAILURE, f"invalid ssh port specified: {port!r}") from None
-                proxycommand = host_config.get("proxycommand")
-                if proxycommand:
-                    log(f"found proxycommand={proxycommand!r} for host {host!r}")
-                    sock = ProxyCommand(proxycommand)
-                    log(f"ProxyCommand({proxycommand})={sock}")
-                    from xpra.util.child_reaper import getChildReaper
-                    cmd = getattr(sock, "cmd", [])
+                ssh_config.parse(f)
+            except Exception as e:
+                log(f"parse({user_config_file})", exc_info=True)
+                log.error(f"Error parsing {user_config_file!r}:")
+                log.estr(e)
+        log(f"parsed user config {user_config_file!r}")
+        try:
+            log("%i hosts found", len(ssh_config.get_hostnames()))
+        except KeyError:
+            pass
+        host_config = ssh_lookup(host)
+        if host_config:
+            log(f"got host config for {host!r}: {host_config}")
+            host = host_config.get("hostname", host)
+            username = host_config.get("user", username)
+            port = host_config.get("port", port)
+            try:
+                port = int(port)
+            except (TypeError, ValueError):
+                raise InitExit(ExitCode.SSH_FAILURE, f"invalid ssh port specified: {port!r}") from None
+            proxycommand = host_config.get("proxycommand")
+            if proxycommand:
+                log(f"found proxycommand={proxycommand!r} for host {host!r}")
+                sock = ProxyCommand(proxycommand)
+                log(f"ProxyCommand({proxycommand})={sock}")
+                from xpra.util.child_reaper import getChildReaper
+                cmd = getattr(sock, "cmd", [])
 
-                    def proxycommand_ended(proc):
-                        log(f"proxycommand_ended({proc}) exit code={proc.poll()}")
-                    getChildReaper().add_process(sock.process, "paramiko-ssh-client", cmd, True, True,
-                                                 callback=proxycommand_ended)
-                    proxy_keys = get_keyfiles(host_config, "proxy_key")
-                    log(f"{proxy_keys=}")
-                    from paramiko.client import SSHClient
-                    ssh_client = SSHClient()
-                    ssh_client.load_system_host_keys()
-                    log("ssh proxy command connect to %s", (host, port, sock))
-                    ssh_client.connect(host, port, sock=sock)
-                    transport = ssh_client.get_transport()
-                    do_connect_to(transport, host,
-                                  username, password,
-                                  host_config or ssh_lookup("*"),
-                                  proxy_keys,
-                                  paramiko_config)
-                    chan = run_remote_xpra(transport, proxy_command, remote_xpra, socket_dir, display_as_args)
-                    peername = (host, port)
-                    conn = SSHProxyCommandConnection(chan, peername, peername, socket_info)
-                    conn.target = host_target_string("ssh", username, host, port, display)
-                    conn.timeout = SOCKET_TIMEOUT
-                    conn.start_stderr_reader()
-                    conn.process = (sock.process, "ssh", cmd)
-                    from xpra.net import bytestreams
-                    from paramiko.ssh_exception import ProxyCommandFailure
-                    bytestreams.CLOSED_EXCEPTIONS = tuple(list(bytestreams.CLOSED_EXCEPTIONS)+[ProxyCommandFailure])
-                    return conn
+                def proxycommand_ended(proc):
+                    log(f"proxycommand_ended({proc}) exit code={proc.poll()}")
+                getChildReaper().add_process(sock.process, "paramiko-ssh-client", cmd, True, True,
+                                             callback=proxycommand_ended)
+                proxy_keys = get_keyfiles(host_config, "proxy_key")
+                log(f"{proxy_keys=}")
+                from paramiko.client import SSHClient
+                ssh_client = SSHClient()
+                ssh_client.load_system_host_keys()
+                log("ssh proxy command connect to %s", (host, port, sock))
+                ssh_client.connect(host, port, sock=sock)
+                transport = ssh_client.get_transport()
+                do_connect_to(transport, host,
+                              username, password,
+                              host_config or ssh_lookup("*"),
+                              proxy_keys,
+                              paramiko_config)
+                chan = run_remote_xpra(transport, proxy_command, remote_xpra, socket_dir, display_as_args)
+                peername = (host, port)
+                conn = SSHProxyCommandConnection(chan, peername, peername, socket_info)
+                conn.target = host_target_string("ssh", username, host, port, display)
+                conn.timeout = SOCKET_TIMEOUT
+                conn.start_stderr_reader()
+                conn.process = (sock.process, "ssh", cmd)
+                from xpra.net import bytestreams
+                from paramiko.ssh_exception import ProxyCommandFailure
+                bytestreams.CLOSED_EXCEPTIONS = tuple(list(bytestreams.CLOSED_EXCEPTIONS)+[ProxyCommandFailure])
+                return conn
 
         keys = get_keyfiles(host_config)
         from xpra.net.socket_util import socket_connect

xpra/net/socket_util.py
@@ -316,11 +316,8 @@ def create_sockets(opts, error_cb:Callable, retry:int=0):
     ssh_upgrades = opts.ssh_upgrade
     if ssh_upgrades:
         try:
-            from xpra.net.ssh.util import nogssapi_context
-            with nogssapi_context():
-                with SilenceWarningsContext(DeprecationWarning):
-                    import paramiko
-            assert paramiko
+            with SilenceWarningsContext(DeprecationWarning):
+                import paramiko
         except ImportError as e:
             from xpra.log import Logger
             sshlog = Logger("ssh")

xpra/net/ssh/util.py
@@ -28,12 +28,6 @@ class nomodule_context:
         return f"nomodule_context({self.module_name})"
 
 
-class nogssapi_context(nomodule_context):
-
-    def __init__(self):
-        super().__init__("gssapi")
-
-
 def get_default_keyfiles() -> list[str]:
     dkf = os.environ.get("XPRA_SSH_DEFAULT_KEYFILES", None)
     if dkf is not None:

xpra/scripts/parsing.py
@@ -612,9 +612,7 @@ def parse_ssh_option(ssh_setting:str) -> list[str]:
         #try paramiko:
         from xpra.log import is_debug_enabled, Logger
         try:
-            from xpra.net.ssh.util import nogssapi_context
-            with nogssapi_context():
-                import paramiko
+            import paramiko
             assert paramiko, "paramiko not found"
             ssh_cmd = ["paramiko"]
             if is_debug_enabled("ssh"):

commit a680dd78be6408cee6a43e512c9fa6de3f04de29

fix breakage introduced in 4b50a2560ab4d8dbf9054982c2862880c393bc95
removing the nogssapi context ended up over-indenting important code

xpra/net/ssh/paramiko_client.py
@@ -273,96 +273,96 @@ def connect_to(display_desc):
             bytestreams.CLOSED_EXCEPTIONS = tuple(list(bytestreams.CLOSED_EXCEPTIONS)+[ProxyCommandFailure])
             return conn
 
-        keys = get_keyfiles()
-        from xpra.net.socket_util import socket_connect
-        if "proxy_host" in display_desc:
-            proxy_host = display_desc["proxy_host"]
-            proxy_port = int(display_desc.get("proxy_port", 22))
-            proxy_username = display_desc.get("proxy_username", username)
-            proxy_password = display_desc.get("proxy_password", password)
-            proxy_keys = get_keyfiles("proxy_key")
-            sock = socket_connect(proxy_host, proxy_port)
-            if not sock:
-                fail(f"SSH proxy transport failed to connect to {proxy_host}:{proxy_port}")
-            middle_transport = do_connect(sock, proxy_host,
-                                          proxy_username, proxy_password,
-                                          ssh_lookup(host) or ssh_lookup("*"),
-                                          proxy_keys,
-                                          paramiko_config)
-            log("Opening proxy channel")
-            chan_to_middle = middle_transport.open_channel("direct-tcpip", (host, port), ("localhost", 0))
-            transport = do_connect(chan_to_middle, host,
-                                   username, password,
-                                   host_config,
-                                   keys,
-                                   paramiko_config)
-            chan = run_remote_xpra(transport, proxy_command, remote_xpra, socket_dir, display_as_args)
-            peername = (host, port)
-            conn = SSHProxyCommandConnection(chan, peername, peername, socket_info)
-            to_str = host_target_string("ssh", username, host, port, display)
-            proxy_str = host_target_string("ssh", proxy_username, proxy_host, proxy_port, None)
-            conn.target = f"{to_str} via {proxy_str}"
-            conn.timeout = SOCKET_TIMEOUT
-            conn.start_stderr_reader()
-            return conn
-
-        # plain TCP connection to the server,
-        # we open it then give the socket to paramiko:
-        auth_modes = get_auth_modes(paramiko_config, host_config, password)
-        log(f"authentication modes={auth_modes}")
-        sock = None
-        transport = None
-        sockname = peername = host
-        while not transport:
-            sock = socket_connect(host, port)
-            if not sock:
-                fail(f"SSH failed to connect to {host}:{port}")
-            sockname = sock.getsockname()
-            peername = sock.getpeername()
-            log(f"paramiko socket_connect: sockname={sockname}, peername={peername}")
-            try:
-                transport = do_connect(sock, host, username, password,
-                                       host_config,
-                                       keys,
-                                       paramiko_config,
-                                       auth_modes)
-            except SSHAuthenticationError as e:
-                log(f"paramiko authentication errors on socket {sock} with modes {auth_modes}: {e.errors}",
-                    exc_info=True)
-                pw_errors = []
-                for errs in e.errors.values():
-                    pw_errors += errs
-                if ("key" in auth_modes or "agent" in auth_modes) and PARAMIKO_SESSION_LOST in pw_errors:
-                    # try connecting again but without 'key' and 'agent' authentication:
-                    # see https://github.com/Xpra-org/xpra/issues/3223
-                    for m in ("key", "agent"):
-                        try:
-                            auth_modes.remove(m)
-                        except KeyError:
-                            pass
-                    log.info(f"retrying SSH authentication with modes {csv(auth_modes)}")
-                    continue
-                raise
-            finally:
-                if sock and not transport:
-                    noerr(sock.shutdown)
-                    noerr(sock.close)
-                    sock = None
-
-        remote_port = display_desc.get("remote_port", 0)
-        if remote_port:
-            # we want to connect directly to a remote port,
-            # we don't need to run a command
-            chan = transport.open_channel("direct-tcpip", ("localhost", remote_port), ('localhost', 0))
-            log(f"direct channel to remote port {remote_port} : {chan}")
-        else:
-            chan = run_remote_xpra(transport, proxy_command, remote_xpra,
-                                   socket_dir, display_as_args, paramiko_config)
-        conn = SSHSocketConnection(chan, sock, sockname, peername, (host, port), socket_info)
-        conn.target = host_target_string("ssh", username, host, port, display)
+    keys = get_keyfiles()
+    from xpra.net.socket_util import socket_connect
+    if "proxy_host" in display_desc:
+        proxy_host = display_desc["proxy_host"]
+        proxy_port = int(display_desc.get("proxy_port", 22))
+        proxy_username = display_desc.get("proxy_username", username)
+        proxy_password = display_desc.get("proxy_password", password)
+        proxy_keys = get_keyfiles("proxy_key")
+        sock = socket_connect(proxy_host, proxy_port)
+        if not sock:
+            fail(f"SSH proxy transport failed to connect to {proxy_host}:{proxy_port}")
+        middle_transport = do_connect(sock, proxy_host,
+                                      proxy_username, proxy_password,
+                                      ssh_lookup(host) or ssh_lookup("*"),
+                                      proxy_keys,
+                                      paramiko_config)
+        log("Opening proxy channel")
+        chan_to_middle = middle_transport.open_channel("direct-tcpip", (host, port), ("localhost", 0))
+        transport = do_connect(chan_to_middle, host,
+                               username, password,
+                               host_config,
+                               keys,
+                               paramiko_config)
+        chan = run_remote_xpra(transport, proxy_command, remote_xpra, socket_dir, display_as_args)
+        peername = (host, port)
+        conn = SSHProxyCommandConnection(chan, peername, peername, socket_info)
+        to_str = host_target_string("ssh", username, host, port, display)
+        proxy_str = host_target_string("ssh", proxy_username, proxy_host, proxy_port, None)
+        conn.target = f"{to_str} via {proxy_str}"
         conn.timeout = SOCKET_TIMEOUT
         conn.start_stderr_reader()
         return conn
+
+    # plain TCP connection to the server,
+    # we open it then give the socket to paramiko:
+    auth_modes = get_auth_modes(paramiko_config, host_config, password)
+    log(f"authentication modes={auth_modes}")
+    sock = None
+    transport = None
+    sockname = peername = host
+    while not transport:
+        sock = socket_connect(host, port)
+        if not sock:
+            fail(f"SSH failed to connect to {host}:{port}")
+        sockname = sock.getsockname()
+        peername = sock.getpeername()
+        log(f"paramiko socket_connect: sockname={sockname}, peername={peername}")
+        try:
+            transport = do_connect(sock, host, username, password,
+                                   host_config,
+                                   keys,
+                                   paramiko_config,
+                                   auth_modes)
+        except SSHAuthenticationError as e:
+            log(f"paramiko authentication errors on socket {sock} with modes {auth_modes}: {e.errors}",
+                exc_info=True)
+            pw_errors = []
+            for errs in e.errors.values():
+                pw_errors += errs
+            if ("key" in auth_modes or "agent" in auth_modes) and PARAMIKO_SESSION_LOST in pw_errors:
+                # try connecting again but without 'key' and 'agent' authentication:
+                # see https://github.com/Xpra-org/xpra/issues/3223
+                for m in ("key", "agent"):
+                    try:
+                        auth_modes.remove(m)
+                    except KeyError:
+                        pass
+                log.info(f"retrying SSH authentication with modes {csv(auth_modes)}")
+                continue
+            raise
+        finally:
+            if sock and not transport:
+                noerr(sock.shutdown)
+                noerr(sock.close)
+                sock = None
+
+    remote_port = display_desc.get("remote_port", 0)
+    if remote_port:
+        # we want to connect directly to a remote port,
+        # we don't need to run a command
+        chan = transport.open_channel("direct-tcpip", ("localhost", remote_port), ('localhost', 0))
+        log(f"direct channel to remote port {remote_port} : {chan}")
+    else:
+        chan = run_remote_xpra(transport, proxy_command, remote_xpra,
+                               socket_dir, display_as_args, paramiko_config)
+    conn = SSHSocketConnection(chan, sock, sockname, peername, (host, port), socket_info)
+    conn.target = host_target_string("ssh", username, host, port, display)
+    conn.timeout = SOCKET_TIMEOUT
+    conn.start_stderr_reader()
+    return conn
 
 
 AUTH_MODES: tuple[str, ...] = ("none", "agent", "key", "password")