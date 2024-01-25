Xpra-org/xpra

commit 28adf9b8a2740aac74f5507bdf7302b73de28613

#2303 modifying os.environ is not thread safe
so use an extra function argument instead

xpra/net/ssh/agent.py
@@ -82,15 +82,18 @@ def clean_agent_socket(uuid: str = "") -> None:
         log.estr(e)
 
 
-def setup_proxy_ssh_socket(cmdline: Iterable[str], auth_sock: str = os.environ.get("SSH_AUTH_SOCK", "")) -> str:
+def setup_proxy_ssh_socket(
+        cmdline: Iterable[str],
+        auth_sock: str = os.environ.get("SSH_AUTH_SOCK", ""),
+        session_dir: str = os.environ.get("XPRA_SESSION_DIR"),
+) -> str:
     log(f"setup_proxy_ssh_socket({cmdline}, {auth_sock!r}")
     # this is the socket path that the ssh client wants us to use:
     # ie: "SSH_AUTH_SOCK=/tmp/ssh-XXXX4KyFhe/agent.726992"
     if not auth_sock or not os.path.exists(auth_sock) or not is_socket(auth_sock):
         log(f"setup_proxy_ssh_socket invalid SSH_AUTH_SOCK={auth_sock!r}")
         return ""
-    session_dir = os.environ.get("XPRA_SESSION_DIR")
-    if not session_dir or not os.path.exists(session_dir):
+    if not session_dir or not os.path.exists(session_dir) or not os.path.isdir(session_dir):
         log(f"setup_proxy_ssh_socket invalid XPRA_SESSION_DIR={session_dir!r}")
         return ""
     # locate the ssh agent uuid,

xpra/scripts/main.py
@@ -3020,9 +3020,8 @@ def run_proxy(error_cb, opts, script_file, cmdline, args, mode, defaults) -> Exi
                 #and we use an OSEnvContext to avoid polluting the env with it
                 #(we need it to use the server ssh agent path functions)
                 session_dir = get_session_dir("attach", opts.sessions_dir, display_name, getuid())
-                with OSEnvContext(XPRA_SESSION_DIR=session_dir):
-                    #ie: "/run/user/$UID/xpra/$DISPLAY/ssh/$UUID
-                    setup_proxy_ssh_socket(cmdline)
+                #ie: "/run/user/$UID/xpra/$DISPLAY/ssh/$UUID
+                setup_proxy_ssh_socket(cmdline, session_dir=session_dir)
             except OSError:
                 sshlog = Logger("ssh")
                 sshlog.error("Error setting up client ssh agent forwarding socket", exc_info=True)

commit 5c9245ddb0d60f7058814a9c5398fdd15dddaa4a

fix breakage introduced by 28adf9b8a2740aac74f5507bdf7302b73de28613
more ssh agent socket functions need a valid 'session_dir' to work, pass it through

xpra/net/ssh/agent.py
@@ -12,16 +12,15 @@ from xpra.log import Logger
 log = Logger("server", "ssh")
 
 
-def ssh_dir_path() -> str:
-    session_dir = os.environ["XPRA_SESSION_DIR"]
+def ssh_dir_path(session_dir: str = os.environ.get("XPRA_SESSION_DIR")) -> str:
     return os.path.join(session_dir, "ssh")
 
 
-def setup_ssh_auth_sock() -> str:
+def setup_ssh_auth_sock(session_dir: str) -> str:
     # the 'ssh' dir contains agent socket symlinks to the real agent socket,
     # so we can just update the "agent" symlink
     # which is the one that applications are told to use
-    ssh_dir = ssh_dir_path()
+    ssh_dir = ssh_dir_path(session_dir)
     if not os.path.exists(ssh_dir):
         os.mkdir(ssh_dir, 0o700)
     # ie: "/run/user/1000/xpra/10/ssh/agent"
@@ -40,8 +39,8 @@ def setup_ssh_auth_sock() -> str:
     return agent_sockpath
 
 
-def get_ssh_agent_path(filename: str) -> str:
-    ssh_dir = ssh_dir_path()
+def get_ssh_agent_path(filename: str, session_dir: str = os.environ.get("XPRA_SESSION_DIR")) -> str:
+    ssh_dir = ssh_dir_path(session_dir)
     if "/" in filename or ".." in filename:
         raise ValueError(f"illegal characters found in ssh agent filename {filename!r}")
     return os.path.join(ssh_dir, filename or "agent.default")
@@ -94,7 +93,7 @@ def setup_proxy_ssh_socket(
         log(f"setup_proxy_ssh_socket invalid SSH_AUTH_SOCK={auth_sock!r}")
         return ""
     if not session_dir or not os.path.exists(session_dir) or not os.path.isdir(session_dir):
-        log(f"setup_proxy_ssh_socket invalid XPRA_SESSION_DIR={session_dir!r}")
+        log(f"setup_proxy_ssh_socket invalid session_dir={session_dir!r}")
         return ""
     # locate the ssh agent uuid,
     # which is used to derive the agent path symlink
@@ -110,7 +109,7 @@ def setup_proxy_ssh_socket(
         log(f"setup_proxy_ssh_socket invalid SSH_AGENT_UUID={agent_uuid!r}")
         return ""
     # ie: "/run/user/$UID/xpra/$DISPLAY/ssh/$UUID
-    agent_uuid_sockpath = get_ssh_agent_path(agent_uuid)
+    agent_uuid_sockpath = get_ssh_agent_path(agent_uuid, session_dir)
     if os.path.exists(agent_uuid_sockpath) or os.path.islink(agent_uuid_sockpath):
         if is_socket(agent_uuid_sockpath):
             log(f"setup_proxy_ssh_socket keeping existing valid socket {agent_uuid_sockpath!r}")

xpra/scripts/main.py
@@ -3016,9 +3016,6 @@ def run_proxy(error_cb, opts, script_file, cmdline, args, mode, defaults) -> Exi
         else:
             try:
                 from xpra.scripts.server import get_session_dir
-                #env var `XPRA_SESSION_DIR` should not be set in an ssh session,
-                #and we use an OSEnvContext to avoid polluting the env with it
-                #(we need it to use the server ssh agent path functions)
                 session_dir = get_session_dir("attach", opts.sessions_dir, display_name, getuid())
                 #ie: "/run/user/$UID/xpra/$DISPLAY/ssh/$UUID
                 setup_proxy_ssh_socket(cmdline, session_dir=session_dir)

xpra/scripts/server.py
@@ -386,7 +386,7 @@ def write_displayfd(display_name : str, fd : int) -> None:
         log.estr(e)
 
 
-def get_session_dir(mode:str, sessions_dir:str, display_name:str, uid:int) -> str:
+def get_session_dir(mode: str, sessions_dir: str, display_name: str, uid: int) -> str:
     session_dir = osexpand(os.path.join(sessions_dir, display_name.lstrip(":")), uid=uid)
     if not os.path.exists(session_dir):
         ROOT = POSIX and getuid()==0
@@ -1413,7 +1413,7 @@ def _do_run_server(script_file:str, cmdline,
         progress(50, "setup ssh agent forwarding")
         try:
             from xpra.net.ssh.agent import setup_ssh_auth_sock
-            ssh_auth_sock = setup_ssh_auth_sock()
+            ssh_auth_sock = setup_ssh_auth_sock(session_dir)
             os.environ["SSH_AUTH_SOCK"] = ssh_auth_sock
             protected_env["SSH_AUTH_SOCK"] = ssh_auth_sock
         except Exception as e: