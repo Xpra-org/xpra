3744bd1213e27dc42b411189a2893485bee7b6ce
Antoine Martin <antoine@xpra.org>
2024-01-29 12:49:19 +0700

    #4044 don't try to print a size we don't have

--- xpra/platform/win32/shadow_server.py
+++ xpra/platform/win32/shadow_server.py
@@ -311,7 +311,11 @@ class ShadowServer(GTKShadowServerBase):
             self.session_name = desktop_name
 
     def print_screen_info(self) -> None:
-        w, h = self.get_root_window_size()
+        size = self.get_root_window_size()
+        if not size:
+            # we probably don't have access to the screen
+            return
+        w, h = size
         try:
             display = prettify_plug_name(self.root.get_screen().get_display().get_name())
         except Exception:
