90f302db6641d819ce2f97269d5d57c2f52db717
Antoine Martin <antoine@xpra.org>
2024-02-03 15:05:32 +0700

    #3542 always initialize pointer position
    and always restore the cursor if we're moving it

--- xpra/server/shadow/gtk_shadow_server_base.py
+++ xpra/server/shadow/gtk_shadow_server_base.py
@@ -303,21 +303,22 @@ class GTKShadowServerBase(ShadowServerBase, GTKServerBase):
             self.suspend_cursor(proto)
             return None
         pointer = super()._adjust_pointer(proto, device_id, wid, opointer)
+        ax = x = int(pointer[0])
+        ay = y = int(pointer[1])
         if window:
             # the window may be at an offset (multi-window for multi-monitor):
             wx, wy, ww, wh = window.get_geometry()
             # or maybe the pointer is off-screen:
-            x, y = pointer[:2]
             if x < 0 or x >= ww or y < 0 or y >= wh:
                 self.suspend_cursor(proto)
                 return None
+            # note: with x11 shadow servers,
+            # X11ServerCore._get_pointer_abs_coordinates() will recalculate
+            # the absolute coordinates from the relative ones,
+            # and it should end up with the same values we calculated here
+            ax = x + wx
+            ay = y + wy
         self.restore_cursor(proto)
-        # note: with x11 shadow servers,
-        # X11ServerCore._get_pointer_abs_coordinates() will recalculate
-        # the absolute coordinates from the relative ones,
-        # and it should end up with the same values we calculated here
-        ax = x + wx
-        ay = y + wy
         return [ax, ay] + list(pointer[2:])
 
     def get_pointer_position(self):
