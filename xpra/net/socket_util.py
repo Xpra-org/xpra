797e3ae25e62f9ba5ed49d2f21bbb1fd79965c06
Antoine Martin <antoine@xpra.org>
2024-02-19 21:28:16 +0700

    typo caused ssh upgrade flag to remain enabled
    even when paramiko is not installed and we can't handle ssh connections,
    causing hard to decipher errors further down the line

--- xpra/net/socket_util.py
+++ xpra/net/socket_util.py
@@ -246,7 +246,7 @@ def create_sockets(opts, error_cb):
             from xpra.log import Logger
             sshlog = Logger("ssh")
             sshlog("import paramiko", exc_info=True)
-            opts.ssh_upgrades = False
+            opts.ssh_upgrade = False
             sshlog.error("Error: cannot enable SSH socket upgrades:")
             sshlog.error(" %s", e)
     log("setting up SSL sockets: %s", csv(bind_ssl))
