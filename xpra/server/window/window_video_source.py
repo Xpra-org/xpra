Xpra-org/xpra

commit 9656a14a46e323e5bf3b6cbf73532b02b5002d5a

remove pointless empty client encoding option
only populate the 'cuda-device-context' key if we have a value

xpra/server/window/window_video_source.py
@@ -1911,7 +1911,9 @@ class WindowVideoSource(WindowSource):
 
     def get_video_encoder_options(self, encoding, width, height) -> Dict[str,Any]:
         #tweaks for "real" video:
-        opts = {"cuda-device-context" : self.cuda_device_context}
+        opts = {}
+        if self.cuda_device_context:
+            options["cuda-device-context"] = self.cuda_device_context
         if not self._fixed_quality and not self._fixed_speed and self._fixed_min_quality<50:
             #only allow bandwidth to drive video encoders
             #when we don't have strict quality or speed requirements:

xpra/server/window/window_source.py
@@ -2685,7 +2685,8 @@ class WindowSource(WindowIconSource):
         log("make_data_packet: image=%s, damage data: %s", image, (self.wid, x, y, w, h, coding))
         start = monotonic()
 
-        options["cuda-device-context"] = self.cuda_device_context
+        if self.cuda_device_context:
+            options["cuda-device-context"] = self.cuda_device_context
         #by default, don't set rowstride (the container format will take care of providing it):
         encoder = self._encoders.get(coding)
         if encoder is None:

commit 062be135cbe8298ba775582b70e5731e04a16d3f

fix typo
This was introduced in commit 9656a14a46e323e5bf3b6cbf73532b02b5002d5a.

xpra/server/window/window_video_source.py
@@ -1922,7 +1922,7 @@ class WindowVideoSource(WindowSource):
         #tweaks for "real" video:
         opts = {}
         if self.cuda_device_context:
-            options["cuda-device-context"] = self.cuda_device_context
+            opts["cuda-device-context"] = self.cuda_device_context
         if not self._fixed_quality and not self._fixed_speed and self._fixed_min_quality<50:
             #only allow bandwidth to drive video encoders
             #when we don't have strict quality or speed requirements: