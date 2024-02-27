Xpra-org/xpra

commit 13288d42587ca532e5a0e8e0a235207002bd6f7d

#3930 don't import from 'typing' if we don't have to

xpra/platform/darwin/shadow_server.py
@@ -4,7 +4,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from typing import Dict, Any, List, Tuple
+from typing import Any
 
 import Quartz.CoreGraphics as CG    #@UnresolvedImport
 
@@ -25,7 +25,7 @@ log = Logger("shadow", "osx")
 
 USE_TIMER = envbool("XPRA_OSX_SHADOW_USE_TIMER", False)
 
-ALPHA : Dict[int,str] = {
+ALPHA : dict[int,str] = {
          CG.kCGImageAlphaNone                  : "AlphaNone",
          CG.kCGImageAlphaPremultipliedLast     : "PremultipliedLast",
          CG.kCGImageAlphaPremultipliedFirst    : "PremultipliedFirst",
@@ -67,12 +67,12 @@ class OSXRootCapture:
         rect = (x, y, width, height)
         return get_CG_imagewrapper(rect)
 
-    def get_info(self) -> Dict[str,Any]:
+    def get_info(self) -> dict[str,Any]:
         return {
             "type" : "CoreGraphics",
             }
 
-    def take_screenshot(self) -> Tuple[int,int,str,int,bytes]:
+    def take_screenshot(self) -> tuple[int,int,str,int,bytes]:
         log("grabbing screenshot")
         return take_screenshot()
 
@@ -83,8 +83,8 @@ class ShadowServer(GTKShadowServerBase):
         super().__init__(multi_window)
         #sanity check:
         check_display()
-        image = CG.CGWindowListCreateImage(CG.CGRectInfinite,
-                    CG.kCGWindowListOptionOnScreenOnly,
+        image = CG.CGWindowlistCreateImage(CG.CGRectInfinite,
+                    CG.kCGWindowlistOptionOnScreenOnly,
                     CG.kCGNullWindowID,
                     CG.kCGWindowImageDefault)
         if image is None:
@@ -126,7 +126,7 @@ class ShadowServer(GTKShadowServerBase):
         #return quickly, and process the list copy via idle add:
         self.idle_add(self.do_screen_refresh, rlist)
 
-    def do_screen_refresh(self, rlist:List) -> None:
+    def do_screen_refresh(self, rlist:list) -> None:
         #TODO: improve damage method to handle lists directly:
         from xpra.rectangle import rectangle     #@UnresolvedImport
         model_rects = {}
@@ -221,13 +221,13 @@ class ShadowServer(GTKShadowServerBase):
         r = CG.CGPostScrollWheelEvent(*event)
         log("CG.CGPostScrollWheelEvent%s=%s", event, r)
 
-    def make_hello(self, source) -> Dict[str,Any]:
+    def make_hello(self, source) -> dict[str,Any]:
         capabilities = GTKServerBase.make_hello(self, source)
         capabilities["shadow"] = True
         capabilities["server_type"] = "Python/gtk2/osx-shadow"
         return capabilities
 
-    def get_info(self, proto, *_args) -> Dict[str,Any]:
+    def get_info(self, proto, *_args) -> dict[str,Any]:
         info = GTKServerBase.get_info(self, proto)
         info.setdefault("features", {})["shadow"] = True
         info.setdefault("server", {})["type"] = "Python/gtk2/osx-shadow"

xpra/platform/posix/shadow_server.py
@@ -5,7 +5,6 @@
 # later version. See the file COPYING for details.
 
 import os
-from typing import Type
 
 from xpra.util import envbool
 from xpra.log import Logger
@@ -16,7 +15,7 @@ def warn(*messages) -> None:
     for m in messages:
         log.warn(m)
 
-def load_screencast() -> Type | None:
+def load_screencast() -> type | None:
     if envbool("XPRA_SHADOW_SCREENCAST", True):
         try:
             from xpra.platform.posix import screencast
@@ -26,7 +25,7 @@ def load_screencast() -> Type | None:
                  f" {e}")
     return None
 
-def load_remotedesktop() -> Type | None:
+def load_remotedesktop() -> type | None:
     if envbool("XPRA_SHADOW_REMOTEDESKTOP", True):
         try:
             from xpra.platform.posix import remotedesktop
@@ -36,7 +35,7 @@ def load_remotedesktop() -> Type | None:
                  f" {e}")
     return None
 
-def load_shadow_wayland(display_name=None) -> Type | None:
+def load_shadow_wayland(display_name=None) -> type | None:
     c = load_remotedesktop() or load_screencast()
     if c:
         os.environ["GDK_BACKEND"] = "wayland"
@@ -46,7 +45,7 @@ def load_shadow_wayland(display_name=None) -> Type | None:
             os.environ["XPRA_NOX11"] = "1"
     return c
 
-def load_shadow_x11() -> Type | None:
+def load_shadow_x11() -> type | None:
     if envbool("XPRA_SHADOW_X11", True):
         try:
             os.environ["GDK_BACKEND"] = "x11"
@@ -59,7 +58,7 @@ def load_shadow_x11() -> Type | None:
 
 
 def ShadowServer(display_name:str="", multi_window:bool=True):
-    c  : Type | None = None
+    c  : type | None = None
     if display_name.startswith("wayland-") or os.path.isabs(display_name):
         c = load_shadow_wayland(display_name)
     elif display_name.startswith(":"):

xpra/platform/win32/shadow_server.py
@@ -7,7 +7,7 @@
 import os
 import re
 from time import monotonic
-from typing import Dict, Any, Tuple, Type, List
+from typing import Any
 from ctypes import create_unicode_buffer, sizeof, byref, c_ulong
 from ctypes.wintypes import RECT, POINT, BYTE
 
@@ -73,12 +73,12 @@ GDI = envbool("XPRA_SHADOW_GDI", True)
 GSTREAMER = envbool("XPRA_SHADOW_GSTREAMER", True)
 
 
-def get_root_window_size() -> Tuple[int,int]:
+def get_root_window_size() -> tuple[int,int]:
     w = GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
     h = GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
     return w, h
 
-def get_monitors() -> List[Dict[str,Any]]:
+def get_monitors() -> List[dict[str,Any]]:
     monitors = []
     for m in EnumDisplayMonitors():
         mi = GetMonitorInfo(m)
@@ -154,7 +154,7 @@ class SeamlessRootWindowModel(RootWindowModel):
         shapelog("refresh_shape() sending notify for updated rectangles: %s", rectangles)
         self.notify("shape")
 
-    def get_shape_rectangles(self, logit=False) -> List:
+    def get_shape_rectangles(self, logit=False) -> list:
         #get the list of windows
         l = log
         if logit or envbool("XPRA_SHAPE_DEBUG", False):
@@ -328,7 +328,7 @@ class ShadowServer(GTKShadowServerBase):
         return init_capture(w, h, self.pixel_depth)
 
 
-    def get_root_window_model_class(self) -> Type:
+    def get_root_window_model_class(self) -> type:
         if SEAMLESS:
             return SeamlessRootWindowModel
         return RootWindowModel
@@ -401,7 +401,7 @@ class ShadowServer(GTKShadowServerBase):
         return models
 
 
-    def get_shadow_monitors(self) -> List:
+    def get_shadow_monitors(self) -> list:
         #convert to the format expected by GTKShadowServerBase:
         monitors = []
         for i, monitor in enumerate(get_monitors()):
@@ -423,7 +423,7 @@ class ShadowServer(GTKShadowServerBase):
         log("refresh()=%s", v)
         return v
 
-    def do_get_cursor_data(self) -> Tuple | None:
+    def do_get_cursor_data(self) -> tuple | None:
         ci = CURSORINFO()
         ci.cbSize = sizeof(CURSORINFO)
         GetCursorInfo(byref(ci))
@@ -448,7 +448,7 @@ class ShadowServer(GTKShadowServerBase):
             ((w,h), [(w,h), ]),
             )
 
-    def get_pointer_position(self) -> Tuple[int,int]:
+    def get_pointer_position(self) -> tuple[int,int]:
         pos = POINT()
         GetPhysicalCursorPos(byref(pos))
         return pos.x, pos.y
@@ -525,13 +525,13 @@ class ShadowServer(GTKShadowServerBase):
         x, y = pointer[:2]
         mouse_event(dwFlags, x, y, dwData, 0)
 
-    def make_hello(self, source) -> Dict[str,Any]:
+    def make_hello(self, source) -> dict[str,Any]:
         capabilities = GTKServerBase.make_hello(self, source)
         capabilities["shadow"] = True
         capabilities["server_type"] = "Python/gtk2/win32-shadow"
         return capabilities
 
-    def get_info(self, proto, *_args) -> Dict[str,Any]:
+    def get_info(self, proto, *_args) -> dict[str,Any]:
         info = GTKServerBase.get_info(self, proto)
         info.update(GTKShadowServerBase.get_info(self, proto))
         info.setdefault("features", {})["shadow"] = True

commit 397e47a696862000cb41bc2cca744539bb1e2bba

fixup typo breakage introduced in 13288d42587ca532e5a0e8e0a235207002bd6f7d

xpra/platform/darwin/shadow_server.py
@@ -84,8 +84,8 @@ class ShadowServer(GTKShadowServerBase):
         super().__init__(multi_window)
         # sanity check:
         check_display()
-        image = CG.CGWindowlistCreateImage(CG.CGRectInfinite,
-                                           CG.kCGWindowlistOptionOnScreenOnly,
+        image = CG.CGWindowListCreateImage(CG.CGRectInfinite,
+                                           CG.kCGWindowListOptionOnScreenOnly,
                                            CG.kCGNullWindowID,
                                            CG.kCGWindowImageDefault)
         if image is None: