b9ca743aff5007011f7941c9ea7f2fe54efe26a9
Antoine Martin <antoine@xpra.org>
2024-04-03 08:31:37 +0700

    #4182 remove 'long' type

--- xpra/x11/bindings/keyboard.pyx
+++ xpra/x11/bindings/keyboard.pyx
@@ -639,7 +639,7 @@ cdef class X11KeyboardBindingsInstance(X11CoreBindingsInstance):
                     for ks in keysyms_strs:
                         if ks in (None, ""):
                             keysym = NoSymbol
-                        elif isinstance(ks, (long, int)):
+                        elif isinstance(ks, int):
                             keysym = ks
                         else:
                             keysym = self._parse_keysym(ks)
