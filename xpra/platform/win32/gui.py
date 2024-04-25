Xpra-org/xpra

commit 13288d42587ca532e5a0e8e0a235207002bd6f7d

#3930 don't import from 'typing' if we don't have to

xpra/platform/win32/gui.py
@@ -9,7 +9,7 @@
 import os
 import sys
 import types
-from typing import Callable, List, Tuple, Dict, Any, Type
+from typing import Callable, Any
 from ctypes import (
     WinDLL, WinError, get_last_error,  # @UnresolvedImport
     CDLL, pythonapi, py_object,
@@ -162,7 +162,7 @@ def get_clipboard_native_class() -> str:
     return "xpra.platform.win32.clipboard.Win32Clipboard"
 
 
-def get_native_notifier_classes() -> List[Type]:
+def get_native_notifier_classes() -> list[type]:
     try:
         from xpra.platform.win32.win32_notifier import Win32_Notifier
         return [Win32_Notifier]
@@ -309,7 +309,7 @@ def win32_propsys_set_group_leader(self, leader):
         log.error("Error: failed to set group leader")
         log.estr(e)
 
-WS_NAMES : Dict[int,str] = {
+WS_NAMES : dict[int,str] = {
             win32con.WS_BORDER              : "BORDER",
             win32con.WS_CAPTION             : "CAPTION",
             win32con.WS_CHILD               : "CHILD",
@@ -442,7 +442,7 @@ def window_state_updated(window):
     log("window_state_updated(%s)", window)
     fixup_window_style(window)
 
-def apply_maxsize_hints(window, hints:Dict[str,Any]):
+def apply_maxsize_hints(window, hints:dict[str,Any]):
     """ extracts the max-size hints from the hints,
         and passes it to the win32hooks class which can implement it
         (as GTK2 does not honour it properly on win32)
@@ -489,7 +489,7 @@ def apply_maxsize_hints(window, hints:Dict[str,Any]):
         hints.pop(x, None)
     window_state_updated(window)
 
-def apply_geometry_hints(self, hints:Dict):
+def apply_geometry_hints(self, hints:dict):
     log("apply_geometry_hints(%s)", hints)
     apply_maxsize_hints(self, hints)
     return self.__apply_geometry_hints(hints)   #call the original saved method
@@ -649,27 +649,27 @@ FE_FONTSMOOTHINGORIENTATIONVRGB = 0x0003
 SPI_GETFONTSMOOTHINGTYPE        = 0x200A
 FE_FONTSMOOTHINGCLEARTYPE       = 0x0002
 FE_FONTSMOOTHINGDOCKING         = 0x8000
-FE_ORIENTATION_STR : Dict[int,str] = {
+FE_ORIENTATION_STR : dict[int,str] = {
                       FE_FONTSMOOTHINGORIENTATIONBGR    : "BGR",
                       FE_FONTSMOOTHINGORIENTATIONRGB    : "RGB",
                       FE_FONTSMOOTHINGORIENTATIONVBGR   : "VBGR",
                       FE_FONTSMOOTHINGORIENTATIONVRGB   : "VRGB",
                       }
-FE_FONTSMOOTHING_STR : Dict[int,str] = {
+FE_FONTSMOOTHING_STR : dict[int,str] = {
     0                           : "Normal",
     FE_FONTSMOOTHINGCLEARTYPE   : "ClearType",
     }
 
 
-def _add_SPI(info:Dict[str,Any], constant:int, name:str, convert:Callable, default:Any=None) -> None:
+def _add_SPI(info:dict[str,Any], constant:int, name:str, convert:Callable, default:Any=None) -> None:
     v = GetIntSystemParametersInfo(constant)
     if v is not None:
         info[name] = convert(v)
     elif default is not None:
         info[name] = default
 
-def get_antialias_info() -> Dict[str,Any]:
-    info : Dict[str,Any] = {}
+def get_antialias_info() -> dict[str,Any]:
+    info : dict[str,Any] = {}
     try:
         _add_SPI(info, SPI_GETFONTSMOOTHING, "enabled", bool)
         #"Valid contrast values are from 1000 to 2200. The default value is 1400."
@@ -685,7 +685,7 @@ def get_antialias_info() -> Dict[str,Any]:
         log.warn("failed to query antialias info: %s", e)
     return info
 
-def get_mouse_config() -> Dict[str,Any]:
+def get_mouse_config() -> dict[str,Any]:
     #not all are present in win32con?
     SM_CMOUSEBUTTONS = 43
     SM_CXDRAG = 68
@@ -704,7 +704,7 @@ def get_mouse_config() -> Dict[str,Any]:
     #rate for each direction:
     _add_SPI(wheel_info, SPI_GETWHEELSCROLLLINES, "lines", int, 3)
     _add_SPI(wheel_info, SPI_GETWHEELSCROLLCHARS, "chars", int, 3)
-    info : Dict[str,Any] = {
+    info : dict[str,Any] = {
             "present"       : bool(GetSystemMetrics(SM_MOUSEPRESENT)),
             "wheel"         : wheel_info,
             "buttons"       : GetSystemMetrics(SM_CMOUSEBUTTONS),
@@ -762,9 +762,9 @@ def get_workarea():
 #ie: for a 60 pixel bottom bar on the second monitor at 1280x800:
 # [(0,0,1920,1080), (0,0,1280,740)]
 MONITORINFOF_PRIMARY = 1
-def get_workareas() -> List[Tuple[int,int,int,int]]:
+def get_workareas() -> list[tuple[int,int,int,int]]:
     try:
-        workareas : List[Tuple[int,int,int,int]] = []
+        workareas : list[tuple[int,int,int,int]] = []
         for m in EnumDisplayMonitors():
             mi = GetMonitorInfo(m)
             screenlog("get_workareas() GetMonitorInfo(%s)=%s", m, mi)
@@ -981,7 +981,7 @@ def getTaskbar():
             TaskbarLib = False
     if not TaskbarLib:
         return None
-    taskbar = cc.CreateObject("{56FDF344-FD6D-11d0-958A-006097C9A090}", interface=TaskbarLib.ITaskbarList3)
+    taskbar = cc.CreateObject("{56FDF344-FD6D-11d0-958A-006097C9A090}", interface=TaskbarLib.ITaskbarlist3)
     taskbar.HrInit()
     return taskbar
 
@@ -1006,7 +1006,7 @@ WTS_SESSION_LOGOFF          = 0x6
 WTS_SESSION_LOCK            = 0x7
 WTS_SESSION_UNLOCK          = 0x8
 WTS_SESSION_REMOTE_CONTROL  = 0x9
-WTS_SESSION_EVENTS : Dict[int, str] = {
+WTS_SESSION_EVENTS : dict[int, str] = {
                       WTS_CONSOLE_CONNECT       : "CONSOLE CONNECT",
                       WTS_CONSOLE_DISCONNECT    : "CONSOLE_DISCONNECT",
                       WTS_REMOTE_CONNECT        : "REMOTE_CONNECT",
@@ -1145,7 +1145,7 @@ class ClientExtras:
                            win32con.VK_RWIN   : "Super_R",
                            win32con.VK_TAB    : "Tab",
                            }.get(vk_code)
-                modifiers : List[str] = []
+                modifiers : list[str] = []
                 kh = self.client.keyboard_helper
                 key_event_type = ALL_KEY_EVENTS.get(wParam)
                 #log("low_level_keyboard_handler(%s, %s, %s) vk_code=%i, scan_code=%i, keyname=%s, key_event_type=%s, focused=%s, keyboard_grabbed=%s", nCode, wParam, lParam, vk_code, scan_code, keyname, key_event_type, focused, self.client.keyboard_grabbed)

xpra/platform/darwin/gui.py
@@ -9,7 +9,7 @@ import math
 import ctypes
 import struct
 from weakref import WeakValueDictionary
-from typing import Any, Callable, Dict, List, Type, Tuple
+from typing import Any, Callable
 
 from gi.repository import GLib      #@UnresolvedImport
 import objc                         #@UnresolvedImport
@@ -164,7 +164,7 @@ def get_clipboard_native_class() -> str:
     return "xpra.platform.darwin.osx_clipboard.OSXClipboardProtocolHelper"
 
 
-def get_native_notifier_classes() -> List[Type]:
+def get_native_notifier_classes() -> list[type]:
     v = []
     if NATIVE_NOTIFIER and NSUserNotificationCenter.defaultUserNotificationCenter():
         v.append(OSX_Notifier)
@@ -177,7 +177,7 @@ def get_native_tray_menu_helper_class() -> Callable | None:
         return getOSXMenuHelper
     return None
 
-def get_native_tray_classes() -> List[Type]:
+def get_native_tray_classes() -> list[type]:
     if get_OSXApplication():
         from xpra.platform.darwin.osx_tray import OSXTray
         return [OSXTray]
@@ -188,9 +188,9 @@ def system_bell(*_args) -> bool:
     return True
 
 
-def _sizetotuple(s) -> Tuple[int,int]:
+def _sizetotuple(s) -> tuple[int,int]:
     return int(s.width), int(s.height)
-def _recttotuple(r) -> Tuple[int,int,int,int]:
+def _recttotuple(r) -> tuple[int,int,int,int]:
     return int(r.origin.x), int(r.origin.y), int(r.size.width), int(r.size.height)
 
 def get_double_click_time() -> int:
@@ -207,7 +207,7 @@ def get_double_click_time() -> int:
     return -1
 
 
-def get_window_min_size() -> Tuple[int,int]:
+def get_window_min_size() -> tuple[int,int]:
     #roughly enough to see the window buttons:
     return 120, 1
 
@@ -285,7 +285,7 @@ def get_vrefresh() -> int:
     return -1
 
 
-def get_display_icc_info() -> Dict[Any,Dict]:
+def get_display_icc_info() -> dict[Any,dict]:
     info = {}
     try:
         err, active_displays, no = CG.CGGetActiveDisplayList(99, None, None)
@@ -307,7 +307,7 @@ def get_icc_info():
     return info
 
 
-def get_colorspace_info(cs) -> Dict[str,Any]:
+def get_colorspace_info(cs) -> dict[str,Any]:
     MODELS : Dict[Any, str] = {
               CG.kCGColorSpaceModelUnknown     : "unknown",
               CG.kCGColorSpaceModelMonochrome  : "monochrome",
@@ -346,11 +346,11 @@ def get_display_mode_info(mode) -> Dict[str,Any]:
             )
     return _call_CG_conv(defs, mode)
 
-def get_display_modes_info(modes) -> Dict[int,Any]:
+def get_display_modes_info(modes) -> dict[int,Any]:
     return dict((i,get_display_mode_info(mode)) for i,mode in enumerate(modes))
 
 
-def _call_CG_conv(defs:Tuple[Tuple[str,str,Callable],...], argument) -> Dict[str,Any]:
+def _call_CG_conv(defs:tuple[tuple[str,str,Callable],...], argument) -> dict[str,Any]:
     #utility for calling functions on CG with an argument,
     #then convert the return value using another function
     #missing functions are ignored, and None values are skipped
@@ -372,7 +372,7 @@ def _call_CG_conv(defs:Tuple[Tuple[str,str,Callable],...], argument) -> Dict[str
             log("function %s does not exist", fn_name)
     return info
 
-def get_display_info(did) -> Dict[str,Any]:
+def get_display_info(did) -> dict[str,Any]:
     defs = (
             ("height",                  "CGDisplayPixelsHigh",              int),
             ("width",                   "CGDisplayPixelsWide",              int),
@@ -407,9 +407,9 @@ def get_display_info(did) -> Dict[str,Any]:
         log("failed to query display modes: %s", e)
     return info
 
-def get_displays_info() -> Dict[str,Any]:
+def get_displays_info() -> dict[str,Any]:
     did = CG.CGMainDisplayID()
-    info : Dict[str,Any] = {
+    info : dict[str,Any] = {
             "main" : get_display_info(did),
             }
     err, active_displays, no = CG.CGGetActiveDisplayList(99, None, None)
@@ -422,7 +422,7 @@ def get_displays_info() -> Dict[str,Any]:
             info.setdefault("online", {})[i] = get_display_info(odid)
     return info
 
-def get_info() -> Dict[str,Any]:
+def get_info() -> dict[str,Any]:
     from xpra.platform.gui import get_info_base
     i = get_info_base()
     try:
@@ -484,7 +484,7 @@ def get_CG_imagewrapper(rect=None):
     argb = CG.CGDataProviderCopyData(prov)
     return ImageWrapper(x, y, width, height, argb, "BGRX", 24, rowstride)
 
-def take_screenshot() -> Tuple[int,int,str,int,bytes]:
+def take_screenshot() -> tuple[int,int,str,int,bytes]:
     log("grabbing screenshot")
     from PIL import Image                       #@UnresolvedImport
     from io import BytesIO
@@ -577,7 +577,7 @@ class AppDelegate(NSObject):
 
     def init(self) -> None:
         super().init()
-        self.callbacks : Dict[str,Callable] = {}
+        self.callbacks : dict[str,Callable] = {}
         self.workspace = None
         self.notificationCenter = None
 

xpra/platform/gui.py
@@ -8,7 +8,7 @@
 import os
 import sys
 import binascii
-from typing import Dict, Any, Tuple, Type
+from typing import Any
 
 from xpra.platform import platform_import
 from xpra.os_util import bytestostr
@@ -61,22 +61,22 @@ def get_clipboard_native_class() -> str:
     return ""
 
 #defaults:
-def get_native_tray_menu_helper_class() -> Type | None:
+def get_native_tray_menu_helper_class() -> type | None:
     #classes that generate menus for xpra's system tray
     #let the toolkit classes use their own
     return None
-def get_native_tray_classes(*_args) -> Tuple[Type | None,...]:
+def get_native_tray_classes(*_args) -> tuple[type | None,...]:
     #the classes we can use for our system tray:
     #let the toolkit classes use their own
     return ()
-def get_native_system_tray_classes(*_args) -> Tuple[Type | None,...]:
+def get_native_system_tray_classes(*_args) -> tuple[type | None,...]:
     #the classes we can use for application system tray forwarding:
     #let the toolkit classes use their own
     return ()
 def system_bell(*_args) -> bool:
     #let the toolkit classes use their own
     return False
-def get_native_notifier_classes() -> Tuple[Type,...]:
+def get_native_notifier_classes() -> tuple[type,...]:
     return ()
 
 
@@ -91,7 +91,7 @@ def get_ydpi() -> int:
     return -1
 
 
-def get_monitors_info(xscale=1, yscale=1) -> Dict[int,Any]:
+def get_monitors_info(xscale=1, yscale=1) -> dict[int,Any]:
     from xpra.gtk_common import gtk_util
     return gtk_util.get_monitors_info(xscale, yscale)
 
@@ -111,17 +111,17 @@ def get_icon_size() -> int:
         return 24
     return 16
 
-def get_antialias_info() -> Dict[str,Any]:
+def get_antialias_info() -> dict[str,Any]:
     return {}
 
-def get_display_icc_info() -> Dict[str,Any]:
+def get_display_icc_info() -> dict[str,Any]:
     #per display info
     return {}
 
-def get_icc_info() -> Dict[str,Any]:
+def get_icc_info() -> dict[str,Any]:
     return default_get_icc_info()
 
-def default_get_icc_info() -> Dict[str,Any]:
+def default_get_icc_info() -> dict[str,Any]:
     ENV_ICC_DATA = os.environ.get("XPRA_ICC_DATA")
     if ENV_ICC_DATA:
         return {
@@ -130,13 +130,13 @@ def default_get_icc_info() -> Dict[str,Any]:
             }
     return get_pillow_icc_info()
 
-def get_pillow_icc_info() -> Dict[str,Any]:
+def get_pillow_icc_info() -> dict[str,Any]:
     screenlog = Logger("screen")
     info = {}
     try:
         from PIL import ImageCms
         from PIL.ImageCms import get_display_profile
-        INTENT_STR : Dict[Any, str] = {}
+        INTENT_STR : dict[Any, str] = {}
         for x in ("PERCEPTUAL", "RELATIVE_COLORIMETRIC", "SATURATION", "ABSOLUTE_COLORIMETRIC"):
             intent = getattr(ImageCms, "Intent", None)
             if intent:
@@ -193,7 +193,7 @@ def get_workareas():
 def get_number_of_desktops() -> int:
     return 1
 
-def get_desktop_names()  -> Tuple[str,...]:
+def get_desktop_names()  -> tuple[str,...]:
     return ()
 
 def get_vrefresh() -> int:
@@ -205,19 +205,19 @@ def get_mouse_config():
 def get_double_click_time() -> int:
     return -1
 
-def get_double_click_distance() -> Tuple[int,int]:
+def get_double_click_distance() -> tuple[int,int]:
     return -1, -1
 
-def get_fixed_cursor_size() -> Tuple[int,int]:
+def get_fixed_cursor_size() -> tuple[int,int]:
     return -1, -1
 
 def get_cursor_size() -> int:
     return -1
 
-def get_window_min_size() -> Tuple[int,int]:
+def get_window_min_size() -> tuple[int,int]:
     return 0, 0
 
-def get_window_max_size() -> Tuple[int,int]:
+def get_window_max_size() -> tuple[int,int]:
     return 2**15-1, 2**15-1
 
 def get_window_frame_size(_x, _y, _w, _h):
@@ -284,7 +284,7 @@ take_screenshot = None
 ClientExtras = None
 
 
-def get_info_base() -> Dict[str,Any]:
+def get_info_base() -> dict[str,Any]:
     def fname(v):
         try:
             return v.__name__

xpra/platform/posix/gui.py
@@ -6,7 +6,7 @@
 
 import os
 import sys
-from typing import List, Type, Tuple, Dict, Any, Callable
+from typing import Any, Callable
 
 from xpra.os_util import (
     bytestostr, get_saved_env,
@@ -106,18 +106,18 @@ def get_clipboard_native_class() -> str:
         return gtk_clipboard_class
     return "xpra.x11.gtk_x11.clipboard.X11Clipboard"
 
-def get_native_system_tray_classes() -> List[Type | None]:
+def get_native_system_tray_classes() -> list[type | None]:
     c = [_try_load_appindicator()]
     traylog("get_native_system_tray_classes()=%s (USE_NATIVE_TRAY=%s)", c, USE_NATIVE_TRAY)
     return c
 
-def get_native_tray_classes() -> List[Type | None]:
+def get_native_tray_classes() -> list[type | None]:
     #could restrict to only DEs that have a broken system tray like "GNOME Shell"?
     c = [_try_load_appindicator()]
     traylog("get_native_tray_classes()=%s (USE_NATIVE_TRAY=%s)", c, USE_NATIVE_TRAY)
     return c
 
-def _try_load_appindicator() -> Type | None:
+def _try_load_appindicator() -> type | None:
     if not USE_NATIVE_TRAY:
         return None
     try:
@@ -135,8 +135,8 @@ def _try_load_appindicator() -> Type | None:
     return None
 
 
-def get_native_notifier_classes() -> List[Callable]:
-    ncs : List[Callable] = []
+def get_native_notifier_classes() -> list[Callable]:
+    ncs : list[Callable] = []
     try:
         from xpra.notifications.dbus_notifier import DBUS_Notifier_factory
         ncs.append(DBUS_Notifier_factory)
@@ -189,7 +189,7 @@ def _get_xsettings_dpi() -> int:
                     return actual_value
     return -1
 
-def _get_randr_dpi() -> Tuple[int,int]:
+def _get_randr_dpi() -> tuple[int,int]:
     if RANDR_DPI and x11_bindings():
         from xpra.x11.common import get_randr_dpi
         from xpra.gtk_common.error import xlog
@@ -210,7 +210,7 @@ def get_ydpi() -> int:
     return _get_randr_dpi()[1]
 
 
-def get_icc_info() -> Dict[str,Any]:
+def get_icc_info() -> dict[str,Any]:
     if x11_bindings():
         from xpra.x11.common import get_icc_data as get_x11_icc_data
         from xpra.gtk_common.error import xsync
@@ -220,8 +220,8 @@ def get_icc_info() -> Dict[str,Any]:
     return default_get_icc_info()
 
 
-def get_antialias_info() -> Dict[str,Any]:
-    info : Dict[str,Any] = {}
+def get_antialias_info() -> dict[str,Any]:
+    info : dict[str,Any] = {}
     if not x11_bindings():
         return info
     try:
@@ -281,7 +281,7 @@ def get_number_of_desktops() -> int:
             return get_x11_number_of_desktops()
     return 0
 
-def get_desktop_names() -> Tuple[str,...]:
+def get_desktop_names() -> tuple[str,...]:
     if x11_bindings():
         from xpra.x11.common import get_desktop_names as get_x11_desktop_names
         from xpra.gtk_common.error import xsync
@@ -321,11 +321,11 @@ def _get_xsettings_int(name:str, default_value:int) -> int:
 def get_double_click_time() -> int:
     return _get_xsettings_int("Net/DoubleClickTime", -1)
 
-def get_double_click_distance() -> Tuple[int,int]:
+def get_double_click_distance() -> tuple[int,int]:
     v = _get_xsettings_int("Net/DoubleClickDistance", -1)
     return v, v
 
-def get_window_frame_sizes() -> Dict:
+def get_window_frame_sizes() -> dict:
     #for X11, have to create a window and then check the
     #_NET_FRAME_EXTENTS value after sending a _NET_REQUEST_FRAME_EXTENTS message,
     #so this is done in the gtk client instead of here...
@@ -412,20 +412,20 @@ def set_shaded(window, shaded:bool) -> None:
 
 
 
-WINDOW_ADD_HOOKS : List[Callable] = []
+WINDOW_ADD_HOOKS : list[Callable] = []
 def add_window_hooks(window) -> None:
     for x in WINDOW_ADD_HOOKS:
         x(window)
     log("add_window_hooks(%s) added %s", window, WINDOW_ADD_HOOKS)
 
-WINDOW_REMOVE_HOOKS : List[Callable] = []
+WINDOW_REMOVE_HOOKS : list[Callable] = []
 def remove_window_hooks(window):
     for x in WINDOW_REMOVE_HOOKS:
         x(window)
     log("remove_window_hooks(%s) added %s", window, WINDOW_REMOVE_HOOKS)
 
 
-def get_info() -> Dict[str,Any]:
+def get_info() -> dict[str,Any]:
     from xpra.platform.gui import get_info_base  # pylint: disable=import-outside-toplevel
     i = get_info_base()
     s = _get_xsettings()
@@ -452,7 +452,7 @@ class XI2_Window:
         self.X11Window = X11WindowBindings()
         self.window = window
         self.xid = window.get_window().get_xid()
-        self.windows : Tuple[int,...] = ()
+        self.windows : tuple[int,...] = ()
         self.motion_valuators = {}
         window.connect("configure-event", self.configured)
         self.configured()
@@ -488,7 +488,7 @@ class XI2_Window:
         self.motion_valuators = {}
 
 
-    def get_parent_windows(self, oxid:int) -> Tuple[int,...]:
+    def get_parent_windows(self, oxid:int) -> tuple[int,...]:
         windows = [oxid]
         root = self.X11Window.get_root_xid()
         xid = oxid
@@ -587,7 +587,7 @@ class XI2_Window:
             #normalize (xinput is always using 15 degrees?)
             client.wheel_event(event.device, wid, dx/XINPUT_WHEEL_DIV, dy/XINPUT_WHEEL_DIV, pointer_data, props)
 
-    def get_pointer_extra_args(self, event) -> Dict[str,Any]:
+    def get_pointer_extra_args(self, event) -> dict[str,Any]:
         def intscaled(f):
             return int(f*1000000), 1000000
         def dictscaled(d):

commit e6387686c1e6d5e9add5ad88379f8b7190317896

#4200 fix breakage introduced by 13288d42587ca532e5a0e8e0a235207002bd6f7d

xpra/platform/win32/gui.py
@@ -1032,7 +1032,7 @@ def getTaskbar():
             TaskbarLib = False
     if not TaskbarLib:
         return None
-    taskbar = cc.CreateObject("{56FDF344-FD6D-11d0-958A-006097C9A090}", interface=TaskbarLib.ITaskbarlist3)
+    taskbar = cc.CreateObject("{56FDF344-FD6D-11d0-958A-006097C9A090}", interface=TaskbarLib.ITaskbarList3)
     taskbar.HrInit()
     return taskbar
 