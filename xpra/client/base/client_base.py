c8c460db288bb41678bd1e69f5447a0c06f18987
Antoine Martin <antoine@xpra.org>
2024-04-25 12:39:42 +0700

    #4198 use utf8 for sending messages to the splash window
    also handle a missing stdin more gracefully,
    and catch errors so they don't become fatal

--- xpra/client/base/client_base.py
+++ xpra/client/base/client_base.py
@@ -182,8 +182,14 @@ class XpraClientBase(ServerInfoMixin, FilePrintMixin):
         if pp.poll() is not None:
             self.progress_process = None
             return
-        noerr(pp.stdin.write, f"{pct}:{text}\n".encode("latin1"))
-        noerr(pp.stdin.flush)
+        stdin = pp.stdin
+        if stdin:
+            try:
+                noerr(stdin.write, f"{pct}:{text}\n".encode("utf8"))
+            except UnicodeEncodeError as e:
+                log.warn(f"Warning: failed to send {text!r} to progress process: {e}")
+                noerr(stdin.write, f"{pct}:\n".encode("utf8"))
+            noerr(stdin.flush)
         if pct==100:
             #it should exit on its own, but just in case:
             #kill it if it's still running after 2 seconds
