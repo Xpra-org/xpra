904e5b9e49df022e3c07cb68e2d8266e734baefe
Antoine Martin <antoine@xpra.org>
2024-06-11 21:59:28 +0700

    compare all the pixels

--- tests/unittests/unit/codecs/color_range_test.py
+++ tests/unittests/unit/codecs/color_range_test.py
@@ -5,6 +5,7 @@
 # later version. See the file COPYING for details.
 
 import unittest
+from collections.abc import Sequence
 
 from xpra.util.str_fn import memoryview_to_bytes, hexstr, sorted_nicely
 from xpra.util.objects import typedict
@@ -26,25 +27,58 @@ TEST_DATA = {
 }
 
 
-def cmp_pixels(img1, img2, tolerance=1) -> bool:
-    # compare the first 4 pixels:
-    for i in range(16):
-        b1 = img1[i]
-        b2 = img2[i]
-        delta = abs(b2-b1)
+def cmp_bytes(b1: bytes, b2: bytes, tolerance=1) -> bool:
+    length = min(len(b1), len(b2))
+    for i in range(length):
+        v1 = b1[i]
+        v2 = b2[i]
+        delta = abs(v2-v1)
         if delta > tolerance:
-            print(f"pixel {i}: {b1} vs {b2}, {delta}>{tolerance}")
+            print(f"pixel {i} /  {length}: {v1:x} vs {v2:x}, {delta:x}>{tolerance:x}")
             return False
     return True
 
 
+def cmp_pixels(pix1: bytes, w1: int, h1: int, stride1: int, bpp1: int,
+               pix2: bytes, w2: int, h2: int, stride2: int, bpp2: int,
+               tolerance=1) -> bool:
+    h = min(h1, h2)
+    w = min(w1, w2)
+    for y in range(h):
+        row1 = pix1[y * stride1: y * stride1 + w * bpp1]
+        row2 = pix2[y * stride2: y * stride2 + w * bpp2]
+        if not cmp_bytes(row1, row2, tolerance):
+            print("expected:")
+            print(hexstr(row1))
+            print("but got:")
+            print(hexstr(row2))
+            print(f"for row {y} / {h}")
+            return False
+    return True
+
+
+def cmp_images(image1: ImageWrapper, image2: ImageWrapper, tolerance=1) -> bool:
+    pixels1 = memoryview_to_bytes(memoryview(image1.get_pixels()))
+    pixels2 = memoryview_to_bytes(memoryview(image2.get_pixels()))
+    return cmp_pixels(
+        pixels1, image1.get_width(), image1.get_height(), image1.get_rowstride(), image1.get_bytesperpixel(),
+        pixels2, image2.get_width(), image2.get_height(), image2.get_rowstride(), image2.get_bytesperpixel(),
+        tolerance)
+
+
 class TestColorRange(unittest.TestCase):
 
-    def test_save_rgb(self):
+    def test_encode_decode_range(self) -> None:
+        self.do_test_encode_decode_range(loader.ENCODER_CODECS, loader.DECODER_CODECS, loader.CSC_CODECS)
+
+    def do_test_encode_decode_range(self,
+                                    encoders: Sequence[str],
+                                    decoders: Sequence[str],
+                                    csc_modules=Sequence[str]) -> None:
         test_info: set[str] = set()
         width = 48
         height = 32
-        for enc_name in loader.ENCODER_CODECS:
+        for enc_name in encoders:
             enc_mod = loader.load_codec(enc_name)
             if not enc_mod:
                 continue
@@ -55,18 +89,11 @@ class TestColorRange(unittest.TestCase):
                     image = make_test_image(rgb_format, width, height, pixel)
                     pixels = image.get_pixels()
 
-                    def cmpimage(other: ImageWrapper, tolerance=1) -> bool:
-                        other_pixels = memoryview_to_bytes(memoryview(other.get_pixels()))
-                        cmp = cmp_pixels(pixels, other_pixels, tolerance)
-                        if not cmp:
-                            print(f"expected {hexstr(pixels[:16])} but got {hexstr(other_pixels[:16])}")
-                        return cmp
-
                     for quality, tolerance in {
                         "100": 1 if (enc_name in ("enc_pillow", "enc_jpeg") and fmt in ("webp", "jpeg")) else 0,
-                        "90": 3 if (enc_name == "enc_pillow") else 2,
-                        "50": 6,
-                        "10": 16,
+                        "90": 6,
+                        "50": 0xd if enc_name == "enc_webp" else 7,
+                        "10": 0x20,
                     }.items():
                         enc_options = typedict({"quality": quality})
                         bdata = enc_mod.encode(fmt, image, options=enc_options)
@@ -77,20 +104,20 @@ class TestColorRange(unittest.TestCase):
                         ext = fmt.replace("/", "")  # ie: "png/L" -> "pngL"
                         filename = f"./{enc_name}-{color}.{ext}"
                         if ext in ("png", "webp", "jpeg"):
-                            # verify compressed image with Pillow:
+                            # verify first 16 bytes when compressed image with Pillow:
                             from io import BytesIO
                             from PIL import Image
                             img = Image.open(BytesIO(file_data))
                             img = img.convert("RGBA")
                             rdata = img.tobytes("raw", rgb_format.replace("X", "A"))
-                            if not cmp_pixels(pixels, rdata, tolerance):
+                            if not cmp_bytes(pixels[:16], rdata[:16], tolerance):
                                 raise RuntimeError(f"pixels reloaded from {filename} do not match:"
-                                                   f"expected {hexstr(pixels[:16])} but got {hexstr(rdata[:16])}"
+                                                   f"expected {hexstr(pixels[:16])} but got {hexstr(rdata[:16])} "
                                                    f"with {enc_options=}")
                             test_info.add(f"{enc_name:12}  {fmt:12}  pillow                                    {rgb_format}")
 
                         # try to decompress to rgb:
-                        for dec_name in loader.DECODER_CODECS:
+                        for dec_name in decoders:
                             dec_mod = loader.load_codec(dec_name)
                             if not dec_mod:
                                 continue
@@ -104,12 +131,12 @@ class TestColorRange(unittest.TestCase):
                             dec_tolerance = tolerance
                             if dec_name == "dec_jpeg":
                                 dec_tolerance += 1
-                            if not cmpimage(rimage, dec_tolerance):
-                                raise RuntimeError(f"decoder {dec_name} produced an image that differs with {enc_options=}")
+                            if not cmp_images(image, rimage, dec_tolerance):
+                                raise RuntimeError(f"decoder {dec_name} from {enc_name} produced an image that differs with {enc_options=}")
                             test_info.add(f"{enc_name:12}  {fmt:12}  {dec_name:12}                              {rgb_format}")
 
                         # try to decompress to yuv
-                        for dec_name in loader.DECODER_CODECS:
+                        for dec_name in decoders:
                             dec_mod = loader.load_codec(dec_name)
                             if not dec_mod:
                                 continue
@@ -126,7 +153,7 @@ class TestColorRange(unittest.TestCase):
                             assert yuv_image
                             yuv_format = yuv_image.get_pixel_format()
                             # find a csc module to convert this back to rgb:
-                            for csc_name in loader.CSC_CODECS:
+                            for csc_name in csc_modules:
                                 csc_mod = loader.load_codec(csc_name)
                                 if not csc_mod:
                                     continue
@@ -141,8 +168,8 @@ class TestColorRange(unittest.TestCase):
                                                        width, height, rgb_format, csc_options)
                                 rgb_image = converter.convert_image(yuv_image)
                                 assert rgb_image
-                                if not cmpimage(rgb_image, dec_tolerance + 1):
-                                    raise RuntimeError(f"decoder {dec_name} produced a YUV image that differs with {enc_options=}"
+                                if not cmp_images(image, rgb_image, dec_tolerance + 1):
+                                    raise RuntimeError(f"decoder {dec_name} from {enc_name} produced a YUV image that differs with {enc_options=}"
                                                        f" (converted to {rgb_format} from {yuv_format} using {csc_name})")
                                 test_info.add(f"{enc_name:12}  {fmt:12}  {dec_name:12}  {yuv_format:12}  {csc_name:12}  {rgb_format:12}")
         print("successfully tested:")
