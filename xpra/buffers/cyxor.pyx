Xpra-org/xpra

commit 6ec2d3622898dd486a1675515a0c033a295b7b94

cosmetic

xpra/buffers/cyxor.pyx
@@ -3,6 +3,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+from collections.abc import Buffer
 #cython: wraparound=False, boundscheck=False
 
 from libc.stdint cimport uint32_t, uintptr_t   # pylint: disable=syntax-error
@@ -16,7 +17,7 @@ cdef extern from "Python.h":
     int PyBUF_ANY_CONTIGUOUS
 
 
-def xor_str(a, b):
+def xor_str(a: Buffer, b: Buffer) -> Buffer:
     if len(a)!=len(b):
         raise ValueError("cyxor cannot xor strings of different lengths (%s:%s vs %s:%s)" % (type(a), len(a), type(b), len(b)))
     cdef Py_buffer py_bufa

xpra/net/protocol/socket_handler.py
@@ -388,7 +388,6 @@ class SocketProtocol:
             self._internal_error("error in network packet write/format", e, exc_info=True)
 
     def _add_packet_to_queue(self, packet: PacketType, synchronous=True, more=False) -> None:
-        log.warn("_add_packet_to_queue%s", (packet, synchronous, more))
         if not more:
             shm = self._source_has_more
             if shm:

xpra/buffers/membuf.pyx
@@ -33,17 +33,20 @@ cdef extern from "memalign.h":
 cdef void free_buf(const void *p, size_t l, void *arg) noexcept nogil:
     free(<void *>p)
 
+
 cdef MemBuf getbuf(size_t l, int readonly=1):
     cdef const void *p = xmemalign(l)
     if p==NULL:
         raise RuntimeError(f"failed to allocate {l} bytes of memory")
     return MemBuf_init(p, l, &free_buf, NULL, readonly)
 
+
 cdef MemBuf padbuf(size_t l, size_t padding, int readonly=1):
     cdef const void *p = xmemalign(l+padding)
     if p==NULL:
         raise RuntimeError(f"failed to allocate {l} bytes of memory")
     return MemBuf_init(p, l, &free_buf, NULL, readonly)
+
 
 cdef MemBuf makebuf(void *p, size_t l, int readonly=1):
     if p==NULL:
@@ -80,7 +83,9 @@ cdef class MemBuf:
 
     def __dealloc__(self):
         if self.dealloc_cb_p != NULL:
-            self.dealloc_cb_p(self.p, self.l, self.dealloc_cb_arg)
+            self.dealloc_cb_p(self.p,
+             self.l, self.dealloc_cb_arg)
+
 
 # Call this instead of constructing a MemBuf directly.  The __cinit__
 # and __init__ methods can only take Python objects, so the real
@@ -102,44 +107,57 @@ cdef MemBuf MemBuf_init(const void *p, size_t l,
 cdef class BufferContext:
     cdef Py_buffer py_buf
     cdef object obj
+
     def __init__(self, obj):
         if not obj:
             raise ValueError(f"invalid buffer object {obj!r} evaluates to False ({type(obj)}")
         self.obj = obj
         memset(&self.py_buf, 0, sizeof(Py_buffer))
+
     def __enter__(self):
         if self.py_buf.buf!=NULL:
             raise RuntimeError("invalid state: buffer has already been obtained")
         if PyObject_GetBuffer(self.obj, &self.py_buf, PyBUF_ANY_CONTIGUOUS):
             raise RuntimeError(f"failed to access buffer of {type(self.obj)}")
         return self
+
     def __exit__(self, *_args):
         if self.py_buf.buf==NULL:
             raise RuntimeError("invalid state: no buffer")
         PyBuffer_Release(&self.py_buf)
+
     def __int__(self):
         if self.py_buf.buf==NULL:
             raise RuntimeError("invalid state: no buffer")
         return int(<uintptr_t> self.py_buf.buf)
+
     def __len__(self):
         return self.py_buf.len
+
     def __repr__(self):
         return "BufferContext(%s)" % self.obj
 
+
 cdef class MemBufContext:
     cdef MemBuf membuf
+
     def __init__(self, membuf):
         if not isinstance(membuf, MemBuf):
             raise ValueError(f"{membuf!r} is not a MemBuf instance: {type(membuf)}")
         self.membuf = membuf
+
     def __enter__(self):
         return self
+
     def __exit__(self, *_args):
         self.membuf = None
+
     def __int__(self):
         return self.membuf.get_mem_ptr()
+
     def __len__(self):
         return len(self.membuf)
+
     def __repr__(self):
         return "MemBufContext(%s)" % self.membuf
 

commit 4c6ae20c28a16460850317121589a27dcc83c1f3

restore compatibility with Python < 3.12
which was broken by 6ec2d3622898dd486a1675515a0c033a295b7b94 because of the new 'Buffer' type

xpra/buffers/cyxor.pyx
@@ -3,7 +3,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from collections.abc import Buffer
+from xpra.common import Buffer
 #cython: wraparound=False, boundscheck=False
 
 from libc.stdint cimport uint32_t, uintptr_t   # pylint: disable=syntax-error

xpra/codecs/argb/argb.pyx
@@ -7,7 +7,7 @@
 #cython: boundscheck=False, wraparound=False
 
 from typing import List, Tuple
-from collections.abc import Buffer
+from xpra.common import Buffer
 
 from xpra.util.env import first_time
 from xpra.buffers.membuf cimport getbuf, MemBuf, buffer_context  # pylint: disable=syntax-error

xpra/codecs/debug.py
@@ -4,8 +4,8 @@
 # later version. See the file COPYING for details.
 
 from time import monotonic
-from collections.abc import Buffer
 
+from xpra.common import Buffer
 from xpra.util.env import envbool
 from xpra.log import Logger
 

xpra/codecs/pillow/decoder.py
@@ -9,8 +9,9 @@ from io import BytesIO
 import PIL
 from PIL import Image
 from typing import Any
-from collections.abc import Callable, Buffer, Sequence
+from collections.abc import Callable, Sequence
 
+from xpra.common import Buffer
 from xpra.util.objects import typedict
 from xpra.util.str_fn import csv, strtobytes, hexstr
 from xpra.codecs.debug import may_save_image

xpra/common.py
@@ -8,7 +8,7 @@
 import binascii
 import os
 from enum import Enum, IntEnum
-from typing import Final
+from typing import TypeAlias, Final
 from collections.abc import Callable
 
 try:
@@ -16,6 +16,15 @@ try:
     from enum import StrEnum
 except ImportError:     # pragma: no cover
     StrEnum = Enum      # type: ignore
+
+try:
+    # Python 3.12 and later:
+    from collections.abc import Buffer as abc_Buffer
+    Buffer: TypeAlias = abc_Buffer
+except ImportError:
+    from typing import ByteString
+    Buffer: TypeAlias = ByteString
+
 
 from xpra.util.env import envint, envbool
 from xpra.util.str_fn import nicestr

xpra/net/common.py
@@ -8,10 +8,10 @@ import socket
 import struct
 import threading
 from typing import Any, Union, TypeAlias, Final
-from collections.abc import Callable, Buffer, Sequence
+from collections.abc import Callable, Sequence
 
 from xpra.net.compression import Compressed, Compressible, LargeStructure
-from xpra.common import noop
+from xpra.common import noop, Buffer
 from xpra.os_util import LINUX, FREEBSD, WIN32
 from xpra.scripts.config import str_to_bool
 from xpra.util.str_fn import repr_ellipsized

xpra/net/compression.py
@@ -6,11 +6,11 @@
 # later version. See the file COPYING for details.
 
 from typing import Any
-from collections.abc import Callable, Sequence, Buffer
+from collections.abc import Callable, Sequence
 from dataclasses import dataclass
 
 from xpra.util.env import envbool
-from xpra.common import MIN_COMPRESS_SIZE, MAX_DECOMPRESSED_SIZE
+from xpra.common import MIN_COMPRESS_SIZE, MAX_DECOMPRESSED_SIZE, Buffer
 
 # all the compressors we know about:
 ALL_COMPRESSORS: Sequence[str] = ("lz4", "zlib", "brotli", "none")

xpra/net/digest.py
@@ -6,8 +6,9 @@
 import os
 import hmac
 import hashlib
-from collections.abc import Callable, Buffer, Sequence
+from collections.abc import Callable, Sequence
 
+from xpra.common import Buffer
 from xpra.util.str_fn import csv, strtobytes, hexstr, memoryview_to_bytes
 from xpra.util.env import envint
 from xpra.log import Logger

xpra/net/lz4/lz4.pyx
@@ -8,8 +8,8 @@
 
 import struct
 from typing import Tuple
-from collections.abc import Buffer
 
+from xpra.common import Buffer
 from libc.stdint cimport uintptr_t
 from xpra.buffers.membuf cimport MemBuf, getbuf
 

xpra/net/packet_encoding.py
@@ -8,10 +8,11 @@
 # pylint: disable=import-outside-toplevel
 
 from typing import Any
-from collections.abc import Callable, Iterable, Sequence, Buffer
+from collections.abc import Callable, Iterable, Sequence
 from dataclasses import dataclass
 
 from xpra.log import Logger
+from xpra.common import Buffer
 from xpra.net.protocol.header import FLAGS_RENCODE, FLAGS_RENCODEPLUS, FLAGS_YAML, FLAGS_NOHEADER, pack_header
 from xpra.util.str_fn import strtobytes
 from xpra.util.env import envbool

xpra/net/protocol/socket_handler.py
@@ -15,7 +15,7 @@ from socket import error as socket_error
 from threading import Lock, RLock, Event, Thread, current_thread
 from queue import Queue, SimpleQueue, Empty, Full
 from typing import Any
-from collections.abc import Buffer, Callable, Iterable, Sequence, Mapping
+from collections.abc import Callable, Iterable, Sequence, Mapping
 
 from xpra.os_util import gi_import
 from xpra.util.objects import typedict
@@ -25,7 +25,7 @@ from xpra.util.str_fn import (
 )
 from xpra.util.env import envint, envbool
 from xpra.util.thread import make_thread, start_thread
-from xpra.common import noop
+from xpra.common import noop, Buffer
 from xpra.net.bytestreams import SOCKET_TIMEOUT, set_socket_timeout
 from xpra.net.protocol.header import (
     unpack_header, pack_header, find_xpra_header,

xpra/net/socket_util.py
@@ -9,9 +9,9 @@ from time import sleep, monotonic
 from ctypes import Structure, c_uint8, sizeof
 from typing import Any
 from importlib.util import find_spec
-from collections.abc import Callable, Buffer
+from collections.abc import Callable
 
-from xpra.common import GROUP, SocketState, noerr
+from xpra.common import GROUP, SocketState, noerr, Buffer
 from xpra.scripts.config import InitException, InitExit, TRUE_OPTIONS
 from xpra.exit_codes import ExitCode
 from xpra.net.common import DEFAULT_PORT, AUTO_ABSTRACT_SOCKET, ABSTRACT_SOCKET_PREFIX

xpra/net/websockets/header.py
@@ -6,8 +6,8 @@
 # Licensed under LGPL version 3 (see docs/LICENSE.LGPL-3)
 
 import struct
-from collections.abc import Buffer
 
+from xpra.common import Buffer
 from xpra.net.websockets.common import OPCODE
 from xpra.net.websockets.mask import hybi_unmask
 

xpra/server/ssh.py
@@ -12,7 +12,7 @@ import hashlib
 import binascii
 from subprocess import Popen, PIPE
 from threading import Event
-from collections.abc import Buffer, Callable
+from collections.abc import Callable
 import paramiko
 
 from xpra.net.bytestreams import pretty_socket
@@ -21,7 +21,7 @@ from xpra.util.env import envint, osexpand, first_time
 from xpra.os_util import getuid, WIN32, POSIX
 from xpra.util.thread import start_thread
 from xpra.scripts.config import str_to_bool
-from xpra.common import SSH_AGENT_DISPATCH
+from xpra.common import SSH_AGENT_DISPATCH, Buffer
 from xpra.platform.paths import get_ssh_conf_dirs, get_xpra_command
 from xpra.log import Logger
 

tests/unittests/unit/net/protocol_test.py
@@ -7,12 +7,11 @@
 import os
 import time
 import unittest
-from collections.abc import Buffer
 
 from xpra.os_util import gi_import
 from xpra.util.str_fn import csv
 from xpra.util.env import envint, envbool
-from xpra.common import noop
+from xpra.common import noop, Buffer
 from xpra.net.protocol import socket_handler, check
 from xpra.net.protocol.constants import CONNECTION_LOST
 from xpra.net.bytestreams import Connection