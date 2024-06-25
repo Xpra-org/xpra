b03d776ad547206ee6afa4eb07f55e0d1a6bbf12
Antoine Martin <antoine@xpra.org>
2024-06-25 19:36:01 +0700

    clipboard tests usually fail if there's a DE

--- tests/unittests/unit/client/osxlike_clipboard_test.py
+++ tests/unittests/unit/client/osxlike_clipboard_test.py
@@ -4,6 +4,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import os
 import unittest
 from unit.client.x11_clipboard_test_util import X11ClipboardTestUtil, has_xclip
 from xpra.os_util import OSX, POSIX
@@ -11,33 +12,31 @@ from xpra.os_util import OSX, POSIX
 
 class OSXLikeClipboardTest(X11ClipboardTestUtil):
 
-	def get_run_env(self):
-		env = super().get_run_env()
-		env.update({
-					"XPRA_CLIPBOARD_WANT_TARGETS"	: "1",
-					"XPRA_CLIPBOARD_GREEDY"			: "1",
-					})
-		return env
+    def get_run_env(self) -> dict:
+        env = super().get_run_env()
+        env.update({
+            "XPRA_CLIPBOARD_WANT_TARGETS"    : "1",
+            "XPRA_CLIPBOARD_GREEDY"            : "1",
+        })
+        return env
 
+    def test_copy(self) -> None:
+        self.do_test_copy()
 
-	def test_copy(self):
-		self.do_test_copy()
+    def test_disabled(self) -> None:
+        self.do_test_copy("disabled")
 
-	def test_disabled(self):
-		self.do_test_copy("disabled")
+    def test_to_server(self) -> None:
+        self.do_test_copy("to-server")
 
-	def test_to_server(self):
-		self.do_test_copy("to-server")
-
-	def test_to_client(self):
-		self.do_test_copy("to-client")
-
+    def test_to_client(self) -> None:
+        self.do_test_copy("to-client")
 
 
 def main():
-	if POSIX and not OSX and has_xclip():
-		unittest.main()
+    if POSIX and not OSX and has_xclip() and not os.environ.get("XDG_CURRENT_DESKTOP"):
+        unittest.main()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- tests/unittests/unit/client/x11_clipboard_test.py
+++ tests/unittests/unit/client/x11_clipboard_test.py
@@ -4,6 +4,7 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import os
 import unittest
 
 from unit.client.x11_clipboard_test_util import X11ClipboardTestUtil, has_xclip
@@ -12,29 +13,28 @@ from xpra.os_util import OSX, POSIX
 
 class X11ClipboardTest(X11ClipboardTestUtil):
 
-	@classmethod
-	def setUpClass(cls):
-		super(X11ClipboardTest, cls).setUpClass()
-		X11ClipboardTest.default_xpra_args += ["--speaker=no", "--microphone=no", "-d clipboard"]
+    @classmethod
+    def setUpClass(cls):
+        super(X11ClipboardTest, cls).setUpClass()
+        X11ClipboardTest.default_xpra_args += ["--speaker=no", "--microphone=no", "-d clipboard"]
 
-	def test_copy(self):
-		self.do_test_copy()
+    def test_copy(self):
+        self.do_test_copy()
 
-	def test_disabled(self):
-		self.do_test_copy("disabled")
+    def test_disabled(self):
+        self.do_test_copy("disabled")
 
-	def test_to_server(self):
-		self.do_test_copy("to-server")
+    def test_to_server(self):
+        self.do_test_copy("to-server")
 
-	def test_to_client(self):
-		self.do_test_copy("to-client")
-
+    def test_to_client(self):
+        self.do_test_copy("to-client")
 
 
 def main():
-	if POSIX and not OSX and has_xclip():
-		unittest.main()
+    if POSIX and not OSX and has_xclip() and not os.environ.get("XDG_CURRENT_DESKTOP"):
+        unittest.main()
 
 
 if __name__ == '__main__':
-	main()
+    main()
