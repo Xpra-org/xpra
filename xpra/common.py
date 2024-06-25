Xpra-org/xpra

commit 04d27ed50c180802215390faa83b4968be5ebb09

we want our buffers to support 'Sized'

xpra/common.py
@@ -8,8 +8,12 @@
 import binascii
 import os
 from enum import Enum, IntEnum
-from typing import TypeAlias, Final
-from collections.abc import Callable
+from typing import Final, Protocol
+from collections.abc import Callable, Sized
+
+from xpra.util.env import envint, envbool
+from xpra.util.str_fn import nicestr
+
 
 try:
     # Python 3.11 and later:
@@ -19,15 +23,14 @@ except ImportError:     # pragma: no cover
 
 try:
     # Python 3.12 and later:
-    from collections.abc import Buffer as abc_Buffer
-    Buffer: TypeAlias = abc_Buffer
+    from collections.abc import Buffer
 except ImportError:
-    from typing import ByteString
-    Buffer: TypeAlias = ByteString
+    from typing import ByteString as Buffer
 
 
-from xpra.util.env import envint, envbool
-from xpra.util.str_fn import nicestr
+class SizedBuffer(Buffer, Sized, Protocol):
+    pass
+
 
 # noinspection PyPep8
 RESOLUTION_ALIASES: dict[str, tuple[int, int]] = {

tests/unittests/unit/net/protocol_test.py
@@ -11,7 +11,7 @@ import unittest
 from xpra.os_util import gi_import
 from xpra.util.str_fn import csv
 from xpra.util.env import envint, envbool
-from xpra.common import noop, Buffer
+from xpra.common import noop, SizedBuffer
 from xpra.net.protocol import socket_handler, check
 from xpra.net.protocol.constants import CONNECTION_LOST
 from xpra.net.bytestreams import Connection
@@ -37,7 +37,7 @@ class FastMemoryConnection(Connection):
         self.write_data = []
         Connection.__init__(self, "local", socktype, {})
 
-    def read(self, n) -> Buffer:
+    def read(self, n) -> SizedBuffer:
         if self.read_buffers is None:
             while self.active:
                 time.sleep(0.1)
@@ -137,7 +137,7 @@ class ProtocolTest(unittest.TestCase):
         self.do_test_invalid_data([b"\0"*1])
         self.do_test_invalid_data([b"P"*8])
 
-    def do_test_invalid_data(self, data: Buffer) -> None:
+    def do_test_invalid_data(self, data: SizedBuffer) -> None:
         errs = []
         proto = self.make_memory_protocol(data)
 

xpra/codecs/pillow/decoder.py
@@ -11,9 +11,9 @@ from PIL import Image
 from typing import Any
 from collections.abc import Callable, Sequence
 
-from xpra.common import Buffer
+from xpra.common import Buffer, SizedBuffer
 from xpra.util.objects import typedict
-from xpra.util.str_fn import csv, strtobytes, hexstr
+from xpra.util.str_fn import csv, strtobytes, hexstr, memoryview_to_bytes
 from xpra.codecs.debug import may_save_image
 from xpra.log import Logger
 
@@ -26,7 +26,7 @@ DECODE_FORMATS = os.environ.get("XPRA_PILLOW_DECODE_FORMATS", "png,png/L,png/P,j
 PNG_HEADER = struct.pack("BBBBBBBB", 137, 80, 78, 71, 13, 10, 26, 10)
 
 
-def is_png(data) -> bool:
+def is_png(data: bytes) -> bool:
     return data.startswith(PNG_HEADER)
 
 
@@ -34,32 +34,32 @@ RIFF_HEADER = b"RIFF"
 WEBP_HEADER = b"WEBP"
 
 
-def is_webp(data) -> bool:
+def is_webp(data: bytes) -> bool:
     return data[:4] == RIFF_HEADER and data[8:12] == WEBP_HEADER
 
 
 JPEG_HEADER = struct.pack("BBB", 0xFF, 0xD8, 0xFF)
 
 
-def is_jpeg(data) -> bool:
+def is_jpeg(data: bytes) -> bool:
     # the jpeg header is actually more complicated than this,
     # but in practice all the data we receive from the server
     # will have this type of header
     return data[:3] == JPEG_HEADER
 
 
-def is_svg(data) -> bool:
+def is_svg(data: bytes) -> bool:
     return strtobytes(data[:5]) == b"<?xml" or strtobytes(data[:4]) == b"<svg"
 
 
 XPM_HEADER = b"/* XPM */"
 
 
-def is_xpm(data) -> bool:
+def is_xpm(data: bytes) -> bool:
     return data[:9] == XPM_HEADER
 
 
-def is_tiff(data) -> bool:
+def is_tiff(data: bytes) -> bool:
     if data[:2] == b"II":
         return data[2] == 42 and data[3] == 0
     if data[:2] == b"MM":
@@ -67,7 +67,7 @@ def is_tiff(data) -> bool:
     return False
 
 
-HEADERS: dict[Callable, str] = {
+HEADERS: dict[Callable[[bytes], bool], str] = {
     is_png: "png",
     is_webp: "webp",
     is_jpeg: "jpeg",
@@ -77,13 +77,14 @@ HEADERS: dict[Callable, str] = {
 }
 
 
-def get_image_type(data) -> str:
+def get_image_type(data: SizedBuffer) -> str:
     if not data:
         return ""
     if len(data) < 32:
         return ""
+    header = memoryview_to_bytes(data[:32])
     for fn, encoding in HEADERS.items():
-        if fn(data):
+        if fn(header):
             return encoding
     return ""
 

xpra/net/compression.py
@@ -10,7 +10,8 @@ from collections.abc import Callable, Sequence
 from dataclasses import dataclass
 
 from xpra.util.env import envbool
-from xpra.common import MIN_COMPRESS_SIZE, MAX_DECOMPRESSED_SIZE, Buffer
+from xpra.util.str_fn import memoryview_to_bytes
+from xpra.common import MIN_COMPRESS_SIZE, MAX_DECOMPRESSED_SIZE, SizedBuffer
 
 # all the compressors we know about:
 ALL_COMPRESSORS: Sequence[str] = ("lz4", "zlib", "brotli", "none")
@@ -26,8 +27,8 @@ PERFORMANCE_COMPRESSION: Sequence[str] = ("lz4", "brotli")
 class Compression:
     name: str
     version: str
-    compress: Callable[[Buffer, int], tuple[int, Buffer]]
-    decompress: Callable[[Buffer], Buffer]
+    compress: Callable[[SizedBuffer, int], tuple[int, SizedBuffer]]
+    decompress: Callable[[SizedBuffer], SizedBuffer]
 
 
 COMPRESSION: dict[str, Compression] = {}
@@ -39,11 +40,11 @@ def init_lz4() -> Compression:
     from xpra.net.lz4.lz4 import compress, decompress, get_version  # @UnresolvedImport
     from xpra.net.protocol.header import LZ4_FLAG
 
-    def lz4_compress(packet: Buffer, level: int) -> tuple[int, memoryview]:
+    def lz4_compress(data: SizedBuffer, level: int) -> tuple[int, memoryview]:
         flag = min(15, level) | LZ4_FLAG
-        return flag, compress(packet, acceleration=max(0, 5 - level // 3))
+        return flag, compress(data, acceleration=max(0, 5 - level // 3))
 
-    def lz4_decompress(data: Buffer) -> Buffer:
+    def lz4_decompress(data: SizedBuffer) -> memoryview:
         return decompress(data, max_size=MAX_DECOMPRESSED_SIZE)
 
     return Compression("lz4", get_version(), lz4_compress, lz4_decompress)
@@ -59,7 +60,7 @@ def init_brotli() -> Compression:
     brotli_compress = compress
     brotli_version = get_version()
 
-    def brotli_compress_shim(packet: Buffer, level: int) -> tuple[int, memoryview]:
+    def brotli_compress_shim(packet: SizedBuffer, level: int) -> tuple[int, memoryview]:
         if len(packet) > 1024 * 1024:
             level = min(9, level)
         else:
@@ -72,14 +73,13 @@ def init_brotli() -> Compression:
 
 
 def init_none() -> Compression:
-    from xpra.util.str_fn import strtobytes
 
-    def nocompress(packet: Buffer, _level) -> tuple[int, Buffer]:
-        if not isinstance(packet, bytes):
-            packet = bytes(str(packet), 'UTF-8')
-        return 0, strtobytes(packet)
+    def nocompress(data: SizedBuffer, _level) -> tuple[int, SizedBuffer]:
+        if isinstance(data, bytes):
+            return 0, data
+        return 0, memoryview_to_bytes(data)
 
-    def nodecompress(v: Buffer) -> Buffer:
+    def nodecompress(v: SizedBuffer) -> SizedBuffer:
         return v
 
     return Compression("none", "0", nocompress, nodecompress)

xpra/net/digest.py
@@ -8,7 +8,7 @@ import hmac
 import hashlib
 from collections.abc import Callable, Sequence
 
-from xpra.common import Buffer
+from xpra.common import SizedBuffer
 from xpra.util.str_fn import csv, strtobytes, hexstr, memoryview_to_bytes
 from xpra.util.env import envint
 from xpra.log import Logger
@@ -62,7 +62,7 @@ def choose_digest(options: Sequence[str]) -> str:
     raise ValueError(f"no known digest options found in '{csv(options)}'")
 
 
-def gendigest(digest: str, password_in, salt_in: Buffer) -> bytes:
+def gendigest(digest: str, password_in, salt_in: SizedBuffer) -> bytes:
     assert password_in and salt_in
     salt: bytes = memoryview_to_bytes(salt_in)
     password: bytes = strtobytes(password_in)

xpra/net/lz4/lz4.pyx
@@ -9,7 +9,7 @@
 import struct
 from typing import Tuple
 
-from xpra.common import Buffer
+from xpra.common import Buffer, SizedBuffer
 from libc.stdint cimport uintptr_t
 from xpra.buffers.membuf cimport MemBuf, getbuf
 
@@ -52,7 +52,7 @@ cdef class compressor:
     def bound(self, int size) -> int:
         return LZ4_compressBound(size)
 
-    def compress(self, data: Buffer, int acceleration=1, int max_size=0, int store_size=True) -> Buffer:
+    def compress(self, data: Buffer, int acceleration=1, int max_size=0, int store_size=True) -> SizedBuffer:
         cdef Py_buffer in_buf
         if PyObject_GetBuffer(data, &in_buf, PyBUF_ANY_CONTIGUOUS):
             raise ValueError("failed to read data from %s" % type(data))
@@ -81,11 +81,11 @@ cdef class compressor:
         PyBuffer_Release(&in_buf)
         return (mem[:(size_header+r)]).toreadonly()
 
-def compress(data: Buffer, acceleration=1) -> Buffer:
+def compress(data: Buffer, acceleration=1) -> SizedBuffer:
     c = compressor()
     return c.compress(data, acceleration)
 
-def decompress(data: Buffer, int max_size=0, int size=0) -> Buffer:
+def decompress(data: Buffer, int max_size=0, int size=0) -> SizedBuffer:
     cdef int size_header = 0
     if size==0:
         size = struct.unpack_from(b"@I", data[:4])[0]

xpra/net/packet_encoding.py
@@ -12,7 +12,7 @@ from collections.abc import Callable, Iterable, Sequence
 from dataclasses import dataclass
 
 from xpra.log import Logger
-from xpra.common import Buffer
+from xpra.common import SizedBuffer
 from xpra.net.protocol.header import FLAGS_RENCODE, FLAGS_RENCODEPLUS, FLAGS_YAML, FLAGS_NOHEADER, pack_header
 from xpra.util.str_fn import strtobytes
 from xpra.util.env import envbool
@@ -30,8 +30,8 @@ class Encoding:
     name: str
     flag: int
     version: str
-    encode: Callable[[Any], tuple[Buffer, int]]
-    decode: Callable[[Buffer], Any]
+    encode: Callable[[Any], tuple[SizedBuffer, int]]
+    decode: Callable[[SizedBuffer], Any]
 
 
 ENCODERS: dict[str, Encoding] = {}
@@ -41,13 +41,13 @@ def init_rencodeplus() -> Encoding:
     from xpra.net.rencodeplus import rencodeplus  # type: ignore[attr-defined]
     rencodeplus_dumps = rencodeplus.dumps  # @UndefinedVariable
 
-    def do_rencodeplus(v) -> tuple[Buffer, int]:
+    def do_rencodeplus(v) -> tuple[SizedBuffer, int]:
         return rencodeplus_dumps(v), FLAGS_RENCODEPLUS
 
     return Encoding("rencodeplus", FLAGS_RENCODEPLUS, rencodeplus.__version__, do_rencodeplus, rencodeplus.loads)
 
 
-def none_encode(data: Buffer) -> tuple[bytes, int]:
+def none_encode(data: SizedBuffer) -> tuple[bytes, int]:
     # just send data as a byte string for clients that don't understand xpra packet format:
     import codecs
 
@@ -59,7 +59,7 @@ def none_encode(data: Buffer) -> tuple[bytes, int]:
     return b(data) + b"\n", FLAGS_NOHEADER
 
 
-def none_decode(data: Buffer) -> Buffer:
+def none_decode(data: SizedBuffer) -> SizedBuffer:
     return data
 
 

xpra/net/protocol/socket_handler.py
@@ -25,7 +25,7 @@ from xpra.util.str_fn import (
 )
 from xpra.util.env import envint, envbool
 from xpra.util.thread import make_thread, start_thread
-from xpra.common import noop, Buffer
+from xpra.common import noop, SizedBuffer
 from xpra.net.bytestreams import SOCKET_TIMEOUT, set_socket_timeout
 from xpra.net.protocol.header import (
     unpack_header, pack_header, find_xpra_header,
@@ -125,12 +125,12 @@ class SocketProtocol:
         self._conn = conn
         self._process_packet_cb: Callable[[Any, PacketType], None] = process_packet_cb
         self.make_chunk_header: Callable = self.make_xpra_header
-        self.make_frame_header: Callable[[str | int, Iterable], Buffer] = self.noframe_header
+        self.make_frame_header: Callable[[str | int, Iterable], SizedBuffer] = self.noframe_header
         self._write_queue: Queue[tuple[Sequence, str, bool, bool] | None] = Queue(1)
-        self._read_queue: Queue[Buffer] = Queue(20)
+        self._read_queue: Queue[SizedBuffer] = Queue(20)
         self._pre_read = None
-        self._process_read: Callable[[Buffer], None] = self.read_queue_put
-        self._read_queue_put: Callable[[Buffer], None] = self.read_queue_put
+        self._process_read: Callable[[SizedBuffer], None] = self.read_queue_put
+        self._read_queue_put: Callable[[SizedBuffer], None] = self.read_queue_put
         self._get_packet_cb: Callable[[], tuple[PacketType, bool, bool]] = get_packet_cb
         # counters:
         self.input_stats: dict[str, int] = {}
@@ -469,11 +469,11 @@ class SocketProtocol:
 
     @staticmethod
     def make_xpra_header(_packet_type: str | int,
-                         proto_flags: int, level: int, index: int, payload_size: int) -> Buffer:
+                         proto_flags: int, level: int, index: int, payload_size: int) -> SizedBuffer:
         return pack_header(proto_flags, level, index, payload_size)
 
     @staticmethod
-    def noframe_header(_packet_type: str | int, _items) -> Buffer:
+    def noframe_header(_packet_type: str | int, _items) -> SizedBuffer:
         return b""
 
     def start_write_thread(self) -> None:
@@ -754,7 +754,7 @@ class SocketProtocol:
         self.output_packetcount += 1
 
     # noinspection PyMethodMayBeStatic
-    def con_write(self, con, buf: Buffer, packet_type: str):
+    def con_write(self, con, buf: SizedBuffer, packet_type: str):
         return con.write(buf, packet_type)
 
     def _read_thread_loop(self) -> None:
@@ -774,7 +774,7 @@ class SocketProtocol:
         self.input_raw_packetcount += 1
         return True
 
-    def con_read(self) -> Buffer:
+    def con_read(self) -> SizedBuffer:
         if self._pre_read:
             r = self._pre_read.pop(0)
             log("con_read() using pre_read value: %r", Ellipsizer(r))
@@ -798,22 +798,22 @@ class SocketProtocol:
         self.close(message)
         return False
 
-    def invalid(self, msg, data) -> None:
+    def invalid(self, msg: str, data: SizedBuffer) -> None:
         GLib.idle_add(self._process_packet_cb, self, [INVALID, msg, data])
         # Then hang up:
         GLib.timeout_add(1000, self._connection_lost, msg)
 
-    def gibberish(self, msg, data) -> None:
+    def gibberish(self, msg: str, data: SizedBuffer) -> None:
         GLib.idle_add(self._process_packet_cb, self, [GIBBERISH, msg, data])
         # Then hang up:
         GLib.timeout_add(self.hangup_delay, self._connection_lost, msg)
 
     # delegates to invalid_header()
     # (so this can more easily be intercepted and overridden)
-    def invalid_header(self, proto, data: Buffer, msg="invalid packet header") -> None:
+    def invalid_header(self, proto, data: SizedBuffer, msg="invalid packet header") -> None:
         self._invalid_header(proto, data, msg)
 
-    def _invalid_header(self, proto, data: Buffer, msg="invalid packet header") -> None:
+    def _invalid_header(self, proto, data: SizedBuffer, msg="invalid packet header") -> None:
         log("invalid_header(%s, %s bytes: '%s', %s)",
             proto, len(data or ""), msg, Ellipsizer(data))
         guess = guess_packet_type(data)
@@ -825,10 +825,10 @@ class SocketProtocol:
                 err += " read buffer=%s (%i bytes)" % (repr_ellipsized(data), len(data))
         self.gibberish(err, data)
 
-    def process_read(self, data: Buffer) -> None:
+    def process_read(self, data: SizedBuffer) -> None:
         self._read_queue_put(data)
 
-    def read_queue_put(self, data: Buffer) -> None:
+    def read_queue_put(self, data: SizedBuffer) -> None:
         # start the parse thread if needed:
         if not self._read_parser_thread and not self._closed:
             if data is None:
@@ -867,14 +867,14 @@ class SocketProtocol:
             from the UI thread will need to use a callback (usually via 'idle_add')
         """
         header = b""
-        read_buffers: list[Buffer] = []
+        read_buffers: list[SizedBuffer] = []
         payload_size = -1
         padding_size = 0
         packet_index = 0
         protocol_flags = 0
         data_size = 0
         compression_level = 0
-        raw_packets: dict[int, Buffer] = {}
+        raw_packets: dict[int, SizedBuffer] = {}
         while not self._closed:
             # log("parse thread: %i items in read queue", self._read_queue.qsize())
             buf = self._read_queue.get()
@@ -977,7 +977,7 @@ class SocketProtocol:
                     # incomplete packet, wait for the rest to arrive
                     break
 
-                data: Buffer
+                data: SizedBuffer
                 buf = read_buffers[0]
                 if len(buf) == payload_size:
                     # exact match, consume it all:
@@ -1263,7 +1263,7 @@ class SocketProtocol:
             log_count("received", icount, getattr(self._conn, "input_bytecount", -1))
             log_count("sent", ocount, getattr(self._conn, "output_bytecount", -1))
 
-    def steal_connection(self, read_callback: Callable[[Buffer], None] | None = None):
+    def steal_connection(self, read_callback: Callable[[SizedBuffer], None] | None = None):
         # so we can re-use this connection somewhere else
         # (frees all protocol threads and resources)
         # Note: this method can only be used with non-blocking sockets,

commit 89559144838b34708f746787323615d357900992

fix builds for Python < 3.12
breakage was introduced in 04d27ed50c180802215390faa83b4968be5ebb09

xpra/common.py
@@ -24,12 +24,13 @@ except ImportError:     # pragma: no cover
 try:
     # Python 3.12 and later:
     from collections.abc import Buffer
+
+    class SizedBuffer(Buffer, Sized, Protocol):
+        pass
 except ImportError:
-    from typing import ByteString as Buffer
-
-
-class SizedBuffer(Buffer, Sized, Protocol):
-    pass
+    class SizedBuffer(Sized, Protocol):
+        def __buffer__(self):
+            raise NotImplementedError()
 
 
 # noinspection PyPep8