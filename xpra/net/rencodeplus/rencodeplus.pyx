bd02429ca0d3cb01b23c9d24f67534f6394e7d99
Antoine Martin <antoine@xpra.org>
2024-06-30 14:07:20 +0700

    stronger types and inline compiler hint

--- xpra/net/rencodeplus/rencodeplus.pyx
+++ xpra/net/rencodeplus/rencodeplus.pyx
@@ -32,7 +32,7 @@ from cpython cimport bool
 from libc.stdlib cimport realloc, free
 from libc.string cimport memcpy
 
-__version__ = ("Cython", 1, 0, 9)
+__version__ = ("Cython", 1, 0, 10)
 
 
 cdef extern from "Python.h":
@@ -41,7 +41,7 @@ cdef extern from "Python.h":
     int PyBUF_ANY_CONTIGUOUS
 
 
-cdef bool big_endian = sys.byteorder!="little"
+cdef bool big_endian = sys.byteorder != "little"
 
 cdef enum:
     # Default number of bits for serialized floats, either 32 or 64 (also a parameter for dumps()).
@@ -80,20 +80,24 @@ cdef enum:
     LIST_FIXED_COUNT = 64
 #assert LIST_FIXED_START + LIST_FIXED_COUNT == 256
 
-cdef swap_byte_order_ushort(unsigned short *s):
+
+cdef void swap_byte_order_ushort(unsigned short *s):
     s[0] = (s[0] >> 8) | (s[0] << 8)
 
-cdef swap_byte_order_short(char *c):
+
+cdef short swap_byte_order_short(char *c):
     cdef short s
     cdef char *p = <char *>&s
     p[0] = c[1]
     p[1] = c[0]
     return s
 
-cdef swap_byte_order_uint(int *i):
+
+cdef void swap_byte_order_uint(int *i):
     i[0] = (i[0] >> 24) | ((i[0] << 8) & 0x00FF0000) | ((i[0] >> 8) & 0x0000FF00) | (i[0] << 24)
 
-cdef swap_byte_order_int(char *c):
+
+cdef int swap_byte_order_int(char *c):
     cdef int i
     cdef char *p = <char *>&i
     p[0] = c[3]
@@ -102,7 +106,8 @@ cdef swap_byte_order_int(char *c):
     p[3] = c[0]
     return i
 
-cdef swap_byte_order_ulong_long(long long *l):
+
+cdef void swap_byte_order_ulong_long(long long *l):
     l[0] = (l[0] >> 56) | \
            ((l[0] << 40) & 0x00FF000000000000) | \
            ((l[0] << 24) & 0x0000FF0000000000) | \
@@ -112,7 +117,8 @@ cdef swap_byte_order_ulong_long(long long *l):
            ((l[0] >> 40) & 0x000000000000FF00) | \
            (l[0] << 56)
 
-cdef swap_byte_order_long_long(char *c):
+
+cdef long long swap_byte_order_long_long(char *c):
     cdef long long l
     cdef char *p = <char *>&l
     p[0] = c[7]
@@ -125,7 +131,8 @@ cdef swap_byte_order_long_long(char *c):
     p[7] = c[0]
     return l
 
-cdef swap_byte_order_float(char *c):
+
+cdef float swap_byte_order_float(char *c):
     cdef float f
     cdef char *p = <char *>&f
     p[0] = c[3]
@@ -133,6 +140,7 @@ cdef swap_byte_order_float(char *c):
     p[2] = c[1]
     p[3] = c[0]
     return f
+
 
 cdef swap_byte_order_double(char *c):
     cdef double d
@@ -147,21 +155,24 @@ cdef swap_byte_order_double(char *c):
     p[7] = c[0]
     return d
 
-cdef write_buffer_char(char **buf, unsigned int *pos, char c):
+
+cdef void write_buffer_char(char **buf, unsigned int *pos, char c):
     buf[0] = <char*>realloc(buf[0], pos[0] + 1)
     if buf[0] == NULL:
         raise MemoryError("Error in realloc, 1 byte needed")
     memcpy(&buf[0][pos[0]], &c, 1)
     pos[0] += 1
 
-cdef write_buffer(char **buf, unsigned int *pos, void* data, int size):
+
+cdef void write_buffer(char **buf, unsigned int *pos, void* data, int size):
     buf[0] = <char*>realloc(buf[0], pos[0] + size)
     if buf[0] == NULL:
         raise MemoryError(f"Error in realloc, {size} bytes needed")
     memcpy(&buf[0][pos[0]], data, size)
     pos[0] += size
 
-cdef encode_char(char **buf, unsigned int *pos, signed char x):
+
+cdef inline void encode_char(char **buf, unsigned int *pos, signed char x):
     if 0 <= x < INT_POS_FIXED_COUNT:
         write_buffer_char(buf, pos, INT_POS_FIXED_START + x)
     elif -INT_NEG_FIXED_COUNT <= x < 0:
@@ -170,7 +181,8 @@ cdef encode_char(char **buf, unsigned int *pos, signed char x):
         write_buffer_char(buf, pos, CHR_INT1)
         write_buffer_char(buf, pos, x)
 
-cdef encode_short(char **buf, unsigned int *pos, short x):
+
+cdef inline void encode_short(char **buf, unsigned int *pos, short x):
     write_buffer_char(buf, pos, CHR_INT2)
     if not big_endian:
         if x > 0:
@@ -179,7 +191,8 @@ cdef encode_short(char **buf, unsigned int *pos, short x):
             x = swap_byte_order_short(<char*>&x)
     write_buffer(buf, pos, &x, sizeof(x))
 
-cdef encode_int(char **buf, unsigned int *pos, int x):
+
+cdef inline void encode_int(char **buf, unsigned int *pos, int x):
     write_buffer_char(buf, pos, CHR_INT4)
     if not big_endian:
         if x > 0:
@@ -188,7 +201,8 @@ cdef encode_int(char **buf, unsigned int *pos, int x):
             x = swap_byte_order_int(<char*>&x)
     write_buffer(buf, pos, &x, sizeof(x))
 
-cdef encode_long_long(char **buf, unsigned int *pos, long long x):
+
+cdef inline void encode_long_long(char **buf, unsigned int *pos, long long x):
     write_buffer_char(buf, pos, CHR_INT8)
     if not big_endian:
         if x > 0:
@@ -197,7 +211,7 @@ cdef encode_long_long(char **buf, unsigned int *pos, long long x):
             x = swap_byte_order_long_long(<char*>&x)
     write_buffer(buf, pos, &x, sizeof(x))
 
-cdef encode_big_number(char **buf, unsigned int *pos, char *x):
+cdef inline void encode_big_number(char **buf, unsigned int *pos, char *x):
     write_buffer_char(buf, pos, CHR_INT)
     write_buffer(buf, pos, x, len(x))
     write_buffer_char(buf, pos, CHR_TERM)
@@ -208,13 +222,13 @@ cdef encode_big_number(char **buf, unsigned int *pos, char *x):
 #        x = swap_byte_order_float(<char *>&x)
 #    write_buffer(buf, pos, &x, sizeof(x))
 
-cdef encode_float64(char **buf, unsigned int *pos, double x):
+cdef inline void encode_float64(char **buf, unsigned int *pos, double x):
     write_buffer_char(buf, pos, CHR_FLOAT64)
     if not big_endian:
         x = swap_byte_order_double(<char *>&x)
     write_buffer(buf, pos, &x, sizeof(x))
 
-cdef encode_memoryview(char **buf, unsigned int *pos, mem):
+cdef inline void encode_memoryview(char **buf, unsigned int *pos, mem):
     cdef Py_buffer mv
     if PyObject_GetBuffer(mem, &mv, PyBUF_ANY_CONTIGUOUS):
         raise ValueError(f"failed to read data from {type(mem)}")
@@ -226,7 +240,7 @@ cdef encode_memoryview(char **buf, unsigned int *pos, mem):
     write_buffer(buf, pos, <char *>p, lx)
     PyBuffer_Release(&mv)
 
-cdef encode_bytes(char **buf, unsigned int *pos, bytes x):
+cdef inline void encode_bytes(char **buf, unsigned int *pos, bytes x):
     cdef char *p
     cdef int lx = len(x)
     s = b"%i/" % lx
@@ -234,7 +248,7 @@ cdef encode_bytes(char **buf, unsigned int *pos, bytes x):
     write_buffer(buf, pos, p, len(s))
     write_buffer(buf, pos, <char *>x, lx)
 
-cdef encode_str(char **buf, unsigned int *pos, bytes x):
+cdef inline void encode_str(char **buf, unsigned int *pos, bytes x):
     cdef char *p
     cdef int lx = len(x)
     if lx < STR_FIXED_COUNT:
@@ -246,13 +260,13 @@ cdef encode_str(char **buf, unsigned int *pos, bytes x):
         write_buffer(buf, pos, p, len(s))
         write_buffer(buf, pos, <char *>x, lx)
 
-cdef encode_none(char **buf, unsigned int *pos):
+cdef inline void encode_none(char **buf, unsigned int *pos):
     write_buffer_char(buf, pos, CHR_NONE)
 
-cdef encode_bool(char **buf, unsigned int *pos, bool x):
+cdef inline void encode_bool(char **buf, unsigned int *pos, bool x):
     write_buffer_char(buf, pos, CHR_TRUE if x else CHR_FALSE)
 
-cdef encode_list(char **buf, unsigned int *pos, x):
+cdef inline void encode_list(char **buf, unsigned int *pos, x):
     if len(x) < LIST_FIXED_COUNT:
         write_buffer_char(buf, pos, LIST_FIXED_START + len(x))
         for i in x:
@@ -263,7 +277,7 @@ cdef encode_list(char **buf, unsigned int *pos, x):
             encode(buf, pos, i)
         write_buffer_char(buf, pos, CHR_TERM)
 
-cdef encode_dict(char **buf, unsigned int *pos, x):
+cdef inline void encode_dict(char **buf, unsigned int *pos, x):
     if len(x) < DICT_FIXED_COUNT:
         write_buffer_char(buf, pos, DICT_FIXED_START + len(x))
         for k, v in x.items():
@@ -276,14 +290,15 @@ cdef encode_dict(char **buf, unsigned int *pos, x):
             encode(buf, pos, v)
         write_buffer_char(buf, pos, CHR_TERM)
 
-cdef object MAX_SIGNED_INT = 2**31
-cdef object MIN_SIGNED_INT = -MAX_SIGNED_INT
-#note: negating the Python value avoids compiler problems
-#(negating the "long long" constant can make it unsigned with some compilers!)
+
+DEF MAX_SIGNED_INT = 2**31
+DEF MIN_SIGNED_INT = -MAX_SIGNED_INT
+# note: negating the Python value avoids compiler problems
+# (negating the "long long" constant can make it unsigned with some compilers!)
 cdef object MAX_SIGNED_LONGLONG = int(2**63)
 cdef object MIN_SIGNED_LONGLONG = -MAX_SIGNED_LONGLONG
 
-cdef encode(char **buf, unsigned int *pos, data):
+cdef void encode(char **buf, unsigned int *pos, data):
     t = type(data)
     if t == int:
         if -128 <= data < 128:
@@ -345,14 +360,15 @@ def dumps(data) -> SizedBuffer:
     return ret
 
 
-cdef decode_char(char *data, unsigned int *pos, long long data_length):
+cdef inline signed char decode_char(char *data, unsigned int *pos, long long data_length):
     cdef signed char c
     check_pos(data, pos[0]+1, data_length)
     memcpy(&c, &data[pos[0]+1], 1)
     pos[0] += 2
     return c
 
-cdef decode_short(char *data, unsigned int *pos, long long data_length):
+
+cdef inline short decode_short(char *data, unsigned int *pos, long long data_length):
     cdef short s
     check_pos(data, pos[0]+2, data_length)
     memcpy(&s, &data[pos[0]+1], 2)
@@ -361,7 +377,8 @@ cdef decode_short(char *data, unsigned int *pos, long long data_length):
         s = swap_byte_order_short(<char*>&s)
     return s
 
-cdef decode_int(char *data, unsigned int *pos, long long data_length):
+
+cdef inline int decode_int(char *data, unsigned int *pos, long long data_length):
     cdef int i
     check_pos(data, pos[0]+4, data_length)
     memcpy(&i, &data[pos[0]+1], 4)
@@ -370,7 +387,8 @@ cdef decode_int(char *data, unsigned int *pos, long long data_length):
         i = swap_byte_order_int(<char*>&i)
     return i
 
-cdef decode_long_long(char *data, unsigned int *pos, long long data_length):
+
+cdef inline long long decode_long_long(char *data, unsigned int *pos, long long data_length):
     cdef long long l
     check_pos(data, pos[0]+8, data_length)
     memcpy(&l, &data[pos[0]+1], 8)
@@ -379,15 +397,18 @@ cdef decode_long_long(char *data, unsigned int *pos, long long data_length):
         l = swap_byte_order_long_long(<char*>&l)
     return l
 
-cdef decode_fixed_pos_int(char *data, unsigned int *pos):
+
+cdef inline unsigned int decode_fixed_pos_int(char *data, unsigned int *pos):
     pos[0] += 1
     return data[pos[0] - 1] - INT_POS_FIXED_START
 
-cdef decode_fixed_neg_int(char *data, unsigned int *pos):
+
+cdef inline int decode_fixed_neg_int(char *data, unsigned int *pos):
     pos[0] += 1
     return (data[pos[0] - 1] - INT_NEG_FIXED_START + 1)*-1
 
-cdef decode_big_number(char *data, unsigned int *pos, long long data_length):
+
+cdef inline object decode_big_number(char *data, unsigned int *pos, long long data_length):
     pos[0] += 1
     cdef int x = 18
     check_pos(data, pos[0]+x, data_length)
@@ -401,7 +422,8 @@ cdef decode_big_number(char *data, unsigned int *pos, long long data_length):
     pos[0] += x + 1
     return big_number
 
-cdef decode_float32(char *data, unsigned int *pos, long long data_length):
+
+cdef inline float decode_float32(char *data, unsigned int *pos, long long data_length):
     cdef float f
     check_pos(data, pos[0]+4, data_length)
     memcpy(&f, &data[pos[0]+1], 4)
@@ -410,7 +432,8 @@ cdef decode_float32(char *data, unsigned int *pos, long long data_length):
         f = swap_byte_order_float(<char*>&f)
     return f
 
-cdef decode_float64(char *data, unsigned int *pos, long long data_length):
+
+cdef inline double decode_float64(char *data, unsigned int *pos, long long data_length):
     cdef double d
     check_pos(data, pos[0]+8, data_length)
     memcpy(&d, &data[pos[0]+1], 8)
@@ -419,14 +442,16 @@ cdef decode_float64(char *data, unsigned int *pos, long long data_length):
         d = swap_byte_order_double(<char*>&d)
     return d
 
-cdef decode_fixed_str(char *data, unsigned int *pos, long long data_length):
+
+cdef inline object decode_fixed_str(char *data, unsigned int *pos, long long data_length):
     cdef unsigned char size = data[pos[0]] - STR_FIXED_START + 1
     check_pos(data, pos[0] + size - 1, data_length)
     s = data[pos[0]+1:pos[0] + size]
     pos[0] += size
     return s.decode("utf8")
 
-cdef decode_str(char *data, unsigned int *pos, long long data_length):
+
+cdef inline object decode_str(char *data, unsigned int *pos, long long data_length):
     cdef unsigned int x = 1
     check_pos(data, pos[0]+x, data_length)
     while (data[pos[0]+x] not in (58, 47)):
@@ -442,12 +467,14 @@ cdef decode_str(char *data, unsigned int *pos, long long data_length):
         return s
     return s.decode("utf8")
 
-cdef decode_fixed_list(char *data, unsigned int *pos, long long data_length):
+
+cdef inline tuple decode_fixed_list(char *data, unsigned int *pos, long long data_length):
     size = <unsigned char>data[pos[0]] - LIST_FIXED_START
     pos[0] += 1
     return tuple(decode(data, pos, data_length) for _ in range(size))
 
-cdef decode_list(char *data, unsigned int *pos, long long data_length):
+
+cdef inline tuple decode_list(char *data, unsigned int *pos, long long data_length):
     l = []
     pos[0] += 1
     while data[pos[0]] != CHR_TERM:
@@ -455,12 +482,14 @@ cdef decode_list(char *data, unsigned int *pos, long long data_length):
     pos[0] += 1
     return tuple(l)
 
-cdef decode_fixed_dict(char *data, unsigned int *pos, long long data_length):
+
+cdef inline dict decode_fixed_dict(char *data, unsigned int *pos, long long data_length):
     size = <unsigned char>data[pos[0]] - DICT_FIXED_START
     pos[0] += 1
     return dict((decode(data, pos, data_length), decode(data, pos, data_length)) for _ in range(size))
 
-cdef decode_dict(char *data, unsigned int *pos, long long data_length):
+
+cdef inline dict decode_dict(char *data, unsigned int *pos, long long data_length):
     d = {}
     pos[0] += 1
     check_pos(data, pos[0], data_length)
@@ -470,12 +499,13 @@ cdef decode_dict(char *data, unsigned int *pos, long long data_length):
     pos[0] += 1
     return d
 
-cdef inline check_pos(char *data, unsigned int pos, long long data_length):
+
+cdef inline void check_pos(char *data, unsigned int pos, long long data_length):
     if pos >= data_length:
         raise IndexError(f"Tried to access data[{pos}] but data len is: {data_length}")
 
 
-cdef decode(char *data, unsigned int *pos, long long data_length):
+cdef object decode(char *data, unsigned int *pos, long long data_length):
     check_pos(data, pos[0], data_length)
     cdef unsigned char typecode = data[pos[0]]
     if typecode == CHR_INT1:
@@ -520,6 +550,7 @@ cdef decode(char *data, unsigned int *pos, long long data_length):
         return decode_dict(data, pos, data_length)
     else:
         raise ValueError(f"unsupported typecode {typecode}")
+
 
 def loads(data):
     """
