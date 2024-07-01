2d51d299e3054f96c86f3969b47ce19ef02e7c23
Antoine Martin <antoine@xpra.org>
2024-07-01 18:45:55 +0700

    #4270 test all combinations and report speed

--- tests/unittests/unit/client/cairo_test.py
+++ tests/unittests/unit/client/cairo_test.py
@@ -4,15 +4,18 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
+import struct
 import cairo
 import unittest
+from time import monotonic
 from io import BytesIO
 from PIL import Image
 
-from xpra.codecs.checks import TEST_COLORS, h2b
+from xpra.codecs.checks import TEST_COLORS
 from xpra.client.gtk3.cairo_workaround import make_image_surface
 
 
+N = 20
 WIDTH, HEIGHT = 256, 256
 
 
@@ -28,26 +31,49 @@ class CairoColorsTest(unittest.TestCase):
 
     def test_compare(self):
         for color_name, hex_pixel in TEST_COLORS.items():
-            r, g, b = (int(hex_pixel[i * 2:(i + 1) * 2], 16) for i in range(3))
-            # try with our code:
-            pixels = h2b(hex_pixel) * WIDTH * HEIGHT
-            img = make_image_surface(cairo.FORMAT_ARGB32, "RGBA", pixels, WIDTH, HEIGHT, WIDTH*4)
-            pimg = surface_to_pil(img)
-            pixel = pimg.getpixel((WIDTH//2, HEIGHT//2))
-            if pixel != (r, g, b):
-                raise ValueError(f"expected {(r, g, b)} but got {pixel}")
+            # color components as integers:
+            r, g, b, a = (int(hex_pixel[i * 2:(i + 1) * 2], 16) for i in range(4))
+            # color components as bytes:
+            rb, gb, bb, ab = (struct.pack("B", pixel_int) for pixel_int in (r, g, b, a))
+            # map component to byte values:
+            pix_vals: dict[str, bytes] = {
+                "R": rb,
+                "G": gb,
+                "B": bb,
+                "A": ab,
+                "X": ab,
+            }
+            for format_name, cairo_format in {
+                "ARGB32": cairo.Format.ARGB32,
+                "RGB24": cairo.Format.RGB24,
+            }.items():
+                # try with our code:
+                for rgb_format in ("RGBA", "RGBX", "BGRA", "BGRX", "RGB", "BGR"):
+                    stride = WIDTH * len(rgb_format)
+                    pixels = (b"".join(pix_vals[component] for component in rgb_format)) * stride * HEIGHT
+                    start = monotonic()
+                    for _ in range(N):
+                        img = make_image_surface(cairo_format, rgb_format, pixels, WIDTH, HEIGHT, stride)
+                    end = monotonic()
+                    if color_name == "blue":
+                        mps = round(N*WIDTH*HEIGHT / (end-start) / 1024 / 1024)
+                        print(f"{rgb_format:<4} to cairo.{format_name: <6} : {mps:>5} MPixels/s")
+                    pimg = surface_to_pil(img)
+                    pixel = pimg.getpixel((WIDTH//2, HEIGHT//2))
+                    if pixel != (r, g, b):
+                        raise ValueError(f"expected {(r, g, b)} but got {pixel}")
 
-            # try painting the color using cairo
-            img = cairo.ImageSurface(cairo.Format.ARGB32, WIDTH, HEIGHT)
-            ctx = cairo.Context(img)
-            ctx.set_source_rgb(r/255, g/255, b/255)
-            ctx.rectangle(0, 0, WIDTH, HEIGHT)
-            ctx.fill()
-            # img.write_to_png(f"{color_name}-cairo.png")
-            pimg = surface_to_pil(img)
-            pixel = pimg.getpixel((WIDTH//2, HEIGHT//2))
-            if pixel != (r, g, b):
-                raise ValueError(f"expected {(r, g, b)} but got {pixel}")
+                # try painting the same color using cairo
+                img = cairo.ImageSurface(cairo_format, WIDTH, HEIGHT)
+                ctx = cairo.Context(img)
+                ctx.set_source_rgb(r/255, g/255, b/255)
+                ctx.rectangle(0, 0, WIDTH, HEIGHT)
+                ctx.fill()
+                # img.write_to_png(f"{color_name}-cairo.png")
+                pimg = surface_to_pil(img)
+                pixel = pimg.getpixel((WIDTH//2, HEIGHT//2))
+                if pixel != (r, g, b):
+                    raise ValueError(f"expected {(r, g, b)} but got {pixel}")
 
 
 def main():
