9991c1ce723c66448a0211448280b8edde94d4e2
Antoine Martin <antoine@xpra.org>
2024-07-04 20:12:21 +0700

    type hints and formatting

--- fs/bin/build_cuda_kernels.py
+++ fs/bin/build_cuda_kernels.py
@@ -15,7 +15,7 @@ from xpra.util.io import get_status_output
 from xpra.util.str_fn import sorted_nicely
 
 
-def get_nvcc_version(command):
+def get_nvcc_version(command: str) -> tuple[int, ...]:
     if not os.path.exists(command):
         return None
     code, out, _ = get_status_output([command, "--version"])
@@ -32,19 +32,19 @@ def get_nvcc_version(command):
     return tuple(int(x) for x in version.split("."))
 
 
-def get_nvcc():
+def get_nvcc() -> [str, tuple[int, ...]]:
     path_options = os.environ.get("PATH", "").split(os.path.pathsep)
     if WIN32:
         nvcc_exe = "nvcc.exe"
         CUDA_DIR = os.environ.get("CUDA_DIR", "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA")
-        path_options += ["./cuda/bin/"]+list(reversed(sorted_nicely(glob.glob(f"{CUDA_DIR}\\*\\bin"))))
+        path_options += ["./cuda/bin/"] + list(reversed(sorted_nicely(glob.glob(f"{CUDA_DIR}\\*\\bin"))))
     else:
         nvcc_exe = "nvcc"
         path_options += ["/usr/local/cuda/bin", "/opt/cuda/bin"]
         path_options += list(reversed(sorted_nicely(glob.glob("/usr/local/cuda*/bin"))))
         path_options += list(reversed(sorted_nicely(glob.glob("/opt/cuda*/bin"))))
     options = [os.path.join(x, nvcc_exe) for x in path_options]
-    #prefer the one we find on the $PATH, if any:
+    # prefer the one we find on the $PATH, if any:
     v = shutil.which(nvcc_exe)
     if v and (v not in options):
         options.insert(0, v)
@@ -56,9 +56,9 @@ def get_nvcc():
     if not nvcc_versions:
         print("unable to find nvcc")
         sys.exit(1)
-    #choose the most recent one:
+    # choose the most recent one:
     nvcc_version, nvcc = list(reversed(sorted(nvcc_versions.items())))[0]
-    if len(nvcc_versions)>1:
+    if len(nvcc_versions) > 1:
         print(f" using version {nvcc_version} from {nvcc}")
     return nvcc, nvcc_version
 
@@ -66,10 +66,10 @@ def get_nvcc():
 def get_gcc_version() -> tuple[int, ...]:
     CC = os.environ.get("CC", "gcc")
     if CC.find("clang") >= 0:
-        return (0, )
+        return (0,)
     exit_code, _, err = get_status_output([CC, "-v"])
     if exit_code != 0:
-        return (0, )
+        return (0,)
     V_LINE = "gcc version "
     gcc_version = []
     for line in err.splitlines():
@@ -100,14 +100,17 @@ def get_nvcc_args(nvcc: str, nvcc_version=(0, 0)) -> list[str]:
     ]
     if get_gcc_version() >= (14, 0):
         clangpp = shutil.which("clang++")
-        nvcc_cmd.append(f"-ccbin={clangpp}")
+        if not clangpp:
+            print("clang++ not found, compilation may fail")
+        else:
+            nvcc_cmd.append(f"-ccbin={clangpp}")
     return nvcc_cmd
 
 
-def main(args):
+def main(args) -> int:
     nvcc, nvcc_version = get_nvcc()
     nvcc_args = get_nvcc_args(nvcc, nvcc_version)
-    if len(args)==1:
+    if len(args) == 1:
         kernels = (
             "XRGB_to_NV12", "XRGB_to_YUV444", "BGRX_to_NV12", "BGRX_to_YUV444",
             "BGRX_to_RGB", "RGBX_to_RGB", "RGBA_to_RGBAP", "BGRA_to_RGBAP",
@@ -123,17 +126,18 @@ def main(args):
         kbuild_cmd = nvcc_args + ["-c", cuda_src, "-o", cuda_bin]
         print(f"* {kernel}")
         nvcc_commands.append(kbuild_cmd)
-    #parallel build:
+    # parallel build:
     nvcc_errors = []
 
-    def nvcc_compile(nvcc_cmd):
+    def nvcc_compile(nvcc_cmd: list[str]) -> None:
         c, stdout, stderr = get_status_output(nvcc_cmd)
-        if c!=0:
+        if c != 0:
             nvcc_errors.append(c)
             print(f"Error: failed to compile CUDA kernel {kernel}")
             print(f" using command: {nvcc_cmd}")
             print(stdout or "")
             print(stderr or "")
+
     nvcc_threads = []
     for cmd in nvcc_commands:
         from threading import Thread
