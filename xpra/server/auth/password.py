3c520276c813dbb3c5081b18277dfa023bc6070c
Antoine Martin <antoine@xpra.org>
2024-07-07 15:12:37 +0700

    simplify: options are always strings

--- xpra/server/auth/password.py
+++ xpra/server/auth/password.py
@@ -1,18 +1,17 @@
 # This file is part of Xpra.
-# Copyright (C) 2016-2023 Antoine Martin <antoine@xpra.org>
+# Copyright (C) 2016-2024 Antoine Martin <antoine@xpra.org>
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
 from collections.abc import Callable
 
-from xpra.util.str_fn import bytestostr
 from xpra.server.auth.sys_auth_base import SysAuthenticator
 
 
 class Authenticator(SysAuthenticator):
 
     def __init__(self, **kwargs):
-        self.value: str = bytestostr(kwargs.pop("value", None))
+        self.value: str = kwargs.pop("value", None)
         super().__init__(**kwargs)
         self.authenticate_check: Callable = self.authenticate_hmac
 
