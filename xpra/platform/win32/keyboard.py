06ea044d25ba3b0806155c2e156a2308661914bd
Antoine Martin <antoine@xpra.org>
2024-07-10 12:01:35 +0700

    ignore win32 layouts without a valid x11 layout name

--- xpra/platform/win32/keyboard.py
+++ xpra/platform/win32/keyboard.py
@@ -68,7 +68,7 @@ def x11_layouts_to_win32_hkl() -> dict[str, int]:
                     code, _, _, _, _layout, _variants = WIN32_LAYOUTS[kbid]
                     log("found keyboard layout '%s' / %#x with variants=%s, code '%s' for kbid=%#x",
                         _layout, kbid, _variants, code, hkli)
-                    if _layout not in layout_to_hkl:
+                    if _layout and _layout not in layout_to_hkl:
                         layout_to_hkl[_layout] = hkli
                         break
     except Exception:
@@ -175,6 +175,8 @@ class Keyboard(KeyboardBase):
         for win32_layout in WIN32_LAYOUTS.values():
             # ("ARA", "Saudi Arabia",   "Arabic",                   1356,   "ar", []),
             x11_layout = win32_layout[4]
+            if not x11_layout:
+                continue
             if x11_layout in x11_layouts:
                 continue
             name = win32_layout[2]
@@ -208,7 +210,7 @@ class Keyboard(KeyboardBase):
                         code, _, _, _, _layout, _variants = win32_layout
                         log("found keyboard layout '%s' / %#x with variants=%s, code '%s' for kbid=%#x",
                             _layout, kbid, _variants, code, hkl)
-                        if _layout not in layouts_defs:
+                        if _layout and _layout not in layouts_defs:
                             layouts_defs[_layout] = hkl
                             break
         except Exception as e:
