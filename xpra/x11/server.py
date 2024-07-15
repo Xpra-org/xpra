85547dac5f0d7fabd361db6d84964c27cba5b6bc
Antoine Martin <antoine@xpra.org>
2024-07-16 00:10:29 +0700

    always free images with an error context

--- xpra/x11/server.py
+++ xpra/x11/server.py
@@ -1228,7 +1228,8 @@ class XpraServer(GObject.GObject, X11ServerBase):
         Gdk.cairo_set_source_pixbuf(cr, pixbuf, wx+x, wy+y)
         cr.set_operator(operator)
         cr.paint()
-        image.free()
+        with xlog:
+            image.free()
 
     def repaint_root_overlay(self) -> None:
         if not self.root_overlay:
