fcbfabd8b98c7d9a7f096047b7ca6e61e87c24aa
Antoine Martin <antoine@xpra.org>
2024-07-18 16:55:13 +0700

    encoder speed is less important when target speed is low

--- xpra/server/window/video_scoring.py
+++ xpra/server/window/video_scoring.py
@@ -60,7 +60,7 @@ def get_speed_score(csc_format, csc_spec, encoder_spec, scaling, target_speed=10
     #the encoder speed matters less
     #when the target speed is low:
     ts = min(100, max(1, target_speed))
-    sscore = (50-ts//2) + speed*100//(100+ts)
+    sscore = (50 - ts // 2) + speed * 100 // (200 + ts)
     if csc_spec:
         #if there is a csc step,
         #then we lose some performance,
