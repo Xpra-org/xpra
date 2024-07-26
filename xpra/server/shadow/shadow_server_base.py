8e494c13f4787d20b441570cad1f87702bdea3d5
Antoine Martin <antoine@xpra.org>
2024-07-26 18:21:55 +0700

    #4307 delay import of notifications module
    which may be disabled

--- xpra/server/shadow/shadow_server_base.py
+++ xpra/server/shadow/shadow_server_base.py
@@ -10,7 +10,6 @@ from collections.abc import Sequence
 from xpra.os_util import gi_import
 from xpra.server.window import batch_config
 from xpra.server.shadow.root_window_model import RootWindowModel
-from xpra.notifications.common import parse_image_path
 from xpra.platform.gui import get_native_notifier_classes, get_wm_name
 from xpra.platform.paths import get_icon_dir
 from xpra.server import features
@@ -213,6 +212,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
             actions: list = []
             hints: dict[str, Any] = {}
             icon_filename = os.path.join(get_icon_dir(), "user.png")
+            from xpra.notifications.common import parse_image_path
             icon = parse_image_path(icon_filename)
             self.notifier.show_notify("", tray, nid, "Xpra", 0, "", title, body, actions, hints, 10 * 1000, icon)
 
@@ -231,6 +231,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
             actions = []
             hints = {}
             icon_filename = os.path.join(get_icon_dir(), "server-connected.png")
+            from xpra.notifications.common import parse_image_path
             icon = parse_image_path(icon_filename)
             self.notifier.show_notify("", tray, NotificationID.STARTUP, "Xpra", replaces_nid, "",
                                       title, body, actions, hints, 10 * 1000, icon)
