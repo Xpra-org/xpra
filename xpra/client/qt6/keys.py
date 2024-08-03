643169e0452fc9b4d9ec9400095dd9a54c8a357c
Antoine Martin <antoine@xpra.org>
2024-08-03 16:01:11 +0700

    #4314 forgot to add key mapping file

---
+++ xpra/client/qt6/keys.py
@@ -0,0 +1,13 @@
+# This file is part of Xpra.
+# Copyright (C) 2024 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from PyQt6.QtCore import Qt
+
+key_names = {}
+
+for attr in dir(Qt.Key):
+    if attr.startswith("Key_"):
+        key_enum = getattr(Qt.Key, attr)
+        key_names[key_enum] = attr[4:]
