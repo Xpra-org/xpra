675bf35b7f25ac3ec4d5ccd83c0920bba4c9d935
Antoine Martin <antoine@xpra.org>
2024-08-04 11:14:35 +0700

    log opengl probe command

--- xpra/server/mixins/display.py
+++ xpra/server/mixins/display.py
@@ -86,6 +86,7 @@ class DisplayManager(StubServerMixin):
             env = self.get_child_env()
             #we want the output so we can parse it:
             env["XPRA_REDIRECT_OUTPUT"] = "0"
+            gllog(f"query_opengl() using {cmd=}, {env=}")
             proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=env)
             out,err = proc.communicate()
             gllog("out(%s)=%s", cmd, out)
