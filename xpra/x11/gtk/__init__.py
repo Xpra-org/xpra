6e10dd488a30ae1cee0cf3e64d04213b7ca58851
Antoine Martin <antoine@xpra.org>
2024-08-05 15:48:04 +0700

    #4317 don't import Gtk / Gdk this early

--- xpra/x11/gtk/__init__.py
+++ xpra/x11/gtk/__init__.py
@@ -3,14 +3,10 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-from xpra.os_util import gi_import
 
-gi_import("Gdk", "3.0")
-gi_import("Gtk", "3.0")
-GdkX11 = gi_import("GdkX11", "3.0")
-
-
-def GDKX11Window(*args, **kwargs) -> GdkX11.X11Window:
+def GDKX11Window(*args, **kwargs):
+    from xpra.os_util import gi_import
+    GdkX11 = gi_import("GdkX11", "3.0")
     # pylint: disable=import-outside-toplevel
     from xpra.gtk.window import new_GDKWindow
     return new_GDKWindow(GdkX11.X11Window, *args, **kwargs)
