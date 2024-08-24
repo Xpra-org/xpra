d54e7781e4865ee94f28ab54a2995889df577225
Antoine Martin <antoine@xpra.org>
2024-08-24 21:20:26 +0700

    reformat

--- xpra/server/features.py
+++ xpra/server/features.py
@@ -3,24 +3,24 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-control         = True
-notifications   = True
-webcam          = True
-clipboard       = True
-audio           = True
-av_sync         = True
-fileprint       = True
-mmap            = True
-ssl             = True
-ssh             = True
-input_devices   = True
-commands        = True
-gstreamer       = True
-dbus            = True
-encoding        = True
-logging         = True
-network_state   = True
-shell           = False
-display         = True
-windows         = True
-rfb             = True
+control = True
+notifications = True
+webcam = True
+clipboard = True
+audio = True
+av_sync = True
+fileprint = True
+mmap = True
+ssl = True
+ssh = True
+input_devices = True
+commands = True
+gstreamer = True
+dbus = True
+encoding = True
+logging = True
+network_state = True
+shell = False
+display = True
+windows = True
+rfb = True
