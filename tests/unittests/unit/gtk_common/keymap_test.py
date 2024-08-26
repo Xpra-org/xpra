a8e07ff0b137a3454ee9672948319f3843b99ad3
Antoine Martin <antoine@xpra.org>
2024-08-26 23:04:53 +0700

    oops: update one more test for b10e9a1d8769e737d4acf649e0e02955ce46599c

--- tests/unittests/unit/gtk_common/keymap_test.py
+++ tests/unittests/unit/gtk_common/keymap_test.py
@@ -4,23 +4,20 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import os
 import unittest
 
-from xpra.os_util import POSIX
-from xpra.gtk.keymap import get_gtk_keymap, do_get_gtk_keymap
+from xpra.gtk.keymap import get_gtk_keymap
 
 
 class TestKeymap(unittest.TestCase):
 
     def test_get_gtk_keymap(self):
-        assert not do_get_gtk_keymap(None, ())
-        if not POSIX or os.environ.get("DISPLAY"):
-            assert get_gtk_keymap()
+        assert get_gtk_keymap()
 
 
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
