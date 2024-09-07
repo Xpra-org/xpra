309a7c60d6df86f94bf842d93224cabfca0e5fa0
Antoine Martin <antoine@xpra.org>
2024-09-07 21:54:23 +0700

    avoid 'Directory listing forbidden' error

--- xpra/net/http/http_handler.py
+++ xpra/net/http/http_handler.py
@@ -398,15 +398,15 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
                 if os.path.exists(index):
                     path = index
                     break
-            else:
+            if os.path.isdir(path):
                 if not self.directory_listing:
                     self.send_error(403, "Directory listing forbidden")
                     return None
-            code, headers, body = list_directory(path)
-            self.send_response(code)
-            self.extra_headers.update(headers)
-            self.end_headers()
-            return body
+                code, headers, body = list_directory(path)
+                self.send_response(code)
+                self.extra_headers.update(headers)
+                self.end_headers()
+                return body
         try:
             code, extra_headers, content = load_path(self.headers, path)
             lm = extra_headers.get("Last-Modified")
