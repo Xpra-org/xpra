903cdaf61071517e0d0e6b92fb3e88b03962b864
Antoine Martin <antoine@xpra.org>
2024-12-05 22:30:14 +0700

    gitignore excluded this file during refactoring

---
+++ xpra/auth/password.py
@@ -0,0 +1,22 @@
+# This file is part of Xpra.
+# Copyright (C) 2016-2024 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from collections.abc import Callable
+
+from xpra.auth.sys_auth_base import SysAuthenticator
+
+
+class Authenticator(SysAuthenticator):
+
+    def __init__(self, **kwargs):
+        self.value: str = kwargs.pop("value", "")
+        super().__init__(**kwargs)
+        self.authenticate_check: Callable = self.authenticate_hmac
+
+    def __repr__(self):
+        return "password"
+
+    def get_password(self) -> str:
+        return self.value
