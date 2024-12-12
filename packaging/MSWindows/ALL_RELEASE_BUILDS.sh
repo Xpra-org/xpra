1b6000100ef779d7be2b6ec3c05280164ea013ce
Antoine Martin <antoine@xpra.org>
2024-12-12 16:53:15 +0700

    cosmetic: add gap in output between light and std builds

--- packaging/MSWindows/ALL_RELEASE_BUILDS.sh
+++ packaging/MSWindows/ALL_RELEASE_BUILDS.sh
@@ -3,4 +3,7 @@
 MSWINDOWS_DIR=`dirname $(readlink -f $0)`
 
 ${MSWINDOWS_DIR}/BUILD.py --light
+echo
+echo "****************************************************************"
+echo
 ${MSWINDOWS_DIR}/BUILD.py --no-light
