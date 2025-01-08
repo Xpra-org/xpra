27286a3bfd1ce546878cfe6a5cf87e6ceb12ecb7
Antoine Martin <antoine@xpra.org>
2025-01-08 16:20:03 +0700

    fix 'exe' subcommand: missing argument

--- src/win32/BUILD.py
+++ src/win32/BUILD.py
@@ -1388,7 +1388,7 @@ def main(argv):
         elif arg == "zip":
             create_zip()
         elif arg == "exe":
-            create_installer()
+            create_installer(args)
         else:
             raise ValueError(f"unknown argument {arg!r}")
     else:
