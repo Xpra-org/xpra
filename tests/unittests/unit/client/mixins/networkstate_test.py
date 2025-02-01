379955da93ae999183218bd0434b8f2ba730fe65
Antoine Martin <antoine@xpra.org>
2025-02-01 13:08:01 +0700

    cosmetic: no tabs

--- tests/unittests/unit/client/mixins/networkstate_test.py
+++ tests/unittests/unit/client/mixins/networkstate_test.py
@@ -15,24 +15,25 @@ from unit.client.mixins.clientmixintest_util import ClientMixinTest
 
 class MixinsTest(ClientMixinTest):
 
-	def test_networkstate(self):
-		opts = AdHocStruct()
-		opts.pings = True
-		opts.bandwidth_limit = 0
-		opts.bandwidth_detection = True
-		self._test_mixin_class(NetworkState, opts, {"start_time" : time.time()})
-		x = self.mixin
-		if x.server_start_time<x.start_time:
-			raise Exception("invalid time: server=%s vs start=%s" % (x.server_start_time, x.start_time))
-		x.uuid = get_hex_uuid()
-		x.send_info_request()
-		packet = ["info-response", {"foo" : "bar"}]
-		self.handle_packet(packet)
-		assert x.server_last_info.get("foo")=="bar"
+    def test_networkstate(self):
+        opts = AdHocStruct()
+        opts.pings = True
+        opts.bandwidth_limit = 0
+        opts.bandwidth_detection = True
+        self._test_mixin_class(NetworkState, opts, {"start_time": time.time()})
+        x = self.mixin
+        if x.server_start_time < x.start_time:
+            raise Exception("invalid time: server=%s vs start=%s" % (x.server_start_time, x.start_time))
+        x.uuid = get_hex_uuid()
+        x.send_info_request()
+        packet = ["info-response", {"foo": "bar"}]
+        self.handle_packet(packet)
+        assert x.server_last_info.get("foo") == "bar"
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 
 if __name__ == '__main__':
-	main()
+    main()
