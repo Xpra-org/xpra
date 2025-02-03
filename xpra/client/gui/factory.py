8fbbff2908080eff6ce009a75b0df3a2ee2d2ee6
Antoine Martin <antoine@xpra.org>
2025-02-03 23:19:20 +0700

    move mixin loading to a factory

---
+++ xpra/client/gui/factory.py
@@ -0,0 +1,49 @@
+# This file is part of Xpra.
+# Copyright (C) 2010 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+
+def get_client_base_classes() -> tuple[type]:
+    from xpra.client.base.client import XpraClientBase
+    from xpra.client.gui import features
+
+    CLIENT_BASES: list[type] = [XpraClientBase]
+    if features.display:
+        from xpra.client.mixins.display import DisplayClient
+        CLIENT_BASES.append(DisplayClient)
+    if features.windows:
+        from xpra.client.mixins.windows import WindowClient
+        CLIENT_BASES.append(WindowClient)
+    if features.webcam:
+        from xpra.client.mixins.webcam import WebcamForwarder
+        CLIENT_BASES.append(WebcamForwarder)
+    if features.audio:
+        from xpra.client.mixins.audio import AudioClient
+        CLIENT_BASES.append(AudioClient)
+    if features.clipboard:
+        from xpra.client.mixins.clipboard import ClipboardClient
+        CLIENT_BASES.append(ClipboardClient)
+    if features.notifications:
+        from xpra.client.mixins.notification import NotificationClient
+        CLIENT_BASES.append(NotificationClient)
+    if features.mmap:
+        from xpra.client.mixins.mmap import MmapClient
+        CLIENT_BASES.append(MmapClient)
+    if features.logging:
+        from xpra.client.mixins.logging import RemoteLogging
+        CLIENT_BASES.append(RemoteLogging)
+    if features.network_state:
+        from xpra.client.mixins.network_state import NetworkState
+        CLIENT_BASES.append(NetworkState)
+    if features.network_listener:
+        from xpra.client.mixins.network_listener import NetworkListener
+        CLIENT_BASES.append(NetworkListener)
+    if features.encoding:
+        from xpra.client.mixins.encodings import Encodings
+        CLIENT_BASES.append(Encodings)
+    if features.tray:
+        from xpra.client.mixins.tray import TrayClient
+        CLIENT_BASES.append(TrayClient)
+
+    return tuple(CLIENT_BASES)
--- xpra/client/gui/ui_client_base.py
+++ xpra/client/gui/ui_client_base.py
@@ -9,6 +9,7 @@ import sys
 from typing import Any
 from collections.abc import Callable, Sequence
 
+from xpra.client.gui.factory import get_client_base_classes
 from xpra.client.base.client import XpraClientBase
 from xpra.client.gui.keyboard_helper import KeyboardHelper
 from xpra.platform import set_name
@@ -29,57 +30,7 @@ from xpra.exit_codes import ExitCode, ExitValue
 from xpra.client.gui import features
 from xpra.log import Logger
 
-CLIENT_BASES: list[type] = [XpraClientBase]
-if features.display:
-    from xpra.client.mixins.display import DisplayClient
-
-    CLIENT_BASES.append(DisplayClient)
-if features.windows:
-    from xpra.client.mixins.windows import WindowClient
-
-    CLIENT_BASES.append(WindowClient)
-if features.webcam:
-    from xpra.client.mixins.webcam import WebcamForwarder
-
-    CLIENT_BASES.append(WebcamForwarder)
-if features.audio:
-    from xpra.client.mixins.audio import AudioClient
-
-    CLIENT_BASES.append(AudioClient)
-if features.clipboard:
-    from xpra.client.mixins.clipboard import ClipboardClient
-
-    CLIENT_BASES.append(ClipboardClient)
-if features.notifications:
-    from xpra.client.mixins.notification import NotificationClient
-
-    CLIENT_BASES.append(NotificationClient)
-if features.mmap:
-    from xpra.client.mixins.mmap import MmapClient
-
-    CLIENT_BASES.append(MmapClient)
-if features.logging:
-    from xpra.client.mixins.logging import RemoteLogging
-
-    CLIENT_BASES.append(RemoteLogging)
-if features.network_state:
-    from xpra.client.mixins.network_state import NetworkState
-
-    CLIENT_BASES.append(NetworkState)
-if features.network_listener:
-    from xpra.client.mixins.network_listener import NetworkListener
-
-    CLIENT_BASES.append(NetworkListener)
-if features.encoding:
-    from xpra.client.mixins.encodings import Encodings
-
-    CLIENT_BASES.append(Encodings)
-if features.tray:
-    from xpra.client.mixins.tray import TrayClient
-
-    CLIENT_BASES.append(TrayClient)
-
-CLIENT_BASES: tuple[type, ...] = tuple(CLIENT_BASES)
+CLIENT_BASES = get_client_base_classes()
 ClientBaseClass = type('ClientBaseClass', CLIENT_BASES, {})
 
 GLib = gi_import("GLib")
