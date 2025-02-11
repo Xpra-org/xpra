4f7f93730ccdfd621531afe7d2dc8a148d8b917b
Antoine Martin <antoine@xpra.org>
2025-02-11 21:52:29 +0700

    #3582 hide unnecessary info / when desired

--- xpra/server/core.py
+++ xpra/server/core.py
@@ -2344,14 +2344,17 @@ class ServerCore(ControlHandler):
             "elapsed_time": int(now - self.start_time),
             "server_type": "core",
             "server.mode": self.get_server_mode(),
-            "hostname": socket.gethostname(),
         }
+        if FULL_INFO > 0:
+            capabilities["hostname"] = socket.gethostname()
         if source is None or "features" in source.wants:
             capabilities |= {
                 "readonly-server": True,
                 "readonly": self.readonly,
-                "server-log": os.environ.get("XPRA_SERVER_LOG", ""),
             }
+            server_log = os.environ.get("XPRA_SERVER_LOG", "")
+            if server_log:
+                capabilities["server-log"] = server_log
         if source and "packet-types" in source.wants:
             capabilities["packet-types"] = tuple(self._aliases.values())
         if source is None or "versions" in source.wants:
