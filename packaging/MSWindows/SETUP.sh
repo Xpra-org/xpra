dc216ad0e1ee6e0d7731fa6ab1abed61c64c65c1
Antoine Martin <antoine@xpra.org>
2025-02-14 20:18:00 +0700

    typo

--- packaging/MSWindows/SETUP.sh
+++ packaging/MSWindows/SETUP.sh
@@ -30,7 +30,7 @@ for x in cryptography cffi pycparser numpy pillow appdirs paramiko comtypes neti
 	$PACMAN ${XPKG}python-${x}
 done
 #not yet available for aarch64?:
-for x in cx_Freeze gssapi; do
+for x in cx-Freeze gssapi; do
 	$PACMAN ${XPKG}python-${x}
 done
 
