Xpra-org/xpra

commit 25b8dd09751fad71104254fed3bc38276b3c0c4a

remove compat shim for old evdi lib versions

xpra/codecs/evdi/capture.pyx
@@ -125,12 +125,11 @@ cdef extern from "evdi_lib.h":
     evdi_selectable evdi_get_event_ready(evdi_handle handle)
     void evdi_set_logging(evdi_logging evdi_logging)
 
-cdef extern from "evdi_compat.h":
-    void evdi_connect_compat(evdi_handle handle, const unsigned char *edid,
+    void evdi_connect(evdi_handle handle, const unsigned char *edid,
           const unsigned int edid_length,
           const uint32_t pixel_area_limit,
           const uint32_t pixel_per_second_limit)
-    void evdi_enable_cursor_events_compat(evdi_handle handle, bint enable)
+    void evdi_enable_cursor_events(evdi_handle handle, bint enable)
 
 
 
@@ -354,7 +353,7 @@ cdef class EvdiDevice:
         log("ddcci_data_handler(%#x)", ddcci_data.address)
 
     def enable_cursor_events(self, enable=True):
-        evdi_enable_cursor_events_compat(self.handle, int(enable))
+        evdi_enable_cursor_events(self.handle, int(enable))
 
 
     def connect(self, edid=DEFAULT_EDID):

xpra/codecs/evdi/evdi_compat.h
@@ -1,31 +1,0 @@
-/**
- * This file is part of Xpra.
- * Copyright (C) 2022 Antoine Martin <antoine@xpra.org>
- * Xpra is released under the terms of the GNU GPL v2, or, at your option, any
- * later version. See the file COPYING for details.
- */
-
-#include <stdint.h>
-#include "evdi_lib.h"
-
-void evdi_connect_compat(evdi_handle handle, const unsigned char *edid,
-          const unsigned int edid_length,
-          const uint32_t pixel_area_limit,
-          const uint32_t pixel_per_second_limit) {
-#if LIBEVDI_VERSION_MAJOR>1 || LIBEVDI_VERSION_MINOR>11
-	return evdi_connect(handle, edid, edid_length, pixel_area_limit, pixel_per_second_limit);
-#else
-	return evdi_connect(handle, edid, edid_length, pixel_per_second_limit);
-#endif
-}
-
-void evdi_enable_cursor_events_compat(evdi_handle handle, int enable) {
-#if LIBEVDI_VERSION_MAJOR>1 || LIBEVDI_VERSION_MINOR>9 || (LIBEVDI_VERSION_MINOR==9 && LIBEVDI_VERSION_PATCH>0)
-	evdi_enable_cursor_events(handle, enable);
-#else
-	//cannot be disabled with older API versions!
-	if (enable) {
-		evdi_enable_cursor_events(handle);
-	}
-#endif
-}

commit 9f9928d8dbbe948fb27ae75f8ee002b06880df48

was broken since 25b8dd09751fad71104254fed3bc38276b3c0c4a

xpra/codecs/evdi/capture.pyx
@@ -402,7 +402,7 @@ cdef class EvdiDevice:
         cdef unsigned int edid_length = len(self.edid)
         cdef uint32_t pixel_per_second_limit = pixel_area_limit*Hz
         log(f"connect with edid {edid!r} (length={edid_length})")
-        evdi_connect_compat(self.handle, edid_bin, <const unsigned int> edid_length,
+        evdi_connect(self.handle, edid_bin, <const unsigned int> edid_length,
                      <const uint32_t> pixel_area_limit,
                      <const uint32_t> pixel_per_second_limit)
 