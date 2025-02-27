2795ccb945ecf6b42e4edfc7f493ff28f2d64131
Antoine Martin <antoine@xpra.org>
2025-02-27 19:18:07 +0700

    file had gone MIA in 33d5ac73c88fb23e3a82fee1442dbad73db16c23

---
+++ xpra/client/base/features.py
@@ -0,0 +1,22 @@
+# This file is part of Xpra.
+# Copyright (C) 2018 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+display = True
+windows = True
+gstreamer = True
+x11 = True
+webcam = True
+audio = True
+clipboard = True
+notifications = True
+dbus = True
+mmap = True
+ssl = True
+ssh = True
+logging = True
+tray = True
+network_state = True
+network_listener = True
+encoding = True
