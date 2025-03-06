abe55f77c6b508fc81302bfdc7776c17faa7b73f
Antoine Martin <antoine@xpra.org>
2025-03-06 12:32:50 +0700

    #4524 honour ssl options in launcher files

--- xpra/client/gtk3/launcher.py
+++ xpra/client/gtk3/launcher.py
@@ -39,7 +39,7 @@ from xpra.scripts.main import (
     configure_network, configure_env, configure_logging,
 )
 from xpra.common import noop
-from xpra.scripts.parsing import is_local, get_ssh_args, parse_ssh_option, get_ssh_proxy_args
+from xpra.scripts.parsing import is_local, get_ssh_args, parse_ssh_option, get_ssh_proxy_args, get_ssl_options
 from xpra.exit_codes import RETRY_EXIT_CODES, ExitCode, ExitValue, exit_str
 from xpra.platform.info import get_username
 from xpra.log import Logger, enable_debug_for
@@ -806,6 +806,8 @@ class ApplicationWindow:
             params["display_name"] = f"{self.config.mode}://{self.config.host}:{self.config.port}"
             if self.config.mode in (MODE_WS, MODE_WSS):
                 params["path"] = self.config.path
+            # always populate ssl so we can auto-upgrade:
+            params["ssl-options"] = get_ssl_options(params, self.config, [])
             if self.config.mode in (MODE_SSL, MODE_WSS, MODE_QUIC) and self.nostrict_host_check.get_active():
                 params["strict-host-check"] = False
 
