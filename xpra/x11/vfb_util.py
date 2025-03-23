51b84d688d0321135378b1bbf2c87471f0d5ad03
Antoine Martin <antoine@xpra.org>
2025-03-23 21:50:08 +0700

    fail faster if xvfb process terminates

--- xpra/x11/vfb_util.py
+++ xpra/x11/vfb_util.py
@@ -330,7 +330,7 @@ def start_Xvfb(xvfb_str: str, vfb_geom, pixel_depth: int, display_name: str, cwd
                     raise InitException(f"xvfb command has terminated: {xvfb_cmd}")
                 # Read the display number from the pipe we gave to Xvfb
                 try:
-                    buf = read_displayfd(r_pipe)
+                    buf = read_displayfd(r_pipe, proc=xvfb)
                 except Exception as e:
                     buf = b""
                     log("read_displayfd(%s)", r_pipe, exc_info=True)
