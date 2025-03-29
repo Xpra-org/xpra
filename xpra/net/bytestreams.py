daf8892abb9f314cfa30eb549fe7488e1985d8f5
Antoine Martin <antoine@xpra.org>
2025-03-29 19:20:30 +0700

    missing module should not propagate

--- xpra/net/bytestreams.py
+++ xpra/net/bytestreams.py
@@ -443,7 +443,7 @@ class SocketConnection(Connection):
                 if iface and iface!="lo":
                     try:
                         from xpra.platform.netdev_query import get_interface_info
-                    except ImportError as e:
+                    except (ModuleNotFoundError, ImportError) as e:
                         log("do_get_socket_info() no netdev_query: %s", e)
                     else:
                         device_info.update(get_interface_info(fd, iface))
