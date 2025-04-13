b7bdb02287bd14395dc42cd77e6678c5be047e5d
Antoine Martin <antoine@xpra.org>
2025-04-13 17:31:40 +0700

    remove unused attributes, add stronger types

--- xpra/client/base/serverinfo.py
+++ xpra/client/base/serverinfo.py
@@ -60,25 +60,19 @@ class ServerInfoMixin(StubClientMixin):
     def __init__(self):  # pylint: disable=super-init-not-called
         super().__init__()
         self._remote_protocol = None
-        self._remote_machine_id = None
-        self._remote_uuid = None
-        self._remote_version = None
-        self._remote_revision = None
+        self._remote_machine_id = ""
+        self._remote_uuid = ""
+        self._remote_version = ""
         self._remote_branch = ""
         self._remote_modifications = 0
-        self._remote_commit = None
+        self._remote_hostname = ""
+        self._remote_display = ""
+        self._remote_platform = ""
+        self._remote_lib_versions = {}
         self._remote_build_date = ""
         self._remote_build_time = ""
-        self._remote_hostname = None
-        self._remote_display = None
-        self._remote_platform = None
-        self._remote_platform_release = None
-        self._remote_platform_platform = None
-        self._remote_platform_linux_distribution = None
-        self._remote_python_version = ""
-        self._remote_lib_versions = {}
         self._remote_subcommands: Sequence[str] = ()
-        self._remote_server_log = None
+        self._remote_server_log = ""
         self._remote_server_mode = ""
 
     def get_caps(self) -> dict[str, Any]:
@@ -98,36 +92,27 @@ class ServerInfoMixin(StubClientMixin):
         self._remote_machine_id = c.strget("machine_id")
         self._remote_uuid = c.strget("uuid")
         self._remote_version = parse_version(c.strget("build.version", c.strget("version")))
-        self._remote_revision = c.strget("build.revision", c.strget("revision"))
         mods = c.get("build.local_modifications")
         if mods and str(mods).find("dfsg") >= 0:  # pragma: no cover
             log.warn("Warning: the xpra server is running a buggy Debian version")
             log.warn(" those are usually out of date and unstable")
         else:
             self._remote_modifications = c.intget("build.local_modifications", 0)
-        self._remote_commit = c.strget("build.commit")
         self._remote_branch = c.strget("build.branch")
-        self._remote_build_date = c.strget("build.date")
-        self._remote_build_time = c.strget("build.time")
         self._remote_hostname = c.strget("hostname")
         self._remote_display = c.strget("display")
         self._remote_platform = c.strget("platform")
-        self._remote_platform_release = c.strget("platform.release")
-        self._remote_platform_platform = c.strget("platform.platform")
-        self._remote_python_version = c.strget("python.version")
+        # unused:
+        # remote_commit = c.strget("build.commit")
+        # remote_platform_platform = c.strget("platform.platform")
+        # remote_python_version = c.strget("python.version")
+        self._remote_build_date = c.strget("build.date")
+        self._remote_build_time = c.strget("build.time")
         self._remote_subcommands = c.strtupleget("subcommands")
         self._remote_server_log = c.strget("server-log")
         self._remote_server_mode = c.strget("server.mode", "server")
         self._remote_lib_versions = get_remote_lib_versions(c)
         # linux distribution is a tuple of different types, ie: ('Linux Fedora' , 20, 'Heisenbug')
-        pld = c.tupleget("platform.linux_distribution")
-        if pld and len(pld) == 3:
-            def san(v):
-                if isinstance(v, int):
-                    return v
-                return bytestostr(v)
-
-            self._remote_platform_linux_distribution = [san(x) for x in pld]
         verr = version_compat_check(self._remote_version)
         if verr is not None:
             vstr = ".".join(str(x) for x in (self._remote_version or ()))
@@ -138,11 +123,19 @@ class ServerInfoMixin(StubClientMixin):
         return True
 
     def print_server_info(self, c: typedict) -> None:
-        distro = c.strtupleget("platform.linux_distribution")
-        i = platform_name(self._remote_platform, distro or self._remote_platform_release)
+        distro = c.tupleget("platform.linux_distribution")
+        if distro and len(distro) == 3:
+            def san(v) -> int | str:
+                if isinstance(v, int):
+                    return v
+                return bytestostr(v)
+            distro = tuple(san(x) for x in distro)
+        platform_release = c.strget("platform.release")
+        remote_revision = c.strget("build.revision", c.strget("revision"))
+        i = platform_name(self._remote_platform, distro or platform_release)
         r = ".".join(str(x) for x in self._remote_version)
-        if self._remote_revision:
-            r += f"-r{self._remote_revision}"
+        if remote_revision:
+            r += f"-r{remote_revision}"
         mode = c.strget("server.mode", "server")
         bits = c.intget("python.bits", 0)
         bitsstr = "" if bits == 0 else f" {bits}-bit"
