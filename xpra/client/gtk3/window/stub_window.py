2f5a720456ff4fdec8cab5756bb3d408d6d64653
Antoine Martin <antoine@xpra.org>
2025-05-08 14:53:42 +0700

    add docstrings

--- xpra/client/gtk3/window/stub_window.py
+++ xpra/client/gtk3/window/stub_window.py
@@ -16,25 +16,28 @@ class StubWindow:
     __gsignals__: dict[str, Any] = {}
 
     def init_window(self, client, metadata: typedict, client_props: typedict) -> None:
-        pass
+        """ setup and initialize the window """
 
     def cleanup(self) -> None:
-        pass
+        """ free any resources """
 
     def get_window_event_mask(self) -> Gdk.EventMask:
         return 0
 
     def init_widget_events(self, widget) -> None:
-        pass
+        """ register events on the widget shown in the window """
 
     def set_icon(self, pixbuf: GdkPixbuf.Pixbuf) -> None:
-        pass
+        """ set the window icon to the given pixbuf """
 
     def get_map_client_properties(self) -> dict[str, Any]:
+        """ client properties to send to the server during a `map` event """
         return {}
 
     def get_configure_client_properties(self) -> dict[str, Any]:
+        """ client properties to send to the server during a `configure` event """
         return {}
 
     def get_info(self) -> dict[str, Any]:
+        """ information for this subsystem """
         return {}
