a79f99f171b6f08e19baf7b61dad48d93cbd4239
Antoine Martin <antoine@xpra.org>
2025-06-09 17:00:05 +0700

    add 'update' subcommand for xpra / apps containers
    saves having to regenerate the whole thing

--- packaging/containers/desktop.sh
+++ packaging/containers/desktop.sh
@@ -25,58 +25,63 @@ TARGET_GID="${TARGET_GID:-1000}"
 TIMEZONE="${TIMEZONE:-Europe/London}"
 # LANG="${LANG:-C}"
 
-buildah rm $CONTAINER || true
-buildah rmi -f $IMAGE_NAME || true
-buildah from --name $CONTAINER $DISTRO:$RELEASE
-buildah run $CONTAINER apt-get update
+if [ "$1" == "update" ]; then
+  buildah run $CONTAINER apt-get update
+  buildah run $CONTAINER apt-get dist-upgrade -y
+else
+  buildah rm $CONTAINER || true
+  buildah rmi -f $IMAGE_NAME || true
+  buildah from --name $CONTAINER $DISTRO:$RELEASE
+  buildah run $CONTAINER apt-get update
 
-buildah copy $CONTAINER "keyboard" "locale" /etc/default/
-buildah run $CONTAINER sh -c "echo $TIMEZONE > /etc/timezone;ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime"
+  buildah copy $CONTAINER "keyboard" "locale" /etc/default/
+  buildah run $CONTAINER sh -c "echo $TIMEZONE > /etc/timezone;ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime"
 
-# add xpra repo:
-buildah run $CONTAINER apt-get install adduser wget ca-certificates --no-install-recommends -y
-buildah run $CONTAINER wget -O "/usr/share/keyrings/xpra.asc" "https://xpra.org/xpra.asc"
-buildah run $CONTAINER wget -O "/etc/apt/sources.list.d/${REPO}.sources" "https://raw.githubusercontent.com/Xpra-org/xpra/master/packaging/repos/${RELEASE}/${REPO}.sources"
-buildah run $CONTAINER apt-get update
+  # add xpra repo:
+  buildah run $CONTAINER apt-get install adduser wget ca-certificates --no-install-recommends -y
+  buildah run $CONTAINER wget -O "/usr/share/keyrings/xpra.asc" "https://xpra.org/xpra.asc"
+  buildah run $CONTAINER wget -O "/etc/apt/sources.list.d/${REPO}.sources" "https://raw.githubusercontent.com/Xpra-org/xpra/master/packaging/repos/${RELEASE}/${REPO}.sources"
+  buildah run $CONTAINER apt-get update
 
-if [ "${FIREFOX}" == "1" ]; then
-  buildah copy $CONTAINER "mozilla-firefox" /etc/apt/preferences.d/
-  buildah run $CONTAINER apt install -y software-properties-common --no-install-recommends
-  buildah run $CONTAINER add-apt-repository -y ppa:mozillateam/ppa
-  buildah run $CONTAINER apt install -y firefox --no-install-recommends
+  if [ "${FIREFOX}" == "1" ]; then
+    buildah copy $CONTAINER "mozilla-firefox" /etc/apt/preferences.d/
+    buildah run $CONTAINER apt install -y software-properties-common --no-install-recommends
+    buildah run $CONTAINER add-apt-repository -y ppa:mozillateam/ppa
+    buildah run $CONTAINER apt install -y firefox --no-install-recommends
+  fi
+
+  if [ "${XPRA}" == "1" ]; then
+    buildah run $CONTAINER apt-get install -y xpra-server xpra-client-gtk3 xserver-xorg-video-dummy xpra-codecs xpra-audio-server xpra-codecs-extras xpra-x11 xpra-html5 --no-install-recommends
+  fi
+
+  if [ "${TOOLS}" == "1" ]; then
+    # add some applications:
+    buildah run $CONTAINER apt-get install xterm strace net-tools iputils-ping --no-install-recommends -y
+    # toys useful for testing video encoders, costs ~30MB:
+    buildah run $CONTAINER apt-get install mesa-utils --no-install-recommends -y
+    buildah run $CONTAINER sh -c "wget https://github.com/VirtualGL/virtualgl/releases/download/3.1.3/virtualgl_3.1.3_amd64.deb;apt-get install ./virtualgl_3.1.3_amd64.deb -y;rm virtualgl*.deb"
+    # xrandr, xdpyinfo etc, costs ~15MB:
+    buildah run $CONTAINER apt-get install x11-xserver-utils x11-utils vulkan-tools --no-install-recommends -y
+  fi
+
+  # install winbar as desktop environment last,
+  # so we can find the applications installed when creating the cache
+  buildah run $CONTAINER apt-get install pulseaudio pavucontrol --no-install-recommends -y
+  buildah run $CONTAINER apt-get install -y "${FILE_MANAGER}"
+  buildah run $CONTAINER apt-get install winbar --no-install-recommends -y
+  buildah run $CONTAINER winbar --create-cache
+  buildah run $CONTAINER apt-get install -y $APPS
+
+  # remove the default "ubuntu" user, and add our one:
+  buildah run $CONTAINER deluser --quiet "ubuntu"
+  buildah run $CONTAINER rm -fr /home/ubuntu
+  buildah run $CONTAINER groupadd -r -g "${TARGET_GID}" "${TARGET_USER}"
+  buildah run $CONTAINER adduser -uid "${TARGET_UID}" -gid "${TARGET_GID}" --disabled-password --comment "no-comment" --shell /bin/bash "${TARGET_USER}"
+  buildah run $CONTAINER usermod -aG "${TARGET_USER_GROUPS}" "${TARGET_USER}"
+  buildah run $CONTAINER sh -c "echo \"${TARGET_USER}:${TARGET_PASSWORD}\" | chpasswd"
+  buildah run $CONTAINER chown -R "${TARGET_UID}:${TARGET_GID}" "/home/${TARGET_USER}"
+  buildah run $CONTAINER sh -c "cd /home/${TARGET_USER};setpriv --reuid ${TARGET_UID} --regid ${TARGET_GID} --init-groups --reset-env mkdir -p .config/winbar Documents Downloads Music Pictures Videos Network"
 fi
-
-if [ "${XPRA}" == "1" ]; then
-  buildah run $CONTAINER apt-get install -y xpra-server xpra-client-gtk3 xserver-xorg-video-dummy xpra-codecs xpra-audio-server xpra-codecs-extras xpra-x11 xpra-html5 --no-install-recommends
-fi
-
-if [ "${TOOLS}" == "1" ]; then
-  # add some applications:
-  buildah run $CONTAINER apt-get install xterm strace net-tools iputils-ping --no-install-recommends -y
-  # toys useful for testing video encoders, costs ~30MB:
-  buildah run $CONTAINER apt-get install mesa-utils --no-install-recommends -y
-  buildah run $CONTAINER sh -c "wget https://github.com/VirtualGL/virtualgl/releases/download/3.1.3/virtualgl_3.1.3_amd64.deb;apt-get install ./virtualgl_3.1.3_amd64.deb -y;rm virtualgl*.deb"
-  # xrandr, xdpyinfo etc, costs ~15MB:
-  buildah run $CONTAINER apt-get install x11-xserver-utils x11-utils vulkan-tools --no-install-recommends -y
-fi
-
-# install winbar as desktop environment last,
-# so we can find the applications installed when creating the cache
-buildah run $CONTAINER apt-get install pulseaudio pavucontrol --no-install-recommends -y
-buildah run $CONTAINER apt-get install -y "${FILE_MANAGER}"
-buildah run $CONTAINER apt-get install winbar --no-install-recommends -y
-buildah run $CONTAINER winbar --create-cache
-buildah run $CONTAINER apt-get install -y $APPS
-
-# remove the default "ubuntu" user, and add our one:
-buildah run $CONTAINER deluser --quiet "ubuntu"
-buildah run $CONTAINER rm -fr /home/ubuntu
-buildah run $CONTAINER groupadd -r -g "${TARGET_GID}" "${TARGET_USER}"
-buildah run $CONTAINER adduser -uid "${TARGET_UID}" -gid "${TARGET_GID}" --disabled-password --comment "no-comment" --shell /bin/bash "${TARGET_USER}"
-buildah run $CONTAINER usermod -aG "${TARGET_USER_GROUPS}" "${TARGET_USER}"
-buildah run $CONTAINER sh -c "echo \"${TARGET_USER}:${TARGET_PASSWORD}\" | chpasswd"
-buildah run $CONTAINER chown -R "${TARGET_UID}:${TARGET_GID}" "/home/${TARGET_USER}"
-buildah run $CONTAINER sh -c "cd /home/${TARGET_USER};setpriv --reuid ${TARGET_UID} --regid ${TARGET_GID} --init-groups --reset-env mkdir -p .config/winbar Documents Downloads Music Pictures Videos Network"
 
 # configure winbar:
 buildah run $CONTAINER setpriv --reuid "${TARGET_UID}" --regid "${TARGET_GID}" --init-groups --reset-env winbar --create-cache
--- packaging/containers/xpra.sh
+++ packaging/containers/xpra.sh
@@ -23,36 +23,40 @@ TARGET_UID="${TARGET_UID:-1000}"
 TARGET_GID="${TARGET_GID:-1000}"
 DEBUG="${DEBUG:-none}"
 
-buildah rm $CONTAINER || true
-buildah rmi -f $IMAGE_NAME || true
-buildah from --name $CONTAINER $DISTRO:$RELEASE
-buildah run $CONTAINER dnf install -y https://download1.rpmfusion.org/free/${DISTRO}/rpmfusion-free-release-${RELEASE}.noarch.rpm
-buildah run $CONTAINER dnf update -y
-buildah run $CONTAINER dnf install -y wget --setopt=install_weak_deps=False
-buildah run $CONTAINER wget -O "/etc/yum.repos.d/${REPO}.repo" "https://raw.githubusercontent.com/Xpra-org/xpra/master/packaging/repos/Fedora/${REPO}.repo"
-buildah run $CONTAINER dnf install -y xpra-filesystem xpra-server xpra-x11 xpra-html5 python3-aioquic python3-pyxdg dbus-daemon dbus-x11 dbus-tools desktop-backgrounds-compat libjxl-utils python3-cups cups-filters cups-pdf --setopt=install_weak_deps=False
-# EL10: system-backgrounds system-logos
-if [ "${AUDIO}" == "1" ]; then
-  buildah run $CONTAINER dnf install -y xpra-audio-server
-fi
-if [ "${CODECS}" == "1" ]; then
-  buildah run $CONTAINER dnf install -y xpra-codecs
-fi
+if [ "$1" == "update" ]; then
+  buildah run $CONTAINER dnf update --refresh -y
+else
+  buildah rm $CONTAINER || true
+  buildah rmi -f $IMAGE_NAME || true
+  buildah from --name $CONTAINER $DISTRO:$RELEASE
+  buildah run $CONTAINER dnf install -y https://download1.rpmfusion.org/free/${DISTRO}/rpmfusion-free-release-${RELEASE}.noarch.rpm
+  buildah run $CONTAINER dnf update -y
+  buildah run $CONTAINER dnf install -y wget --setopt=install_weak_deps=False
+  buildah run $CONTAINER wget -O "/etc/yum.repos.d/${REPO}.repo" "https://raw.githubusercontent.com/Xpra-org/xpra/master/packaging/repos/Fedora/${REPO}.repo"
+  buildah run $CONTAINER dnf install -y xpra-filesystem xpra-server xpra-x11 xpra-html5 python3-aioquic python3-pyxdg dbus-daemon dbus-x11 dbus-tools desktop-backgrounds-compat libjxl-utils python3-cups cups-filters cups-pdf --setopt=install_weak_deps=False
+  # EL10: system-backgrounds system-logos
+  if [ "${AUDIO}" == "1" ]; then
+    buildah run $CONTAINER dnf install -y xpra-audio-server
+  fi
+  if [ "${CODECS}" == "1" ]; then
+    buildah run $CONTAINER dnf install -y xpra-codecs
+  fi
 
-if [ "${TOOLS}" == "1" ]; then
-  buildah run $CONTAINER dnf install -y strace xterm net-tools lsof xpra-client socat glxgears mesa-demos xdpyinfo VirtualGL pavucontrol --setopt=install_weak_deps=False
+  if [ "${TOOLS}" == "1" ]; then
+    buildah run $CONTAINER dnf install -y strace xterm net-tools lsof xpra-client socat glxgears mesa-demos xdpyinfo VirtualGL pavucontrol --setopt=install_weak_deps=False
+  fi
+
+  buildah run $CONTAINER groupadd -r -g "${TARGET_GID}" "${TARGET_USER}"
+  buildah run $CONTAINER adduser -u "${TARGET_UID}" -g "${TARGET_GID}" --shell /bin/bash "${TARGET_USER}"
+  buildah run $CONTAINER usermod -aG "${TARGET_USER_GROUPS}" "${TARGET_USER}"
+  buildah run $CONTAINER sh -c "echo \"${TARGET_USER}:${TARGET_PASSWORD}\" | chpasswd"
+
+  # dbus setup
+  buildah run $CONTAINER sh -c "mkdir -m 755 -p /var/lib/dbus;dbus-uuidgen > /var/lib/dbus/machine-id"
+  buildah copy $CONTAINER allow-all.conf /etc/dbus-1/system.d/
+  # just use the system-wide ssl certificate:
+  buildah run $CONTAINER sh -c "chmod 644 /etc/xpra/ssl/*.pem"
 fi
-
-buildah run $CONTAINER groupadd -r -g "${TARGET_GID}" "${TARGET_USER}"
-buildah run $CONTAINER adduser -u "${TARGET_UID}" -g "${TARGET_GID}" --shell /bin/bash "${TARGET_USER}"
-buildah run $CONTAINER usermod -aG "${TARGET_USER_GROUPS}" "${TARGET_USER}"
-buildah run $CONTAINER sh -c "echo \"${TARGET_USER}:${TARGET_PASSWORD}\" | chpasswd"
-
-# dbus setup
-buildah run $CONTAINER sh -c "mkdir -m 755 -p /var/lib/dbus;dbus-uuidgen > /var/lib/dbus/machine-id"
-buildah copy $CONTAINER allow-all.conf /etc/dbus-1/system.d/
-# just use the system-wide ssl certificate:
-buildah run $CONTAINER sh -c "chmod 644 /etc/xpra/ssl/*.pem"
 
 # save space:
 buildah run $CONTAINER rm -fr /var/cache/*dnf*
