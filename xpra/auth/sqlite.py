c24dd4b76d12ea1a7934cc4284791b1880b2f928
Antoine Martin <antoine@xpra.org>
2025-06-09 18:16:35 +0700

    Revert "make linter happier"
    This reverts commit bcaf451cf6c47dd7b7e15ebc41d00e5f5453f53d.

--- xpra/auth/sqlite.py
+++ xpra/auth/sqlite.py
@@ -75,9 +75,8 @@ class SqliteDatabaseUtil(DatabaseUtilBase):
         import sqlite3  # pylint: disable=import-outside-toplevel
         db = sqlite3.connect(self.uri)
         cursor = db.cursor()
-        args = [sql] + list(sqlargs)
-        log("%s.execute%s", cursor, args)
-        cursor.execute(args)
+        log("%s.execute%s", cursor, (sql, sqlargs))
+        cursor.execute(sql, *sqlargs)
         if cursor_cb:
             cursor_cb(cursor)
         db.commit()
