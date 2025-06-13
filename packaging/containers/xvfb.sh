2475a63c99c5f6c5d981e5d26fae19d8efaa1664
Antoine Martin <antoine@xpra.org>
2025-06-13 11:45:27 +0700

    simplify using utility functions

--- packaging/containers/xvfb.sh
+++ packaging/containers/xvfb.sh
@@ -17,52 +17,67 @@ TOOLS="${TOOLS:-0}"
 TARGET_USER="${TARGET_USER:-xvfb-user}"
 TARGET_UID="${TARGET_UID:-1000}"
 
+run () {
+  buildah run $CONTAINER "$@"
+}
+
+copy () {
+  buildah copy $CONTAINER "$@"
+}
+
+add () {
+  run apk add "$@"
+}
+
 buildah rm $CONTAINER || true
 buildah rmi -f $IMAGE_NAME || true
 buildah from --name $CONTAINER $DISTRO
-buildah run $CONTAINER apk update
-buildah run $CONTAINER apk add su-exec
+run apk update
+run apk add su-exec
 
-buildah run $CONTAINER adduser -D -H -u "${TARGET_UID}" "TARGET_USER"
+run adduser -D -H -u "${TARGET_UID}" "TARGET_USER"
 
 
 if [ "${XDUMMY}" == "1" ]; then
-  buildah run $CONTAINER apk add xf86-video-dummy xorg-server
+  add xf86-video-dummy xorg-server
 else
-  buildah run $CONTAINER apk add xvfb
+  add xvfb
 fi
 
 if [ "${OPENGL}" == "1" ]; then
-  buildah run $CONTAINER apk add mesa-gl mesa-dri-gallium
+  add mesa-gl mesa-dri-gallium
 fi
 
 if [ "${TOOLS}" == "1" ]; then
   # for debugging:
-  buildah run $CONTAINER apk add socat util-linux-misc ghostscript-fonts
-  buildah run $CONTAINER apk add xterm mesa-utils mesa-osmesa
+  add socat util-linux-misc ghostscript-fonts
+  add xterm mesa-utils mesa-osmesa
   # vgl is currently only available in the 'testing' repo:
-  buildah run $CONTAINER apk add virtualgl --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+  add virtualgl --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 fi
 
 if [ "${TRIM}" == "1" ]; then
   # trim down unused directories:
-  buildah run $CONTAINER rm -fr /media /mnt /opt /srv /usr/local /usr/share/apk /usr/share/aclocal /usr/share/man /usr/share/util-macros
-  buildah run $CONTAINER rm -fr /etc/apk /etc/crontabs /etc/logrotate.d /etc/network /etc/nsswitch.conf /etc/periodic /etc/profile* /etc/ssl* /etc/udhcpc /etc/opt
+  run rm -fr /media /mnt /opt /srv /usr/local /usr/share/apk /usr/share/aclocal /usr/share/man /usr/share/util-macros
+  run rm -fr /etc/apk /etc/crontabs /etc/logrotate.d /etc/network /etc/nsswitch.conf /etc/periodic /etc/profile* /etc/ssl* /etc/udhcpc /etc/opt
   # extra OpenGL drivers:
-  # buildah run $CONTAINER rm -fr /usr/share/util-macros /usr/lib/gallium-pipe/pipe_crocus.so /usr/lib/gallium-pipe/pipe_i915.so /usr/lib/gallium-pipe/pipe_iris.so /usr/lib/gallium-pipe/pipe_nouveau.so /usr/lib/gallium-pipe/pipe_r300.so /usr/lib/gallium-pipe/pipe_r600.so /usr/lib/gallium-pipe/pipe_radeonsi.so /usr/lib/gallium-pipe/pipe_vmwgfx.so
+  # run rm -fr /usr/share/util-macros /usr/lib/gallium-pipe/pipe_crocus.so /usr/lib/gallium-pipe/pipe_i915.so /usr/lib/gallium-pipe/pipe_iris.so /usr/lib/gallium-pipe/pipe_nouveau.so /usr/lib/gallium-pipe/pipe_r300.so /usr/lib/gallium-pipe/pipe_r600.so /usr/lib/gallium-pipe/pipe_radeonsi.so /usr/lib/gallium-pipe/pipe_vmwgfx.so
   # remove the ability to install more packages:
-  buildah run $CONTAINER rm -fr /lib/apk /var/*
+  run rm -fr /lib/apk /var/*
   # ideally:
-  # buildah run $CONTAINER apk remove busybox
+  # run apk remove busybox
 fi
 
 if [ "${XDUMMY}" == "1" ]; then
   rm -f xorg.conf
   wget https://raw.githubusercontent.com/Xpra-org/xpra/refs/heads/master/fs/etc/xpra/xorg.conf
-  buildah run $CONTAINER mkdir /etc/X11
-  buildah copy $CONTAINER xorg.conf /etc/X11
-  buildah config --entrypoint "su-exec ${TARGET_USER} /usr/bin/Xorg -novtswitch -logfile /tmp/Xorg.log -config /etc/X11/xorg.conf +extension Composite +extension GLX +extension RANDR +extension RENDER -extension DOUBLE-BUFFER -nolisten tcp -noreset -ac $XDISPLAY" $CONTAINER
+  run mkdir /etc/X11
+  copy xorg.conf /etc/X11
+  rm -f xorg.conf
+  XVFB_COMMAND="/usr/bin/Xorg -novtswitch -logfile /tmp/Xorg.log -config /etc/X11/xorg.conf +extension Composite +extension GLX +extension RANDR +extension RENDER -extension DOUBLE-BUFFER -nolisten tcp -noreset -ac $XDISPLAY"
 else
-  buildah config --entrypoint "su-exec ${TARGET_USER} /usr/bin/Xvfb -ac -noreset +extension GLX +extension Composite +extension RANDR +extension Render -extension DOUBLE-BUFFER -nolisten tcp -ac $XDISPLAY" $CONTAINER
+  XVFB_COMMAND="/usr/bin/Xvfb -ac -noreset +extension GLX +extension Composite +extension RANDR +extension Render -extension DOUBLE-BUFFER -nolisten tcp -ac $XDISPLAY"
 fi
+ENTRYPOINT="su-exec ${TARGET_USER} ${XVFB_COMMAND}"
+buildah config --entrypoint "${ENTRYPOINT}" $CONTAINER
 buildah commit $CONTAINER $IMAGE_NAME
