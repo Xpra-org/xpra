6b671e7c9aed5e53bdbaf8610887a39820340eca
Antoine Martin <antoine@xpra.org>
2025-06-24 23:36:24 +0700

    #4465 test data causes gstreamer unit test failures

--- xpra/codecs/checks.py
+++ xpra/codecs/checks.py
@@ -203,12 +203,6 @@ TEST_COMPRESSED_DATA: dict[str, dict[str, dict[tuple[int, int], TEST_DATA]]] = {
     },
     "av1": {
         "YUV420P": {
-            (320, 240): (
-                unhex("12000a0b200000043cffbf83fff304321e135f580100000064da5eaa0fbffffffffe7101ff919be2abfac8dbed66a0"),
-            ),
-            (1920, 1080): (
-                unhex("8c018a0101102308040000000400000079780000000ad70000215f9000005eabbfc3fe0010cc808080c2633010000000008001050101041040400040080120182013d0d1d9ce8863558243b3fb18c22065eca93b2adc73a4f26a67b3a849de98f4acbf45384979de56eaf34381b2d0c099d9c78faa2a25ed316ee6bb4c28637ce4816d949ae7bb00c071976bc280"),
-            ),
             (64, 64): (
                 unhex("12000a0b00000002affff036be40103219110100010000004b17c531ecb5321932af9b2fab54ee58012c"),
             ),
