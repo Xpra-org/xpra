f081def020b4eb981d5d5c047088e69b17abd319
Antoine Martin <antoine@xpra.org>
2025-06-30 16:17:51 +0700

    split connect function

--- xpra/scripts/main.py
+++ xpra/scripts/main.py
@@ -62,6 +62,7 @@ from xpra.scripts.config import (
     make_defaults_struct, str_to_bool, parse_bool_or, has_audio_support, name_to_field,
     xvfb_command,
 )
+from xpra.net.bytestreams import SOCKET_TIMEOUT, VSOCK_TIMEOUT, SocketConnection
 from xpra.net.common import (
     DEFAULT_PORTS, SOCKET_TYPES, AUTO_ABSTRACT_SOCKET, ABSTRACT_SOCKET_PREFIX,
     verify_hyperv_available,
@@ -1286,192 +1287,218 @@ def host_target_string(dtype: str, username: str, host: str, port: int, display:
     return target
 
 
-def connect_to(display_desc: dict[str, Any], opts, debug_cb=noop, ssh_fail_cb=noop):
-    from xpra.net.bytestreams import SOCKET_TIMEOUT, VSOCK_TIMEOUT, SocketConnection
+def connect_to_ssh(display_desc: dict[str, Any], opts, debug_cb=noop, ssh_fail_cb=noop):
+    if display_desc.get("is_paramiko", False):
+        from xpra.net.ssh import paramiko_client
+        conn = paramiko_client.connect_to(display_desc)
+    else:
+        from xpra.net.ssh import exec_client
+        conn = exec_client.connect_to(display_desc, opts, debug_cb, ssh_fail_cb)
+    dtype = display_desc["type"]
+    if dtype == "vnc+ssh":
+        conn.socktype = "vnc"
+        conn.socktype_wrapped = "ssh"
+    return conn
+
+
+def connect_to_namedpipe(display_desc: dict[str, Any]):
+    pipe_name = display_desc["named-pipe"]
+    if not WIN32:
+        raise InitException("named pipes are only supported on MS Windows")
+    import errno
+    from xpra.platform.win32.dotxpra import PIPE_PATH, PIPE_ROOT
+    from xpra.platform.win32.namedpipes.connection import NamedPipeConnection, connect_to_namedpipe
+    if pipe_name.startswith(PIPE_ROOT):
+        # absolute pipe path already specified
+        path = pipe_name
+    else:
+        path = PIPE_PATH + pipe_name
+    try:
+        pipe_handle = connect_to_namedpipe(path)
+    except Exception as e:
+        try:
+            if e.args[0] == errno.ENOENT:
+                raise InitException(f"the named pipe {pipe_name!r} does not exist: {e}") from None
+        except AttributeError:
+            pass
+        raise InitException(f"failed to connect to the named pipe {pipe_name!r}:\n {e}") from None
+    timeout = display_desc.get("timeout", SOCKET_TIMEOUT)
+    conn = NamedPipeConnection(pipe_name, pipe_handle, {})
+    conn.timeout = timeout
+    conn.target = f"namedpipe://{pipe_name}/"
+    return conn
+
+
+def connect_to_vsock(display_desc: dict[str, Any]):
+    cid, iport = display_desc["vsock"]
+    from xpra.net.vsock.vsock import (  # pylint: disable=no-name-in-module
+        connect_vsocket,
+        CID_TYPES, CID_ANY, PORT_ANY,
+    )
+    sock = connect_vsocket(cid=cid, port=iport)
+    sock.settimeout(None)
+    conn = SocketConnection(sock, "local", "host", (CID_TYPES.get(cid, cid), iport), "vsock")
+    conn.timeout = VSOCK_TIMEOUT
+    conn.target = "vsock://%s:%s" % (
+        "any" if cid == CID_ANY else cid,
+        "any" if iport == PORT_ANY else iport,
+    )
+    return conn
+
+
+def connect_to_hyperv(display_desc: dict[str, Any]):
+    vmid, service = display_desc["hyperv"]
+    verify_hyperv_available()
+    sock = socket.socket(socket.AF_HYPERV, socket.SOCK_STREAM, socket.HV_PROTOCOL_RAW)
+    sock.connect((vmid, service))
+    sock.settimeout(None)
+    conn = SocketConnection(sock, "local", "host", (vmid, service), "hyperv")
+    conn.timeout = VSOCK_TIMEOUT
+    conn.target = f"hyperv://{vmid}.{service}"
+    return conn
+
+
+def connect_to_quic(display_desc: dict[str, Any], opts):
+    host = display_desc["host"]
+    port = display_desc["port"]
+    path = "/" + display_desc.get("display", "")
+    ssl_options = display_desc.get("ssl-options", {})
+    ssl_server_verify_mode = ssl_options.get("server-verify-mode", opts.ssl_server_verify_mode)
+    ssl_ca_certs = ssl_options.get("ca-certs", opts.ssl_ca_certs)
+    ssl_cert = ssl_options.get("cert", opts.ssl_cert)
+    ssl_key = ssl_options.get("key", opts.ssl_key)
+    ssl_key_password = ssl_options.get("key-password", opts.ssl_key_password)
+    ssl_server_name = ssl_options.get("server-hostname")
+    try:
+        from xpra.net.quic.client import quic_connect, FAST_OPEN
+        import aioquic
+        assert aioquic
+    except ImportError as e:
+        raise InitExit(ExitCode.SOCKET_CREATION_ERROR, f"cannot use quic sockets: {e}") from None
+    fast_open = display_desc.get("fast-open", FAST_OPEN)
+    conn = quic_connect(host, port, path, fast_open,
+                        ssl_cert, ssl_key, ssl_key_password,
+                        ssl_ca_certs, ssl_server_verify_mode, ssl_server_name)
+    return conn
+
+
+def connect_to_tcp(display_desc: dict[str, Any]):
+    dtype = display_desc["type"]
     display_name = display_desc["display_name"]
     timeout = display_desc.get("timeout", SOCKET_TIMEOUT)
-    dtype = display_desc["type"]
-    if dtype in ("ssh", "vnc+ssh"):
-        if display_desc.get("is_paramiko", False):
-            from xpra.net.ssh import paramiko_client
-            conn = paramiko_client.connect_to(display_desc)
+    sock = retry_socket_connect(display_desc)
+    # use non-blocking until the connection is finalized
+    sock.settimeout(0.1)
+    conn = SocketConnection(sock, sock.getsockname(), sock.getpeername(), display_name,
+                            dtype, socket_options=display_desc)
+
+    if dtype in ("ssl", "wss"):
+        raw_sock = sock
+        from xpra.net.ssl_util import ssl_handshake, ssl_wrap_socket
+        # convert option names to function arguments:
+        ssl_options = {k.replace("-", "_"): v for k, v in display_desc.get("ssl-options", {}).items()}
+        try:
+            sock = ssl_wrap_socket(sock, **ssl_options)
+        except ValueError as e:
+            raise InitExit(ExitCode.SSL_FAILURE, f"ssl setup failed: {e}")
+        if not sock:
+            raise RuntimeError(f"failed to wrap socket {raw_sock} as {dtype!r}")
+        ssl_handshake(sock)
+        conn._socket = sock
+        conn.timeout = timeout
+
+    # wrap in a websocket:
+    if dtype in ("ws", "wss"):
+        host = display_desc["host"]
+        port = display_desc.get("port", 0)
+        # do the websocket upgrade and switch to binary
+        try:
+            from xpra.net.websockets.common import client_upgrade
+        except ImportError as e:  # pragma: no cover
+            raise InitExit(ExitCode.UNSUPPORTED, f"cannot handle websocket connection: {e}") from None
         else:
-            from xpra.net.ssh import exec_client
-            conn = exec_client.connect_to(display_desc, opts, debug_cb, ssh_fail_cb)
-        if dtype == "vnc+ssh":
-            conn.socktype = "vnc"
-            conn.socktype_wrapped = "ssh"
-        return conn
+            display_path = display_desc_to_display_path(display_desc)
+            client_upgrade(conn.read, conn.write, host, port, display_path)
+    conn.target = get_host_target_string(display_desc)
+    return conn
 
-    if dtype == "socket":
-        if not hasattr(socket, "AF_UNIX"):  # pragma: no cover
-            raise InitExit(ExitCode.UNSUPPORTED, "unix domain sockets are not available on this operating system")
 
-        def sockpathfail_cb(msg) -> NoReturn:
-            raise InitException(msg)
+def connect_to_socket(display_desc: dict[str, Any]):
+    if not hasattr(socket, "AF_UNIX"):  # pragma: no cover
+        raise InitExit(ExitCode.UNSUPPORTED, "unix domain sockets are not available on this operating system")
 
-        sockpath = ""
-        sock = None
-        if display_name and not display_desc.get("socket_path") and AUTO_ABSTRACT_SOCKET:
-            # see if we can just connect to the abstract socket if one exists:
-            from xpra.platform.dotxpra import strip_display_prefix
-            sockpath = "@" + ABSTRACT_SOCKET_PREFIX + strip_display_prefix(display_name)
-            actual_path = "\0" + sockpath[1:] if sockpath.startswith("@") else sockpath
-            sock = socket.socket(socket.AF_UNIX)
-            sock.settimeout(1.0)
+    def sockpathfail_cb(msg) -> NoReturn:
+        raise InitException(msg)
+
+    sockpath = ""
+    sock = None
+    timeout = display_desc.get("timeout", SOCKET_TIMEOUT)
+    display_name = display_desc["display_name"]
+    if display_name and not display_desc.get("socket_path") and AUTO_ABSTRACT_SOCKET:
+        # see if we can just connect to the abstract socket if one exists:
+        from xpra.platform.dotxpra import strip_display_prefix
+        sockpath = "@" + ABSTRACT_SOCKET_PREFIX + strip_display_prefix(display_name)
+        actual_path = "\0" + sockpath[1:] if sockpath.startswith("@") else sockpath
+        sock = socket.socket(socket.AF_UNIX)
+        sock.settimeout(1.0)
+        try:
+            sock.connect(actual_path)
+        except OSError:
+            get_logger().debug(f"failed to connect to abstract socket {sockpath}")
+            noerr(sock.close)
+            sock = None
+    if not sock:
+        sockpath = get_sockpath(display_desc, sockpathfail_cb)
+        display_desc["socket_path"] = sockpath
+        actual_path = "\0" + ABSTRACT_SOCKET_PREFIX + sockpath[1:] if sockpath.startswith("@") else sockpath
+        sock = socket.socket(socket.AF_UNIX)
+        sock.settimeout(timeout)
+        start = monotonic()
+        while monotonic() - start < timeout:
             try:
                 sock.connect(actual_path)
-            except OSError:
-                get_logger().debug(f"failed to connect to abstract socket {sockpath}")
+                break
+            except ConnectionRefusedError as e:
+                elapsed = monotonic() - start
+                get_logger().debug("%s, retrying %i < %i", e, elapsed, timeout)
+                time.sleep(0.5)
+                continue
+            except Exception as e:
+                get_logger().debug(f"failed to connect using {sock.connect}({sockpath})", exc_info=True)
                 noerr(sock.close)
-                sock = None
-        if not sock:
-            sockpath = get_sockpath(display_desc, sockpathfail_cb)
-            display_desc["socket_path"] = sockpath
-            actual_path = "\0" + ABSTRACT_SOCKET_PREFIX + sockpath[1:] if sockpath.startswith("@") else sockpath
-            sock = socket.socket(socket.AF_UNIX)
-            sock.settimeout(timeout)
-            start = monotonic()
-            while monotonic() - start < timeout:
-                try:
-                    sock.connect(actual_path)
-                    break
-                except ConnectionRefusedError as e:
-                    elapsed = monotonic() - start
-                    get_logger().debug("%s, retrying %i < %i", e, elapsed, timeout)
-                    time.sleep(0.5)
-                    continue
-                except Exception as e:
-                    get_logger().debug(f"failed to connect using {sock.connect}({sockpath})", exc_info=True)
-                    noerr(sock.close)
-                    raise InitExit(ExitCode.CONNECTION_FAILED, f"failed to connect to {sockpath!r}:\n {e}") from None
-        try:
-            sock.settimeout(None)
-            conn = SocketConnection(sock, sock.getsockname(), sock.getpeername(), display_name, dtype)
-            conn.timeout = timeout
-            target = "socket://"
-            username = display_desc.get("username")
-            if username:
-                target += f"{username}@"
-            target += sockpath
-            conn.target = target
-            return conn
-        except OSError:
-            noerr(sock.close)
-            raise
-
-    if dtype == "named-pipe":  # pragma: no cover
-        pipe_name = display_desc["named-pipe"]
-        if not WIN32:
-            raise InitException("named pipes are only supported on MS Windows")
-        import errno
-        from xpra.platform.win32.dotxpra import PIPE_PATH, PIPE_ROOT
-        from xpra.platform.win32.namedpipes.connection import NamedPipeConnection, connect_to_namedpipe
-        if pipe_name.startswith(PIPE_ROOT):
-            # absolute pipe path already specified
-            path = pipe_name
-        else:
-            path = PIPE_PATH + pipe_name
-        try:
-            pipe_handle = connect_to_namedpipe(path)
-        except Exception as e:
-            try:
-                if e.args[0] == errno.ENOENT:
-                    raise InitException(f"the named pipe {pipe_name!r} does not exist: {e}") from None
-            except AttributeError:
-                pass
-            raise InitException(f"failed to connect to the named pipe {pipe_name!r}:\n {e}") from None
-        conn = NamedPipeConnection(pipe_name, pipe_handle, {})
+                raise InitExit(ExitCode.CONNECTION_FAILED, f"failed to connect to {sockpath!r}:\n {e}") from None
+    try:
+        sock.settimeout(None)
+        conn = SocketConnection(sock, sock.getsockname(), sock.getpeername(), display_name, "socket")
         conn.timeout = timeout
-        conn.target = f"namedpipe://{pipe_name}/"
+        target = "socket://"
+        username = display_desc.get("username")
+        if username:
+            target += f"{username}@"
+        target += sockpath
+        conn.target = target
         return conn
+    except OSError:
+        noerr(sock.close)
+        raise
 
+
+def connect_to(display_desc: dict[str, Any], opts, debug_cb=noop, ssh_fail_cb=noop):
+    dtype = display_desc["type"]
+    if dtype in ("ssh", "vnc+ssh"):
+        return connect_to_ssh(display_desc, opts, debug_cb, ssh_fail_cb)
+    if dtype == "socket":
+        return connect_to_socket(display_desc)
+    if dtype == "named-pipe":  # pragma: no cover
+        return connect_to_namedpipe(display_desc)
     if dtype == "vsock":
-        cid, iport = display_desc["vsock"]
-        from xpra.net.vsock.vsock import (  # pylint: disable=no-name-in-module
-            connect_vsocket,
-            CID_TYPES, CID_ANY, PORT_ANY,
-        )
-        sock = connect_vsocket(cid=cid, port=iport)
-        sock.settimeout(None)
-        conn = SocketConnection(sock, "local", "host", (CID_TYPES.get(cid, cid), iport), dtype)
-        conn.timeout = VSOCK_TIMEOUT
-        conn.target = "vsock://%s:%s" % (
-            "any" if cid == CID_ANY else cid,
-            "any" if iport == PORT_ANY else iport,
-        )
-        return conn
-
+        return connect_to_vsock(display_desc)
     if dtype == "hyperv":
-        vmid, service = display_desc["hyperv"]
-        verify_hyperv_available()
-        sock = socket.socket(socket.AF_HYPERV, socket.SOCK_STREAM, socket.HV_PROTOCOL_RAW)
-        sock.connect((vmid, service))
-        sock.settimeout(None)
-        conn = SocketConnection(sock, "local", "host", (vmid, service), dtype)
-        conn.timeout = VSOCK_TIMEOUT
-        conn.target = f"hyperv://{vmid}.{service}"
-        return conn
-
+        return connect_to_hyperv(display_desc)
     if dtype == "quic":
-        host = display_desc["host"]
-        port = display_desc["port"]
-        path = "/" + display_desc.get("display", "")
-        ssl_options = display_desc.get("ssl-options", {})
-        ssl_server_verify_mode = ssl_options.get("server-verify-mode", opts.ssl_server_verify_mode)
-        ssl_ca_certs = ssl_options.get("ca-certs", opts.ssl_ca_certs)
-        ssl_cert = ssl_options.get("cert", opts.ssl_cert)
-        ssl_key = ssl_options.get("key", opts.ssl_key)
-        ssl_key_password = ssl_options.get("key-password", opts.ssl_key_password)
-        ssl_server_name = ssl_options.get("server-hostname")
-        try:
-            from xpra.net.quic.client import quic_connect, FAST_OPEN
-            import aioquic
-            assert aioquic
-        except ImportError as e:
-            raise InitExit(ExitCode.SOCKET_CREATION_ERROR, f"cannot use quic sockets: {e}") from None
-        fast_open = display_desc.get("fast-open", FAST_OPEN)
-        conn = quic_connect(host, port, path, fast_open,
-                            ssl_cert, ssl_key, ssl_key_password,
-                            ssl_ca_certs, ssl_server_verify_mode, ssl_server_name)
-        return conn
-
+        return connect_to_quic(display_desc, opts)
     if dtype in ("tcp", "ssl", "ws", "wss", "vnc"):
-        sock = retry_socket_connect(display_desc)
-        # use non-blocking until the connection is finalized
-        sock.settimeout(0.1)
-        conn = SocketConnection(sock, sock.getsockname(), sock.getpeername(), display_name,
-                                dtype, socket_options=display_desc)
-
-        if dtype in ("ssl", "wss"):
-            raw_sock = sock
-            from xpra.net.ssl_util import ssl_handshake, ssl_wrap_socket
-            # convert option names to function arguments:
-            ssl_options = {k.replace("-", "_"): v for k, v in display_desc.get("ssl-options", {}).items()}
-            try:
-                sock = ssl_wrap_socket(sock, **ssl_options)
-            except ValueError as e:
-                raise InitExit(ExitCode.SSL_FAILURE, f"ssl setup failed: {e}")
-            if not sock:
-                raise RuntimeError(f"failed to wrap socket {raw_sock} as {dtype!r}")
-            ssl_handshake(sock)
-            conn._socket = sock
-            conn.timeout = timeout
-
-        # wrap in a websocket:
-        if dtype in ("ws", "wss"):
-            host = display_desc["host"]
-            port = display_desc.get("port", 0)
-            # do the websocket upgrade and switch to binary
-            try:
-                from xpra.net.websockets.common import client_upgrade
-            except ImportError as e:  # pragma: no cover
-                raise InitExit(ExitCode.UNSUPPORTED, f"cannot handle websocket connection: {e}") from None
-            else:
-                display_path = display_desc_to_display_path(display_desc)
-                client_upgrade(conn.read, conn.write, host, port, display_path)
-        conn.target = get_host_target_string(display_desc)
-        return conn
+        return connect_to_tcp(display_desc)
     raise InitException(f"unsupported display type: {dtype}")
 
 
