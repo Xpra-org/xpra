Xpra-org/xpra

commit 88bc3f353ce002f083514a3c22e59e17b5c82e1b

clean_session_files already calls rm_session_dir

xpra/server/subsystem/sessionfiles.py
@@ -5,7 +5,7 @@
 
 from typing import Any
 
-from xpra.scripts.session import rm_session_dir, clean_session_files
+from xpra.scripts.session import clean_session_files
 from xpra.server.subsystem.stub import StubServerMixin
 from xpra.log import Logger
 
@@ -21,7 +21,6 @@ class SessionFilesServer(StubServerMixin):
         if stop:
             log("clean_session_files(%s)", self.session_files)
             clean_session_files(*self.session_files)
-            rm_session_dir()
 
     def get_info(self, _proto) -> dict[str, Any]:
         return {

commit ab44a5d13ebb31ad5bc83a846053b2b8f500c9e5

call rm_session_dir explicitly when needed
this fixes breakage introduced in 88bc3f353ce002f083514a3c22e59e17b5c82e1b

xpra/scripts/session.py
@@ -217,7 +217,6 @@ def clean_session_files(*filenames: str) -> None:
                 clean_session_path(p)
         else:
             clean_session_path(path)
-    rm_session_dir()
 
 
 def clean_session_path(path: str) -> None:

xpra/server/subsystem/sessionfiles.py
@@ -5,7 +5,7 @@
 
 from typing import Any
 
-from xpra.scripts.session import clean_session_files
+from xpra.scripts.session import clean_session_files, rm_session_dir
 from xpra.server.subsystem.stub import StubServerMixin
 from xpra.log import Logger
 
@@ -21,6 +21,8 @@ class SessionFilesServer(StubServerMixin):
         if stop:
             log("clean_session_files(%s)", self.session_files)
             clean_session_files(*self.session_files)
+            if stop:
+                rm_session_dir()
 
     def get_info(self, _proto) -> dict[str, Any]:
         return {