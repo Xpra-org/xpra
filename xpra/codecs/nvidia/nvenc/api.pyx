06db6756859b6163de25b6cd84bf47a14a3d5341
Antoine Martin <antoine@xpra.org>
2025-07-09 11:53:35 +0700

    python definitions must live in the pyx

--- xpra/codecs/nvidia/nvenc/api.pyx
+++ xpra/codecs/nvidia/nvenc/api.pyx
@@ -72,6 +72,60 @@ cdef str presetstr(GUID preset):
     return CODEC_PRESETS_GUIDS.get(s, s)
 
 
+NV_ENC_STATUS_TXT: Dict[NVENCSTATUS, str] = {
+    NV_ENC_SUCCESS : "This indicates that API call returned with no errors.",
+    NV_ENC_ERR_NO_ENCODE_DEVICE       : "This indicates that no encode capable devices were detected",
+    NV_ENC_ERR_UNSUPPORTED_DEVICE     : "This indicates that devices pass by the client is not supported.",
+    NV_ENC_ERR_INVALID_ENCODERDEVICE  : "This indicates that the encoder device supplied by the client is not valid.",
+    NV_ENC_ERR_INVALID_DEVICE         : "This indicates that device passed to the API call is invalid.",
+    NV_ENC_ERR_DEVICE_NOT_EXIST       : """This indicates that device passed to the API call is no longer available and
+ needs to be reinitialized. The clients need to destroy the current encoder
+ session by freeing the allocated input output buffers and destroying the device
+ and create a new encoding session.""",
+    NV_ENC_ERR_INVALID_PTR            : "This indicates that one or more of the pointers passed to the API call is invalid.",
+    NV_ENC_ERR_INVALID_EVENT          : "This indicates that completion event passed in ::NvEncEncodePicture() call is invalid.",
+    NV_ENC_ERR_INVALID_PARAM          : "This indicates that one or more of the parameter passed to the API call is invalid.",
+    NV_ENC_ERR_INVALID_CALL           : "This indicates that an API call was made in wrong sequence/order.",
+    NV_ENC_ERR_OUT_OF_MEMORY          : "This indicates that the API call failed because it was unable to allocate enough memory to perform the requested operation.",
+    NV_ENC_ERR_ENCODER_NOT_INITIALIZED: """This indicates that the encoder has not been initialized with
+::NvEncInitializeEncoder() or that initialization has failed.
+The client cannot allocate input or output buffers or do any encoding
+related operation before successfully initializing the encoder.""",
+    NV_ENC_ERR_UNSUPPORTED_PARAM      : "This indicates that an unsupported parameter was passed by the client.",
+    NV_ENC_ERR_LOCK_BUSY              : """This indicates that the ::NvEncLockBitstream() failed to lock the output
+buffer. This happens when the client makes a non blocking lock call to
+access the output bitstream by passing NV_ENC_LOCK_BITSTREAM::doNotWait flag.
+This is not a fatal error and client should retry the same operation after
+few milliseconds.""",
+    NV_ENC_ERR_NOT_ENOUGH_BUFFER      : "This indicates that the size of the user buffer passed by the client is insufficient for the requested operation.",
+    NV_ENC_ERR_INVALID_VERSION        : "This indicates that an invalid struct version was used by the client.",
+    NV_ENC_ERR_MAP_FAILED             : "This indicates that ::NvEncMapInputResource() API failed to map the client provided input resource.",
+    NV_ENC_ERR_NEED_MORE_INPUT        : """
+This indicates encode driver requires more input buffers to produce an output
+bitstream. If this error is returned from ::NvEncEncodePicture() API, this
+is not a fatal error. If the client is encoding with B frames then,
+::NvEncEncodePicture() API might be buffering the input frame for re-ordering.
+A client operating in synchronous mode cannot call ::NvEncLockBitstream()
+API on the output bitstream buffer if ::NvEncEncodePicture() returned the
+::NV_ENC_ERR_NEED_MORE_INPUT error code.
+The client must continue providing input frames until encode driver returns
+::NV_ENC_SUCCESS. After receiving ::NV_ENC_SUCCESS status the client can call
+::NvEncLockBitstream() API on the output buffers in the same order in which
+it has called ::NvEncEncodePicture().
+""",
+    NV_ENC_ERR_ENCODER_BUSY : """This indicates that the HW encoder is busy encoding and is unable to encode
+the input. The client should call ::NvEncEncodePicture() again after few milliseconds.""",
+    NV_ENC_ERR_EVENT_NOT_REGISTERD : """This indicates that the completion event passed in ::NvEncEncodePicture()
+API has not been registered with encoder driver using ::NvEncRegisterAsyncEvent().""",
+    NV_ENC_ERR_GENERIC : "This indicates that an unknown internal error has occurred.",
+    NV_ENC_ERR_INCOMPATIBLE_CLIENT_KEY  : "This indicates that the client is attempting to use a feature that is not available for the license type for the current system.",
+    NV_ENC_ERR_UNIMPLEMENTED : "This indicates that the client is attempting to use a feature that is not implemented for the current version.",
+    NV_ENC_ERR_RESOURCE_REGISTER_FAILED : "This indicates that the ::NvEncRegisterResource API failed to register the resource.",
+    NV_ENC_ERR_RESOURCE_NOT_REGISTERED : "This indicates that the client is attempting to unregister a resource that has not been successfully registered.",
+    NV_ENC_ERR_RESOURCE_NOT_MAPPED : "This indicates that the client is attempting to unmap a resource that has not been successfully mapped.",
+}
+
+
 cdef str nvencStatusInfo(NVENCSTATUS ret):
     return NV_ENC_STATUS_TXT.get(ret, str(ret))
 
