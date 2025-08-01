5c7d0faf8ba0f5b749be5e01e472771bfcab359a
Antoine Martin <antoine@xpra.org>
2025-08-01 23:12:08 +0700

    derp: typo in fallback code path

--- xpra/platform/posix/gui.py
+++ xpra/platform/posix/gui.py
@@ -255,7 +255,7 @@ def get_display_size() -> tuple[int, int]:
         with xsync:
             return X11WindowBindings().get_root_size()
     Gdk = gi_import("Gdk")
-    screen = Gdk.Screen.get_default
+    screen = Gdk.Screen.get_default()
     if not screen:
         raise RuntimeError("unable to access the screen via Gdk")
     return screen.get_width(), screen.get_height()
