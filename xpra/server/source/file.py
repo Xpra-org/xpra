fc6f72d10ca1b587135f59717c9805c8fee4b8fc
Antoine Martin <antoine@xpra.org>
2025-08-24 02:04:23 +0700

    emit method belongs on the module instance, not individual source subsystem classes

--- xpra/server/source/file.py
+++ xpra/server/source/file.py
@@ -15,6 +15,10 @@ log = Logger("file")
 
 class FileConnection(FileTransferHandler, StubClientConnection):
 
+    def __init__(self):
+        FileTransferHandler.__init__(self)
+        StubClientConnection.__init__(self)
+
     @classmethod
     def is_needed(cls, caps: typedict) -> bool:
         return bool("file" in caps or caps.boolget("file-transfer"))
