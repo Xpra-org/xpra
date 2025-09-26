764c08c6a800faca4c1c8c3e3eeb0583da2aa793
Antoine Martin <antoine@xpra.org>
2025-09-26 22:40:15 +0700

    the file transfer dialog is useful
    even if none of the transfers are set to require the user to first accept the operation

--- xpra/client/gtk3/gtk_tray_menu_base.py
+++ xpra/client/gtk3/gtk_tray_menu_base.py
@@ -1380,14 +1380,6 @@ class GTKTrayMenuBase(MenuHelper):
         self.transfers = self.menuitem("Transfers", "transfer.png",
                                        "Files and URLs forwarding",
                                        self.client.show_ask_data_dialog)
-        def enable_transfers(*args):
-            log("enable_transfers%s ask=%s", args, ())
-            has_ask = (self.client.remote_file_transfer_ask or
-                       self.client.remote_printing_ask or
-                       self.client.remote_open_files_ask or
-                       self.client.remote_open_url_ask)
-            set_sensitive(self.transfers, has_ask)
-        self.after_handshake(enable_transfers)
         return self.transfers
 
     def make_uploadmenuitem(self):
