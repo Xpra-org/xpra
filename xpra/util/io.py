fb90edd53a41b80bdc5eb9667689cf0be7b14970
Antoine Martin <antoine@xpra.org>
2025-10-01 19:02:22 +0700

    fall through to other checks

--- xpra/util/io.py
+++ xpra/util/io.py
@@ -99,8 +99,8 @@ def wait_for_socket(sockpath: str, timeout=1) -> bool:
 
 def is_writable(path: str, uid: int, gid: int) -> bool:
     from xpra.os_util import getuid
-    if uid == getuid():
-        return os.access(path, os.W_OK)
+    if uid == getuid() and os.access(path, os.W_OK):
+        return True
     try:
         s = os.stat(path)
     except OSError as e:
