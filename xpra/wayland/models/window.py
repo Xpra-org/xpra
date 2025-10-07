00b04fa003a319f6523c1ce50bdcebfc8b98c913
Antoine Martin <antoine@xpra.org>
2025-10-07 15:52:51 +0700

    add window model

---
+++ xpra/wayland/models/window.py
@@ -0,0 +1,144 @@
+# This file is part of Xpra.
+# Copyright (C) 2025 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from socket import gethostname
+
+from xpra.util.gobject import one_arg_signal
+from xpra.codecs.image import ImageWrapper
+from xpra.server.window.model import WindowModelStub
+from xpra.os_util import gi_import
+from xpra.log import Logger
+
+log = Logger("wayland", "window")
+
+GObject = gi_import("GObject")
+
+
+class Window(WindowModelStub):
+    __gproperties__ = {
+        "geometry": (
+            GObject.TYPE_PYOBJECT,
+            "current coordinates (x, y, w, h, border) for the window", "",
+            GObject.ParamFlags.READABLE,
+        ),
+        # bits per pixel
+        "depth": (
+            GObject.TYPE_INT,
+            "window bit depth", "",
+            -1, 64, -1,
+            GObject.ParamFlags.READABLE,
+        ),
+        # if the window depth is 32 bit
+        "has-alpha": (
+            GObject.TYPE_BOOLEAN,
+            "Does the window use transparency", "",
+            False,
+            GObject.ParamFlags.READABLE,
+        ),
+        # from WM_CLIENT_MACHINE
+        "client-machine": (
+            GObject.TYPE_PYOBJECT,
+            "Host where client process is running", "",
+            GObject.ParamFlags.READABLE,
+        ),
+        # from XResGetClientPid
+        "pid": (
+            GObject.TYPE_INT,
+            "PID of owning process", "",
+            -1, 65535, -1,
+            GObject.ParamFlags.READABLE,
+        ),
+        # from _NET_WM_NAME or WM_NAME
+        "title": (
+            GObject.TYPE_PYOBJECT,
+            "Window title", "",
+            GObject.ParamFlags.READABLE,
+        ),
+        # from WM_WINDOW_ROLE
+        "role": (
+            GObject.TYPE_PYOBJECT,
+            "The window's role (ICCCM session management)", "",
+            GObject.ParamFlags.READABLE,
+        ),
+        # from WM_COMMAND
+        "command": (
+            GObject.TYPE_PYOBJECT,
+            "Command used to start or restart the client", "",
+            GObject.ParamFlags.READABLE,
+        ),
+    }
+
+    __common_signals__ = {
+        # signals we emit:
+        "unmanaged": one_arg_signal,
+        "initiate-moveresize": one_arg_signal,
+        "grab": one_arg_signal,
+        "ungrab": one_arg_signal,
+        "bell": one_arg_signal,
+        "client-contents-changed": one_arg_signal,
+        "motion": one_arg_signal,
+    }
+
+    # things that we expose:
+    _property_names = [
+        "depth", "has-alpha",
+        "client-machine", "pid",
+        "title", "role",
+        "command",
+    ]
+    # exposed and changing (should be watched for notify signals):
+    _dynamic_property_names = [
+        "title", "command",
+    ]
+    # should not be exported to the clients:
+    _internal_property_names = []
+    _MODELTYPE = "Wayland"
+
+    def __init__(self):
+        super().__init__()
+        self._internal_set_property("client-machine", gethostname())
+
+    def __repr__(self) -> str:  # pylint: disable=arguments-differ
+        return "WaylandWindow"
+
+    def setup(self) -> None:
+        self._managed = True
+
+    def unmanage(self, exiting=False) -> None:
+        if self._managed:
+            self.emit("unmanaged", exiting)
+
+    def do_unmanaged(self, wm_exiting: bool) -> None:
+        if not self._managed:
+            return
+        self._managed = False
+        self.managed_disconnect()
+
+    def acknowledge_changes(self) -> None:
+        if not self._managed:
+            return
+
+    def get_image(self, x: int, y: int, width: int, height: int) -> ImageWrapper | None:
+        return None
+
+    def get_dimensions(self) -> tuple[int, int]:
+        # just extracts the size from the geometry:
+        return 800, 600
+
+    def get_geometry(self) -> tuple[int, int, int, int]:
+        return 0, 0, 800, 600
+
+    ################################
+    # Actions
+    ################################
+
+    def raise_window(self) -> None:
+        pass
+
+    def set_active(self) -> None:
+        pass
+
+    def request_close(self) -> bool:
+        pass
