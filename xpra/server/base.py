4a9abba2c4aad2649b3544aaf83aa8e480641089
Antoine Martin <antoine@xpra.org>
2025-10-10 21:18:03 +0700

    also honour XPRA_CLIENT_CAN_SHUTDOWN with exit requests

--- xpra/server/base.py
+++ xpra/server/base.py
@@ -143,6 +143,9 @@ class ServerBase(ServerBaseClass):
     ######################################################################
     # shutdown / exit commands:
     def _process_exit_server(self, _proto, packet: PacketType = ("exit-server", )) -> None:
+        if not self.client_shutdown:
+            log.warn("Warning: ignoring exit request")
+            return
         reason: ConnectionMessage | str = ConnectionMessage.SERVER_EXIT
         message = "Exiting in response to client request"
         if len(packet) > 1:
