2c7a6360dd69006849a39810a552319d391f3de8
Antoine Martin <antoine@xpra.org>
2025-11-04 23:13:05 +0700

    #4698 don't bundle GStreamer video codecs to avoid crashes

--- setup.py
+++ setup.py
@@ -328,7 +328,7 @@ cuda_rebuild_ENABLED    = None if (nvidia_ENABLED and not WIN32) else False
 csc_libyuv_ENABLED      = DEFAULT and pkg_config_exists("libyuv")
 gstreamer_ENABLED       = DEFAULT
 gstreamer_audio_ENABLED = gstreamer_ENABLED
-gstreamer_video_ENABLED = gstreamer_ENABLED
+gstreamer_video_ENABLED = gstreamer_ENABLED and not OSX
 example_ENABLED         = DEFAULT
 win32_tools_ENABLED     = WIN32 and DEFAULT
 
