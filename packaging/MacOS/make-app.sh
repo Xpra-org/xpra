b20396b8e005fbe560f00ba592fd16211929da98
Antoine Martin <antoine@xpra.org>
2025-11-05 17:08:28 +0700

    remove duplicated entries

--- packaging/MacOS/make-app.sh
+++ packaging/MacOS/make-app.sh
@@ -386,7 +386,7 @@ GST_PLUGIN_DIR="./gstreamer-1.0"
 if [ "$STRIP_GSTREAMER_PLUGINS" == "1" ]; then
 	KEEP="./gstreamer-1.0.keep"
 	mkdir ${KEEP}
-	PLUGINS="app audio coreelements cutter removesilence faac faad flac oss osxaudio speex volume vorbis wav lame opus ogg gdp isomp4 matroska videoconvert x264 vpx"
+	PLUGINS="app audio coreelements cutter removesilence faac faad flac oss osxaudio speex volume vorbis wav lame opus ogg gdp isomp4 matroska"
 	#video sink for testing:
 	PLUGINS="${PLUGINS} autodetect osxvideo"
 	#video support:
