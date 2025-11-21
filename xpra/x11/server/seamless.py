Xpra-org/xpra

commit 17b20c9c6e1ecef1a84eb5d71116017cfa2e883b

move force ungrab to x11 display module

xpra/x11/subsystem/display.py
@@ -15,6 +15,7 @@ from xpra.x11.error import xlog, xsync, XError
 from xpra.exit_codes import ExitCode
 from xpra.util.system import is_X11
 from xpra.scripts.config import FALSE_OPTIONS, InitExit
+from xpra.net.common import Packet
 from xpra.common import (
     get_refresh_rate_for_value, parse_env_resolutions, parse_resolutions,
     MAX_WINDOW_SIZE, NotificationID,
@@ -27,8 +28,18 @@ from xpra.log import Logger
 GLib = gi_import("GLib")
 
 log = Logger("screen")
+grablog = Logger("server", "grab")
 
 DUMMY_WIDTH_HEIGHT_MM = envbool("XPRA_DUMMY_WIDTH_HEIGHT_MM", True)
+
+
+def x11_ungrab() -> None:
+    grablog("X11_ungrab")
+    with xsync:
+        from xpra.x11.bindings.core import X11CoreBindings
+        core = X11CoreBindings()
+        core.UngrabKeyboard()
+        core.UngrabPointer()
 
 
 def check_xvfb(xvfb: Popen | None, timeout=0) -> bool:
@@ -114,6 +125,7 @@ class X11DisplayManager(DisplayManager):
             caps |= {
                 "resize_screen": self.randr,
                 "resize_exact": self.randr_exact_size,
+                "force_ungrab": True,
             }
             if self.randr:
                 sizes = self.get_all_screen_sizes()
@@ -538,3 +550,16 @@ class X11DisplayManager(DisplayManager):
 
     def set_dpi(self, xdpi: int, ydpi: int) -> None:
         """ overridden in the seamless server """
+
+    ################################################################
+    # force-ungrab:
+
+    def _process_force_ungrab(self, proto, _packet: Packet) -> None:
+        # ignore the window id: wid = packet[1]
+        grablog("force ungrab from %s", proto)
+        x11_ungrab()
+
+    # noinspection PyMethodMayBeStatic
+    def init_packet_handlers(self) -> None:
+        super().init_packet_handlers()
+        self.add_packets("force-ungrab", main_thread=True)

xpra/pointer/nopointer.py
@@ -17,11 +17,15 @@ class NoPointerDevice:
 
     @staticmethod
     def move_pointer(x: int, y: int, props: dict) -> None:
-        log("xtest_fake_motion%s", (x, y, props))
+        log("npointer.move_pointer%s", (x, y, props))
 
     @staticmethod
     def click(button: int, pressed: bool, props: dict) -> None:
-        log("xtest_fake_button(%i, %s, %s)", button, pressed, props)
+        log("npointer.click(%i, %s, %s)", button, pressed, props)
+
+    @staticmethod
+    def wheel_motion(button: int, distance: float) -> None:
+        raise NotImplementedError()
 
     @staticmethod
     def has_precise_wheel() -> bool:

xpra/server/subsystem/pointer.py
@@ -281,6 +281,30 @@ class PointerServer(StubServerMixin):
         if self.process_mouse_common(proto, device_id, wid, pdata, props):
             self._update_modifiers(proto, wid, modifiers)
 
+    def _process_wheel_motion(self, proto, packet: Packet) -> None:
+        assert self.pointer_device.has_precise_wheel()
+        ss = self.get_server_source(proto)
+        if not ss:
+            return
+        wid = packet.get_wid()
+        button = packet.get_u8(2)
+        distance = packet.get_i64(3)
+        pointer = packet.get_ints(4)
+        modifiers = packet.get_strs(5)
+        # _buttons = packet[6]
+        device_id = -1
+        props = {}
+        self.record_wheel_event(wid, button)
+        if self.do_process_mouse_common(proto, device_id, wid, pointer, props):
+            self.last_mouse_user = ss.uuid
+            self._update_modifiers(proto, wid, modifiers)
+            self.pointer_device.wheel_motion(button, distance / 1000.0)  # pylint: disable=no-member
+
+    def record_wheel_event(self, wid: int, button: int) -> None:
+        log("recording scroll event for button %i", button)
+        for ss in self.window_sources():
+            ss.record_scroll_event(wid)
+
     ######################################################################
     # input devices:
     def _process_input_devices(self, _proto, packet: Packet) -> None:
@@ -341,6 +365,7 @@ class PointerServer(StubServerMixin):
             # mouse:
             "pointer-button", "pointer",
             "pointer-position",  # pre v5
+            "wheel-motion",
             # setup:
             "input-devices",
             main_thread=True

xpra/x11/server/core.py
@@ -9,12 +9,11 @@ import threading
 from typing import Any
 
 from xpra.os_util import gi_import
-from xpra.x11.bindings.core import set_context_check, X11CoreBindings
+from xpra.x11.bindings.core import set_context_check
 from xpra.x11.error import XError, xswallow, xsync, xlog, verify_sync
 from xpra.common import noerr
 from xpra.util.objects import typedict
 from xpra.util.env import envbool
-from xpra.net.common import Packet
 from xpra.server.base import ServerBase
 from xpra.log import Logger
 
@@ -25,7 +24,6 @@ set_context_check(verify_sync)
 log = Logger("x11", "server")
 keylog = Logger("x11", "server", "keyboard")
 pointerlog = Logger("x11", "server", "pointer")
-grablog = Logger("server", "grab")
 
 ALWAYS_NOTIFY_MOTION = envbool("XPRA_ALWAYS_NOTIFY_MOTION", False)
 
@@ -46,10 +44,6 @@ class X11ServerCore(ServerBase):
     def save_server_mode(self) -> None:
         from xpra.x11.xroot_props import root_set
         root_set("XPRA_SERVER_MODE", "latin1", self.session_type)
-
-    def init_packet_handlers(self) -> None:
-        super().init_packet_handlers()
-        self.add_packets("force-ungrab", "wheel-motion", main_thread=True)
 
     def do_cleanup(self) -> None:
         # prop_del does its own xsync:
@@ -82,10 +76,6 @@ class X11ServerCore(ServerBase):
     def make_hello(self, source) -> dict[str, Any]:
         capabilities = super().make_hello(source)
         capabilities["server_type"] = "Python/x11"
-        if "features" in source.wants:
-            capabilities |= {
-                "force_ungrab": True,
-            }
         return capabilities
 
     def get_ui_info(self, proto, wids=None, *args) -> dict[str, Any]:
@@ -142,19 +132,6 @@ class X11ServerCore(ServerBase):
             server_source.set_keymap(self.keyboard_config, self.keys_pressed, force, translate_only)
             self.keyboard_config = server_source.keyboard_config
 
-    def _process_force_ungrab(self, proto, _packet: Packet) -> None:
-        # ignore the window id: wid = packet[1]
-        grablog("force ungrab from %s", proto)
-        self.X11_ungrab()
-
-    # noinspection PyMethodMayBeStatic
-    def X11_ungrab(self) -> None:
-        grablog("X11_ungrab")
-        with xsync:
-            core = X11CoreBindings()
-            core.UngrabKeyboard()
-            core.UngrabPointer()
-
     def _motion_signaled(self, model, event) -> None:
         pointerlog("motion_signaled(%s, %s) last mouse user=%s", model, event, self.last_mouse_user)
         # find the window model for this gdk window:
@@ -165,26 +142,6 @@ class X11ServerCore(ServerBase):
             if ALWAYS_NOTIFY_MOTION or self.last_mouse_user is None or self.last_mouse_user != ss.uuid:
                 if hasattr(ss, "update_mouse"):
                     ss.update_mouse(wid, event.x_root, event.y_root, event.x, event.y)
-
-    def _process_wheel_motion(self, proto, packet: Packet) -> None:
-        assert self.pointer_device.has_precise_wheel()
-        ss = self.get_server_source(proto)
-        if not ss:
-            return
-        wid = packet.get_wid()
-        button = packet.get_u8(2)
-        distance = packet.get_i64(3)
-        pointer = packet.get_ints(4)
-        modifiers = packet.get_strs(5)
-        # _buttons = packet[6]
-        device_id = -1
-        props = {}
-        self.record_wheel_event(wid, button)
-        with xsync:
-            if self.do_process_mouse_common(proto, device_id, wid, pointer, props):
-                self.last_mouse_user = ss.uuid
-                self._update_modifiers(proto, wid, modifiers)
-                self.pointer_device.wheel_motion(button, distance / 1000.0)  # pylint: disable=no-member
 
     def get_pointer_device(self, deviceid: int):
         # pointerlog("get_pointer_device(%i) input_devices_data=%s", deviceid, self.input_devices_data)
@@ -269,8 +226,3 @@ class X11ServerCore(ServerBase):
         except XError:
             pointerlog("button_action%s", (device_id, wid, pointer, button, pressed, props), exc_info=True)
             pointerlog.error("Error: failed (un)press mouse button %s", button)
-
-    def record_wheel_event(self, wid: int, button: int) -> None:
-        pointerlog("recording scroll event for button %i", button)
-        for ss in self.window_sources():
-            ss.record_scroll_event(wid)

xpra/x11/server/xtest_pointer.py
@@ -30,5 +30,9 @@ class XTestPointerDevice:
             XTestBindings().xtest_fake_button(button, pressed)
 
     @staticmethod
+    def wheel_motion(button: int, distance: float) -> None:
+        raise NotImplementedError()
+
+    @staticmethod
     def has_precise_wheel() -> bool:
         return False

commit ff20426ad30afeca6d0cfdc17006d25c04f3d7f8

17b20c9c6e1ecef1a84eb5d71116017cfa2e883b broke ungrab

xpra/x11/server/seamless.py
@@ -232,7 +232,7 @@ class SeamlessServer(GObject.GObject, ServerBase):
             # normally we set this value when we receive the NotifyUngrab
             # but at this point in the cleanup, we probably won't, so force set it:
             self._has_grab = 0
-            self.X11_ungrab()
+            self.x11_ungrab()
         super().do_cleanup()
 
     def last_client_exited(self) -> None:
@@ -240,7 +240,7 @@ class SeamlessServer(GObject.GObject, ServerBase):
         super().last_client_exited()
         if self._has_grab:
             self._has_grab = 0
-            self.X11_ungrab()
+            self.x11_ungrab()
 
     def update_size_constraints(self, minw, minh, maxw, maxh) -> None:
         wm = self._wm

xpra/x11/subsystem/display.py
@@ -627,6 +627,10 @@ class X11DisplayManager(DisplayManager):
         grablog("force ungrab from %s", proto)
         x11_ungrab()
 
+    @staticmethod
+    def x11_ungrab():
+        x11_ungrab()
+
     # noinspection PyMethodMayBeStatic
     def init_packet_handlers(self) -> None:
         DisplayManager.init_packet_handlers(self)