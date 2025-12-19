0019e6483ec1f2422548d057a1983b3e4f10f362
Carl Kittelberger <icedream@icedream.pw>
2025-12-19 10:19:51 +0100

    fix wrong include path for AMF headers

--- setup.py
+++ setup.py
@@ -2860,7 +2860,7 @@ if amf_ENABLED:
         amf_kwargs = pkgconfig("amf")
     except ValueError:
         amf_kwargs = {
-            "extra_compile_args": "-I" + find_header_file("AMF", isdir=True) + "/AMF",
+            "extra_compile_args": "-I" + find_header_file("AMF", isdir=True),
             # "extra_link_args": ("-lpam", "-lpam_misc"),
         }
         print(f"using default amf args: {amf_kwargs}")
