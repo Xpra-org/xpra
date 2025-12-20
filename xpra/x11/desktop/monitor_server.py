3061ace52e6ca35e5d2daf47bcd671e381588119
Antoine Martin <antoine@xpra.org>
2025-12-20 16:55:37 +0700

    skip duplicated monitors pointing to the same outputs

--- xpra/x11/desktop/monitor_server.py
+++ xpra/x11/desktop/monitor_server.py
@@ -132,8 +132,14 @@ class XpraMonitorServer(DesktopServerBase):
     def load_existing_windows(self) -> None:
         with (xlog):
             monitors = RandRBindings().get_monitor_properties()
+            screenlog("load_existing_windows() found monitors=%r", monitors)
+            outputs = set()
             for i, monitor in monitors.items():
-                self.add_monitor_model(i + 1, monitor)
+                moutputs = set(monitor.get("outputs", ()))
+                # skip this monitor if we already have another one with the same outputs:
+                if monitor.get("primary") or not outputs.issuperset(moutputs):
+                    outputs.update(moutputs)
+                    self.add_monitor_model(i + 1, monitor)
         # does not fire: (because of GTK?)
         # RandR.select_crtc_output_changes()
         # screen = gdk.Screen.get_default()
