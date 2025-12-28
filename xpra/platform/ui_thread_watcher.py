058d4ddbc56f7625209411047ae9d10151b56cb7
Antoine Martin <antoine@xpra.org>
2025-12-28 19:17:21 +0700

    shorter window for race conditions

--- xpra/platform/ui_thread_watcher.py
+++ xpra/platform/ui_thread_watcher.py
@@ -107,7 +107,6 @@ class UIThreadWatcher:
             elapsed = monotonic() - scheduled_at
         else:
             elapsed = 0
-        self.ui_wakeup_timer = 0
         log("ui_thread_wakeup(%s) elapsed=%.2fms", scheduled_at, 1000 * elapsed)
         self.last_ui_thread_time = monotonic()
         # UI thread was blocked?
@@ -117,6 +116,7 @@ class UIThreadWatcher:
                 self.announced_blocked = False
             self.ui_blocked = False
             run_callbacks(self.resume_callbacks)
+        self.ui_wakeup_timer = 0
         return False
 
     def poll_ui_loop(self) -> None:
