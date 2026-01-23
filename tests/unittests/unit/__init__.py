3b2e9fdf676a181ff7399c0b1c4378bc8892f327
Antoine Martin <antoine@xpra.org>
2026-01-23 23:35:11 +0700

    all unit tests should have this environment variable set

--- tests/unittests/unit/__init__.py
+++ tests/unittests/unit/__init__.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # This file is part of Xpra.
 # Copyright (C) 2014 Antoine Martin <antoine@xpra.org>
+
+import os
+
+if "XPRA_UNIT_TEST" not in os.environ:
+    os.environ["XPRA_UNIT_TEST"] = "1"
