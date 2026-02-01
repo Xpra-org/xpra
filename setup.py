dc32636f6f548399f56463f604c2aafc982ac4a8
Antoine Martin <antoine@xpra.org>
2026-02-02 00:10:20 +0700

    enable avif on macos
    now that we can actually build something useful:
    https://github.com/Xpra-org/gtk-osx-build/commit/ace4a8be345e079010fdd68a8c01668012c0f199

--- setup.py
+++ setup.py
@@ -302,7 +302,7 @@ webp_encoder_ENABLED    = webp_ENABLED
 webp_decoder_ENABLED    = webp_ENABLED
 jpeg_encoder_ENABLED    = DEFAULT and pkg_config_version("1.2", "libturbojpeg")
 jpeg_decoder_ENABLED    = DEFAULT and pkg_config_version("1.4", "libturbojpeg")
-avif_ENABLED            = DEFAULT and pkg_config_version("0.9", "libavif") and not OSX
+avif_ENABLED            = DEFAULT and pkg_config_version("0.9", "libavif")
 avif_encoder_ENABLED    = avif_ENABLED
 avif_decoder_ENABLED    = avif_ENABLED
 vpx_ENABLED             = DEFAULT and pkg_config_version("1.7", "vpx") and BITS==64
