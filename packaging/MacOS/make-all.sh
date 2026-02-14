0ee933b35e340f9c372983b908e130e57a79e938
Antoine Martin <antoine@xpra.org>
2026-02-14 18:00:19 +0700

    make it possible to call this script from any path

--- packaging/MacOS/make-all.sh
+++ packaging/MacOS/make-all.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "${CODESIGN_KEYNAME}" ]; then
 	export CODESIGN_KEYNAME="xpra.org"
 fi
+
+MACOS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${MACOS_SCRIPT_DIR}" || exit 1
 
 ./make-app.sh && ./make-DMG.sh && ./make-PKG.sh
