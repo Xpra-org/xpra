88aef793c21dbf4b8dc62a9cea0f401256c85550
Antoine Martin <antoine@xpra.org>
2026-02-14 17:58:30 +0700

    remove separate signing script

--- packaging/MacOS/make-all.sh
+++ packaging/MacOS/make-all.sh
@@ -4,4 +4,4 @@ if [ -z "${CODESIGN_KEYNAME}" ]; then
 	export CODESIGN_KEYNAME="xpra.org"
 fi
 
-./make-app.sh && ./sign-app.sh && ./make-DMG.sh && ./make-PKG.sh
+./make-app.sh && ./make-DMG.sh && ./make-PKG.sh
