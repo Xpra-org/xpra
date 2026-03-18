ac431a96b7aa8d91e2a5c060cdbdca5501309d49
Antoine Martin <antoine@xpra.org>
2026-03-18 20:42:42 +0700

    verify if key is locked

--- packaging/MacOS/sign-app.sh
+++ packaging/MacOS/sign-app.sh
@@ -7,7 +7,16 @@ fi
 
 if [ ! -z "${CODESIGN_KEYNAME}" ]; then
 		echo "Signing with key '${CODESIGN_KEYNAME}'"
-        codesign --deep --force --verify --verbose --sign "${CODESIGN_KEYNAME}" ${APP_DIR}
+		# verify that it is unlocked:
+		if [ -z "${KEYCHAIN}" ]; then
+			KEYCHAIN="$HOME/Library/Keychains/login.keychain-db"
+			echo "using default login keychain ${KEYCHAIN}"
+		fi
+		if ! security show-keychain-info "$KEYCHAIN" 2>/dev/null; then
+			echo "Keychain is locked, cannot sign!"
+			exit 1
+		fi
+		codesign --deep --force --verify --verbose --sign "${CODESIGN_KEYNAME}" ${APP_DIR}
 else
 		echo "Signing skipped (no keyname)"
 fi
