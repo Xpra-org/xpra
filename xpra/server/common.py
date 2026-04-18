5b03d4a0d7df893e7332956848a68aeda33dac01
Antoine Martin <antoine@xpra.org>
2026-04-18 19:29:18 +0700

    #4871 missed from 100e27c171eb08b787ec205d2409c3ecb922e257

---
+++ xpra/server/common.py
@@ -0,0 +1,21 @@
+# This file is part of Xpra.
+# Copyright (C) 2026 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from typing import Final
+from collections.abc import Sequence
+
+from xpra.common import noop
+
+
+GET_SOURCES_BY_TYPE: Final[str] = "get_sources_by_type"
+
+
+def get_sources_by_type(server, subsystem_type: type, exclude=None) -> Sequence:
+    fn = getattr(server, GET_SOURCES_BY_TYPE, noop)
+    if fn == noop:
+        from xpra.log import Logger
+        Logger("server").error("Error: no %r in %s", GET_SOURCES_BY_TYPE, server)
+        return ()
+    return fn(subsystem_type, exclude)
