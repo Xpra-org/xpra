d3210bcc3e74f7c594c34f9dd3b52bfc92232f17
Antoine Martin <antoine@xpra.org>
2026-05-21 15:55:35 +0700

    clipboard import can fail if Gtk is blocked

--- xpra/wayland/subsystem/clipboard.py
+++ xpra/wayland/subsystem/clipboard.py
@@ -4,12 +4,21 @@
 # later version. See the file COPYING for details.
 
 from xpra.server.subsystem.clipboard import ClipboardManager
+from xpra.log import Logger
+
+log = Logger("wayland", "clipboard")
 
 
 class WaylandClipboardManager(ClipboardManager):
 
     def get_clipboard_class(self):
-        from xpra.wayland.clipboard import WaylandClipboard
+        try:
+            from xpra.wayland.clipboard import WaylandClipboard
+        except ImportError as e:
+            log("get_clipboard_class()", exc_info=True)
+            log.warn("Warning: unable to load the Wayland clipboard backend")
+            log.warn(" %s", e)
+            return None
 
         def make_wayland_clipboard(*args, **kwargs) -> WaylandClipboard:
             return WaylandClipboard(*args, compositor=self.server.compositor, **kwargs)
