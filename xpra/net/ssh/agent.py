0bc3b458e78dbdc33d0f6020b0b8377562d8d5ea
Antoine Martin <antoine@xpra.org>
2026-05-29 20:42:06 +0700

    reject more unexpected / tricky agent paths

--- xpra/net/ssh/agent.py
+++ xpra/net/ssh/agent.py
@@ -49,7 +49,7 @@ def setup_ssh_auth_sock(session_dir: str) -> str:
 
 def get_ssh_agent_path(filename: str, session_dir: str = "") -> str:
     ssh_dir = ssh_dir_path(session_dir)
-    if "/" in filename or ".." in filename:
+    if any((c and c in filename) for c in (os.path.sep, os.path.altsep, ":", "..", "~")) or filename == ".":
         raise ValueError(f"illegal characters found in ssh agent filename {filename!r}")
     return os.path.join(ssh_dir, filename or "agent.default")
 
