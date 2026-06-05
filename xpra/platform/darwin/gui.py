d2541c72251c35f3bc77d703f8ca9ca201efaa88
Antoine Martin <antoine@xpra.org>
2026-06-05 10:51:24 +0700

    derp, this is a function

--- xpra/platform/darwin/gui.py
+++ xpra/platform/darwin/gui.py
@@ -218,7 +218,7 @@ def get_workareas() -> Sequence[tuple[int, int, int, int]]:
 
 def get_display_size() -> tuple[int, int]:
     Gdk = gi_import("Gdk")
-    screen = Gdk.Screen.get_default
+    screen = Gdk.Screen.get_default()
     if not screen:
         raise RuntimeError("unable to access the screen via Gdk")
     return screen.get_width(), screen.get_height()
