dd1162349ed4c6831dc57663d83d92d7112cfef7
Antoine Martin <antoine@xpra.org>
2026-06-06 22:44:29 +0700

    bug: give callback function instead of calling it on the spot

--- xpra/client/gtk3/tray_menu.py
+++ xpra/client/gtk3/tray_menu.py
@@ -1536,7 +1536,7 @@ class GTKTrayMenu(GTKMenuHelper):
         self._call_non_OR_windows({"deiconify": (), "present": ()})
 
     def make_raisewindowsmenuitem(self) -> Gtk.ImageMenuItem:
-        return self.handshake_menuitem("Raise Windows", "raise.png", None, self._raise_all_windows())
+        return self.handshake_menuitem("Raise Windows", "raise.png", None, self._raise_all_windows)
 
     def _minimize_all_windows(self, *_args) -> Gtk.ImageMenuItem:
         self._call_non_OR_windows({"iconify": ()})
