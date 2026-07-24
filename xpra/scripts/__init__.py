43ba0dfce3d78c6f2ff580d0a8dfe2d3fa748c58
Antoine Martin <antoine@xpra.org>
2026-07-24 21:47:17 +0700

    try harder to set CRYPTOGRAPHY_OPENSSL_NO_LEGACY early
    this should silence some warnings annoying 'legacy' warnings

--- xpra/scripts/__init__.py
+++ xpra/scripts/__init__.py
@@ -3,13 +3,13 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import os
 import sys
-
-APPNAME = os.environ.get("APPNAME", "")
 
 # workaround for macos where we need to setup the application name early,
 # before we import the macos components
-if sys.platform == "darwin" and APPNAME:
-    from xpra.util.system import set_proc_title
-    set_proc_title(APPNAME)
+if sys.platform == "darwin":
+    import os
+    if appname := os.environ.get("APPNAME", ""):
+        from xpra.util.system import set_proc_title
+        set_proc_title(appname)
+    os.environ.setdefault("CRYPTOGRAPHY_OPENSSL_NO_LEGACY", "1")
