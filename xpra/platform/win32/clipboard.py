765f3d0414e80c674a7c5060f4991f6a9d060aa8
Antoine Martin <antoine@xpra.org>
2026-08-05 18:56:09 +0700

    ensure all attributes are initialized in __init__
    this was safe as it was since we had to go through got_token to use the attributes,
    but could break in the future, and is untidy
    
    (cherry picked from commit d3910f11e6f094bb02662de03769f476acbcc6ff)

--- xpra/platform/win32/clipboard.py
+++ xpra/platform/win32/clipboard.py
@@ -405,6 +405,9 @@ class Win32ClipboardProxy(ClipboardProxyCore):
         self.window = window
         self.send_clipboard_request_handler = send_clipboard_request_handler
         self.send_clipboard_token_handler = send_clipboard_token_handler
+        #the targets and the data the peer sent us with the last token:
+        self.targets = ()
+        self.target_data = {}
         super().__init__(selection)
 
     def with_clipboard_lock(self, success_callback, failure_callback, retries=RETRY, delay=DELAY):
