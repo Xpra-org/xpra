f5096602a0c0642479613a8630e92aef0b4a9e51
Antoine Martin <antoine@xpra.org>
2026-08-10 17:55:05 +0700

    these test commands were removed in e36002df6b3ac6526a0632080c6557dc391ac52e

--- tests/unittests/unit/scripts/main_test.py
+++ tests/unittests/unit/scripts/main_test.py
@@ -350,12 +350,7 @@ class TestMain(unittest.TestCase):
             "_pass",
             #"send-file", needs a server socket
             #"splash", has its own test module
-            "clipboard-test",
-            "keyboard-test",
             "toolbox",
-            "colors-test",
-            "colors-gradient-test",
-            "transparent-colors",
         ]
         for args in subcommands:
             proc = self._run_subcommand(args, 10)
