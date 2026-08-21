0cfecd0ea941ec48a85a15cd6e5eab416a18df3a
Antoine Martin <antoine@xpra.org>
2026-08-21 21:48:50 +0700

    workaround for missing openat2 in some emulated environments

--- packaging/debian/xserver-xorg-video-dummy.sh
+++ packaging/debian/xserver-xorg-video-dummy.sh
@@ -7,7 +7,17 @@ fi
 DUMMY_TAR_XZ=`ls ../pkgs/xf86-video-dummy-*.tar.xz`
 dirname=`echo ${DUMMY_TAR_XZ} | sed 's+../pkgs/++g' | sed 's/.tar.xz//' | sort -V | tail -n 1`
 rm -fr "./${dirname}"
-tar -Jxf ${DUMMY_TAR_XZ}
+#GNU tar 1.35 and later use the `openat2` syscall for extraction, which some
+#versions of qemu don't implement - extraction then fails with
+#"Cannot open: Function not implemented", see:
+#https://github.com/Xpra-org/repo-build-scripts/issues/15
+#bsdtar doesn't use `openat2`, so prefer it when it is installed
+#(unlike GNU tar, it restores xattrs, ACLs and file flags unless told not to):
+TAR="tar"
+if command -v bsdtar > /dev/null; then
+	TAR="bsdtar --no-xattrs --no-acls --no-fflags"
+fi
+${TAR} -Jxf ${DUMMY_TAR_XZ}
 pushd "./${dirname}"
 ln -sf ../xserver-xorg-video-dummy ./debian
 
