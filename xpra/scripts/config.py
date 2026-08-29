ebef75878ba10a5e66567f53bcace95b0a5230b8
Antoine Martin <antoine@xpra.org>
2026-08-29 23:55:07 +0700

    sharing must always be a string

--- xpra/scripts/config.py
+++ xpra/scripts/config.py
@@ -1261,7 +1261,7 @@ def get_defaults() -> dict[str, Any]:
         "xsettings"         : ["auto", "no"][int(OSX or WIN32)],
         "gsettings-sync"    : ["auto", "no"][int(OSX or WIN32)],
         "system-tray"       : True,
-        "sharing"           : None,
+        "sharing"           : "auto",
         "lock"              : None,
         "delay-tray"        : False,
         "windows"           : True,
