43062554b2a70230ebd675f874045b19b9066e5b
Antoine Martin <antoine@xpra.org>
2026-09-01 18:07:05 +0700

    #5025 constant had been moved

--- xpra/platform/posix/sd_listen.pyx
+++ xpra/platform/posix/sd_listen.pyx
@@ -56,7 +56,7 @@ def get_sd_listen_sockets() -> Sequence[Tuple[str, Any, Tuple[str, int]]]:
 
 
 def get_sd_socket_type(fd) -> str:
-    from xpra.net.common import TCP_SOCKTYPES
+    from xpra.net.socktypes import TCP_SOCKTYPES
     socktype = os.environ.get("XPRA_SD%i_SOCKET_TYPE" % fd)
     if not socktype:
         socktype = os.environ.get("XPRA_SD_SOCKET_TYPE", "tcp")
