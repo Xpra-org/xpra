Xpra-org/xpra

commit 4a22b231a1b617401a7d8390da65cfa2c3ba49c8

#4523 split file and printer

xpra/server/source/factory.py
@@ -31,9 +31,12 @@ def get_enabled_mixins() -> Sequence[type]:
     if features.webcam:
         from xpra.server.source.webcam import WebcamConnection
         mixins.append(WebcamConnection)
-    if features.fileprint:
-        from xpra.server.source.fileprint import FilePrintConnection
-        mixins.append(FilePrintConnection)
+    if features.file:
+        from xpra.server.source.file import FileConnection
+        mixins.append(FileConnection)
+    if features.printer:
+        from xpra.server.source.printer import PrinterConnection
+        mixins.append(PrinterConnection)
     if features.mmap:
         from xpra.server.source.mmap import MMAP_Connection
         mixins.append(MMAP_Connection)

xpra/server/source/file.py
@@ -1,0 +1,39 @@
+# This file is part of Xpra.
+# Copyright (C) 2010 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from typing import Any
+
+from xpra.util.objects import typedict
+from xpra.net.file_transfer import FileTransferHandler
+from xpra.server.source.stub import StubClientConnection
+from xpra.log import Logger
+
+log = Logger("file")
+
+
+class FileConnection(FileTransferHandler, StubClientConnection):
+
+    @classmethod
+    def is_needed(cls, caps: typedict) -> bool:
+        return bool("file" in caps or caps.boolget("file-transfer"))
+
+    def parse_client_caps(self, c: typedict) -> None:
+        FileTransferHandler.parse_file_transfer_caps(self, c)
+
+    def get_info(self) -> dict[str, Any]:
+        return {
+            "file-transfers": FileTransferHandler.get_info(self),
+        }
+
+    def init_from(self, _protocol, server) -> None:
+        self.init_attributes()
+        # copy attributes
+        for x in (
+                "file_transfer", "file_transfer_ask", "file_size_limit", "file_chunks",
+                "open_files", "open_files_ask",
+                "open_url", "open_url_ask",
+                "file_ask_timeout", "open_command",
+        ):
+            setattr(self, x, getattr(server.file_transfer, x))

xpra/server/source/printer.py
@@ -41,11 +41,11 @@ def find_auth_password_file(auth_defs: Sequence[AuthDef]) -> str:
     return ""
 
 
-class FilePrintConnection(FileTransferHandler, StubClientConnection):
+class PrinterConnection(FileTransferHandler, StubClientConnection):
 
     @classmethod
     def is_needed(cls, caps: typedict) -> bool:
-        return bool("file" in caps or caps.boolget("file-transfer") or caps.boolget("printing"))
+        return caps.boolget("printing")
 
     def init_state(self) -> None:
         self.printers: dict[str, dict] = {}
@@ -57,13 +57,15 @@ class FilePrintConnection(FileTransferHandler, StubClientConnection):
         self.remove_printers()
 
     def parse_client_caps(self, c: typedict) -> None:
-        FileTransferHandler.parse_file_transfer_caps(self, c)
+        FileTransferHandler.parse_printer_caps(self, c)
         self.machine_id = c.strget("machine_id")
 
     def get_info(self) -> dict[str, Any]:
         return {
-            "printers": self.printers,
-            "file-transfers": FileTransferHandler.get_info(self),
+            "printer": {
+                "printers": self.printers,
+                "file-transfers": FileTransferHandler.get_info(self),
+            },
         }
 
     def init_from(self, _protocol, server) -> None:
@@ -71,10 +73,7 @@ class FilePrintConnection(FileTransferHandler, StubClientConnection):
         self.unix_socket_paths: list[str] = server.unix_socket_paths
         # copy attributes
         for x in (
-                "file_transfer", "file_transfer_ask", "file_size_limit", "file_chunks",
-                "printing", "printing_ask", "open_files", "open_files_ask",
-                "open_url", "open_url_ask",
-                "file_ask_timeout", "open_command",
+                "printing", "printing_ask",
         ):
             setattr(self, x, getattr(server.file_transfer, x))
 

tests/unittests/unit/server/source/source_mixins_test.py
@@ -143,12 +143,12 @@ class SourceMixinsTest(unittest.TestCase):
             "encodings.core": ("rgb32", "rgb24"),
         })
 
-    def test_fileprint(self):
-        from xpra.server.source.fileprint import FilePrintConnection
+    def test_file(self):
+        from xpra.server.source.file import FileConnection
         from xpra.net.file_transfer import FileTransferAttributes
-        self._test_mixin_class(FilePrintConnection, {
-            "file_transfer" : FileTransferAttributes(),
-            "machine_id"    : "123",
+        self._test_mixin_class(FileConnection, {
+            "file_transfer": FileTransferAttributes(),
+            "machine_id": "123",
         })
 
     def test_idle(self):

xpra/client/base/factory.py
@@ -16,9 +16,12 @@ def get_client_base_classes() -> tuple[type, ...]:
     if features.debug:
         from xpra.client.base.debug import DebugClient
         CLIENT_BASES.append(DebugClient)
-    if features.file_transfer:
-        from xpra.client.base.fileprint import FilePrintMixin
-        CLIENT_BASES.append(FilePrintMixin)
+    if features.file:
+        from xpra.client.base.file import FileMixin
+        CLIENT_BASES.append(FileMixin)
+    if features.printer:
+        from xpra.client.base.printer import PrinterMixin
+        CLIENT_BASES.append(PrinterMixin)
     if features.control:
         from xpra.client.base.control import ControlClient
         CLIENT_BASES.append(ControlClient)

xpra/client/base/file.py
@@ -1,0 +1,184 @@
+# This file is part of Xpra.
+# Copyright (C) 2010 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+from typing import Any
+
+from xpra.os_util import gi_import
+from xpra.util.objects import typedict
+from xpra.util.str_fn import csv
+from xpra.util.env import envint, envbool
+from xpra.net.file_transfer import FileTransferHandler
+from xpra.client.base.stub import StubClientMixin
+from xpra.util.thread import start_thread
+from xpra.log import Logger
+
+GLib = gi_import("GLib")
+
+printlog = Logger("printing")
+filelog = Logger("file")
+
+DELETE_PRINTER_FILE = envbool("XPRA_DELETE_PRINTER_FILE", True)
+SKIP_STOPPED_PRINTERS = envbool("XPRA_SKIP_STOPPED_PRINTERS", True)
+INIT_PRINTING_DELAY = envint("XPRA_INIT_PRINTING_DELAY", 2)
+
+
+class FileMixin(StubClientMixin, FileTransferHandler):
+
+    def __init__(self):
+        StubClientMixin.__init__(self)
+        FileTransferHandler.__init__(self)
+        self.printer_attributes = []
+        self.send_printers_timer: int = 0
+        self.exported_printers = None
+        self.remote_request_file: bool = False
+
+    def init(self, opts) -> None:
+        # printing and file transfer:
+        FileTransferHandler.init_opts(self, opts)
+
+    def init_authenticated_packet_handlers(self) -> None:
+        self.add_packets(
+            "open-url", "send-file",
+            "send-data-request", "send-data-response",
+            "ack-file-chunk", "send-file-chunk",
+        )
+
+    def get_caps(self) -> dict[str, Any]:
+        return {"file": self.get_file_transfer_features()}
+
+    def cleanup(self) -> None:
+        # we must clean printing before FileTransferHandler, which turns the printing flag off!
+        self.cleanup_printing()
+        FileTransferHandler.cleanup(self)
+
+    def parse_server_capabilities(self, c: typedict) -> bool:
+        self.parse_file_transfer_caps(c)
+        self.remote_request_file = c.boolget("request-file", False)
+        return True
+
+    def parse_printing_capabilities(self, caps: typedict) -> None:
+        printlog("parse_printing_capabilities() client printing support=%s", self.printing)
+        if self.printing:
+            server_printing = caps.boolget("printing")
+            printlog("parse_printing_capabilities() server printing support=%s", server_printing)
+            if server_printing:
+                self.printer_attributes = caps.strtupleget("printer.attributes",
+                                                           ("printer-info", "device-uri"))
+                GLib.timeout_add(INIT_PRINTING_DELAY * 1000, self.init_printing)
+
+    def init_printing(self) -> None:
+        try:
+            from xpra.platform.printing import init_printing  # pylint: disable=import-outside-toplevel
+            printlog("init_printing=%s", init_printing)
+            init_printing(self.send_printers)
+        except Exception as e:
+            printlog.error("Error initializing printing support:")
+            printlog.estr(e)
+            self.printing = False
+        else:
+            self.send_printers()
+        printlog("init_printing() enabled=%s", self.printing)
+
+    def cleanup_printing(self) -> None:
+        printlog("cleanup_printing() printing=%s", self.printing)
+        if not self.printing:
+            return
+        self.cancel_send_printers_timer()
+        try:
+            from xpra.platform.printing import cleanup_printing  # pylint: disable=import-outside-toplevel
+            printlog("cleanup_printing=%s", cleanup_printing)
+            cleanup_printing()
+        except ImportError:
+            printlog("cleanup_printing()", exc_info=True)
+        except Exception as e:
+            printlog("cleanup_printing()", exc_info=True)
+            printlog.warn("Warning: failed to cleanup printing subsystem:")
+            printlog.warn(" %s", e)
+
+    def send_printers(self, *args) -> None:
+        printlog("send_printers%s timer=%s", args, self.send_printers_timer)
+        # dbus can fire dozens of times for a single printer change,
+        # so we wait a bit and fire via a timer to try to batch things together:
+        if self.send_printers_timer:
+            return
+        self.send_printers_timer = GLib.timeout_add(500, self.do_send_printers)
+
+    def cancel_send_printers_timer(self) -> None:
+        spt = self.send_printers_timer
+        printlog("cancel_send_printers_timer() send_printers_timer=%s", spt)
+        if spt:
+            self.send_printers_timer = 0
+            GLib.source_remove(spt)
+
+    def do_send_printers(self) -> None:
+        self.send_printers_timer = 0
+        start_thread(self.send_printers_thread, "send-printers", True)
+
+    def send_printers_thread(self) -> None:
+        from xpra.platform.printing import get_printers, get_mimetypes  # pylint: disable=import-outside-toplevel
+        try:
+            printers = get_printers()
+        except Exception as e:
+            printlog("%s", get_printers, exc_info=True)
+            printlog.error("Error: cannot access the list of printers")
+            printlog.estr(e)
+            return
+        printlog("send_printers_thread() found printers=%s", printers)
+        try:
+            # remove xpra-forwarded printers to avoid loops and multi-forwards,
+            # also ignore stopped printers
+            # and only keep the attributes that the server cares about (self.printer_attributes)
+            exported_printers = {}
+
+            def used_attrs(d):
+                # filter attributes so that we only compare things that are actually used
+                if not d:
+                    return d
+                return {pk: pv for pk, pv in d.items() if pk in self.printer_attributes}
+
+            for k, v in printers.items():
+                device_uri = v.get("device-uri", "")
+                if device_uri:
+                    # this is specific to the `cups` backend:
+                    printlog("send_printers_thread() device-uri(%s)=%s", k, device_uri)
+                    if device_uri.startswith("xpraforwarder"):
+                        printlog("do_send_printers() skipping xpra forwarded printer=%s", k)
+                        continue
+                state = v.get("printer-state")
+                # "3" if the destination is idle,
+                # "4" if the destination is printing a job,
+                # "5" if the destination is stopped.
+                if state == 5 and SKIP_STOPPED_PRINTERS:
+                    printlog("do_send_printers() skipping stopped printer=%s", k)
+                    continue
+                attrs = used_attrs(v)
+                # add mimetypes:
+                attrs["mimetypes"] = get_mimetypes()
+                exported_printers[k] = attrs
+            if self.exported_printers is None:
+                # not been sent yet, ensure we can use the dict below:
+                self.exported_printers = {}
+            elif exported_printers == self.exported_printers:
+                printlog("send_printers_thread() exported printers unchanged: %s", self.exported_printers)
+                return
+            # show summary of what has changed:
+            added = tuple(k for k in exported_printers if k not in self.exported_printers)
+            if added:
+                printlog("send_printers_thread() new printers: %s", added)
+            removed = tuple(k for k in self.exported_printers if k not in exported_printers)
+            if removed:
+                printlog("send_printers_thread() printers removed: %s", removed)
+            modified = tuple(k for k, v in exported_printers.items() if
+                             self.exported_printers.get(k) != v and k not in added)
+            if modified:
+                printlog("send_printers_thread() printers modified: %s", modified)
+            printlog("send_printers_thread() printers=%s", exported_printers.keys())
+            printlog("send_printers_thread() exported printers=%s", csv(str(x) for x in exported_printers))
+            self.exported_printers = exported_printers
+            self.send("printers", self.exported_printers)
+        except Exception as e:
+            printlog("do_send_printers()", exc_info=True)
+            printlog.error("Error sending the list of printers")
+            printlog.estr(e)

xpra/client/base/printer.py
@@ -24,7 +24,7 @@ SKIP_STOPPED_PRINTERS = envbool("XPRA_SKIP_STOPPED_PRINTERS", True)
 INIT_PRINTING_DELAY = envint("XPRA_INIT_PRINTING_DELAY", 2)
 
 
-class FilePrintMixin(StubClientMixin, FileTransferHandler):
+class PrinterMixin(StubClientMixin, FileTransferHandler):
 
     def __init__(self):
         StubClientMixin.__init__(self)
@@ -46,7 +46,7 @@ class FilePrintMixin(StubClientMixin, FileTransferHandler):
         )
 
     def get_caps(self) -> dict[str, Any]:
-        return self.get_file_transfer_features()
+        return {"printer": self.get_printer_features()}
 
     def cleanup(self) -> None:
         # we must clean printing before FileTransferHandler, which turns the printing flag off!
@@ -55,7 +55,7 @@ class FilePrintMixin(StubClientMixin, FileTransferHandler):
 
     def parse_server_capabilities(self, c: typedict) -> bool:
         self.parse_printing_capabilities(c)
-        self.parse_file_transfer_caps(c)
+        self.parse_printer_caps(c)
         self.remote_request_file = c.boolget("request-file", False)
         return True
 

xpra/server/factory.py
@@ -21,9 +21,9 @@ def get_server_base_classes() -> tuple[type, ...]:
     if features.debug:
         from xpra.server.subsystem.debug import DebugServer
         classes.append(DebugServer)
-    if features.fileprint:
-        from xpra.server.subsystem.fileprint import FilePrintServer
-        classes.append(FilePrintServer)
+    if features.file:
+        from xpra.server.subsystem.file import FileServer
+        classes.append(FileServer)
     if features.mmap:
         from xpra.server.subsystem.mmap import MMAP_Server
         classes.append(MMAP_Server)

commit c368f097f277ef11da2e3e4b17b01e642d864cce

fix #5028 the printer source mixin was wiping the file transfer attributes
`FileConnection` and `PrinterConnection` are mixed into the same
`ClientConnection` instance and both inherited `FileTransferHandler`,
so they shared a single set of attributes.
Both `__init__` and `init_from` called `init_attributes()`, which resets
that whole set to the defaults, and each mixin then restored only its own
half - so `PrinterConnection`, which runs last, wiped the file half.

With printing enabled (the default), that silently disabled server to
client file transfers, `open-files`, `open-url`, and reverted a
non-default `file-size-limit` to `1G`.
No client side setting could reach it: `do_control_file_command` checks
the server's own per-connection flag.

Regression from 4a22b231a1: the pre-split `FilePrintConnection` copied
the whole attribute set.

`PrinterConnection` no longer inherits from `FileTransferHandler`, so the
file transfer state has a single owner - which is what the same commit
already did on the client side, see `xpra/client/base/printer.py`.
It now only sets `printing` / `printing_ask`, from the `PrinterServer`
copy that owns them (`setup` and `init_printing` may disable printing
based on the local sockets and authentication).
Since print jobs travel over the file transfer state and packet handlers,
`FileConnection` is now also required whenever printing is advertised.

`PrinterConnection.get_info` no longer repeats the whole
`file-transfers` block under `printer.file-transfers`.

The source mixins were only ever unit tested in isolation, which is
exactly the case where this cannot happen: `test_file_printer` exercises
the two together and fails without this fix.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01HXPQfxNf3aVuy3ELkxrYLd

xpra/server/source/factory.py
@@ -49,6 +49,8 @@ def get_enabled_mixins() -> Sequence[type]:
     if features.file:
         from xpra.server.source.file import FileConnection
         mixins.append(FileConnection)
+    # must be after the file mixin: `PrinterConnection` has no file-transfer state
+    # of its own, it sets `printing` on the state owned by `FileConnection`
     if features.printer:
         from xpra.server.source.printer import PrinterConnection
         mixins.append(PrinterConnection)

xpra/server/source/file.py
@@ -19,7 +19,13 @@ class FileConnection(FileTransferHandler, StubClientConnection):
 
     @classmethod
     def is_needed(cls, caps: typedict) -> bool:
-        return bool("file" in caps or BACKWARDS_COMPATIBLE and caps.boolget("file-transfer"))
+        if "file" in caps:
+            return True
+        # printer forwarding shares this subsystem's state and packet handlers,
+        # so this mixin is also required whenever `PrinterConnection` is:
+        if caps.dictget("printer"):
+            return True
+        return bool(BACKWARDS_COMPATIBLE and (caps.boolget("file-transfer") or caps.boolget("printing")))
 
     def parse_client_caps(self, c: typedict) -> None:
         FileTransferHandler.parse_file_transfer_caps(self, c)
@@ -30,7 +36,7 @@ class FileConnection(FileTransferHandler, StubClientConnection):
         }
 
     def init_from(self, _protocol, server) -> None:
-        self.init_attributes()
+        # (`__init__` has just called `init_attributes()` to reset the defaults)
         # `FileServer` is the standalone subsystem instance:
         file_transfer = server.subsystems["file"].file_transfer
         # copy attributes

xpra/server/source/printer.py
@@ -11,7 +11,6 @@ from xpra.net.common import BACKWARDS_COMPATIBLE
 from xpra.util.objects import typedict
 from xpra.util.env import envbool
 from xpra.os_util import get_machine_id
-from xpra.net.file_transfer import FileTransferHandler
 from xpra.auth.auth_helper import AuthDef
 from xpra.server.source.stub import StubClientConnection
 from xpra.log import Logger
@@ -42,7 +41,16 @@ def find_auth_password_file(auth_defs: Sequence[AuthDef]) -> str:
     return ""
 
 
-class PrinterConnection(FileTransferHandler, StubClientConnection):
+class PrinterConnection(StubClientConnection):
+    """
+    Printer forwarding.
+    `printing` / `remote_printing` / `remote_printing_ask` and the packets carrying
+    the print jobs are all part of the same `FileTransferHandler` state as regular
+    file transfers - which `FileConnection` owns, and which it mixes in whenever
+    printing is advertised (see `FileConnection.is_needed`).
+    So this subsystem has no `FileTransferHandler` of its own,
+    it owns only the printer configuration state.
+    """
 
     @classmethod
     def is_needed(cls, caps: typedict) -> bool:
@@ -60,28 +68,28 @@ class PrinterConnection(FileTransferHandler, StubClientConnection):
         self.remove_printers()
 
     def parse_client_caps(self, c: typedict) -> None:
-        FileTransferHandler.parse_printer_caps(self, c)
+        # `parse_printer_caps` comes from the `FileConnection` half of this instance:
+        self.parse_printer_caps(c)
         self.machine_id = c.strget("machine_id")
 
     def get_info(self) -> dict[str, Any]:
         return {
             "printer": {
                 "devices": self.printers,
-                "file-transfers": FileTransferHandler.get_info(self),
+                "printing": self.printing,
+                "printing-ask": self.printing_ask,
             },
         }
 
     def init_from(self, _protocol, server) -> None:
-        self.init_attributes()
+        # no `init_attributes()` here: the file-transfer attributes are shared with
+        # `FileConnection` (same instance), which is the subsystem that owns them
         self.unix_socket_paths: list[str] = server.unix_socket_paths
         # `PrinterServer` owns its own copy of the file-transfer attrs
-        # (init_printing may disable `printing` based on local sockets/auth):
+        # (`setup` / `init_printing` may disable `printing` based on local sockets/auth):
         file_transfer = server.subsystems["printer"].file_transfer
-        # copy attributes
-        for x in (
-                "printing", "printing_ask",
-        ):
-            setattr(self, x, getattr(file_transfer, x))
+        self.printing = file_transfer.printing
+        self.printing_ask = file_transfer.printing_ask
 
     ######################################################################
     # printing:

tests/unittests/unit/server/source/source_mixins_test.py
@@ -38,10 +38,12 @@ class SourceMixinsTest(unittest.TestCase):
         "speaker_codecs": (),
     }
 
-    def _test_mixin_class(self, mixin_class, server_props=None, client_caps=None, protocol=None, test_fn=None):
-        return self._test_mixin_classes((mixin_class, ), server_props, client_caps, protocol, test_fn)
+    def _test_mixin_class(self, mixin_class, server_props=None, client_caps=None, protocol=None, test_fn=None,
+                          subsystems=None):
+        return self._test_mixin_classes((mixin_class, ), server_props, client_caps, protocol, test_fn, subsystems)
 
-    def _test_mixin_classes(self, mixin_classes, server_props=None, client_caps=None, protocol=None, test_fn=None):
+    def _test_mixin_classes(self, mixin_classes, server_props=None, client_caps=None, protocol=None, test_fn=None,
+                            subsystems=None):
         assert mixin_classes
         for mixin_class in mixin_classes:
             assert mixin_class.is_needed(typedict(client_caps or {})) in (True, False)
@@ -68,9 +70,12 @@ class SourceMixinsTest(unittest.TestCase):
             "file": server,
             "idle": server,
             "mmap": server,
+            "printer": server,
             "webcam": server,
             "window": server,
         }
+        # some subsystems keep state of their own that the fake server can't stand in for:
+        server.subsystems.update(subsystems or {})
         #fake client caps object (as a typedict):
         d = typedict()
         if client_caps:
@@ -228,6 +233,43 @@ class SourceMixinsTest(unittest.TestCase):
             "file_transfer": FileTransferAttributes(),
             "machine_id": "123",
         })
+
+    def test_file_printer(self):
+        # `FileConnection` and `PrinterConnection` are mixed into the same instance
+        # and share a single set of file-transfer attributes:
+        # initializing the printer half must not wipe the file half - see #5028
+        from xpra.server.source.file import FileConnection
+        from xpra.server.source.printer import PrinterConnection
+        from xpra.net.file_transfer import FileTransferAttributes
+        file_transfer = FileTransferAttributes()
+        file_transfer.init_attributes(file_transfer="yes", file_size_limit="10M",
+                                      open_files="yes", open_url="yes", open_command="open-it")
+        # `PrinterServer` keeps its own copy, which is the one that owns `printing`:
+        printer_ft = AdHocStruct()
+        printer_ft.file_transfer = FileTransferAttributes()
+        printer_ft.file_transfer.init_attributes(printing="yes")
+
+        def check(_c, source):
+            # `cleanup()` resets the attributes, so this has to run before it:
+            self.assertTrue(source.file_transfer)
+            self.assertTrue(source.open_files)
+            self.assertTrue(source.open_url)
+            self.assertEqual(source.open_command, "open-it")
+            self.assertEqual(source.file_size_limit, 10 * 1000 * 1000)
+            # and `printing` must come from the printer subsystem's own copy:
+            self.assertTrue(source.printing)
+            self.assertTrue(source.remote_file_transfer)
+            self.assertTrue(source.remote_printing)
+
+        self._test_mixin_classes((FileConnection, PrinterConnection), {
+            "file_transfer": file_transfer,
+            "machine_id": "123",
+        }, {
+            # `printing` is parsed from the `file` namespace with backwards compatibility,
+            # from the `printer` one without it:
+            "file": {"enabled": True, "printing": True},
+            "printer": {"printing": True},
+        }, test_fn=check, subsystems={"printer": printer_ft})
 
     def test_idle(self):
         from xpra.server.source.idle_mixin import IdleConnection