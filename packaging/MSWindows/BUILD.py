394c0af848767ba640723f763de28a136b1cbdb9
Antoine Martin <antoine@xpra.org>
2026-09-12 23:37:45 +0700

    win32: locate verpatch executable

--- packaging/MSWindows/BUILD.py
+++ packaging/MSWindows/BUILD.py
@@ -1248,10 +1248,12 @@ def export_sbom() -> None:
 
 
 def verpatch() -> None:
+    verpatch_exe = find_command("verpatch", "VERPATCH",
+                                "packaging/MSWindows/tools/verpatch.exe")
     EXCLUDE = ("plink", "openssh", "openssl", "paexec")
 
     def run_verpatch(filename: str, descr: str) -> None:
-        log_command(["verpatch", filename,
+        log_command([verpatch_exe, filename,
                      "/s", "desc", descr,
                      "/va", version_info.padded,
                      "/s", "company", "xpra.org",
