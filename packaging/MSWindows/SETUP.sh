9cdfae5256eb318ffdf45342b387943e9d0d5d14
Antoine Martin <antoine@xpra.org>
2026-09-25 14:19:18 +0700

    libyuv-git is now just libyuv

--- packaging/MSWindows/SETUP.sh
+++ packaging/MSWindows/SETUP.sh
@@ -15,7 +15,7 @@ PACMAN=${PACMAN:-"pacman --noconfirm --needed -S"}
 #most packages get installed here: (python, gtk, etc):
 $PACMAN ${XPKG}python ${XPKG}libnotify ${XPKG}gtk3
 #media libraries (more than we actually need):
-$PACMAN ${XPKG}libavif ${XPKG}libyuv-git ${XPKG}gst-plugins-good ${XPKG}gst-plugins-bad ${XPKG}gst-plugins-ugly
+$PACMAN ${XPKG}libavif ${XPKG}libyuv ${XPKG}gst-plugins-good ${XPKG}gst-plugins-bad ${XPKG}gst-plugins-ugly
 #Intel oneVPL: HEVC 4:4:4 hardware decode on Intel GPUs:
 $PACMAN ${XPKG}libvpl
 #more codecs:
